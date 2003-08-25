fd0807266fbf39bdba8be9280b6060cca5cca551
John Hunter <jdh2358@gmail.com>
2003-08-25 14:35:02 +0000

    fixed python2.3 deprecation warnings
    svn path=/trunk/matplotlib/; revision=20

--- examples/simple_plot.py
+++ examples/simple_plot.py
@@ -2,7 +2,7 @@ from matplotlib.matlab import *
 
 t = arange(0.0, 2.0, 0.01)
 s = sin(2*pi*t)
-plot(t, s)
+plot(t, s, 'o')
 xlabel('time (s)')
 ylabel('voltage (mV)')
 title('About as simple as it gets, folks')
