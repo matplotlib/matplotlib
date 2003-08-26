d8d5ff599510f82069fe13f9ed5e4b02e7da6106
John Hunter <jdh2358@gmail.com>
2003-08-26 13:52:07 +0000

    added figtext example
    svn path=/trunk/matplotlib/; revision=21

---
+++ examples/figtext.py
@@ -0,0 +1,26 @@
+from matplotlib.matlab import *
+
+def f(t):
+    s1 = cos(2*pi*t)
+    e1 = exp(-t)
+    return multiply(s1,e1)
+
+t1 = arange(0.0, 5.0, 0.1)
+t2 = arange(0.0, 5.0, 0.02)
+t3 = arange(0.0, 2.0, 0.01)
+
+
+subplot(121)
+plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+title('subplot 1')
+ylabel('Damped oscillation')
+gcf().text(0.55, 0.95, 'Figure Title', fontsize=12)
+
+subplot(122)
+plot(t3, cos(2*pi*t3), 'r--')
+xlabel('time (s)')
+title('subplot 2')
+ylabel('Undamped')
+
+show()
+
