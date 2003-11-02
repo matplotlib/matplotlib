4919cc34b715d10ce65b646e31341f3b0eecbfa0
John Hunter <jdh2358@gmail.com>
2003-11-02 21:58:03 +0000

    moved pcolor to axes class
    svn path=/trunk/matplotlib/; revision=53

--- examples/pcolor_demo.py
+++ examples/pcolor_demo.py
@@ -7,6 +7,7 @@ def func3(x,y):
 
 # make these smaller to increase the resolution
 dx, dy = 0.05, 0.05
+
 x = arange(-3.0, 3.0, dx)
 y = arange(-3.0, 3.0, dy)
 X,Y = meshgrid(x, y)
@@ -14,7 +15,7 @@ X,Y = meshgrid(x, y)
 Z = func3(X, Y)
 pcolor(X, Y, Z, shading='flat')
 axis('off')
-#savefig('pcolor_demo', dpi=300)
+#savefig('pcolor_demo')
 show()
 
     
