d34100c218a3f467f9f3a4750eb3e4ecf8e6e908
John Hunter <jdh2358@gmail.com>
2003-11-02 19:43:30 +0000

    added ct demo
    svn path=/trunk/matplotlib/; revision=48

---
+++ examples/ct_demo.py
@@ -0,0 +1,13 @@
+from matplotlib.matlab import *
+
+# data are 256x256 16 bit integers
+dfile = 'data/s1045.ima'
+im = fromstring(file(dfile).read(), Int16).astype(Float)
+im.shape = 256, 256
+# flip upside down
+im = array([im[i] for i in arange(255,-1,-1)])
+
+pcolor(im, shading='flat')
+axis('off')
+savefig('ct_demo')
+show()
