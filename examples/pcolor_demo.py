e0e064f1272d47db059e76b3a6719fa9ea532e6d
John Hunter <jdh2358@gmail.com>
2003-11-02 19:17:17 +0000

    added pcolor
    svn path=/trunk/matplotlib/; revision=47

---
+++ examples/pcolor_demo.py
@@ -0,0 +1,20 @@
+from __future__ import division
+from matplotlib.matlab import *
+
+def func3(x,y):
+    return (1- x/2 + x**5 + y**3)*exp(-x**2-y**2)
+
+
+# make these smaller to increase the resolution
+dx, dy = 0.05, 0.05
+x = arange(-3.0, 3.0, dx)
+y = arange(-3.0, 3.0, dy)
+X,Y = meshgrid(x, y)
+
+Z = func3(X, Y)
+pcolor(X, Y, Z, shading='flat')
+axis('off')
+#savefig('pcolor_demo', dpi=300)
+show()
+
+    
