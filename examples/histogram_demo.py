d3db1bcd07482b640df606ceb5bd56fe430211dd
John Hunter <jdh2358@gmail.com>
2003-11-11 22:23:51 +0000

    refactored axes
    svn path=/trunk/matplotlib/; revision=58

--- examples/histogram_demo.py
+++ examples/histogram_demo.py
@@ -6,16 +6,13 @@ x = mu + sigma*randn(10000)
 # the histogram of the data
 n, bins, patches = hist(x, 50, normed=1)
 # add a 'best fit' line
-
-
 y = normpdf( bins, mu, sigma)
-#l = plot(bins, y, 'r--')
-#set(l, 'linewidth', 2)
-#set(gca(), 'xlim', [40, 160])  
+l = plot(bins, y, 'r--')
+set(l, 'linewidth', 2)
+set(gca(), 'xlim', [40, 160])  
 
 xlabel('Smarts')
 ylabel('Probability')
 title('Histogram of IQ: mu=100, sigma=15')
-
-savefig('histogram_demo', dpi=150)
+savefig('histogram_demo')
 show()
--- examples/text_themes.py
+++ examples/text_themes.py
@@ -15,9 +15,9 @@ t2 = arange(0.0, 5.0, 0.02)
 
 plot(t1, f(t1), 'bo', t2, f(t2), 'k')
 title('Damped exponential decay', font, fontsize=14, color='r')
-text(3, 0.65, 'cos(2 pi t) exp(-t)', font, color='k')
+text(2, 0.65, 'cos(2 pi t) exp(-t)', font, color='k')
 xlabel('time (s)', font)
 ylabel('voltage (mV)', font)
 
-savefig('text_themes', dpi=150)
+savefig('text_themes')
 show()
