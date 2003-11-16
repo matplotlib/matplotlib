b3e2a9d8efbb316b4779e0c1ab5a6cbb158e2800
John Hunter <jdh2358@gmail.com>
2003-11-16 17:11:29 +0000

    fixed scatter demo bug
    svn path=/trunk/matplotlib/; revision=66

--- examples/mri_demo.py
+++ examples/mri_demo.py
@@ -2,12 +2,12 @@ from matplotlib.matlab import *
 
 # data are 256x256 16 bit integers
 dfile = 'data/s1045.ima'
-im = fromstring(file(dfile).read(), Int16).astype(Float)
+im = fromstring(file(dfile, 'rb').read(), Int16).astype(Float)
 im.shape = 256, 256
 # flip upside down
 im = array([im[i] for i in arange(255,-1,-1)])
 
 pcolor(im, shading='flat')
 axis('off')
-savefig('mri_demo')
+#savefig('mri_demo')
 show()
