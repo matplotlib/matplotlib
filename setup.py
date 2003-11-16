b01b6fc41a384ec7542bee6f9ac8a50e30234c5b
John Hunter <jdh2358@gmail.com>
2003-11-16 20:23:52 +0000

    implemented new edge/face handling in backends
    svn path=/trunk/matplotlib/; revision=67

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ data.extend(glob.glob('fonts/ttf/*.ttf'))
 data.extend(glob.glob('images/*.xpm'))
 
 setup(name="matplotlib",
-      version= '0.40d',
+      version= '0.40e',
       description = "Matlab style python plotting package",
       author = "John D. Hunter",
       author_email="jdhunter@ace.bsd.uchicago.edu",
