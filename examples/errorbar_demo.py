6486244b063299fd95e599dbb722fdcfa09b47cf
John Hunter <jdh2358@gmail.com>
2003-11-20 15:15:44 +0000

    fixed errobar cap sizes
    svn path=/trunk/matplotlib/; revision=76

--- examples/errorbar_demo.py
+++ examples/errorbar_demo.py
@@ -2,11 +2,23 @@ from matplotlib.matlab import *
 
 t = arange(0.1, 4, 0.1)
 s = exp(-t)
-e = 0.1*randn(len(s))
+e = 0.1*abs(randn(len(s)))
+f = 0.1*abs(randn(len(s)))
+g = 2*e
+h = 2*f
 
-errorbar(t, s, e, fmt='o')
+#errorbar(t, s, e, fmt='o')             # vertical symmetric
+#errorbar(t, s, None, f, fmt='o')       # horizontal symmetric
+errorbar(t, s, e, f, fmt='o')          # both symmetric
+#errorbar(t, s, [e,g], [f,h], fmt='o')  # both asymmetric
+#errorbar(t, s, [e,g], f, fmt='o')      # both mixed
+#errorbar(t, s, e, [f,h], fmt='o')      # both mixed
+#errorbar(t, s, [e,g], fmt='o')         # vertical asymmetric
+#errorbar(t, s, yerr=e, fmt='o')        # named
+#errorbar(t, s, xerr=f, fmt='o')        # named
+
 xlabel('Distance (m)')
 ylabel('Height (m)')
-title('Mean an standard error as a function of distance')
+title('Mean and standard error as a function of distance')
+#savefig('errorbar_demo')
 show()
-
