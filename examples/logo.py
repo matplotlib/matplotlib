ea47c57cb882dfd66df8f2e135fe6ef4e3beed2c
John Hunter <jdh2358@gmail.com>
2003-11-20 20:59:42 +0000

    fixed some examples
    svn path=/trunk/matplotlib/; revision=78

--- examples/logo.py
+++ examples/logo.py
@@ -7,13 +7,16 @@ x = 1000*0.1*fromstring(
     file('data/membrane.dat', 'rb').read(), typecode=Float32)
 # 0.0005 is the sample interval
 t = 0.0005*arange(len(x))
-figure(1, size=(700,100))
-#subplot(111, axisbg='#afafaf')
-subplot(111, axisbg=(205,179,139))
-#subplot(111, axisbg='k')
+figure(1, figsize=(7,1), dpi=100)
+ax = subplot(111, axisbg='y')
 plot(t, x)
-text(1.1,-55,'matplotlib', color=(47,79,79), fontsize=50, fontname='Courier')
-#text(1.1,-55,'matplotlib', color='b', fontsize=50, fontname='Courier')
+text(0.5, 0.5,'matplotlib', color='r',
+     fontsize=40, fontname='Courier',
+     horizontalalignment='center',
+     verticalalignment='center',
+     transx = ax.xaxis.transAxis,
+     transy = ax.yaxis.transAxis,
+     )
 axis([1, 1.72,-60, 10])
 set(gca(), 'xticklabels', [])
 set(gca(), 'yticklabels', [])
--- examples/mpl_with_glade.py
+++ examples/mpl_with_glade.py
@@ -38,9 +38,9 @@ class WidgetsWrapper:
         self.widgets = gtk.glade.XML('mpl_with_glade.glade')
         self.widgets.signal_autoconnect(GladeHandlers.__dict__)
 
-        self.figure = Figure(figsize=(5,4), dpi=100)
+        self.figure = Figure(figsize=(8,6), dpi=72)
         self.figure.show()
-        self.axis = Subplot(111)
+        self.axis = Subplot(self.figure, 111)
         self.figure.add_axis(self.axis)
         t = arange(0.0,3.0,0.01)
         s = sin(2*pi*t)
