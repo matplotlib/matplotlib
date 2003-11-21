e6cb8f4a5fa5ac428fbef7c6959d9c5c002c01ae
John Hunter <jdh2358@gmail.com>
2003-11-21 18:06:33 +0000

    2 new examples
    svn path=/trunk/matplotlib/; revision=88

---
+++ examples/psd_demo.py
@@ -0,0 +1,38 @@
+"""
+Compute the power spectral density of a signal
+"""
+
+from matplotlib.matlab import *
+
+dt = 0.01
+t = arange(0,10,dt)
+nse = randn(len(t))                 # white noise
+r = exp(-t/0.05)
+cnse = convolve(nse, r, mode=2)*dt  # colored noise
+cnse = cnse[:len(t)]
+s = 0.1*sin(2*pi*t) + cnse
+figure(1)
+plot(t,s)
+
+figure(2)
+psd(s, 512, 1/dt)
+savefig('psd_demo')
+show()
+
+"""
+% compare with matlab
+dt = 0.01;
+t = [0:dt:10];
+s = sin(2*pi*t);
+nse = randn(size(t));
+r = exp(-t/0.05);
+cnse = conv(nse, r)*dt;
+cnse = cnse(1:length(t));
+s = 0.1*sin(2*pi*t) + cnse;
+figure(1)
+plot(t,s)
+
+figure(2)
+psd(s, 512, 1/dt)
+savefig('psd_demo')
+"""
