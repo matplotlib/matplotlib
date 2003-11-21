707b87288cde77a27fc76ec222a120e0134fe4aa
John Hunter <jdh2358@gmail.com>
2003-11-21 04:43:55 +0000

    added htdocs to repository
    svn path=/trunk/matplotlib/; revision=79

--- examples/data_helper.py
+++ examples/data_helper.py
@@ -1,7 +1,6 @@
 # Some functions to load a return data for the plot demos
 
-from Numeric import fromstring, argsort, take
-import Numeric as numpy
+from Numeric import fromstring, argsort, take, array
 def get_two_stock_data():
     """
     load stock time and price data for two stocks The return values
@@ -20,3 +19,32 @@ def get_two_stock_data():
     d2, p2 = M2[:,0], M2[:,1]
     return (d1,p1,d2,p2)
 
+
+def get_daily_data():
+    """
+    return stock1 and stock2 instances, each of which have attributes
+
+      open, high, low, close, volume
+
+    as numeric arrays
+    
+    """
+    class C: pass
+
+    def get_ticker(ticker):
+        vals = []
+        lines = file( 'data/%s.csv' % ticker ).readlines()
+        for line in lines[1:]:
+            vals.append([float(val) for val in line.split(',')[1:]])
+            
+        M = array(vals)
+        c = C()
+        c.open = M[:,0]
+        c.high = M[:,1]
+        c.low = M[:,2]
+        c.close = M[:,3]
+        c.volume = M[:,4]
+        return c
+    c1 = get_ticker('intc')
+    c2 = get_ticker('msft')
+    return c1, c2
