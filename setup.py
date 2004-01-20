f9944f5ad2c91999bbd0bd52176fc42ee608051f
John Hunter <jdh2358@gmail.com>
2004-01-20 15:21:29 +0000

    added axes bar patch
    svn path=/trunk/matplotlib/; revision=100

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ data.extend(glob.glob('fonts/ttf/*.ttf'))
 data.extend(glob.glob('images/*.xpm'))
 
 setup(name="matplotlib",
-      version= '0.41c',
+      version= '0.41e',
       description = "Matlab style python plotting package",
       author = "John D. Hunter",
       author_email="jdhunter@ace.bsd.uchicago.edu",
