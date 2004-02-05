f19c7d795291989c47619c3e752c6e6893146d65
John Hunter <jdh2358@gmail.com>
2004-02-05 15:42:19 +0000

    added paint backend
    svn path=/trunk/matplotlib/; revision=130

--- examples/simple_plot.py
+++ examples/simple_plot.py
@@ -1,12 +1,12 @@
 from matplotlib.matlab import *
 
-t = arange(0.0, 1.0, 0.002)
+t = arange(0.0, 1.0, 0.02)
 s = sin(2*2*pi*t)
-plot(t, s)
+plot(t, s, '+')
 
 xlabel('time (s)')
 ylabel('voltage (mV)')
 title('About as simple as it gets, folks')
 grid(True)
-savefig('painttest')
-
+savefig('simple_plot', dpi=300)
+show()
