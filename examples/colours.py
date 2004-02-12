5b4865bef88a8d3a43412f2d36ba8bf827d29c76
John Hunter <jdh2358@gmail.com>
2004-02-12 18:35:15 +0000

    added some htdocs files
    svn path=/trunk/matplotlib/; revision=142

---
+++ examples/colours.py
@@ -0,0 +1,50 @@
+"""
+Some simple functions to generate colours.
+"""
+from Numeric import asarray
+from matplotlib.mlab import linspace
+from matplotlib.backend_bases import arg_to_rgb
+
+def pastel(colour, weight=2.4):
+    """ Convert colour into a nice pastel shade"""
+    rgb = asarray(arg_to_rgb(colour))
+    # scale colour 
+    maxc = max(rgb)
+    if maxc < 1.0 and maxc > 0:
+        # scale colour
+        scale = 1.0 / maxc
+        rgb = rgb * scale
+    # now decrease saturation
+    total = sum(rgb)
+    slack = 0
+    for x in rgb:
+        slack += 1.0 - x
+
+    # want to increase weight from total to weight
+    # pick x s.t.  slack * x == weight - total
+    # x = (weight - total) / slack
+    x = (weight - total) / slack
+
+    rgb = [c + (x * (1.0-c)) for c in rgb]
+
+    return rgb
+
+def get_colours(n):
+    """ Return n pastel colours. """
+    base = asarray([[1,0,0], [0,1,0], [0,0,1]])
+
+    if n <= 3:
+        return base[0:n]
+
+    # how many new colours to we need to insert between
+    # red and green and between green and blue?
+    needed = (((n - 3) + 1) / 2, (n - 3) / 2)
+
+    colours = []
+    for start in (0, 1):
+        for x in linspace(0, 1, needed[start]+2):
+            colours.append((base[start] * (1.0 - x)) +
+                           (base[start+1] * x))
+
+    return [pastel(c) for c in colours[0:n]]
+
---
+++ examples/vertical_ticklabels.py
@@ -0,0 +1,7 @@
+from matplotlib.matlab import *
+
+plot([1,2,3,4], [1,4,9,16])
+set(gca(), 'xticks', [1,2,3,4])
+t = set(gca(), 'xticklabels', ['Frogs', 'Hogs', 'Bogs', 'Slogs'])
+set(t, 'rotation', 'vertical')
+show()
