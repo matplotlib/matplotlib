0ede385eae37482564bd53fd0adcbcc211c5dc6d
John Hunter <jdh2358@gmail.com>
2004-02-27 14:47:49 +0000

    added matplotlibrc and tk win32 compile fixes
    svn path=/trunk/matplotlib/; revision=164

--- examples/mathtext_demo.py
+++ examples/mathtext_demo.py
@@ -6,5 +6,5 @@ ylabel(r'$\Delta_{i+1}$')
 tex = r'$\cal{R}\prod_{i=\alpha_{i+1}}^\infty a_i\rm{sin}(2 \pi f x_i)$'
 text(0.4, 2.6, tex, fontsize=20)
 title(r'$\Delta_i \rm{versus} \Delta_{i+1}$', fontsize=15)
-#savefig('mathtext_demo', dpi=100)
+savefig('mathtext_demo', dpi=100)
 show()
