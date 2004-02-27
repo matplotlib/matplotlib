b6f8ddbfe5a172884b176426a1d68db93b739dec
John Hunter <jdh2358@gmail.com>
2004-02-27 21:46:31 +0000

    close to porting mathtext to gtk
    svn path=/trunk/matplotlib/; revision=167

--- examples/mathtext_demo.py
+++ examples/mathtext_demo.py
@@ -1,10 +1,10 @@
 from matplotlib.matlab import *
-
+subplot(111, axisbg='y')
 plot([1,2,3])
 xlabel(r'$\Delta_i$')
 ylabel(r'$\Delta_{i+1}$')
 tex = r'$\cal{R}\prod_{i=\alpha_{i+1}}^\infty a_i\rm{sin}(2 \pi f x_i)$'
-text(0.4, 2.6, tex, fontsize=20)
+text(1, 2.6, tex, fontsize=20, color='r')
 title(r'$\Delta_i \rm{versus} \Delta_{i+1}$', fontsize=15)
 savefig('mathtext_demo', dpi=100)
 show()
