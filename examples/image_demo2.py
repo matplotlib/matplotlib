85d9a954734e80312d3ecd6e36e025b22058e044
John Hunter <jdh2358@gmail.com>
2004-03-15 15:54:53 +0000

    added 2nd image demo
    svn path=/trunk/matplotlib/; revision=181

---
+++ examples/image_demo2.py
@@ -0,0 +1,32 @@
+from matplotlib.matlab import *
+
+w, h = 512, 512
+s = file('data/ct.raw', 'rb').read()
+A = fromstring(s, typecode=UInt16).astype(Float)
+A *= 1.0/max(A)
+A.shape = w, h
+
+im = imshow(A)
+im.set_aspect('free')
+
+# plot some data with the image; currently broken with aspect preserve
+gca().set_image_extent(0, 25, 0, 25)
+markers = [(15.9, 14.5), (16.8, 15)]
+x,y = zip(*markers)
+plot(x, y, 'o')
+#axis([0,25,0,25])
+
+
+
+#axis('off')
+title('CT density')
+
+if 0:
+    x = sum(A,0)
+    subplot(212)
+    bar(arange(w), x)
+    set(gca(), 'xlim', [0,h-1])
+    ylabel('density')
+    set(gca(), 'xticklabels', [])
+show()
+
