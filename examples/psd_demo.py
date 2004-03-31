7394dbab0153f413bafda68e51847a7145f10a43
John Hunter <jdh2358@gmail.com>
2004-03-31 15:41:11 +0000

    added a couple of examples; fixed a gtk clip bug
    svn path=/trunk/matplotlib/; revision=217

--- examples/psd_demo.py
+++ examples/psd_demo.py
@@ -1,23 +1,25 @@
-"""
-Compute the power spectral density of a signal
-"""
+# python
 
 from matplotlib.matlab import *
 
 dt = 0.01
 t = arange(0,10,dt)
-nse = randn(len(t))                 # white noise
+nse = randn(len(t))
 r = exp(-t/0.05)
-cnse = convolve(nse, r, mode=2)*dt  # colored noise
+
+cnse = convolve(nse, r, mode=2)*dt
 cnse = cnse[:len(t)]
 s = 0.1*sin(2*pi*t) + cnse
+
 figure(1)
 plot(t,s)
 
 figure(2)
 psd(s, 512, 1/dt)
-#savefig('psd_demo')
-show()
+
+savefig('psd_demo.png')
+
+
 
 """
 % compare with matlab
