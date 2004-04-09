0181e4cedf910bdd6a458d8a6ba37a0d038f36dc
John Hunter <jdh2358@gmail.com>
2004-04-09 14:14:54 +0000

    added font props demo
    svn path=/trunk/matplotlib/; revision=235

---
+++ examples/font_properties_demo.py
@@ -0,0 +1,20 @@
+from matplotlib.font_manager import set_default_font, get_default_font
+from matplotlib.matlab import *
+
+font = get_default_font()
+font.set_family('serif')  # this operates on the default font!
+
+otherFont = font.copy()   # this is an independent font
+otherFont.set_family('monospace')
+otherFont.set_weight('bold')
+otherFont.set_size('larger')
+
+plot(arange(20))
+title('something')
+
+set_default_font(otherFont)
+for i in range(1,15,2):
+    text(i, i, 'label %d'%i, color='g')     # uses otherFont
+set_default_font(font)                     # restore default
+xlabel('hi mom')                           # uses font
+show()
