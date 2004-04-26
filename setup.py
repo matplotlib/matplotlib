41d618099e5da19f5ff13eb80074a548c74c87db
John Hunter <jdh2358@gmail.com>
2004-04-26 17:44:08 +0000

    sync to 0.53.1
    svn path=/trunk/matplotlib/; revision=256

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ if BUILD_IMAGE:
     build_image(ext_modules, packages, BUILD_IMAGE)
 
 setup(name="matplotlib",
-      version= '0.53.1a',
+      version= '0.53.1',
       description = "Matlab style python plotting package",
       author = "John D. Hunter",
       author_email="jdhunter@ace.bsd.uchicago.edu",
