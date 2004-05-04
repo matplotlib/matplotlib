5a5d851f1518728df60fcc75d1584e84e574c1e8
John Hunter <jdh2358@gmail.com>
2004-05-04 18:37:44 +0000

    major overhaul of text
    svn path=/trunk/matplotlib/; revision=278

---
+++ examples/dash_control.py
@@ -0,0 +1,12 @@
+"""
+You can precisely specify dashes with an on/off ink rect sequence in
+points.
+"""
+from matplotlib.matlab import *
+
+dashes = [5,2,10,5] # 5 points on, 2 off, 3 on, 1 off
+
+l, = plot(arange(20), '--')
+l.set_dashes(dashes)
+#savefig('dash_control')
+show()
