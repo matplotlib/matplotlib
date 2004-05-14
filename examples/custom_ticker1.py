15c489cbdc4d00a63a7e6c7ef36e34cd1a1eea4d
John Hunter <jdh2358@gmail.com>
2004-05-14 18:39:24 +0000

    added new examples
    svn path=/trunk/matplotlib/; revision=293

---
+++ examples/custom_ticker1.py
@@ -0,0 +1,28 @@
+"""
+The new ticker code was designed to explicity support user customized
+ticking.  The documentation
+http://matplotlib.sourceforge.net/matplotlib.ticker.html details this
+process.  That code defines a lot of preset tickers but was primarily
+designed to be user extensible.
+
+In this example a user defined function is used to format the ticks in
+millions of dollars on the y axis
+"""
+from matplotlib.ticker import FuncFormatter
+from matplotlib.matlab import *
+
+x =     arange(4)
+money = [1.5e5, 2.5e6, 5.5e6, 2.0e7]
+
+def millions(x, pos):
+    'The two args are the value and tick position'
+    return '$%1.1fM' % (x*1e-6)
+
+formatter = FuncFormatter(millions)
+
+ax = subplot(111)
+ax.yaxis.set_major_formatter(formatter)
+bar(x, money)
+ax.set_xticks( x + 0.5)
+ax.set_xticklabels( ('Bill', 'Fred', 'Mary', 'Sue') )
+show()
---
+++ examples/stem_plot.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from matplotlib.matlab import *
+
+x = linspace(0.1, 2*pi, 10)
+markerline, stemlines, baseline = stem(x, cos(x), '-.')
+set(markerline, 'markerfacecolor', 'b')
+set(baseline, 'color','r', 'linewidth', 2) 
+
+show()
