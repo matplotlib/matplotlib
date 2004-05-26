55d308146d200598e23cc088a139f47b774fd3de
John Hunter <jdh2358@gmail.com>
2004-05-26 22:19:27 +0000

    gtkagg fixes
    svn path=/trunk/matplotlib/; revision=318

--- src/_gtkagg.cpp
+++ src/_gtkagg.cpp
@@ -1,87 +1,74 @@
-/* Helper functions for converting an agg image buffer to a gtk drawable
-   efficiently
-*/
-#include <Python.h>
-#include <pygobject.h>
-#include <pygtk/pygtk.h>
 #include <cstring>
 #include <cerrno>
 #include <cstdio>
+#include <iostream>
+#include <cmath>
+#include <utility>
+
+#include <pygobject.h>
+#include <pygtk/pygtk.h>
 
 #include "_backend_agg.h"
 
-static PyObject *ErrorObject;
-static PyTypeObject *PyGObject_Type=NULL;    
-
-
-static PyObject *
-_agg_to_gtk_drawable(PyObject *self, PyObject *args) {
-
-  PyGObject *py_drawable = NULL;                         
-  GdkDrawable *drawable = NULL;   
-  GdkGC* gc = NULL;
+// the extension module
+class _gtkagg_module : public Py::ExtensionModule<_gtkagg_module>
+{
+public:
+  _gtkagg_module()
+    : Py::ExtensionModule<_gtkagg_module>( "_gtkagg" )
+  {
+    add_varargs_method("agg_to_gtk_drawable", 
+		       &_gtkagg_module::agg_to_gtk_drawable, 
+		       "Draw to a gtk drawable from a agg buffer.");
+    initialize( "The _gtkagg module" );
+  }
   
-  PyObject* aggo;
-
-
-  if (!PyArg_ParseTuple(args, "O!O", PyGObject_Type, 
-			&py_drawable, &aggo))
-      return NULL;
-
-
-  RendererAgg* aggRenderer = (RendererAgg*)aggo;
-
-  drawable = GDK_DRAWABLE(py_drawable->obj);
-  gc = gdk_gc_new(drawable);
-
-  unsigned int width = aggRenderer->get_width();
-  unsigned int height = aggRenderer->get_height();
-
-  gdk_draw_rgb_32_image(drawable, gc, 0, 0, 
-			width, 
-			height, 
-			GDK_RGB_DITHER_NORMAL,
-			aggRenderer->pixBuffer,
-			width*4);
-
-  Py_INCREF(Py_None);
-  return Py_None;
-}
+  virtual ~_gtkagg_module() {}
   
+private:
+  
+  Py::Object agg_to_gtk_drawable(const Py::Tuple &args) {
 
+    args.verify_length(2);  
 
+  
+    PyGObject *py_drawable = (PyGObject *)(args[0].ptr());
+    RendererAgg* aggRenderer = static_cast<RendererAgg*>(args[1].ptr());
 
+    
+    GdkDrawable *drawable = GDK_DRAWABLE(py_drawable->obj);
+    GdkGC* gc = gdk_gc_new(drawable);
 
-static struct PyMethodDef _gtkagg_methods[] = {
-  {"agg_to_gtk_drawable", (PyCFunction)_agg_to_gtk_drawable, METH_VARARGS, 
-   "Draw to a gtk drawable from a agg buffer."},
-  {NULL,		NULL}		/* sentinel */
+    unsigned int width = aggRenderer->get_width();
+    unsigned int height = aggRenderer->get_height();
+
+    gdk_draw_rgb_32_image(drawable, gc, 0, 0, 
+			  width, 
+			  height, 
+			  GDK_RGB_DITHER_NORMAL,
+			  aggRenderer->pixBuffer,
+			  width*4);
+
+    return Py::Object();
+
+  }
 };
 
 
 extern "C"
-DL_EXPORT(void) init_gtkagg(void)
+DL_EXPORT(void)
+  init_gtkagg(void)
 {
-  PyObject *module, *d;
-  
-
-
   init_pygobject();
   init_pygtk();
-  /* Create the module and add the functions */
-  Py_InitModule("_gtkagg", _gtkagg_methods);  
-  module = PyImport_ImportModule("gobject");
-  if (module) {
-    PyGObject_Type =
-      (PyTypeObject*)PyObject_GetAttrString(module, "GObject");
-    Py_DECREF(module);
-  }
-  /* Add some symbolic constants to the module */
-  d = PyModule_GetDict(module);
-  ErrorObject = PyString_FromString("_gtkagg.error");
-  PyDict_SetItemString(d, "error", ErrorObject);
-  
-  /* Check for errors */
-  if (PyErr_Occurred())
-    Py_FatalError("can't initialize module _gtkagg");
-}
+  //suppress unused warning by creating in two lines
+  static _gtkagg_module* _gtkagg = NULL; 
+  _gtkagg = new _gtkagg_module;
+
+};
+
+
+
+
+
+
