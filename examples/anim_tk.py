ebeffc666551f5384f27f27ea628adb2f206006e
John Hunter <jdh2358@gmail.com>
2004-05-26 17:22:03 +0000

    added examples/anim_tk.py
    svn path=/trunk/matplotlib/; revision=317

---
+++ examples/anim_tk.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2.3
+
+import matplotlib.matlab
+import Tkinter as Tk
+import matplotlib.numerix as numerix
+
+fig = matplotlib.matlab.figure(1)
+ind = numerix.arange(60)
+x_tmp=[]
+for i in range(100):
+    x_tmp.append(numerix.sin((ind+i)*numerix.pi/15.0))
+
+X=numerix.array(x_tmp)
+lines = matplotlib.matlab.plot(X[:,0],'o')
+
+manager = matplotlib.matlab.get_current_fig_manager()
+def updatefig(*args):
+    updatefig.count += 1
+    lines[0].set_ydata(X[:,updatefig.count%60])
+    manager.canvas.draw()
+    return updatefig.count
+updatefig.count=-1
+
+
+manager.show()
+import time
+tstart = time.time()
+while 1:
+    cnt = updatefig()
+    if cnt==100: break
+    
+
+
+
+print 'elapsed', 100.0/(time.time() - tstart)
