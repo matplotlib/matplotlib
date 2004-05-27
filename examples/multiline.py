8dd8e7fdf1cd277c7a5b4c1a8d46b5cd46859483
John Hunter <jdh2358@gmail.com>
2004-05-27 21:14:22 +0000

    added some missing files
    svn path=/trunk/matplotlib/; revision=320

---
+++ examples/multiline.py
@@ -0,0 +1,13 @@
+from matplotlib.matlab import *
+plot(arange(10))
+xlabel('this is a xlabel\n(with newlines!)')
+ylabel('this is vertical\ntest', multialignment='center')
+#ylabel('this is another!')
+text(2, 7,'this is\nyet another test',
+     rotation=45,
+     horizontalalignment = 'center',
+     verticalalignment   = 'top',     
+     multialignment      = 'center')
+#savefig('multiline')
+grid(True)
+show()
