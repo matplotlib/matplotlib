1363066a46f04ba4b857b87f012aba1344a09706
John Hunter <jdh2358@gmail.com>
2004-06-07 02:39:35 +0000

    uptodate?
    svn path=/trunk/matplotlib/; revision=329

--- examples/legend_demo2.py
+++ examples/legend_demo2.py
@@ -5,10 +5,12 @@ from matplotlib.matlab import *
 t1 = arange(0.0, 2.0, 0.1)
 t2 = arange(0.0, 2.0, 0.01)
 
-l1 = plot(t2, exp(-t2))
+# note that plot returns a list of lines.  The "l1, = plot" usage
+# extracts the first element of the list inot l1 using tuple
+# unpacking.  So l1 is a Line2D instance, not a sequence of lines
+l1,    = plot(t2, exp(-t2))
 l2, l3 = plot(t2, sin(2*pi*t2), '--go', t1, log(1+t1), '.')
-l4 = plot(t2, exp(-t2)*sin(2*pi*t2), 'rs-.')
-
+l4,    = plot(t2, exp(-t2)*sin(2*pi*t2), 'rs-.')
 
 legend( (l2, l4), ('oscillatory', 'damped'), 'upper right')
 xlabel('time')
