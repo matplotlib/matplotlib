6bb54d94ef66f6e8441b8116d2197ebeb55a17a0
Todd Miller <jmiller@stsci.edu>
2004-06-11 22:09:00 +0000

    Fixed typo in MPLIB_BASE for darwin.
    svn path=/trunk/matplotlib/; revision=341

--- setupext.py
+++ setupext.py
@@ -35,7 +35,7 @@ basedir = {
     'win32'  : ['win32_static',],
     'linux2' : ['/usr/local', '/usr',],
     'linux'  : ['/usr/local', '/usr',],
-    'darwin' : [os.getenv('MBLIB_BASE') or '/usr/local', '/usr', '/sw'],
+    'darwin' : [os.getenv('MPLIB_BASE') or '/usr/local', '/usr', '/sw'],
     'sunos5' : [os.getenv('MPLIB_BASE') or '/usr/local',],
 }
 
