b8883a7572a7000a4511a837b3d5325b7ac42fe2
John Hunter <jdh2358@gmail.com>
2004-06-16 14:55:28 +0000

    removed gtkgd
    svn path=/trunk/matplotlib/; revision=346

--- examples/font_properties_demo.py
+++ examples/font_properties_demo.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
-from matplotlib.font_manager import set_default_font, get_default_font
 from matplotlib.matlab import *
 
-font = get_default_font()
-font.set_family('serif')  # this operates on the default font!
-
-otherFont = font.copy()   # this is an independent font
-otherFont.set_family('monospace')
-otherFont.set_weight('bold')
-otherFont.set_size('larger')
+font = {'family' : 'monospace',
+        'weight' : 'bold',
+        'size'   : 'larger',
+        }
 
 plot(arange(20))
 title('something')
 
-set_default_font(otherFont)
+rc('font', **font)  # pass in the font dict as kwargs
 for i in range(1,15,2):
-    text(i, i, 'label %d'%i, color='g')     # uses otherFont
-set_default_font(font)                     # restore default
-xlabel('hi mom')                           # uses font
+    text(i, i, 'label %d'%i, color='g')     # uses font
+
+rcdefaults() # restore default
+xlabel('hi mom')              
 show()
