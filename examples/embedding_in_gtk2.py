55b39fffa34d340c75eb874c35dd843877b2e29e
Steve Chaplin <stevech1097@yahoo.com.au>
2004-06-20 12:20:08 +0000

    SC 20/06/2004
    svn path=/trunk/matplotlib/; revision=352

--- examples/embedding_in_gtk2.py
+++ examples/embedding_in_gtk2.py
@@ -10,13 +10,13 @@ from matplotlib.figure import Figure
 import gtk
 
 win = gtk.Window()
-win.set_name("Embedding in GTK")
+win.set_title("Embedding in GTK")
 win.connect("destroy", gtk.mainquit)
 win.set_border_width(5)
 
 vbox = gtk.VBox(spacing=3)
-win.add(vbox)
 vbox.show()
+win.add(vbox)
 
 fig = Figure(figsize=(5,4), dpi=100)
 ax = fig.add_subplot(111)
@@ -30,18 +30,17 @@ canvas = FigureCanvasGTK(fig)  # a gtk.DrawingArea
 canvas.show()
 vbox.pack_start(canvas)
 
-
 toolbar = NavigationToolbar(canvas, win)
 toolbar.show()
 vbox.pack_start(toolbar, gtk.FALSE, gtk.FALSE)
 
-buttonQuit = gtk.Button('Quit')
-buttonQuit.connect('clicked', gtk.mainquit)
-buttonQuit.show()
-vbox.pack_start(buttonQuit)
-
+#buttonQuit = gtk.Button('Quit')
+#buttonQuit.connect('clicked', gtk.mainquit)
+#buttonQuit.show()
+#vbox.pack_start(buttonQuit)
 
 
 if __name__=='__main__':
     win.show()
-    gtk.mainloop()
+#    gtk.mainloop()
+    gtk.main()
