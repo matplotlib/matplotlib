2361407d79709e890228de2357cb71ebd20d40b7
John Hunter <jdh2358@gmail.com>
2004-06-22 16:55:44 +0000

    added layer_images example
    svn path=/trunk/matplotlib/; revision=355

---
+++ examples/layer_images.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+See pcolor_demo2 for a much faster way of generating pcolor plots
+"""
+from __future__ import division
+from matplotlib.matlab import *
+
+def func3(x,y):
+    return (1- x/2 + x**5 + y**3)*exp(-x**2-y**2)
+
+
+# make these smaller to increase the resolution
+dx, dy = 0.05, 0.05
+
+x = arange(-3.0, 3.0, dx)
+y = arange(-3.0, 3.0, dy)
+X,Y = meshgrid(x, y)
+
+Z1 = rand(10,6)
+im1 = imshow(Z1, cmap = Grayscale(256))
+im1.set_interpolation('nearest')
+hold(True)
+
+Z2 = func3(X, Y)
+im2 = imshow(Z2, cmap=ColormapJet(256), alpha=0.75)
+im2.set_interpolation('nearest')
+
+show()
+
+    
