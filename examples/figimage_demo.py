29b36ca31630d991165ef1286e13c8742a8c8930
John Hunter <jdh2358@gmail.com>
2004-06-25 14:20:46 +0000

    updated fonts_demo and added kw version
    svn path=/trunk/matplotlib/; revision=364

--- examples/figimage_demo.py
+++ examples/figimage_demo.py
@@ -8,12 +8,12 @@ rc('axes', hold=True)
 rc('image', origin='lower')
 figure(1, frameon=False)
 Z = arange(10000.0); Z.shape = 100,100
-Z[:,20:] = 1
+Z[:,50:] = 1
 jet() # sets the default
-im1 = figimage(Z, xo=50,  yo=50)  # you can also pass cmap=cm.jet as kwarg
+im1 = figimage(Z, xo=50,  yo=0)  # you can also pass cmap=cm.jet as kwarg
 im2 = figimage(Z, xo=100, yo=100, alpha=.8)
-gray()  # overrides current and sets default
-#savefig('figimage_demo')
+#gray()  # overrides current and sets default
+savefig('figimage_demo')
 
 show()
 
--- examples/fonts_demo.py
+++ examples/fonts_demo.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from matplotlib.font_manager import fontManager, get_default_font, set_default_font
+from matplotlib.font_manager import fontManager, FontProperties
 from matplotlib.matlab import *
 
 subplot(111, axisbg='w')
 
-font0 = get_default_font()
-
+font0 = FontProperties()
+alignment = {'horizontalalignment':'center', 'verticalalignment':'center'}
 ###  Show family options
 
 family = ['serif', 'sans-serif', 'cursive', 'fantasy', 'monospace']
@@ -15,7 +15,7 @@ font1 = font0.copy()
 font1.set_size('large')
 
 t = text(-0.8, 0.9, 'family', fontproperties=font1,
-         horizontalalignment='center', verticalalignment='center')
+         **alignment)
 
 yp = [0.7, 0.5, 0.3, 0.1, -0.1, -0.3, -0.5]
 
@@ -25,48 +25,48 @@ for k in range(5):
     if k == 2:
         font.set_name('Script MT')
     t = text(-0.8, yp[k], family[k], fontproperties=font,
-             horizontalalignment='center', verticalalignment='center')
+             **alignment)
 
 ###  Show style options
 
 style  = ['normal', 'italic', 'oblique']
 
 t = text(-0.4, 0.9, 'style', fontproperties=font1,
-         horizontalalignment='center', verticalalignment='center')
+         **alignment)
 
 for k in range(3):
     font = font0.copy()
     font.set_family('sans-serif')
     font.set_style(style[k])
     t = text(-0.4, yp[k], style[k], fontproperties=font,
-             horizontalalignment='center', verticalalignment='center')
+             **alignment)
 
 ###  Show variant options
 
 variant= ['normal', 'small-caps']
 
-t = text(0.0, 0.9, 'variant', fontproperties=font1,
-         horizontalalignment='center', verticalalignment='center')
+t = text(0.2, 0.9, 'variant', fontproperties=font1,
+         **alignment)
 
 for k in range(1):
     font = font0.copy()
     font.set_family('serif')
     font.set_variant(variant[k])
-    t = text( 0.0, yp[k], variant[k], fontproperties=font,
-             horizontalalignment='center', verticalalignment='center')
+    t = text( 0.2, yp[k], variant[k], fontproperties=font,
+             **alignment)
 
 ###  Show weight options
 
 weight = ['light', 'normal', 'medium', 'semibold', 'bold', 'heavy', 'black']
 
 t = text( 0.4, 0.9, 'weight', fontproperties=font1,
-         horizontalalignment='center', verticalalignment='center')
+         **alignment)
 
 for k in range(7):
     font = font0.copy()
     font.set_weight(weight[k])
     t = text( 0.4, yp[k], weight[k], fontproperties=font,
-             horizontalalignment='center', verticalalignment='center')
+             **alignment)
 
 ###  Show size options
 
@@ -74,35 +74,35 @@ size  = ['xx-small', 'x-small', 'small', 'medium', 'large',
          'x-large', 'xx-large']
 
 t = text( 0.8, 0.9, 'size', fontproperties=font1,
-         horizontalalignment='center', verticalalignment='center')
+         **alignment)
 
 for k in range(7):
     font = font0.copy()
     font.set_size(size[k])
     t = text( 0.8, yp[k], size[k], fontproperties=font,
-             horizontalalignment='center', verticalalignment='center')
+             **alignment)
 
 ###  Show bold italic
-
+left = 0.1
 font = font0.copy()
 font.set_style('italic')
 font.set_weight('bold')
 font.set_size('x-small')
-t = text(-0.2, 0.1, 'bold italic', fontproperties=font,
-         horizontalalignment='center', verticalalignment='center')
+t = text(left, 0.1, 'bold italic', fontproperties=font,
+         **alignment)
 
 font = font0.copy()
 font.set_style('italic')
 font.set_weight('bold')
 font.set_size('medium')
-t = text(-0.2, -0.1, 'bold italic', fontproperties=font,
-         horizontalalignment='center', verticalalignment='center')
+t = text(left, 0.2, 'bold italic', fontproperties=font,
+         **alignment)
 
 font = font0.copy()
 font.set_style('italic')
 font.set_weight('bold')
 font.set_size('x-large')
-t = text(-0.2, -0.3, 'bold italic', fontproperties=font,
-         horizontalalignment='center', verticalalignment='center')
+t = text(left, 0.3, 'bold italic', fontproperties=font,
+         **alignment)
 
 show()
