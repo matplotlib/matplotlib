829dfcf57c05b36588f5906c1f6f2293e3e6dad1
Steve Chaplin <stevech1097@yahoo.com.au>
2004-06-26 13:55:56 +0000

    SC 26/06/2004
    svn path=/trunk/matplotlib/; revision=372

--- examples/mri_demo.py
+++ examples/mri_demo.py
@@ -6,7 +6,8 @@ dfile = 'data/s1045.ima'
 im = fromstring(file(dfile, 'rb').read(), Int16).astype(Float)
 im.shape = 256, 256
 
-imshow(im, ColormapJet(256))
+#imshow(im, ColormapJet(256))
+imshow(im, cmap=cm.jet)
 axis('off')
 #savefig('mri_demo')
 show()
