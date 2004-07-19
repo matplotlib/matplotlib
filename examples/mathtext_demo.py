485af836d2893b77dd42389db09144c52f64cc1d
John Hunter <jdh2358@gmail.com>
2004-07-19 19:19:10 +0000

    added vertical mathtext for agg
    svn path=/trunk/matplotlib/; revision=406

--- examples/mathtext_demo.py
+++ examples/mathtext_demo.py
@@ -16,17 +16,13 @@ x = arange(0.0, 3.0, 0.1)
 #plot(x, sin(2*pi*x))
 grid(True)
 xlabel(r'$\Delta_i^j$', fontsize=20)
-ylabel(r'$\Delta_{i+1}^j$', fontsize=20,  # no rotation yet for mathtext
-       verticalalignment='center',
-       horizontalalignment='right',
-       rotation='horizontal')
+ylabel(r'$\Delta_{i+1}^j$', fontsize=20)
 tex = r'$\cal{R}\prod_{i=\alpha_{i+1}}^\infty a_i\rm{sin}(2 \pi f x_i)$'
 #tex = r'$\cal{R}\prod^\infty a_i\rm{sin}(2 \pi f x_i)$'
 
 #tex = r'$\alpha\beta\gamma$'
 text(1, 2.6, tex, fontsize=20)
 title(r'$\Delta_i^j \hspace{0.4} \rm{versus} \hspace{0.4} \Delta_{i+1}^j$', fontsize=20)
-#savefig('mathtext_demo_small', dpi=100)
-savefig('mathtext_demo_large', dpi=300)
+savefig('mathtext_demo')
 
 show()
