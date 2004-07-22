fa0ff700fb1f559591840c74b36a0786551a6411
John Hunter <jdh2358@gmail.com>
2004-07-22 14:13:27 +0000

    more memory leak fixes
    svn path=/trunk/matplotlib/; revision=423

--- unit/ft2font_memleak.py
+++ unit/ft2font_memleak.py
@@ -1,6 +1,7 @@
 import sys, time, os
 from matplotlib.numerix import rand
 from matplotlib.ft2font import FT2Font
+from matplotlib.backends.backend_ps import encodeTTFasPS
 
 fname = '/usr/local/share/matplotlib/Vera.ttf'
 
@@ -17,10 +18,13 @@ for i in range(N):
     font.clear()
     font.set_text('hi mom', 60)
     font.set_size(12, 72)
+
     #glyph = font.load_char(int(140*rand()))
     font.draw_glyphs_to_bitmap()
     #font.draw_glyph_to_bitmap(0, 0, glyph)
 
+
+    #s = encodeTTFasPS(fname)
     val = report_memory(i)
     if i==1: start = val
 
