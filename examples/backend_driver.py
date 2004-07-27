71853604932a27f3f277218e136f0944cb1e8eb8
John Hunter <jdh2358@gmail.com>
2004-07-27 21:43:39 +0000

    fixed image bug in svg
    svn path=/trunk/matplotlib/; revision=452

--- examples/backend_driver.py
+++ examples/backend_driver.py
@@ -89,8 +89,8 @@ def drive(backend):
 times = {}
 backends = ['PS', 'GD', 'Paint', 'Agg', 'Template']
 #backends.extend([ 'GTK', 'WX', 'TkAgg'])
-backends = [ 'Agg']
-backends = [ 'SVG', 'PS', 'Agg', 'Template']
+backends = [ 'SVG']
+#backends = [ 'SVG', 'PS', 'Agg', 'Template']
 
 for backend in backends:
     print 'testing %s' % backend
