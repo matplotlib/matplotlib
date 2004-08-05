c12cc8a1050033f7cf2bd236162f4b5861600d3b
John Hunter <jdh2358@gmail.com>
2004-08-05 16:04:48 +0000

    some wx and gtk backend fixes
    svn path=/trunk/matplotlib/; revision=474

--- examples/scatter_demo2.py
+++ examples/scatter_demo2.py
@@ -11,13 +11,13 @@ volume = (15*intc.volume[:-2]/intc.volume[0])**2
 close = 0.003*intc.close[:-2]/0.003*intc.open[:-2]
 p = scatter(delta1[:-1], delta1[1:], c=close, s=volume)
 set(p, 'alpha', 0.75)
-set(gca(), 'xticks', arange(-0.06, 0.061, 0.02))
-set(gca(), 'yticks', arange(-0.06, 0.061, 0.02))
+#set(gca(), 'xticks', arange(-0.06, 0.061, 0.02))
+#set(gca(), 'yticks', arange(-0.06, 0.061, 0.02))
 xlabel(r'$\Delta_i$', size='x-large')
 ylabel(r'$\Delta_{i+1}$', size='x-large')
 title(r'Volume and percent change')
 grid(True)
-savefig('scatter_demo2')
+#savefig('scatter_demo2')
 
 show()
 
