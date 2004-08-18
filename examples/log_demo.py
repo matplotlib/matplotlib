e2818825a9020b1960c270dd0611ebb62688bbe2
Grégory Lielens <gregory.lielens@fft.be>
2004-08-18 15:36:19 +0000

    added base and subs properties for logscaling this is user-definable using set_xscale and set_yscale methods example : set_xscale('log') -> base 10, subticks at range(2,10)*10**i           set_xscale('log',8) -> base 8, subticks at range(2,8)*8**i           set_xscale('log',subs=[2,5]) -> base 10, subticks only on [2,5]*10**i
    svn path=/trunk/matplotlib/; revision=501

--- examples/log_demo.py
+++ examples/log_demo.py
@@ -6,12 +6,17 @@ t = arange(dt, 20.0, dt)
 
 subplot(311)
 semilogy(t, exp(-t/5.0))
+title('semilogy')
 
 subplot(312)
 semilogx(t, sin(2*pi*t))
+title('semilogx')
 
 subplot(313)
-loglog(t, exp(-t/10.0))
+loglog(t, 20*exp(-t/10.0))
+gca().set_xscale('log',base=4)
+gca().set_yscale('log',base=8,subs=[2,4,6])
 grid(True)
+title('loglog with custom base and subs logscaling')
 #savefig('log_demo')
 show()
