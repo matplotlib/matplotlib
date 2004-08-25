4a2df761461f51c80dc6c5ebd697ccadb2899a89
John Hunter <jdh2358@gmail.com>
2004-08-25 21:08:04 +0000

    updated pyparsing module
    svn path=/trunk/matplotlib/; revision=511

--- examples/mathtext_demo.py
+++ examples/mathtext_demo.py
@@ -13,16 +13,13 @@ from matplotlib.matlab import *
 subplot(111, axisbg='y')
 plot([1,2,3])
 x = arange(0.0, 3.0, 0.1)
-#plot(x, sin(2*pi*x))
+
 grid(True)
 xlabel(r'$\Delta_i^j$', fontsize=20)
 ylabel(r'$\Delta_{i+1}^j$', fontsize=20)
 tex = r'$\cal{R}\prod_{i=\alpha_{i+1}}^\infty a_i\rm{sin}(2 \pi f x_i)$'
-#tex = r'$\cal{R}\prod^\infty a_i\rm{sin}(2 \pi f x_i)$'
-
-#tex = r'$\alpha\beta\gamma$'
 text(1, 1.6, tex, fontsize=20)
 title(r'$\Delta_i^j \hspace{0.4} \rm{versus} \hspace{0.4} \Delta_{i+1}^j$', fontsize=20)
-#savefig('mathtext_demo')
+savefig('mathtext_demo')
 
 show()
