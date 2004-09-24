ed831f771aa25e7a0e38a7c00d3e624c0f7a1cf7
John Hunter <jdh2358@gmail.com>
2004-09-24 15:30:28 +0000

    added some more example
    svn path=/trunk/matplotlib/; revision=542

---
+++ examples/axhspan_demo.py
@@ -0,0 +1,42 @@
+from matplotlib.matlab import *
+figure(1)
+plot(10*rand(12), 'o')
+xlim(0,15)
+xticks([2, 4, 8, 12], ('John', 'Hunter', 'Was', 'Here'))
+
+ylim(-1,10)
+yticks(range(8))
+
+figure(2)
+t = arange(-1,2, .01)
+s = sin(2*pi*t)
+plot(t,s)
+# draw a thick red hline at y=0 that spans the xrange
+l = axhline(linewidth=4, color='r')
+
+# draw a default hline at y=1 that spans the xrange
+l = axhline(y=1)
+
+# draw a default vline at x=1 that spans the xrange
+l = axvline(x=1)
+
+# draw a thick blue vline at x=0 that spans the the upper quadrant of
+# the yrange
+l = axvline(x=0, ymin=0.75, linewidth=4, color='b')
+
+# draw a default hline at y=.5 that spans the the middle half of
+# the axes
+l = axhline(y=.5, xmin=0.25, xmax=0.75)
+
+p = axhspan(0.25, 0.75, facecolor=0.5, alpha=0.5)
+
+p = axvspan(1.25, 1.55, facecolor='g', alpha=0.5)
+
+axis([-1,2,-1,2])
+
+
+
+
+
+show()
+     
---
+++ examples/movie_demo.py
@@ -0,0 +1,20 @@
+import os, sys
+from matplotlib.matlab import *
+
+files = []
+figure(figsize=(5,5))
+ax = subplot(111)
+for i in range(50):  # 50 frames
+    cla()
+    imshow(rand(5,5), interpolation='nearest')
+    fname = '_tmp%03d.png'%i
+    print 'Saving frame', fname
+    savefig(fname)
+    files.append(fname)
+
+print 'Making movie animation.mpg - this make take a while'
+#os.system("mencoder 'mf://_tmp*.png' -mf type=png:fps=10 -ovc lavc -lavcopts vcodec=wmv2 -oac copy -o animation.mpg")
+os.system("convert _tmp*.png animation.mpg")
+
+# cleanup
+for fname in files: os.remove(fname)
