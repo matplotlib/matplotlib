360a3049535cec0c19aa8fee0d6096ef05c8a821
John Hunter <jdh2358@gmail.com>
2004-09-28 16:36:54 +0000

    added some fill demos
    svn path=/trunk/matplotlib/; revision=545

--- examples/fill_between.py
+++ examples/fill_between.py
@@ -1,20 +1,14 @@
 from matplotlib.matlab import *
-from matplotlib.patches import Polygon
 
-def fill_between(ax, x, y1, y2, **kwargs):
-    # add x,y2 in reverse order
-    verts = zip(x,y1) + [(x[i], y2[i]) for i in range(len(x)-1,-1,-1)]
-    poly = Polygon(verts, **kwargs)
-    ax.add_patch(poly)
-    ax.autoscale_view()
-    return poly
+x1 = arange(0, 2, 0.01)
+y1 = sin(2*pi*x1)
+y2 = sin(4*pi*x1) + 2
 
-x = arange(0, 2, 0.01)
-y1 = sin(2*pi*x)
-y2 = sin(4*pi*x) + 2
-ax = gca()
-
-p = fill_between(ax, x, y1, y2, facecolor='g')
-p.set_alpha(0.5)
+# reverse x and y2 so the polygon fills in order
+x = concatenate( (x1,x1[::-1]) )
+y = concatenate( (y1,y2[::-1]) )
+                 
+p = fill(x, y, facecolor='g')
+set(p, alpha=0.5)
 show()
                  
