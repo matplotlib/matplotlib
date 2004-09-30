e801cdc47dfc3b7771536ea198014df539389f87
John Hunter <jdh2358@gmail.com>
2004-09-30 17:23:53 +0000

    added andrew dalkes strftime code
    svn path=/trunk/matplotlib/; revision=557

--- lib/matplotlib/dates.py
+++ lib/matplotlib/dates.py
@@ -74,7 +74,7 @@ Date formatters
   DateIndexFormatter - date plots with implicit x indexing.
   
 """
-import sys
+import sys, re, time
 
 import matplotlib
 
@@ -215,8 +215,15 @@ class DateFormatter(Formatter):
     """
     Tick location is seconds since the epoch.  Use a strftime format
     string
+
+    python only supports datetime strftime formatting for years
+    greater than 1900.  Thanks to Andrew Dalke, Dalke Scientific
+    Software who contributed the strftime code below to include dates
+    earlier than this year
     """
 
+    illegal_s = re.compile(r"((^|[^%])(%%)*%s)")
+    
     def __init__(self, fmt, tz=None):
         """
         fmt is an strftime format string; tz is the tzinfo instance
@@ -227,10 +234,60 @@ class DateFormatter(Formatter):
 
     def __call__(self, x, pos=0):
         dt = num2date(x, self.tz)
-        return dt.strftime(self.fmt)
+        return self.strftime(dt, self.fmt)
 
     def set_tzinfo(self, tz):
         self.tz = tz
+
+    def _findall(self, text, substr):
+         # Also finds overlaps
+         sites = []
+         i = 0
+         while 1:
+             j = text.find(substr, i)
+             if j == -1:
+                 break
+             sites.append(j)
+             i=j+1
+         return sites
+
+    # Dalke: I hope I did this math right.  Every 28 years the
+    # calendar repeats, except through century leap years excepting
+    # the 400 year leap years.  But only if you're using the Gregorian
+    # calendar.
+
+    def strftime(self, dt, fmt):
+         fmt = self.illegal_s.sub(r"\1", fmt)
+         fmt = fmt.replace("%s", "s")
+         if dt.year > 1900:
+             return dt.strftime(fmt)
+
+         year = dt.year
+         # For every non-leap year century, advance by
+         # 6 years to get into the 28-year repeat cycle
+         delta = 2000 - year
+         off = 6*(delta // 100 + delta // 400)
+         year = year + off
+
+         # Move to around the year 2000
+         year = year + ((2000 - year)//28)*28
+         timetuple = dt.timetuple()
+         s1 = time.strftime(fmt, (year,) + timetuple[1:])
+         sites1 = self._findall(s1, str(year))
+
+         s2 = time.strftime(fmt, (year+28,) + timetuple[1:])
+         sites2 = self._findall(s2, str(year+28))
+
+         sites = []
+         for site in sites1:
+             if site in sites2:
+                 sites.append(site)
+
+         s = s1
+         syear = "%4d" % (dt.year,)
+         for site in sites:
+             s = s[:site] + syear + s[site+4:]
+         return s
 
 class IndexDateFormatter(Formatter):
     """
