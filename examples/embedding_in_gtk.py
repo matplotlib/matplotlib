0975e5d9443f0d1f9ea48ab7dbbaae411a021bbd
Steve Chaplin <stevech1097@yahoo.com.au>
2004-10-01 08:38:04 +0000

    SC 01/10/2004
    svn path=/trunk/matplotlib/; revision=567

--- examples/embedding_in_gtk.py
+++ examples/embedding_in_gtk.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# show how to add a matplotlib FigureCanvasGTK widget to a gtk.Window
+
 from matplotlib.numerix import arange, sin, pi
 
 import matplotlib
@@ -6,7 +8,7 @@ matplotlib.use('GTKAgg')  # or 'GTK'
 
 from matplotlib.axes import Subplot
 
-# swith comments for gtk over gtkagg
+# switch comments for gtk over gtkagg
 #from matplotlib.backends.backend_gtk import FigureCanvasGTK as FigureCanvas
 from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -15,27 +17,16 @@ import gtk
 
 win = gtk.Window()
 win.set_title("Embedding in GTK")
-win.connect("destroy", gtk.mainquit)
-
-vbox = gtk.VBox(spacing=3)
-win.add(vbox)
+win.connect("destroy", lambda x: gtk.main_quit())
 
 f = Figure(figsize=(5,4), dpi=100)
 a = f.add_subplot(111)
 t = arange(0.0,3.0,0.01)
 s = sin(2*pi*t)
-
 a.plot(t,s)
 
-
 canvas = FigureCanvas(f)  # a gtk.DrawingArea
-vbox.pack_start(canvas)
-
-#button = gtk.Button('Quit')
-#button.connect('clicked', lambda b: gtk.mainquit())
-#button.show()
-#vbox.pack_start(button)
+win.add(canvas)
 
 win.show_all()
-#gtk.mainloop()
 gtk.main()
--- examples/embedding_in_gtk2.py
+++ examples/embedding_in_gtk2.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+# show how to add a matplotlib FigureCanvasGTK widget and a toolbar to a gtk.Window
+
 from matplotlib.numerix import arange, sin, pi
 
 import matplotlib
 matplotlib.use('GTKAgg')  # or 'GTK'
 
-# swith comments for gtk over gtkagg
+# switch comments for gtk over gtkagg
 from matplotlib.backends.backend_gtk import FigureCanvasGTK as FigureCanvas
 #from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanvas
 
@@ -13,15 +15,14 @@ from matplotlib.backends.backend_gtk import NavigationToolbar2GTK as NavigationT
 
 from matplotlib.axes import Subplot
 from matplotlib.figure import Figure
+
 import gtk
 
 win = gtk.Window()
 win.set_title("Embedding in GTK")
-win.connect("destroy", gtk.mainquit)
-win.set_border_width(5)
+win.connect("destroy", lambda x: gtk.main_quit())
 
-vbox = gtk.VBox(spacing=3)
-vbox.show()
+vbox = gtk.VBox()
 win.add(vbox)
 
 fig = Figure(figsize=(5,4), dpi=100)
@@ -31,22 +32,11 @@ s = sin(2*pi*t)
 
 ax.plot(t,s)
 
-
 canvas = FigureCanvas(fig)  # a gtk.DrawingArea
-canvas.show()
 vbox.pack_start(canvas)
 
 toolbar = NavigationToolbar(canvas)
-toolbar.show()
 vbox.pack_start(toolbar, gtk.FALSE, gtk.FALSE)
 
-#buttonQuit = gtk.Button('Quit')
-#buttonQuit.connect('clicked', gtk.mainquit)
-#buttonQuit.show()
-#vbox.pack_start(buttonQuit)
-
-
-if __name__=='__main__':
-    win.show()
-#    gtk.mainloop()
-    gtk.main()
+win.show_all()
+gtk.main()
