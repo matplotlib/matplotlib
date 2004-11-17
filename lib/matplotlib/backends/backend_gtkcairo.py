36d1a9c8816626bf176c4d50acc699d049089417
Steve Chaplin <stevech1097@yahoo.com.au>
2004-11-17 15:07:40 +0000

    SC 17/11/2004
    svn path=/trunk/matplotlib/; revision=693

--- lib/matplotlib/backends/backend_gtkcairo.py
+++ lib/matplotlib/backends/backend_gtkcairo.py
@@ -11,23 +11,18 @@ def _fn_name(): return sys._getframe(1).f_code.co_name
 from matplotlib import verbose
 from matplotlib.cbook import True, False
 from matplotlib.figure import Figure
-from backend_cairo import FigureCanvasCairo, RendererCairo, IMAGE_FORMAT, \
-     IMAGE_FORMAT_DEFAULT, print_figure_fn
+from backend_cairo import RendererCairo
 from backend_gtk import gtk, FigureManagerGTK, FigureCanvasGTK, show,    \
-     draw_if_interactive, error_msg, NavigationToolbar
+     draw_if_interactive, error_msg, NavigationToolbar, IMAGE_FORMAT, \
+     IMAGE_FORMAT_DEFAULT
 
 import cairo
 import cairo.gtk
 
-backend_version = 'GTK(%s),Cairo(%s)' % ("%d.%d.%d" % gtk.pygtk_version, 'unknown')
-#backend_version = 'GTK(%d.%d.%d),Cairo(%s)' % (gtk.pygtk_version, cairo.version)
-
-print 'backend_version' , backend_version
+backend_version = 'PyGTK(%s),PyCairo(%s)' % ("%d.%d.%d" % gtk.pygtk_version, 'unknown')
+#backend_version = 'PyGTK(%d.%d.%d),PyCairo(%s)' % (gtk.pygtk_version, cairo.version)
 
 DEBUG = False
-
-# Image formats that this backend supports, same as backend_cairo + jpg from gtk
-IMAGE_FORMAT += ['jpg']
 
 
 def new_figure_manager(num, *args, **kwargs):
@@ -40,10 +35,8 @@ def new_figure_manager(num, *args, **kwargs):
     return FigureManagerGTK(canvas, num)
 
 
-class FigureCanvasGTKCairo(FigureCanvasGTK, FigureCanvasCairo):
-    """
-    Override GTK._render_to_pixmap() to use Cairo rather than GDK renderer
-    Override print_figure() to use Cairo rather than GDK.
+class FigureCanvasGTKCairo(FigureCanvasGTK):
+    """Override GTK._render_to_pixmap() to use Cairo rather than GDK renderer
     """
     def _render_to_pixmap(self, width, height):
         create_pixmap = False
@@ -66,25 +59,3 @@ class FigureCanvasGTKCairo(FigureCanvasGTK, FigureCanvasCairo):
         self._renderer = RendererCairo (self._surface, matrix, width, height, self.figure.dpi)
         self._renderer._set_width_height (width, height)
         self.figure.draw (self._renderer)
-
-
-    def print_figure(self, filename, dpi=150, facecolor='w', edgecolor='w',
-                     orientation='portrait'):
-
-        root, ext = os.path.splitext(filename)       
-        ext = ext.lower()[1:]
-        if ext == '':
-            ext      = IMAGE_FORMAT_DEFAULT
-            filename = filename + '.' + ext
-
-        ext = ext.lower()
-        if ext in ('jpg'):
-            # render with Cairo, save to file with with GDK
-            FigureCanvasGTK.print_figure (self, filename, dpi, facecolor, edgecolor,
-                                          orientation)
-        elif ext in IMAGE_FORMAT:
-            print_figure_fn (self.figure, filename, dpi, facecolor, edgecolor,
-                             orientation)
-        else:
-            error_msg('Format "%s" is not supported.\nSupported formats are %s.' %
-                      (ext, ', '.join(IMAGE_FORMAT)))
