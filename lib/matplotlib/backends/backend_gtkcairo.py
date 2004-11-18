f9aa3319f3dcd9a45c60ad6d88c243b27b43567c
Steve Chaplin <stevech1097@yahoo.com.au>
2004-11-18 11:03:16 +0000

    SC 18/11/2004
    svn path=/trunk/matplotlib/; revision=696

--- lib/matplotlib/backends/backend_gtkcairo.py
+++ lib/matplotlib/backends/backend_gtkcairo.py
@@ -2,19 +2,8 @@
 GTK+ Matplotlib interface using Cairo (not GDK) drawing operations.
 Author: Steve Chaplin
 """
-from __future__ import division
-
-import os
-import sys
-def _fn_name(): return sys._getframe(1).f_code.co_name
-
-from matplotlib import verbose
-from matplotlib.cbook import True, False
-from matplotlib.figure import Figure
+from backend_gtk import *
 from backend_cairo import RendererCairo
-from backend_gtk import gtk, FigureManagerGTK, FigureCanvasGTK, show, \
-     draw_if_interactive, error_msg, NavigationToolbar, IMAGE_FORMAT, \
-     IMAGE_FORMAT_DEFAULT
 
 import cairo
 import cairo.gtk
@@ -29,7 +18,7 @@ def new_figure_manager(num, *args, **kwargs):
     """
     Create a new figure manager instance
     """
-    if DEBUG: print 'backend_gtkcairo.%s()' % _fn_name()
+    if DEBUG: print 'backend_gtkcairo.%s()' % fn_name()
     thisFig = Figure(*args, **kwargs)
     canvas = FigureCanvasGTKCairo(thisFig)
     return FigureManagerGTK(canvas, num)
@@ -38,6 +27,6 @@ def new_figure_manager(num, *args, **kwargs):
 class FigureCanvasGTKCairo(FigureCanvasGTK):
     def _renderer_init(self):
         """Override to use Cairo rather than GDK renderer"""
-        if DEBUG: print 'backend_gtkcairo.%s()' % _fn_name()
+        if DEBUG: print 'backend_gtkcairo.%s()' % fn_name()
         matrix = cairo.Matrix ()
         self._renderer = RendererCairo (matrix, self.figure.dpi)
