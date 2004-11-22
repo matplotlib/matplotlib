6dc7cfafc2ce58d14f24920fd4e642ff0f1371b4
John Hunter <jdh2358@gmail.com>
2004-11-22 18:57:12 +0000

    fixed null message prob on gtk
    svn path=/trunk/matplotlib/; revision=705

--- examples/anim_tk.py
+++ examples/anim_tk.py
@@ -6,17 +6,22 @@ import matplotlib.matlab
 
 #import Tkinter as Tk
 import matplotlib.numerix as numerix
-
 fig = matplotlib.matlab.figure(1)
 ind = numerix.arange(60)
+
+
+
 x_tmp=[]
 for i in range(100):
     x_tmp.append(numerix.sin((ind+i)*numerix.pi/15.0))
 
 X=numerix.array(x_tmp)
+
+
 lines = matplotlib.matlab.plot(X[:,0],'o')
 
 manager = matplotlib.matlab.get_current_fig_manager()
+
 def updatefig(*args):
     updatefig.count += 1
     lines[0].set_ydata(X[:,updatefig.count%60])
@@ -24,13 +29,17 @@ def updatefig(*args):
     return updatefig.count
 updatefig.count=-1
 
+def run(*args):
+    print 'called run'
 
+    import time
+    tstart = time.time()
+    while 1:
+        cnt = updatefig()
+        if cnt==100: break
+        print 'elapsed', 100.0/(time.time() - tstart)
+    
+import Tkinter as Tk
+manager.window.after(10, run)
 manager.show()
-import time
-tstart = time.time()
-while 1:
-    cnt = updatefig()
-    if cnt==100: break
-
-
-print 'elapsed', 100.0/(time.time() - tstart)
+Tk.mainloop()
