7d4a1e8c3ef88cfa916ad3f8e5e1bdd13b41b01a
John Hunter <jdh2358@gmail.com>
2004-11-24 15:42:38 +0000

    added popd
    svn path=/trunk/matplotlib/; revision=716

--- lib/matplotlib/cbook.py
+++ lib/matplotlib/cbook.py
@@ -411,7 +411,19 @@ if __name__=='__main__':
     assert( not allequal(('a', 'b')))    
 
 
+# python 2.2 dicts don't have pop
 def popd(d, *args):
+    """
+    Should behave like python2.3 pop method; d is a dict
+
+    # returns value for key and deletes item; raises a KeyError if key
+    # is not in dict
+    val = popd(d, key)
+
+    # returns value for key if key exists, else default.  Delete key,
+    # val item if it exists.  Will not raise a KeyError
+    val = popd(d, key, default)      
+    """
     if len(args)==1:
         key = args[0]
         val = d[key]
