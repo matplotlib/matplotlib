4b174976415863f33f000779da4ecde2cd9b8924
John Hunter <jdh2358@gmail.com>
2004-12-10 20:43:21 +0000

    added contour_demo.py
    svn path=/trunk/matplotlib/; revision=757

--- examples/contour_demo.py
+++ examples/contour_demo.py
@@ -10,5 +10,9 @@ Z2 = bivariate_normal(X, Y, 1.5, 0.5, 1, 1)
 # difference of Gaussians
 im = imshow(Z2-Z1, interpolation='bilinear', origin='lower')
 axis('off')
+levels, colls = contour(Z2-Z1, levels=6, linewidths=arange(.5, 4, .5),
+                        colors=('r', 'green', 'blue', (1,1,0), '#afeeee', 0.5))
+legend(loc='lower left')
+colorbar()
 show()
 
