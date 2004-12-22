30f240fcd3b427d9c04e69ff89753af6a3979e52
John Hunter <jdh2358@gmail.com>
2004-12-22 19:28:45 +0000

    syncing to 0.65.1
    svn path=/trunk/matplotlib/; revision=807

--- examples/backend_driver.py
+++ examples/backend_driver.py
@@ -102,8 +102,8 @@ def drive(backend, python='python2.3'):
 
 times = {}
 #backends = ['Agg', 'Cairo', 'GDK', 'PS', 'SVG', 'Template']
-#backends = ['Agg', 'PS', 'Template']
-backends = [ 'GTK', 'WX', 'TkAgg']
+backends = ['Agg', 'SVG', 'PS', 'Template']
+#backends = [ 'GTK', 'WX', 'TkAgg']
 
 python = 'python2.3'
 for backend in backends:
