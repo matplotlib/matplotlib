d9072e318590f8fe35dc7626acd618c00d9d2243
John Hunter <jdh2358@gmail.com>
2004-12-29 04:48:37 +0000

    added toggle images example
    svn path=/trunk/matplotlib/; revision=825

--- examples/toggle_images.py
+++ examples/toggle_images.py
@@ -1,7 +1,6 @@
-"""
-toggle between two images by pressing "b"
+""" toggle between two images by pressing "b"
 
-The basic idea is to load two images (they can be different shapes) and plot them to the same axes with hold "on".  Then, toggle the visibility property of them using keypress event handling
+The basic idea is to load two images (they can be different shapes) and plot them to the same axes with hold "on".  Then, toggle the visible property of them using keypress event handling
 
 As usual, we'll define some random images for demo.  Real data is much more exciting
 """
@@ -12,14 +11,16 @@ x1 = rand(100,100)
 x2 = rand(150, 175)
 
 im1 = imshow(x1, interpolation='nearest')
-im2 = imshow(x2, interpolation='nearest', hold=True, visibility=False)
+im2 = imshow(x2, interpolation='nearest', hold=True)
+im2.set_visible(False)
+
 def toggle_images(event):
-    'toggle the visibility state of the two images'
+    'toggle the visible state of the two images'
     if event.key != 'b': return
-    b1 = im1.get_visibility()
-    b2 = im2.get_visibility()
-    im1.set_visibility(not b1)
-    im2.set_visibility(not b2)
+    b1 = im1.get_visible()
+    b2 = im2.get_visible()
+    im1.set_visible(not b1)
+    im2.set_visible(not b2)
     draw()
 
 connect('key_press_event', toggle_images)
