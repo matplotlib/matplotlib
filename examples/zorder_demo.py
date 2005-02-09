cc05b6058b870222b93a1a9fc1bf9a3b0aaf9fba
Fernando Perez <fernando.perez@berkeley.edu>
2005-02-09 22:21:25 +0000

    Improved explanation of individual z-ordering for lines, added second example illustrating this.
    svn path=/trunk/matplotlib/; revision=949

--- examples/zorder_demo.py
+++ examples/zorder_demo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 The default drawing order for axes is patches, lines, text.  This
-order is determined by the zorder attribute.  The following default
+order is determined by the zorder attribute.  The following defaults
 are set
 
 Artist                      Z-order
@@ -9,12 +9,15 @@ Patch / PatchCollection      1
 Line2D / LineCollection      2
 Text                         3
 
-You can change the order for individual artists by setting the zorder.
+You can change the order for individual artists by setting the zorder.  Any
+individual plot() call can set a value for the zorder of that particular item.
 
 In the fist subplot below, the lines are drawn above the patch
 collection from the scatter, which is the default.
 
-In the subplot below, the order is reversed
+In the subplot below, the order is reversed.
+
+The second figure shows how to control the zorder of individual lines.
 """
 
 from pylab import *
@@ -27,5 +30,14 @@ scatter(x,y,s=120)
 subplot(212)
 plot(x, y, 'r', zorder=1, lw=3)
 scatter(x,y,s=120, zorder=2)
+
+# A new figure, with individually ordered items
+x=frange(0,2*pi,npts=100)
+figure()
+plot(x,sin(x),linewidth=10, color='black',label='zorder=10',zorder = 10)  # on top
+plot(x,cos(1.3*x),linewidth=10, color='red', label='zorder=1',zorder = 1) # bottom
+plot(x,sin(2.1*x),linewidth=10, color='green', label='zorder=3',zorder = 3)
+axhline(0,linewidth=10, color='blue', label='zorder=2',zorder = 2)
+legend()
 
 show()
