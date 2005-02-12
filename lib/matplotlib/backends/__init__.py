a549796553a5bd364f2fa20cf7346b4db0f2183f
Fernando Perez <fernando.perez@berkeley.edu>
2005-02-12 01:43:34 +0000

    Minor cleanups, ipython now handles draw_if_interactive properly without ugly pylab hacks.
    svn path=/trunk/matplotlib/; revision=959

--- lib/matplotlib/backends/__init__.py
+++ lib/matplotlib/backends/__init__.py
@@ -1,7 +1,6 @@
 import sys
 import matplotlib
 
-
 __all__ = ['backend','show','draw_if_interactive','error_msg',
            'new_figure_manager', 'backend_version']
 
@@ -13,7 +12,6 @@ all_backends       = interactive_bk + non_interactive_bk
 backend = matplotlib.get_backend()
 if backend not in all_backends:
     raise ValueError, 'Unrecognized backend %s' % backend
-
 
 # Import the requested backend into a generic module object
 backend_name = 'backend_'+backend.lower()
@@ -27,23 +25,11 @@ if hasattr(backend_mod,'backend_version'):
     backend_version = getattr(backend_mod,'backend_version')
 else: backend_version = 'unknown'
 
-
 # Now define the public API according to the kind of backend in use
 if backend in interactive_bk:
     error_msg  = backend_mod.error_msg
     show       = backend_mod.show
-    __draw_int = backend_mod.draw_if_interactive
-
-    # wrap draw_if_interactive with a flag which detects if it was called.
-    # This allows tools like ipython to properly manage interactive scripts.
-    # In python 2.4, this can be cleanly done with a simple decorator.
-    def draw_if_interactive():
-        draw_if_interactive._called = True
-        __draw_int()
-    # Flag to store state, so external callers (like ipython) can keep track
-    # of draw calls.
-    draw_if_interactive._called = False
-    draw_if_interactive.__doc__ = __draw_int.__doc__
+    draw_if_interactive = backend_mod.draw_if_interactive
 else:  # non-interactive backends
     def draw_if_interactive():  pass
     def show(): pass
@@ -59,5 +45,4 @@ elif backend in ['WX','WXAgg']:
     Toolbar = backend_mod.Toolbar
     __all__.append('Toolbar')
 
-matplotlib.verbose.report('backend %s version %s' %
-                          (backend, backend_version))
+matplotlib.verbose.report('backend %s version %s' % (backend,backend_version))
