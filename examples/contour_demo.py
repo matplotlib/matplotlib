9522444f93265a7bf79e1ff79195b1634d1127bf
John Hunter <jdh2358@gmail.com>
2005-02-14 18:41:21 +0000

    0.72 release
    svn path=/trunk/matplotlib/; revision=971

--- examples/contour_demo.py
+++ examples/contour_demo.py
@@ -2,7 +2,8 @@
 from pylab import *
 
 delta = 0.025
-x = y = arange(-3.0, 3.0, delta)
+x = arange(-3.0, 3.0, delta)
+y = arange(-2.0, 2.0, delta)
 X, Y = meshgrid(x, y)
 Z1 = bivariate_normal(X, Y, 1.0, 1.0, 0.0, 0.0)
 Z2 = bivariate_normal(X, Y, 1.5, 0.5, 1, 1)
