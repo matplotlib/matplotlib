7cd24f9a737e663e211e735019b327bd093ea4bc
John Hunter <jdh2358@gmail.com>
2005-05-31 14:30:34 +0000

    removed tex_ddemo bbox
    svn path=/trunk/matplotlib/; revision=1382

--- examples/tex_demo.py
+++ examples/tex_demo.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from pylab import *
+rc('text', usetex=True)
 figure(1)
 ax = axes([0.1, 0.1, 0.8, 0.7])
 t = arange(0.0, 1.0+0.01, 0.01)
@@ -9,7 +10,7 @@ plot(t, s)
 xlabel(r'\bf{time (s)}')
 ylabel(r'\it{voltage (mV)}',fontsize=16)
 title(r"\TeX\ is Number $\displaystyle\sum_{n=1}^\infty\frac{-e^{i\pi}}{2^n}$!", 
-        {'bbox':{'fc':0.8,'pad':0}}, fontsize=16, color='r')
+        fontsize=16, color='r')
 grid(True)
 savefig('tex_demo.eps')
 
