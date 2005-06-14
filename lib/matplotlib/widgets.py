8ba5d88a0ac02a9751998c4cd3cb49a3f62954de
John Hunter <jdh2358@gmail.com>
2005-06-14 22:34:45 +0000

    added check boxes
    svn path=/trunk/matplotlib/; revision=1459

--- lib/matplotlib/widgets.py
+++ lib/matplotlib/widgets.py
@@ -8,7 +8,8 @@ to be to accommodate your widget.
 """
 
 from matplotlib.mlab import linspace, dist
-from matplotlib.patches import Circle
+from matplotlib.patches import Circle, Rectangle
+from matplotlib.lines import Line2D
 from matplotlib.numerix import array
 
 class Widget:
@@ -230,6 +231,117 @@ class Slider(Widget):
 
 
 
+class CheckButtons(Widget):
+    """
+    A GUI neutral radio button
+
+    The following attributes are exposed
+
+     ax - the Axes instance the buttons are in
+     labels - a list of text.Text instances
+     rectangles - a list of patch.Rectangle instances
+     checks - a list of patch.Line2D instances     
+
+    Connect to the RadioButtons with the on_clicked method
+    """
+    def __init__(self, ax, labels, actives):
+        """
+        Add check buttons to axes.Axes instance ax
+
+        labels is a len(buttons) list of labels as strings
+
+        actives is a len(buttons) list of booleans indicating whether
+         the button is active
+
+        """
+
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_navigate(False)        
+        dy = 1./(len(labels)+1)
+        ys = linspace(1-dy, dy, len(labels))
+        cnt = 0
+        axcolor = ax.get_axis_bgcolor()
+
+        self.labels = []
+        self.lines = []
+        self.rectangles = []
+
+        lineparams = {'color':'k', 'linewidth':1.25, 'transform':ax.transAxes,
+                      'solid_capstyle':'butt'}
+        for y, label in zip(ys, labels):
+            t = ax.text(0.25, y, label, transform=ax.transAxes,
+                        horizontalalignment='left',
+                        verticalalignment='center')
+
+            w, h = dy/2., dy/2.
+            x, y = 0.05, y-h/2.
+
+            p = Rectangle(xy=(x,y), width=w, height=h,
+                          facecolor=axcolor,
+                          transform=ax.transAxes)
+
+
+            l1 = Line2D([x, x+w], [y+h, y], **lineparams)
+            l2 = Line2D([x, x+w], [y, y+h], **lineparams)
+
+            l1.set_visible(actives[cnt])
+            l2.set_visible(actives[cnt])
+            self.labels.append(t)
+            self.rectangles.append(p)
+            self.lines.append((l1,l2))
+            ax.add_patch(p)
+            ax.add_line(l1)
+            ax.add_line(l2)            
+            cnt += 1
+
+        ax.figure.canvas.mpl_connect('button_press_event', self._clicked)
+        self.ax = ax
+
+
+        self.cnt = 0
+        self.observers = {}
+
+        self.actives = actives
+        
+    def _clicked(self, event):
+        if event.button !=1 : return
+        if event.inaxes != self.ax: return
+
+        for p,t,lines in zip(self.rectangles, self.labels, self.lines):
+            if (t.get_window_extent().contains(event.x, event.y) or
+                p.get_window_extent().contains(event.x, event.y) ):
+                l1, l2 = lines
+                l1.set_visible(not l1.get_visible())
+                l2.set_visible(not l2.get_visible())            
+                thist = t
+                break
+        else:
+            return
+            
+
+        if self.drawon: self.ax.figure.canvas.draw() 
+
+        if not self.eventson: return
+        for cid, func in self.observers.items():
+            func(thist.get_text())
+
+
+    def on_clicked(self, func):
+        """
+        When the button is clicked, call this func with button label
+
+        A connection id is returned which can be used to disconnect
+        """
+        cid = self.cnt
+        self.observers[cid] = func
+        self.cnt += 1        
+        return cid
+
+    def disconnect(self, cid):
+        'remove the observer with connection id cid'
+        try: del self.observers[cid]
+        except KeyError: pass
 
 
 class RadioButtons(Widget):
@@ -339,6 +451,8 @@ class RadioButtons(Widget):
         'remove the observer with connection id cid'
         try: del self.observers[cid]
         except KeyError: pass
+
+
                 
 class SubplotTool(Widget):
     """
