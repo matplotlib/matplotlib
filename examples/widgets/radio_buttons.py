545b96585915b8a874ede30b96a0f4a0781cfbb5
John Hunter <jdh2358@gmail.com>
2005-06-14 21:08:17 +0000

    added sliders demo
    svn path=/trunk/matplotlib/; revision=1455

--- examples/widgets/radio_buttons.py
+++ examples/widgets/radio_buttons.py
@@ -9,7 +9,8 @@ ax = subplot(111)
 l, = ax.plot(t, s0, lw=2, color='red')
 subplots_adjust(left=0.3)
 
-rax = axes([0.05, 0.7, 0.15, 0.15])
+axcolor = 'lightgoldenrodyellow'
+rax = axes([0.05, 0.7, 0.15, 0.15], axisbg=axcolor)
 radio = RadioButtons(rax, ('2 Hz', '4 Hz', '8 Hz'))
 def hzfunc(label):
     hzdict = {'2 Hz':s0, '4 Hz':s1, '8 Hz':s2}
@@ -18,14 +19,14 @@ def hzfunc(label):
     draw()
 radio.on_clicked(hzfunc)
 
-rax = axes([0.05, 0.4, 0.15, 0.15])
+rax = axes([0.05, 0.4, 0.15, 0.15], axisbg=axcolor)
 radio = RadioButtons(rax, ('red', 'blue', 'green'))
 def colorfunc(label):
     l.set_color(label)
     draw()
 radio.on_clicked(colorfunc)
 
-rax = axes([0.05, 0.1, 0.15, 0.15])
+rax = axes([0.05, 0.1, 0.15, 0.15], axisbg=axcolor)
 radio = RadioButtons(rax, ('-', '--', '-.', 'steps', ':'))
 def stylefunc(label):
     l.set_linestyle(label)
