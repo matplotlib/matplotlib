7db39d7c7dcf34341cf05fbc3bc207a892399df7
John Hunter <jdh2358@gmail.com>
2005-06-14 21:03:23 +0000

    added sliders demo
    svn path=/trunk/matplotlib/; revision=1454

---
+++ examples/widgets/sliders.py
@@ -0,0 +1,34 @@
+from pylab import *
+from matplotlib.widgets import Slider, Button
+
+ax = subplot(111)
+subplots_adjust(bottom=0.25)
+t = arange(0.0, 1.0, 0.001)
+s = sin(2*pi*t)
+l, = plot(t,s, lw=2)
+axis([0, 1, -10, 10])
+axfreq = axes([0.125, 0.1, 0.775, 0.03])
+axamp  = axes([0.125, 0.15, 0.775, 0.03])
+
+sfreq = Slider(axfreq, 'Freq', 0.1, 30.0, valinit=1)
+samp = Slider(axamp, 'Amp', 0.1, 10.0, valinit=1)
+
+def update(val):
+    amp = samp.val
+    freq = sfreq.val
+    l.set_ydata(amp*sin(2*pi*freq*t))
+    draw()
+sfreq.on_changed(update)
+samp.on_changed(update)
+
+resetax = axes([0.8, 0.01, 0.1, 0.05])
+button = Button(resetax, 'Reset')
+
+def reset(event):
+    sfreq.reset()
+    samp.reset()
+button.on_clicked(reset)
+    
+
+show()
+
