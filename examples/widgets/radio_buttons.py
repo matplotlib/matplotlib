f05e121cfbd0380234117d93b18abf233af46948
John Hunter <jdh2358@gmail.com>
2005-06-14 20:22:07 +0000

    added radio buttons
    svn path=/trunk/matplotlib/; revision=1451

--- examples/widgets/radio_buttons.py
+++ examples/widgets/radio_buttons.py
@@ -3,19 +3,29 @@ from pylab import *
 t = arange(0.0, 2.0, 0.01)
 s0 = sin(2*pi*t)
 s1 = sin(4*pi*t)
-s2 = sin(6*pi*t)
+s2 = sin(8*pi*t)
 
 ax = subplot(111)
-l, = ax.plot(t, s0)
+l, = ax.plot(t, s0, lw=2, color='red')
 subplots_adjust(left=0.3)
 
 rax = axes([0.05, 0.4, 0.175, 0.175])
 radio = RadioButtons(rax, ('button 1', 'button 2', 'button 3'))
 
+
 def func(label):
-    if label=='button 1': l.set_ydata(s0)
-    elif label=='button 2': l.set_ydata(s1)
-    elif label=='button 3': l.set_ydata(s2)
+    if label=='button 1':
+        ydata = s0
+        color = 'red'
+    elif label=='button 2':
+        ydata = s1
+        color='blue'        
+    elif label=='button 3':
+        ydata = s2
+        color='green'
+
+    l.set_ydata(ydata)
+    l.set_color(color)
     draw()
 radio.on_clicked(func)
     
