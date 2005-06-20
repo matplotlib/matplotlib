855fc89c617a2f2190248093482b58d67ab65deb
Charles Moad <cwmoad@gmail.com>
2005-06-20 16:54:17 +0000

    Refactored nib and backend for cocoa to work with multiple figures/windows.
    svn path=/trunk/matplotlib/; revision=1493

--- lib/matplotlib/backends/backend_cocoaagg.py
+++ lib/matplotlib/backends/backend_cocoaagg.py
@@ -29,14 +29,18 @@ from matplotlib.backend_bases import FigureManagerBase
 from backend_agg import FigureCanvasAgg
 import pylab
 
+DEBUG = True
+
 mplBundle = NSBundle.bundleWithPath_(matplotlib.get_data_path())
 
 def new_figure_manager(num, *args, **kwargs):
+    if DEBUG: print >>sys.stderr, 'new_figure_manager'
     thisFig = Figure( *args, **kwargs )
     canvas = FigureCanvasAgg(thisFig)
     return FigureManagerCocoaAgg(canvas, num)
     
 def show():
+    if DEBUG: print >>sys.stderr, 'show'
     # Let the cocoa run loop take over
     NSApplication.sharedApplication().run()
 	
@@ -52,16 +56,17 @@ class MatplotlibController(NibClassBuilder.AutoBaseClass):
     #  PlotView plotView
     
     def awakeFromNib(self):
-	self.plotWindow.setAcceptsMouseMovedEvents_(True)
-	self.plotWindow.useOptimizedDrawing_(True)
-	self.plotWindow.makeKeyAndOrderFront_(self)
-	self.plotWindow.makeFirstResponder_(self.plotView)
-
+	if DEBUG: print 'MPLController awakeFromNib'
         # Get a reference to the active canvas
         self.canvas = pylab.get_current_fig_manager().canvas
+	self.plotWindow.plotView = self.plotView
+	self.plotView.canvas = self.canvas
+	
+	# Make imageview first responder for key events
+	self.plotWindow.makeFirstResponder_(self.plotView)
 
-        # Issue a update
-        self.windowDidResize_(self)
+	# Issue a resize to update plot
+	self.plotWindow.windowDidResize_(self)
 
     def saveFigure_(self, sender):
         pass
@@ -69,17 +74,24 @@ class MatplotlibController(NibClassBuilder.AutoBaseClass):
     def quit_(self, sender):
         pass
 
+class PlotWindow(NibClassBuilder.AutoBaseClass):
+    def awakeFromNib(self):
+	if DEBUG: print 'PlotWindow awakeFromNib'
+	self.setAcceptsMouseMovedEvents_(True)
+	self.useOptimizedDrawing_(True)
+	self.makeKeyAndOrderFront_(self)
+	self.setDelegate_(self)
+
     def windowDidResize_(self, sender):
-        w,h = self.plotView.frame().size
-        dpi = self.canvas.figure.dpi.get()
-        self.canvas.figure.set_figsize_inches(w / dpi, h / dpi)
+        w,h = self.plotView.bounds().size
+        dpi = self.plotView.canvas.figure.dpi.get()
+        self.plotView.canvas.figure.set_figsize_inches(w / dpi, h / dpi)
         self.plotView.updatePlot()
 
 class PlotView(NibClassBuilder.AutoBaseClass):
     def awakeFromNib(self):
+	if DEBUG: print 'PlotView awakeFromNib'
 	self.setImageFrameStyle_(NSImageFrameGroove)
-
-	self.canvas = pylab.get_current_fig_manager().canvas
 
     def updatePlot(self):
         self.canvas.draw() # tell the agg to render
@@ -106,6 +118,7 @@ class PlotView(NibClassBuilder.AutoBaseClass):
     def mouseDown_(self, event):
 	loc = self.convertPoint_fromView_(event.locationInWindow(), None)
 	type = event.type()
+	if DEBUG: print >>sys.stderr, 'mouseDown_:', loc, type
 	if (type == NSLeftMouseDown):
 	    button = 1
 	else:
@@ -116,12 +129,14 @@ class PlotView(NibClassBuilder.AutoBaseClass):
 
     def mouseDragged_(self, event):
 	loc = self.convertPoint_fromView_(event.locationInWindow(), None)
+	if DEBUG: print >>sys.stderr, 'mouseDragged_:', loc
 	self.canvas.motion_notify_event(loc.x, loc.y)
 	self.updatePlot()
 
     def mouseUp_(self, event):
 	loc = self.convertPoint_fromView_(event.locationInWindow(), None)
 	type = event.type()
+	if DEBUG: print >>sys.stderr, 'mouseUp_:', loc, type
 	if (type == NSLeftMouseUp):
 	    button = 1
 	else:
@@ -131,12 +146,15 @@ class PlotView(NibClassBuilder.AutoBaseClass):
 	self.updatePlot()
 
     def keyDown_(self, event):
+	if DEBUG: print >>sys.stderr, 'keyDown_', event.keyCode()
 	self.canvas.key_press_event(event.keyCode())
 
     def keyUp_(self, event):
+	if DEBUG: print >>sys.stderr, 'keyUp_', event.keyCode()
 	self.canvas.key_release_event(event.keyCode())
 
 class MPLBootstrap(NSObject):
+    # Loads the nib containing the PlotWindow and PlotView
     def startWithBundle_(self, bundle):
 	NSApplicationLoad()
 	if not bundle.loadNibFile_externalNameTable_withZone_(
@@ -149,10 +167,14 @@ class MPLBootstrap(NSObject):
 class FigureManagerCocoaAgg(FigureManagerBase):
     def __init__(self, canvas, num):
         FigureManagerBase.__init__(self, canvas, num)
-        if not WMEnable('Matplotlib'):
-            print >>sys.stderr, 'Unable to hook to native window manager!'
-            sys.exit()
 
+	# If there are multiple figures we only need to enable once
+        try:
+	    WMEnable('Matplotlib')
+	except:
+	    pass
+
+	# Load a new PlotWindow
 	MPLBootstrap.alloc().init().performSelectorOnMainThread_withObject_waitUntilDone_(
 	    'startWithBundle:',
 	    mplBundle,
@@ -204,7 +226,7 @@ def WMEnable(name='Python'):
         return False
     err = d['CPSEnableForegroundOperation'](psn)
     if err:
-        print >>sys.stderr, 'CPSEnableForegroundOperation', (err, psn)
+        #print >>sys.stderr, 'CPSEnableForegroundOperation', (err, psn)
         return False
     err = d['SetFrontProcess'](psn)
     if err:
