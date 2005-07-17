855db8e04c0061bbcf78023221096126dbb50d71
Steve Chaplin <stevech1097@yahoo.com.au>
2005-07-17 04:34:53 +0000

    SC
    svn path=/trunk/matplotlib/; revision=1549

--- examples/embedding_in_gtk2.py
+++ examples/embedding_in_gtk2.py
@@ -4,28 +4,22 @@ show how to add a matplotlib FigureCanvasGTK or FigureCanvasGTKAgg widget and
 a toolbar to a gtk.Window
 """
 
+from matplotlib.axes import Subplot
+from matplotlib.figure import Figure
 from matplotlib.numerix import arange, sin, pi
-
-import matplotlib
-#matplotlib.use('GTK')
-matplotlib.use('GTKAgg')
 
 # switch comments for gtk over gtkagg
 #from matplotlib.backends.backend_gtk import FigureCanvasGTK as FigureCanvas
 from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanvas
-
 # or NavigationToolbar for classic
 from matplotlib.backends.backend_gtkagg import NavigationToolbar2GTKAgg as NavigationToolbar
-
-from matplotlib.axes import Subplot
-from matplotlib.figure import Figure
 
 import gtk
 
 win = gtk.Window()
+win.connect("destroy", lambda x: gtk.main_quit())
 win.set_default_size(400,300)
 win.set_title("Embedding in GTK")
-win.connect("destroy", lambda x: gtk.main_quit())
 
 vbox = gtk.VBox()
 win.add(vbox)
