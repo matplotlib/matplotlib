8aa08e3a5a0740eb9fc84089ef0814b45d2f8c71
Steve Chaplin <stevech1097@yahoo.com.au>
2005-07-17 04:34:53 +0000

    SC
    svn path=/trunk/matplotlib/; revision=1549

--- examples/embedding_in_gtk.py
+++ examples/embedding_in_gtk.py
@@ -4,25 +4,20 @@ show how to add a matplotlib FigureCanvasGTK or FigureCanvasGTKAgg widget to a
 gtk.Window
 """
 
-from matplotlib.numerix import arange, sin, pi
-
-import matplotlib
-#matplotlib.use('GTK')
-matplotlib.use('GTKAgg')
-
 from matplotlib.axes import Subplot
+from matplotlib.figure import Figure
+from matplotlib.numerix import arange, sin, pi
 
 # switch comments for gtk over gtkagg
 #from matplotlib.backends.backend_gtk import FigureCanvasGTK as FigureCanvas
 from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanvas
-from matplotlib.figure import Figure
 
 import gtk
 
 win = gtk.Window()
+win.connect("destroy", lambda x: gtk.main_quit())
 win.set_default_size(400,300)
 win.set_title("Embedding in GTK")
-win.connect("destroy", lambda x: gtk.main_quit())
 
 f = Figure(figsize=(5,4), dpi=100)
 a = f.add_subplot(111)
---
+++ examples/embedding_in_gtk3.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""
+demonstrate adding a FigureCanvasGTK/GTKAgg widget to a gtk.ScrolledWindow
+"""
+
+from matplotlib.axes import Subplot
+from matplotlib.figure import Figure
+from matplotlib.numerix import arange, sin, pi
+
+# switch comments for gtk over gtkagg
+from matplotlib.backends.backend_gtk import FigureCanvasGTK as FigureCanvas
+#from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanvas
+
+import gtk
+
+win = gtk.Window()
+win.connect("destroy", lambda x: gtk.main_quit())
+win.set_default_size(400,300)
+win.set_title("Embedding in GTK")
+
+f = Figure(figsize=(5,4), dpi=100)
+a = f.add_subplot(111)
+t = arange(0.0,3.0,0.01)
+s = sin(2*pi*t)
+a.plot(t,s)
+
+sw = gtk.ScrolledWindow()
+win.add (sw)
+# A scrolled window border goes outside the scrollbars and viewport
+sw.set_border_width (10)
+# policy: ALWAYS, AUTOMATIC, NEVER
+sw.set_policy (hscrollbar_policy=gtk.POLICY_AUTOMATIC, 
+               vscrollbar_policy=gtk.POLICY_ALWAYS)
+
+canvas = FigureCanvas(f)  # a gtk.DrawingArea
+canvas.set_size_request(800,600)
+sw.add_with_viewport (canvas)
+
+win.show_all()
+gtk.main()
