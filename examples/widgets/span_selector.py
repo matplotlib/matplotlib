f1569dfffac9b0568fd868901630dcd7141ab163
John Hunter <jdh2358@gmail.com>
2005-07-26 20:26:37 +0000

    added some new widgets
    svn path=/trunk/matplotlib/; revision=1566

--- examples/widgets/span_selector.py
+++ examples/widgets/span_selector.py
@@ -1,16 +1,20 @@
+"""
+The HorizontalSpanSelector is a mouse widget to select a xmin/xmax
+range.  When you left click drag in the axes, a rectangle shows the
+selected region.  When you release, the rectangle disappears and a
+callback is called with min/max.
+"""
 import pylab
 from matplotlib.widgets import HorizontalSpanSelector
 
 fig = pylab.figure(figsize=(8,6))
-ax = fig.add_axes([0.075, 0.25, 0.9, 0.725], axisbg='#FFFFCC')
-#ax = fig.add_subplot(111, axisbg='#FFFFCC')
-canvas = ax.figure.canvas
+ax = fig.add_subplot(111, axisbg='#FFFFCC')
 
 x,y = 4*(pylab.rand(2,100)-.5)
 ax.plot(x,y,'o')
 ax.set_xlim(-2,2)
 ax.set_ylim(-2,2)
-title('Press left mouse button and drag to test')
+ax.set_title('Press left mouse button and drag to test')
 
 
 def onselect(xmin, xmax):
