019694f1d272988ea8bf3e87b86132367f4e5420
John Hunter <jdh2358@gmail.com>
2005-09-07 15:00:41 +0000

    ticked version num
    svn path=/trunk/matplotlib/; revision=1719

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -143,7 +143,7 @@ the exception of those in mlab.py provided by matplotlib.
 from __future__ import generators
 
 
-__version__  = '0.84.cvs.2'
+__version__  = '0.84.cvs.3'
 __revision__ = '$Revision$'
 __date__     = '$Date$'
 
