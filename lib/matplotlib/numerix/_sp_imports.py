e759325fdf82a4af115601a20396c7a157a9ec3d
Andrew Straw <strawman@astraw.com>
2005-12-11 01:29:38 +0000

    Removed Python 2.4 specific code.
    svn path=/trunk/matplotlib/; revision=1913

--- lib/matplotlib/numerix/_sp_imports.py
+++ lib/matplotlib/numerix/_sp_imports.py
@@ -1,11 +1,10 @@
 # Not sure why all the equivalences don't exist ...
-from scipy import (
-    Int8, UnsignedInt8,
-    Int16, UnsignedInt16,
-    Int32, uint32,
-    Float32, Float64,
-    Complex32, Complex64,
-    Float, Int, Complex)
+from scipy import Int8, UnsignedInt8, \
+     Int16, UnsignedInt16, \
+     Int32, uint32, \
+     Float32, Float64, \
+     Complex32, Complex64, \
+     Float, Int, Complex
 
 UInt8 = UnsignedInt8
 UInt16 = UnsignedInt16
@@ -26,5 +25,5 @@ class _TypeNamespace:
 
 nx = _TypeNamespace()
 
-from scipy import (inf, infty, Infinity)
+from scipy import inf, infty, Infinity
 infinity = Infinity
