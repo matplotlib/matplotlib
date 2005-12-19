5baf12d02d26f32288cd0bc0dcad6de25f12b881
Andrew Straw <strawman@astraw.com>
2005-12-19 07:04:15 +0000

    Bugfix allowing build on OS X 10.4, gcc 4.0 reported by Tom Loredo and patch     submitted by Jouni K Sepp��nen. -- ADS
    svn path=/trunk/matplotlib/; revision=1921

--- src/_transforms.cpp
+++ src/_transforms.cpp
@@ -443,7 +443,8 @@ Bbox::update(const Py::Tuple &args) {
 
 Py::Object
 Bbox::update_numerix(const Py::Tuple &args) {
-  //update the boox from the numerix arrays x and y
+  //update the box from the numerix arrays x and y
+  using std::isnan;
   _VERBOSE("Bbox::update_numerix");
 
   args.verify_length(3);
