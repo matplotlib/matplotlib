b296ed359671371a658b49dbaea9044ba233a79d
Travis Oliphant <teoliphant@gmail.com>
2006-01-20 21:38:30 +0000

    Added getmaskorNone function to numerix.ma to fix numpy masked arrays and cntr.c
    svn path=/trunk/matplotlib/; revision=1977

--- lib/matplotlib/numerix/ma/__init__.py
+++ lib/matplotlib/numerix/ma/__init__.py
@@ -3,10 +3,17 @@ from matplotlib.numerix import which
 if which[0] == "numarray":
     from numarray.ma import *
     nomask = None
+    getmaskorNone = getmask
 elif which[0] == "numeric":
     from MA import *
     nomask = None
+    getmaskorNone = getmask
 elif which[0] == "numpy":
     from numpy.core.ma import *
+    def getmaskorNone(obj):
+        _msk = getmask(obj)
+	if _msk is nomask:
+            return None
+        return _msk
 else:
     raise RuntimeError("invalid numerix selector")
