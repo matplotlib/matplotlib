268e56a332440fa8e9cc6c1ccf51167b8d3355d3
Charles Moad <cwmoad@gmail.com>
2006-01-20 18:44:09 +0000

    Added namespace support for toolkits. Added some python2.2 compatibility changes.  Some issues still exist.
    svn path=/trunk/matplotlib/; revision=1974

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -153,7 +153,6 @@ import distutils.sysconfig
 # Needed for toolkit setuptools support
 try:
     __import__('pkg_resources').declare_namespace(__name__)
-    print __name__, __file__
 except ImportError:
     pass # must not have setuptools
 
