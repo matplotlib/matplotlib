0842d0bc334e2fd82616d6c61ccb3dfc0af00741
Andrew Straw <strawman@astraw.com>
2006-01-28 18:28:02 +0000

    trivial shortening of source code line lengths to fit terminal window.
    svn path=/trunk/matplotlib/; revision=2010

--- setupegg.py
+++ setupegg.py
@@ -3,4 +3,6 @@ Poor man's setuptools script...
 """
 
 from setuptools import setup
-execfile('setup.py', {'additional_params' : {'namespace_packages' : ['matplotlib.toolkits']}})
+execfile('setup.py',
+         {'additional_params' :
+          {'namespace_packages' : ['matplotlib.toolkits']}})
