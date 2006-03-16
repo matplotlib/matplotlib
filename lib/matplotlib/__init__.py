34de8d02cdd3cf7d6d3a43f6f6e464b9a95ffb76
Charles Moad <cwmoad@gmail.com>
2006-03-16 13:52:37 +0000

    0.87.2 bump
    svn path=/trunk/matplotlib/; revision=2150

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -143,7 +143,7 @@ the exception of those in mlab.py provided by matplotlib.
 from __future__ import generators
 
 
-__version__  = '0.87.2svn'
+__version__  = '0.87.2'
 __revision__ = '$Revision$'
 __date__     = '$Date$'
 
