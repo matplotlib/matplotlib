f3c9775a14d76d868ffba7d86f56eaf1fbb725e3
John Hunter <jdh2358@gmail.com>
2006-05-19 14:12:35 +0000

    added custom symbol to scatter
    svn path=/trunk/matplotlib/; revision=2402

---
+++ examples/scatter_custom_symbol.py
@@ -0,0 +1,17 @@
+from pylab import figure, nx, show
+
+# unit area ellipse
+rx, ry = 3., 1.
+area = rx * ry * nx.pi
+theta = nx.arange(0, 2*nx.pi+0.01, 0.1)
+verts = zip(rx/area*nx.cos(theta), ry/area*nx.sin(theta))
+
+x,y,s,c = nx.rand(4, 30)
+s*= 10**2. 
+
+
+fig = figure()
+ax = fig.add_subplot(111)
+ax.scatter(x,y,s,c,marker=None,verts =verts)
+
+show()
