5b2b12059d89944025e10527e754398e0e4f5cbb
Eric Firing <efiring@hawaii.edu>
2006-05-19 07:28:01 +0000

    New demo showing multiple images with common cmap, colorbar.
    svn path=/trunk/matplotlib/; revision=2400

---
+++ examples/multi_image.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+'''
+Make a set of images with a single colormap, norm, and colorbar.
+
+It also illustrates colorbar tick labelling with a multiplier.
+'''
+
+import pylab
+from matplotlib import cm, colors
+from matplotlib.font_manager import FontProperties
+from matplotlib.numerix.mlab import amin, amax
+
+Nr = 3
+Nc = 2
+
+fig = pylab.gcf()
+cmap = cm.cool
+
+figtitle = 'Multiple images'
+t = pylab.gcf().text(0.5, 0.95, figtitle,
+               horizontalalignment='center',
+               fontproperties=FontProperties(size=16))
+
+cax = fig.add_axes([0.2, 0.08, 0.6, 0.04])
+
+w = 0.4
+h = 0.22
+ax = []
+images = []
+vmin = 1e40
+vmax = -1e40
+for i in range(Nr):
+    for j in range(Nc):
+        pos = [0.075 + j*1.1*w, 0.18 + i*1.2*h, w, h]
+        a = fig.add_axes(pos)
+        if i > 0:
+            a.set_xticklabels([])
+        # Make some fake data with a range that varies
+        # somewhat from one plot to the next.
+        data =((1+i+j)/10.0)*pylab.rand(10,20)*1e-6
+        dd = pylab.ravel(data)
+        # Manually find the min and max of all colors for
+        # use in setting the color scale.
+        vmin = min(vmin, amin(dd))
+        vmax = max(vmax, amax(dd))
+        images.append(a.imshow(data, cmap=cmap))
+
+        ax.append(a)
+
+# Set the first image as the master, with all the others
+# observing it for changes in cmap or norm.
+norm = colors.normalize(vmin=vmin, vmax=vmax)
+for i, im in enumerate(images):
+    im.set_norm(norm)
+    if i > 0:
+        images[0].add_observer(im)
+
+# The colorbar is also based on this master image.
+fig.colorbar(images[0], cax, orientation='horizontal')
+
+# We need the following only if we want to run this
+# script interactively and be able to change the colormap.
+pylab.gci._current = images[0]
+
+pylab.show()
+
+
+
+
+
+
