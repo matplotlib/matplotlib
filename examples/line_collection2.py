c1fb858855db31169005b25b1a1caac7ed90ef3b
Eric Firing <efiring@hawaii.edu>
2006-05-30 23:29:30 +0000

    Example of LineCollection with colormap
    svn path=/trunk/matplotlib/; revision=2424

---
+++ examples/line_collection2.py
@@ -0,0 +1,36 @@
+from pylab import *
+from matplotlib.collections import LineCollection
+
+# In order to efficiently plot many lines in a single set of axes,
+# Matplotlib has the ability to add the lines all at once. Here is a
+# simple example showing how it is done.
+
+N = 50
+x = arange(N)
+# Here are many sets of y to plot vs x
+ys = [x+i for i in x]
+
+# We need to set the plot limits, the will not autoscale
+ax = axes()
+ax.set_xlim((amin(x),amax(x)))
+ax.set_ylim((amin(amin(ys)),amax(amax(ys))))
+
+# colors is sequence of rgba tuples
+# linestyle is a string or dash tuple. Legal string values are
+#          solid|dashed|dashdot|dotted.  The dash tuple is (offset, onoffseq)
+#          where onoffseq is an even length tuple of on and off ink in points.
+#          If linestyle is omitted, 'solid' is used
+# See matplotlib.collections.LineCollection for more information
+line_segments = LineCollection([zip(x,y) for y in ys], # Make a sequence of x,y pairs
+                                linewidths    = (0.5,1,1.5,2),
+                                linestyle = 'solid')
+line_segments.set_array(x)
+ax.add_collection(line_segments)
+fig = gcf()
+axcb = fig.colorbar(line_segments)
+axcb.set_label('Line Number')
+ax.set_title('Line Collection with mapped colors')
+sci(line_segments) # This allows interactive changing of the colormap.
+show()
+
+
