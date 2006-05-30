9efbf04076cd8c2a0047df2455a95f8a03211ba3
Eric Firing <efiring@hawaii.edu>
2006-05-30 23:48:59 +0000

    Slight modification of Jordan Dawe's quiver patch.
    svn path=/trunk/matplotlib/; revision=2425

--- examples/quiver_demo.py
+++ examples/quiver_demo.py
@@ -13,11 +13,13 @@ quiver( U, V, 0.8, color='r' )
 
 figure()
 quiver( U, V)
+title('quiver(U,V)')
 
 figure()
 quiver( U, V, color=U+V )
 
 figure()
 quiver( X, Y, U, V )
+title('quiver(X,Y,U,V); needs a scale parameter')
 show()
 
