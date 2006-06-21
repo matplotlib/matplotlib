d074f646549e9b70f11fcdc9cc660ce724c832ff
John Hunter <jdh2358@gmail.com>
2006-06-21 13:26:57 +0000

    added custom figure class hook
    svn path=/trunk/matplotlib/; revision=2503

--- examples/custom_figure_class.py
+++ examples/custom_figure_class.py
@@ -7,13 +7,13 @@ from matplotlib.figure import Figure
 class MyFigure(Figure):
     def __init__(self, *args, **kwargs):
         """
-        custom kwarg figtitle is passed on to 
+        custom kwarg figtitle is a figure title
         """
+        figtitle = kwargs.pop('figtitle', 'hi mom')
         Figure.__init__(self, *args, **kwargs)
-        figtitle = kwargs.get('figtitle', 'hi mom')
         self.text(0.5, 0.95, figtitle, ha='center')
 
-fig = figure(FigureClass=MyFigure)
+fig = figure(FigureClass=MyFigure, figtitle='my title')
 ax = fig.add_subplot(111)
 ax.plot([1,2,3])
 fig.savefig('test.png')
