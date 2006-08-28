19bd05adc52d53bee63bce16de8caff2f1837cef
Edin Salkovic <edin.salkovic@gmail.com>
2006-08-28 20:29:34 +0000

    mathtext2 - added support for complex sub/superscripts (i.e.fractions etc.)
    svn path=/trunk/matplotlib/; revision=2715

--- examples/mathtext2_demo.py
+++ examples/mathtext2_demo.py
@@ -55,7 +55,7 @@ grid(True)
 #xlabel(r'$\Delta_i^j$', fontsize=20)
 #ylabel(r'$\Delta_{i+1}^j$', fontsize=20)
 #tex = r'$\cal{R}\prod_{i=\alpha_{i+1}}^\infty a_i\rm{sin}(2 \pi f x_i)$'
-#tex = ur"$1^2_3$"
+tex = ur"$1^j_3$"
 #tex = ur"$Tj_1j_jj_gT$"
 #tex = ur"$F_1^1y_{1_{2_{3_2\sum_1^2{4}^6}}3}1_23$"
 #tex = ur"$x_2{\cal TRALALA}\sum_1^2$"
@@ -64,10 +64,10 @@ grid(True)
 #tex = ur"$Tutinjac\ fff\sin\exp$"
 #tex = ur"$\sin\exp{\rm sin\ exp}$"
 #tex = ur"$a^{\sin x}\sin b\sin(x/x), {\rm sin}(x/x){\rm sin\ }(x/x)$"
-tex = ur"$1\frac {\int_{-\infty}^\infty} 22$"
+#tex = ur"$1\frac {\int_{-\infty}^\infty} 22$"
 #tex = ur"$\frac{\int_{-\infty}^\infty} 2$"
 #tex = ur"$1_\frac{\sum^2_{i_{23}=0}} 2678$"
-#tex = ur"$1_{\frac{\sum^2_{i_{23}=0}} 2 345}678$"
+tex = ur"$1_{\frac{\sum^2_{i_{23}=0}}{\sum_{i=\frac94}^\infty} 345}678$"
 text(0.5, 2., tex, fontsize=20)
 tex = r'${\cal R}\prod_{i=\alpha_{i+1}}^\infty a_i\sin\exp(2 \pi f x_i)$'
 text(1, 1.9, tex, fontsize=20)
