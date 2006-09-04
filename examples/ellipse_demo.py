41ec0cff75effa043eadd039a4907a4d38713114
John Hunter <jdh2358@gmail.com>
2006-09-04 16:47:57 +0000

    made aspect equal for ellipse demo
    svn path=/trunk/matplotlib/; revision=2748

--- examples/ellipse_demo.py
+++ examples/ellipse_demo.py
@@ -5,7 +5,7 @@ NUM = 250
 
 ells = [Ellipse(rand(2)*10, rand(), rand(), rand()*360) for i in xrange(NUM)]
 
-a = subplot(111)
+a = subplot(111, aspect='equal')
 for e in ells:
     a.add_artist(e)
     e.set_clip_box(a.bbox)
