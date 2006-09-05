1cc35659407e6c2067ff77afafb85d55f5822ea6
John Hunter <jdh2358@gmail.com>
2006-09-05 01:18:53 +0000

    added inside poly memleak unit test
    svn path=/trunk/matplotlib/; revision=2753

---
+++ unit/inside_poly_memleak.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os, sys, time
+import matplotlib.nxutils as nxutils
+import matplotlib.numerix as nx
+
+def report_memory(i):
+    pid = os.getpid()
+    a2 = os.popen('ps -p %d -o rss,sz' % pid).readlines()
+    print i, '  ', a2[1],
+    return int(a2[1].split()[1])
+
+
+
+for i in range(500):
+    report_memory(i)
+    verts = nx.mlab.rand(100, 2)
+    b = nxutils.pnpoly(x, y, verts)
+    
+    for i in range(500):
+        report_memory(i)
+        verts = nx.mlab.rand(100, 2)
+        points = nx.mlab.rand(10000,2)
+        mask = nxutils.points_inside_poly(points, verts)
+
