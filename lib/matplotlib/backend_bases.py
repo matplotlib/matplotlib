1e6ccdeff9449624734849679baa9059efcdc658
John Hunter <jdh2358@gmail.com>
2006-09-05 02:10:05 +0000

    fixed a backend_template draw_arc bug
    svn path=/trunk/matplotlib/; revision=2758

--- lib/matplotlib/backend_bases.py
+++ lib/matplotlib/backend_bases.py
@@ -33,7 +33,8 @@ class RendererBase:
         """
         pass
 
-    def draw_arc(self, gcEdge, rgbFace, x, y, width, height, angle1, angle2, rotation):
+
+    def draw_arc(self, gc, rgbFace, x, y, width, height, angle1, angle2, rotation):
         """
         Draw an arc using GraphicsContext instance gcEdge, centered at x,y,
         with width and height and angles from 0.0 to 360.0
