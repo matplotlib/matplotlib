0b458248530ddfe5d6dc64ffc6469481c7209c71
John Hunter <jdh2358@gmail.com>
2006-09-05 02:00:40 +0000

    fixed a nxutils syntax error
    svn path=/trunk/matplotlib/; revision=2757

--- src/nxutils.c
+++ src/nxutils.c
@@ -221,7 +221,7 @@ static PyMethodDef module_methods[] = {
   {"pnpoly",  pnpoly, METH_VARARGS, "inside = pnpoly(x, y, xyverts)\nreturn 1 if x,y is inside the polygon defined by the sequence of x,y vertices in xyverts"},
   {"points_inside_poly",  points_inside_poly, METH_VARARGS, "mask = points_inside_poly(xypoints, xyverts)\nreturn a mask of length xypoints indicating whether each x,y point is inside the polygon defined by the sequence of x,y vertices in xyverts"},
   {NULL}  /* Sentinel */
-}
+};
 
 #ifdef NUMARRAY
 #if PY_MINOR_VERSION > 2
