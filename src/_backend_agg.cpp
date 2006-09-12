e3cb5d73363fb45f3fb0798da7481c11cec85d22
Charles Moad <cwmoad@gmail.com>
2006-09-12 16:22:55 +0000

    removed M_PI warning
    svn path=/trunk/matplotlib/; revision=2765

--- src/_backend_agg.cpp
+++ src/_backend_agg.cpp
@@ -38,9 +38,15 @@
 #endif
 #endif
 
+#ifndef M_PI
 #define M_PI       3.14159265358979323846
+#endif
+#ifndef M_PI_4
 #define M_PI_4     0.785398163397448309616
+#endif
+#ifndef M_PI_2
 #define M_PI_2     1.57079632679489661923
+#endif
 
 /* ------------ RendererAgg methods ------------- */
 
