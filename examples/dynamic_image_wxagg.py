6913cfd8a61f8d68e84f6a3aa460991a6b000960
John Hunter <jdh2358@gmail.com>
2006-10-05 19:59:13 +0000

    added aix to setupext
    svn path=/trunk/matplotlib/; revision=2794

--- examples/dynamic_image_wxagg.py
+++ examples/dynamic_image_wxagg.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 Copyright (C) 2003-2004 Jeremy O'Donoghue and others
- 
+
 License: This work is licensed under the PSF. A copy should be included
 with this source code, and is also available at
 http://www.python.org/psf/license.html
@@ -18,7 +18,7 @@ matplotlib.use('WXAgg')
 # numerix=numarray, it is important to compile matplotlib for numarray
 # by setting NUMERIX = 'numarray' in setup.py before building
 from matplotlib import rcParams
-rcParams['numerix'] = 'numarray'
+##rcParams['numerix'] = 'numarray'
 
 
 # jdh: you can import cm directly, you don't need to go via
@@ -32,15 +32,15 @@ from matplotlib.backends.backend_wxagg import Toolbar, FigureCanvasWxAgg
 
 from matplotlib.figure import Figure
 import matplotlib.numerix as numerix
-from wxPython.wx import *
+import wx
 
 
-TIMER_ID = wxNewId()
+TIMER_ID = wx.NewId()
 
-class PlotFigure(wxFrame):
+class PlotFigure(wx.Frame):
 
     def __init__(self):
-        wxFrame.__init__(self, None, -1, "Test embedded wxFigure")
+        wx.Frame.__init__(self, None, -1, "Test embedded wxFigure")
 
         self.fig = Figure((5,4), 75)
         self.canvas = FigureCanvasWxAgg(self, -1, self.fig)
@@ -51,19 +51,25 @@ class PlotFigure(wxFrame):
         # you don't need this under Linux
         tw, th = self.toolbar.GetSizeTuple()
         fw, fh = self.canvas.GetSizeTuple()
-        self.toolbar.SetSize(wxSize(fw, th))
+        self.toolbar.SetSize(wx.Size(fw, th))
+
+        # Initialise the timer - wxPython requires this to be connected to
+        # the receiving event handler
+        self.t = wx.Timer(self, TIMER_ID)
+        self.t.Start(10)
 
         # Create a figure manager to manage things
 
         # Now put all into a sizer
-        sizer = wxBoxSizer(wxVERTICAL)
+        sizer = wx.BoxSizer(wx.VERTICAL)
         # This way of adding to sizer allows resizing
-        sizer.Add(self.canvas, 1, wxLEFT|wxTOP|wxGROW)
+        sizer.Add(self.canvas, 1, wx.LEFT|wx.TOP|wx.GROW)
         # Best to allow the toolbar to resize!
-        sizer.Add(self.toolbar, 0, wxGROW)
+        sizer.Add(self.toolbar, 0, wx.GROW)
         self.SetSizer(sizer)
         self.Fit()
-        EVT_TIMER(self, TIMER_ID, self.onTimer)
+        self.Bind(wx.EVT_TIMER, self.onTimer, id=TIMER_ID)
+        self.Bind(wx.EVT_CLOSE, self.onClose)
 
     def init_plot_data(self):
         # jdh you can add a subplot directly from the fig rather than
@@ -77,13 +83,11 @@ class PlotFigure(wxFrame):
         z = numerix.sin(self.x) + numerix.cos(self.y)
         self.im = a.imshow( z, cmap=cm.jet)#, interpolation='nearest')
 
-
-
     def GetToolBar(self):
-        # You will need to override GetToolBar if you are using an 
+        # You will need to override GetToolBar if you are using an
         # unmanaged toolbar in your frame
         return self.toolbar
-		
+
     def onTimer(self, evt):
         self.x += numerix.pi/15
         self.y += numerix.pi/20
@@ -92,20 +96,18 @@ class PlotFigure(wxFrame):
         self.canvas.draw()
         #self.canvas.gui_repaint()  # jdh wxagg_draw calls this already
         
+    def onClose(self, evt):
+        self.t.Stop()
+        evt.Skip()
+
     def onEraseBackground(self, evt):
         # this is supposed to prevent redraw flicker on some X servers...
         pass
-        
+
 if __name__ == '__main__':
-    app = wxPySimpleApp()
+    app = wx.PySimpleApp()
     frame = PlotFigure()
     frame.init_plot_data()
-    
-    # Initialise the timer - wxPython requires this to be connected to
-    # the receiving event handler
-    t = wxTimer(frame, TIMER_ID)
-    t.Start(10)
-    
+
     frame.Show()
     app.MainLoop()
-
