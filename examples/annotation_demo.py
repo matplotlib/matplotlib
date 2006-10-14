f21cfba7832a6cf68a07356de7992321cbd1ab7c
John Hunter <jdh2358@gmail.com>
2006-10-14 22:08:17 +0000

    extended annotations
    svn path=/trunk/matplotlib/; revision=2816

--- examples/annotation_demo.py
+++ examples/annotation_demo.py
@@ -47,4 +47,50 @@ g = Annotation(line, 'localmax', loc=(2.25, 1), padx=20, pady=80,
 ax.add_artist(g)
 
 fig.savefig('annotation_demo')
+
+
+# here are some annotations using various coordinate systems.  If you
+# pass in loc=(x,y) where x,y are scalars, then you can specify the
+# following strings for the coordinate system
+#  'figure points'   : points from the lower left corner of the figure
+#  'figure pixels'   : pixels from the lower left corner of the figure
+#  'figure fraction' : 0,0 is lower left of figure and 1,1 is upper, right
+#  'axes points'     : points from lower left corneer of axes
+#  'axes pixels'     : pixels from lower left corneer of axes
+#  'axes fraction'   : 0,1 is lower left of axes and 1,1 is upper right
+#  'data'            : use the coordinate system of the object being annotated (default)
+
+
+fig = figure()
+ax = fig.add_subplot(111, autoscale_on=False, xlim=(-1,5), ylim=(-3,5))
+
+t = nx.arange(0.0, 5.0, 0.01)
+s = nx.sin(2*nx.pi*t)
+line, = ax.plot(t, s, lw=3, color='purple')
+
+a = Annotation(ax, 'A: center', loc=(.5, .5),  coords='axes fraction')
+ax.add_artist(a)
+
+b = Annotation(fig, 'B: pixels', loc=(20, 20),  coords='figure pixels')
+ax.add_artist(b)
+
+c = Annotation(fig, 'C: points', loc=(100, 300),  coords='figure points')
+ax.add_artist(c)
+
+d = Annotation(line, 'D: data', loc=(1, 2),  coords='data')
+ax.add_artist(d)
+
+# use positive points or pixels to specify from left, bottom
+e = Annotation(fig, 'E: a figure title (fraction)', loc=(.05, .95),  coords='figure fraction',
+               horizontalalignment='left', verticalalignment='top',
+               fontsize=20)
+ax.add_artist(e)
+
+# use negative points or pixels to specify from right, top
+f = Annotation(fig, 'F: a figure title (points)', loc=(-10, -10),  coords='figure points',
+               horizontalalignment='right', verticalalignment='top',
+               fontsize=20)
+ax.add_artist(f)
+
+
 show()
