2a84e1c22c2a35f8b6b2537be50e544bc99a4519
John Hunter <jdh2358@gmail.com>
2006-12-05 03:33:32 +0000

    additional kwarg docs for init funcs
    svn path=/trunk/matplotlib/; revision=2908

--- lib/matplotlib/lines.py
+++ lib/matplotlib/lines.py
@@ -139,9 +139,35 @@ class Line2D(Artist):
                  ):
         """
         Initialize the line instance
-        
-        The following line properties are customizable by kwargs
-%(line2d)s
+
+        The kwargs are Line2D properties:
+          alpha: float
+          animated: [True | False]
+          antialiased or aa: [True | False]
+          clip_box: a matplotlib.transform.Bbox instance
+          clip_on: [True | False]
+          color or c: any matplotlib color
+          dash_capstyle: ['butt' | 'round' | 'projecting']
+          dash_joinstyle: ['miter' | 'round' | 'bevel']
+          dashes: sequence of on/off ink in points
+          data: (array xdata, array ydata)
+          figure: a matplotlib.figure.Figure instance
+          label: any string
+          linestyle or ls: [ '-' | '--' | '-.' | ':' | 'steps' | 'None' | ' ' | '' ]
+          linewidth or lw: float value in points
+          lod: [True | False]
+          marker: [ '+' | ',' | '.' | '1' | '2' | '3' | '4'
+          markeredgecolor or mec: any matplotlib color
+          markeredgewidth or mew: float value in points
+          markerfacecolor or mfc: any matplotlib color
+          markersize or ms: float
+          solid_capstyle: ['butt' | 'round' |  'projecting']
+          solid_joinstyle: ['miter' | 'round' | 'bevel']
+          transform: a matplotlib.transform transformation instance
+          visible: [True | False]
+          xdata: array
+          ydata: array
+          zorder: any number
         """
         Artist.__init__(self)
 
