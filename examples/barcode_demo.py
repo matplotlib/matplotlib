4c6b6f541dffb390c4091b2d60c4edeec4089fae
John Hunter <jdh2358@gmail.com>
2007-02-13 20:55:47 +0000

    added binary colormap and barcode demo
    svn path=/trunk/matplotlib/; revision=3019

--- examples/barcode_demo.py
+++ examples/barcode_demo.py
@@ -1,22 +1,23 @@
 from pylab import figure, show, cm, nx
 
+# the bar
+x = nx.mlab.rand(500)
+x[x>0.7] = 1.
+x[x<=0.7] = 0.
+
 axprops = dict(xticks=[], yticks=[])
 barprops = dict(aspect='auto', cmap=cm.binary, interpolation='nearest')
 
 fig = figure()
 
 # a vertical barcode
-x = nx.mlab.rand(500,1)
-x[x>0.8] = 1.
-x[x<=0.8] = 0.
+x.shape = len(x), 1
 ax = fig.add_axes([0.1, 0.3, 0.1, 0.6], **axprops)
 ax.imshow(x, **barprops)
 
 
 # a horizontal barcode
-x = nx.mlab.rand(1,500)
-x[x>0.8] = 1.
-x[x<=0.8] = 0.
+x.shape = 1, len(x)
 ax = fig.add_axes([0.3, 0.1, 0.6, 0.1], **axprops)
 ax.imshow(x, **barprops)
 
