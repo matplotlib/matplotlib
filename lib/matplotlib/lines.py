matplotlib/matplotlib

commit 9ec954ea37cdf15a21310c62d0af9f50a83a2d24

small cleanups and corrections
svn path=/trunk/matplotlib/; revision=3178

lib/matplotlib/lines.py
@@ -220,7 +220,7 @@ class Line2D(Artist):
         self.verticalOffset = None
 
         # update kwargs before updating data to give the caller a
-        # chance to init axes (and hence unit support) 
+        # chance to init axes (and hence unit support)
         self.update(kwargs)
 
         self.set_data(xdata, ydata)
@@ -232,7 +232,7 @@ class Line2D(Artist):
         off a backend_bases.PickEvent with the additional attribute"ind"
         which is a sequence of indices into the data that meet the criteria
         """
-        if not self.pickable(): return 
+        if not self.pickable(): return
 
         if self._newstyle:
             # transform in backend
@@ -261,8 +261,8 @@ class Line2D(Artist):
             hit, props = picker(self, mouseevent)
             if hit:
                 self.figure.canvas.pick_event(mouseevent, self, **props)
-                
-            
+
+
 
     def get_window_extent(self, renderer):
         self._newstyle = hasattr(renderer, 'draw_markers')
@@ -297,7 +297,7 @@ class Line2D(Artist):
             self._xcid = ax.xaxis.callbacks.connect('units', self.recache)
         if ax.yaxis is not None:
             self._ycid = ax.yaxis.callbacks.connect('units', self.recache)
-        
+
     def set_data(self, *args):
         """
         Set the x and y data
@@ -395,7 +395,7 @@ class Line2D(Artist):
         self._newstyle = hasattr(renderer, 'draw_markers')
         gc = renderer.new_gc()
         self._set_gc_clip(gc)
-        
+
         gc.set_foreground(self._color)
         gc.set_antialiased(self._antialiased)
         gc.set_linewidth(self._linewidth)
@@ -432,7 +432,7 @@ class Line2D(Artist):
 
 
         if self._marker is not None:
-            
+
             gc = renderer.new_gc()
             self._set_gc_clip(gc)
             if (is_string_like(self._markeredgecolor) and
@@ -708,7 +708,14 @@ class Line2D(Artist):
             path.end_poly()
             renderer.draw_markers(gc, path, rgbFace, xt, yt, self.get_transform())
         else:
-            for (x,y) in zip(xt, yt):
+            # Use of draw_arc is left effectively disabled (since
+            # now self._newstyle is always True) because it is
+            # much slower (for Agg) than using the polygon approx with
+            # draw_markers.  To speed it up we need a
+            # draw_arcs or draw_circles backend counterpart to draw_markers.
+            xtt, ytt = self.get_transform().numerix_x_y(xt, yt)
+            for i, x in enumerate(xtt):
+                y = ytt[i]
                 renderer.draw_arc(gc, rgbFace,
                                   x, y, w, w, 0.0, 360.0, 0.0)
 

lib/matplotlib/artist.py
@@ -57,7 +57,7 @@ class Artist:
             #print 'artist.convert_xunits no conversion: ax=%s'%ax
             return x
         return ax.xaxis.convert_units(x)
-        
+
     def convert_yunits(self, y):
         """for artists in an axes, if the yaxis as units support,
         convert y using yaxis unit type
@@ -77,7 +77,7 @@ class Artist:
     def get_axes(self):
         'return the axes instance the artist resides in, or None'
         return self.axes
-    
+
     def add_callback(self, func):
         oid = self._oid
         self._propobservers[oid] = func
@@ -127,7 +127,7 @@ class Artist:
         """
         # if mouseevent x, y are within picker call self.figure.canvas.pick_event(self, mouseevent)
         pass
-    
+
     def set_picker(self, picker):
         """
         set the epsilon for picking used by this artist
@@ -139,7 +139,7 @@ class Artist:
           boolean - if True then picking will be enabled and the
             artist will fire a pick event if the mouse event is over
             the artist
-        
+
           float - if picker is a number it is interpreted as an
             epsilon tolerance in points and the the artist will fire
             off an event if it's data is within epsilon of the mouse
@@ -166,7 +166,7 @@ class Artist:
         'return the Pickeration instance used by this artist'
         return self._picker
 
-    
+
     def is_figure_set(self):
         return self.figure is not None
 
@@ -238,7 +238,7 @@ class Artist:
         ACCEPTS: [True | False]
         """
         self._clipon = b
-        if not b: 
+        if not b:
 	    self.clipbox = None
             self._clippath = None
         self.pchanged()

lib/matplotlib/collections.py
@@ -93,6 +93,7 @@ class PatchCollection(Collection, ScalarMappable):
           transOffset = identity_transform(),
           norm = None,  # optional for ScalarMappable
           cmap = None,  # ditto
+          label = ''
 
     offsets and transOffset are used to translate the patch after
     rendering (default no offsets)

lib/matplotlib/patches.py
@@ -202,8 +202,8 @@ class Patch(Artist):
         gc.set_linewidth(self._linewidth)
         gc.set_alpha(self._alpha)
         gc.set_antialiased(self._antialiased)
-        if self.get_clip_on(): gc.set_clip_rectangle(
-            self.clipbox.get_bounds())
+        if self.get_clip_on():
+            gc.set_clip_rectangle(self.clipbox.get_bounds())
         gc.set_capstyle('projecting')
 
         if not self.fill or self._facecolor is None: rgbFace = None
@@ -706,7 +706,7 @@ class CirclePolygon(RegularPolygon):
 class Ellipse(Patch):
     """
     A scale-free ellipse
-        """
+    """
     def __init__(self, xy, width, height, angle=0.0, **kwargs):
         """
         xy - center of ellipse
@@ -724,18 +724,7 @@ class Ellipse(Patch):
         self.width, self.height = width, height
         self.angle = angle
 
-        x,y = self.center
-        l,r = x-width/2.0, x+width/2.0
-        b,t = y-height/2.0, y+height/2.0
-
-        # self.verts = array(((x,y),(l,y),(x,t),(r,y),(x,b)), Float)
-
-
     def get_verts(self):
-        """
-        Not actually used for rendering.  Provided to conform to
-        Patch super class.
-        """
         x,y = self.center
         l,r = x-self.width/2.0, x+self.width/2.0
         b,t = y-self.height/2.0, y+self.height/2.0
@@ -751,8 +740,8 @@ class Ellipse(Patch):
         gc.set_linewidth(self._linewidth)
         gc.set_alpha(self._alpha)
         gc.set_antialiased(self._antialiased)
-        if self.get_clip_on(): gc.set_clip_rectangle(
-            self.clipbox.get_bounds())
+        if self.get_clip_on():
+            gc.set_clip_rectangle(self.clipbox.get_bounds())
         gc.set_capstyle('projecting')
 
         if not self.fill or self._facecolor is None: rgbFace = None
@@ -761,11 +750,13 @@ class Ellipse(Patch):
         if self._hatch:
             gc.set_hatch(self._hatch )
 
-        tverts = self.get_transform().seq_xy_tups(self.get_verts()) # center is first vert
+        tverts = self.get_transform().seq_xy_tups(self.get_verts())
+        # center is first vert
         width = tverts[3,0] - tverts[1,0]
         height = tverts[2,1] - tverts[4,1]
 
-        renderer.draw_arc(gc, rgbFace, tverts[0,0], tverts[0,1], width, height, 0.0, 360.0, self.angle)
+        renderer.draw_arc(gc, rgbFace, tverts[0,0], tverts[0,1],
+                          width, height, 0.0, 360.0, self.angle)
 
 class Circle(Ellipse):
     """

commit 0c2ca5383838c94a60cb6425089cba7988872a57

revert lines.py to correct error introduced in r3178
svn path=/trunk/matplotlib/; revision=3195

lib/matplotlib/lines.py
@@ -708,14 +708,7 @@ class Line2D(Artist):
             path.end_poly()
             renderer.draw_markers(gc, path, rgbFace, xt, yt, self.get_transform())
         else:
-            # Use of draw_arc is left effectively disabled (since
-            # now self._newstyle is always True) because it is
-            # much slower (for Agg) than using the polygon approx with
-            # draw_markers.  To speed it up we need a
-            # draw_arcs or draw_circles backend counterpart to draw_markers.
-            xtt, ytt = self.get_transform().numerix_x_y(xt, yt)
-            for i, x in enumerate(xtt):
-                y = ytt[i]
+            for (x,y) in zip(xt,yt):
                 renderer.draw_arc(gc, rgbFace,
                                   x, y, w, w, 0.0, 360.0, 0.0)
 