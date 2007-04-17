e927e25e0289e406ac525f98c490e44784b9b319
John Hunter <jdh2358@gmail.com>
2007-04-17 14:02:52 +0000

    added autofmt for date plots
    svn path=/trunk/matplotlib/; revision=3239

--- examples/load_converter.py
+++ examples/load_converter.py
@@ -1,10 +1,14 @@
-from pylab import figure, show, datestr2num, load
+from matplotlib.dates import strpdate2num
+from matplotlib.mlab import load
+from pylab import figure, show
+
 dates, closes = load(
     'data/msft.csv', delimiter=',',
-    converters={0:datestr2num}, skiprows=1, usecols=(0,2),
-    unpack=True)
+    converters={0:strpdate2num('%d-%b-%y')},
+    skiprows=1, usecols=(0,2), unpack=True)
 
 fig = figure()
 ax = fig.add_subplot(111)
-ax.plot_date(dates, closes)
+ax.plot_date(dates, closes, '-')
+fig.autofmt_xdate()
 show()
