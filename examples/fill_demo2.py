9b9e42cf049d09ac1d69810f902ce1ef5bff8251
Jeff Whitaker <jswhit@fastmail.fm>
2007-05-22 11:44:03 +0000

    fix typos that prevented script from working
    svn path=/trunk/matplotlib/; revision=3289

--- examples/fill_demo2.py
+++ examples/fill_demo2.py
@@ -1,8 +1,8 @@
-from pylab import fig, nx, show
+from pylab import figure, nx, show
 fig = figure()
-ax = fig,add_subplot(111)
+ax = fig.add_subplot(111)
 t = nx.arange(0.0,3.01,0.01)
 s = nx.sin(2*nx.pi*t)
 c = nx.sin(4*nx.pi*t)
-ax.fill(t, s, 'blue', t, c, 'green', alpha=0.2)
+ax.fill(t, s, 'b', t, c, 'g', alpha=0.2)
 show()
