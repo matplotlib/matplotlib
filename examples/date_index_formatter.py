341b28a76158f65bf1930b199d72f7a640d2d665
John Hunter <jdh2358@gmail.com>
2007-06-11 20:57:42 +0000

    added index date formatter example
    svn path=/trunk/matplotlib/; revision=3385

---
+++ examples/date_index_formatter.py
@@ -0,0 +1,37 @@
+
+"""
+When plotting daily data, a frequent request is to plot the data
+ignoring skips, eg no extra spaces for weekends.  This is particularly
+common in financial time series, when you may have data for M-F and
+not Sat, Sun and you don't want gaps in the x axis.  The approach is
+to simply use the integer index for the xdata and a custom tick
+Formatter to get the appropriate date string for a given index.
+"""
+
+import numpy
+from matplotlib.mlab import csv2rec
+from pylab import figure, show
+from matplotlib.dates import Formatter
+
+r = csv2rec('data/msft.csv')[-40:]
+
+class MyFormatter(Formatter):
+    def __init__(self, dates, fmt='%Y-%m-%d'):
+        self.dates = dates
+        self.fmt = fmt
+
+    def __call__(self, x, pos=0):
+        'Return the label for time x at position pos'
+        ind = int(round(x))
+        if ind>=len(self.dates) or ind<0: return ''
+
+        return self.dates[ind].strftime(self.fmt)
+
+formatter = MyFormatter(r.date)
+
+fig = figure()
+ax = fig.add_subplot(111)
+ax.xaxis.set_major_formatter(formatter)
+ax.plot(numpy.arange(len(r)), r.close, 'o-')
+fig.autofmt_xdate()
+show()
