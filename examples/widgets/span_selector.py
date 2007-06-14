fe0a2825265ee2416f742f301a5b9c6ba79972a1
John Hunter <jdh2358@gmail.com>
2007-06-14 13:29:45 +0000

    updated span selector example
    svn path=/trunk/matplotlib/; revision=3392

--- examples/widgets/span_selector.py
+++ examples/widgets/span_selector.py
@@ -1,33 +1,40 @@
 #!/usr/bin/env python
 """
-The SpanSelector is a mouse widget to select a vmin/vmax
-range.  When you left click drag in the axes, a rectangle shows the
-selected region.  When you release, the rectangle disappears and a
-callback is called with min/max.
+The SpanSelector is a mouse widget to select a xmin/xmax range and plot the
+detail view of the selected region in the lower axes
 """
-import pylab
+import numpy as npy
+from pylab import figure, show
 from matplotlib.widgets import SpanSelector
 
-fig = pylab.figure(figsize=(8,6))
+fig = figure(figsize=(8,6))
 ax = fig.add_subplot(211, axisbg='#FFFFCC')
 
-x,y = 4*(pylab.rand(2,100)-.5)
-ax.plot(x,y,'o')
-ax.set_xlim(-2,2)
+x = npy.arange(0.0, 5.0, 0.01)
+y = npy.sin(2*npy.pi*x) + 0.5*npy.random.randn(len(x))
+
+ax.plot(x, y, '-')
 ax.set_ylim(-2,2)
 ax.set_title('Press left mouse button and drag to test')
 
+ax2 = fig.add_subplot(212, axisbg='#FFFFCC')
+line2, = ax2.plot(x, y, '-')
 
-def onselect(vmin, vmax):
-    print vmin, vmax
+
+def onselect(xmin, xmax):
+    indmin, indmax = npy.searchsorted(x, (xmin, xmax))
+    indmax = min(len(x)-1, indmax)
+
+    thisx = x[indmin:indmax]
+    thisy = y[indmin:indmax]
+    line2.set_data(thisx, thisy)
+    ax2.set_xlim(thisx[0], thisx[-1])
+    ax2.set_ylim(thisy.min(), thisy.max())
+    fig.canvas.draw()
 
 # set useblit True on gtkagg for enhanced performance
 span = SpanSelector(ax, onselect, 'horizontal', useblit=False,
                     rectprops=dict(alpha=0.5, facecolor='red') )
 
-ax2 = fig.add_subplot(212)
-ax2.plot([1,2,3])
 
-span2 = SpanSelector(ax2, onselect, 'vertical')
-
-pylab.show()
+show()
