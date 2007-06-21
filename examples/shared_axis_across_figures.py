a862f0d1335edbcc172e99cd3896e01024f4177b
John Hunter <jdh2358@gmail.com>
2007-06-21 22:20:51 +0000

    added shared axis demo
    svn path=/trunk/matplotlib/; revision=3405

---
+++ examples/shared_axis_across_figures.py
@@ -0,0 +1,26 @@
+"""
+connect the data limits on the axes in one figure with the axes in
+another.  This is not the right way to do this for two axes in the
+same figure -- use the sharex and sharey property in that case
+"""
+import numpy
+from pylab import figure, show
+
+fig1 = figure()
+fig2 = figure()
+
+ax1 = fig1.add_subplot(111)
+ax2 = fig2.add_subplot(111)
+
+ax1.plot(numpy.random.rand(100), 'o')
+ax2.plot(numpy.random.rand(100), 'o')
+
+ax1.sharex_foreign(ax2)
+ax2.sharex_foreign(ax1)
+
+ax1.sharey_foreign(ax2)
+ax2.sharey_foreign(ax1)
+
+
+
+show()
