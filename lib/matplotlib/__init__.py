0c1956f1faf8ab759b2c0653e9f2d699715c9f97
Norbert Nemec <Norbert@Nemec-online.de>
2007-06-30 20:36:09 +0000

    code reorganization to remove active call to 'get_data_path' from otherwise static list of rcdefault values
    svn path=/trunk/matplotlib/; revision=3430

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -309,107 +309,6 @@ class Verbose:
 
 verbose=Verbose()
 
-def _get_home():
-    """Find user's home directory if possible.
-    Otherwise raise error.
-
-    :see:  http://mail.python.org/pipermail/python-list/2005-February/263921.html
-    """
-    path=''
-    try:
-        path=os.path.expanduser("~")
-    except:
-        pass
-    if not os.path.isdir(path):
-        for evar in ('HOME', 'USERPROFILE', 'TMP'):
-            try:
-                path = os.environ[evar]
-                if os.path.isdir(path):
-                    break
-            except: pass
-    if path:
-        return path
-    else:
-        raise RuntimeError('please define environment variable $HOME')
-
-
-
-get_home = verbose.wrap('$HOME=%s', _get_home, always=False)
-
-def _get_configdir():
-    """
-    Return the string representing the configuration dir.  
-
-    default is HOME/.matplotlib.  you can override this with the
-    MPLCONFIGDIR environment variable
-    """
-
-    configdir = os.environ.get('MPLCONFIGDIR')
-    if configdir is not None:
-        if not _is_writable_dir(configdir):
-            raise RuntimeError('Could not write to MPLCONFIGDIR="%s"'%configdir)
-        return configdir
-    
-    h = get_home()
-    p = os.path.join(get_home(), '.matplotlib')
-    
-    if os.path.exists(p):
-        if not _is_writable_dir(p):
-            raise RuntimeError("'%s' is not a writable dir; you must set %s/.matplotlib to be a writable dir.  You can also set environment variable MPLCONFIGDIR to any writable directory where you want matplotlib data stored "%h)
-    else:
-        if not _is_writable_dir(h):
-            raise RuntimeError("Failed to create %s/.matplotlib; consider setting MPLCONFIGDIR to a writable directory for matplotlib configuration data"%h)
-
-        os.mkdir(p)
-
-    return p
-get_configdir = verbose.wrap('CONFIGDIR=%s', _get_configdir, always=False)
-
-
-def _get_data_path():
-    'get the path to matplotlib data'
-
-    if os.environ.has_key('MATPLOTLIBDATA'):
-        path = os.environ['MATPLOTLIBDATA']
-        if os.path.isdir(path): return path
-
-    path = os.sep.join([os.path.dirname(__file__), 'mpl-data'])
-    if os.path.isdir(path): return path
-
-    # setuptools' namespace_packages may highjack this init file
-    # so need to try something known to be in matplotlib, not basemap
-    import matplotlib.afm
-    path = os.sep.join([os.path.dirname(matplotlib.afm.__file__), 'mpl-data'])
-    if os.path.isdir(path): return path
-
-    # py2exe zips pure python, so still need special check
-    if getattr(sys,'frozen',None):
-        path = os.path.join(os.path.split(sys.path[0])[0], 'matplotlibdata')
-        if os.path.isdir(path):  return path
-        else:
-            # Try again assuming sys.path[0] is a dir not a exe
-            path = os.path.join(sys.path[0], 'matplotlibdata')
-            if os.path.isdir(path): return path
-
-    raise RuntimeError('Could not find the matplotlib data files')
-
-get_data_path = verbose.wrap('matplotlib data path %s', _get_data_path, always=False)
-
-def get_py2exe_datafiles():
-    datapath = get_data_path()
-    head, tail = os.path.split(datapath)
-    d = {}
-    for root, dirs, files in os.walk(datapath):
-        # Need to explicitly remove cocoa_agg files or py2exe complains
-        # NOTE I dont know why, but do as previous version
-        if 'Matplotlib.nib' in files:
-            files.remove('Matplotlib.nib')
-        files = [os.path.join(root, filename) for filename in files]
-        root = root.replace(tail, 'matplotlibdata')
-        root = root[root.index('matplotlibdata'):]
-        d[root] = files
-    return d.items()
-
 def checkdep_dvipng():
     try:
         stdin, stdout = os.popen4('dvipng -version')
@@ -763,7 +662,7 @@ defaultParams = {
     'numerix'           : ['numpy', validate_numerix],
     'maskedarray'       : [False, validate_bool],
     'toolbar'           : ['toolbar2', validate_toolbar],
-    'datapath'          : [get_data_path(), validate_path_exists],
+    'datapath'          : [None, validate_path_exists],   # handled by _get_data_path_cached
     'units'             : [False, validate_bool],
     'interactive'       : [False, validate_bool],
     'timezone'          : ['UTC', str],
@@ -927,6 +826,115 @@ defaultParams = {
     }
 
 
+def _get_home():
+    """Find user's home directory if possible.
+    Otherwise raise error.
+
+    :see:  http://mail.python.org/pipermail/python-list/2005-February/263921.html
+    """
+    path=''
+    try:
+        path=os.path.expanduser("~")
+    except:
+        pass
+    if not os.path.isdir(path):
+        for evar in ('HOME', 'USERPROFILE', 'TMP'):
+            try:
+                path = os.environ[evar]
+                if os.path.isdir(path):
+                    break
+            except: pass
+    if path:
+        return path
+    else:
+        raise RuntimeError('please define environment variable $HOME')
+
+
+
+get_home = verbose.wrap('$HOME=%s', _get_home, always=False)
+
+def _get_configdir():
+    """
+    Return the string representing the configuration dir.  
+
+    default is HOME/.matplotlib.  you can override this with the
+    MPLCONFIGDIR environment variable
+    """
+
+    configdir = os.environ.get('MPLCONFIGDIR')
+    if configdir is not None:
+        if not _is_writable_dir(configdir):
+            raise RuntimeError('Could not write to MPLCONFIGDIR="%s"'%configdir)
+        return configdir
+    
+    h = get_home()
+    p = os.path.join(get_home(), '.matplotlib')
+    
+    if os.path.exists(p):
+        if not _is_writable_dir(p):
+            raise RuntimeError("'%s' is not a writable dir; you must set %s/.matplotlib to be a writable dir.  You can also set environment variable MPLCONFIGDIR to any writable directory where you want matplotlib data stored "%h)
+    else:
+        if not _is_writable_dir(h):
+            raise RuntimeError("Failed to create %s/.matplotlib; consider setting MPLCONFIGDIR to a writable directory for matplotlib configuration data"%h)
+
+        os.mkdir(p)
+
+    return p
+get_configdir = verbose.wrap('CONFIGDIR=%s', _get_configdir, always=False)
+
+
+def _get_data_path():
+    'get the path to matplotlib data'
+
+    if os.environ.has_key('MATPLOTLIBDATA'):
+        path = os.environ['MATPLOTLIBDATA']
+        if not os.path.isdir(path):
+            raise RuntimeError('Path in environment MATPLOTLIBDATA not a directory')
+        return path
+
+    path = os.sep.join([os.path.dirname(__file__), 'mpl-data'])
+    if os.path.isdir(path): return path
+
+    # setuptools' namespace_packages may highjack this init file
+    # so need to try something known to be in matplotlib, not basemap
+    import matplotlib.afm
+    path = os.sep.join([os.path.dirname(matplotlib.afm.__file__), 'mpl-data'])
+    if os.path.isdir(path): return path
+
+    # py2exe zips pure python, so still need special check
+    if getattr(sys,'frozen',None):
+        path = os.path.join(os.path.split(sys.path[0])[0], 'matplotlibdata')
+        if os.path.isdir(path): return path
+        else:
+            # Try again assuming sys.path[0] is a dir not a exe
+            path = os.path.join(sys.path[0], 'matplotlibdata')
+            if os.path.isdir(path): return path
+
+    raise RuntimeError('Could not find the matplotlib data files')
+
+def _get_data_path_cached():
+    if defaultParams['datapath'][0] is None:
+        defaultParams['datapath'][0] = _get_data_path()
+    return defaultParams['datapath'][0]
+
+get_data_path = verbose.wrap('matplotlib data path %s', _get_data_path_cached, always=False)
+
+def get_py2exe_datafiles():
+    datapath = get_data_path()
+    head, tail = os.path.split(datapath)
+    d = {}
+    for root, dirs, files in os.walk(datapath):
+        # Need to explicitly remove cocoa_agg files or py2exe complains
+        # NOTE I dont know why, but do as previous version
+        if 'Matplotlib.nib' in files:
+            files.remove('Matplotlib.nib')
+        files = [os.path.join(root, filename) for filename in files]
+        root = root.replace(tail, 'matplotlibdata')
+        root = root[root.index('matplotlibdata'):]
+        d[root] = files
+    return d.items()
+
+
 def matplotlib_fname():
     """
     Return the path to the rc file
@@ -1062,6 +1070,9 @@ def rc_params(fail_on_error=False):
         cval = validate_key(key, val, line, cnt, fname, fail_on_error)
         if cval is not None:
             ret[key] = cval
+
+    if ret['datapath'] is None:
+        ret['datapath'] = get_data_path()
 
     verbose.report('loaded rc file %s'%fname)
 
