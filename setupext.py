3001743457d9ac2ca09b7d3b6ecfcc89c94790b4
Andrew Straw <strawman@astraw.com>
2007-07-13 21:55:07 +0000

    revert r3512, which broke on Mac
    svn path=/trunk/matplotlib/; revision=3515

--- setupext.py
+++ setupext.py
@@ -809,3 +809,11 @@ def build_subprocess(ext_modules, packages):
         )
     add_base_flags(module)
     ext_modules.append(module)
+
+def build_isnan(ext_modules, packages):
+    module = Extension(
+        'matplotlib._isnan',
+        [ 'src/_isnan.c'],
+        )
+    add_base_flags(module)
+    ext_modules.append(module)
--- src/_backend_agg.cpp
+++ src/_backend_agg.cpp
@@ -23,6 +23,7 @@
 #include "_backend_agg.h"
 #include "_transforms.h"
 #include "mplutils.h"
+#include "MPL_isnan.h"
 
 #include "swig_runtime.h"
 
@@ -1584,7 +1585,7 @@ RendererAgg::draw_lines(const Py::Tuple& args) {
         moveto = true;
         continue;
       }
-      if (isnan(thisx) || isnan(thisy)) {
+      if (MPL_isnan64(thisx) || MPL_isnan64(thisy)) {
         moveto = true;
         continue;
       }
