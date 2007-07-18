40f1caf1679d03b991aec17702f723baf72651e9
John Hunter <jdh2358@gmail.com>
2007-07-18 21:52:01 +0000

    screwing aournd with delegates
    svn path=/trunk/matplotlib/; revision=3568

--- mpl1/test.py
+++ mpl1/test.py
@@ -1,15 +1,36 @@
 import numpy
-from enthought.traits.api import HasTraits, Array
-import mtraits
+from enthought.traits.api import HasTraits, Array, Delegate, Trait
+
+Affine = Array('d', (3,3),
+               value=numpy.array([[1,0,0], [0,1,0], [0,0,1]], numpy.float_))
+
+class C(HasTraits):
+    affine1 = Affine
+    affine2 = Affine    
+    affine = Affine
+
+    def _affine1_changed(self, old, new):
+        self.affine = numpy.dot(new, self.affine2)
+
+    def _affine2_changed(self, old, new):
+        self.affine = numpy.dot(self.affine1, new)
 
 
-class Path(HasTraits):
-    """
-    The path is an object that talks to the backends, and is an
-    intermediary between the high level path artists like Line and
-    Polygon, and the backend renderer
-    """
-    strokecolor = mtraits.color('white')
+class D(HasTraits):
+    affine = Delegate('c')
+    c = Trait(C)
 
-p = Path()
-print 'strokecolor', p.strokecolor
+    
+c = C()
+d = D()
+d.affine = c.affine
+
+
+print 'before c', type(c.affine), c.affine
+print 'before d', type(d.affine), d.affine
+
+c.affine1 = numpy.random.rand(3,3)
+print 'after c', type(c.affine), c.affine
+print 'after d', type(d.affine), d.affine
+
+
