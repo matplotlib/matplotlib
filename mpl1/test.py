4c5a25665e72d2c07746ecb7f35a12e6e9762fc0
John Hunter <jdh2358@gmail.com>
2007-07-20 13:44:10 +0000

    traits cleanup
    svn path=/trunk/matplotlib/; revision=3586

--- mpl1/test.py
+++ mpl1/test.py
@@ -1,50 +1,19 @@
-import numpy as npy
-import enthought.traits.api as traits
+from enthought.traits.api import *
+
+def Alias(name):
+    return Property(lambda obj: getattr(obj, name),
+                    lambda obj, val: setattr(obj, name, val))
+
+class Path(HasTraits):
+    strokecolor = Color()
 
 
-class C(traits.HasTraits):
-    x = traits.Float(0.0)
-    y = traits.Float(1.0)
+class Line(Path):
+   color = Alias('strokecolor')
 
-class MyC(C):
-    xy = traits.Float(0.0)
-
-    def __init__(self, c):
-        self.x = c.x
-        self.y = c.y
-
-        c.sync_trait('x', self)
-        c.sync_trait('y', self)
-
-    def _x_changed(self, old, new):
-        self.xy = self.x * self.y
-
-    def _y_changed(self, old, new):
-        self.xy = self.x * self.y
-
-
-# C objects are created at top level
-c = C()
-c.x = 1
-c.y = 1
-
-
-
-
-
-class Backend:
-    
-    def register_c(self, c):
-        # only gets C objects after creation
-        self.myc = MyC(c)
-
-backend = Backend()
-
-backend.register_c(c)
-
-c.x = 4
-
-print backend.myc.xy
-
-
-        
+   def __init__(self, x, color='red'):
+       self.x = x
+       self.color = color
+       
+line = Line(1)
+print line.strokecolor
