4afe225d2c3227e27ba0073063d782c83fba6195
Eric Firing <efiring@hawaii.edu>
2007-07-20 08:49:00 +0000

    Reversed last commit
    svn path=/trunk/matplotlib/; revision=3583

--- npyma/__init__.py
+++
@@ -1,8 +0,0 @@
-from matplotlib.numerix import use_maskedarray
-
-if use_maskedarray:
-    from maskedarray import *
-    print "using maskedarray"
-else:
-    from numpy.core.ma import *
-    #print "using ma"
