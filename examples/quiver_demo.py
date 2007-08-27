97bc0041451890372b4b8c7202e57a76e83f3d88
Michael Droettboom <mdboom@gmail.com>
2007-08-27 19:33:45 +0000

    Allow markup kwarg in more places.
    svn path=/trunk/matplotlib/; revision=3741

--- examples/quiver_demo.py
+++ examples/quiver_demo.py
@@ -17,8 +17,9 @@ V = sin(Y)
 #1
 figure()
 Q = quiver( U, V)
-qk = quiverkey(Q, 0.5, 0.92, 2, '2 m/s', labelpos='W',
-                    fontproperties={'weight': 'bold'})
+qk = quiverkey(Q, 0.5, 0.92, 2, r'$2 \frac{m}{s}$', labelpos='W',
+               fontproperties={'weight': 'bold'},
+               markup="tex")
 l,r,b,t = axis()
 dx, dy = r-l, t-b
 axis([l-0.05*dx, r+0.05*dx, b-0.05*dy, t+0.05*dy])
@@ -28,10 +29,11 @@ title('Minimal arguments, no kwargs')
 #2
 figure()
 Q = quiver( X, Y, U, V, units='width')
-qk = quiverkey(Q, 0.9, 0.95, 2, '2 m/s',
-                            labelpos='E',
-                            coordinates='figure',
-                            fontproperties={'weight': 'bold'})
+qk = quiverkey(Q, 0.9, 0.95, 2, r'$2 \frac{m}{s}$',
+               labelpos='E',
+               coordinates='figure',
+               fontproperties={'weight': 'bold'},
+               markup="tex")
 axis([-1, 7, -1, 7])
 title('scales with plot width, not view')
 
@@ -39,7 +41,7 @@ title('scales with plot width, not view')
 figure()
 Q = quiver( X[::3, ::3], Y[::3, ::3], U[::3, ::3], V[::3, ::3],
             pivot='mid', color='r', units='inches' )
-qk = quiverkey(Q, 0.5, 0.03, 1, '1 m/s', fontproperties={'weight': 'bold'})
+qk = quiverkey(Q, 0.5, 0.03, 1, r'$1 \frac{m}{s}$', fontproperties={'weight': 'bold'}, markup="tex")
 plot( X[::3, ::3], Y[::3, ::3], 'k.')
 axis([-1, 7, -1, 7])
 title("pivot='mid'; every third arrow; units='inches'")
@@ -48,9 +50,10 @@ title("pivot='mid'; every third arrow; units='inches'")
 figure()
 M = sqrt(pow(U, 2) + pow(V, 2))
 Q = quiver( X, Y, U, V, M, units='x', pivot='tip', width=0.022, scale=1/0.15)
-qk = quiverkey(Q, 0.9, 1.05, 1, '1 m/s',
+qk = quiverkey(Q, 0.9, 1.05, 1, r'$1 \frac{m}{s}$',
                             labelpos='E',
-                            fontproperties={'weight': 'bold'})
+                            fontproperties={'weight': 'bold'},
+               markup="tex")
 plot(X, Y, 'k.')
 axis([-1, 7, -1, 7])
 title("scales with x view; pivot='tip'")
@@ -60,7 +63,7 @@ figure()
 Q = quiver( X[::3, ::3], Y[::3, ::3], U[::3, ::3], V[::3, ::3],
              color='r', units='x',
             linewidths=(2,), edgecolors=('k'), headaxislength=5 )
-qk = quiverkey(Q, 0.5, 0.03, 1, '1 m/s', fontproperties={'weight': 'bold'})
+qk = quiverkey(Q, 0.5, 0.03, 1, r'$1 \frac{m}{s}$', fontproperties={'weight': 'bold'}, markup="tex")
 axis([-1, 7, -1, 7])
 title("triangular head; scale with x view; black edges")
 
