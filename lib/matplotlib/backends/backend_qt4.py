84f10e1d0101ccb4b3aacc773b1518cddbe05fa1
Michael Droettboom <mdboom@gmail.com>
2007-09-24 16:54:37 +0000

    Merged revisions 3873-3884 via svnmerge from http://matplotlib.svn.sf.net/svnroot/matplotlib/trunk/matplotlib
    ........
      r3874 | jouni | 2007-09-22 02:48:49 -0400 (Sat, 22 Sep 2007) | 3 lines
      
      Replace some generator expressions by list comprehensions for
      Python 2.3 compatibility
    ........
      r3879 | dsdale | 2007-09-24 08:56:38 -0400 (Mon, 24 Sep 2007) | 2 lines
      
      fix backend_qt* bug with multiple plot windows and show()
    ........
      r3880 | dsdale | 2007-09-24 09:00:12 -0400 (Mon, 24 Sep 2007) | 2 lines
      
      backend_qt* bugfix for multiple plot windows and show()
    ........
      r3881 | dsdale | 2007-09-24 09:01:17 -0400 (Mon, 24 Sep 2007) | 2 lines
      
      fix backend_wxagg reference in new config module
    ........
      r3882 | dsdale | 2007-09-24 09:03:01 -0400 (Mon, 24 Sep 2007) | 3 lines
      
      modifed embedding_in_qt* examples so they can be run from ipython with 
      -q*thread
    ........
      r3883 | dsdale | 2007-09-24 11:11:58 -0400 (Mon, 24 Sep 2007) | 2 lines
      
      fix bug in improved support for multiple windows in backend_qt4
    ........
    
    svn path=/branches/transforms/; revision=3885

--- lib/matplotlib/backends/backend_qt4.py
+++ lib/matplotlib/backends/backend_qt4.py
@@ -46,10 +46,10 @@ def _create_qApp():
         qApp = QtGui.QApplication( [" "] )
         QtCore.QObject.connect( qApp, QtCore.SIGNAL( "lastWindowClosed()" ),
                             qApp, QtCore.SLOT( "quit()" ) )
-    else:
-      # someone else aready created the qApp and
-      # we let them handle the event-loop control.
-      show._needmain = False
+        #remember that matplotlib created the qApp - will be used by show()
+        _create_qApp.qAppCreatedHere = True
+
+_create_qApp.qAppCreatedHere = False
 
 def show():
     """
@@ -65,11 +65,8 @@ def show():
     if figManager != None:
         figManager.canvas.draw()
 
-    if ( show._needmain ):
-      qApp.exec_()
-      show._needmain = False
-
-show._needmain = True
+    if _create_qApp.qAppCreatedHere:
+        QtGui.qApp.exec_()
 
 
 def new_figure_manager( num, *args, **kwargs ):
