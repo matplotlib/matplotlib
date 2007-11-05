8369c9c310224f451b89aeecb78dbc3b1705ab04
Eric Firing <efiring@hawaii.edu>
2007-11-05 19:54:49 +0000

    make step_demo use npyma
    svn path=/trunk/matplotlib/; revision=4113

--- examples/step_demo.py
+++ examples/step_demo.py
@@ -1,5 +1,6 @@
 import numpy as npy
-from pylab import *
+from matplotlib.numerix import npyma as ma
+from matplotlib.pyplot import step, legend, xlim, ylim, show
 
 x = npy.arange(1, 7, 0.4)
 y0 = npy.sin(x)
@@ -13,7 +14,7 @@ step(x, y, where='mid', label='mid')
 y -= 0.5
 step(x, y, where='post', label='post')
 
-y = npy.ma.masked_where((y0>-0.15)&(y0<0.15), y - 0.5)
+y = ma.masked_where((y0>-0.15)&(y0<0.15), y - 0.5)
 step(x,y, label='masked (pre)')
 
 legend()
@@ -22,3 +23,4 @@ xlim(0, 7)
 ylim(-0.5, 4)
 
 show()
+
