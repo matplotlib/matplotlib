93423fcbb6cf5c56bdb02d43819e8f34b3681ee7
Darren Dale <dsdale24@gmail.com>
2007-11-13 13:50:29 +0000

    updated creation of default config files at build time: * modify matplotlib.conf based on available backends and setup.cfg * modify backend selection: SVG, Agg, TkAgg, WXAgg, GTK, GTKAgg, selection in setup.cfg
    svn path=/trunk/matplotlib/; revision=4242

--- lib/matplotlib/config/mplconfig.py
+++ lib/matplotlib/config/mplconfig.py
@@ -65,7 +65,7 @@ class MPLConfig(TConfig):
     class backend(TConfig):
         """Valid backends are: 'GTKAgg', 'GTKCairo', 'QtAgg', 'Qt4Agg',
         'TkAgg', 'Agg', 'Cairo', 'PS', 'PDF', 'SVG'"""
-        use = T.Trait('TkAgg', mplT.BackendHandler())
+        use = T.Trait('Agg', mplT.BackendHandler())
 
         class cairo(TConfig):
             format = T.Trait('png', 'png', 'ps', 'pdf', 'svg')
@@ -525,5 +525,5 @@ def rcdefaults():
 ##############################################################################
 if __name__ == "__main__":
     mplConfig = MPLConfig()
-    tconf2File(mplConfig, '../mpl-data/matplotlib.conf', force=True)
-    print 'Default matplotlib.conf created in ../mpl-data'
+    tconf2File(mplConfig, '../mpl-data/matplotlib.conf.template', force=True)
+    print 'matplotlib.conf.template created in ../mpl-data'
