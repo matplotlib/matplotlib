0066c56adf548c0797d88aa3e5afc496a59dc58d
John Hunter <jdh2358@gmail.com>
2007-12-17 04:37:38 +0000

    mods to support dates in csv2rec and friends
    svn path=/trunk/matplotlib/; revision=4755

--- unit/mlab_unit.py
+++ unit/mlab_unit.py
@@ -1,7 +1,6 @@
-import unittest
+import datetime, StringIO, unittest
 import matplotlib.mlab as mlab
 import numpy
-import StringIO
 
 class TestMlab(unittest.TestCase):
     def test_csv2rec_closefile(self):
@@ -21,11 +20,22 @@ class TestMlab(unittest.TestCase):
         # lost precision when passing through repr(). csv2rec was
         # affected by this. This test will only pass on numpy >=
         # 1.0.5.
-        ra=numpy.rec.array([(123, 1197346475.0137341, 'a,bc'),
-                            (456, 123.456, 'd\'ef'),
-                            (789, 0.000000001, 'ghi'),
+        delta = datetime.timedelta(days=1)
+        date0 = datetime.date(2007,12,16)
+        date1 = date0 + delta
+        date2 = date1 + delta
+
+        delta = datetime.timedelta(days=1)
+        datetime0 = datetime.datetime(2007,12,16,22,29,34,924122)
+        datetime1 = datetime0 + delta
+        datetime2 = datetime1 + delta
+        ra=numpy.rec.fromrecords([
+                (123, date0, datetime0, 1197346475.0137341, 'a,bc'),
+                (456, date1, datetime1, 123.456, 'd\'ef'),
+                (789, date2, datetime2, 0.000000001, 'ghi'),
                             ],
-                           dtype=[('a', '<i8'), ('b', '<f8'), ('c', '|S3')])
+            names='intdata,datedata,datetimedata,floatdata,stringdata')
+
         fh = StringIO.StringIO()
         mlab.rec2csv( ra, fh )
         fh.seek(0)
@@ -36,6 +46,8 @@ class TestMlab(unittest.TestCase):
             fh.seek(0)
         ra2 = mlab.csv2rec(fh)
         fh.close()
+        #print 'ra', ra
+        #print 'ra2', ra2
         for name in ra.dtype.names:
             if 0:
                 print name, repr(ra[name]), repr(ra2[name])
