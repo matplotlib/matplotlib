f22ef5b506c73e4d763d7fdbabe62b5b0625452f
Eric Firing <efiring@hawaii.edu>
2007-12-26 07:23:27 +0000

    Make numerix.ma and numerix.npyma work with numpy 1.05
    The numpy maskedarray branch is scheduled to become the
    trunk for 1.05.  It includes a change from ma.py being in
    numpy/core to ma being a module under numpy, so the import
    syntax is different in numerix.ma and numerix.npyma.
    
    svn path=/trunk/matplotlib/; revision=4788

--- lib/matplotlib/numerix/npyma/__init__.py
+++ lib/matplotlib/numerix/npyma/__init__.py
@@ -4,5 +4,8 @@ if use_maskedarray:
     from maskedarray import *
     print "using maskedarray"
 else:
-    from numpy.core.ma import *
+    try:
+        from numpy.ma import *        # numpy 1.05 and later
+    except ImportError:
+        from numpy.core.ma import *   # earlier
     #print "using ma"
