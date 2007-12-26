3b6ba9ebb35542976b2baefce5fd178a832332a3
Eric Firing <efiring@hawaii.edu>
2007-12-26 07:51:19 +0000

    Fix bug in errorbar, reported by Noriko Minakawa
    svn path=/trunk/matplotlib/; revision=4789

--- lib/matplotlib/axes.py
+++ lib/matplotlib/axes.py
@@ -3684,6 +3684,8 @@ class Axes(martist.Artist):
             lines_kw['linewidth']=kwargs['linewidth']
         if 'lw' in kwargs:
             lines_kw['lw']=kwargs['lw']
+        if 'transform' in kwargs:
+            lines_kw['transform'] = kwargs['transform']
 
         # arrays fine here, they are booleans and hence not units
         if not iterable(lolims):
@@ -3719,6 +3721,8 @@ class Axes(martist.Artist):
                 plot_kw['markeredgewidth']=kwargs['markeredgewidth']
             if 'mew' in kwargs:
                 plot_kw['mew']=kwargs['mew']
+            if 'transform' in kwargs:
+                plot_kw['transform'] = kwargs['transform']
 
         if xerr is not None:
             if iterable(xerr) and len(xerr)==2 and iterable(xerr[0]) and iterable(xerr[1]):
