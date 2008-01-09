ef54a1fbff4ea3f3c033d2f3499eb3aa02bb8067
Darren Dale <dsdale24@gmail.com>
2008-01-09 19:08:08 +0000

    added figure.autolayout setting to traited config
    svn path=/trunk/matplotlib/; revision=4831

--- lib/matplotlib/config/mplconfig.py
+++ lib/matplotlib/config/mplconfig.py
@@ -238,6 +238,7 @@ class MPLConfig(TConfig):
         dpi = T.Float(80)
         facecolor = T.Trait('0.75', mplT.ColorHandler())
         edgecolor = T.Trait('white', mplT.ColorHandler())
+        autolayout = T.false
 
         class subplot(TConfig):
             """The figure subplot parameters.  All dimensions are fraction
@@ -407,6 +408,7 @@ class RcParamsWrapper(dict):
         'figure.dpi' : (self.tconfig.figure, 'dpi'),
         'figure.facecolor' : (self.tconfig.figure, 'facecolor'),
         'figure.edgecolor' : (self.tconfig.figure, 'edgecolor'),
+        'figure.autolayout' : (self.tconfig.figure, 'autolayout'),
 
         'figure.subplot.left' : (self.tconfig.figure.subplot, 'left'),
         'figure.subplot.right' : (self.tconfig.figure.subplot, 'right'),
