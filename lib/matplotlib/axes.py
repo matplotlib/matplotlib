e6315e01ae6a4d5d555b2dea104454d5d460b6c1
Michael Droettboom <mdboom@gmail.com>
2008-01-23 18:27:05 +0000

    Fix relim again (thanks Darren Dale)
    svn path=/trunk/matplotlib/; revision=4890

--- lib/matplotlib/axes.py
+++ lib/matplotlib/axes.py
@@ -1200,12 +1200,11 @@ class Axes(martist.Artist):
     def relim(self):
         'recompute the datalimits based on current artists'
         self.dataLim.ignore(True)
+        self.ignore_existing_data_limits = True
         for line in self.lines:
-            self.ignore_existing_data_limits = True
             self._update_line_limits(line)
 
         for p in self.patches:
-            self.ignore_existing_data_limits = True
             self._update_patch_limits(p)
 
     def update_datalim(self, xys):
