c53a5f829d4f1998bc13c18158989bd88e93a087
Darren Dale <dsdale24@gmail.com>
2008-03-14 12:22:28 +0000

    removed an unnecessary call to FigureCanvasAgg.draw in the qt*agg backends
    svn path=/trunk/matplotlib/; revision=5002

--- lib/matplotlib/backends/backend_qt4agg.py
+++ lib/matplotlib/backends/backend_qt4agg.py
@@ -130,7 +130,6 @@ class FigureCanvasQTAgg( FigureCanvasQT, FigureCanvasAgg ):
 
         if DEBUG: print "FigureCanvasQtAgg.draw", self
         self.replot = True
-        FigureCanvasAgg.draw(self)
         self.update()
 
     def blit(self, bbox=None):
