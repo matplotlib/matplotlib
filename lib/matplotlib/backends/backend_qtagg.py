bcaad2780f646537a373bc8708ba9c1646edc61e
Darren Dale <dsdale24@gmail.com>
2008-04-11 15:16:16 +0000

    revert commits 5002, 5031
    svn path=/trunk/matplotlib/; revision=5037

--- lib/matplotlib/backends/backend_qtagg.py
+++ lib/matplotlib/backends/backend_qtagg.py
@@ -136,6 +136,7 @@ class FigureCanvasQTAgg( FigureCanvasAgg, FigureCanvasQT ):
         if DEBUG: print "FigureCanvasQtAgg.draw", self
         self.replot = True
         FigureCanvasAgg.draw(self)
+        self.repaint(False)
 
     def blit(self, bbox=None):
         """
