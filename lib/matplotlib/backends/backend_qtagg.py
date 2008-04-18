7d74ad51276a6ef6ce4c980cd0c7e24f51ade322
Darren Dale <dsdale24@gmail.com>
2008-04-18 15:20:03 +0000

    dont remove repaint/update from draw(), or updates to the figure after the initial draw will be ignored. We need a different solution for the double drawing problem reported for the Wx and Qt* backends
    svn path=/branches/v0_91_maint/; revision=5046

--- lib/matplotlib/backends/backend_qtagg.py
+++ lib/matplotlib/backends/backend_qtagg.py
@@ -135,6 +135,7 @@ class FigureCanvasQTAgg( FigureCanvasAgg, FigureCanvasQT ):
         if DEBUG: print "FigureCanvasQtAgg.draw", self
         self.replot = True
         FigureCanvasAgg.draw(self)
+        self.repaint(False)
 
     def blit(self, bbox=None):
         """
