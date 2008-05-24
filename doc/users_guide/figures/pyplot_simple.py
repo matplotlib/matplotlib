31295ec05f498ff4a72740296d7feb9fb8567456
John Hunter <jdh2358@gmail.com>
2008-05-24 21:05:14 +0000

    removed old doc dir
    svn path=/trunk/matplotlib/; revision=5255

--- doc/users_guide/figures/pyplot_simple.py
+++
@@ -1,4 +0,0 @@
-import matplotlib.pyplot as plt
-plt.plot([1,2,3])
-plt.ylabel('some numbers')
-
--- doc/users_guide/figures/pyplot_three.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-# evenly sampled time at 200ms intervals
-t = np.arange(0., 5., 0.2)
-
-# red dashes, blue squares and green triangles
-plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
-
