bec2a7f3cacefaef4e73bbce639c312af9ee83e9
John Hunter <jdh2358@gmail.com>
2008-05-24 21:04:44 +0000

    fixed the latex build
    svn path=/trunk/matplotlib/; revision=5254

--- docs/conf.py
+++ docs/conf.py
@@ -139,9 +139,11 @@ latex_font_size = '11pt'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, document class [howto/manual]).
+
 latex_documents = [
-  ('index', 'Matplotlib.tex', 'Matplotlib', 'John Hunter, Darren Dale', 'Michael Droettboom', 'manual'),
+  ('index', 'Matplotlib.tex', 'Matplotlib', 'John Hunter, Darren Dale, Michael Droettboom', 'manual'),
 ]
+
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
