e6709fc92b99047e485a6c293918e2cfd98fb3be
John Hunter <jdh2358@gmail.com>
2008-05-26 17:04:38 +0000

    added a line vertex selector widget
    svn path=/branches/v0_91_maint/; revision=5272

--- lib/matplotlib/lines.py
+++ lib/matplotlib/lines.py
@@ -1467,8 +1467,33 @@ class Line2D(Artist):
 class VertexSelector:
     """
     manage the callbacks to maintain a list of selected vertices for
-    matplotlib.lines.Lin2D. Derived classes should override
+    matplotlib.lines.Line2D. Derived classes should override
     process_selected to do something with the picks
+
+    Here is an example which highlights the selected verts with red
+    circles::
+
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import matplotlib.lines as lines
+
+        class HighlightSelected(lines.VertexSelector):
+            def __init__(self, line, fmt='ro', **kwargs):
+                lines.VertexSelector.__init__(self, line)
+                self.markers, = self.axes.plot([], [], fmt, **kwargs)
+
+            def process_selected(self, ind, xs, ys):
+                self.markers.set_data(xs, ys)
+                self.canvas.draw()
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        x, y = np.random.rand(2, 30)
+        line, = ax.plot(x, y, 'bs-', picker=5)
+
+        selector = HighlightSelected(line)
+        plt.show()
+
     """
     def __init__(self, line):
         """
