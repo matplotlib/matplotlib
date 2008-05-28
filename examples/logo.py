f4fed8831da3230f6bee499b84ba6beaa8b769d1
John Hunter <jdh2358@gmail.com>
2008-05-28 18:03:15 +0000

    added keywords to configure sliders for sf patch 1866207
    svn path=/branches/v0_91_maint/; revision=5292

--- examples/logo.py
+++ examples/logo.py
@@ -8,7 +8,7 @@ x = 1000*0.1*fromstring(
     file('data/membrane.dat', 'rb').read(), float32)
 # 0.0005 is the sample interval
 t = 0.0005*arange(len(x))
-figure(1, figsize=(7,1), dpi=100)
+figure(1, figsize=(4,1), dpi=70)
 ax = subplot(111, axisbg='y')
 plot(t, x)
 text(0.5, 0.5,'matplotlib', color='r',
@@ -20,5 +20,5 @@ text(0.5, 0.5,'matplotlib', color='r',
 axis([1, 1.72,-60, 10])
 setp(gca(), 'xticklabels', [])
 setp(gca(), 'yticklabels', [])
-#savefig('logo2.png', dpi=300)
+savefig('logo2.png', dpi=70)
 show()
