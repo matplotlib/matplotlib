c6da3da04af4c59ab95c5ce1524eb0ae1bc1ffa8
John Hunter <jdh2358@gmail.com>
2008-05-29 21:12:59 +0000

    added demo showing how to use curvy paths directly
    svn path=/trunk/matplotlib/; revision=5310

---
+++ examples/api/path_patch_demo.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.path as mpath
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+
+Path = mpath.Path
+
+pathdata = [
+    (Path.MOVETO, (0, 0)),
+    (Path.CURVE4, (-1, 0)),
+    (Path.CURVE4, (-1, 1)),
+    (Path.CURVE4, (0, 1)),
+    (Path.LINETO, (2, 1)),
+    (Path.CURVE4, (3, 1)),
+    (Path.CURVE4, (3, 0)),
+    (Path.CURVE4, (2, 0)),
+    (Path.CLOSEPOLY, (0, 0)),
+    ]
+
+codes, verts = zip(*pathdata)
+path = mpath.Path(verts, codes)
+
+patch = mpatches.PathPatch(path, facecolor='green', edgecolor='yellow', alpha=0.5)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.add_patch(patch)
+
+ax.set_xlim(-5,5)
+ax.set_ylim(-5,5)
+
+plt.show()
+
+
