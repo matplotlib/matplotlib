c690d194b13e7ecc48ca3c8cff7dfa50e7b3a1d0
John Hunter <jdh2358@gmail.com>
2008-05-30 16:58:02 +0000

    updated path example
    svn path=/trunk/matplotlib/; revision=5322

--- examples/api/path_patch_demo.py
+++ examples/api/path_patch_demo.py
@@ -9,26 +9,28 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 
 pathdata = [
-    (Path.MOVETO, (0, 0)),
-    (Path.CURVE4, (-1, 0)),
-    (Path.CURVE4, (-1, 1)),
-    (Path.CURVE4, (0, 1)),
-    (Path.LINETO, (2, 1)),
-    (Path.CURVE4, (3, 1)),
-    (Path.CURVE4, (3, 0)),
-    (Path.CURVE4, (2, 0)),
-    (Path.CLOSEPOLY, (0, 0)),
+    (Path.MOVETO, (1.58, -2.57)),
+    (Path.CURVE4, (0.35, -1.1)),
+    (Path.CURVE4, (-1.75, 2.0)),
+    (Path.CURVE4, (0.375, 2.0)),
+    (Path.LINETO, (0.85, 1.15)),
+    (Path.CURVE4, (2.2, 3.2)),
+    (Path.CURVE4, (3, 0.05)),
+    (Path.CURVE4, (2.0, -0.5)),
+    (Path.CLOSEPOLY, (1.58, -2.57)),
     ]
 
 codes, verts = zip(*pathdata)
 path = mpath.Path(verts, codes)
-patch = mpatches.PathPatch(path, facecolor='green', edgecolor='yellow', alpha=0.5)
+patch = mpatches.PathPatch(path, facecolor='red', edgecolor='yellow', alpha=0.5)
 ax.add_patch(patch)
 
-
-ax.set_xlim(-5,5)
-ax.set_ylim(-5,5)
-
+x, y = zip(*path.vertices)
+line, = ax.plot(x, y, 'go-')
+ax.grid()
+ax.set_xlim(-3,4)
+ax.set_ylim(-3,4)
+ax.set_title('spline paths')
 plt.show()
 
 
