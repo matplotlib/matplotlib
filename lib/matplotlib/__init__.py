f42da8a909d00343f99906e270dd9d64b27cb10e
Charles Moad <cwmoad@gmail.com>
2008-05-30 03:09:53 +0000

    new release prep
    svn path=/trunk/matplotlib/; revision=5314

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -55,7 +55,7 @@ gmail.com and a host of others).
 """
 from __future__ import generators
 
-__version__  = '0.98pre'
+__version__  = '0.98.0'
 __revision__ = '$Revision$'
 __date__     = '$Date$'
 
