0964b8af3871f9fd8e906e64832404766258b841
Eric Firing <efiring@hawaii.edu>
2008-06-09 17:13:29 +0000

    Try again to make temporary fix for backend case-sensitivity
    svn path=/trunk/matplotlib/; revision=5435

--- lib/matplotlib/backends/__init__.py
+++ lib/matplotlib/backends/__init__.py
@@ -1,19 +1,21 @@
 
 import matplotlib
 from matplotlib.rcsetup import interactive_bk
+from matplotlib.rcsetup import non_interactive_bk
+from matplotlib.rcsetup import all_backends
 from matplotlib.rcsetup import validate_backend
 
 __all__ = ['backend','show','draw_if_interactive',
            'new_figure_manager', 'backend_version']
 
-backend = matplotlib.get_backend()   # makes sure it is lower case
-validate_backend(backend)
+backend = matplotlib.get_backend() # validates, to match all_backends
 
 def pylab_setup():
     'return new_figure_manager, draw_if_interactive and show for pylab'
     # Import the requested backend into a generic module object
 
     backend_name = 'backend_'+backend
+    backend_name = backend_name.lower() # until we banish mixed case
     backend_mod = __import__('matplotlib.backends.'+backend_name,
                              globals(),locals(),[backend_name])
 
@@ -36,7 +38,7 @@ def pylab_setup():
 
     # Additional imports which only happen for certain backends.  This section
     # should probably disappear once all backends are uniform.
-    if backend in ['wx','wxagg']:
+    if backend.lower() in ['wx','wxagg']:
         Toolbar = backend_mod.Toolbar
         __all__.append('Toolbar')
 
