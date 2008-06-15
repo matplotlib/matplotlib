15ec8a47b469a5e1e39ddb8d675f6d1183fec9e9
Andrew Straw <strawman@astraw.com>
2008-06-15 20:38:00 +0000

    fix example to run with current MPL and numpy
    svn path=/trunk/matplotlib/; revision=5549

--- examples/units/date_converter.py
+++ examples/units/date_converter.py
@@ -1,7 +1,8 @@
 import date_support # set up the date converters
 import datetime
 from matplotlib.dates import drange
-from pylab import figure, show, nx
+from pylab import figure, show
+import numpy as np
 
 
 xmin = datetime.date(2007,1,1)
@@ -12,7 +13,7 @@ xdates = drange(xmin, xmax, delta)
 fig = figure()
 fig.subplots_adjust(bottom=0.2)
 ax = fig.add_subplot(111)
-ax.plot(xdates, nx.mlab.rand(len(xdates)), 'o')
+ax.plot(xdates, np.random.rand(len(xdates)), 'o')
 ax.set_xlim(datetime.date(2007,2,1), datetime.date(2007,3,1))
 
 fig.autofmt_xdate()
