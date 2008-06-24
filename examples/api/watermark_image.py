4d11bf46002f91b846899059c778abab3e218b1b
John Hunter <jdh2358@gmail.com>
2008-06-24 18:58:41 +0000

    added simple watermark examples
    svn path=/trunk/matplotlib/; revision=5669

---
+++ examples/api/watermark_image.py
@@ -0,0 +1,22 @@
+"""
+Use a PNG file as a watermark
+"""
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.image as image
+import matplotlib.pyplot as plt
+
+im = image.imread('../data/logo2.png')
+im[:,:,-1] = 0.5  # set the alpha channel
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+ax.plot(np.random.rand(20), '-o', ms=20, lw=2, alpha=0.7, mfc='orange')
+ax.grid()
+fig.figimage(im, 10, 10)
+
+fig.savefig('watermarked', transparent=True)
+
