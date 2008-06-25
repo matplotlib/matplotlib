86461f67d4c200c5d356106d129048e6fa2bca4f
John Hunter <jdh2358@gmail.com>
2008-06-25 21:43:09 +0000

    updated the autotick label faq to use bboxes and transforms rather than iteration
    svn path=/trunk/matplotlib/; revision=5673

--- doc/pyplots/auto_subplots_adjust.py
+++ doc/pyplots/auto_subplots_adjust.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import matplotlib.transforms as mtransforms
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(range(10))
@@ -7,13 +7,24 @@ ax.set_yticks((2,5,7))
 labels = ax.set_yticklabels(('really, really, really', 'long', 'labels'))
 
 def on_draw(event):
+   bboxes = []
    for label in labels:
        bbox = label.get_window_extent()
-       if bbox.xmin<0:
-           fig.subplots_adjust(left=1.1*fig.subplotpars.left)
-           fig.canvas.draw()
-           break
+       # the figure transform goes from relative coords->pixels and we
+       # want the inverse of that
+       bboxi = bbox.inverse_transformed(fig.transFigure)
+       bboxes.append(bboxi)
+
+   # this is the bbox that bounds all the bboxes, again in relative
+   # figure coords
+   bbox = mtransforms.Bbox.union(bboxes)
+   if fig.subplotpars.left < bbox.width:
+       # we need to move it over
+       fig.subplots_adjust(left=1.1*bbox.width) # pad a little
+       fig.canvas.draw()
+   return False
 
 fig.canvas.mpl_connect('draw_event', on_draw)
 
 plt.show()
+
