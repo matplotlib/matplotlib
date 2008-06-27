dd31a35a4b4e693180bbc5c72a8c8f9d2782664b
John Hunter <jdh2358@gmail.com>
2008-06-27 15:34:32 +0000

    added pyplots examples which I forgot to svn add in prev commit
    svn path=/trunk/matplotlib/; revision=5690

---
+++ doc/pyplots/xcorr_demo.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x,y = np.random.randn(2,100)
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+ax1.xcorr(x, y, usevlines=True, maxlags=50, normed=True, lw=2)
+ax1.grid(True)
+ax1.axhline(0, color='black', lw=2)
+
+ax2 = fig.add_subplot(212, sharex=ax1)
+ax2.acorr(x, usevlines=True, normed=True, maxlags=50, lw=2)
+ax2.grid(True)
+ax2.axhline(0, color='black', lw=2)
+
+plt.show()
+
