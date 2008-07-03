46ef01d322970e1daa83a5ba4a1467f5af6c8e96
John Hunter <jdh2358@gmail.com>
2008-07-03 14:30:13 +0000

    added findobj
    svn path=/trunk/matplotlib/; revision=5709

---
+++ examples/pylab_examples/findobj_demo.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.text as text
+
+a = np.arange(0,3,.02)
+b = np.arange(0,3,.02)
+c = np.exp(a)
+d = c[::-1]
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(a,c,'k--',a,d,'k:',a,c+d,'k')
+plt.legend(('Model length', 'Data length', 'Total message length'),
+           'upper center', shadow=True)
+plt.ylim([-1,20])
+plt.grid(False)
+plt.xlabel('Model complexity --->')
+plt.ylabel('Message length --->')
+plt.title('Minimum Message Length')
+
+# match on arbitrary function
+def myfunc(x):
+    return hasattr(x, 'set_color')
+
+for o in fig.findobj(myfunc):
+    o.set_color('blue')
+
+# match on class instances
+for o in fig.findobj(text.Text):
+    o.set_fontstyle('italic')
+
+
+
+plt.show()
