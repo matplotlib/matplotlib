b8f5ddf9daf959258b0e74033e250f1cdf4cf08d
John Hunter <jdh2358@gmail.com>
2008-07-12 21:33:54 +0000

    added support for external backends
    svn path=/trunk/matplotlib/; revision=5754

--- lib/matplotlib/backends/__init__.py
+++ lib/matplotlib/backends/__init__.py
@@ -1,9 +1,6 @@
 
 import matplotlib
-from matplotlib.rcsetup import interactive_bk
-from matplotlib.rcsetup import non_interactive_bk
-from matplotlib.rcsetup import all_backends
-from matplotlib.rcsetup import validate_backend
+
 
 __all__ = ['backend','show','draw_if_interactive',
            'new_figure_manager', 'backend_version']
@@ -14,27 +11,23 @@ def pylab_setup():
     'return new_figure_manager, draw_if_interactive and show for pylab'
     # Import the requested backend into a generic module object
 
-    backend_name = 'backend_'+backend
-    backend_name = backend_name.lower() # until we banish mixed case
-    backend_mod = __import__('matplotlib.backends.'+backend_name,
+    if backend.startswith('module://'):
+        backend_name = backend[9:]
+    else:
+        backend_name = 'backend_'+backend
+        backend_name = backend_name.lower() # until we banish mixed case
+        backend_name = 'matplotlib.backends.%s'%backend_name.lower()
+    backend_mod = __import__(backend_name,
                              globals(),locals(),[backend_name])
 
     # Things we pull in from all backends
     new_figure_manager = backend_mod.new_figure_manager
 
-    if hasattr(backend_mod,'backend_version'):
-        backend_version = getattr(backend_mod,'backend_version')
-    else: backend_version = 'unknown'
 
-
-
-    # Now define the public API according to the kind of backend in use
-    if backend in interactive_bk:
-        show = backend_mod.show
-        draw_if_interactive = backend_mod.draw_if_interactive
-    else:  # non-interactive backends
-        def draw_if_interactive():  pass
-        def show(): pass
+    def do_nothing(*args, **kwargs): pass
+    backend_version = getattr(backend_mod,'backend_version', 'unknown')
+    show = getattr(backend_mod, 'show', do_nothing)
+    draw_if_interactive = getattr(backend_mod, 'draw_if_interactive', do_nothing)
 
     # Additional imports which only happen for certain backends.  This section
     # should probably disappear once all backends are uniform.
