a412f8a9610b598b4feb06080969e100e9d2d534
Andrew Straw <strawman@astraw.com>
2008-07-17 17:06:52 +0000

    update nan example to demonstrate bug
    svn path=/trunk/matplotlib/; revision=5773

--- examples/pylab_examples/nan_test.py
+++ examples/pylab_examples/nan_test.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
 """
-Example: simple line plot with NaNs inserted.
+Example: simple line plots with NaNs inserted.
 """
 from pylab import *
 
 t = arange(0.0, 1.0+0.01, 0.01)
 s = cos(2*2*pi*t)
 t[41:60] = NaN
+
+subplot(2,1,1)
 plot(t, s, '-', lw=2)
 
 xlabel('time (s)')
 ylabel('voltage (mV)')
 title('A sine wave with a gap of NaNs between 0.4 and 0.6')
+grid(True)
+
+subplot(2,1,2)
+t[0] = NaN
+t[-1] = NaN
+plot(t, s, '-', lw=2)
+
+xlabel('time (s)')
+ylabel('voltage (mV)')
+title('More NaNs at 0.0 and 1.0')
 grid(True)
 
 show()
