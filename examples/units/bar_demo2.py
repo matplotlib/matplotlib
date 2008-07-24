minor cleanup of units example, added some to backend driver, more to be fixed
svn path=/trunk/matplotlib/; revision=5850

examples/units/bar_demo2.py
@@ -7,10 +7,11 @@ assumed) or units (conversions applied to get the numbers to current
 units)
 
 """
+import numpy as np
 from basic_units import cm, inch
-from pylab import figure, show, nx
+from pylab import figure, show
 
-cms = cm *nx.arange(0, 10, 2)
+cms = cm *np.arange(0, 10, 2)
 bottom=0*cm
 width=0.8*cm
 

examples/units/bar_unit_demo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
+import numpy as np
 from basic_units import cm, inch
-from pylab import figure, show,nx
+from pylab import figure, show
 
 N = 5
 menMeans = (150*cm, 160*cm, 146*cm, 172*cm, 155*cm)
@@ -9,7 +10,7 @@ menStd =   ( 20*cm,  30*cm,  32*cm,  10*cm,  20*cm)
 fig = figure()
 ax = fig.add_subplot(111)
 
-ind = nx.arange(N)    # the x locations for the groups
+ind = np.arange(N)    # the x locations for the groups
 width = 0.35         # the width of the bars
 p1 = ax.bar(ind, menMeans, width, color='r', bottom=0*cm, yerr=menStd)
 

examples/units/radian_demo.py
@@ -1,7 +1,8 @@
+import numpy as np
 from basic_units import radians, degrees, cos
-from pylab import figure, show, nx
+from pylab import figure, show
 
-x = nx.arange(0, 15, 0.01) * radians
+x = np.arange(0, 15, 0.01) * radians
 
 fig = figure()
 fig.subplots_adjust(hspace=0.3)