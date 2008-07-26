dae2b21cbc271d2d3611e72eb56249bbab8e6ba7
pkienzle <pkienzle@localhost>
2008-07-26 18:42:22 +0000

    clipped line must recache in set_data
    svn path=/trunk/matplotlib/; revision=5887

--- examples/pylab_examples/clippedline.py
+++ examples/pylab_examples/clippedline.py
@@ -19,6 +19,8 @@ class ClippedLine(Line2D):
 
     def set_data(self, *args, **kwargs):
         Line2D.set_data(self, *args, **kwargs)
+        if self._invalid: 
+            self.recache()
         self.xorig = np.array(self._x)
         self.yorig = np.array(self._y)
 
