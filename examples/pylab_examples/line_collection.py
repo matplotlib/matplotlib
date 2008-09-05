bf813daaa4f3f6dfb729517876ea18d022e00104
Eric Firing <efiring@hawaii.edu>
2008-09-05 03:23:30 +0000

    Improve masked array support in collections and quiver
    svn path=/trunk/matplotlib/; revision=6067

--- examples/pylab_examples/line_collection.py
+++ examples/pylab_examples/line_collection.py
@@ -1,20 +1,28 @@
-from pylab import *
+import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
-from matplotlib.colors import ColorConverter
-colorConverter = ColorConverter()
+from matplotlib.colors import colorConverter
+
+import numpy as np
 
 # In order to efficiently plot many lines in a single set of axes,
 # Matplotlib has the ability to add the lines all at once. Here is a
 # simple example showing how it is done.
 
-x = arange(200)
+x = np.arange(100)
 # Here are many sets of y to plot vs x
-ys = [x+i for i in x]
+ys = x[:50, np.newaxis] + x[np.newaxis, :]
 
-# We need to set the plot limits, the will not autoscale
-ax = axes()
-ax.set_xlim((amin(x),amax(x)))
-ax.set_ylim((amin(amin(ys)),amax(amax(ys))))
+segs = np.zeros((50, 100, 2), float)
+segs[:,:,1] = ys
+segs[:,:,0] = x
+
+# Mask some values to test masked array support:
+segs = np.ma.masked_where((segs > 50) & (segs < 60), segs)
+
+# We need to set the plot limits.
+ax = plt.axes()
+ax.set_xlim(x.min(), x.max())
+ax.set_ylim(ys.min(), ys.max())
 
 # colors is sequence of rgba tuples
 # linestyle is a string or dash tuple. Legal string values are
@@ -22,12 +30,13 @@ ax.set_ylim((amin(amin(ys)),amax(amax(ys))))
 #          where onoffseq is an even length tuple of on and off ink in points.
 #          If linestyle is omitted, 'solid' is used
 # See matplotlib.collections.LineCollection for more information
-line_segments = LineCollection([zip(x,y) for y in ys], # Make a sequence of x,y pairs
+line_segments = LineCollection(segs,
                                 linewidths    = (0.5,1,1.5,2),
                                 colors        = [colorConverter.to_rgba(i) \
                                                  for i in ('b','g','r','c','m','y','k')],
                                 linestyle = 'solid')
 ax.add_collection(line_segments)
-show()
+ax.set_title('Line collection with masked arrays')
+plt.show()
 
 
