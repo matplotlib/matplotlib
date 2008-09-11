15a4174cf22d3b09e2ca78e6eb31052c086359c3
Michael Droettboom <mdboom@gmail.com>
2008-09-11 20:28:11 +0000

    Fix backticks in PS output.
    svn path=/branches/v0_91_maint/; revision=6086

--- lib/matplotlib/backends/backend_ps.py
+++ lib/matplotlib/backends/backend_ps.py
@@ -97,6 +97,7 @@ def quote_ps_string(s):
     s=s.replace("(", "\\(")
     s=s.replace(")", "\\)")
     s=s.replace("'", "\\251")
+    s=s.replace("`", "\\301")
     s=re.sub(r"[^ -~\n]", lambda x: r"\%03o"%ord(x.group()), s)
     return s
 
