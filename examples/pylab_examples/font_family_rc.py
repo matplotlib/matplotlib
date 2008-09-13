97f8b14bf2471350ee5f6700b7be4e6dcc275aff
John Hunter <jdh2358@gmail.com>
2008-09-13 17:06:53 +0000

    added a couple of font examples
    svn path=/trunk/matplotlib/; revision=6091

---
+++ examples/pylab_examples/font_family_rc.py
@@ -0,0 +1,29 @@
+"""
+You can explicitly set which font family is picked up for a given font
+style (eg 'serif', 'sans-serif', or 'monospace').
+
+In the example below, we only allow one font family (Tahoma) for the
+san-serif font style.  You the default family with the font.family rc
+param, eg::
+
+  rcParams['font.family'] = 'sans-serif'
+
+and for the font.family you set a list of font styles to try to find
+in order::
+
+  rcParams['font.sans-serif'] = ['Tahoma', 'Bitstream Vera Sans', 'Lucida Grande', 'Verdana']
+
+
+"""
+from matplotlib import rcParams
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['Tahoma']
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot([1,2,3], label='test')
+
+ax.legend()
+plt.show()
+
