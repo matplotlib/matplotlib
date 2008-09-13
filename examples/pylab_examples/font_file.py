8b3c24b85d400cf2802518016a2cc7f389eed839
John Hunter <jdh2358@gmail.com>
2008-09-13 17:06:53 +0000

    added a couple of font examples
    svn path=/trunk/matplotlib/; revision=6091

---
+++ examples/pylab_examples/font_file.py
@@ -0,0 +1,18 @@
+"""
+Although it is usually not a good idea to explicitly point to a single
+ttf file for a font instance, you can do so using the
+font_manager.FontProperties fname argument (for a more flexible
+solution, see the font_fmaily_rc.py and fonts_demo.py examples).
+"""
+import matplotlib.font_manager as fm
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot([1,2,3])
+
+prop = fm.FontProperties(fname='/Library/Fonts/Tahoma.ttf')
+ax.set_title('this is a special font', fontproperties=prop)
+plt.show()
+
