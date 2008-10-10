03752b723e3b1538c7f942e72f733caf562a3185
Eric Firing <efiring@hawaii.edu>
2008-10-10 19:43:34 +0000

    Permit chunking in the backend_agg draw_path() method. This is experimental, and disabled by default; it can be enabled by setting the rcParams['agg.path.chunksize'] before a path is drawn.
    svn path=/trunk/matplotlib/; revision=6178

--- lib/matplotlib/backends/backend_agg.py
+++ lib/matplotlib/backends/backend_agg.py
@@ -62,7 +62,7 @@ class RendererAgg(RendererBase):
         self._renderer = _RendererAgg(int(width), int(height), dpi, debug=False)
         if __debug__: verbose.report('RendererAgg.__init__ _RendererAgg done',
                                      'debug-annoying')
-        self.draw_path = self._renderer.draw_path
+        #self.draw_path = self._renderer.draw_path  # see below
         self.draw_markers = self._renderer.draw_markers
         self.draw_path_collection = self._renderer.draw_path_collection
         self.draw_quad_mesh = self._renderer.draw_quad_mesh
@@ -75,6 +75,28 @@ class RendererAgg(RendererBase):
         self.bbox = Bbox.from_bounds(0, 0, self.width, self.height)
         if __debug__: verbose.report('RendererAgg.__init__ done',
                                      'debug-annoying')
+
+    def draw_path(self, gc, path, transform, rgbFace=None):
+        nmax = rcParams['agg.path.chunksize'] # here at least for testing
+        npts = path.vertices.shape[0]
+        if nmax > 100 and npts > nmax and path.should_simplify:
+            nch = npy.ceil(npts/float(nmax))
+            chsize = int(npy.ceil(npts/nch))
+            i0 = npy.arange(0, npts, chsize)
+            i1 = npy.zeros_like(i0)
+            i1[:-1] = i0[1:] - 1
+            i1[-1] = npts
+            for ii0, ii1 in zip(i0, i1):
+                v = path.vertices[ii0:ii1,:]
+                c = path.codes
+                if c is not None:
+                    c = c[ii0:ii1]
+                    c[0] = Path.MOVETO # move to end of last chunk
+                p = Path(v, c)
+                self._renderer.draw_path(gc, p, transform, rgbFace)
+        else:
+            self._renderer.draw_path(gc, path, transform, rgbFace)
+
 
     def draw_mathtext(self, gc, x, y, s, prop, angle):
         """
