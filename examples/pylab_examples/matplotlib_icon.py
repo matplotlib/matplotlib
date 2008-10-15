643b6b5722446f5dfd13636b9748c3f4bcaa6f2e
Michael Droettboom <mdboom@gmail.com>
2008-10-15 17:46:36 +0000

    Use small icon in documentation; large logo for home page only. Update application icon to use new colors from website.
    svn path=/trunk/matplotlib/; revision=6209

--- examples/pylab_examples/matplotlib_icon.py
+++ examples/pylab_examples/matplotlib_icon.py
@@ -3,19 +3,19 @@
 make the matplotlib svg minimization icon
 """
 import matplotlib
-matplotlib.use('SVG')
+#matplotlib.use('Svg')
 from pylab import *
 
 rc('grid', ls='-', lw=2, color='k')
 fig = figure(figsize=(1, 1), dpi=72)
-axes([0.025, 0.025, 0.95, 0.95], axisbg='y')
+axes([0.025, 0.025, 0.95, 0.95], axisbg='#bfd1d4')
 
 t = arange(0, 2, 0.05)
 s = sin(2*pi*t)
-plot(t,s, linewidth=4)
+plot(t,s, linewidth=4, color="#ca7900")
 axis([-.2, 2.2, -1.2, 1.2])
 
-grid(True)
+# grid(True)
 setp(gca(), xticklabels=[], yticklabels=[])
-#savefig('../images/matplotlib.svg', facecolor='0.75')
+savefig('matplotlib.svg', facecolor='0.75')
 
