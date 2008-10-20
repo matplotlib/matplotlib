49d9456130201eb9ff52a5ac03778823a3c86f68
John Hunter <jdh2358@gmail.com>
2008-10-20 20:54:24 +0000

    added image thumbnail example
    svn path=/trunk/matplotlib/; revision=6285

---
+++ examples/misc/image_thumbnail.py
@@ -0,0 +1,29 @@
+"""
+You can use matplotlib to generate thumbnails from existing images.
+matplotlib natively supports PNG files on the input side, and other
+image types transparently if your have PIL installed
+"""
+
+# build thumbnails of all images in a directory
+import sys, os, glob
+import matplotlib.image as image
+
+
+if len(sys.argv)!=2:
+    print 'Usage: python %s IMAGEDIR'%__file__
+    raise SystemExit
+indir = sys.argv[1]
+if not os.path.exists(indir):
+    print 'Could not find input directory "%s"'%indir
+    raise SystemExit
+
+outdir = 'thumbs'
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+
+for fname in glob.glob(os.path.join(indir, '*.png')):
+    basedir, basename = os.path.split(fname)
+    outfile = os.path.join(outdir, basename)
+    fig = image.thumbnail(fname, outfile, scale=0.15)
+    print 'saved thumbnail of %s to %s'%(fname, outfile)
+
