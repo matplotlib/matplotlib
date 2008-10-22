8540e921e7fff457f0aa96c65ea44cd47a1362f7
Michael Droettboom <mdboom@gmail.com>
2008-10-22 20:03:12 +0000

    Add clip path support on images.  Add a new example and update the dolphin fest.
    svn path=/trunk/matplotlib/; revision=6302

--- examples/pylab_examples/dolphin.py
+++ examples/pylab_examples/dolphin.py
@@ -1,8 +1,10 @@
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle, PathPatch
 from matplotlib.path import Path
 from matplotlib.transforms import Affine2D
 import numpy as np
+
 
 r = np.random.rand(50)
 t = np.random.rand(50) * np.pi * 2.0
@@ -11,9 +13,15 @@ y = r * np.sin(t)
 
 fig = plt.figure(figsize=(6,6))
 ax = plt.subplot(111)
-circle = Circle((0, 0), 1, facecolor=(0,0,0.8),
+circle = Circle((0, 0), 1, facecolor='none',
                 edgecolor=(0,0.8,0.8), linewidth=3, alpha=0.5)
 ax.add_patch(circle)
+
+im = plt.imshow(np.random.random((100, 100)),
+                origin='lower', cmap=cm.winter,
+                interpolation='spline36',
+                extent=([-1, 1, -1, 1]), zorder=1000)
+im.set_clip_path(circle)
 
 plt.plot(x, y, 'o', color=(0.9, 0.9, 1.0), alpha=0.8)
 
