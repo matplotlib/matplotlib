b1ef4cfbb9fc107472b8f95ee84b1c32816e61fa
pkienzle <pkienzle@localhost>
2008-11-14 22:19:02 +0000

    patches: allow partial rings with Wedge
    svn path=/trunk/matplotlib/; revision=6404

--- examples/api/patch_collection.py
+++ examples/api/patch_collection.py
@@ -25,12 +25,20 @@ for x1,y1,r,t1,t2 in zip(x, y, radii, theta1, theta2):
     wedge = Wedge((x1,y1), r, t1, t2)
     patches.append(wedge)
 
+# Some limiting conditions on Wedge
+patches += [
+    Wedge((.3,.7), .1, 0, 360),             # Full circle
+    Wedge((.7,.8), .2, 0, 360, width=0.05), # Full ring
+    Wedge((.8,.3), .2, 0, 45),              # Full sector
+    Wedge((.8,.3), .2, 45, 90, width=0.10), # Ring sector
+]
+
 for i in range(N):
     polygon = Polygon(pylab.rand(N,2), True)
     patches.append(polygon)
 
 colors = 100*pylab.rand(len(patches))
-p = PatchCollection(patches, cmap=matplotlib.cm.jet)
+p = PatchCollection(patches, cmap=matplotlib.cm.jet, alpha=0.4)
 p.set_array(pylab.array(colors))
 ax.add_collection(p)
 pylab.colorbar(p)
