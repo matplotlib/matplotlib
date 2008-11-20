34780e53d7df0de3253647f42bc86a24a8e9262f
Eric Firing <efiring@hawaii.edu>
2008-11-20 23:03:17 +0000

    Fix specification of gray level in slider_demo; thanks to Matthias Michler
    svn path=/trunk/matplotlib/; revision=6425

--- examples/widgets/slider_demo.py
+++ examples/widgets/slider_demo.py
@@ -26,7 +26,7 @@ sfreq.on_changed(update)
 samp.on_changed(update)
 
 resetax = axes([0.8, 0.025, 0.1, 0.04])
-button = Button(resetax, 'Reset', color=axcolor, hovercolor=0.975)
+button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
 def reset(event):
     sfreq.reset()
     samp.reset()
