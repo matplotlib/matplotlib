bbb419781cc98f688a4480d5c5b717e8723d463b
John Hunter <jdh2358@gmail.com>
2008-11-23 19:04:35 +0000

    generalized fill between poly collection
    svn path=/trunk/matplotlib/; revision=6432

--- examples/api/filled_masked_regions.py
+++ examples/api/filled_masked_regions.py
@@ -8,31 +8,36 @@ import matplotlib.collections as collections
 
 
 t = np.arange(0.0, 2, 0.01)
-s = np.sin(2*np.pi*t)
+s1 = np.sin(2*np.pi*t)
+s2 = 1.2*np.sin(4*np.pi*t)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.set_title('using fill_between_masked')
-ax.plot(t, s, '-')
+ax.set_title('using fill_between_where')
+ax.plot(t, s1, t, s2)
 ax.axhline(0, color='black', lw=2)
 
-collection = collections.PolyCollection.fill_between_masked(t, s, s>=0, yboundary=0, color='green', alpha=0.5)
+collection = collections.PolyCollection.fill_between_where(
+	   t, s1, s2, s1>=s2, color='green', alpha=0.5)
 ax.add_collection(collection)
 
-collection = collections.PolyCollection.fill_between_masked(t, s, s<=0, yboundary=0, color='red', alpha=0.5)
+collection = collections.PolyCollection.fill_between_where(
+	   t, s1, s2, s1<=s2, color='red', alpha=0.5)
 ax.add_collection(collection)
 
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.set_title('using span_masked')
-ax.plot(t, s, '-')
+ax.plot(t, s1, '-')
 ax.axhline(0, color='black', lw=2)
 
-collection = collections.BrokenBarHCollection.span_masked(t, s>0, ymin=0, ymax=1, facecolor='green', alpha=0.5)
+collection = collections.BrokenBarHCollection.span_masked(
+	   t, s1>0, ymin=0, ymax=1, facecolor='green', alpha=0.5)
 ax.add_collection(collection)
 
-collection = collections.BrokenBarHCollection.span_masked(t, s<0, ymin=-1, ymax=0, facecolor='red', alpha=0.5)
+collection = collections.BrokenBarHCollection.span_masked(
+	   t, s1<0, ymin=-1, ymax=0, facecolor='red', alpha=0.5)
 ax.add_collection(collection)
 
 
