eaa70f0c03bc471a0ba4d1c9e29efe857e15d258
Jouni K. Seppänen <jks@iki.fi>
2008-12-08 12:26:53 +0000

    Fix SymmetricalLogLocator bug that caused platform-specific behavior
    svn path=/trunk/matplotlib/; revision=6505

--- lib/matplotlib/type1font.py
+++ lib/matplotlib/type1font.py
@@ -17,6 +17,7 @@ If extending this class, see also: Adobe Type 1 Font Format, Adobe
 Systems Incorporated, third printing, v1.1, 1993. ISBN 0-201-57044-0.
 """
 
+import re
 import struct
 
 class Type1Font(object):
@@ -28,6 +29,7 @@ class Type1Font(object):
         finally:
             file.close()
         self.parts = self._split(data)
+        self._parse()
 
     def _read(self, file):
         rawdata = file.read()
@@ -97,8 +99,86 @@ class Type1Font(object):
 
         return data[:len1], binary, data[idx:]
 
+    _whitespace = re.compile(r'[\0\t\r\014\n ]+')
+    _delim = re.compile(r'[()<>[]{}/%]')
+    _token = re.compile(r'/{0,2}[^]\0\t\r\v\n ()<>{}/%[]+')
+    _comment = re.compile(r'%[^\r\n\v]*')
+    _instring = re.compile(r'[()\\]')
+    def _parse(self):
+        """
+        A very limited kind of parsing to find the Encoding of the
+        font.
+        """
+        def tokens(text):
+            """
+            Yield pairs (position, token), ignoring comments and
+            whitespace. Numbers count as tokens.
+            """
+            pos = 0
+            while pos < len(text):
+                match = self._comment.match(text[pos:]) or self._whitespace.match(text[pos:])
+                if match:
+                    pos += match.end()
+                elif text[pos] == '(':
+                    start = pos
+                    pos += 1
+                    depth = 1
+                    while depth:
+                        match = self._instring.search(text[pos:])
+                        if match is None: return
+                        if match.group() == '(':
+                            depth += 1
+                            pos += 1
+                        elif match.group() == ')':
+                            depth -= 1
+                            pos += 1
+                        else:
+                            pos += 2
+                    yield (start, text[start:pos])
+                elif text[pos:pos+2] in ('<<', '>>'):
+                    yield (pos, text[pos:pos+2])
+                    pos += 2
+                elif text[pos] == '<':
+                    start = pos
+                    pos += text[pos:].index('>')
+                    yield (start, text[start:pos])
+                else:
+                    match = self._token.match(text[pos:])
+                    if match:
+                        yield (pos, match.group())
+                        pos += match.end()
+                    else:
+                        yield (pos, text[pos])
+                        pos += 1
+
+        enc_starts, enc_ends = None, None
+        state = 0
+        # State transitions:
+        # 0 -> /Encoding -> 1
+        # 1 -> StandardEncoding -> 2 -> def -> (ends)
+        # 1 -> dup -> 4 -> put -> 5
+        # 5 -> dup -> 4 -> put -> 5
+        # 5 -> def -> (ends)
+        for pos,token in tokens(self.parts[0]):
+            if state == 0 and token == '/Encoding':
+                enc_starts = pos
+                state = 1
+            elif state == 1 and token == 'StandardEncoding':
+                state = 2
+            elif state in (2,5) and token == 'def':
+                enc_ends = pos+3
+                break
+            elif state in (1,5) and token == 'dup':
+                state = 4
+            elif state == 4 and token == 'put':
+                state = 5
+        self.enc_starts, self.enc_ends = enc_starts, enc_ends
+                
+    
 if __name__ == '__main__':
     import sys
     font = Type1Font(sys.argv[1])
     parts = font.parts
     print len(parts[0]), len(parts[1]), len(parts[2])
+    print parts[0][font.enc_starts:font.enc_ends]
+
