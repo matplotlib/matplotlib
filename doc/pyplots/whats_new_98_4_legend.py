e9714d8e41a32e6cf6a77eb673081d2a86bc206e
Darren Dale <dsdale24@gmail.com>
2008-12-11 03:25:31 +0000

    fixed a typo in whats_new_98_4_legend.py
    svn path=/trunk/matplotlib/; revision=6561

--- doc/pyplots/whats_new_98_4_legend.py
+++ doc/pyplots/whats_new_98_4_legend.py
@@ -8,7 +8,7 @@ t1 = np.arange(0.0, 1.0, 0.01)
 for n in [1, 2, 3, 4]:
     plt.plot(t1, t1**n, label="n=%d"%(n,))
 
-leg = plt.legend(loc='best', nncol=2, mode="expand", shadow=True, fancybox=True)
+leg = plt.legend(loc='best', ncol=2, mode="expand", shadow=True, fancybox=True)
 leg.get_frame().set_alpha(0.5)
 
 
