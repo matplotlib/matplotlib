55878ba1ae104cdcf538a3945fe0eb3e857fd338
Jae-Joon Lee <lee.j.joon@gmail.com>
2008-12-21 03:46:05 +0000

    fix hatch bug in pdf backend
    svn path=/branches/v0_98_5_maint/; revision=6688

--- examples/pylab_examples/hatch_demo.py
+++ examples/pylab_examples/hatch_demo.py
@@ -1,18 +1,25 @@
 """
-Hatching (pattern filled polygons) is supported currently on PS
+Hatching (pattern filled polygons) is supported currently on PS and PDF
 backend only.  See the set_patch method in
 http://matplotlib.sf.net/matplotlib.patches.html#Patch
 for details
 """
-import matplotlib
-matplotlib.use('PS')
-from pylab import figure
+import matplotlib.pyplot as plt
 
-fig = figure()
-ax = fig.add_subplot(111)
-bars = ax.bar(range(1,5), range(1,5), color='gray', ecolor='black')
+fig = plt.figure()
+ax1 = fig.add_subplot(121)
+ax1.annotate("Hatch is only supported in the PS and PDF backend", (1, 1),
+             xytext=(0, 5),
+             xycoords="axes fraction", textcoords="offset points", ha="center"
+             )
+ax1.bar(range(1,5), range(1,5), color='gray', ecolor='black', hatch="/")
+
+
+ax2 = fig.add_subplot(122)
+bars = ax2.bar(range(1,5), range(1,5), color='gray', ecolor='black')
 
 patterns = ('/', '+', 'x', '\\')
 for bar, pattern in zip(bars, patterns):
      bar.set_hatch(pattern)
-fig.savefig('hatch4.ps')
+
+plt.show()
