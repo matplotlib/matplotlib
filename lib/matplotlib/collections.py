Fix units handling in scatter
svn path=/branches/v0_98_5_maint/; revision=6781

lib/matplotlib/collections.py
@@ -174,8 +174,8 @@ class Collection(artist.Artist, cm.ScalarMappable):
                 ys = self.convert_yunits(ys)
                 paths.append(mpath.Path(zip(xs, ys), path.codes))
             if len(self._offsets):
-                xs = self.convert_xunits(self._offsets[:0])
-                ys = self.convert_yunits(self._offsets[:1])
+                xs = self.convert_xunits(self._offsets[:,0])
+                ys = self.convert_yunits(self._offsets[:,1])
                 offsets = zip(xs, ys)
 
         offsets = np.asarray(offsets, np.float_)