e2fc8af21b6fdcb244b6884c967006ca9efe7c18
John Hunter <jdh2358@gmail.com>
2009-01-14 17:54:36 +0000

    added Paul Ivanov's gui independent pong example
    svn path=/trunk/matplotlib/; revision=6783

---
+++ examples/event_handling/pong_gtk.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# For detailed comments on animation and the techniques used here, see
+# the wiki entry
+# http://www.scipy.org/wikis/topical_software/MatplotlibAnimation
+import time
+
+import gtk, gobject
+
+import matplotlib
+matplotlib.use('GTKAgg')
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pipong 
+from numpy.random import randn, randint
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+canvas = ax.figure.canvas
+
+
+def start_anim(event):
+    gobject.idle_add(animation.draw,animation)
+    canvas.mpl_disconnect(start_anim.cid)
+
+animation = pipong.Game(ax)
+start_anim.cid = canvas.mpl_connect('draw_event', start_anim)
+
+
+tstart = time.time()
+plt.grid() # to ensure proper background restore
+plt.show()
+print 'FPS:' , animation.cnt/(time.time()-tstart)
---
+++ examples/event_handling/pong_qt.py
@@ -0,0 +1,42 @@
+# For detailed comments on animation and the techniqes used here, see
+# the wiki entry http://www.scipy.org/Cookbook/Matplotlib/Animations
+
+import os, sys
+import matplotlib
+matplotlib.use('QtAgg') # qt3 example
+
+from qt import *
+# Note: color-intensive applications may require a different color allocation
+# strategy.
+QApplication.setColorSpec(QApplication.NormalColor)
+
+TRUE  = 1
+FALSE = 0
+ITERS = 1000
+
+import pylab as p
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import pipong 
+from numpy.random import randn, randint
+
+class BlitQT(QObject):
+    def __init__(self):
+        QObject.__init__(self, None, "app")
+
+        self.ax = plt.subplot(111)
+        self.animation = pipong.Game(self.ax)
+
+    def timerEvent(self, evt):
+       self.animation.draw(evt)
+
+plt.grid() # to ensure proper background restore
+
+app = BlitQT()
+# for profiling
+app.tstart = time.time()
+app.startTimer(0)
+
+plt.show()
+print 'FPS:' , app.animation.cnt/(time.time()-app.tstart)
