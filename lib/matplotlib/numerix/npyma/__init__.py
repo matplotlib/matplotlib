830329b07048c6f0d814c520762892554ed4e37f
Eric Firing <efiring@hawaii.edu>
2009-02-24 21:15:49 +0000

    Deleted numerix
    svn path=/trunk/matplotlib/; revision=6931

--- lib/matplotlib/numerix/npyma/__init__.py
+++
@@ -1,7 +0,0 @@
-import warnings
-
-warnings.warn("npyma is obsolete and will be removed", DeprecationWarning)
-try:
-    from numpy.ma import *        # numpy 1.05 and later
-except ImportError:
-    from numpy.core.ma import *   # earlier
