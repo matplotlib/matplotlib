61a68a7fd2a4e88cd02c5bc9ab95f3635e715407
Michael Droettboom <mdboom@gmail.com>
2009-03-02 23:04:27 +0000

    Merged revisions 6946 via svnmerge from https://matplotlib.svn.sf.net/svnroot/matplotlib/branches/v0_98_5_maint
    ........
      r6946 | mdboom | 2009-03-02 17:50:44 -0500 (Mon, 02 Mar 2009) | 2 lines
      
      Fix bug in doc build.
    ........
    
    svn path=/trunk/matplotlib/; revision=6947

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ import ipython_console_highlighting
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['matplotlib.sphinxext.mathmpl', 'math_symbol_table',
-              'sphinx.ext.autodoc', # 'matplotlib.sphinxext.only_directives',
+              'sphinx.ext.autodoc', 'matplotlib.sphinxext.only_directives',
               'matplotlib.sphinxext.plot_directive', 'inheritance_diagram',
               'gen_gallery', 'gen_rst']
 
