a69d5b90ecadb7e6cbb26b04ed865660328058d3
Michael Droettboom <mdboom@gmail.com>
2009-03-19 12:52:46 +0000

    Fix wavy baseline with STIX fonts -- again.
    svn path=/branches/v0_98_5_maint/; revision=6995

--- lib/matplotlib/mathtext.py
+++ lib/matplotlib/mathtext.py
@@ -201,7 +201,7 @@ class MathtextBackendAggRender(MathtextBackend):
 
     def render_glyph(self, ox, oy, info):
         info.font.draw_glyph_to_bitmap(
-            self.image, ox, oy - info.metrics.ymax, info.glyph)
+            self.image, ox, oy - info.metrics.iceberg, info.glyph)
 
     def render_rect_filled(self, x1, y1, x2, y2):
         height = max(int(y2 - y1) - 1, 0)
