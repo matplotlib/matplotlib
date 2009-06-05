9818b806691c6955116c6068982fa64692a8cc14
John Hunter <jdh2358@gmail.com>
2009-06-05 16:51:21 +0000

    added example to fill_between to show how to create multiple axes spans where a condition is true
    svn path=/trunk/matplotlib/; revision=7180

--- examples/pylab_examples/fill_between.py
+++ examples/pylab_examples/fill_between.py
@@ -55,10 +55,11 @@ ax.plot(x, y, color='black')
 # use the data coordinates for the x-axis and the axes coordinates for the y-axis
 import matplotlib.transforms as mtransforms
 trans = mtransforms.blended_transform_factory(ax.transData, ax.transAxes)
-ax.axhline(0.5, color='green', lw=2, alpha=0.5)
-ax.axhline(-0.5, color='red', lw=2, alpha=0.5)
-ax.fill_between(x, 0, 1, where=y2>0.5, facecolor='green', alpha=0.5, transform=trans)
-ax.fill_between(x, 0, 1, where=y2<-0.5, facecolor='red', alpha=0.5, transform=trans)
+theta = 0.9
+ax.axhline(theta, color='green', lw=2, alpha=0.5)
+ax.axhline(-theta, color='red', lw=2, alpha=0.5)
+ax.fill_between(x, 0, 1, where=y>theta, facecolor='green', alpha=0.5, transform=trans)
+ax.fill_between(x, 0, 1, where=y<-theta, facecolor='red', alpha=0.5, transform=trans)
 
 
 
