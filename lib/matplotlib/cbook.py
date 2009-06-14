1ec225383167582306fd289fe84da862b5cfbce2
Eric Firing <efiring@hawaii.edu>
2009-06-14 01:32:20 +0000

    partial cleanup of mlab and of the pylab imports from mlab
    svn path=/trunk/matplotlib/; revision=7217

--- lib/matplotlib/cbook.py
+++ lib/matplotlib/cbook.py
@@ -761,36 +761,6 @@ def allpairs(x):
 
 
 
-
-# python 2.2 dicts don't have pop--but we don't support 2.2 any more
-def popd(d, *args):
-    """
-    Should behave like python2.3 :meth:`dict.pop` method; *d* is a
-    :class:`dict`::
-
-      # returns value for key and deletes item; raises a KeyError if key
-      # is not in dict
-      val = popd(d, key)
-
-      # returns value for key if key exists, else default.  Delete key,
-      # val item if it exists.  Will not raise a KeyError
-      val = popd(d, key, default)
-
-    """
-    warnings.warn("Use native python dict.pop method", DeprecationWarning)
-    # warning added 2008/07/22
-    if len(args)==1:
-        key = args[0]
-        val = d[key]
-        del d[key]
-    elif len(args)==2:
-        key, default = args
-        val = d.get(key, default)
-        try: del d[key]
-        except KeyError: pass
-    return val
-
-
 class maxdict(dict):
     """
     A dictionary with a maximum size; this doesn't override all the
