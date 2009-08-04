47c1fd371bd5e10c8eab243c261268899b4b3ec5
Jouni K. Seppänen <jks@iki.fi>
2009-08-04 19:51:08 +0000

    Fix indentation error in mpl_data_demo.py - thanks to Gökhan Sever
    svn path=/trunk/matplotlib/; revision=7355

--- examples/misc/mpl_data_demo.py
+++ examples/misc/mpl_data_demo.py
@@ -1,7 +1,7 @@
-    """
-    Grab mpl data from the ~/.matplotlib/mpl_data cache if it exists, else
-    fetch it from svn and cache it
-    """
+"""
+Grab mpl data from the ~/.matplotlib/mpl_data cache if it exists, else
+fetch it from svn and cache it
+"""
 import matplotlib.cbook as cbook
 import matplotlib.pyplot as plt
 fname = cbook.get_mpl_data('lena.png', asfileobj=False)
