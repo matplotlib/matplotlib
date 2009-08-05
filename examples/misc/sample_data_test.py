29c89eb189b6ac80ba89a955625d4ff9c8172c6f
John Hunter <jdh2358@gmail.com>
2009-08-05 15:14:35 +0000

    use proper file handle to the sample data
    svn path=/trunk/matplotlib/; revision=7371

--- examples/misc/sample_data_test.py
+++ examples/misc/sample_data_test.py
@@ -8,6 +8,19 @@ when you rerun this script you will get the updated data (and the new
 svn version will be cached in ~/.matplotlib/sample_data)
 """
 
+import matplotlib.mlab as mlab
 import matplotlib.cbook as cbook
-fh = cbook.get_sample_data('testdir/subdir/testsub.csv')
+
+# get the file handle to the cached data and print the contents
+datafile = 'testdir/subdir/testsub.csv'
+fh = cbook.get_sample_data(datafile)
 print fh.read()
+
+# make sure we can read it using csv2rec
+fh.seek(0)
+r = mlab.csv2rec(fh)
+
+print mlab.rec2txt(r)
+
+fh.close()
+
