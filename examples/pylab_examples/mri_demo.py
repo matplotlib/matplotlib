c457cf929e632c1e744aaa416a88a29e623b7514
John Hunter <jdh2358@gmail.com>
2009-08-05 17:21:09 +0000

    fixed another example to use sample data
    svn path=/trunk/matplotlib/; revision=7390

--- examples/pylab_examples/mri_demo.py
+++ examples/pylab_examples/mri_demo.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from pylab import *
-
+import matplotlib.cbook as cbook
 # data are 256x256 16 bit integers
-dfile = '../data/s1045.ima'
-im = fromstring(file(dfile, 'rb').read(), uint16).astype(float)
+dfile = cbook.get_sample_data('s1045.ima', asfileobj=False)
+print 'loading image', dfile
+im = np.fromstring(file(dfile, 'rb').read(), np.uint16).astype(float)
 im.shape = 256, 256
 
 #imshow(im, ColormapJet(256))
