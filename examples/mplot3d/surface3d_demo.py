aca89b10e6b70fa53815e86d492715a77f2275bf
John Hunter <jdh2358@gmail.com>
2009-08-08 12:21:29 +0000

    clean up mplot3d examples: use pyplot noy pylab and numpy rather than list comps and python random module
    svn path=/branches/v0_99_maint/; revision=7428

--- examples/mplot3d/surface3d_demo.py
+++ examples/mplot3d/surface3d_demo.py
@@ -1,10 +1,9 @@
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-import pylab
-import random
+import matplotlib.pyplot as plt
 import numpy as np
 
-fig = pylab.figure()
+fig = plt.figure()
 ax = Axes3D(fig)
 X = np.arange(-5, 5, 0.25)
 Y = np.arange(-5, 5, 0.25)
@@ -13,5 +12,5 @@ R = np.sqrt(X**2 + Y**2)
 Z = np.sin(R)
 ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet)
 
-pylab.show()
+plt.show()
 
