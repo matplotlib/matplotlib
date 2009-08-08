bccaca52d169586a601700c923fa8643d4808039
John Hunter <jdh2358@gmail.com>
2009-08-08 15:16:57 +0000

    simplify poly array in example
    svn path=/branches/v0_99_maint/; revision=7432

--- examples/api/histogram_path_demo.py
+++ examples/api/histogram_path_demo.py
@@ -22,34 +22,25 @@ ax = fig.add_subplot(111)
 data = np.random.randn(1000)
 n, bins = np.histogram(data, 50)
 
-
 # get the corners of the rectangles for the histogram
 left = np.array(bins[:-1])
 right = np.array(bins[1:])
 bottom = np.zeros(len(left))
 top = bottom + n
-nrects = len(left)
-
-XY = np.zeros((nrects, 4, 2))
-XY[:,0,0] = left
-XY[:,0,1] = bottom
-
-XY[:,1,0] = left
-XY[:,1,1] = top
-
-XY[:,2,0] = right
-XY[:,2,1] = top
-
-XY[:,3,0] = right
-XY[:,3,1] = bottom
 
 
+# we need a (numrects x numsides x 2) numpy array for the path helper
+# function to build a compound path
+XY = np.array([[left,left,right,right], [bottom,top,top,bottom]]).T
 
+# get the Path object
 barpath = path.Path.make_compound_path_from_polys(XY)
-print barpath.codes[:7], barpath.codes[-7:]
+
+# make a patch out of it
 patch = patches.PathPatch(barpath, facecolor='blue', edgecolor='gray', alpha=0.8)
 ax.add_patch(patch)
 
+# update the view limits
 ax.set_xlim(left[0], right[-1])
 ax.set_ylim(bottom.min(), top.max())
 
