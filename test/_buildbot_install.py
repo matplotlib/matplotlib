4864a0efae11b1736669d4b62cd3952814ab76eb
Andrew Straw <strawman@astraw.com>
2009-08-23 05:18:45 +0000

    add buildbot scripts
    svn path=/trunk/matplotlib/; revision=7535

---
+++ test/_buildbot_install.py
@@ -0,0 +1,15 @@
+"""This script will install matplotlib to a virtual environment to
+faciltate testing."""
+import shutil, os, sys
+from subprocess import Popen, PIPE, STDOUT
+
+from _buildbot_util import check_call
+
+TARGET='PYmpl'
+
+if os.path.exists(TARGET):
+    shutil.rmtree(TARGET)
+
+check_call('virtualenv %s'%(TARGET,))
+TARGET_py = os.path.join(TARGET,'bin','python')
+check_call('%s setup.py install'%TARGET_py)
