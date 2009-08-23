a94ea7673c630585b5e80639ec027b79c2bc55c9
John Hunter <jdh2358@gmail.com>
2009-08-23 01:31:40 +0000

    updated 7 of 9 images; leaving 2 for pos control on buildbot
    svn path=/trunk/matplotlib/; revision=7526

--- test/consolidate_diff_images.sh
+++ test/consolidate_diff_images.sh
@@ -4,4 +4,4 @@ set -e
 TARGET_DIR="diff-images"
 rm -rf $TARGET_DIR
 mkdir $TARGET_DIR
-find . -name 'failed-diff-*png' | xargs mv --target-directory=$TARGET_DIR
+find . -name 'failed-diff-*png' -exec mv {} $TARGET_DIR/ \;
