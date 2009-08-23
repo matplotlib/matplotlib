31a92ca14a711db606e7c76661686236ac1e06f0
Andrew Straw <strawman@astraw.com>
2009-08-23 19:12:20 +0000

    erase /home/astraw/.matplotlib before testing
    svn path=/trunk/matplotlib/; revision=7544

--- test/_buildbot_test.py
+++ test/_buildbot_test.py
@@ -10,6 +10,9 @@ TARGET=os.path.abspath('PYmpl')
 if not os.path.exists(TARGET):
     raise RuntimeError('the virtualenv target directory was not found')
 
+check_call('%s -c "import shutil,matplotlib; x=matplotlib.get_configdir(); shutil.rmtree(x)"'%TARGET_py)
+
+shutil.rmtree( os.expanduser( os.path.join('~','.matplotlib')))
 TARGET_py = os.path.join(TARGET,'bin','python')
 check_call('%s run-mpl-test.py --all'%TARGET_py,
            cwd='test')
