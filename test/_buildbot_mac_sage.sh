ffc4e41de491eba3cf4e4f26961e3ae0241fec06
John Hunter <jdh2358@gmail.com>
2009-08-23 18:59:07 +0000

    fixes to sage build slave
    svn path=/trunk/matplotlib/; revision=7543

--- test/_buildbot_mac_sage.sh
+++ test/_buildbot_mac_sage.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-PREFIX=/Users/jdh2358/devbb make -f make.osx mpl_install
+#!/bin/bash
+set -e
+export PYTHON=/Users/jdh2358/dev/bin/python
+export PREFIX=/Users/jdh2358/devbb 
+export PYTHONPATH=${PREFIX}/lib/python2.6/site-packages:/Users/jdh2358/dev/lib/python2.6/site-packages
+
+make -f make.osx mpl_install
+echo ${PYTHONPATH}
+
+cd test && python run-mpl-test.py --all --keep-failed
