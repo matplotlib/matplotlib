3cfd737a8fb2350ae574f65e11160c528b41f307
John Hunter <jdh2358@gmail.com>
2009-08-30 13:35:12 +0000

    applied Gael's ginput patch
    svn path=/branches/v0_99_maint/; revision=7598

--- lib/matplotlib/figure.py
+++ lib/matplotlib/figure.py
@@ -12,7 +12,6 @@ contains all the plot elements.  The following classes are defined
 
 """
 import numpy as np
-import time
 
 import artist
 from artist import Artist, allow_rasterization
@@ -1104,6 +1103,12 @@ class Figure(Artist):
         (or potentially both mouse buttons at once) terminates the input.
 
         Right clicking cancels last input.
+
+        The buttons used for the various actions (adding points, removing
+        points, terminating the inputs) can be overriden via the
+        arguments *mouse_add*, *mouse_pop* and *mouse_stop*, that give
+        the associated mouse button: 1 for left, 2 for middle, 3 for
+        right.
 
         The keyboard can also be used to select points in case your mouse
         does not have one or more of the buttons.  The delete and backspace
