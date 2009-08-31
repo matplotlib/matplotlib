ef62147ca9dec1e3c2ac6b113d61d4b8732cb7bd
John Hunter <jdh2358@gmail.com>
2009-08-31 02:42:44 +0000

    make the buildbot point to devbb
    svn path=/trunk/matplotlib/; revision=7610

--- test/_buildbot_mac_sage.sh
+++ test/_buildbot_mac_sage.sh
@@ -3,6 +3,7 @@ set -e
 rm -rf ${HOME}/.matplotlib/*
 rm -rf build
 
+export PATH=${HOME}/dev/bin:$PATH
 export PYTHON=${HOME}/dev/bin/python
 export PREFIX=${HOME}/devbb 
 export PYTHONPATH=${PREFIX}/lib/python2.6/site-packages:${HOME}/dev/lib/python2.6/site-packages
