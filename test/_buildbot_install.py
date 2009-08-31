19e9bd9f80f56ccb8bd1edfc3f721e8f1555a40d
Andrew Straw <strawman@astraw.com>
2009-08-31 16:48:46 +0000

    buildbot: allow passing arguments to select virtualenv and nose
    This is useful for getting the buildbot to run Python2.4.
    
    svn path=/trunk/matplotlib/; revision=7615

--- test/_buildbot_install.py
+++ test/_buildbot_install.py
@@ -2,8 +2,20 @@
 faciltate testing."""
 import shutil, os, sys
 from subprocess import Popen, PIPE, STDOUT
+from optparse import OptionParser
 
 from _buildbot_util import check_call
+
+usage = """%prog [options]"""
+parser = OptionParser(usage)
+parser.add_option('--virtualenv',type='string',default='virtualenv',
+                  help='string to invoke virtualenv')
+parser.add_option('--easy-install-nose',action='store_true',default=False,
+                  help='run "easy_install nose" in the virtualenv')
+(options, args) = parser.parse_args()
+if len(args)!=0:
+    parser.print_help()
+    sys.exit(0)
 
 TARGET='PYmpl'
 
@@ -15,6 +27,10 @@ if 1:
     if os.path.exists(build_path):
         shutil.rmtree(build_path)
 
-check_call('virtualenv %s'%(TARGET,))
+check_call('%s %s'%(options.virtualenv,TARGET))
 TARGET_py = os.path.join(TARGET,'bin','python')
+TARGET_easy_install = os.path.join(TARGET,'bin','easy_install')
+
+if options.easy_install_nose:
+    check_call('%s nose'%TARGET_easy_install)
 check_call('%s setup.py install'%TARGET_py)
