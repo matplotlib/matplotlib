901d83f99cbe2de85b33a43be8a123406ebabf2d
Andrew Straw <strawman@astraw.com>
2009-09-06 01:43:59 +0000

    testing: give more informative error when nose plugin not installed
    svn path=/trunk/matplotlib/; revision=7651

--- lib/matplotlib/testing/decorators.py
+++ lib/matplotlib/testing/decorators.py
@@ -16,7 +16,7 @@ def knownfailureif(fail_condition, msg=None):
                 result = f(*args, **kwargs)
             except:
                 if fail_condition:
-                    raise KnownFailureTest(msg)
+                    raise KnownFailureTest(msg) # An error here when running nose means that you don't have the matplotlib.testing.noseclasses:KnownFailure plugin in use.
                 else:
                     raise
             if fail_condition and fail_condition != 'indeterminate':
