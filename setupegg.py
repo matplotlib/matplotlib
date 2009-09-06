5bdcaf4ba176d7b1fdd4e6d96b3188d8da54efc6
Andrew Straw <strawman@astraw.com>
2009-09-06 01:42:43 +0000

    testing: install KnownFailure nose plugin if using setuptools
    svn path=/trunk/matplotlib/; revision=7644

--- setupegg.py
+++ setupegg.py
@@ -5,4 +5,9 @@ Poor man's setuptools script...
 from setuptools import setup
 execfile('setup.py',
          {'additional_params' :
-         {'namespace_packages' : ['mpl_toolkits']}})
+          {'namespace_packages' : ['mpl_toolkits'],
+           'entry_points': {'nose.plugins':
+                            [
+    'KnownFailure =  matplotlib.testing.noseclasses:KnownFailure',
+    ]
+                            }}})
