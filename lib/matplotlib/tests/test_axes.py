39af0e5e84b75f60d7d7bfcd125fb6d2395b15ef
Andrew Straw <strawman@astraw.com>
2009-09-07 20:02:39 +0000

    test conversion: move old-style test to new-style test
    svn path=/trunk/matplotlib/; revision=7685

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -58,3 +58,39 @@ def test_basic_annotate():
                  xytext=(3, 3), textcoords='offset points' )
 
     fig.savefig( 'offset_points' )
+
+@image_comparison(baseline_images=['polar_axes'])
+def test_polar_annotations():
+    """Polar Plot Annotations"""
+
+    # you can specify the xypoint and the xytext in different
+    # positions and coordinate systems, and optionally turn on a
+    # connecting line and mark the point with a marker.  Annotations
+    # work on polar axes too.  In the example below, the xy point is
+    # in native coordinates (xycoords defaults to 'data').  For a
+    # polar axes, this is in (theta, radius) space.  The text in this
+    # example is placed in the fractional figure coordinate system.
+    # Text keyword args like horizontal and vertical alignment are
+    # respected
+
+    # Setup some data
+    r = np.arange(0.0, 1.0, 0.001 )
+    theta = 2.0 * 2.0 * np.pi * r
+
+    fig = pylab.figure()
+    ax = fig.add_subplot( 111, polar=True )
+    line, = ax.plot( theta, r, color='#ee8d18', lw=3 )
+
+    ind = 800
+    thisr, thistheta = r[ind], theta[ind]
+    ax.plot([thistheta], [thisr], 'o')
+    ax.annotate('a polar annotation',
+                xy=(thistheta, thisr),  # theta, radius
+                xytext=(0.05, 0.05),    # fraction, fraction
+                textcoords='figure fraction',
+                arrowprops=dict(facecolor='black', shrink=0.05),
+                horizontalalignment='left',
+                verticalalignment='bottom',
+                )
+
+    fig.savefig( 'polar_axes' )
--- test/test_plots/TestAnnotation.py
+++ test/test_plots/TestAnnotation.py
@@ -42,45 +42,6 @@ class TestAnnotation( MplTestCase ):
       pass
 
    #--------------------------------------------------------------------
-   def testPolarAnnotations( self ):
-      """Polar Plot Annotations"""
-
-      # you can specify the xypoint and the xytext in different
-      # positions and coordinate systems, and optionally turn on a
-      # connecting line and mark the point with a marker.  Annotations
-      # work on polar axes too.  In the example below, the xy point is
-      # in native coordinates (xycoords defaults to 'data').  For a
-      # polar axes, this is in (theta, radius) space.  The text in this
-      # example is placed in the fractional figure coordinate system.
-      # Text keyword args like horizontal and vertical alignment are
-      # respected
-
-      # Setup some data
-      r = npy.arange(0.0, 1.0, 0.001 )
-      theta = 2.0 * 2.0 * npy.pi * r
-
-      fname = self.outFile( "polar_axes.png" )
-
-      fig = figure()
-      ax = fig.add_subplot( 111, polar=True )
-      line, = ax.plot( theta, r, color='#ee8d18', lw=3 )
-
-      ind = 800
-      thisr, thistheta = r[ind], theta[ind]
-      ax.plot([thistheta], [thisr], 'o')
-      ax.annotate('a polar annotation',
-                  xy=(thistheta, thisr),  # theta, radius
-                  xytext=(0.05, 0.05),    # fraction, fraction
-                  textcoords='figure fraction',
-                  arrowprops=dict(facecolor='black', shrink=0.05),
-                  horizontalalignment='left',
-                  verticalalignment='bottom',
-                  )
-
-      fig.savefig( fname )
-      self.checkImage( fname )
-
-   #--------------------------------------------------------------------
    def testPolarCoordAnnotations( self ):
       """Polar Coordinate Annotations"""
 
