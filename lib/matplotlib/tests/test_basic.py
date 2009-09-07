b65d1db0dc83185a6b994745a33a4246588c8d0e
Andrew Straw <strawman@astraw.com>
2009-09-07 20:56:17 +0000

    testing: remove docstrings on tests, which makes easier discovery of test location
    svn path=/trunk/matplotlib/; revision=7701

--- lib/matplotlib/tests/test_basic.py
+++ lib/matplotlib/tests/test_basic.py
@@ -2,10 +2,8 @@ from nose.tools import assert_equal
 from matplotlib.testing.decorators import knownfailureif
 
 def test_simple():
-    '''very simple example test'''
     assert_equal(1+1,2)
 
 @knownfailureif(True)
-def test_simple_fail():
-    '''very simple example test that should fail'''
+def test_simple_knownfail():
     assert_equal(1+1,3)
