eaf54918270423c1b5890d3978922edda36489e1
Andrew Straw <strawman@astraw.com>
2009-09-07 16:32:28 +0000

    testing: remove previous test images, preventing upload of removed tests
    svn path=/trunk/matplotlib/; revision=7673

--- test/_buildbot_test.py
+++ test/_buildbot_test.py
@@ -1,6 +1,6 @@
 """This script will install matplotlib to a virtual environment to
 faciltate testing."""
-import shutil, os, sys
+import shutil, os, sys, glob
 from subprocess import Popen, PIPE, STDOUT
 
 from _buildbot_util import check_call
@@ -12,6 +12,11 @@ if not os.path.exists(TARGET):
 
 TARGET_py = os.path.join(TARGET,'bin','python')
 check_call('%s -c "import shutil,matplotlib; x=matplotlib.get_configdir(); shutil.rmtree(x)"'%TARGET_py)
+
+
+previous_test_images = glob.glob(os.path.join('test','failed-diff-*.png'))
+for fname in previous_test_images:
+    os.unlink(fname)
 
 check_call('%s run-mpl-test.py --verbose --all --keep-failed'%TARGET_py,
            cwd='test')
