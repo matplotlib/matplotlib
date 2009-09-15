73711ebfa1a8777e01765e591451e5cc3a6e440d
Jouni K. Seppänen <jks@iki.fi>
2009-09-15 12:12:19 +0000

    Don't fail on AFM files containing floating-point bounding boxes
    svn path=/branches/v0_99_maint/; revision=7762

--- lib/matplotlib/afm.py
+++ lib/matplotlib/afm.py
@@ -165,7 +165,8 @@ def _parse_char_metrics(fh):
         num = _to_int(vals[0].split()[1])
         wx = _to_float(vals[1].split()[1])
         name = vals[2].split()[1]
-        bbox = _to_list_of_ints(vals[3][2:])
+        bbox = _to_list_of_floats(vals[3][2:])
+        bbox = map(int, bbox)
         # Workaround: If the character name is 'Euro', give it the corresponding
         # character code, according to WinAnsiEncoding (see PDF Reference).
         if name == 'Euro':
