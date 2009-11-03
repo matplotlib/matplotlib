ae377a26a1f4b7dd294819c3b68bc17f995d1f3b
Michael Droettboom <mdboom@gmail.com>
2009-11-03 17:57:52 +0000

    Merged revisions 7924 via svnmerge from https://matplotlib.svn.sf.net/svnroot/matplotlib/branches/v0_99_maint
    ........
      r7924 | mdboom | 2009-11-03 11:27:13 -0500 (Tue, 03 Nov 2009) | 2 lines
      
      [2880836] Save from toolbar changes directory with WX backend
    ........
    
    svn path=/trunk/matplotlib/; revision=7925

--- lib/matplotlib/backends/backend_wx.py
+++ lib/matplotlib/backends/backend_wx.py
@@ -1795,7 +1795,7 @@ class NavigationToolbar2Wx(NavigationToolbar2, wx.ToolBar):
         default_file = "image." + self.canvas.get_default_filetype()
         dlg = wx.FileDialog(self._parent, "Save to file", "", default_file,
                             filetypes,
-                            wx.SAVE|wx.OVERWRITE_PROMPT|wx.CHANGE_DIR)
+                            wx.SAVE|wx.OVERWRITE_PROMPT)
         dlg.SetFilterIndex(filter_index)
         if dlg.ShowModal() == wx.ID_OK:
             dirname  = dlg.GetDirectory()
