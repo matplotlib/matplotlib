947af089f79518e07313b6408cb4036673014f6c
Jeff Whitaker <jswhit@fastmail.fm>
2009-11-14 15:58:16 +0000

    make sure data points are plotted on top of filled contours.
    svn path=/trunk/matplotlib/; revision=7966

--- examples/pylab_examples/griddata_demo.py
+++ examples/pylab_examples/griddata_demo.py
@@ -11,15 +11,15 @@ y = uniform(-2,2,npts)
 z = x*np.exp(-x**2-y**2)
 # define grid.
 xi = np.linspace(-2.1,2.1,100)
-yi = np.linspace(-2.1,2.1,100)
+yi = np.linspace(-2.1,2.1,200)
 # grid the data.
-zi = griddata(x,y,z,xi,yi)
+zi = griddata(x,y,z,xi,yi,interp='linear')
 # contour the gridded data, plotting dots at the nonuniform data points.
 CS = plt.contour(xi,yi,zi,15,linewidths=0.5,colors='k')
 CS = plt.contourf(xi,yi,zi,15,cmap=plt.cm.jet)
 plt.colorbar() # draw colorbar
 # plot data points.
-plt.scatter(x,y,marker='o',c='b',s=5)
+plt.scatter(x,y,marker='o',c='b',s=5,zorder=10)
 plt.xlim(-2,2)    
 plt.ylim(-2,2)     
 plt.title('griddata test (%d points)' % npts)
