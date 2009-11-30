4eba0b4b42688a0905fff55b96d2d45d8cf807f6
Andrew Straw <strawman@astraw.com>
2009-11-30 09:06:48 +0000

    buildbot test running: run tests in MPL root dir (bypasses weird bug with scons build)
    svn path=/trunk/matplotlib/; revision=7987

--- test/_buildbot_test.py
+++ test/_buildbot_test.py
@@ -1,7 +1,6 @@
 """This script will install matplotlib to a virtual environment to
 faciltate testing."""
-import shutil, os, sys, glob
-from subprocess import Popen, PIPE, STDOUT
+import os, glob
 
 from _buildbot_util import check_call
 
@@ -13,10 +12,8 @@ if not os.path.exists(TARGET):
 TARGET_py = os.path.join(TARGET,'bin','python')
 check_call('%s -c "import shutil,matplotlib; x=matplotlib.get_configdir(); shutil.rmtree(x)"'%TARGET_py)
 
-
-previous_test_images = glob.glob(os.path.join('test','failed-diff-*.png'))
+previous_test_images = glob.glob('failed-diff-*.png')
 for fname in previous_test_images:
     os.unlink(fname)
 
-check_call('%s -c "import sys, matplotlib; success = matplotlib.test(verbosity=2); sys.exit(not success)"'%TARGET_py,
-           cwd='test')
+check_call('%s -c "import sys, matplotlib; success = matplotlib.test(verbosity=2); sys.exit(not success)"'%TARGET_py)
