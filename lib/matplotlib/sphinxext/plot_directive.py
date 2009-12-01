f61e7a5b5cf28728cfbd5653f04d69b12d6f9651
Michael Droettboom <mdboom@gmail.com>
2009-12-01 19:01:45 +0000

    [2906157] Let sphinx reference matplotlib-created plots (backport)
    svn path=/branches/v0_99_maint/; revision=7998

--- lib/matplotlib/sphinxext/plot_directive.py
+++ lib/matplotlib/sphinxext/plot_directive.py
@@ -276,8 +276,8 @@ def plot_directive(name, arguments, options, content, lineno,
     # treated as relative to the root of the documentation tree.  We need
     # to support both methods here.
     tmpdir = os.path.join('build', outdir)
+    tmpdir = os.path.abspath(tmpdir)
     if sphinx_version < (0, 6):
-        tmpdir = os.path.abspath(tmpdir)
         prefix = ''
     else:
         prefix = '/'
@@ -343,6 +343,36 @@ def plot_directive(name, arguments, options, content, lineno,
 
     return []
 
+def mark_plot_labels(app, document):
+    """
+    To make plots referenceable, we need to move the reference from
+    the "htmlonly" (or "latexonly") node to the actual figure node
+    itself.
+    """
+    for name, explicit in document.nametypes.iteritems():
+        if not explicit:
+            continue
+        labelid = document.nameids[name]
+        if labelid is None:
+            continue
+        node = document.ids[labelid]
+        if node.tagname in ('html_only', 'latex_only'):
+            for n in node:
+                if n.tagname == 'figure':
+                    sectname = name
+                    for c in n:
+                        if c.tagname == 'caption':
+                            sectname = c.astext()
+                            break
+
+                    node['ids'].remove(labelid)
+                    node['names'].remove(name)
+                    n['ids'].append(labelid)
+                    n['names'].append(name)
+                    document.settings.env.labels[name] = \
+                        document.settings.env.docname, labelid, sectname
+                    break
+
 def setup(app):
     setup.app = app
     setup.config = app.config
@@ -354,3 +384,4 @@ def setup(app):
         ['png', 'hires.png', 'pdf'],
         True)
 
+    app.connect('doctree-read', mark_plot_labels)
