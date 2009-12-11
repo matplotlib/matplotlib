7a91f965b8dbe68bb4db961a3eb8d9a2bf74f6b4
Andrew Straw <strawman@astraw.com>
2009-12-11 00:59:25 +0000

    tests: add tests for rec2csv and csv2rec
    svn path=/trunk/matplotlib/; revision=8020

--- lib/matplotlib/tests/test_mlab.py
+++ lib/matplotlib/tests/test_mlab.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.mlab as mlab
+import tempfile
+from nose.tools import raises
 
 def test_colinear_pca():
     a = mlab.PCA._get_colinear()
@@ -8,3 +10,25 @@ def test_colinear_pca():
     assert(np.allclose(pca.fracs[2:], 0.))
     assert(np.allclose(pca.Y[:,2:], 0.))
 
+def test_recarray_csv_roundtrip():
+    expected = np.recarray((99,),
+                          [('x',np.float),('y',np.float),('t',np.float)])
+    expected['x'][0] = 1
+    expected['y'][1] = 2
+    expected['t'][2] = 3
+    fd = tempfile.TemporaryFile(suffix='csv')
+    mlab.rec2csv(expected,fd)
+    fd.seek(0)
+    actual = mlab.csv2rec(fd)
+    fd.close()
+    assert np.allclose( expected['x'], actual['x'] )
+    assert np.allclose( expected['y'], actual['y'] )
+    assert np.allclose( expected['t'], actual['t'] )
+
+@raises(ValueError)
+def test_rec2csv_bad_shape():
+    bad = np.recarray((99,4),[('x',np.float),('y',np.float)])
+    fd = tempfile.TemporaryFile(suffix='csv')
+
+    # the bad recarray should trigger a ValueError for having ndim > 1.
+    mlab.rec2csv(bad,fd)
