f9429f8a23e336e5c6d38dd5cc0a30cded96a0fd
Andrew Straw <strawman@astraw.com>
2010-01-04 17:22:32 +0000

    buildbot: debug documentation uploads
    svn path=/trunk/matplotlib/; revision=8073

--- test/_buildbot_doc.sh
+++ test/_buildbot_doc.sh
@@ -2,13 +2,31 @@
 set -e
 
 TARGET=`pwd`/PYmpl
+echo "activating virtual environment"
 source $TARGET/bin/activate
 
+echo "removing MPL config dir"
 python -c "import shutil,matplotlib; x=matplotlib.get_configdir(); shutil.rmtree(x)"
 
+echo "calling 'easy_install sphinx'"
 easy_install sphinx
 
+echo "calling 'cd doc'"
 cd doc
 
+echo "calling 'python make.py clean'"
 python make.py clean
+
+echo "calling 'python make.py all'"
 python make.py all
+
+# SourceForce needs the below
+echo "configuring for upload to SourceForge"
+
+echo "Options +Indexes" > build/html/.htaccess
+
+chmod -R a+r build
+find build -type d | xargs chmod a+rx
+
+echo "listing built files"
+find build
