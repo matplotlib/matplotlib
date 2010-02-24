18a01adfe5a9f17d9174468e07cf10f65f7e806f
Eric Firing <efiring@hawaii.edu>
2010-02-24 18:30:42 +0000

    cntr.c: patch by Ian Thomas to fix bug ID 2956378
    svn path=/trunk/matplotlib/; revision=8152

--- src/cntr.c
+++ src/cntr.c
@@ -605,6 +605,9 @@ edge_walker (Csite * site,  int pass2)
                 }
                 if (fwd < 0 && level0 && left < 0)
                 {
+		    /* remove J0_START from this boundary edge as boundary is
+		     * included by the upwards slit from contour line below. */
+                    data[edge] &= ~J0_START;
                     if (n_kind) kcp[n_kind] += kind_start_slit;
                     return slit_cutter (site, 0, pass2);
                 }
