abd728c31af01b4b10e9514cd31dd89be4f447b4
Ryan May <rmay31@gmail.com>
2010-04-16 19:01:30 +0000

    Add basic example/test for close_event.
    svn path=/trunk/matplotlib/; revision=8238

---
+++ examples/event_handling/close_event.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def handle_close(evt):
+    print 'Closed Figure!'
+
+fig = plt.figure()
+fig.canvas.mpl_connect('close_event', handle_close)
+
+plt.text(0.35, 0.5, 'Close Me!', dict(size=30))
+plt.show()
