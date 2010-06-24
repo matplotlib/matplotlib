f7126f73f0102f503edfae441c8e62f3c7f5d29f
Michael Droettboom <mdboom@gmail.com>
2010-06-24 14:48:00 +0000

    [3020704] set_xlim() problem Bug in PathClipper when vertices fall precisely on the clipping edge.
    svn path=/trunk/matplotlib/; revision=8461

--- lib/matplotlib/tests/test_simplification.py
+++ lib/matplotlib/tests/test_simplification.py
@@ -186,6 +186,23 @@ def test_throw_rendering_complexity_exceeded():
     else:
         rcParams['path.simplify'] = True
 
+@image_comparison(baseline_images=['clipper_edge'])
+def test_clipper():
+    dat = (0, 1, 0, 2, 0, 3, 0, 4, 0, 5)
+    fig = plt.figure(figsize=(2, 1))
+    fig.subplots_adjust(left = 0, bottom = 0, wspace = 0, hspace = 0)
+
+    ax = fig.add_axes((0, 0, 1.0, 1.0), ylim = (0, 5), autoscale_on = False)
+    ax.plot(dat)
+    ax.xaxis.set_major_locator(plt.MultipleLocator(1))
+    ax.xaxis.set_major_formatter(plt.NullFormatter())
+    ax.yaxis.set_major_locator(plt.MultipleLocator(1))
+    ax.yaxis.set_major_formatter(plt.NullFormatter())
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    ax.set_xlim(5, 9)
+    fig.savefig('clipper_edge')
 
 if __name__=='__main__':
     import nose
