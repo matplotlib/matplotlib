b9f61365b9b13bae6ec5ae3fc5d6bca60693cd23
John Hunter <jdh2358@gmail.com>
2010-06-29 13:30:09 +0000

    bump version number for rc
    svn path=/trunk/matplotlib/; revision=8476

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -99,7 +99,7 @@ to MATLAB&reg;, a registered trademark of The MathWorks, Inc.
 """
 from __future__ import generators
 
-__version__  = '1.0.svn'
+__version__  = '1.0rc1'
 __revision__ = '$Revision$'
 __date__     = '$Date$'
 
