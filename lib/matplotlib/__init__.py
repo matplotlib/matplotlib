668a769fb6ef399476d573bf86cceb2fa9168495
John Hunter <jdh2358@gmail.com>
2010-07-06 13:56:31 +0000

    tagging for release 1.0
    svn path=/trunk/matplotlib/; revision=8503

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -99,7 +99,7 @@ to MATLAB&reg;, a registered trademark of The MathWorks, Inc.
 """
 from __future__ import generators
 
-__version__  = '1.0.0svn'
+__version__  = '1.0.0'
 __revision__ = '$Revision$'
 __date__     = '$Date$'
 
