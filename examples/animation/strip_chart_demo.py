aa9bf9b192744d7bc90600bfb96cc266dd81a249
Ryan May <rmay31@gmail.com>
2010-08-26 03:23:25 +0000

    Add previous animation examples ported to new framework, as well as a few new ones.
    svn path=/trunk/matplotlib/; revision=8660

---
+++ examples/animation/strip_chart_demo.py
@@ -0,0 +1,49 @@
+"""
+Emulate an oscilloscope.  Requires the animation API introduced in
+matplotlib 1.0 SVN.
+"""
+import matplotlib
+import numpy as np
+from matplotlib.lines import Line2D
+import matplotlib.pyplot as plt
+from animation import FuncAnimation
+
+class Scope:
+    def __init__(self, ax, maxt=10, dt=0.01):
+        self.ax = ax
+        self.dt = dt
+        self.maxt = maxt
+        self.tdata = [0]
+        self.ydata = [0]
+        self.line = Line2D(self.tdata, self.ydata)
+        self.ax.add_line(self.line)
+        self.ax.set_ylim(-.1, 1.1)
+        self.ax.set_xlim(0, self.maxt)
+
+    def update(self, y):
+        lastt = self.tdata[-1]
+        if lastt > self.tdata[0] + self.maxt: # reset the arrays
+            self.tdata = [self.tdata[-1]]
+            self.ydata = [self.ydata[-1]]
+            self.ax.set_xlim(self.tdata[0], self.tdata[0] + self.maxt)
+
+        t = self.tdata[-1] + self.dt
+        self.tdata.append(t)
+        self.ydata.append(y)
+        self.line.set_data(self.tdata, self.ydata)
+        return self.line,
+
+def emitter(p=0.01):
+    'return a random value with probability p, else 0'
+    while True:
+        v = np.random.rand(1)
+        if v > p:
+            yield 0.
+        else:
+            yield np.random.rand(1)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+scope = Scope(ax)
+ani = FuncAnimation(fig, scope.update, emitter, interval=10, blit=True)
+plt.show()
