6eea698ba26e07f4f4cf9c5855c2839fb75663ff
Stefan van der Walt <stefan@sun.ac.za>
2011-03-05 23:55:51 +0200

    compare: Use same histogram bins for all comparisons.

--- lib/matplotlib/testing/compare.py
+++ lib/matplotlib/testing/compare.py
@@ -198,11 +198,14 @@ def compare_images( expected, actual, tol, in_decorator=False ):
 
    # compare the resulting image histogram functions
    rms = 0
+   bins = np.arange(257)
    for i in xrange(0, 3):
       h1p = expectedImage[:,:,i]
       h2p = actualImage[:,:,i]
-      h1h = np.histogram(h1p, bins=256)[0]
-      h2h = np.histogram(h2p, bins=256)[0]
+
+      h1h = np.histogram(h1p, bins=bins)[0]
+      h2h = np.histogram(h2p, bins=bins)[0]
+
       rms += np.sum(np.power((h1h-h2h), 2))
    rms = np.sqrt(rms / (256 * 3))
 
