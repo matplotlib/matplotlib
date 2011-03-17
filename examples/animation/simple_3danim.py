4ce5aec0b0f2c3bc785fef2732d8230c3a637da0
Christoph Gohlke <cgohlke@uci.edu>
2011-03-17 09:26:02 -0400

    Miscellaneous fixes to the examples to make them run on both Python 3.x and Python 2.x

--- examples/animation/simple_3danim.py
+++ examples/animation/simple_3danim.py
@@ -15,7 +15,7 @@ def Gen_RandLine(length, dims=2) :
     """
     lineData = np.empty((dims, length))
     lineData[:, 0] = np.random.rand(dims)
-    for index in xrange(1, length) :
+    for index in range(1, length) :
         # scaling the random numbers by 0.1 so
         # movement is small compared to position.
         # subtraction by 0.5 is to change the range to [-0.5, 0.5]
