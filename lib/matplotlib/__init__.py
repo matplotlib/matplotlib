ac63bbbc6366817fd217b8acbdc5665f60bc5359
Paul Ivanov <pivanov314@gmail.com>
2011-03-25 17:20:16 -0700

    include test_text in default_test_modules

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -968,7 +968,8 @@ default_test_modules = [
     'matplotlib.tests.test_spines',
     'matplotlib.tests.test_image',
     'matplotlib.tests.test_simplification',
-    'matplotlib.tests.test_mathtext'
+    'matplotlib.tests.test_mathtext',
+    'matplotlib.tests.test_text'
     ]
 
 def test(verbosity=0):
