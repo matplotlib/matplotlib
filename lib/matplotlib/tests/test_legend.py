f641d334fa46ef116d08d71f13481df6b13ca66d
Michael Droettboom <mdboom@gmail.com>
2011-04-04 10:59:06 -0400

    Move auto_legend.py tests into regression test framework.

---
+++ lib/matplotlib/tests/test_legend.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from matplotlib.testing.decorators import image_comparison, knownfailureif
+import matplotlib.pyplot as plt
+from nose.tools import assert_raises
+from numpy.testing import assert_array_equal
+
+@image_comparison(baseline_images=['legend_auto1'])
+def test_legend_auto1():
+    'Test automatic legend placement'
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    x = np.arange(100)
+    ax.plot(x, 50-x, 'o', label='y=1')
+    ax.plot(x, x-50, 'o', label='y=-1')
+    ax.legend(loc=0)
+    fig.savefig('legend_auto1')
+
+@image_comparison(baseline_images=['legend_auto2'])
+def test_legend_auto2():
+    'Test automatic legend placement'
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    x = np.arange(100)
+    b1 = ax.bar(x, x, color='m')
+    b2 = ax.bar(x, x[::-1], color='g')
+    ax.legend([b1[0], b2[0]], ['up', 'down'], loc=0)
+    fig.savefig('legend_auto2')
+
