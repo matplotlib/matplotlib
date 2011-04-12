4c8e5a3f9483c52a569855209ccf2768bb7355ff
Tobias Hoppe <tobi@hoppeban.de>
2011-04-12 10:42:01 +0200

    Rewritten listFiles in cbook.py to work on Py3k
    Using os.walk() instead of os.path.walk() to support Py3k
    Tested to work on Python2.6 - Python3.2

--- lib/matplotlib/cbook.py
+++ lib/matplotlib/cbook.py
@@ -1047,27 +1047,22 @@ def listFiles(root, patterns='*', recurse=1, return_folders=0):
     import os.path, fnmatch
     # Expand patterns from semicolon-separated string to list
     pattern_list = patterns.split(';')
-    # Collect input and output arguments into one bunch
-    class Bunch:
-        def __init__(self, **kwds): self.__dict__.update(kwds)
-    arg = Bunch(recurse=recurse, pattern_list=pattern_list,
-        return_folders=return_folders, results=[])
+    results = []
 
-    def visit(arg, dirname, files):
-        # Append to arg.results all relevant files (and perhaps folders)
+    for dirname, dirs, files in os.walk(root):
+        # Append to results all relevant files (and perhaps folders)
         for name in files:
             fullname = os.path.normpath(os.path.join(dirname, name))
-            if arg.return_folders or os.path.isfile(fullname):
-                for pattern in arg.pattern_list:
+            if return_folders or os.path.isfile(fullname):
+                for pattern in pattern_list:
                     if fnmatch.fnmatch(name, pattern):
-                        arg.results.append(fullname)
+                        results.append(fullname)
                         break
         # Block recursion if recursion was disallowed
-        if not arg.recurse: files[:]=[]
+        if not recurse:
+            break
 
-    os.path.walk(root, visit, arg)
-
-    return arg.results
+    return results
 
 def get_recursive_filelist(args):
     """
