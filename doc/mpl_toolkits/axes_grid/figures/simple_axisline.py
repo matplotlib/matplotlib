22222e4d5cf4e9509e080e2ee70dd19ecfe52800
Michael Droettboom <mdboom@gmail.com>
2011-05-06 15:27:20 -0400

    Restores files accidentally removed in commit 0e6dad5230

---
+++ doc/mpl_toolkits/axes_grid/figures/simple_axisline.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.axes_grid.axislines import SubplotZero
+
+if 1:
+
+    fig = plt.figure(1)
+    fig.subplots_adjust(right=0.85)
+    ax = SubplotZero(fig, 1, 1, 1)
+    fig.add_subplot(ax)
+
+    # make right and top axis invisible
+    ax.axis["right"].set_visible(False)
+    ax.axis["top"].set_visible(False)
+
+    # make xzero axis (horizontal axis line through y=0) visible.
+    ax.axis["xzero"].set_visible(True)
+    ax.axis["xzero"].label.set_text("Axis Zero")
+
+    ax.set_ylim(-2, 4)
+    ax.set_xlabel("Label X")
+    ax.set_ylabel("Label Y")
+    # or
+    #ax.axis["bottom"].label.set_text("Label X")
+    #ax.axis["left"].label.set_text("Label Y")
+
+    # make new (right-side) yaxis, but wth some offset
+    offset = (20, 0)
+    new_axisline = ax.get_grid_helper().new_fixed_axis
+
+    ax.axis["right2"] = new_axisline(loc="right",
+                                     offset=offset,
+                                     axes=ax)
+    ax.axis["right2"].label.set_text("Label Y2")
+
+    ax.plot([-2,3,2])
+    plt.draw()
+    plt.show()
+
---
+++ doc/mpl_toolkits/axes_grid/figures/simple_rgb.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.axes_grid1.axes_rgb import RGBAxes
+
+def get_demo_image():
+    import numpy as np
+    from matplotlib.cbook import get_sample_data
+    f = get_sample_data("axes_grid/bivariate_normal.npy", asfileobj=False)
+    z = np.load(f)
+    # z is a numpy array of 15x15
+    return z, (-3,4,-4,3)
+
+def get_rgb():
+    Z, extent = get_demo_image()
+
+    Z[Z<0] = 0.
+    Z = Z/Z.max()
+
+    R = Z[:13,:13]
+    G = Z[2:,2:]
+    B = Z[:13,2:]
+
+    return R, G, B
+
+
+fig = plt.figure(1)
+ax = RGBAxes(fig, [0.1, 0.1, 0.8, 0.8])
+
+r, g, b = get_rgb()
+kwargs = dict(origin="lower", interpolation="nearest")
+ax.imshow_rgb(r, g, b, **kwargs)
+
+ax.RGB.set_xlim(0., 9.5)
+ax.RGB.set_ylim(0.9, 10.6)
+
+
+plt.draw()
+plt.show()
