551d35c17b3140c5043575976c3bba51e53def6a
Skipper Seabold <jsseabold@gmail.com>
2011-06-02 17:39:47 -0400

    Patched pyplot.subplots for squeeze==False

--- lib/matplotlib/pyplot.py
+++ lib/matplotlib/pyplot.py
@@ -799,6 +799,9 @@ def subplots(nrows=1, ncols=1, sharex=False, sharey=False, squeeze=True,
             ret = fig, axarr[0,0]
         else:
             ret = fig, axarr.squeeze()
+    else:
+        # returned axis array will be always 2-d, even if nrows=ncols=1
+        return fig, axarr.reshape(nrows, ncols)
 
 
     return ret
