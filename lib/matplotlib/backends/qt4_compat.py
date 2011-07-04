5f287e128bd9f5bd4928aa5f946a1954d2c5a48c
Eric Firing <efiring@hawaii.edu>
2011-07-04 10:38:59 -1000

    qt4_compat.py: updated to match ipython 0.11 RC 1.
    It is based on IPython/external/qt.py. The previous version was
    based on an earlier version of that file, but fails with the present
    ipython:
    
    http://www.mail-archive.com/matplotlib-devel@lists.sourceforge.net/msg08332.html

--- lib/matplotlib/backends/qt4_compat.py
+++ lib/matplotlib/backends/qt4_compat.py
@@ -2,60 +2,69 @@
 """
 
 import os
+import warnings
 
 # Available APIs.
 QT_API_PYQT = 'pyqt'
 QT_API_PYSIDE = 'pyside'
 
-# Select PyQt4 or PySide using an environment variable, in the same way IPython does.
-# IPython is using PyQt as default (for now) so we will too.
-QT_API = os.environ.get('QT_API', QT_API_PYQT)
+def prepare_pyqt4():
+    # For PySide compatibility, use the new-style string API that automatically
+    # converts QStrings to Unicode Python strings. Also, automatically unpack
+    # QVariants to their underlying objects.
+    import sip
+    sip.setapi('QString', 2)
+    sip.setapi('QVariant', 2)
 
-if QT_API == QT_API_PYQT:
+# Select Qt binding, using the QT_API environment variable if available.
+QT_API = os.environ.get('QT_API')
+if QT_API is None:
     try:
-        from PyQt4 import QtCore, QtGui
+        import PySide
+        if PySide.__version_info__ < (1,0,3):
+            warnings.warn("PySide found with version < 1.0.3; trying PyQt4")
+            raise ImportError
+        QT_API = QT_API_PYSIDE
     except ImportError:
-        raise ImportError("Qt4 backend requires that PyQt4 is installed.")
+        try:
+            prepare_pyqt4()
+            import PyQt4
+            QT_API = QT_API_PYQT
+        except ImportError:
+            raise ImportError('Cannot import PySide or PyQt4')
+
+elif QT_API == QT_API_PYQT:
+    # Note: This must be called *before* PyQt4 is imported.
+    prepare_pyqt4()
+
+# Now perform the imports.
+if QT_API == QT_API_PYQT:
+    from PyQt4 import QtCore, QtGui, QtSvg
+
     # Alias PyQt-specific functions for PySide compatibility.
+    QtCore.Signal = QtCore.pyqtSignal
     try:
         QtCore.Slot = QtCore.pyqtSlot
     except AttributeError:
-        QtCore.Slot = pyqtSignature # Not a perfect match but 
+        QtCore.Slot = pyqtSignature # Not a perfect match but
                                     # works in simple cases
     QtCore.Property = QtCore.pyqtProperty
     __version__ = QtCore.PYQT_VERSION_STR
-    import sip
-    try :
-        if sip.getapi("QString") > 1 :
-            # Use new getSaveFileNameAndFilter()
-            _getSaveFileName = lambda self, msg, start, filters, \
-                                      selectedFilter : \
-                                QtGui.QFileDialog.getSaveFileNameAndFilter( \
-                                self, msg, start, filters, selectedFilter)[0]
-        else :
-            # Use old getSaveFileName()
-            _getSaveFileName = QtGui.QFileDialog.getSaveFileName
-    except (AttributeError, KeyError) :
-        # call to getapi() can fail in older versions of sip
-        # Use the old getSaveFileName()
-        _getSaveFileName = QtGui.QFileDialog.getSaveFileName
+
+    # Use new getSaveFileNameAndFilter()
+    _getSaveFileName = lambda self, msg, start, filters, \
+                              selectedFilter : \
+                        QtGui.QFileDialog.getSaveFileNameAndFilter( \
+                        self, msg, start, filters, selectedFilter)[0]
+
+
 
 elif QT_API == QT_API_PYSIDE:
-    try:
-        from PySide import QtCore, QtGui, __version__, __version_info__
-    except ImportError:
-        raise ImportError("Qt4 backend requires that PySide is installed.")
+    from PySide import QtCore, QtGui, __version__, __version_info__
     if __version_info__ < (1,0,3):
-        raise ImportError("Matplotlib backend_qt4 and backend_qt4agg require PySide >=1.0.3")
+        raise ImportError(
+            "Matplotlib backend_qt4 and backend_qt4agg require PySide >=1.0.3")
 
-    # Alias PySide-specific function for PyQt compatibilty
-    QtCore.pyqtProperty = QtCore.Property
-    QtCore.pyqtSignature = QtCore.Slot # Not a perfect match but 
-                                       # works in simple cases
-
-    _getSaveFileName = lambda self, msg, start, filters, selectedFilter : \
-                        QtGui.QFileDialog.getSaveFileName(self,  \
-                        msg, start, filters, selectedFilter)[0]
 else:
     raise RuntimeError('Invalid Qt API %r, valid values are: %r or %r' %
                        (QT_API, QT_API_PYQT, QT_API_PYSIDE))
