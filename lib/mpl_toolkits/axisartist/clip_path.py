0679e935f464f01306a8321ed5cb15670b2eec2f
Jae-Joon Lee <lee.j.joon@gmail.com>
2011-08-10 10:06:45 +0900

    fix axisartist behavior when axis limit is inverted

--- lib/mpl_toolkits/axisartist/clip_path.py
+++ lib/mpl_toolkits/axisartist/clip_path.py
@@ -12,12 +12,23 @@ def atan2(dy, dx):
 # FIXME : The current algorithm seems to return incorrect angle when the line
 # ends at the boundary.
 
-def clip(xlines, ylines, x0, clip="right"):
+def clip(xlines, ylines, x0, clip="right", xdir=True, ydir=True):
 
     clipped_xlines = []
     clipped_ylines = []
 
     _pos_angles = []
+
+    if xdir:
+        xsign = 1
+    else:
+        xsign = -1
+
+    if ydir:
+        ysign = 1
+    else:
+        ysign = -1
+        
 
     for x, y in zip(xlines, ylines):
 
@@ -49,7 +60,7 @@ def clip(xlines, ylines, x0, clip="right"):
                     dx = x[i+1] - x[i]
                     dy = y[i+1] - y[i]
 
-                a = degrees(atan2(dy, dx))
+                a = degrees(atan2(ysign*dy, xsign*dx))
                 _pos_angles.append((x0, y0, a))
 
             elif c == 1:
@@ -63,7 +74,7 @@ def clip(xlines, ylines, x0, clip="right"):
                     dx = x[i+1] - x[i]
                     dy = y[i+1] - y[i]
 
-                a = degrees(atan2(dy, dx))
+                a = degrees(atan2(ysign*dy, xsign*dx))
                 _pos_angles.append((x0, y0, a))
 
                 #print x[i], x[i+1]
@@ -82,10 +93,28 @@ def clip_line_to_rect(xline, yline, bbox):
 
     x0, y0, x1, y1 = bbox.extents
 
-    lx1, ly1, c_right_ = clip([xline], [yline], x1, clip="right")
-    lx2, ly2, c_left_ = clip(lx1, ly1, x0, clip="left")
-    ly3, lx3, c_top_ = clip(ly2, lx2, y1, clip="right")
-    ly4, lx4, c_bottom_ = clip(ly3, lx3, y0, clip="left")
+    xdir = x1 > x0
+    ydir = y1 > y0
+    
+    if x1 > x0:
+        lx1, ly1, c_right_ = clip([xline], [yline], x1, clip="right", xdir=xdir, ydir=ydir)
+        lx2, ly2, c_left_ = clip(lx1, ly1, x0, clip="left", xdir=xdir, ydir=ydir)
+    else:
+        lx1, ly1, c_right_ = clip([xline], [yline], x0, clip="right", xdir=xdir, ydir=ydir)
+        lx2, ly2, c_left_ = clip(lx1, ly1, x1, clip="left", xdir=xdir, ydir=ydir)
+
+    if y1 > y0:
+        ly3, lx3, c_top_ = clip(ly2, lx2, y1, clip="right", xdir=ydir, ydir=xdir)
+        ly4, lx4, c_bottom_ = clip(ly3, lx3, y0, clip="left", xdir=ydir, ydir=xdir)
+    else:
+        ly3, lx3, c_top_ = clip(ly2, lx2, y0, clip="right", xdir=ydir, ydir=xdir)
+        ly4, lx4, c_bottom_ = clip(ly3, lx3, y1, clip="left", xdir=ydir, ydir=xdir)
+
+        
+    # lx1, ly1, c_right_ = clip([xline], [yline], x1, clip="right")
+    # lx2, ly2, c_left_ = clip(lx1, ly1, x0, clip="left")
+    # ly3, lx3, c_top_ = clip(ly2, lx2, y1, clip="right")
+    # ly4, lx4, c_bottom_ = clip(ly3, lx3, y0, clip="left")
 
     #c_left = [((x, y), (a+90)%180-180) for (x, y, a) in c_left_ \
     #          if bbox.containsy(y)]
