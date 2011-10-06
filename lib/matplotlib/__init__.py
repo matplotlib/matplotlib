404a8d82c67ed12f0b247559a4a34376437601f8
jdh2358 <jdh2358@gmail.com>
2011-10-06 10:01:09 -0500

    merge v1.1.x into master

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -99,7 +99,7 @@ to MATLAB&reg;, a registered trademark of The MathWorks, Inc.
 """
 from __future__ import generators
 
-__version__  = '1.1.0'
+__version__  = '1.1.x'
 
 import os, re, shutil, subprocess, sys, warnings
 import distutils.sysconfig
