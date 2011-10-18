matplotlib/matplotlib

commit 6938b00255830cb81af2d427b85b7c777d6f5356

backend_qt4agg: eliminate duplicate call to FigureCanvasAgg.draw()
The basic problem is that the most efficient way to eliminate the
duplication appears to be to do the Agg drawing in the paintEvent,
not in draw(); but then a draw() followed by copy_from_bbox may
result in a copy being made before the Agg draw has occurred.
The solution here is to do that drawing inside copy_from_bbox
when necessary.

Side note: it seems that by using the update() method, the
qt4agg draw() is acting more like draw_idle() than like the draw()
methods in the other interactive backends.


svn path=/trunk/matplotlib/; revision=8922

lib/matplotlib/backends/backend_qt4agg.py
@@ -110,7 +110,7 @@ class FigureCanvasQTAgg( FigureCanvasQT, FigureCanvasAgg ):
             w = int(r) - int(l)
             h = int(t) - int(b)
             t = int(b) + h
-            reg = self.copy_from_bbox(bbox)
+            reg = FigureCanvasAgg.copy_from_bbox(self, bbox)
             stringBuffer = reg.to_string_argb()
             qImage = QtGui.QImage(stringBuffer, w, h, QtGui.QImage.Format_ARGB32)
             pixmap = QtGui.QPixmap.fromImage(qImage)
@@ -127,7 +127,6 @@ class FigureCanvasQTAgg( FigureCanvasQT, FigureCanvasAgg ):
 
         if DEBUG: print "FigureCanvasQtAgg.draw", self
         self.replot = True
-        FigureCanvasAgg.draw(self)
         self.update()
 
     def blit(self, bbox=None):
@@ -139,6 +138,16 @@ class FigureCanvasQTAgg( FigureCanvasQT, FigureCanvasAgg ):
         l, b, w, h = bbox.bounds
         t = b + h
         self.repaint(l, self.renderer.height-t, w, h)
+
+    def copy_from_bbox(self, *args):
+        """
+        If a draw() has been called but the update() has not
+        occurred, draw into the agg canvas before copying.
+        """
+        if self.replot:
+            FigureCanvasAgg.draw(self)
+            self.replot = False
+        return FigureCanvasAgg.copy_from_bbox(self, *args)
 
     def print_figure(self, *args, **kwargs):
         FigureCanvasAgg.print_figure(self, *args, **kwargs)

commit b18a1fbc7ae4cebb36675f46ddcd10b504669170

backend_qt4agg: draw() immediately calls FigureCanvasAgg.draw()
This is the latest in a series of modifications to the Qt4Agg
drawing strategy going back several years.  It simplifies the
code and should solve the problem introduced in 6938b0025; that
is, delaying the Agg draw operation until the paintEvent breaks
code expecting that draw operation to have occurred immediately.
The problem was reported here:
http://sourceforge.net/mailarchive/message.php?msg_id=28245744

lib/matplotlib/backends/backend_qt4agg.py
@@ -59,7 +59,6 @@ class FigureCanvasQTAgg( FigureCanvasQT, FigureCanvasAgg ):
         self.drawRect = False
         self.rect = []
         self.blitbox = None
-        self.replot = True
         self.setAttribute(QtCore.Qt.WA_OpaquePaintEvent)
 
     def drawRectangle( self, rect ):
@@ -69,7 +68,7 @@ class FigureCanvasQTAgg( FigureCanvasQT, FigureCanvasAgg ):
 
     def paintEvent( self, e ):
         """
-        Draw to the Agg backend and then copy the image to the qt.drawable.
+        Copy the image from the Agg canvas to the qt.drawable.
         In Qt, all drawing should be done inside of here when a widget is
         shown onscreen.
         """
@@ -77,10 +76,6 @@ class FigureCanvasQTAgg( FigureCanvasQT, FigureCanvasAgg ):
         #FigureCanvasQT.paintEvent( self, e )
         if DEBUG: print 'FigureCanvasQtAgg.paintEvent: ', self, \
            self.get_width_height()
-
-        if self.replot:
-            FigureCanvasAgg.draw(self)
-            self.replot = False
 
         if self.blitbox is None:
             # matplotlib is in rgba byte order.  QImage wants to put the bytes
@@ -121,11 +116,13 @@ class FigureCanvasQTAgg( FigureCanvasQT, FigureCanvasAgg ):
 
     def draw( self ):
         """
-        Draw the figure when xwindows is ready for the update
+        Draw the figure with Agg, and queue a request
+        for a Qt draw.
         """
-
-        if DEBUG: print "FigureCanvasQtAgg.draw", self
-        self.replot = True
+        # The Agg draw is done here; delaying it until the paintEvent
+        # causes problems with code that uses the result of the
+        # draw() to update plot elements.
+        FigureCanvasAgg.draw(self)
         self.update()
 
     def blit(self, bbox=None):