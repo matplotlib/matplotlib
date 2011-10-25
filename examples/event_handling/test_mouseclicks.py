0a5ddc4e9953dc92ef2303d9884f4be89ec37b3d
Daniel Hyams <dhyams@gmail.com>
2011-10-25 17:12:41 -0400

    Some small stylistic changes as per comments from BR.

--- examples/event_handling/test_mouseclicks.py
+++ examples/event_handling/test_mouseclicks.py
@@ -2,14 +2,14 @@
 import matplotlib
 #matplotlib.use("WxAgg")
 #matplotlib.use("TkAgg")
-matplotlib.use("GTKAgg")
+#matplotlib.use("GTKAgg")
 #matplotlib.use("QtAgg")
 #matplotlib.use("Qt4Agg")
+#matplotlib.use("CocoaAgg")
+#matplotlib.use("MacOSX")
 import matplotlib.pyplot as plt
-import math
-import numpy
 
-print "***** TESTING WITH BACKEND: %s"%matplotlib.get_backend() + " *****"
+#print "***** TESTING WITH BACKEND: %s"%matplotlib.get_backend() + " *****"
 
 def OnClick(event):
    if event.dblclick:
@@ -25,9 +25,7 @@ fig = plt.gcf()
 cid_up = fig.canvas.mpl_connect('button_press_event',OnClick)
 cid_down = fig.canvas.mpl_connect('button_release_event',OnRelease)
 
-t = numpy.arange(0.0,3.0,0.01)
-s = numpy.sin(2*math.pi*t)
-plt.plot(t,s)
+plt.gca().text(0.5,0.5,"Click on the canvas to test mouse events.",ha="center",va="center")
 
 plt.show()
 
