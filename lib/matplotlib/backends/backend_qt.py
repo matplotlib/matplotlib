287b814a132a976f5f0d8002687ce68dba2eb507
Jae-Joon Lee <lee.j.joon@gmail.com>
2011-11-04 16:34:21 +0900

    fix print statements in backend_qt.py and backend_qt4.py

--- lib/matplotlib/backends/backend_qt.py
+++ lib/matplotlib/backends/backend_qt.py
@@ -114,7 +114,7 @@ class FigureCanvasQT( qt.QWidget, FigureCanvasBase ):
         y = self.figure.bbox.height - event.pos().y()
         button = self.buttond[event.button()]
         FigureCanvasBase.button_press_event( self, x, y, button, dblclick=True )
-        if DEBUG: print 'button doubleclicked:', event.button()
+        if DEBUG: print('button doubleclicked:', event.button())
 
     def mouseMoveEvent( self, event ):
         x = event.x()
