32f7f4876fd98efb52ed5da59aa296eab7fc49ec
Michael Droettboom <mdboom@gmail.com>
2011-11-17 09:49:24 -0500

    Avoid a recursive import problem in unicode_safe()

--- lib/matplotlib/cbook.py
+++ lib/matplotlib/cbook.py
@@ -28,15 +28,18 @@ major, minor1, minor2, s, tmp = sys.version_info
 # On some systems, getpreferredencoding sets the locale, which has
 # side effects.  Passing False eliminates those side effects.
 
-try:
-    preferredencoding = locale.getpreferredencoding(
-        matplotlib.rcParams['axes.formatter.use_locale']).strip()
-    if not preferredencoding:
-        preferredencoding = None
-except (ValueError, ImportError, AttributeError):
-    preferredencoding = None
 
 def unicode_safe(s):
+    import matplotlib
+
+    try:
+        preferredencoding = locale.getpreferredencoding(
+            matplotlib.rcParams['axes.formatter.use_locale']).strip()
+        if not preferredencoding:
+            preferredencoding = None
+    except (ValueError, ImportError, AttributeError):
+        preferredencoding = None
+
     if preferredencoding is None: return unicode(s)
     else: return unicode(s, preferredencoding)
 
