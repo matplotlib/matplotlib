e6ab69183c606246bdf60111ec163ff80919f9f7
Paul Ivanov <pivanov314@gmail.com>
2012-02-06 13:57:55 -0800

    fix typos and unused imports in ex/event_handling

--- examples/event_handling/pipong.py
+++ examples/event_handling/pipong.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # A matplotlib based game of Pong illustrating one way to write interactive
-# animation which are easily ported to multiply backends
+# animation which are easily ported to multiple backends
 # pipong.py was written by Paul Ivanov <http://pirsquared.org>
 
 from __future__ import print_function
