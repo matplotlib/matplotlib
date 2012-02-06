8c37466d4f5b2489c6848e4b2dc934594d02824c
Michael Droettboom <mdboom@gmail.com>
2012-02-06 14:26:16 -0500

    Fix single pixel markers.  The pixel shape is now optimized so it does the right thing in the Agg backend.  For raster backends, linejoin and cap styles is now correctly passed to and used in the backend.

--- lib/matplotlib/backends/backend_ps.py
+++ lib/matplotlib/backends/backend_ps.py
@@ -72,7 +72,7 @@ class PsBackendHelper(object):
         self._cached["gs_exe"] = gs_exe
         return gs_exe
 
-    
+
     @property
     def gs_version(self):
         """
@@ -97,7 +97,7 @@ class PsBackendHelper(object):
         True if the installed ghostscript supports ps2write device.
         """
         return self.gs_version[0] >= 9
-    
+
 ps_backend_helper = PsBackendHelper()
 
 papersize = {'letter': (8.5,11),
@@ -582,6 +582,11 @@ grestore
 
         # construct the generic marker command:
         ps_cmd = ['/o {', 'gsave', 'newpath', 'translate'] # dont want the translate to be global
+        jint = gc.get_joinstyle()
+        ps_cmd.append('%d setlinejoin' % jint)
+        cint = gc.get_capstyle()
+        ps_cmd.append('%d setlinecap' % cint)
+
         ps_cmd.append(self._convert_path(marker_path, marker_trans,
                                          simplify=False))
 
