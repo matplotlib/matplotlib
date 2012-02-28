b9bde63ea63ff55695ddf4e59e67b11fa92692f7
Damon McDougall <damon.mcdougall@gmail.com>
2012-02-28 14:12:25 -0500

    Added comments to trisurf example.
    This mesh was taken from the triplot demo. I have taken out an input
    point rotation, this is in the interest of keeping the example code to a
    minimum.

--- examples/mplot3d/trisurf3d_demo.py
+++ examples/mplot3d/trisurf3d_demo.py
@@ -5,15 +5,23 @@ import numpy as np
 
 n_angles = 36
 n_radii = 8
-min_radius = 0.25
-radii = np.linspace(min_radius, 0.95, n_radii)
 
+# An array of radii
+# Does not include radius r=0, this is to eliminate duplicate points
+radii = np.linspace(0.125, 1.0, n_radii)
+
+# An array of angles
 angles = np.linspace(0, 2*np.pi, n_angles, endpoint=False)
-angles = np.repeat(angles[...,np.newaxis], n_radii, axis=1)
-angles[:,1::2] += np.pi/n_angles
 
+# Repeat all angles for each radius
+angles = np.repeat(angles[...,np.newaxis], n_radii, axis=1)
+
+# Convert polar (radii, angles) coords to cartesian (x, y) coords
+# (0, 0) is added here. There are no duplicate points in the (x, y) plane
 x = np.append(0, (radii*np.cos(angles)).flatten())
 y = np.append(0, (radii*np.sin(angles)).flatten())
+
+# Pringle surface
 z = np.sin(-x*y)
 
 fig = plt.figure()
