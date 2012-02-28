fa371c3751ba15ea91225153f829feb8969e0b56
Damon McDougall <damon.mcdougall@gmail.com>
2012-02-28 11:22:22 -0500

    Added plot_trisurf to examples.
    This produces an example plot of a 3D surface (in this case, the pringle
        function) on a triangular grid.

---
+++ examples/mplot3d/trisurf3d_demo.py
@@ -0,0 +1,25 @@
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+n_angles = 36
+n_radii = 8
+min_radius = 0.25
+radii = np.linspace(min_radius, 0.95, n_radii)
+
+angles = np.linspace(0, 2*math.pi, n_angles, endpoint=False)
+angles = np.repeat(angles[...,np.newaxis], n_radii, axis=1)
+angles[:,1::2] += math.pi/n_angles
+
+x = np.append(0, (radii*np.cos(angles)).flatten())
+y = np.append(0, (radii*np.sin(angles)).flatten())
+z = np.sin(-x*y)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+ax.plot_trisurf(x, y, z, cmap=cm.jet, linewidth=0.2)
+
+plt.show()
