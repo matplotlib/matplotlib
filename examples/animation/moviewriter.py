33e4bb0a05c897556da4a0c594780c8b80ae79e4
Ryan May <rmay31@gmail.com>
2012-02-29 21:24:45 -0600

    Add example that manually uses a movie writer object.

---
+++ examples/animation/moviewriter.py
@@ -0,0 +1,27 @@
+# This example uses a MovieWriter directly to grab individual frames and
+# write them to a file. This avoids any event loop integration, but has
+# the advantage of working with even the Agg backend. This is not recommended
+# for use in an interactive setting.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as manimation
+
+FFMpegWriter = manimation.writers['ffmpeg']
+writer = FFMpegWriter(fps=15)
+
+fig = plt.figure()
+l, = plt.plot([], [], 'k-o')
+
+plt.xlim(-5, 5)
+plt.ylim(-5, 5)
+
+x0,y0 = 0, 0
+
+with writer.saving(fig, "writer_test.mp4", 100):
+    for i in range(100):
+        x0 += 0.1 * np.random.randn()
+        y0 += 0.1 * np.random.randn()
+        l.set_data(x0, y0)
+        writer.grab_frame()
+
