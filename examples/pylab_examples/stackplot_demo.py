66745486001025932d1c58b7d5bd55be88437925
Damon McDougall <damon.mcdougall@gmail.com>
2012-04-14 10:47:33 -0400

    Updated stackplot example.
    Stackplot example now reflects usage of variadic calling signature.

--- examples/pylab_examples/stackplot_demo.py
+++ examples/pylab_examples/stackplot_demo.py
@@ -5,7 +5,14 @@ fnx = lambda : np.random.randint(5, 50, 10)
 y = np.row_stack((fnx(), fnx(), fnx()))
 x = np.arange(10)
 
+y1, y2, y3 = fnx(), fnx(), fnx()
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.stackplot(x, y)
+plt.show()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.stackplot(x, y1, y2, y3)
 plt.show()
