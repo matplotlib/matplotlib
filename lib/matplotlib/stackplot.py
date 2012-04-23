a66ca21b447a39c6806f21ca73c8d0d4a87de847
Damon McDougall <damon.mcdougall@gmail.com>
2012-04-23 19:52:09 -0400

    Added support for custom colour cycle.
    New keyword argument 'colors' can be passed to allow custom colouring of
    the stacked areas in a stacked area plot.

--- lib/matplotlib/stackplot.py
+++ lib/matplotlib/stackplot.py
@@ -24,7 +24,11 @@ def stackplot(axes, x, *args, **kwargs):
 
           stackplot(x, y)               # where y is MxN
           staclplot(x, y1, y2, y3, y4)  # where y1, y2, y3, y4, are all 1xNm
-    *kwargs* : All keyword arguments are those supported by
+
+    Keyword arguments:
+    *colors* : A list or tuple of colors. These will be cycled through and
+               used to colour the stacked areas.
+               All other keyword arguments are passed to
                :func:`~matplotlib.Axes.fill_between`
 
     Returns
@@ -37,6 +41,10 @@ def stackplot(axes, x, *args, **kwargs):
         y = np.atleast_2d(*args)
     elif len(args) > 1:
         y = np.row_stack(args)
+
+    colors = kwargs.pop('colors', None)
+    if colors is not None:
+        axes.set_color_cycle(colors)
 
     # Assume data passed has not been 'stacked', so stack it here.
     y_stack = np.cumsum(y, axis=0)
