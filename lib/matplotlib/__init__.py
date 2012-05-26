0f81dffc05a98fe63e987c2b865a718c53ded7e4
Matthew Emmett <memmett@unc.edu>
2012-05-25 21:22:50 -0400

    Add docstring to rc_context and accept a dictionary too.

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -912,12 +912,39 @@ def rc_file(fname):
 
 
 class rc_context(object):
-    def __init__(self, fname=None):
+    """
+    Return a context manager for managing rc settings.
+
+    This allows one to do::
+
+    >>> with mpl.rc_context(fname='screen.rc'):
+    >>>     plt.plot(x, a)
+    >>>     with mpl.rc_context(fname='print.rc'):
+    >>>         plt.plot(x, b)
+    >>>     plt.plot(x, c)
+    
+    The 'a' vs 'x' and 'c' vs 'x' plots would have settings from
+    'screen.rc', while the 'b' vs 'x' plot would have settings from
+    'print.rc'.
+
+    A dictionary can also be passed to the context manager::
+
+    >>> with mpl.rc_context(rc={'text.usetex': True}, fname='screen.rc'):
+    >>>     plt.plot(x, a)
+
+    The 'rc' dictionary takes precedence over the settings loaded from
+    'fname'.  Passing a dictionary only is also valid.
+    """
+
+    def __init__(self, rc=None, fname=None):
+        self.rcdict = rc
         self.fname = fname
     def __enter__(self):
         self._rcparams = rcParams.copy()
         if self.fname:
             rc_file(self.fname)
+        if self.rcdict:
+            rcParams.update(self.rcdict)
     def __exit__(self, type, value, tb):
         rcParams.update(self._rcparams)
 
