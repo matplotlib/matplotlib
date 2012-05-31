2d29c3a177991f73cf907af1289c59fa3f2bcd4e
Brett Graham <graham@rowland.harvard.edu>
2012-05-31 19:45:50 -0400

    adding more flexability to subplots axis sharing

--- lib/matplotlib/pyplot.py
+++ lib/matplotlib/pyplot.py
@@ -793,15 +793,27 @@ def subplots(nrows=1, ncols=1, sharex=False, sharey=False, squeeze=True,
       *ncols* : int
         Number of columns of the subplot grid.  Defaults to 1.
 
-      *sharex* : bool
+      *sharex* : string or bool
         If *True*, the X axis will be shared amongst all subplots.  If
         *True* and you have multiple rows, the x tick labels on all but
         the last row of plots will have visible set to *False*
+        If a string must be one of "row", "col", "all", or "none".
+        "all" has the same effect as *True*, "none" has the same effect
+        as *False*.
+        If "row", each subplot row will share a X axis.
+        If "col", each subplot column will share a X axis and the x tick
+        labels on all but the last row will have visible set to *False*.
 
-      *sharey* : bool
+      *sharey* : string or bool
         If *True*, the Y axis will be shared amongst all subplots. If
         *True* and you have multiple columns, the y tick labels on all but
         the first column of plots will have visible set to *False*
+        If a string must be one of "row", "col", "all", or "none".
+        "all" has the same effect as *True*, "none" has the same effect
+        as *False*.
+        If "row", each subplot row will share a Y axis.
+        If "col", each subplot column will share a Y axis and the y tick
+        labels on all but the last row will have visible set to *False*.
 
       *squeeze* : bool
         If *True*, extra dimensions are squeezed out from the
@@ -860,6 +872,24 @@ def subplots(nrows=1, ncols=1, sharex=False, sharey=False, squeeze=True,
         # Four polar axes
         plt.subplots(2, 2, subplot_kw=dict(polar=True))
     """
+    # for backwards compatability
+    if isinstance(sharex, bool):
+        if sharex:
+            sharex = "all"
+        else:
+            sharex = "none"
+    if isinstance(sharey, bool):
+        if sharey:
+            sharey = "all"
+        else:
+            sharey = "none"
+    share_values = ["all", "row", "col", "none"]
+    if sharex not in share_values:
+        raise ValueError("sharex [%s] must be one of %s" % \
+                (sharex, share_values))
+    if sharey not in share_values:
+        raise ValueError("sharey [%s] must be one of %s" % \
+                (sharey, share_values))
 
     if subplot_kw is None:
         subplot_kw = {}
@@ -873,34 +903,52 @@ def subplots(nrows=1, ncols=1, sharex=False, sharey=False, squeeze=True,
 
     # Create first subplot separately, so we can share it if requested
     ax0 = fig.add_subplot(nrows, ncols, 1, **subplot_kw)
-    if sharex:
-        subplot_kw['sharex'] = ax0
-    if sharey:
-        subplot_kw['sharey'] = ax0
+    #if sharex:
+    #    subplot_kw['sharex'] = ax0
+    #if sharey:
+    #    subplot_kw['sharey'] = ax0
     axarr[0] = ax0
+
+    r, c = np.mgrid[:nrows, :ncols]
+    r = r.flatten() * ncols
+    c = c.flatten()
+    lookup = {
+            "none": np.arange(nplots),
+            "all": np.zeros(nplots, dtype=int),
+            "row": r,
+            "col": c,
+            }
+    sxs = lookup[sharex]
+    sys = lookup[sharey]
 
     # Note off-by-one counting because add_subplot uses the MATLAB 1-based
     # convention.
     for i in range(1, nplots):
-        axarr[i] = fig.add_subplot(nrows, ncols, i+1, **subplot_kw)
-
-
+        if sxs[i] == i:
+            subplot_kw['sharex'] = None
+        else:
+            subplot_kw['sharex'] = axarr[sxs[i]]
+        if sys[i] == i:
+            subplot_kw['sharey'] = None
+        else:
+            subplot_kw['sharey'] = axarr[sys[i]]
+        axarr[i] = fig.add_subplot(nrows, ncols, i + 1, **subplot_kw)
 
     # returned axis array will be always 2-d, even if nrows=ncols=1
     axarr = axarr.reshape(nrows, ncols)
 
-
     # turn off redundant tick labeling
-    if sharex and nrows>1:
+    if sharex in ["col", "all"] and nrows > 1:
+    #if sharex and nrows>1:
         # turn off all but the bottom row
-        for ax in axarr[:-1,:].flat:
+        for ax in axarr[:-1, :].flat:
             for label in ax.get_xticklabels():
                 label.set_visible(False)
 
-
-    if sharey and ncols>1:
+    if sharey in ["row", "all"] and ncols > 1:
+    #if sharey and ncols>1:
         # turn off all but the first column
-        for ax in axarr[:,1:].flat:
+        for ax in axarr[:, 1:].flat:
             for label in ax.get_yticklabels():
                 label.set_visible(False)
 
