c6fac36ed3c5f14c2a9863d9c68f6a8fa068fcbc
endolith <endolith@gmail.com>
2012-06-03 01:28:50 -0400

    fix typos

--- examples/pylab_examples/polar_scatter.py
+++ examples/pylab_examples/polar_scatter.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # a polar scatter plot; size increases radially in this example and
 # color increases with angle (just to verify the symbols are being
-# scattered correctlu).  In a real example, this would be wasting
-# dimensionlaity of the plot
+# scattered correctly).  In a real example, this would be wasting
+# dimensionality of the plot
 from pylab import *
 
 N = 150
