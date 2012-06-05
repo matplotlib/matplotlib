d14700892d94c7bf6f455991a2ac13a182aec136
endolith <endolith@gmail.com>
2012-06-05 11:55:29 -0400

    coolwarm is specifically designed for diverging data on shaded 3D surfaces

--- examples/mplot3d/subplot3d_demo.py
+++ examples/mplot3d/subplot3d_demo.py
@@ -17,7 +17,7 @@ Y = np.arange(-5, 5, 0.25)
 X, Y = np.meshgrid(X, Y)
 R = np.sqrt(X**2 + Y**2)
 Z = np.sin(R)
-surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet,
+surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
         linewidth=0, antialiased=False)
 ax.set_zlim3d(-1.01, 1.01)
 
