0f8d32bfee4ec9c77bf29fde792182560c72c08d
John Hunter <jdh2358@gmail.com>
2012-06-30 14:02:02 -0500

    set version string for 1.1.1 release

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -99,7 +99,7 @@ to MATLAB&reg;, a registered trademark of The MathWorks, Inc.
 """
 from __future__ import generators
 
-__version__  = '1.1.1rc'
+__version__  = '1.1.1'
 __version__numpy__ = '1.4' # minimum required numpy version
 
 import os, re, shutil, subprocess, sys, warnings
