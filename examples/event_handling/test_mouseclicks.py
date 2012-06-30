1b4408a494b20b11a7260181596f8fadde7dad6a
cgohlke <cgohlke@uci.edu>
2012-06-29 18:59:28 -0700

    Fix test_mouseclicks.py on Python 3

--- examples/event_handling/test_mouseclicks.py
+++ examples/event_handling/test_mouseclicks.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
 import matplotlib
 #matplotlib.use("WxAgg")
 #matplotlib.use("TkAgg")
@@ -9,23 +11,25 @@ import matplotlib
 #matplotlib.use("MacOSX")
 import matplotlib.pyplot as plt
 
-#print "***** TESTING WITH BACKEND: %s"%matplotlib.get_backend() + " *****"
+#print("***** TESTING WITH BACKEND: %s"%matplotlib.get_backend() + " *****")
+
 
 def OnClick(event):
-   if event.dblclick:
-      print "DBLCLICK",event
-   else:
-      print "DOWN    ",event
+    if event.dblclick:
+        print("DBLCLICK", event)
+    else:
+        print("DOWN    ", event)
+
 
 def OnRelease(event):
-    print   "UP      ",event
+    print("UP      ", event)
 
 
 fig = plt.gcf()
-cid_up = fig.canvas.mpl_connect('button_press_event',OnClick)
-cid_down = fig.canvas.mpl_connect('button_release_event',OnRelease)
+cid_up = fig.canvas.mpl_connect('button_press_event', OnClick)
+cid_down = fig.canvas.mpl_connect('button_release_event', OnRelease)
 
-plt.gca().text(0.5,0.5,"Click on the canvas to test mouse events.",ha="center",va="center")
+plt.gca().text(0.5, 0.5, "Click on the canvas to test mouse events.",
+               ha="center", va="center")
 
 plt.show()
-
