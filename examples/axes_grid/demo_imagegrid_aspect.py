d9da58e1828402de1609071ee360a53121f5d1a0
Jae-Joon Lee <lee.j.joon@gmail.com>
2012-07-20 21:32:59 +0900

    axes_grid1: ImageGrid respect the aspect ratio of axes. This addresses #955

---
+++ examples/axes_grid/demo_imagegrid_aspect.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.axes_grid1 import ImageGrid
+fig = plt.figure(1)
+
+grid1 = ImageGrid(fig, 121, (2,2), axes_pad=0.1,
+                 aspect=True, share_all=True)
+
+for i in [0, 1]:
+    grid1[i].set_aspect(2)
+
+
+grid2 = ImageGrid(fig, 122, (2,2), axes_pad=0.1,
+                 aspect=True, share_all=True)
+
+
+for i in [1, 3]:
+    grid2[i].set_aspect(2)
+
+plt.show()
