b092bfdd1124f65f6c337b3f9cf24b5fe2c9ca75
pelson <pelson.pub@gmail.com>
2012-08-19 16:49:44 +0100

    Added %s support for labels.

--- lib/matplotlib/tests/test_legend.py
+++ lib/matplotlib/tests/test_legend.py
@@ -1,9 +1,8 @@
 import numpy as np
 
-from matplotlib.testing.decorators import image_comparison, knownfailureif
+from matplotlib.testing.decorators import image_comparison
 import matplotlib.pyplot as plt
-from nose.tools import assert_raises
-from numpy.testing import assert_array_equal
+
 
 @image_comparison(baseline_images=['legend_auto1'], tol=1.5e-3, remove_text=True)
 def test_legend_auto1():
@@ -15,6 +14,7 @@ def test_legend_auto1():
     ax.plot(x, x-50, 'o', label='y=-1')
     ax.legend(loc=0)
 
+
 @image_comparison(baseline_images=['legend_auto2'], remove_text=True)
 def test_legend_auto2():
     'Test automatic legend placement'
@@ -24,3 +24,14 @@ def test_legend_auto2():
     b1 = ax.bar(x, x, color='m')
     b2 = ax.bar(x, x[::-1], color='g')
     ax.legend([b1[0], b2[0]], ['up', 'down'], loc=0)
+    
+    
+@image_comparison(baseline_images=['legend_various_labels'], remove_text=True)
+def test_various_labels():
+    # tests all sorts of label types
+    fig = plt.figure()
+    ax = fig.add_subplot(121)
+    x = np.arange(100)
+    ax.plot(range(4), 'o', label=1)
+    ax.plot(range(4, 1, -1), 'o', label='__nolegend__')
+    ax.legend(numpoints=1)
