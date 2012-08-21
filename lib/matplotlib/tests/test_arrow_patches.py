24e59e0ffb887f0a6fd75a199698e4a7076aed48
Jae-Joon Lee <lee.j.joon@gmail.com>
2012-08-22 03:45:35 +0900

    fix drawing error of fancyarrow of simple style when two points are too close. This addresses #566. Initial patch submitted by Rolf Barinka.

---
+++ lib/matplotlib/tests/test_arrow_patches.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from matplotlib.testing.decorators import image_comparison
+
+def draw_arrow(ax, t, r):
+    ax.annotate('', xy=(0.5, 0.5+r), xytext=(0.5, 0.5), size=30,
+                arrowprops=dict(arrowstyle=t,
+                                fc="b", ec='k'))
+
+@image_comparison(baseline_images=['fancyarrow_test_image'])
+def test_fancyarrow():
+    r = [0.4, 0.3, 0.2, 0.1]
+    t = ["fancy", "simple"]
+
+    fig, axes = plt.subplots(len(t), len(r), squeeze=False,
+                             subplot_kw=dict(aspect=True),
+                         figsize=(8, 4.5))
+
+    for i_r, r1 in enumerate(r):
+        for i_t, t1 in enumerate(t):
+            ax = axes[i_t, i_r]
+            draw_arrow(ax, t1, r1)
+            ax.tick_params(labelleft=False, labelbottom=False)
+
+
+if __name__=='__main__':
+    import nose
+    nose.runmodule(argv=['-s','--with-doctest'], exit=False)
+
