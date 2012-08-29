77e245a5b9dc6da86a6529f2e4db59f9d23a60da
Jae-Joon Lee <lee.j.joon@gmail.com>
2012-08-29 22:34:20 +0900

    implement tight_layout.get_subplotspec_list and uses this to check if all the axes are supported by tight_layout

--- lib/matplotlib/figure.py
+++ lib/matplotlib/figure.py
@@ -1422,17 +1422,17 @@ class Figure(Artist):
             labels) will fit into. Default is (0, 0, 1, 1).
         """
 
-        from tight_layout import get_renderer, get_tight_layout_figure
+        from tight_layout import get_renderer, get_tight_layout_figure, \
+             get_subplotspec_list
 
-        subplot_axes = [ax for ax in self.axes if isinstance(ax, SubplotBase)]
-        if len(subplot_axes) < len(self.axes):
+        if None in get_subplotspec_list(self.axes):
             warnings.warn("tight_layout can only process Axes that descend "
                           "from SubplotBase; results might be incorrect.")
 
         if renderer is None:
             renderer = get_renderer(self)
 
-        kwargs = get_tight_layout_figure(self, subplot_axes, renderer,
+        kwargs = get_tight_layout_figure(self, self.axes, renderer,
                                          pad=pad, h_pad=h_pad, w_pad=w_pad,
                                          rect=rect)
 
