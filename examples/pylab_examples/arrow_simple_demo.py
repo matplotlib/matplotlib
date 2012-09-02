8a5dc5547256a3b350f83b6746feb46d01308cc9
Damon McDougall <damon.mcdougall@gmail.com>
2012-09-02 09:43:15 +0100

    Remove interaction directly with the figure

--- examples/pylab_examples/arrow_simple_demo.py
+++ examples/pylab_examples/arrow_simple_demo.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
+ax = plt.axes()
 ax.arrow(0, 0, 0.5, 0.5, head_width=0.05, head_length=0.1, fc='k', ec='k')
 plt.show()
