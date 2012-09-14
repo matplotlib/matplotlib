e837b402891f2b3cc2948c2ca874cac4e3db45eb
Michael Droettboom <mdboom@gmail.com>
2012-09-14 11:24:09 -0400

    Build the tarball from Python -- this should provide a lot more flexibility later.

--- tests.py
+++ tests.py
@@ -5,6 +5,8 @@
 # See http://somethingaboutorange.com/mrl/projects/nose/1.0.0/usage.html
 # for options.
 
+import os
+
 import matplotlib
 matplotlib.use('agg')
 
@@ -12,9 +14,40 @@ import nose
 from matplotlib.testing.noseclasses import KnownFailure
 from matplotlib import default_test_modules
 
+
 def run():
-    nose.main(addplugins=[KnownFailure()],
-              defaultTest=default_test_modules)
+    try:
+        nose.main(addplugins=[KnownFailure()],
+                  defaultTest=default_test_modules)
+    except SystemExit as e:
+        # When running in TRAVIS, we want to write the failed images
+        # out to the log.
+        if e.code == 0 or 'TRAVIS' not in os.environ:
+            raise
+
+        import base64
+        import io
+        import tarfile
+        output = io.BytesIO()
+
+        with tarfile.open(fileobj=output, mode="w|bz2") as tar:
+            for root, dirs, files in os.walk('.'):
+                for file in files:
+                    if os.path.splitext(file)[0].endswith('-failed-diff'):
+                        path = os.path.join(root, file)
+                        tar.add(path)
+                        path = path.replace('-failed-diff', '')
+                        tar.add(path)
+
+        print(
+            "\nThe following is a base64-encoded tar.bz2 file containing the\n"
+            "failed images from the test run.  Use the\n"
+            "get_travis_results.py script to download it from Travis and\n"
+            "extract it.\n")
+
+        print(base64.b64encode(output.getvalue()))
+
+        raise
 
 if __name__ == '__main__':
     run()
