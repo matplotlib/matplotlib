656c88f3e5467f087125efb2cbff65fa0332092e
Michael Droettboom <mdboom@gmail.com>
2012-09-19 13:33:32 -0400

    Update version to 1.2.0rc2

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -99,7 +99,7 @@ to MATLAB&reg;, a registered trademark of The MathWorks, Inc.
 """
 from __future__ import print_function
 
-__version__  = '1.3.x'
+__version__  = '1.2.0rc2'
 __version__numpy__ = '1.4' # minimum required numpy version
 
 import os, re, shutil, subprocess, sys, warnings
