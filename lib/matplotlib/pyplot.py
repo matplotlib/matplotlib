196454efcde4172d8fd76ba3ee4f824a814ec7bb
montefra <franz.bergesund@gmail.com>
2012-09-21 12:06:15 +0200

    pyplot/readfile. *gridon* default sets to rcParams[axis.grid] and deprecated

--- lib/matplotlib/pyplot.py
+++ lib/matplotlib/pyplot.py
@@ -2092,7 +2092,7 @@ def polar(*args, **kwargs):
 
 def plotfile(fname, cols=(0,), plotfuncs=None,
              comments='#', skiprows=0, checkrows=5, delimiter=',', names=None,
-             subplots=True, newfig=True, 
+             subplots=True, newfig=True, gridon=rcParams['axis.grid'],
              **kwargs):
     """
     Plot the data in in a file.
@@ -2129,6 +2129,9 @@ def plotfile(fname, cols=(0,), plotfuncs=None,
     If *newfig* is *True*, the plot always will be made in a new figure;
     if *False*, it will be made in the current figure if one exists,
     else in a new figure.
+
+    *gridon*: turn the axes grids on or off. Default from rcParams.
+    This keyword is deprecated and will be removed in future versions.
 
     kwargs are passed on to plotting functions.
 
@@ -2188,6 +2191,8 @@ def plotfile(fname, cols=(0,), plotfuncs=None,
                     ax = fig.add_subplot(N-1,1,i, sharex=ax1)
             elif i==1:
                 ax = fig.add_subplot(1,1,1)
+
+	    ax.grid(gridon)
 
             yname, y = getname_val(cols[i])
             ynamelist.append(yname)
