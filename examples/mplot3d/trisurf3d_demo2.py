bd6f52a0eb794e7504ce04d61f47511ee34158ff
Damon McDougall <damon.mcdougall@gmail.com>
2012-09-22 12:31:24 +0100

    Add comments to the example for clarity

--- examples/mplot3d/trisurf3d_demo2.py
+++ examples/mplot3d/trisurf3d_demo2.py
@@ -3,17 +3,25 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.tri as mtri
 
+# u, v are parameterisation variables
 u = (np.linspace(0, 2.0 * np.pi, endpoint=True, num=50) * np.ones((10, 1))).flatten()
 v = np.repeat(np.linspace(-0.5, 0.5, endpoint=True, num=10), repeats=50).flatten()
 
+# This is the Mobius mapping, taking a u, v pair and returning an x, y, z
+# triple
 x = (1 + 0.5 * v * np.cos(u / 2.0)) * np.cos(u)
 y = (1 + 0.5 * v * np.cos(u / 2.0)) * np.sin(u)
 z = 0.5 * v * np.sin(u / 2.0)
 
+# Triangulate parameter space to determine the triangles
 tri = mtri.Triangulation(u, v)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1, projection='3d')
+
+# The triangles in parameter space determnine which x, y, z points are
+# connected by an edge
 ax.plot_trisurf(x, y, z, triangles=tri.triangles)
+
 ax.set_zlim(-1, 1)
 plt.show()
