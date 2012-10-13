59cb40362cedf2dca6ca672626c9eae3f3fa2296
Damon McDougall <damon.mcdougall@gmail.com>
2012-10-14 00:07:13 +0100

    Make setupegg symlink correct dateutil library
    Setupegg assumes the install tree is the same as the source tree. This
    is not the case, since a different version of the dateutil library is
    used depending on whether python version 2 or 3 is used. This fix
    symlinks the correct version at run-time.

--- setupegg.py
+++ setupegg.py
@@ -2,7 +2,21 @@
 Poor man's setuptools script...
 """
 
+import os
+import sys
 from setuptools import setup
+
+# Setupegg assumes the install tree and source tree are exactly the same. Since
+# this is not the case, symlink the correct dateutil dir depending on which
+# version of python is used
+os.chdir('lib')
+if not os.path.isdir('dateutil'):
+    if sys.version_info[0] >= 3:
+        os.symlink('dateutil_py3', 'dateutil')
+    else:
+        os.symlink('dateutil_py2', 'dateutil')
+os.chdir('..')
+
 execfile('setup.py',
          {'additional_params' :
           {'namespace_packages' : ['mpl_toolkits'],
