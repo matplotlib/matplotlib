1a9fc0868bcdf8283d700747533d2b265a2413e1
Michael Droettboom <mdboom@gmail.com>
2012-10-15 12:21:16 -0400

    Create the symlink without using chdir.

--- setupegg.py
+++ setupegg.py
@@ -9,13 +9,11 @@ from setuptools import setup
 # Setupegg assumes the install tree and source tree are exactly the same. Since
 # this is not the case, symlink the correct dateutil dir depending on which
 # version of python is used
-os.chdir('lib')
-if not os.path.isdir('dateutil'):
+if not os.path.isdir('lib/dateutil'):
     if sys.version_info[0] >= 3:
-        os.symlink('dateutil_py3', 'dateutil')
+        os.symlink('dateutil_py3', 'lib/dateutil')
     else:
-        os.symlink('dateutil_py2', 'dateutil')
-os.chdir('..')
+        os.symlink('dateutil_py2', 'lib/dateutil')
 
 execfile('setup.py',
          {'additional_params' :
