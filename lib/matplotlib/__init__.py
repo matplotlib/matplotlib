f74c9b3815c47f60a83fcb182fa62a6ac61df56c
Jens H. Nielsen <jenshnielsen@gmail.com>
2012-10-25 00:02:02 +0200

    Add the bbox tests to default tests

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -1070,6 +1070,7 @@ default_test_modules = [
     'matplotlib.tests.test_backend_svg',
     'matplotlib.tests.test_backend_pgf',
     'matplotlib.tests.test_basic',
+    'matplotlib.tests.test_bbox_tight',
     'matplotlib.tests.test_cbook',
     'matplotlib.tests.test_colorbar',
     'matplotlib.tests.test_colors',
