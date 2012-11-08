e12a16e806442437c592684602f1360ebb657967
Paul Ivanov <pivanov314@gmail.com>
2012-11-08 10:20:51 -0800

    revert to OO interface (no pyplot) in OO example

--- examples/api/agg_oo.py
+++ examples/api/agg_oo.py
@@ -6,10 +6,12 @@ A pure OO (look Ma, no pylab!) example using the agg backend
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
-fig, ax = plt.subplots()
+fig = Figure()
+canvas = FigureCanvas(fig)
+ax = fig.add_subplot(111)
 ax.plot([1,2,3])
 ax.set_title('hi mom')
 ax.grid(True)
 ax.set_xlabel('time')
 ax.set_ylabel('volts')
-fig.canvas.print_figure('test')
+canvas.print_figure('test')
