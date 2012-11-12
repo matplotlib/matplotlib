28b93f7f2213b6277c5c0826d8bb6b8c82897723
Pierre Villeneuve <pierre.villeneuve@gmail.com>
2012-11-12 08:15:29 -0800

    Continue propagating resize event up the chain.
    added this line to allow event to continue propagating up to base class.
    My image viewer application was not responding to resize events until I
    put this line in place.

--- lib/matplotlib/backends/backend_qt4.py
+++ lib/matplotlib/backends/backend_qt4.py
@@ -301,6 +301,7 @@ class FigureCanvasQT( QtGui.QWidget, FigureCanvasBase ):
         self.draw()
         self.update()
         QtGui.QWidget.resizeEvent(self, event)
+        FigureCanvasBase.resize_event(self)
 
     def sizeHint( self ):
         w, h = self.get_width_height()
