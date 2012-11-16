50757d2d6509245213426f7712e9c308623b3efb
Paul Ivanov <pivanov314@gmail.com>
2012-11-15 19:06:18 -0800

    DOC: make example cursor show up in the docs

--- examples/widgets/cursor.py
+++ examples/widgets/cursor.py
@@ -16,4 +16,9 @@ ax.set_ylim(-2, 2)
 # set useblit = True on gtkagg for enhanced performance
 cursor = Cursor(ax, useblit=True, color='red', linewidth=2 )
 
+# The next two lines are here just to "simulate" for what the cursor will look
+# like for the gallery, because the documentation is generated without a GUI
+ax.axvline(.3, 0, 1, color='red', linewidth=2)
+ax.axhline(.14, 0, 1, color='red', linewidth=2)
+
 plt.show()
