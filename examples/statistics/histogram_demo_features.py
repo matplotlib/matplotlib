79b622f5f98d17013d9fd91f56c278352f9ba727
Tony S Yu <tsyu80@gmail.com>
2012-12-21 23:13:56 -0800

    Consolidate histogram examples

--- examples/statistics/histogram_demo_features.py
+++ examples/statistics/histogram_demo_features.py
@@ -26,5 +26,8 @@ n, bins, patches = plt.hist(x, num_bins, normed=1, facecolor='green', alpha=0.5)
 # add a 'best fit' line
 y = mlab.normpdf(bins, mu, sigma)
 plt.plot(bins, y, 'r--')
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title(r'Histogram of IQ: $\mu=100$, $\sigma=15$')
 
 plt.show()
