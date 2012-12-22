4f9cbc15b4c5f1d6f26706f0d8411bbc28959677
Tony S Yu <tsyu80@gmail.com>
2012-12-22 10:28:51 -0800

    Cleanup and move vertical_ticklabels demo

--- examples/pylab_examples/vertical_ticklabels.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from pylab import *
-
-plot([1,2,3,4], [1,4,9,16])
-locs, labels = xticks([1,2,3,4], ['Frogs', 'Hogs', 'Bogs', 'Slogs'])
-setp(labels, 'rotation', 'vertical')
-show()
