0d6f19cf51b5aea6fcac3c93a2032313b8d0cf89
Tony S Yu <tsyu80@gmail.com>
2012-12-22 17:05:28 -0800

    Cleanup and move polar_bar demo

---
+++ examples/pie_and_polar_charts/polar_bar_demo.py
@@ -0,0 +1,21 @@
+"""
+Demo of bar plot on a polar axis.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+N = 20
+theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False)
+radii = 10 * np.random.rand(N)
+width = np.pi / 4 * np.random.rand(N)
+
+ax = plt.subplot(111, polar=True)
+bars = ax.bar(theta, radii, width=width, bottom=0.0)
+
+# Use custom colors and opacity
+for r, bar in zip(radii, bars):
+    bar.set_facecolor(plt.cm.jet(r / 10.))
+    bar.set_alpha(0.5)
+
+plt.show()
