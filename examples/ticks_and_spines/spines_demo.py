d9ff941dafde9dc2e0e7cfff56c903e1786d8f79
Tony S Yu <tsyu80@gmail.com>
2012-12-23 19:56:25 -0800

    Tweak subplot layout to prevent label clipping

--- examples/ticks_and_spines/spines_demo.py
+++ examples/ticks_and_spines/spines_demo.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 x = np.linspace(0, 2 * np.pi, 100)
 y = 2 * np.sin(x)
 
-fig, (ax0, ax1) = plt.subplots(ncols=2)
+fig, (ax0, ax1) = plt.subplots(nrows=2)
 
 ax0.plot(x, y)
 ax0.set_title('normal spines')
@@ -26,4 +26,6 @@ ax1.spines['top'].set_visible(False)
 ax1.yaxis.set_ticks_position('left')
 ax1.xaxis.set_ticks_position('bottom')
 
+# Tweak spacing between subplots to prevent labels from overlapping
+plt.subplots_adjust(hspace=0.5)
 plt.show()
