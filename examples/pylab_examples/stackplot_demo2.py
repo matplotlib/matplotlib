6975bcbb431e8e4276832f33de044b4c931822b1
Damon McDougall <damon.mcdougall@gmail.com>
2013-01-16 15:58:44 -0600

    Add stackplot baseline demo

---
+++ examples/pylab_examples/stackplot_demo2.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(0)
+def layers(n, m):
+    def bump(a):
+        x = 1 / (.1 + np.random.random())
+        y = 2 * np.random.random() - .5
+        z = 10 / (.1 + np.random.random())
+        for i in range(m):
+            w = (i / float(m) - y) * z
+            a[i] += x * np.exp(-w * w)
+    a = np.zeros((m, n))
+    for i in range(n):
+        for j in range(5):
+            bump(a[:, i])
+    return a
+
+d=layers(3, 100)
+
+plt.subplots()
+plt.stackplot(range(100), d.T, baseline='wiggle')
+plt.show()
