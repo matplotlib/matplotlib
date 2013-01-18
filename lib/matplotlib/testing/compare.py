45be3cf170cacefcedde64753fbd44beb43598b5
Matt Giuca <mgiuca@google.com>
2013-01-18 14:41:11 +1100

    compare: PEP-8 formatting.

--- lib/matplotlib/testing/compare.py
+++ lib/matplotlib/testing/compare.py
@@ -300,8 +300,8 @@ def compare_images( expected, actual, tol, in_decorator=False ):
    # open the image files and remove the alpha channel (if it exists)
    expectedImage = _png.read_png_int( expected )
    actualImage = _png.read_png_int( actual )
-   expectedImage = expectedImage[:,:,:3]
-   actualImage = actualImage[:,:,:3]
+   expectedImage = expectedImage[:, :, :3]
+   actualImage = actualImage[:, :, :3]
 
    actualImage, expectedImage = crop_to_same(actual, actualImage, expected, expectedImage)
 
