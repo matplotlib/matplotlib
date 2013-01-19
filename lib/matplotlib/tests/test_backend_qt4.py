7a929cab359d2a7da513fc8a166b959ac4030c2d
Thomas A Caswell <tcaswell@gmail.com>
2013-01-19 15:04:21 -0600

    test for issue #1683

--- lib/matplotlib/tests/test_backend_qt4.py
+++ lib/matplotlib/tests/test_backend_qt4.py
@@ -1,10 +1,25 @@
 from matplotlib import rcParams
 from matplotlib import pyplot as plt
 from matplotlib.testing.decorators import cleanup
+from matplotlib._pylab_helpers import Gcf
+import copy
+
 
 @cleanup
 def test_fig_close():
-    rcParams['backend'] = 'qt4agg'
+    # force switch to the Qt4 backend
+    plt.switch_backend('Qt4Agg')
 
+    #save the state of Gcf.figs
+    init_figs = copy.copy(Gcf.figs)
+
+    # make a figure using pyplot interface
     fig = plt.figure()
-    fig.close()
+
+    # simulate user clicking the close button by reaching in
+    # and calling close on the underlying Qt object
+    fig.canvas.manager.window.close()
+
+    # assert that we have removed the reference to the FigureManager
+    # that got added by plt.figure()
+    assert(init_figs == Gcf.figs)
