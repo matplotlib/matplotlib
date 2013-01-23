def3327ee7d4da22a39b598b8c6b460b855d587e
Thomas A Caswell <tcaswell@gmail.com>
2013-01-23 15:21:45 -0600

    added knownfailureif to test based on if qt4_compat could be imported, as this should hit either PySide or PyQt4, depending on which is installed.  (I have faith that the setup code that will make sure that if only one of them is installed, it defaults to using that one)

--- lib/matplotlib/tests/test_backend_qt4.py
+++ lib/matplotlib/tests/test_backend_qt4.py
@@ -1,10 +1,18 @@
 from matplotlib import pyplot as plt
 from matplotlib.testing.decorators import cleanup
+from matplotlib.testing.decorators import knownfailureif
 from matplotlib._pylab_helpers import Gcf
 import copy
 
+try:
+    import matplotlib.backends.qt4_compat
+    HAS_QT = True
+except ImportError:
+    HAS_QT = False
+
 
 @cleanup
+@knownfailureif(not HAS_QT)
 def test_fig_close():
     # force switch to the Qt4 backend
     plt.switch_backend('Qt4Agg')
