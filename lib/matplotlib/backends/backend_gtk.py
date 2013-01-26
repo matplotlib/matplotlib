968d23f5f4caf0076e171bdac20c26b2cba2563f
Martin Spacek <git@mspacek.mm.st>
2013-01-26 14:11:02 -0800

    Split line

--- lib/matplotlib/backends/backend_gtk.py
+++ lib/matplotlib/backends/backend_gtk.py
@@ -1018,7 +1018,8 @@ class NavigationToolbar(gtk.Toolbar):
 
 
 class FileChooserDialog(gtk.FileChooserDialog):
-    """GTK+ 2.4 file selector which presents the user with a menu of supported image formats
+    """GTK+ 2.4 file selector which presents the user with a menu
+    of supported image formats
     """
     def __init__ (self,
                   title   = 'Save file',
