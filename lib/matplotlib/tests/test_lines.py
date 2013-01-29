e49f7dcdac9eaa8c125ad636f24cd96e4103b0ea
Damon McDougall <damon.mcdougall@gmail.com>
2013-01-29 17:55:29 -0600

    Add image test for setting line collection dashes

--- lib/matplotlib/tests/test_lines.py
+++ lib/matplotlib/tests/test_lines.py
@@ -6,7 +6,7 @@ from nose.tools import assert_true
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from matplotlib.testing.decorators import cleanup
+from matplotlib.testing.decorators import cleanup, image_comparison
 
 @cleanup
 def test_set_line_coll_dash():
@@ -19,3 +19,12 @@ def test_set_line_coll_dash():
     cs = ax.contour(np.random.randn(20, 30), linestyles=[(0, (3, 3))])
 
     assert True
+
+
+@image_comparison(baseline_images=['line_collection_dashes'], remove_text=True)
+def test_set_line_coll_dash_image():
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    np.random.seed(0)
+    cs = ax.contour(np.random.randn(20, 30), linestyles=[(0, (3, 3))])
