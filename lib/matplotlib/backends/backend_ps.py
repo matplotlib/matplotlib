matplotlib/matplotlib

commit cc617006f7f0a18396cecf4a9f1e222f1ee5204e

Change behavior of `font.*` rcParams so they take effect only on to-be-created text objects

lib/matplotlib/font_manager.py
@@ -808,11 +808,10 @@ class FontProperties(object):
         'fantasy', or 'monospace', or a real font name.
         """
         if family is None:
-            self._family = None
-        else:
-            if is_string_like(family):
-                family = [family]
-            self._family = family
+            family = rcParams['font.family']
+        if is_string_like(family):
+            family = [family]
+        self._family = family
     set_name = set_family
 
     def set_style(self, style):
@@ -820,6 +819,8 @@ class FontProperties(object):
         Set the font style.  Values are: 'normal', 'italic' or
         'oblique'.
         """
+        if style is None:
+            style = rcParams['font.style']
         if style not in ('normal', 'italic', 'oblique', None):
             raise ValueError("style must be normal, italic or oblique")
         self._slant = style
@@ -829,6 +830,8 @@ class FontProperties(object):
         """
         Set the font variant.  Values are: 'normal' or 'small-caps'.
         """
+        if variant is None:
+            variant = rcParams['font.variant']
         if variant not in ('normal', 'small-caps', None):
             raise ValueError("variant must be normal or small-caps")
         self._variant = variant
@@ -840,14 +843,15 @@ class FontProperties(object):
         'regular', 'book', 'medium', 'roman', 'semibold', 'demibold',
         'demi', 'bold', 'heavy', 'extra bold', 'black'
         """
-        if weight is not None:
-            try:
-                weight = int(weight)
-                if weight < 0 or weight > 1000:
-                    raise ValueError()
-            except ValueError:
-                if weight not in weight_dict:
-                    raise ValueError("weight is invalid")
+        if weight is None:
+            weight = rcParams['font.weight']
+        try:
+            weight = int(weight)
+            if weight < 0 or weight > 1000:
+                raise ValueError()
+        except ValueError:
+            if weight not in weight_dict:
+                raise ValueError("weight is invalid")
         self._weight = weight
 
     def set_stretch(self, stretch):
@@ -857,14 +861,15 @@ class FontProperties(object):
         'semi-expanded', 'expanded', 'extra-expanded' or
         'ultra-expanded', or a numeric value in the range 0-1000.
         """
-        if stretch is not None:
-            try:
-                stretch = int(stretch)
-                if stretch < 0 or stretch > 1000:
-                    raise ValueError()
-            except ValueError:
-                if stretch not in stretch_dict:
-                    raise ValueError("stretch is invalid")
+        if stretch is None:
+            stretch = rcParams['font.weight']
+        try:
+            stretch = int(stretch)
+            if stretch < 0 or stretch > 1000:
+                raise ValueError()
+        except ValueError:
+            if stretch not in stretch_dict:
+                raise ValueError("stretch is invalid")
         self._stretch = stretch
 
     def set_size(self, size):
@@ -873,12 +878,13 @@ class FontProperties(object):
         'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large'
         or an absolute font size, e.g., 12.
         """
-        if size is not None:
-            try:
-                size = float(size)
-            except ValueError:
-                if size is not None and size not in font_scalings:
-                    raise ValueError("size is invalid")
+        if size is None:
+            size = rcParams['font.size']
+        try:
+            size = float(size)
+        except ValueError:
+            if size is not None and size not in font_scalings:
+                raise ValueError("size is invalid")
         self._size = size
 
     def set_file(self, file):

lib/matplotlib/rcsetup.py
@@ -262,7 +262,7 @@ def validate_colorlist(s):
 
 def validate_stringlist(s):
     'return a list'
-    if type(s) is str:
+    if type(s) in (str, unicode):
         return [v.strip() for v in s.split(',')]
     else:
         assert type(s) in [list, tuple]
@@ -513,7 +513,7 @@ defaultParams = {
 
 
     ## font props
-    'font.family':     ['sans-serif', str],       # used by text object
+    'font.family':     ['sans-serif', validate_stringlist],  # used by text object
     'font.style':      ['normal', str],
     'font.variant':    ['normal', str],
     'font.stretch':    ['normal', str],

commit 2ea3dcb9466630cd758ea5342ffd59558a08b31b

Fixes a number of bugs introduced by cc61700.  texmanager will now still pass when font.family is a list of strings (though there's no sane way for LaTeX to deal with that, but that's nothing new).

lib/matplotlib/backends/backend_ps.py
@@ -659,7 +659,7 @@ grestore
         color = '%1.3f,%1.3f,%1.3f'% gc.get_rgb()[:3]
         fontcmd = {'sans-serif' : r'{\sffamily %s}',
                'monospace'  : r'{\ttfamily %s}'}.get(
-                rcParams['font.family'], r'{\rmfamily %s}')
+                rcParams['font.family'][0], r'{\rmfamily %s}')
         s = fontcmd % s
         tex = r'\color[rgb]{%s} %s' % (color, s)
 

lib/matplotlib/font_manager.py
@@ -805,7 +805,9 @@ class FontProperties(object):
         """
         Change the font family.  May be either an alias (generic name
         is CSS parlance), such as: 'serif', 'sans-serif', 'cursive',
-        'fantasy', or 'monospace', or a real font name.
+        'fantasy', or 'monospace', a real font name or a list of real
+        font names.  Real font names are not supported when
+        `text.usetex` is `True`.
         """
         if family is None:
             family = rcParams['font.family']
@@ -1064,9 +1066,9 @@ class FontManager:
                 options = [x.lower() for x in options]
                 if family2 in options:
                     idx = options.index(family2)
-                    return 0.1 * (float(idx) / len(options))
+                    return 0.1 * ((float(idx) / len(options)) + float(i))
             elif family1 == family2:
-                return 0.0
+                return 0.1 * float(i)
         return 1.0
 
     def score_style(self, style1, style2):

lib/matplotlib/pyplot.py
@@ -278,11 +278,13 @@ def xkcd():
         # This figure will be in regular style
         fig2 = plt.figure()
     """
+    if rcParams['text.usetex']:
+        raise RuntimeError("xkcd mode is not compatible with text.usetex = True")
+
     from matplotlib import patheffects
     context = rc_context()
     try:
-        rcParams['text.usetex'] = False
-        rcParams['font.family'] = 'Humor Sans'
+        rcParams['font.family'] = ['Humor Sans', 'Comic Sans MS']
         rcParams['font.size'] = 14.0
         rcParams['path.sketch'] = (1, 100, 2)
         rcParams['path.effects'] = [

lib/matplotlib/texmanager.py
@@ -166,13 +166,15 @@ Could not rename old TeX cache dir "%s": a suitable configuration
                  'available'))
 
         mkdirs(self.texcache)
-        ff = rcParams['font.family'].lower()
-        if ff in self.font_families:
-            self.font_family = ff
+        ff = rcParams['font.family']
+        if len(ff) == 1 and ff[0].lower() in self.font_families:
+            self.font_family = ff[0].lower()
         else:
-            mpl.verbose.report('The %s font family is not compatible with '
-                               'LaTeX. serif will be used by default.' % ff,
-                               'helpful')
+            mpl.verbose.report(
+                'font.family must be one of (%s) when text.usetex is True. '
+                'serif will be used by default.' %
+                   ', '.join(self.font_families),
+                'helpful')
             self.font_family = 'serif'
 
         fontconfig = [self.font_family]

matplotlibrc.template
@@ -101,7 +101,8 @@ backend      : %(backend)s
 # 'sans-serif' (e.g., Helvetica), 'cursive' (e.g., Zapf-Chancery),
 # 'fantasy' (e.g., Western), and 'monospace' (e.g., Courier).  Each of
 # these font families has a default list of font names in decreasing
-# order of priority associated with them.
+# order of priority associated with them.  When text.usetex is False,
+# font.family may also be one or more concrete font names.
 #
 # The font.style property has three values: normal (or roman), italic
 # or oblique.  The oblique style will be used for italic, if it is not