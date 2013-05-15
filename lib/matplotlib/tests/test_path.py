fbd592ca8aecc36bf8ab50194a4be270cae248e4
Michael Droettboom <mdboom@gmail.com>
2013-05-15 11:39:38 -0400

    Use with statement for assert_raises

--- lib/matplotlib/tests/test_path.py
+++ lib/matplotlib/tests/test_path.py
@@ -2,8 +2,7 @@ from matplotlib.path import Path
 from nose.tools import assert_raises
 
 def test_readonly_path():
-    def readonly():
-        path = Path.unit_circle()
-        path.vertices = path.vertices * 2.0
+    path = Path.unit_circle()
 
-    assert_raises(AttributeError, readonly)
+    with assert_raises(AttributeError):
+        path.vertices = path.vertices * 2.0
