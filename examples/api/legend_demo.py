93a1700372e8c7b7ee1a03b2da15c56b8ad19aef
Tony S Yu <tsyu80@gmail.com>
2013-05-21 00:47:22 -0500

    Tweak aesthetics of plots in screenshots page.
    These changes also cleanup the code a bit, as described by MEP 12.

--- examples/api/legend_demo.py
+++ examples/api/legend_demo.py
@@ -1,41 +1,42 @@
+"""
+Demo of the legend function with a few features.
+
+In addition to the basic legend, this demo shows a few optional features:
+
+    * Custom legend placement.
+    * A keyword argument to a drop-shadow.
+    * Setting the background color.
+    * Setting the font size.
+    * Setting the line width.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-a = np.arange(0,3,.02)
-b = np.arange(0,3,.02)
+
+# Example data
+a = np.arange(0,3, .02)
+b = np.arange(0,3, .02)
 c = np.exp(a)
 d = c[::-1]
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(a,c,'k--',a,d,'k:',a,c+d,'k')
-leg = ax.legend(('Model length', 'Data length', 'Total message length'),
-           'upper center', shadow=True)
-ax.set_ylim([-1,20])
-ax.grid(False)
-ax.set_xlabel('Model complexity --->')
-ax.set_ylabel('Message length --->')
-ax.set_title('Minimum Message Length')
+# Create plots with pre-defined labels.
+# Alternatively, you can pass labels explicitly when calling `legend`.
+fig, ax = plt.subplots()
+ax.plot(a, c, 'k--', label='Model length')
+ax.plot(a, d, 'k:', label='Data length')
+ax.plot(a, c+d, 'k', label='Total message length')
 
-ax.set_yticklabels([])
-ax.set_xticklabels([])
+# Now add the legend with some customizations.
+legend = ax.legend(loc='upper center', shadow=True)
 
-# set some legend properties.  All the code below is optional.  The
-# defaults are usually sensible but if you need more control, this
-# shows you how
+# The frame is matplotlib.patches.Rectangle instance surrounding the legend.
+frame  = legend.get_frame()
+frame.set_facecolor('0.90')
 
-# the matplotlib.patches.Rectangle instance surrounding the legend
-frame  = leg.get_frame()  
-frame.set_facecolor('0.80')    # set the frame face color to light gray
+# Set the fontsize
+for label in legend.get_texts():
+    label.set_fontsize('large')
 
-# matplotlib.text.Text instances
-for t in leg.get_texts():
-    t.set_fontsize('small')    # the legend text fontsize
-
-# matplotlib.lines.Line2D instances
-for l in leg.get_lines():
-    l.set_linewidth(1.5)  # the legend line width
+for label in legend.get_lines():
+    label.set_linewidth(1.5)  # the legend line width
 plt.show()
-
-
-
