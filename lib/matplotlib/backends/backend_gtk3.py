44b8c2612b7ab7206a399522f184548d0302b067
Eric Firing <efiring@hawaii.edu>
2013-05-23 14:09:10 -1000

    backend_gtk3: same fix as for backend_gtk, don't hide file chooser

--- lib/matplotlib/backends/backend_gtk3.py
+++ lib/matplotlib/backends/backend_gtk3.py
@@ -881,7 +881,6 @@ class FileChooserDialog(Gtk.FileChooserDialog):
             filename = self.get_filename()
             break
 
-        self.hide()
         return filename, self.ext
 
 class DialogLineprops:
