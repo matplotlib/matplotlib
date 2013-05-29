33faad1c1e8937995b97d2b973e75a61d77ecb4d
Michael Droettboom <mdboom@gmail.com>
2013-05-29 11:09:50 -0400

    Wait until font caches exist before starting tests

--- tests.py
+++ tests.py
@@ -5,12 +5,22 @@
 # See http://somethingaboutorange.com/mrl/projects/nose/1.0.0/usage.html
 # for options.
 
+import os
+import time
+
 import matplotlib
 matplotlib.use('agg')
 
 import nose
 from matplotlib.testing.noseclasses import KnownFailure
 from matplotlib import default_test_modules
+
+from matplotlib import font_manager
+# Make sure the font caches are created before starting any possibly
+# parallel tests
+if font_manager._fmcache is not None:
+    while not os.path.exists(font_manager._fmcache):
+        time.sleep(0.5)
 
 def run():
     nose.main(addplugins=[KnownFailure()],
