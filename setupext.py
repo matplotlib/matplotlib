07c828d5083a90dcf0c9d62fa6679a81a1d07e2f
Michael Droettboom <mdboom@gmail.com>
2013-05-29 15:39:56 -0400

    Don't include references to system PyCXX when building a tarball

--- setupext.py
+++ setupext.py
@@ -695,7 +695,7 @@ class CXX(SetupPackage):
                 return str(e) + ' Using local copy.'
 
     def add_flags(self, ext):
-        if self.found_external:
+        if self.found_external and not 'sdist' in sys.argv:
             support_dir = os.path.normpath(
                    os.path.join(
                        sys.prefix,
