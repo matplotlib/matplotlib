91ac8850dbb0281870682d5e1d5d5c3af8c11c09
Magnus Nord <magnus.nord@ntnu.no>
2013-06-27 17:00:32 +0200

    Change anchored_artists.AnchoredSizeBar: docstring, vertical size, color
    - Add size_vertical variable, which sets the height of the size bar.
    - Add color argument, which change the color of rectangle bar (not the
      label, need to figure out how to do that).
    - Docstring: listing the possible arguments and adding an example.

--- lib/mpl_toolkits/axes_grid1/anchored_artists.py
+++ lib/mpl_toolkits/axes_grid1/anchored_artists.py
@@ -65,17 +65,48 @@ class AnchoredEllipse(AnchoredOffsetbox):
 
 class AnchoredSizeBar(AnchoredOffsetbox):
     def __init__(self, transform, size, label, loc,
-                 pad=0.1, borderpad=0.1, sep=2, prop=None, frameon=True,
+                 pad=0.1, borderpad=0.1, sep=2, prop=None, 
+                 frameon=True, size_vertical=0, color='black',
                  **kwargs):
         """
         Draw a horizontal bar with the size in data coordinate of the give axes.
         A label will be drawn underneath (center-aligned).
 
-        pad, borderpad in fraction of the legend font size (or prop)
-        sep in points.
+        Parameters:
+        -----------
+        transform : matplotlib transformation object
+        size : int or float
+          horizontal length of the size bar, given in data coordinates
+        label : str
+        loc : int
+        pad : int or float, optional
+          in fraction of the legend font size (or prop)
+        borderpad : int or float, optional
+          in fraction of the legend font size (or prop)
+        sep : int or float, optional
+          in points
+        frameon : bool, optional
+          if True, will draw a box around the horizontal bar and label
+        size_vertical : int or float, optional
+          vertical length of the size bar, given in data coordinates
+        color : str, optional
+          color for the size bar and label
+        
+        Example:
+        --------
+        >>>> import matplotlib.pyplot as plt
+        >>>> import numpy as np
+        >>>> from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
+        >>>> fig, ax = plt.subplots()
+        >>>> ax = imshow(np.random.random((10,10)))
+        >>>> bar = AnchoredSizeBar(ax.transData, 3, '3 units', pad=0.5, loc=4, sep=5, borderpad=0.5, frameon=False, size_vertical=0.5, color='white')
+        >>>> ax.add_artist(bar)
+        >>>> plt.show()
+
         """
+
         self.size_bar = AuxTransformBox(transform)
-        self.size_bar.add_artist(Rectangle((0,0), size, 0, fc="none"))
+        self.size_bar.add_artist(Rectangle((0,0), size, size_vertical, fill=True, facecolor=color, edgecolor=color))
 
         self.txt_label = TextArea(label, minimumdescent=False)
 
