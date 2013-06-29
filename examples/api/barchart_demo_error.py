73b263a963247a8d1313ec660b0ce81efea63ebd
Gustavo Bragança <ga.braganca@gmail.com>
2013-06-29 12:53:50 -0300

    Add a bar plot with errors example

---
+++ examples/api/barchart_demo_error.py
@@ -0,0 +1,15 @@
+"""
+a bar plot with errorbars
+"""
+import matplotlib.pyplot as plt
+
+N = 5
+values = (20, 35, 30, 35, 27)
+errors = (2, 3, 4, 1, 2)
+pos = (0, 1, 2, 3, 4)
+
+plt.bar(left=pos, height=values, width=0.5, color='r', yerr=errors)
+
+plt.title("Bar plot with errors")
+
+plt.show()
