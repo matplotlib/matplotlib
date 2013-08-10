matplotlib/matplotlib

commit f8344a14504f35d97509498b9fc103eb19b9fc77

Fix support for datetime axes in 2d plots.
Fixes the error raisesd when 2d datetime coordinates are passed to
contour/contourf and the errors when 1d or 2d datetime coordinates
are passed to pcolor/pcolormesh.

lib/matplotlib/units.py
@@ -44,6 +44,7 @@ datetime objects::
 """
 from __future__ import print_function
 from matplotlib.cbook import iterable, is_numlike
+import numpy as np
 
 
 class AxisInfo:
@@ -131,6 +132,10 @@ class Registry(dict):
 
         if classx is not None:
             converter = self.get(classx)
+
+        if isinstance(x, np.ndarray) and x.size:
+            converter = self.get_converter(x.ravel()[0])
+            return converter
 
         if converter is None and iterable(x):
             for thisx in x:

lib/matplotlib/axes/_axes.py
@@ -4472,10 +4472,16 @@ class Axes(_AxesBase):
         X, Y, C = self._pcolorargs('pcolor', *args, allmatch=False)
         Ny, Nx = X.shape
 
+        # unit conversion allows e.g. datetime objects as axis values
+        self._process_unit_info(xdata=X, ydata=Y, kwargs=kwargs)
+        X = self.convert_xunits(X)
+        Y = self.convert_yunits(Y)
+
         # convert to MA, if necessary.
         C = ma.asarray(C)
         X = ma.asarray(X)
         Y = ma.asarray(Y)
+
         mask = ma.getmaskarray(X) + ma.getmaskarray(Y)
         xymask = (mask[0:-1, 0:-1] + mask[1:, 1:] +
                   mask[0:-1, 1:] + mask[1:, 0:-1])
@@ -4667,6 +4673,11 @@ class Axes(_AxesBase):
         C = C.ravel()
         X = X.ravel()
         Y = Y.ravel()
+
+        # unit conversion allows e.g. datetime objects as axis values
+        self._process_unit_info(xdata=X, ydata=Y, kwargs=kwargs)
+        X = self.convert_xunits(X)
+        Y = self.convert_yunits(Y)
 
         coords = np.zeros(((Nx * Ny), 2), dtype=float)
         coords[:, 0] = X

lib/matplotlib/dates.py
@@ -203,6 +203,10 @@ def _to_ordinalf(dt):
     return base
 
 
+# a version of _to_ordinalf that can operate on numpy arrays
+_to_ordinalf_np_vectorized = np.vectorize(_to_ordinalf)
+
+
 def _from_ordinalf(x, tz=None):
     """
     Convert Gregorian float of the date, preserving hours, minutes,
@@ -229,6 +233,10 @@ def _from_ordinalf(x, tz=None):
     return dt
 
 
+# a version of _from_ordinalf that can operate on numpy arrays
+_from_ordinalf_np_vectorized = np.vectorize(_from_ordinalf)
+
+
 class strpdate2num:
     """
     Use this class to parse date strings to matplotlib datenums when
@@ -246,6 +254,10 @@ class strpdate2num:
         return date2num(datetime.datetime(*time.strptime(s, self.fmt)[:6]))
 
 
+# a version of dateutil.parser.parse that can operate on nump0y arrays
+_dateutil_parser_parse_np_vectorized = np.vectorize(dateutil.parser.parse)
+
+
 def datestr2num(d):
     """
     Convert a date string to a datenum using
@@ -256,7 +268,10 @@ def datestr2num(d):
         dt = dateutil.parser.parse(d)
         return date2num(dt)
     else:
-        return date2num([dateutil.parser.parse(s) for s in d])
+        d = np.asarray(d)
+        if not d.size:
+            return d
+        return date2num(_dateutil_parser_parse_np_vectorized(d))
 
 
 def date2num(d):
@@ -273,7 +288,10 @@ def date2num(d):
     if not cbook.iterable(d):
         return _to_ordinalf(d)
     else:
-        return np.asarray([_to_ordinalf(val) for val in d])
+        d = np.asarray(d)
+        if not d.size:
+            return d
+        return _to_ordinalf_np_vectorized(d)
 
 
 def julian2num(j):
@@ -310,7 +328,10 @@ def num2date(x, tz=None):
     if not cbook.iterable(x):
         return _from_ordinalf(x, tz)
     else:
-        return [_from_ordinalf(val, tz) for val in x]
+        x = np.asarray(x)
+        if not x.size:
+            return x
+        return _from_ordinalf_np_vectorized(x, tz).tolist()
 
 
 def drange(dstart, dend, delta):
@@ -1292,6 +1313,9 @@ class DateConverter(units.ConversionInterface):
     @staticmethod
     def default_units(x, axis):
         'Return the tzinfo instance of *x* or of its first element, or None'
+        if isinstance(x, np.ndarray):
+            x = x.ravel()
+
         try:
             x = x[0]
         except (TypeError, IndexError):

lib/matplotlib/tests/test_axes.py
@@ -1,5 +1,6 @@
 from nose.tools import assert_equal
 from nose.tools import assert_raises
+import datetime
 
 import numpy as np
 from numpy import ma
@@ -662,6 +663,59 @@ def test_pcolormesh():
 
     ax = fig.add_subplot(133)
     ax.pcolormesh(Qx,Qz,Z, shading="gouraud")
+
+
+@image_comparison(baseline_images=['pcolormesh_datetime_axis'],
+                  extensions=['png'], remove_text=False)
+def test_pcolormesh_datetime_axis():
+    fig = plt.figure()
+    fig.subplots_adjust(hspace=0.4, top=0.98, bottom=.15)
+    base = datetime.datetime(2013, 1, 1)
+    x = np.array([base + datetime.timedelta(days=d) for d in range(21)])
+    y = np.arange(21)
+    z1, z2 = np.meshgrid(np.arange(20), np.arange(20))
+    z = z1 * z2
+    plt.subplot(221)
+    plt.pcolormesh(x[:-1], y[:-1], z)
+    plt.subplot(222)
+    plt.pcolormesh(x, y, z)
+    x = np.repeat(x[np.newaxis], 21, axis=0)
+    y = np.repeat(y[:, np.newaxis], 21, axis=1)
+    plt.subplot(223)
+    plt.pcolormesh(x[:-1, :-1], y[:-1, :-1], z)
+    plt.subplot(224)
+    plt.pcolormesh(x, y, z)
+    for ax in fig.get_axes():
+        for label in ax.get_xticklabels():
+            label.set_ha('right')
+            label.set_rotation(30)
+
+
+@image_comparison(baseline_images=['pcolor_datetime_axis'],
+                  extensions=['png'], remove_text=False)
+def test_pcolor_datetime_axis():
+    fig = plt.figure()
+    fig.subplots_adjust(hspace=0.4, top=0.98, bottom=.15)
+    base = datetime.datetime(2013, 1, 1)
+    x = np.array([base + datetime.timedelta(days=d) for d in range(21)])
+    y = np.arange(21)
+    z1, z2 = np.meshgrid(np.arange(20), np.arange(20))
+    z = z1 * z2
+    plt.subplot(221)
+    plt.pcolor(x[:-1], y[:-1], z)
+    plt.subplot(222)
+    plt.pcolor(x, y, z)
+    x = np.repeat(x[np.newaxis], 21, axis=0)
+    y = np.repeat(y[:, np.newaxis], 21, axis=1)
+    plt.subplot(223)
+    plt.pcolor(x[:-1, :-1], y[:-1, :-1], z)
+    plt.subplot(224)
+    plt.pcolor(x, y, z)
+    for ax in fig.get_axes():
+        for label in ax.get_xticklabels():
+            label.set_ha('right')
+            label.set_rotation(30)
+
 
 def test_pcolorargs():
     n = 12

lib/matplotlib/tests/test_contour.py
@@ -1,3 +1,5 @@
+import datetime
+
 import numpy as np
 
 from matplotlib.testing.decorators import cleanup, image_comparison
@@ -175,6 +177,32 @@ def test_given_colors_levels_and_extends():
             plt.contour(data, colors=colors, levels=levels[:last_level], extend=extend)
     
         plt.colorbar()
+
+
+@image_comparison(baseline_images=['contour_datetime_axis'],
+                  extensions=['png'], remove_text=False)
+def test_contour_datetime_axis():
+    fig = plt.figure()
+    fig.subplots_adjust(hspace=0.4, top=0.98, bottom=.15)
+    base = datetime.datetime(2013, 1, 1)
+    x = np.array([base + datetime.timedelta(days=d) for d in range(20)])
+    y = np.arange(20)
+    z1, z2 = np.meshgrid(np.arange(20), np.arange(20))
+    z = z1 * z2
+    plt.subplot(221)
+    plt.contour(x, y, z)
+    plt.subplot(222)
+    plt.contourf(x, y, z)
+    x = np.repeat(x[np.newaxis], 20, axis=0)
+    y = np.repeat(y[:, np.newaxis], 20, axis=1)
+    plt.subplot(223)
+    plt.contour(x, y, z)
+    plt.subplot(224)
+    plt.contourf(x, y, z)
+    for ax in fig.get_axes():
+        for label in ax.get_xticklabels():
+            label.set_ha('right')
+            label.set_rotation(30)
 
 
 if __name__ == '__main__':

commit a881a5c071a43c04a5595f51f6b3c4c99f4a460b

Fix a recursion problem with masked arrays in get_converter introduced in f8344a14504
If a masked array with the first element masked is passed in to get_converter, the   converter = self.get_converter(xravel()[0])  call causes a RuntimeError: maximum recursion depth exceeded.  This patch will pass the first unmasked value, or skip the call if all elements are masked.

lib/matplotlib/units.py
@@ -134,8 +134,17 @@ class Registry(dict):
             converter = self.get(classx)
 
         if isinstance(x, np.ndarray) and x.size:
-            converter = self.get_converter(x.ravel()[0])
-            return converter
+            xravel = x.ravel()
+            try:
+                # pass the first value of x that is not masked back to get_converter
+                if not np.all(xravel.mask):
+                    # some elements are not masked
+                    converter = self.get_converter(xravel[np.argmin(xravel.mask)])
+                    return converter
+            except AttributeError:
+                # not a masked_array
+                converter = self.get_converter(xravel[0])
+                return converter
 
         if converter is None and iterable(x):
             for thisx in x: