8b69b80a1113e92e3788dd630e6a2add93674d6d
Tobias Megies <megies@geophysik.uni-muenchen.de>
2013-09-12 15:23:00 +0200

    multipage pdf: show usage of with statement in example

--- examples/pylab_examples/multipage_pdf.py
+++ examples/pylab_examples/multipage_pdf.py
@@ -41,3 +41,20 @@ d['ModDate'] = datetime.datetime.today()
 
 # Remember to close the object - otherwise the file will not be usable
 pdf.close()
+
+# Or use the with statement, the file gets properly closed at the end:
+with PdfPages('multipage_pdf2.pdf') as pdf:
+
+    figure(figsize=(3, 3))
+    plot(range(7), [3, 1, 4, 1, 5, 9, 2], 'r-o')
+    title('Page One')
+    pdf.savefig()
+    close()
+
+    rc('text', usetex=True)
+    figure(figsize=(8, 6))
+    x = np.arange(0, 5, 0.1)
+    plot(x, np.sin(x), 'b-')
+    title('Page Two')
+    pdf.savefig()
+    close()
