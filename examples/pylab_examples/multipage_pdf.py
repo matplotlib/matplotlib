c60dfdd4758eb1533d8a1d501dbf22dca5f3cba7
Tobias Megies <megies@geophysik.uni-muenchen.de>
2013-09-18 14:06:59 +0200

    show only preferred usage for PdfPages and use preferred pyplot import style

--- examples/pylab_examples/multipage_pdf.py
+++ examples/pylab_examples/multipage_pdf.py
@@ -2,34 +2,33 @@
 
 import datetime
 import numpy as np
-import matplotlib
 from matplotlib.backends.backend_pdf import PdfPages
-from pylab import *
+import matplotlib.pyplot as plt
 
 # Create the PdfPages object to which we will save the pages:
 # The with statement makes sure that the PdfPages object is closed properly at
 # the end of the block, even if an Exception occurs.
 with PdfPages('multipage_pdf.pdf') as pdf:
-    figure(figsize=(3,3))
-    plot(range(7), [3,1,4,1,5,9,2], 'r-o')
-    title('Page One')
+    plt.figure(figsize=(3, 3))
+    plt.plot(range(7), [3, 1, 4, 1, 5, 9, 2], 'r-o')
+    plt.title('Page One')
     pdf.savefig()  # saves the current figure into a pdf page
-    close()
+    plt.close()
 
-    rc('text', usetex=True)
-    figure(figsize=(8,6))
-    x = np.arange(0,5,0.1)
-    plot(x, np.sin(x), 'b-')
-    title('Page Two')
+    plt.rc('text', usetex=True)
+    plt.figure(figsize=(8, 6))
+    x = np.arange(0, 5, 0.1)
+    plt.plot(x, np.sin(x), 'b-')
+    plt.title('Page Two')
     pdf.savefig()
-    close()
+    plt.close()
 
-    rc('text', usetex=False)
-    fig=figure(figsize=(4,5))
-    plot(x, x*x, 'ko')
-    title('Page Three')
-    pdf.savefig(fig) # or you can pass a Figure object to pdf.savefig
-    close()
+    plt.rc('text', usetex=False)
+    fig = plt.figure(figsize=(4, 5))
+    plt.plot(x, x*x, 'ko')
+    plt.title('Page Three')
+    pdf.savefig(fig)  # or you can pass a Figure object to pdf.savefig
+    plt.close()
 
     # We can also set the file's metadata via the PdfPages object:
     d = pdf.infodict()
@@ -37,5 +36,5 @@ with PdfPages('multipage_pdf.pdf') as pdf:
     d['Author'] = u'Jouni K. Sepp\xe4nen'
     d['Subject'] = 'How to create a multipage pdf file and set its metadata'
     d['Keywords'] = 'PdfPages multipage keywords author title subject'
-    d['CreationDate'] = datetime.datetime(2009,11,13)
+    d['CreationDate'] = datetime.datetime(2009, 11, 13)
     d['ModDate'] = datetime.datetime.today()
