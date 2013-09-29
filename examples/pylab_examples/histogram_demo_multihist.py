1e54ecf8c01fbab4f80c49fb0d7fe9f911eeaa80
Tony S Yu <tsyu80@gmail.com>
2013-09-29 08:23:21 -0500

    PEP8 fixes

--- examples/pylab_examples/histogram_demo_multihist.py
+++ examples/pylab_examples/histogram_demo_multihist.py
@@ -2,28 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+n_bins = 10
+x = np.random.randn(1000, 3)
+
 fig, axes = plt.subplots(nrows=2, ncols=2)
 ax0, ax1, ax2, ax3 = axes.flat
 
-x = np.random.randn(1000,3)
-
 colors = ['crimson', 'burlywood', 'chartreuse']
-n, bins, patches = ax0.hist(x, 10, normed=1, histtype='bar',
-                            color=colors, label=colors)
+ax0.hist(x, n_bins, normed=1, histtype='bar', color=colors, label=colors)
 ax0.legend(prop={'size': 10})
 ax0.set_title('bar')
 
-
-n, bins, patches = ax1.hist(x, 10, normed=1, histtype='bar', stacked=True)
+ax1.hist(x, n_bins, normed=1, histtype='bar', stacked=True)
 ax1.set_title('stacked bar')
 
-n, bins, patches = ax2.hist(x, 10, histtype='step', stacked=True, fill=True)
+ax2.hist(x, n_bins, histtype='step', stacked=True, fill=True)
 ax2.set_title('stepfilled')
 
 # Make a multiple-histogram of data-sets with different length.
 x_multi = [np.random.randn(n) for n in [10000, 5000, 2000]]
-
-n, bins, patches = ax3.hist(x_multi, 10, histtype='bar')
+ax3.hist(x_multi, n_bins, histtype='bar')
 ax3.set_title('different sample sizes')
 
 plt.tight_layout()
