c3cce1c0a255111caf055144a058a28ab971f319
Tony S Yu <tsyu80@gmail.com>
2013-09-29 08:23:21 -0500

    PEP8 fixes

--- examples/pylab_examples/histogram_demo_cumulative.py
+++ examples/pylab_examples/histogram_demo_cumulative.py
@@ -2,18 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import mlab
 
-mu, sigma = 200, 25
+
+mu = 200
+sigma = 25
+n_bins = 50
 x = mu + sigma*np.random.randn(10000)
-n, bins, patches = plt.hist(x, 50, normed=1, histtype='step', cumulative=True)
+
+n, bins, patches = plt.hist(x, n_bins, normed=1,
+                            histtype='step', cumulative=True)
 
 # Add a line showing the expected distribution.
-y = mlab.normpdf( bins, mu, sigma).cumsum()
+y = mlab.normpdf(bins, mu, sigma).cumsum()
 y /= y[-1]
 plt.plot(bins, y, 'k--', linewidth=1.5)
 
 # Overlay a reverted cumulative histogram.
-n, bins, patches = plt.hist(x, bins=bins, normed=1,
-    histtype='step', cumulative=-1)
+plt.hist(x, bins=bins, normed=1, histtype='step', cumulative=-1)
 
 plt.grid(True)
 plt.ylim(0, 1.05)
--- examples/pylab_examples/histogram_demo_histtypes.py
+++ examples/pylab_examples/histogram_demo_histtypes.py
@@ -3,22 +3,22 @@ import matplotlib.pyplot as plt
 from matplotlib import mlab
 
 
-mu, sigma = 200, 25
+mu = 200
+sigma = 25
+n_bins = 50
 x = mu + sigma*np.random.randn(10000)
 
 fig, (ax0, ax1) = plt.subplots(ncols=2, figsize=(8, 4))
 
-n, bins, patches = ax0.hist(x, 50, normed=1, histtype='stepfilled',
+n, bins, patches = ax0.hist(x, n_bins, normed=1, histtype='stepfilled',
                             facecolor='g', alpha=0.75)
-
 # Add a line showing the expected distribution.
-y = mlab.normpdf( bins, mu, sigma)
+y = mlab.normpdf(bins, mu, sigma)
 ax0.plot(bins, y, 'k--', linewidth=1.5)
-
 ax0.set_title('stepfilled')
 
 # Create a histogram by providing the bin edges (unequally spaced).
-bins = [100,150,180,195,205,220,250,300]
+bins = [100, 150, 180, 195, 205, 220, 250, 300]
 n, bins, patches = ax1.hist(x, bins, normed=1, histtype='bar', rwidth=0.8)
 ax1.set_title('unequal bins')
 
