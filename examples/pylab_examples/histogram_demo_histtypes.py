3c00b53c246a7e1be5cb7975f0133387b9a2962d
Tony S Yu <tsyu80@gmail.com>
2013-09-29 07:35:39 -0500

    Replace multiple figures with subplots

--- examples/pylab_examples/histogram_demo_histtypes.py
+++ examples/pylab_examples/histogram_demo_histtypes.py
@@ -7,46 +7,44 @@ from matplotlib.mlab import normpdf
 mu, sigma = 200, 25
 x = mu + sigma*np.random.randn(10000)
 
-n, bins, patches = plt.hist(x, 50, normed=1, histtype='stepfilled',
+fig, (ax0, ax1, ax2) = plt.subplots(ncols=3, figsize=(8, 3))
+
+n, bins, patches = ax0.hist(x, 50, normed=1, histtype='stepfilled',
                             facecolor='g', alpha=0.75)
 
 # Add a line showing the expected distribution.
 y = normpdf( bins, mu, sigma)
-plt.plot(bins, y, 'k--', linewidth=1.5)
-
-
-plt.figure()
+ax0.plot(bins, y, 'k--', linewidth=1.5)
 
 # Create a histogram by providing the bin edges (unequally spaced).
 bins = [100,125,150,160,170,180,190,200,210,220,230,240,250,275,300]
-n, bins, patches = plt.hist(x, bins, normed=1, histtype='bar', rwidth=0.8)
+n, bins, patches = ax1.hist(x, bins, normed=1, histtype='bar', rwidth=0.8)
 
-plt.figure()
-
-n, bins, patches = plt.hist(x, 50, normed=1, histtype='step', cumulative=True)
+n, bins, patches = ax2.hist(x, 50, normed=1, histtype='step', cumulative=True)
 
 # Add a line showing the expected distribution.
 y = normpdf( bins, mu, sigma).cumsum()
 y /= y[-1]
-plt.plot(bins, y, 'k--', linewidth=1.5)
+ax2.plot(bins, y, 'k--', linewidth=1.5)
 
 # Create a second data-set with a smaller standard deviation.
 sigma2 = 15.
 x = mu + sigma2*np.random.randn(10000)
 
-n, bins, patches = plt.hist(x, bins=bins, normed=1, histtype='step', cumulative=True)
+n, bins, patches = ax2.hist(x, bins=bins, normed=1, histtype='step', cumulative=True)
 
 # Add a line showing the expected distribution.
 y = normpdf( bins, mu, sigma2).cumsum()
 y /= y[-1]
-plt.plot(bins, y, 'r--', linewidth=1.5)
+ax2.plot(bins, y, 'r--', linewidth=1.5)
 
 # Overlay a reverted cumulative histogram.
-n, bins, patches = plt.hist(x, bins=bins, normed=1,
+n, bins, patches = ax2.hist(x, bins=bins, normed=1,
     histtype='step', cumulative=-1)
 
 
-plt.grid(True)
-plt.ylim(0, 1.05)
+ax2.grid(True)
+ax2.set_ylim(0, 1.05)
 
+plt.tight_layout()
 plt.show()
