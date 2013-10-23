366e2225d71b1622e23066190c7ca25604f1614f
Michael Droettboom <mdboom@gmail.com>
2013-10-23 09:07:39 -0400

    Add comment

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -1191,6 +1191,9 @@ def interactive(b):
 
 def is_interactive():
     'Return true if plot mode is interactive'
+    # ps1 exists if the python interpreter is running in an
+    # interactive console; sys.flags.interactive is true if a script
+    # is being run via "python -i".
     b = rcParams['interactive'] and (
         hasattr(sys, 'ps1') or sys.flags.interactive)
     return b
