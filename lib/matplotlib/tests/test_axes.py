827653aa0d13e83e7a2306270325a700d5b5df75
Untom <unterthiner@bioinf.jku.at>
2013-11-04 16:11:38 +0100

    Fixed tests that rely on useOffset=true.

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -85,6 +85,7 @@ def test_formatter_large_small():
     fig, ax = plt.subplots(1)
     x = [0.500000001, 0.500000002]
     y = [1e64, 1.1e64]
+    ax.ticklabel_format(useOffset=True, axis='y')
     ax.plot(x, y)
 
 @image_comparison(baseline_images=["twin_axis_locaters_formatters"])
