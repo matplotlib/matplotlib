962ed31483668d496aece83187af24b460fc370c
RutgerK <kassies@gmail.com>
2013-11-18 13:21:36 +0100

    Update interpolation_methods.py

--- examples/images_contours_and_fields/interpolation_methods.py
+++ examples/images_contours_and_fields/interpolation_methods.py
@@ -8,19 +8,21 @@ import numpy as np
 # from the docs:
 
 # If interpolation is None, default to rc image.interpolation. See also
-# the filternorm and filterrad parameters. If interpolation is 'none', then 
-# no interpolation is performed on the Agg, ps and pdf backends. Other 
+# the filternorm and filterrad parameters. If interpolation is 'none', then
+# no interpolation is performed on the Agg, ps and pdf backends. Other
 # backends will fall back to 'nearest'.
 #
 # http://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.imshow
 
-methods = [None, 'none', 'nearest', 'bilinear', 'bicubic', 'spline16', 
-          'spline36', 'hanning', 'hamming', 'hermite', 'kaiser', 'quadric', 
-          'catrom', 'gaussian', 'bessel', 'mitchell', 'sinc', 'lanczos']
+methods = [None, 'none', 'nearest', 'bilinear', 'bicubic', 'spline16',
+           'spline36', 'hanning', 'hamming', 'hermite', 'kaiser', 'quadric',
+           'catrom', 'gaussian', 'bessel', 'mitchell', 'sinc', 'lanczos']
 
-grid = np.random.rand(4,4)
+grid = np.random.rand(4, 4)
 
-fig, ax = plt.subplots(3,6,figsize=(12,6), subplot_kw={'xticks': [], 'yticks': []})
+fig, ax = plt.subplots(3, 6, figsize=(12, 6),
+                       subplot_kw={'xticks': [], 'yticks': []})
+
 fig.subplots_adjust(hspace=0.3, wspace=0.05)
 
 ax = ax.ravel()
