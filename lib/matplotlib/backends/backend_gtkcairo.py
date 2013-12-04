b099ddd89c9241566fa52dfdd2bb8ba8f4c00578
Thomas A Caswell <tcaswell@gmail.com>
2013-12-03 22:15:53 -0600

    pep8 on backend_gtkcairo.py

--- lib/matplotlib/backends/backend_gtkcairo.py
+++ lib/matplotlib/backends/backend_gtkcairo.py
@@ -8,7 +8,7 @@ from __future__ import (absolute_import, division, print_function,
 import six
 
 import gtk
-if gtk.pygtk_version < (2,7,0):
+if gtk.pygtk_version < (2, 7, 0):
     import cairo.gtk
 
 from matplotlib.backends import backend_cairo
@@ -26,7 +26,8 @@ def new_figure_manager(num, *args, **kwargs):
     """
     Create a new figure manager instance
     """
-    if _debug: print('backend_gtkcairo.%s()' % fn_name())
+    if _debug:
+        print('backend_gtkcairo.%s()' % fn_name())
     FigureClass = kwargs.pop('FigureClass', Figure)
     thisFig = FigureClass(*args, **kwargs)
     return new_figure_manager_given_figure(num, thisFig)
@@ -41,12 +42,12 @@ def new_figure_manager_given_figure(num, figure):
 
 
 class RendererGTKCairo (backend_cairo.RendererCairo):
-    if gtk.pygtk_version >= (2,7,0):
-        def set_pixmap (self, pixmap):
+    if gtk.pygtk_version >= (2, 7, 0):
+        def set_pixmap(self, pixmap):
             self.gc.ctx = pixmap.cairo_create()
     else:
-        def set_pixmap (self, pixmap):
-            self.gc.ctx = cairo.gtk.gdk_cairo_create (pixmap)
+        def set_pixmap(self, pixmap):
+            self.gc.ctx = cairo.gtk.gdk_cairo_create(pixmap)
 
 
 class FigureCanvasGTKCairo(backend_cairo.FigureCanvasCairo, FigureCanvasGTK):
@@ -55,16 +56,17 @@ class FigureCanvasGTKCairo(backend_cairo.FigureCanvasCairo, FigureCanvasGTK):
 
     def _renderer_init(self):
         """Override to use cairo (rather than GDK) renderer"""
-        if _debug: print('%s.%s()' % (self.__class__.__name__, _fn_name()))
-        self._renderer = RendererGTKCairo (self.figure.dpi)
+        if _debug:
+            print('%s.%s()' % (self.__class__.__name__, _fn_name()))
+        self._renderer = RendererGTKCairo(self.figure.dpi)
 
 
 class FigureManagerGTKCairo(FigureManagerGTK):
     def _get_toolbar(self, canvas):
         # must be inited after the window, drawingArea and figure
         # attrs are set
-        if matplotlib.rcParams['toolbar']=='toolbar2':
-            toolbar = NavigationToolbar2GTKCairo (canvas, self.window)
+        if matplotlib.rcParams['toolbar'] == 'toolbar2':
+            toolbar = NavigationToolbar2GTKCairo(canvas, self.window)
         else:
             toolbar = None
         return toolbar
