676f28ed37ad4deccfe6efa2b967d50cfeac4b11
Paul Hobson <pmhobson@gmail.com>
2013-12-16 13:00:37 -0800

    DOC: impoved example boxplot figure layout and confirmed that they build with docs

--- examples/statistics/bxp_demo.py
+++ examples/statistics/bxp_demo.py
@@ -24,7 +24,7 @@ print(stats[0].keys())
 fs = 10 # fontsize
 
 # demonstrate how to toggle the display of different elements:
-fig, axes = plt.subplots(nrows=2, ncols=3)
+fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(6,6))
 axes[0, 0].bxp(stats)
 axes[0, 0].set_title('Default', fontsize=fs)
 
@@ -32,10 +32,10 @@ axes[0, 1].bxp(stats, showmeans=True)
 axes[0, 1].set_title('showmeans=True', fontsize=fs)
 
 axes[0, 2].bxp(stats, showmeans=True, meanline=True)
-axes[0, 2].set_title('showmeans=True, meanline=True', fontsize=fs)
+axes[0, 2].set_title('showmeans=True,\nmeanline=True', fontsize=fs)
 
 axes[1, 0].bxp(stats, showbox=False, showcaps=False)
-axes[1, 0].set_title('Tufte Style (showbox=False, showcaps=False)', fontsize=fs)
+axes[1, 0].set_title('Tufte Style\n(showbox=False,\nshowcaps=False)', fontsize=fs)
 
 axes[1, 1].bxp(stats, shownotches=True)
 axes[1, 1].set_title('notch=True', fontsize=fs)
@@ -47,6 +47,7 @@ for ax in axes.flatten():
     ax.set_yscale('log')
     ax.set_yticklabels([])
 
+fig.subplots_adjust(hspace=0.4)
 plt.show()
 
 
@@ -59,23 +60,24 @@ meanpointprops = dict(marker='D', markeredgecolor='black',
                       markerfacecolor='firebrick')
 meanlineprops = dict(linestyle='--', linewidth=2.5, color='purple')
 
-fig, axes = plt.subplots(nrows=2, ncols=2)
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(6,6))
 axes[0, 0].bxp(stats, boxprops=boxprops)
 axes[0, 0].set_title('Custom boxprops', fontsize=fs)
 
 axes[0, 1].bxp(stats, flierprops=flierprops, medianprops=medianprops)
-axes[0, 1].set_title('Custom medianprops and flierprops', fontsize=fs)
+axes[0, 1].set_title('Custom medianprops\nand flierprops', fontsize=fs)
 
 axes[1, 0].bxp(stats, meanprops=meanpointprops, meanline=False,
                    showmeans=True)
-axes[1, 0].set_title('Custom mean as point', fontsize=fs)
+axes[1, 0].set_title('Custom mean\nas point', fontsize=fs)
 
 axes[1, 1].bxp(stats, meanprops=meanlineprops, meanline=True, showmeans=True)
-axes[1, 1].set_title('Custom mean as line', fontsize=fs)
+axes[1, 1].set_title('Custom mean\nas line', fontsize=fs)
 
 for ax in axes.flatten():
     ax.set_yscale('log')
     ax.set_yticklabels([])
 
 fig.suptitle("I never said they'd be pretty")
+fig.subplots_adjust(hspace=0.4)
 plt.show()
