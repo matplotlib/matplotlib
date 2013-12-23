bcb2f11c00c7b3b6bde84a623fb19cf7c5b41a7e
Rohan Walker <rtrwalker@gmail.com>
2013-12-24 10:34:37 +1100

    pep8 fixes

--- examples/pylab_examples/markevery_demo.py
+++ examples/pylab_examples/markevery_demo.py
@@ -31,7 +31,7 @@ cases = [None,
          (0.0, 0.1), (0.45, 0.1)]
 
 #define the figure size and grid layout properties
-figsize = (10,8)
+figsize = (10, 8)
 cols = 3
 gs = gridspec.GridSpec(len(cases) // cols + 1, cols)
 
@@ -42,26 +42,26 @@ y = np.sin(x) + 1.0 + delta
 
 #plot each markevery case for linear x and y scales
 fig1 = plt.figure(num=1, figsize=figsize)
-ax=[]
+ax = []
 for i, case in enumerate(cases):
     row = (i // cols)
     col = i % cols
-    ax.append(fig1.add_subplot(gs[row,col]))
+    ax.append(fig1.add_subplot(gs[row, col]))
     ax[-1].set_title('markevery=%s' % str(case))
-    ax[-1].plot(x, y,'o', ls='-', ms=4,  markevery=case)
+    ax[-1].plot(x, y, 'o', ls='-', ms=4,  markevery=case)
 #fig1.tight_layout()
 
 #plot each markevery case for log x and y scales
 fig2 = plt.figure(num=2, figsize=figsize)
-axlog=[]
+axlog = []
 for i, case in enumerate(cases):
     row = (i // cols)
     col = i % cols
-    axlog.append(fig2.add_subplot(gs[row,col]))
+    axlog.append(fig2.add_subplot(gs[row, col]))
     axlog[-1].set_title('markevery=%s' % str(case))
     axlog[-1].set_xscale('log')
     axlog[-1].set_yscale('log')
-    axlog[-1].plot(x, y,'o', ls='-', ms=4,  markevery=case)
+    axlog[-1].plot(x, y, 'o', ls='-', ms=4,  markevery=case)
 fig2.tight_layout()
 
 #plot each markevery case for linear x and y scales but zoomed in
@@ -69,14 +69,14 @@ fig2.tight_layout()
 #it is always interpreted with respect to the first data point which might be
 #different to the first visible data point.
 fig3 = plt.figure(num=3, figsize=figsize)
-axzoom=[]
+axzoom = []
 for i, case in enumerate(cases):
     row = (i // cols)
     col = i % cols
-    axzoom.append(fig3.add_subplot(gs[row,col]))
+    axzoom.append(fig3.add_subplot(gs[row, col]))
     axzoom[-1].set_title('markevery=%s' % str(case))
-    axzoom[-1].plot(x, y,'o', ls='-', ms=4,  markevery=case)
-    axzoom[-1].set_xlim((6,6.7))
+    axzoom[-1].plot(x, y, 'o', ls='-', ms=4,  markevery=case)
+    axzoom[-1].set_xlim((6, 6.7))
     axzoom[-1].set_ylim((1.1, 1.7))
 fig3.tight_layout()
 
@@ -86,13 +86,13 @@ theta = 2 * np.pi * r
 
 #plot each markevery case for polar plots
 fig4 = plt.figure(num=4, figsize=figsize)
-axpolar=[]
+axpolar = []
 for i, case in enumerate(cases):
     row = (i // cols)
     col = i % cols
-    axpolar.append(fig4.add_subplot(gs[row,col], polar = True))
+    axpolar.append(fig4.add_subplot(gs[row, col], polar = True))
     axpolar[-1].set_title('markevery=%s' % str(case))
-    axpolar[-1].plot(theta, r,'o', ls='-', ms=4,  markevery=case)
+    axpolar[-1].plot(theta, r, 'o', ls='-', ms=4,  markevery=case)
 fig4.tight_layout()
 
 plt.show()
