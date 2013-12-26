073f3d541006b0b2580f115f13a285b23b35a71a
Michael <michs@users.noreply.github.com>
2013-12-26 14:59:39 +0100

    Restored conformity to Freedesktop standard and capability to run on clusters whith readonly home directories on compute nodes.
    The standard allow on Linux to specify configuration and cache directories
    independent from the existence of a home directory. The functions
    _get_xdg_config_dir() and _get_xdg_cache_dir() respect this now.
    
    Function _get_config_or_cache_dir() allow now to use also on Linux an
    existing old style configuration directory "~/.matplotlib". If this does
    not exist, the configuration directory will be expected in the location
    specified by the XDG_CONFIG_HOME environment variable. If this location
    is undefined or it cannot be found resp. created, a temp. configuration
    directory will be created in the directory for temp. files.

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -531,11 +531,12 @@ def _get_xdg_config_dir():
     base directory spec
     <http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html>`_.
     """
-    home = get_home()
-    if home is None:
-        return None
-    else:
-        return os.environ.get('XDG_CONFIG_HOME', os.path.join(home, '.config'))
+    path = os.environ.get('XDG_CONFIG_HOME')
+    if not path:
+        path = get_home()
+        if path:
+            path = os.path.join(path, '.config')
+    return path
 
 
 def _get_xdg_cache_dir():
@@ -544,11 +545,12 @@ def _get_xdg_cache_dir():
     base directory spec
     <http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html>`_.
     """
-    home = get_home()
-    if home is None:
-        return None
-    else:
-        return os.environ.get('XDG_CACHE_HOME', os.path.join(home, '.cache'))
+    path = os.environ.get('XDG_CACHE_HOME')
+    if not path:
+        path = get_home()
+        if path:
+            path = os.path.join(path, '.cache')
+    return path
 
 
 def _get_config_or_cache_dir(xdg_base):
@@ -566,13 +568,16 @@ def _get_config_or_cache_dir(xdg_base):
 
     p = None
     h = get_home()
-    if h is not None:
+    if h:
         p = os.path.join(h, '.matplotlib')
-        if (sys.platform.startswith('linux') and
-            xdg_base is not None):
-            p = os.path.join(xdg_base, 'matplotlib')
+        if sys.platform.startswith('linux') and not os.path.exists(p):
+            # no old config exists
+            if xdg_base:
+                p = os.path.join(xdg_base, 'matplotlib')
+            else:
+                p = None
 
-    if p is not None:
+    if p:
         if os.path.exists(p):
             if _is_writable_dir(p):
                 return p
