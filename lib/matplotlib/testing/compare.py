6fe16cfd7548a7b7614b318a2359089da22fb004
Phil Elson <pelson.pub@gmail.com>
2014-01-10 10:09:09 +0000

    Manual PEP8 fixes.

--- lib/matplotlib/testing/compare.py
+++ lib/matplotlib/testing/compare.py
@@ -1,40 +1,27 @@
-#=======================================================================
-
-""" A set of utilities for comparing results.
 """
-#=======================================================================
+Provides a collection of utilities for comparing (image) results.
 
+"""
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import six
-from six.moves import xrange
+
+import hashlib
+import os
+import shutil
+
+import numpy as np
 
 import matplotlib
 from matplotlib.compat import subprocess
 from matplotlib.testing.noseclasses import ImageComparisonFailure
-from matplotlib.testing import image_util
 from matplotlib import _png
 from matplotlib import _get_cachedir
 from matplotlib import cbook
 from distutils import version
-import hashlib
-import math
-import os
-import numpy as np
-import shutil
-import sys
-from functools import reduce
 
-#=======================================================================
-
-__all__ = [
-    'compare_float',
-    'compare_images',
-    'comparable_formats',
-]
-
-#-----------------------------------------------------------------------
+__all__ = ['compare_float', 'compare_images', 'comparable_formats']
 
 
 def make_test_filename(fname, purpose):
@@ -47,32 +34,29 @@ def make_test_filename(fname, purpose):
 
 
 def compare_float(expected, actual, relTol=None, absTol=None):
-    """Fail if the floating point values are not close enough, with
-       the givem message.
+    """
+    Fail if the floating point values are not close enough, with
+    the given message.
 
     You can specify a relative tolerance, absolute tolerance, or both.
+
     """
     if relTol is None and absTol is None:
-        exMsg = "You haven't specified a 'relTol' relative tolerance "
-        exMsg += "or a 'absTol' absolute tolerance function argument.  "
-        exMsg += "You must specify one."
-        raise ValueError(exMsg)
-
+        raise ValueError("You haven't specified a 'relTol' relative "
+                         "tolerance or a 'absTol' absolute tolerance "
+                         "function argument. You must specify one.")
     msg = ""
 
     if absTol is not None:
         absDiff = abs(expected - actual)
         if absTol < absDiff:
-            expectedStr = str(expected)
-            actualStr = str(actual)
-            absDiffStr = str(absDiff)
-            absTolStr = str(absTol)
+            template = ['',
+                        'Expected: {expected}',
+                        'Actual:   {actual}',
+                        'Abs diff: {absDiff}',
+                        'Abs tol:  {absTol}']
 
-            msg += "\n"
-            msg += "  Expected: " + expectedStr + "\n"
-            msg += "  Actual:   " + actualStr + "\n"
-            msg += "  Abs Diff: " + absDiffStr + "\n"
-            msg += "  Abs Tol:  " + absTolStr + "\n"
+            msg += '\n  '.join([line.format(locals()) for line in template])
 
     if relTol is not None:
         # The relative difference of the two values.  If the expected value is
@@ -82,29 +66,16 @@ def compare_float(expected, actual, relTol=None, absTol=None):
             relDiff = relDiff / abs(expected)
 
         if relTol < relDiff:
-
             # The relative difference is a ratio, so it's always unitless.
-            relDiffStr = str(relDiff)
-            relTolStr = str(relTol)
+            template = ['',
+                        'Expected: {expected}',
+                        'Actual:   {actual}',
+                        'Rel diff: {relDiff}',
+                        'Rel tol:  {relTol}']
 
-            expectedStr = str(expected)
-            actualStr = str(actual)
+            msg += '\n  '.join([line.format(locals()) for line in template])
 
-            msg += "\n"
-            msg += "  Expected: " + expectedStr + "\n"
-            msg += "  Actual:   " + actualStr + "\n"
-            msg += "  Rel Diff: " + relDiffStr + "\n"
-            msg += "  Rel Tol:  " + relTolStr + "\n"
-
-    if msg:
-        return msg
-    else:
-        return None
-
-#-----------------------------------------------------------------------
-# A dictionary that maps filename extensions to functions that map
-# parameters old and new to a list that can be passed to Popen to
-# convert files with that extension to png format.
+    return msg or None
 
 
 def get_cache_dir():
@@ -122,7 +93,7 @@ def get_cache_dir():
     return cache_dir
 
 
-def get_file_hash(path, block_size=2 ** 20):
+def get_file_hash(path, block_size=2**20):
     md5 = hashlib.md5()
     with open(path, 'rb') as fd:
         while True:
@@ -131,8 +102,6 @@ def get_file_hash(path, block_size=2 ** 20):
                 break
             md5.update(data)
     return md5.hexdigest()
-
-converter = {}
 
 
 def make_external_conversion_command(cmd):
@@ -152,36 +121,51 @@ def make_external_conversion_command(cmd):
 
     return convert
 
-gs, gs_v = matplotlib.checkdep_ghostscript()
-if gs_v is not None:
-    cmd = lambda old, new: \
-        [gs, '-q', '-sDEVICE=png16m', '-dNOPAUSE', '-dBATCH',
-         '-sOutputFile=' + new, old]
-    converter['pdf'] = make_external_conversion_command(cmd)
-    converter['eps'] = make_external_conversion_command(cmd)
 
-if matplotlib.checkdep_inkscape() is not None:
-    cmd = lambda old, new: \
-        ['inkscape', '-z', old, '--export-png', new]
-    converter['svg'] = make_external_conversion_command(cmd)
+def _update_converter():
+    gs, gs_v = matplotlib.checkdep_ghostscript()
+    if gs_v is not None:
+        cmd = lambda old, new: \
+            [gs, '-q', '-sDEVICE=png16m', '-dNOPAUSE', '-dBATCH',
+             '-sOutputFile=' + new, old]
+        converter['pdf'] = make_external_conversion_command(cmd)
+        converter['eps'] = make_external_conversion_command(cmd)
+    
+    if matplotlib.checkdep_inkscape() is not None:
+        cmd = lambda old, new: \
+            ['inkscape', '-z', old, '--export-png', new]
+        converter['svg'] = make_external_conversion_command(cmd)
+
+
+#: A dictionary that maps filename extensions to functions which
+#: themselves map arguments `old` and `new` (filenames) to a list of strings.
+#: The list can then be passed to Popen to convert files with that
+#: extension to png format.
+converter = {}
+_update_converter()
 
 
 def comparable_formats():
-    '''Returns the list of file formats that compare_images can compare
-    on this system.'''
+    """
+    Returns the list of file formats that compare_images can compare
+    on this system.
+    
+    """
     return ['png'] + list(six.iterkeys(converter))
 
 
 def convert(filename, cache):
-    '''
+    """
     Convert the named file into a png file.  Returns the name of the
     created file.
 
     If *cache* is True, the result of the conversion is cached in
-    `~/.matplotlib/test_cache/`.  The caching is based on a hash of the
-    exact contents of the input file.  The is no limit on the size of
-    the cache, so it may need to be manually cleared periodically.
-    '''
+    `matplotlib._get_cachedir() + '/test_cache/'`.  The caching is based
+    on a hash of the exact contents of the input file.  The is no limit
+    on the size of the cache, so it may need to be manually cleared
+    periodically.
+
+    """
     base, extension = filename.rsplit('.', 1)
     if extension not in converter:
         raise ImageComparisonFailure(
@@ -200,9 +184,9 @@ def convert(filename, cache):
             cache_dir = None
 
         if cache_dir is not None:
-            hash = get_file_hash(filename)
+            hash_value = get_file_hash(filename)
             new_ext = os.path.splitext(newname)[1]
-            cached_file = os.path.join(cache_dir, hash + new_ext)
+            cached_file = os.path.join(cache_dir, hash_value + new_ext)
             if os.path.exists(cached_file):
                 shutil.copyfile(cached_file, newname)
                 return newname
@@ -214,13 +198,20 @@ def convert(filename, cache):
 
     return newname
 
+#: Maps file extensions to a function which takes a filename as its
+#: only argument to return a list suitable for execution with Popen.
+#: The purpose of this is so that the result file (with the given 
+#: extension) can be verified with tools such as xmllint for svg. 
 verifiers = {}
+
+# Turning this off, because it seems to cause multiprocessing issues
+if matplotlib.checkdep_xmllint() and False:
+    verifiers['svg'] = lambda filename: [
+        'xmllint', '--valid', '--nowarning', '--noout', filename]
 
 
 def verify(filename):
-    """
-    Verify the file through some sort of verification tool.
-    """
+    """Verify the file through some sort of verification tool."""
     if not os.path.exists(filename):
         raise IOError("'%s' does not exist" % filename)
     base, extension = filename.rsplit('.', 1)
@@ -239,11 +230,6 @@ def verify(filename):
                 msg += "Standard error:\n%s\n" % stderr
             raise IOError(msg)
 
-# Turning this off, because it seems to cause multiprocessing issues
-if matplotlib.checkdep_xmllint() and False:
-    verifiers['svg'] = lambda filename: [
-        'xmllint', '--valid', '--nowarning', '--noout', filename]
-
 
 def crop_to_same(actual_path, actual_image, expected_path, expected_image):
     # clip the images to the same size -- this is useful only when
@@ -257,12 +243,12 @@ def crop_to_same(actual_path, actual_image, expected_path, expected_image):
 
 
 def calculate_rms(expectedImage, actualImage):
-    # calculate the per-pixel errors, then compute the root mean square error
+    "Calculate the per-pixel errors, then compute the root mean square error."
     num_values = np.prod(expectedImage.shape)
     abs_diff_image = abs(expectedImage - actualImage)
 
-    # On Numpy 1.6, we can use bincount with minlength, which is much faster than
-    # using histogram
+    # On Numpy 1.6, we can use bincount with minlength, which is much
+    # faster than using histogram
     expected_version = version.LooseVersion("1.6")
     found_version = version.LooseVersion(np.__version__)
     if found_version >= expected_version:
@@ -277,24 +263,34 @@ def calculate_rms(expectedImage, actualImage):
 
 
 def compare_images(expected, actual, tol, in_decorator=False):
-    '''Compare two image files - not the greatest, but fast and good enough.
+    """
+    Compare two "image" files checking differences within a tolerance.
+    
+    The two given filenames may point to files which are convertible to
+    PNG via the `.converter` dictionary. The underlying RMS is calculated
+    with the `.calculate_rms` function.
+    
+    Parameters
+    ----------
+    expected : str
+        The filename of the expected image.
+    actual :str
+        The filename of the actual image.
+    tol : float
+        The tolerance (a color value difference, where 255 is the
+        maximal difference).  The test fails if the average pixel
+        difference is greater than this value.
+    in_decorator : bool
+        If called from image_comparison decorator, this should be
+        True. (default=False)
+    
+    Example
+    -------
+    img1 = "./baseline/plot.png"
+    img2 = "./output/plot.png"
+    compare_images( img1, img2, 0.001 ):
 
-    = EXAMPLE
-
-    # img1 = "./baseline/plot.png"
-    # img2 = "./output/plot.png"
-    #
-    # compare_images( img1, img2, 0.001 ):
-
-    = INPUT VARIABLES
-    - expected  The filename of the expected image.
-    - actual    The filename of the actual image.
-    - tol       The tolerance (a color value difference, where 255 is the
-                maximal difference).  The test fails if the average pixel
-                difference is greater than this value.
-    - in_decorator If called from image_comparison decorator, this should be
-                True. (default=False)
-    '''
+    """
     if not os.path.exists(actual):
         msg = "Output image %s does not exist." % actual
         raise Exception(msg)
@@ -329,10 +325,6 @@ def compare_images(expected, actual, tol, in_decorator=False):
     expectedImage = expectedImage.astype(np.int16)
     actualImage = actualImage.astype(np.int16)
 
-    # compare the resulting image histogram functions
-    expected_version = version.LooseVersion("1.6")
-    found_version = version.LooseVersion(np.__version__)
-
     rms = calculate_rms(expectedImage, actualImage)
 
     diff_image = make_test_filename(actual, 'failed-diff')
@@ -344,23 +336,19 @@ def compare_images(expected, actual, tol, in_decorator=False):
 
     save_diff_image(expected, actual, diff_image)
 
-    if in_decorator:
-        results = dict(
-            rms=rms,
-            expected=str(expected),
-            actual=str(actual),
-            diff=str(diff_image),
-        )
-        return results
-    else:
-        # old-style call from mplTest directory
-        msg = "  Error: Image files did not match.\n"       \
-              "  RMS Value: " + str(rms) + "\n" \
-              "  Expected:\n    " + str(expected) + "\n"  \
-              "  Actual:\n    " + str(actual) + "\n"      \
-              "  Difference:\n    " + str(diff_image) + "\n"      \
-              "  Tolerance: " + str(tol) + "\n"
-        return msg
+    results = dict(rms=rms, expected=str(expected),
+                   actual=str(actual), diff=str(diff_image))
+
+    if not in_decorator:
+        # Then the results should be a string suitable for stdout.
+        template = ['Error: Image files did not match.',
+                    'RMS Value: {rms}',
+                    'Expected:  \n    {expected}',
+                    'Actual:    \n    {actual}',
+                    'Difference:\n    {diff}',
+                    'Tolerance: \n    {tol}',]
+        results = '\n  '.join([line.format(**results) for line in template])
+    return results
 
 
 def save_diff_image(expected, actual, output):
