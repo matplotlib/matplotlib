40747912469133bcc92071458dc95f1d079e36a4
Michael Droettboom <mdboom@gmail.com>
2014-01-27 12:55:08 -0500

    Add test

---
+++ lib/matplotlib/tests/test_font_manager.py
@@ -0,0 +1,18 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import six
+
+import os
+
+from matplotlib.font_manager import findfont, FontProperties
+from matplotlib import rc_context
+
+
+def test_font_priority():
+    with rc_context(rc={
+            'font.sans-serif':
+            ['cmmi10', 'Bitstream Vera Sans']}):
+        font = findfont(
+            FontProperties(family=["sans-serif"]))
+    assert os.path.basename(font) == 'cmmi10.ttf'
