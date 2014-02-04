5ba6210ca6ca3e0fb3f6defe17b17410558ba4f1
Federico Ariza <ariza.federico@gmail.com>
2014-02-04 09:47:07 -0500

    bug in combined key press

--- lib/matplotlib/backend_bases.py
+++ lib/matplotlib/backend_bases.py
@@ -3408,8 +3408,7 @@ class NavigationBase(object):
     def trigger_tool(self, name):
         """Trigger on a tool
 
-        This is a convenient method to programatically "click" on
-        Tools
+        Method to programatically "click" on Tools
         """
         self._trigger_tool(name, None, False)
 
@@ -3433,6 +3432,8 @@ class NavigationBase(object):
             return
 
         name = self._keys.get(event.key, None)
+        if name is None:
+            return
         self._trigger_tool(name, event, False)
 
     def _get_instance(self, name):
