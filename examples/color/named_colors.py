20d21c00037906e3dc43020ce73f122c6111bb49
Federico Ariza <ariza.federico@gmail.com>
2014-02-10 13:33:10 -0500

    sorted by hue, sat, val

--- examples/color/named_colors.py
+++ examples/color/named_colors.py
@@ -1,24 +1,38 @@
-#issue https://github.com/matplotlib/matplotlib/issues/2164
-
 import numpy as np
 import matplotlib.pyplot as plt
-from  matplotlib.colors import cnames
+from matplotlib import cbook
+from  matplotlib import colors
 from itertools import chain
 
-print len(cnames.keys())
-n = len(cnames)
+n = len(colors.cnames)
 ncols = n / 15
 nrows = n / ncols
-names = cnames.keys()
-colors = cnames.values()
+
+names = colors.cnames.keys()
+hex_ = colors.cnames.values()
+rgb = [colors.hex2color(color) for color in hex_]
+hsv = [colors.rgb_to_hsv(color) for color in rgb]
+
+hue = [color[0] for color in hsv]
+sat = [color[1] for color in hsv]
+val = [color[2] for color in hsv]
+
+ind = np.lexsort((val, sat, hue))
+
+names_ = []
+colors_ = []
+
+for i in ind:
+    names_.append(names[i])
+    colors_.append(hex_[i])
 
 fig, axes_ = plt.subplots(nrows=nrows, ncols=ncols)
 axes = list(chain(*axes_))
 
 for i in range(n):
-    title = axes[i].set_title(names[i])
+    title = axes[i].set_title(names_[i])
     title.set_size('xx-small')
-    axes[i].set_axis_bgcolor(colors[i]) 
+    axes[i].set_axis_bgcolor(colors_[i]) 
     axes[i].spines['right'].set_visible(False)
     axes[i].spines['top'].set_visible(False)
     axes[i].spines['bottom'].set_visible(False)
@@ -26,10 +40,5 @@ for i in range(n):
     axes[i].set_xticks([])
     axes[i].set_yticks([])
 
-#     axes[i].set_label(names[i])
-
-# for axes in list(chain(*axes_)):
-#     axes.set_axis_off()
-
-# print nrows, ncols, fig, axes
+plt.tight_layout()
 plt.show()
