d38932a447191136239b1419223dad8d04d1fa86
Federico Ariza <ariza.federico@gmail.com>
2014-02-11 09:41:09 -0500

    three column arrangement

--- examples/color/named_colors.py
+++ examples/color/named_colors.py
@@ -2,45 +2,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cbook
 from  matplotlib import colors
-
+ 
 colors_ = colors.cnames.items()
-
+ 
 #add the single letter colors
 for name, rgb in colors.ColorConverter.colors.items():
     hex_ = colors.rgb2hex(rgb)
     colors_.append((name, hex_))
-
+ 
 #hex color values
 hex_ = [color[1] for color in colors_]
 #rgb equivalent
 rgb = [colors.hex2color(color) for color in hex_]
 #hsv equivalent
 hsv = [colors.rgb_to_hsv(color) for color in rgb]
-
+ 
 #split the hsv to sort
 hue = [color[0] for color in hsv]
 sat = [color[1] for color in hsv]
 val = [color[2] for color in hsv]
-
+ 
 #sort by hue, saturation and value
 ind = np.lexsort((val, sat, hue))
 sorted_colors = [colors_[i] for i in ind]
-
+ 
 n = len(sorted_colors)
-ncols = 10
+ncols = 3
 nrows = int(np.ceil(1. * n / ncols))
-
-fig = plt.figure()
+ 
+fig = plt.figure(figsize=(ncols*5, nrows*4))
 for i, (name, color) in enumerate(sorted_colors):
     ax = fig.add_subplot(nrows, ncols, i + 1,
-                         axisbg=color,
                          xticks=[], yticks=[])
-    title = ax.set_title(name)
-    title.set_size('xx-small')
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
-    ax.spines['left'].set_visible(False)
+    ax.text(0.55, 0.5, name, fontsize=12,
+                horizontalalignment='left',
+                verticalalignment='center')
+    
+    ax.hlines(0.5, 0, 0.5, color='black', linewidth=9)
+    ax.hlines(0.5, 0, 0.5, color=color, linewidth=8)
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    ax.set_axis_off()
 
-plt.tight_layout()
+fig.subplots_adjust(left=0.01, right=0.99,
+                    top=0.99, bottom=0.01,
+                    hspace=1, wspace=0.1)
 plt.show()
