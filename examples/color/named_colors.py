29d8e1eff2a740c5ea03f4d35ba42ed9fb213b3d
Federico Ariza <ariza.federico@gmail.com>
2014-02-12 10:40:18 -0500

    docstring and pep8

--- examples/color/named_colors.py
+++ examples/color/named_colors.py
@@ -1,44 +1,50 @@
+"""
+Visualization of named colors.
+
+Simple plot example with the named colors and its visual representation.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import cbook
-from  matplotlib import colors
- 
+from matplotlib import colors
+
 colors_ = colors.cnames.items()
- 
-#add the single letter colors
+
+# Add the single letter colors.
 for name, rgb in colors.ColorConverter.colors.items():
     hex_ = colors.rgb2hex(rgb)
     colors_.append((name, hex_))
- 
-#hex color values
+
+# Transform to hex color values.
 hex_ = [color[1] for color in colors_]
-#rgb equivalent
+# Get the rgb equivalent.
 rgb = [colors.hex2color(color) for color in hex_]
-#hsv equivalent
+# Get the hsv equivalent.
 hsv = [colors.rgb_to_hsv(color) for color in rgb]
- 
-#split the hsv to sort
+
+# Split the hsv values to sort.
 hue = [color[0] for color in hsv]
 sat = [color[1] for color in hsv]
 val = [color[2] for color in hsv]
- 
-#sort by hue, saturation and value
+
+# Sort by hue, saturation and value.
 ind = np.lexsort((val, sat, hue))
 sorted_colors = [colors_[i] for i in ind]
- 
+
 n = len(sorted_colors)
 ncols = 3
 nrows = int(np.ceil(1. * n / ncols))
- 
+
 fig = plt.figure(figsize=(ncols*5, nrows*4))
 for i, (name, color) in enumerate(sorted_colors):
-    ax = fig.add_subplot(nrows, ncols, i + 1,
-                         xticks=[], yticks=[])
+    ax = fig.add_subplot(nrows, ncols, i + 1)
     ax.text(0.55, 0.5, name, fontsize=12,
-                horizontalalignment='left',
-                verticalalignment='center')
-    
-    ax.hlines(0.5, 0, 0.5, color='black', linewidth=9)
+            horizontalalignment='left',
+            verticalalignment='center')
+
+    # Add extra black line a little bit thicker to make
+    # clear colors more visible.
+    ax.hlines(0.5, 0, 0.5, color='black', linewidth=10)
     ax.hlines(0.5, 0, 0.5, color=color, linewidth=8)
     ax.set_xlim(0, 1)
     ax.set_ylim(0, 1)
