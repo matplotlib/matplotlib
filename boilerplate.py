4c1e57efaeb1359e2d268727a45a5e0f09f3b8da
sfroid <sfroid@sfroid.net>
2014-03-25 12:02:36 -0700

    fixing pyplot.py generating boilerplate code to avoid unicode strings

--- boilerplate.py
+++ boilerplate.py
@@ -217,6 +217,13 @@ def boilerplate_gen():
             args.pop(0) # remove 'self' argument
             if defaults is None:
                 defaults = ()
+            else:
+                def_edited = []
+                for val in defaults:
+                    if isinstance(val, unicode):
+                        val = val.encode('ascii', 'ignore')
+                    def_edited.append(val)
+                defaults = tuple(def_edited)
 
             # How to call the wrapped function
             call = []
