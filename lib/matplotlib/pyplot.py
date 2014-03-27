5c0e52ad45f5b8fe66f372db7259b64be35a4bb4
Kimmo Palin <kimmo.palin@helsinki.fi>
2014-03-27 10:09:44 +0200

    Added center and frame arguments for pie-charts

--- lib/matplotlib/pyplot.py
+++ lib/matplotlib/pyplot.py
@@ -3060,7 +3060,7 @@ def phase_spectrum(x, Fs=None, Fc=None, window=None, pad_to=None, sides=None,
 def pie(x, explode=None, labels=None, colors=None, autopct=None,
         pctdistance=0.6, shadow=False, labeldistance=1.1, startangle=None,
         radius=None, counterclock=True, wedgeprops=None, textprops=None,
-        hold=None):
+        hold=None,center=(0,0),frame=False):
     ax = gca()
     # allow callers to override the hold state by passing hold=True|False
     washold = ax.ishold()
@@ -3072,7 +3072,8 @@ def pie(x, explode=None, labels=None, colors=None, autopct=None,
                      autopct=autopct, pctdistance=pctdistance, shadow=shadow,
                      labeldistance=labeldistance, startangle=startangle,
                      radius=radius, counterclock=counterclock,
-                     wedgeprops=wedgeprops, textprops=textprops)
+                     wedgeprops=wedgeprops, textprops=textprops,
+                     center=center,frame=frame)
         draw_if_interactive()
     finally:
         ax.hold(washold)
