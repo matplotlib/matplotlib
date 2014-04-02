d6fc4719ee63be62bc7b8582f4de2ecf489fd48f
Kevin Chan <kvii.chan@gmail.com>
2014-04-01 20:42:18 -0400

    Fixed bug for regression test #1181 in scipy unit tests; ksdensity is now referred to as gaussian_kde and exists as a class in mlab. Fixed list comp position bug and updated examples

--- examples/statistics/violinplot_demo.py
+++ examples/statistics/violinplot_demo.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 # fake data
 fs = 10 # fontsize
-pos = range(5)
+pos = [1,2,4,5,7,8]
 data = [np.random.normal(size=100) for i in pos]
 
 # TODO: future customizability dicts go here
@@ -25,22 +25,28 @@ data = [np.random.normal(size=100) for i in pos]
 
 fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(6,6))
 
-axes[0, 0].violinplot(data, pos, width=0.1)
+axes[0, 0].violinplot(data, pos, points=20, widths=0.1, showmeans=True,
+                      showextrema=True, showmedians=True)
 axes[0, 0].set_title('Custom violinplot 1', fontsize=fs)
 
-axes[0, 1].violinplot(data, pos, width=0.3)
+axes[0, 1].violinplot(data, pos, points=40, widths=0.3, showmeans=True,
+                      showextrema=True, showmedians=True)
 axes[0, 1].set_title('Custom violinplot 2', fontsize=fs)
 
-axes[0, 2].violinplot(data, pos, width=0.5)
+axes[0, 2].violinplot(data, pos, points=60, widths=0.5, showmeans=True,
+                      showextrema=True, showmedians=True)
 axes[0, 2].set_title('Custom violinplot 3', fontsize=fs)
 
-axes[1, 0].violinplot(data, pos, width=0.7)
+axes[1, 0].violinplot(data, pos, points=80, vert=False, widths=0.7,
+                      showmeans=True, showextrema=True, showmedians=True)
 axes[1, 0].set_title('Custom violinplot 4', fontsize=fs)
 
-axes[1, 1].violinplot(data, pos, width=0.9)
+axes[1, 1].violinplot(data, pos, points=100, vert=False, widths=0.9,
+                      showmeans=True, showextrema=True, showmedians=True)
 axes[1, 1].set_title('Custom violinplot 5', fontsize=fs)
 
-axes[1, 2].violinplot(data, pos, width=1.1)
+axes[1, 2].violinplot(data, pos, points=200, vert=False, widths=1.1,
+                      showmeans=True, showextrema=True, showmedians=True)
 axes[1, 2].set_title('Custom violinplot 6', fontsize=fs)
 
 for ax in axes.flatten():
