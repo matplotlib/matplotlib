72b7bed016d9c382224f718353c31c18b757b727
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2014-05-07 18:13:58 +0200

    Reduced figure size for smoother animation

--- examples/animation/rain.py
+++ examples/animation/rain.py
@@ -10,14 +10,18 @@ import matplotlib.pyplot as plt
 from  matplotlib.animation import FuncAnimation
 
 
-fig = plt.figure(figsize=(8,8))
+# Create new figure
+fig = plt.figure(figsize=(7,7))
 fig.patch.set_facecolor('white')
 ax = fig.add_axes([0,0,1,1], frameon=False, aspect=1)
 
+# Create rain data
 P = np.zeros(50, dtype=[('position', float, 2),
                         ('size',     float, 1),
                         ('growth',   float, 1),
                         ('color',    float, 4)])
+
+# Scatter plot is used to animated rain drops
 scat = ax.scatter(P['position'][:,0], P['position'][:,1], P['size'], lw=0.5,
                   animated=True, edgecolors = P['color'], facecolors='None')
 ax.set_xlim(0,1), ax.set_xticks([])
@@ -32,10 +36,13 @@ def update(frame):
     # Make all circles bigger
     P['size'] += P['growth']
 
-    #  Pick new position for oldest rain drop
+    # Pick a new position for oldest rain drop
     P['position'][i] = np.random.uniform(0,1,2)
+    # Reset size
     P['size'][i]     = 5
+    # Reset color
     P['color'][i]    = 0,0,0,1
+    # Choose a random growth factor
     P['growth'][i]   = np.random.uniform(50,200)
 
     # Update scatter plots
