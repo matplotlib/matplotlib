24bc071a2807acc5a749efcf679d73931583e284
Thomas A Caswell <tcaswell@gmail.com>
2014-05-18 21:14:06 -0400

    STY : minor pep8 fix

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -4485,8 +4485,8 @@ class Axes(_AxesBase):
             corner of the axes. If None, default to rc `image.origin`.
 
         extent : scalars (left, right, bottom, top), optional, default: None
-            The location, in data-coordinates, of the lower-left and upper-right
-            corners of the image.  If `None`, the image is positioned such that
+            The location, in data-coordinates, of the lower-left and
+            upper-right  If `None`, the image is positioned such that
             the pixel centers fall on zero-based (row, column) indices.
 
         shape : scalars (columns, rows), optional, default: None
