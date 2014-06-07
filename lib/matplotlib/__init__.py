62571bee762b8ca31c306587aa633ade6e08672e
Benjamin Root <ben.v.root@gmail.com>
2014-06-07 12:55:08 -0400

    Adding mplot3d's tests to the default test suite

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -1360,6 +1360,7 @@ default_test_modules = [
     'matplotlib.tests.test_tightlayout',
     'matplotlib.tests.test_transforms',
     'matplotlib.tests.test_triangulation',
+    'mpl_toolkits.tests.test_mplot3d',
     ]
 
 
