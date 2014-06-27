b9ca456ee96fd125d5fed525e5908d6d7e97ca0a
Benjamin Root <ben.v.root@gmail.com>
2014-06-27 10:10:49 -0400

    removed trailing whitespace

--- examples/user_interfaces/mpl_with_glade_316.py
+++ examples/user_interfaces/mpl_with_glade_316.py
@@ -30,9 +30,9 @@ def main():
 
     canvas = FigureCanvas(figure)  # a Gtk.DrawingArea
     canvas.set_size_request(800,600)
-    sw.add_with_viewport(canvas)    
-    #End of Matplotlib specific code    
-    
+    sw.add_with_viewport(canvas)
+    #End of Matplotlib specific code
+
     window.show_all()
     Gtk.main()
 
