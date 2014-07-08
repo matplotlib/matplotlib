d00536760fcebf9398e573e804562342036273c8
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2014-07-08 09:07:50 -0400

    more pep8

--- examples/style_sheets/plot_bmh.py
+++ examples/style_sheets/plot_bmh.py
@@ -1,15 +1,21 @@
 """
 This example demonstrates the "bmh" style, which is the design used in the 
 Bayesian Methods for Hackers online book.
-
 """
 from numpy.random import beta
 import matplotlib.pyplot as plt
 
 plt.style.use('bmh')
-plt.hist(beta(10,10,size=10000),histtype="stepfilled", bins=25, alpha=0.8, normed=True)
-plt.hist(beta(4,12,size=10000),histtype="stepfilled", bins=25, alpha=0.7, normed=True)
-plt.hist(beta(50,12,size=10000),histtype="stepfilled", bins=25, alpha=0.8, normed=True)
-plt.hist(beta(6,55,size=10000),histtype="stepfilled", bins=25, alpha=0.8, normed=True)
+
+
+def plot_beta_hist(a, b):
+    plt.hist(beta(a, b, size=10000), histtype="stepfilled",
+             bins=25, alpha=0.8, normed=True)
+    return
+
+plot_beta_hist(10, 10)
+plot_beta_hist(4, 12)
+plot_beta_hist(50, 12)
+plot_beta_hist(6, 55)
 
 plt.show()
