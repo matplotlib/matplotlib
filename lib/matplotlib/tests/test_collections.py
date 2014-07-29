14fe5db92cc1e026ee07e208df71451af4010670
Lennart Fricke <lennart@die-frickes.eu>
2014-07-29 09:43:22 +0200

    Make baseline image  for testing single_dash pattern in collections.py seperate image (to allow multiprocess testing...) and remove text

--- lib/matplotlib/tests/test_collections.py
+++ lib/matplotlib/tests/test_collections.py
@@ -325,7 +325,8 @@ def test__EventCollection__set_linestyle():
     splt.set_title('EventCollection: set_linestyle')
 
 
-@image_comparison(baseline_images=['EventCollection_plot__set_linestyle'])
+@image_comparison(baseline_images=['EventCollection_plot__set_ls_dash'],
+                  remove_text=True)
 def test__EventCollection__set_linestyle_single_dash():
     '''
     check to make sure set_linestyle accepts a single dash pattern
