993781393801decfab04e5415053629c2ae8c4c9
Thomas A Caswell <tcaswell@gmail.com>
2014-08-06 10:43:55 -0400

    BUG : guard marker from sym parsing in bxp
    add a guard against `None` returned as `marker`
    by `_process_plot_format(sym)`.

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -3298,7 +3298,8 @@ class Axes(_AxesBase):
                 # process the symbol string
                 # discarded linestyle
                 _, marker, color = _process_plot_format(sym)
-                flierprops['marker'] = marker
+                if marker is not None:
+                    flierprops['marker'] = marker
                 if color is not None:
                     flierprops['color'] = color
                     # assume that if color is passed in the user want
