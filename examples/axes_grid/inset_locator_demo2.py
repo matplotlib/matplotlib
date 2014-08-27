d0d9632a31ea29e6d64bf15514134e8f15d30cc5
Thomas Hisch <t.hisch@gmail.com>
2014-08-27 22:53:36 +0200

    pep8ify: examples/axes_grid
    Signed-off-by: Thomas Hisch <t.hisch@gmail.com>

--- examples/axes_grid/inset_locator_demo2.py
+++ examples/axes_grid/inset_locator_demo2.py
@@ -5,27 +5,28 @@ from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 
 import numpy as np
 
+
 def get_demo_image():
     from matplotlib.cbook import get_sample_data
     import numpy as np
     f = get_sample_data("axes_grid/bivariate_normal.npy", asfileobj=False)
     z = np.load(f)
     # z is a numpy array of 15x15
-    return z, (-3,4,-4,3)
+    return z, (-3, 4, -4, 3)
 
-fig, ax = plt.subplots(figsize=[5,4])
+fig, ax = plt.subplots(figsize=[5, 4])
 
 # prepare the demo image
 Z, extent = get_demo_image()
 Z2 = np.zeros([150, 150], dtype="d")
 ny, nx = Z.shape
-Z2[30:30+ny, 30:30+nx] = Z
+Z2[30:30 + ny, 30:30 + nx] = Z
 
 # extent = [-3, 4, -4, 3]
 ax.imshow(Z2, extent=extent, interpolation="nearest",
           origin="lower")
 
-axins = zoomed_inset_axes(ax, 6, loc=1) # zoom = 6
+axins = zoomed_inset_axes(ax, 6, loc=1)  # zoom = 6
 axins.imshow(Z2, extent=extent, interpolation="nearest",
              origin="lower")
 
@@ -43,4 +44,3 @@ mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
 
 plt.draw()
 plt.show()
-
