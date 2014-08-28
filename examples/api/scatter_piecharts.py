791788b4503b365e73fd53eaa1d97f24e07da710
Thomas Hisch <t.hisch@gmail.com>
2014-08-28 09:58:59 +0200

    fix awkward line break induced by autopep8

--- examples/api/scatter_piecharts.py
+++ examples/api/scatter_piecharts.py
@@ -29,14 +29,12 @@ xy2 = list(zip(x, y))
 
 x = [0] + np.cos(np.linspace(2*math.pi*r2, 2*math.pi, 10)).tolist()
 y = [0] + np.sin(np.linspace(2*math.pi*r2, 2*math.pi, 10)).tolist()
-xy3 = list(zip(x,y))
+xy3 = list(zip(x, y))
 
 
 fig, ax = plt.subplots()
-ax.scatter(np.arange(3), np.arange(3), marker=(
-    xy1, 0), s=sizes, facecolor='blue')
-ax.scatter(np.arange(3), np.arange(3), marker=(
-    xy2, 0), s=sizes, facecolor='green')
-ax.scatter(np.arange(3), np.arange(3), marker=(
-    xy3, 0), s=sizes, facecolor='red')
+d = np.arange(3)
+ax.scatter(d, d, marker=(xy1, 0), s=sizes, facecolor='blue')
+ax.scatter(d, d, marker=(xy2, 0), s=sizes, facecolor='green')
+ax.scatter(d, d, marker=(xy3, 0), s=sizes, facecolor='red')
 plt.show()
