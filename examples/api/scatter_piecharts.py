Fix scaling of custom markers
See also #3441.

examples/api/scatter_piecharts.py
@@ -21,19 +21,24 @@ sizes = [60,80,120]
 x = [0] + np.cos(np.linspace(0, 2*math.pi*r1, 10)).tolist()
 y = [0] + np.sin(np.linspace(0, 2*math.pi*r1, 10)).tolist()
 xy1 = list(zip(x,y))
+s1 = max(max(x),max(y))
 
 # ...
 x = [0] + np.cos(np.linspace(2*math.pi*r1, 2*math.pi*r2, 10)).tolist()
 y = [0] + np.sin(np.linspace(2*math.pi*r1, 2*math.pi*r2, 10)).tolist()
 xy2 = list(zip(x,y))
+s2 = max(max(x),max(y))
 
 x = [0] + np.cos(np.linspace(2*math.pi*r2, 2*math.pi, 10)).tolist()
 y = [0] + np.sin(np.linspace(2*math.pi*r2, 2*math.pi, 10)).tolist()
 xy3 = list(zip(x,y))
-
+s3 = max(max(x),max(y))
 
 fig, ax = plt.subplots()
-ax.scatter( np.arange(3), np.arange(3), marker=(xy1,0), s=sizes, facecolor='blue' )
-ax.scatter( np.arange(3), np.arange(3), marker=(xy2,0), s=sizes, facecolor='green' )
-ax.scatter( np.arange(3), np.arange(3), marker=(xy3,0), s=sizes, facecolor='red' )
+ax.scatter( np.arange(3), np.arange(3), marker=(xy1,0),
+            s=[ s1*s1*x for x in sizes ], facecolor='blue' )
+ax.scatter( np.arange(3), np.arange(3), marker=(xy2,0),
+            s=[ s2*s2*x for x in sizes ], facecolor='green' )
+ax.scatter( np.arange(3), np.arange(3), marker=(xy3,0),
+            s=[ s3*s3*x for x in sizes ], facecolor='red' )
 plt.show()