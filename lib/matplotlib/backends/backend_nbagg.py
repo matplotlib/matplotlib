126fb9c3eb820ecf08bca5efb8796eb0a55d9cd6
Thomas A Caswell <tcaswell@gmail.com>
2014-09-03 16:23:20 -0400

    BUG : fix list comprehensions over class members
    Due to scoping fixes in py3k, list comprehensions over class level
    attributes during class definition does not work (see
    http://stackoverflow.com/questions/13905741/accessing-class-variables-from-a-list-comprehension-in-the-class-definition).
    
    Superficially Fixes #3436. There seem to be other issues

--- lib/matplotlib/backends/backend_nbagg.py
+++ lib/matplotlib/backends/backend_nbagg.py
@@ -63,25 +63,25 @@ def connection_info():
                   str(len(pylab_helpers.Gcf._activeQue)))
     return '\n'.join(result)
 
+# Note: Version 3.2 icons, not the later 4.0 ones.
+# http://fontawesome.io/3.2.1/icons/
+_FONT_AWESOME_CLASSES = {
+    'home': 'icon-home',
+    'back': 'icon-arrow-left',
+    'forward': 'icon-arrow-right',
+    'zoom_to_rect': 'icon-check-empty',
+    'move': 'icon-move',
+    None: None
+}
 
 class NavigationIPy(NavigationToolbar2WebAgg):
-    # Note: Version 3.2 icons, not the later 4.0 ones.
-    # http://fontawesome.io/3.2.1/icons/
-    _font_awesome_classes = {
-        'home': 'icon-home',
-        'back': 'icon-arrow-left',
-        'forward': 'icon-arrow-right',
-        'zoom_to_rect': 'icon-check-empty',
-        'move': 'icon-move',
-        None: None
-    }
 
     # Use the standard toolbar items + download button
     toolitems = [(text, tooltip_text,
                   _font_awesome_classes[image_file], name_of_method)
                  for text, tooltip_text, image_file, name_of_method
                  in NavigationToolbar2.toolitems
-                 if image_file in _font_awesome_classes]
+                 if image_file in _FONT_AWESOME_CLASSES]
 
 
 class FigureManagerNbAgg(FigureManagerWebAgg):
