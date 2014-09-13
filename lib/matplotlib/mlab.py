06e42a6d7af6a88c935de119fb8cf4f1860b8730
Cimarron Mittelsteadt <cimarronm@gmail.com>
2014-09-13 11:20:05 -0700

    STY: No line split for URL

--- lib/matplotlib/mlab.py
+++ lib/matplotlib/mlab.py
@@ -2852,13 +2852,13 @@ def csv2rec(fname, comments='#', skiprows=0, checkrows=0, delimiter=',',
     any of the data are missing
 
     - *dayfirst*: default is False so that MM-DD-YY has precedence over
-      DD-MM-YY.  See http://labix.org/python-dateutil\
-      #head-b95ce2094d189a89f80f5ae52a05b4ab7b41af47
+      DD-MM-YY.  See
+      http://labix.org/python-dateutil#head-b95ce2094d189a89f80f5ae52a05b4ab7b41af47
       for further information.
 
     - *yearfirst*: default is False so that MM-DD-YY has precedence over
-      YY-MM-DD. See http://labix.org/python-dateutil\
-      #head-b95ce2094d189a89f80f5ae52a05b4ab7b41af47
+      YY-MM-DD. See
+      http://labix.org/python-dateutil#head-b95ce2094d189a89f80f5ae52a05b4ab7b41af47
       for further information.
 
       If no rows are found, *None* is returned -- see
