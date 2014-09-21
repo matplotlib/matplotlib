ef2c9fbb871a528e75c52586f5db03a431debfa5
Jouni K. Seppänen <jks@iki.fi>
2014-09-21 17:46:34 +0300

    Reduce the emitting of object definitions in ps and svg
    The cost calculations are very rough, back-of-the-envelope.
    In the example from #3345 I get significant file size reductions:
    
    	2.3M	before.svg
    	1.8M	after.svg
    	916K	before.ps
    	672K	after.ps

--- lib/matplotlib/backends/backend_svg.py
+++ lib/matplotlib/backends/backend_svg.py
@@ -607,6 +607,23 @@ class RendererSVG(RendererBase):
                              offsets, offsetTrans, facecolors, edgecolors,
                              linewidths, linestyles, antialiaseds, urls,
                              offset_position):
+        # Is the optimization worth it? Rough calculation:
+        # cost of emitting a path in-line is
+        #    (len_path + 5) * uses_per_path
+        # cost of definition+use is
+        #    (len_path + 3) + 9 * uses_per_path
+        len_path = len(paths[0].vertices) if len(paths) > 0 else 0
+        uses_per_path = self._iter_collection_uses_per_path(
+            paths, all_transforms, offsets, facecolors, edgecolors)
+        should_do_optimization = \
+            len_path + 9 * uses_per_path + 3 < (len_path + 5) * uses_per_path
+        if not should_do_optimization:
+            return RendererBase.draw_path_collection(
+                self, gc, master_transform, paths, all_transforms,
+                offsets, offsetTrans, facecolors, edgecolors,
+                linewidths, linestyles, antialiaseds, urls,
+                offset_position)
+
         writer = self.writer
         path_codes = []
         writer.start('defs')
