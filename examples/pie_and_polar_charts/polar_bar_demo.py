26f44f862361c8bba3c89a455175630e27d8a8e5
Matthew Conway <matthew.f.conway@gmail.com>
2014-09-27 11:30:01 -0400

    functionalize polar_bar_demo

--- examples/pie_and_polar_charts/polar_bar_demo.py
+++ examples/pie_and_polar_charts/polar_bar_demo.py
@@ -6,16 +6,22 @@ import matplotlib.pyplot as plt
 
 
 N = 20
-theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False)
-radii = 10 * np.random.rand(N)
-width = np.pi / 4 * np.random.rand(N)
+ex_theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False)
+ex_radii = 10 * np.random.rand(N)
+ex_width = np.pi / 4 * np.random.rand(N)
 
-ax = plt.subplot(111, polar=True)
-bars = ax.bar(theta, radii, width=width, bottom=0.0)
+
+def polar_bar_demo(theta,radii,width):
+
+    ax = plt.subplot(111, polar=True)
+    bars = ax.bar(theta, radii, width=width, bottom=0.0)
 
 # Use custom colors and opacity
-for r, bar in zip(radii, bars):
-    bar.set_facecolor(plt.cm.jet(r / 10.))
-    bar.set_alpha(0.5)
+    for r, bar in zip(radii, bars):
+        bar.set_facecolor(plt.cm.jet(r / 10.))
+        bar.set_alpha(0.5)
 
-plt.show()
+    plt.show()
+    return ax
+
+polar_bar_demo(ex_theta,ex_radii,ex_width)
