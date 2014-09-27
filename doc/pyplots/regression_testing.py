738e7db9ae3598a16f55c55bf69870048f63dff4
swupnil <swupnil@gmail.com>
2014-09-27 12:58:46 -0400

    Create regression_testing.py

---
+++ doc/pyplots/regression_testing.py
@@ -0,0 +1,30 @@
+## numpy is used for creating fake data
+import numpy as np 
+import matplotlib as mpl 
+
+## agg backend is used to create plot as a .png file
+mpl.use('agg')
+
+import matplotlib.pyplot as plt 
+
+## Create data
+np.random.seed(10)
+collectn_1 = np.random.normal(100, 10, 200)
+collectn_2 = np.random.normal(80, 30, 200)
+collectn_3 = np.random.normal(90, 20, 200)
+collectn_4 = np.random.normal(70, 25, 200)
+
+## combine these different collections into a list    
+data_to_plot = [collectn_1, collectn_2, collectn_3, collectn_4]
+
+# Create a figure instance
+fig = plt.figure(1, figsize=(9, 6))
+
+# Create an axes instance
+ax = fig.add_subplot(111)
+
+# Create the boxplot
+bp = ax.boxplot(data_to_plot)
+
+# Save the figure
+#fig.savefig('fig1.png', bbox_inches='tight')
