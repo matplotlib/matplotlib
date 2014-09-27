1198d7bd38a6d23431c58fd4dff2ce7bf23499e8
acombs <acombs@yahoo.com>
2014-09-27 14:22:40 -0400

    pep8 cleanup on scatter_demo.py

--- examples/shapes_and_collections/scatter_demo.py
+++ examples/shapes_and_collections/scatter_demo.py
@@ -4,18 +4,17 @@ Simple demo of a scatter plot.
 import numpy as np
 import matplotlib.pyplot as plt
 
-def scatter_demo(ax, N=50, max_radius=15, alpha=0.5):
-	N = N
-	x = np.random.rand(N)
-	y = np.random.rand(N)
-	colors = np.random.rand(N)
-	area = np.pi * (max_radius * np.random.rand(N))**2 #0 to max_radius point radiuses
 
-	c = ax.scatter(x, y, s=area, c=colors, alpha=alpha)
-	return c
+def scatter_demo(ax, N=50, max_radius=15, alpha=0.5):
+    N = N
+    x = np.random.rand(N)
+    y = np.random.rand(N)
+    colors = np.random.rand(N)
+    area = np.pi * (max_radius * np.random.rand(N))**2  # area 0 to max_radius
+
+    c = ax.scatter(x, y, s=area, c=colors, alpha=alpha)
+    return c
 
 ax = plt.subplot(111)
 scatter_demo(ax)
 plt.show()
-
-
