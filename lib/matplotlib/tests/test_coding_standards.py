9366eb4e8dd44a30aa952f6cd7a52addcdc523be
Thomas A Caswell <tcaswell@gmail.com>
2014-09-30 09:21:57 -0400

    TST : made search for examples smarter
    Will find examples if cwd is anywhere is the
    source tree.

--- lib/matplotlib/tests/test_coding_standards.py
+++ lib/matplotlib/tests/test_coding_standards.py
@@ -259,6 +259,15 @@ def test_pep8_conformance_installed_files():
 def test_pep8_conformance_examples():
     mpldir = os.environ.get('MPL_REPO_DIR', None)
     if mpldir is None:
+        # try and guess!
+        fp = os.getcwd()
+        while len(fp) > 2:
+            if os.path.isdir(os.path.join(fp, 'examples')):
+                mpldir = fp
+                break
+            fp, tail = os.path.split(fp)
+
+    if mpldir is None:
         raise KnownFailureTest("can not find the examples, set env "
                                "MPL_REPO_DIR to point to the top-level path "
                                "of the source tree")
