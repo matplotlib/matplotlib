a2ed47febfc8ea548f8ba9a5ed1f9491a2ce2a5e
Federico Ariza <ariza.federico@gmail.com>
2014-10-17 14:12:51 -0400

    using pydispatch

--- examples/user_interfaces/navigation.py
+++ examples/user_interfaces/navigation.py
@@ -4,7 +4,7 @@ matplotlib.use('GTK3Cairo')
 matplotlib.rcParams['toolbar'] = 'navigation'
 import matplotlib.pyplot as plt
 from matplotlib.backend_tools import ToolBase
-
+from pydispatch import dispatcher
 
 # Create a simple tool to list all the tools
 class ListTools(ToolBase):
@@ -14,7 +14,7 @@ class ListTools(ToolBase):
  
     def trigger(self, event):
         tools = self.navigation.get_tools()
- 
+  
         print ('_' * 80)
         print ("{0:12} {1:45} {2}".format('Name (id)',
                                           'Tool description',
@@ -25,7 +25,7 @@ class ListTools(ToolBase):
             print ("{0:12} {1:45} {2}".format(name,
                                               tools[name]['description'],
                                               keys))
-        print ('_' * 80)
+        print ('_' * 80)  
  
  
 # A simple example of copy canvas
@@ -45,6 +45,9 @@ class CopyToolGTK3(ToolBase):
         clipboard.set_image(pb)
 
 
+
+
+
 fig = plt.figure()
 plt.plot([1, 2, 3])
 
@@ -52,8 +55,9 @@ plt.plot([1, 2, 3])
 fig.canvas.manager.navigation.add_tool('List', ListTools)
 if matplotlib.rcParams['backend'] == 'GTK3Cairo':
     fig.canvas.manager.navigation.add_tool('copy', CopyToolGTK3)
-#  
-# Just for fun, lets remove the forward button
-fig.canvas.manager.navigation.remove_tool('forward')
+ 
+# # Just for fun, lets remove the forward button
+# fig.canvas.manager.navigation.remove_tool('forward')
+
 
 plt.show()
