53b6056b254d8da950fecb17ecdfcfc07ad8330b
Steven Silvester <steven.silvester@ieee.org>
2014-10-19 15:29:09 -0500

    Add to default_test_modules

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -1429,6 +1429,7 @@ default_test_modules = [
     'matplotlib.tests.test_transforms',
     'matplotlib.tests.test_triangulation',
     'mpl_toolkits.tests.test_mplot3d',
+    'matplotlib.tests.test_widgets',
     ]
 
 
