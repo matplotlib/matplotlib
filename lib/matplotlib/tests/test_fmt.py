d1ac2228a8281f00108af19dc8deae7fc65f84f1
Thomas A Caswell <tcaswell@gmail.com>
2014-10-19 13:13:31 -0400

    TST : added test to make sure bad fmts fail
    Needs more test cases.  This obviously can't cover every case
    that fails.

--- lib/matplotlib/tests/test_fmt.py
+++ lib/matplotlib/tests/test_fmt.py
@@ -4,7 +4,7 @@ from matplotlib.testing.decorators import cleanup
 import six
 
 
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_raises
 
 from matplotlib.axes._axes import _process_plot_format
 
@@ -12,6 +12,11 @@ from matplotlib.axes._axes import _process_plot_format
 @cleanup
 def _fmt_parsing_helper(fmt, ref):
     assert_equal(ref, _process_plot_format(fmt))
+
+
+@cleanup
+def _fmt_parsing_invalid_helper(fmt):
+    assert_raises(ValueError, _process_plot_format, fmt)
 
 
 def test_fmt_parsing():
@@ -25,3 +30,10 @@ def test_fmt_parsing():
 
     for fmt, target in test_cases:
         yield _fmt_parsing_helper, fmt, target
+
+
+def test_fmt_parsing_fail():
+    test_strings = ('bb-',
+                    'xog')
+    for fmt in test_strings:
+        yield _fmt_parsing_invalid_helper, fmt
