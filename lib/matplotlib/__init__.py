41e897ed8156b473c9d29fa37cb16dd4a885a872
Thomas A Caswell <tcaswell@gmail.com>
2014-10-22 21:53:27 -0400

    REL : version bump back to 1.4.x

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -107,7 +107,7 @@ import sys
 import distutils.version
 from itertools import chain
 
-__version__ = str('1.4.2')
+__version__ = str('1.4.x')
 __version__numpy__ = str('1.6')  # minimum required numpy version
 
 try:
