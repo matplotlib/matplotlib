0b255a71334915fb5ec30832252b0340dc675d25
Marek Rudnicki <marekrud@gmail.com>
2014-11-13 22:57:04 +0100

    Add legend.facecolor and edgecolor to rcParams.

--- lib/matplotlib/rcsetup.py
+++ lib/matplotlib/rcsetup.py
@@ -661,6 +661,9 @@ defaultParams = {
     # the relative size of legend markers vs. original
     'legend.markerscale': [1.0, validate_float],
     'legend.shadow': [False, validate_bool],
+    'legend.facecolor': ['w', validate_color], # background color; white
+    'legend.edgecolor': ['k', validate_color], # edge color; black
+
 
     ## tick properties
     'xtick.major.size':  [4, validate_float],    # major xtick size in points
