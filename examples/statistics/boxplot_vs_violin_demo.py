6419b0e3ed17fbfed06d65d5b70498c8cb3d9ac3
Thomas Hisch <t.hisch@gmail.com>
2014-11-15 23:10:41 +0100

    fix pep8 violations in the new boxplot examples

--- examples/statistics/boxplot_vs_violin_demo.py
+++ examples/statistics/boxplot_vs_violin_demo.py
@@ -11,32 +11,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig, axes = plt.subplots(nrows=1,ncols=2, figsize=(12,5))
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
 
 # generate some random test data
 all_data = [np.random.normal(0, std, 100) for std in range(6, 10)]
 
 # plot violin plot
 axes[0].violinplot(all_data,
-               showmeans=False,
-               showmedians=True 
-               )
+                   showmeans=False,
+                   showmedians=True)
 axes[0].set_title('violin plot')
 
 # plot box plot
 axes[1].boxplot(all_data)
 axes[1].set_title('box plot')
 
-# adding horizontal grid lines 
+# adding horizontal grid lines
 for ax in axes:
-    ax.yaxis.grid(True) 
-    ax.set_xticks([y+1 for y in range(len(all_data))], )
+    ax.yaxis.grid(True)
+    ax.set_xticks([y+1 for y in range(len(all_data))])
     ax.set_xlabel('xlabel')
     ax.set_ylabel('ylabel')
 
 # add x-tick labels
-plt.setp(axes, xticks=[y+1 for y in range(len(all_data))], 
-        xticklabels=['x1', 'x2', 'x3', 'x4'],
-        )    
-    
+plt.setp(axes, xticks=[y+1 for y in range(len(all_data))],
+         xticklabels=['x1', 'x2', 'x3', 'x4'])
 plt.show()
