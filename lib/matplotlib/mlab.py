edd2a872321004b3e73d319fb3f72ad8e9213469
Thomas A Caswell <tcaswell@gmail.com>
2014-11-26 15:34:02 -0500

    Merge pull request #3845 from mbyt/out_of_memory_access_as_strided
    BUG: ensure integer shape, strides for as_strided

--- lib/matplotlib/mlab.py
+++ lib/matplotlib/mlab.py
@@ -590,6 +590,11 @@ def stride_windows(x, n, noverlap=None, axis=0):
     if n > x.size:
         raise ValueError('n cannot be greater than the length of x')
 
+    # np.lib.stride_tricks.as_strided easily leads to memory corruption for
+    # non integer shape and strides, i.e. noverlap or n. See #3845.
+    noverlap = int(noverlap)
+    n = int(n)
+
     step = n - noverlap
     if axis == 0:
         shape = (n, (x.shape[-1]-noverlap)//step)
@@ -641,6 +646,10 @@ def stride_repeat(x, n, axis=0):
             return np.atleast_2d(x).T
     if n < 1:
         raise ValueError('n cannot be less than 1')
+
+    # np.lib.stride_tricks.as_strided easily leads to memory corruption for
+    # non integer shape and strides, i.e. n. See #3845.
+    n = int(n)
 
     if axis == 0:
         shape = (n, x.size)
