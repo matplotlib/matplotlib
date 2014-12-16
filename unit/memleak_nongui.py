8fc1c3018caaf1e83fcef4fff436e4197b9ebe1c
David Haberthür <email@davidhaberthuer.ch>
2014-12-16 14:33:51 +0100

    Fixed PEP8 coding style violations of "unit"folder
    `pep8 unit/*.py --ignore=E501` tells us everything is ok.
    
    As part of http://24pullrequests.com

--- unit/memleak_nongui.py
+++ unit/memleak_nongui.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
-import os,matplotlib
+import matplotlib
 matplotlib.use('Agg')
 from matplotlib.figure import Figure
 from matplotlib.cbook import report_memory
+
 
 def plot():
     fig = Figure()
@@ -10,8 +11,9 @@ def plot():
     while True:
         print(i, report_memory(i))
         fig.clf()
-        ax = fig.add_axes([0.1,0.1,0.7,0.7])
-        ax.plot([1,2,3])
+        ax = fig.add_axes([0.1, 0.1, 0.7, 0.7])
+        ax.plot([1, 2, 3])
         i += 1
 
-if __name__ == '__main__': plot()
+if __name__ == '__main__':
+    plot()
