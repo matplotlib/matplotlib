316b1847ead98e9eaba44f7bad39425e321f43e3
itziakos <mail@itziakos.gr>
2015-01-30 15:54:45 +0000

    remove duplicate imports and pyflakes cleanup

--- lib/matplotlib/tests/test_text.py
+++ lib/matplotlib/tests/test_text.py
@@ -1,16 +1,20 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals)
 
 import six
+import warnings
 
 import numpy as np
 from numpy.testing import assert_almost_equal
-import matplotlib
-from matplotlib.testing.decorators import image_comparison, knownfailureif, cleanup
-import matplotlib.pyplot as plt
-import warnings
 from nose import SkipTest
-from nose.tools import with_setup, assert_raises, eq_, ok_
+from nose.tools import assert_raises, eq_
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.testing.decorators import image_comparison, cleanup
+from matplotlib.figure import Figure
+from matplotlib.text import Annotation, Text
+from matplotlib.backends.backend_agg import RendererAgg
 
 
 @image_comparison(baseline_images=['font_styles'])
@@ -24,60 +28,77 @@ def test_font_styles():
         return FontProperties(fname=path)
 
     from matplotlib.font_manager import FontProperties, findfont
-    warnings.filterwarnings('ignore', ('findfont: Font family \[u?\'Foo\'\] '+
-                            'not found. Falling back to .'),
-                            UserWarning,
-                            module='matplotlib.font_manager')
-    fig = plt.figure()
+    warnings.filterwarnings(
+        'ignore',
+        ('findfont: Font family \[u?\'Foo\'\] not found. Falling back to .'),
+        UserWarning,
+        module='matplotlib.font_manager')
+
+    plt.figure()
     ax = plt.subplot(1, 1, 1)
 
-    normalFont = find_matplotlib_font(family="sans-serif",
-                                       style="normal",
-                                       variant="normal",
-                                       size=14,
-                                      )
-    ax.annotate("Normal Font", (0.1, 0.1), xycoords='axes fraction',
-                  fontproperties=normalFont)
+    normalFont = find_matplotlib_font(
+        family="sans-serif",
+        style="normal",
+        variant="normal",
+        size=14)
+    ax.annotate(
+        "Normal Font",
+        (0.1, 0.1),
+        xycoords='axes fraction',
+        fontproperties=normalFont)
 
-    boldFont = find_matplotlib_font(family="Foo",
-                                     style="normal",
-                                     variant="normal",
-                                     weight="bold",
-                                     stretch=500,
-                                     size=14,
-                                     )
-    ax.annotate("Bold Font", (0.1, 0.2), xycoords='axes fraction',
-                  fontproperties=boldFont)
+    boldFont = find_matplotlib_font(
+        family="Foo",
+        style="normal",
+        variant="normal",
+        weight="bold",
+        stretch=500,
+        size=14)
+    ax.annotate(
+        "Bold Font",
+        (0.1, 0.2),
+        xycoords='axes fraction',
+        fontproperties=boldFont)
 
-    boldItemFont = find_matplotlib_font(family="sans serif",
-                                         style="italic",
-                                         variant="normal",
-                                         weight=750,
-                                         stretch=500,
-                                         size=14,
-                                         )
-    ax.annotate("Bold Italic Font", (0.1, 0.3), xycoords='axes fraction',
-                  fontproperties=boldItemFont)
+    boldItemFont = find_matplotlib_font(
+        family="sans serif",
+        style="italic",
+        variant="normal",
+        weight=750,
+        stretch=500,
+        size=14)
+    ax.annotate(
+        "Bold Italic Font",
+        (0.1, 0.3),
+        xycoords='axes fraction',
+        fontproperties=boldItemFont)
 
-    lightFont = find_matplotlib_font(family="sans-serif",
-                                      style="normal",
-                                      variant="normal",
-                                      weight=200,
-                                      stretch=500,
-                                      size=14,
-                                      )
-    ax.annotate("Light Font", (0.1, 0.4), xycoords='axes fraction',
-                  fontproperties=lightFont)
+    lightFont = find_matplotlib_font(
+        family="sans-serif",
+        style="normal",
+        variant="normal",
+        weight=200,
+        stretch=500,
+        size=14)
+    ax.annotate(
+        "Light Font",
+        (0.1, 0.4),
+        xycoords='axes fraction',
+        fontproperties=lightFont)
 
-    condensedFont = find_matplotlib_font(family="sans-serif",
-                                          style="normal",
-                                          variant="normal",
-                                          weight=500,
-                                          stretch=100,
-                                          size=14,
-                                          )
-    ax.annotate("Condensed Font", (0.1, 0.5), xycoords='axes fraction',
-                  fontproperties=condensedFont)
+    condensedFont = find_matplotlib_font(
+        family="sans-serif",
+        style="normal",
+        variant="normal",
+        weight=500,
+        stretch=100,
+        size=14)
+    ax.annotate(
+        "Condensed Font",
+        (0.1, 0.5),
+        xycoords='axes fraction',
+        fontproperties=condensedFont)
 
     ax.set_xticks([])
     ax.set_yticks([])
@@ -85,18 +106,20 @@ def test_font_styles():
 
 @image_comparison(baseline_images=['multiline'])
 def test_multiline():
-    fig = plt.figure()
+    plt.figure()
     ax = plt.subplot(1, 1, 1)
     ax.set_title("multiline\ntext alignment")
 
-    plt.text(0.2, 0.5, "TpTpTp\n$M$\nTpTpTp", size=20,
-             ha="center", va="top")
+    plt.text(
+        0.2, 0.5, "TpTpTp\n$M$\nTpTpTp", size=20, ha="center", va="top")
 
-    plt.text(0.5, 0.5, "TpTpTp\n$M^{M^{M^{M}}}$\nTpTpTp", size=20,
-             ha="center", va="top")
+    plt.text(
+        0.5, 0.5, "TpTpTp\n$M^{M^{M^{M}}}$\nTpTpTp", size=20,
+        ha="center", va="top")
 
-    plt.text(0.8, 0.5, "TpTpTp\n$M_{q_{q_{q}}}$\nTpTpTp", size=20,
-             ha="center", va="top")
+    plt.text(
+        0.8, 0.5, "TpTpTp\n$M_{q_{q_{q}}}$\nTpTpTp", size=20,
+        ha="center", va="top")
 
     plt.xlim(0, 1)
     plt.ylim(0, 0.8)
@@ -137,15 +160,15 @@ def test_contains():
     fig = plt.figure()
     ax = plt.axes()
 
-    mevent = mbackend.MouseEvent('button_press_event', fig.canvas, 0.5,
-                                 0.5, 1, None)
+    mevent = mbackend.MouseEvent(
+        'button_press_event', fig.canvas, 0.5, 0.5, 1, None)
 
     xs = np.linspace(0.25, 0.75, 30)
     ys = np.linspace(0.25, 0.75, 30)
     xs, ys = np.meshgrid(xs, ys)
 
-    txt = plt.text(0.48, 0.52, 'hello world', ha='center', fontsize=30,
-                   rotation=30)
+    txt = plt.text(
+        0.48, 0.52, 'hello world', ha='center', fontsize=30, rotation=30)
     # uncomment to draw the text's bounding box
     # txt.set_bbox(dict(edgecolor='black', facecolor='none'))
 
@@ -155,9 +178,7 @@ def test_contains():
 
     for x, y in zip(xs.flat, ys.flat):
         mevent.x, mevent.y = plt.gca().transAxes.transform_point([x, y])
-
         contains, _ = txt.contains(mevent)
-
         color = 'yellow' if contains else 'red'
 
         # capture the viewLim, plot a point, and reset the viewLim
@@ -169,7 +190,7 @@ def test_contains():
 @image_comparison(baseline_images=['titles'])
 def test_titles():
     # left and right side titles
-    fig = plt.figure()
+    plt.figure()
     ax = plt.subplot(1, 1, 1)
     ax.set_title("left title", loc="left")
     ax.set_title("right title", loc="right")
@@ -179,15 +200,17 @@ def test_titles():
 
 @image_comparison(baseline_images=['text_alignment'])
 def test_alignment():
-    fig = plt.figure()
+    plt.figure()
     ax = plt.subplot(1, 1, 1)
 
     x = 0.1
     for rotation in (0, 30):
         for alignment in ('top', 'bottom', 'baseline', 'center'):
-            ax.text(x, 0.5, alignment + " Tj", va=alignment, rotation=rotation,
-                    bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.5))
-            ax.text(x, 1.0, r'$\sum_{i=0}^{j}$', va=alignment, rotation=rotation)
+            ax.text(
+                x, 0.5, alignment + " Tj", va=alignment, rotation=rotation,
+                bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.5))
+            ax.text(
+                x, 1.0, r'$\sum_{i=0}^{j}$', va=alignment, rotation=rotation)
             x += 0.1
 
     ax.plot([0, 1], [0.5, 0.5])
@@ -202,8 +225,8 @@ def test_alignment():
 @image_comparison(baseline_images=['axes_titles'], extensions=['png'])
 def test_axes_titles():
     # Related to issue #3327
-    fig = plt.figure()
-    ax = plt.subplot(1,1,1)
+    plt.figure()
+    ax = plt.subplot(1, 1, 1)
     ax.set_title('center', loc='center', fontsize=20, fontweight=700)
     ax.set_title('left', loc='left', fontsize=12, fontweight=400)
     ax.set_title('right', loc='right', fontsize=12, fontweight=400)
@@ -214,7 +237,8 @@ def test_set_position():
     fig, ax = plt.subplots()
 
     # test set_position
-    ann = ax.annotate('test', (0, 0), xytext=(0, 0), textcoords='figure pixels')
+    ann = ax.annotate(
+        'test', (0, 0), xytext=(0, 0), textcoords='figure pixels')
     plt.draw()
 
     init_pos = ann.get_window_extent(fig.canvas.renderer)
@@ -227,7 +251,8 @@ def test_set_position():
         assert a + shift_val == b
 
     # test xyann
-    ann = ax.annotate('test', (0, 0), xytext=(0, 0), textcoords='figure pixels')
+    ann = ax.annotate(
+        'test', (0, 0), xytext=(0, 0), textcoords='figure pixels')
     plt.draw()
 
     init_pos = ann.get_window_extent(fig.canvas.renderer)
@@ -288,13 +313,8 @@ def test_bbox_clipping():
 
 
 def test_text_annotation_get_window_extent():
-    from matplotlib.figure import Figure
-    from matplotlib.text import Annotation, Text
-    from matplotlib.backends.backend_agg import RendererAgg
-
     figure = Figure(dpi=100)
     renderer = RendererAgg(200, 200, 100)
-
 
     # Only text annotation
     annotation = Annotation('test', xy=(0, 0))
@@ -324,10 +344,6 @@ def test_text_annotation_get_window_extent():
 
 
 def test_text_with_arrow_annotation_get_window_extent():
-    from matplotlib.figure import Figure
-    from matplotlib.text import Annotation, Text
-    from matplotlib.backends.backend_agg import RendererAgg
-
     figure = Figure(dpi=100)
     renderer = RendererAgg(600, 600, 100)
     headwidth = 21
@@ -354,10 +370,6 @@ def test_text_with_arrow_annotation_get_window_extent():
 
 
 def test_arrow_annotation_get_window_extent():
-    from matplotlib.figure import Figure
-    from matplotlib.text import Annotation
-    from matplotlib.backends.backend_agg import RendererAgg
-
     figure = Figure(dpi=100)
     figure.set_figwidth(2.0)
     figure.set_figheight(2.0)
@@ -381,10 +393,6 @@ def test_arrow_annotation_get_window_extent():
 
 
 def test_empty_annotation_get_window_extent():
-    from matplotlib.figure import Figure
-    from matplotlib.text import Annotation
-    from matplotlib.backends.backend_agg import RendererAgg
-
     figure = Figure(dpi=100)
     figure.set_figwidth(2.0)
     figure.set_figheight(2.0)
