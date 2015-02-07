e532e97f3dd648f71ddff2b458128d36b0061846
Kangwon Lee <kangwon.lee@kpu.ac.kr>
2015-02-07 10:07:24 -0800

    added pprint();
    setUp() : reload setupext to make sure newest code is under test
    
    renamed test_check_None() -> test_check() : expected return different
    
    added test_get_extension() : calls FreeType get_extension() and pprint include_dirs

--- test_4062.py
+++ test_4062.py
@@ -1,13 +1,22 @@
 import setupext
 import unittest
+from pprint import pprint
+reload(setupext)
 
 class testFreetypeRobustInstallation (unittest.TestCase):
     def setUp(self):
         self.ft = setupext.FreeType()
 
-    def test_check_None(self):
+    def test_check(self):
         self.assertEqual(r"version 2.5.5", self.ft.check())
-        
+
+    def test_get_extension(self):
+        '''test FreeType get_extension() for returnedn .include_dirs'''
+        print "test_get_extension ".ljust(60, '+')
+        ext = self.ft.get_extension()
+        print "ext.include_dirs ".ljust(30, '+')
+        pprint (ext.include_dirs)
+
     def test_make_extension(self):
         ext = setupext.make_extension('test', [])
         print "test_make_extension ".ljust(60, '*')
