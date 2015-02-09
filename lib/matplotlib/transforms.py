matplotlib/matplotlib

commit 91725d8665975de9a26d631445ab1448b0391e1a

use _fast_from_codes_and_verts in transform code

lib/matplotlib/transforms.py
@@ -1380,8 +1380,9 @@ class Transform(TransformNode):
         ``transform_path(path)`` is equivalent to
         ``transform_path_affine(transform_path_non_affine(values))``.
         """
-        return Path(self.transform_non_affine(path.vertices), path.codes,
-                    path._interpolation_steps)
+        x = self.transform_non_affine(path.vertices)
+        return Path._fast_from_codes_and_verts(x, path.codes,
+                {'interpolation_steps': path._interpolation_steps})
 
     def transform_angles(self, angles, pts, radians=False, pushoff=1e-5):
         """
@@ -2587,8 +2588,8 @@ class TransformedPath(TransformNode):
             self._transformed_path = \
                 self._transform.transform_path_non_affine(self._path)
             self._transformed_points = \
-                Path(self._transform.transform_non_affine(self._path.vertices),
-                     None, self._path._interpolation_steps)
+                Path._fast_from_codes_and_verts(self._transform.transform_non_affine(self._path.vertices),
+                        None, {'interpolation_steps': self._path._interpolation_steps})
         self._invalid = 0
 
     def get_transformed_points_and_affine(self):

commit 34a85ed3e7702eb29b9644136d834d97545acf5d

Fix #4074: Bug introduced in 91725d8

lib/matplotlib/transforms.py
@@ -1422,7 +1422,8 @@ class Transform(TransformNode):
         """
         x = self.transform_non_affine(path.vertices)
         return Path._fast_from_codes_and_verts(x, path.codes,
-                {'interpolation_steps': path._interpolation_steps})
+                {'interpolation_steps': path._interpolation_steps,
+                 'should_simplify': path.should_simplify})
 
     def transform_angles(self, angles, pts, radians=False, pushoff=1e-5):
         """
@@ -2628,8 +2629,11 @@ class TransformedPath(TransformNode):
             self._transformed_path = \
                 self._transform.transform_path_non_affine(self._path)
             self._transformed_points = \
-                Path._fast_from_codes_and_verts(self._transform.transform_non_affine(self._path.vertices),
-                        None, {'interpolation_steps': self._path._interpolation_steps})
+                Path._fast_from_codes_and_verts(
+                    self._transform.transform_non_affine(self._path.vertices),
+                    None,
+                    {'interpolation_steps': self._path._interpolation_steps,
+                     'should_simplify': self._path.should_simplify})
         self._invalid = 0
 
     def get_transformed_points_and_affine(self):