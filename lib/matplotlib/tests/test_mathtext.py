2a5923a435ae2d6cf7beb33d3154367e34d6bd30
Michael Droettboom <mdboom@gmail.com>
2015-03-03 13:26:36 -0500

    Add test

--- lib/matplotlib/tests/test_mathtext.py
+++ lib/matplotlib/tests/test_mathtext.py
@@ -92,7 +92,8 @@ math_tests = [
     r"$\left(2 \, a=b\right)$", # Sage bug #8125
     r"$? ! &$", # github issue #466
     r'$\operatorname{cos} x$', # github issue #553
-    r'$\sum _{\genfrac{}{}{0}{}{0\leq i\leq m}{0<j<n}}P\left(i,j\right)$'
+    r'$\sum _{\genfrac{}{}{0}{}{0\leq i\leq m}{0<j<n}}P\left(i,j\right)$',
+    r'$\left|\minus\right\rangle$' # github issue #3374
 ]
 
 digits = "0123456789"
