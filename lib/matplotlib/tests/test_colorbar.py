3b63c19d6b51353ffaab4305f624be0e7677f95e
Michael Droettboom <mdboom@gmail.com>
2015-03-04 14:55:10 -0500

    Disable miter limit in SVG, PS and Agg backends

--- lib/matplotlib/tests/test_colorbar.py
+++ lib/matplotlib/tests/test_colorbar.py
@@ -6,6 +6,7 @@ import six
 import numpy as np
 from numpy import ma
 import matplotlib
+from matplotlib import rc_context
 from matplotlib.testing.decorators import image_comparison, cleanup
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
@@ -258,27 +259,27 @@ def test_colorbarbase():
     baseline_images=['colorbar_closed_patch'],
     remove_text=True)
 def test_colorbar_closed_patch():
-    fig = plt.figure(figsize=(8,6))
+    fig = plt.figure(figsize=(8, 6))
     ax1 = fig.add_axes([0.05, 0.85, 0.9, 0.1])
     ax2 = fig.add_axes([0.1, 0.65, 0.75, 0.1])
     ax3 = fig.add_axes([0.05, 0.45, 0.9, 0.1])
     ax4 = fig.add_axes([0.05, 0.25, 0.9, 0.1])
     ax5 = fig.add_axes([0.05, 0.05, 0.9, 0.1])
 
-    cmap = cm.jet
-    cmap.set_under('w')
-    cmap.set_over('w')
+    cmap = get_cmap("RdBu", lut=5)
 
-    im = ax1.pcolormesh(np.linspace(0,10,16).reshape((4,4)))
+    im = ax1.pcolormesh(np.linspace(0, 10, 16).reshape((4, 4)), cmap=cmap)
+    values = np.linspace(0, 10, 5)
 
-    plt.colorbar(im,cax=ax2,cmap=cmap,orientation='horizontal',
-                 extend='both',extendfrac=0.5)
-    plt.colorbar(im,cax=ax3,cmap=cmap,orientation='horizontal',
-                 extend='both',)
-    plt.colorbar(im,cax=ax4,cmap=cmap,orientation='horizontal',
-                 extend='both',extendrect=True)
-    plt.colorbar(im,cax=ax5,cmap=cmap,orientation='horizontal',
-                 extend='neither')
+    with rc_context({'axes.linewidth': 16}):
+        plt.colorbar(im, cax=ax2, cmap=cmap, orientation='horizontal',
+                     extend='both', extendfrac=0.5, values=values)
+        plt.colorbar(im, cax=ax3, cmap=cmap, orientation='horizontal',
+                     extend='both', values=values)
+        plt.colorbar(im, cax=ax4, cmap=cmap, orientation='horizontal',
+                     extend='both', extendrect=True, values=values)
+        plt.colorbar(im, cax=ax5, cmap=cmap, orientation='horizontal',
+                     extend='neither', values=values)
 
 
 if __name__ == '__main__':
