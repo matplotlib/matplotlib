dc347240fb0f4a72ddd4367caf04d3740381b1b2
Michael Droettboom <mdboom@gmail.com>
2015-03-25 08:41:22 -0400

    Fix Angstrom issues.
    Add support for \mathring.
    Make \AA symbol match LaTeX.

--- lib/matplotlib/tests/test_mathtext.py
+++ lib/matplotlib/tests/test_mathtext.py
@@ -93,7 +93,8 @@ math_tests = [
     r"$? ! &$", # github issue #466
     r'$\operatorname{cos} x$', # github issue #553
     r'$\sum _{\genfrac{}{}{0}{}{0\leq i\leq m}{0<j<n}}P\left(i,j\right)$',
-    r"$\left\Vert a \right\Vert \left\vert b \right\vert \left| a \right| \left\| b\right\| \Vert a \Vert \vert b \vert$"
+    r"$\left\Vert a \right\Vert \left\vert b \right\vert \left| a \right| \left\| b\right\| \Vert a \Vert \vert b \vert$",
+    r'$\mathring{A}  \stackrel{\circ}{A}  \AA$'
 ]
 
 digits = "0123456789"
