592818a175edf46aa497bec9b42a4333dd449467
Thomas A Caswell <tcaswell@gmail.com>
2015-03-24 22:50:07 -0400

    Merge pull request #4264 from olanmatt/fix_4068
    BUG : Fix for unpickling polar plot
    
    closes #4068

--- lib/matplotlib/tests/test_pickle.py
+++ lib/matplotlib/tests/test_pickle.py
@@ -127,6 +127,7 @@ def test_simple():
     pickle.dump(fig, BytesIO(), pickle.HIGHEST_PROTOCOL)
 
 
+@cleanup
 @image_comparison(baseline_images=['multi_pickle'],
                   extensions=['png'], remove_text=True)
 def test_complete():
@@ -195,6 +196,7 @@ def test_complete():
     assert_equal(fig.get_label(), 'Figure with a label?')
 
 
+@cleanup
 def test_no_pyplot():
     # tests pickle-ability of a figure not created with pyplot
     from matplotlib.backends.backend_pdf import FigureCanvasPdf as fc
@@ -207,12 +209,14 @@ def test_no_pyplot():
     pickle.dump(fig, BytesIO(), pickle.HIGHEST_PROTOCOL)
 
 
+@cleanup
 def test_renderer():
     from matplotlib.backends.backend_agg import RendererAgg
     renderer = RendererAgg(10, 20, 30)
     pickle.dump(renderer, BytesIO())
 
 
+@cleanup
 def test_image():
     # Prior to v1.4.0 the Image would cache data which was not picklable
     # once it had been drawn.
@@ -225,6 +229,7 @@ def test_image():
     pickle.dump(fig, BytesIO())
 
 
+@cleanup
 def test_grid():
     from matplotlib.backends.backend_agg import new_figure_manager
     manager = new_figure_manager(1000)
@@ -236,6 +241,16 @@ def test_grid():
     manager.canvas.draw()
 
     pickle.dump(ax, BytesIO())
+
+
+@cleanup
+def test_polar():
+    ax = plt.subplot(111, polar=True)
+    fig = plt.gcf()
+    result = BytesIO()
+    pf = pickle.dumps(fig)
+    pickle.loads(pf)
+    plt.draw()
 
 
 if __name__ == '__main__':
