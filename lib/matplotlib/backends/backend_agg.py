0c51d22c728c6b6c23849aebb9db932d683d93f8
Thomas A Caswell <tcaswell@gmail.com>
2015-05-05 10:49:36 -0400

    FIX : round image size rather than clip
    Round images sizes to the nearest integer rather than clipping when
    passing down to the c++ layer.  This is mostly a problem when using
    imsave/figimage to render an image 1:1 and is a result of float
    precision issues when computing number of pixels from inches/dpi.
    
    Closes #4280

--- lib/matplotlib/backends/backend_agg.py
+++ lib/matplotlib/backends/backend_agg.py
@@ -95,7 +95,9 @@ class RendererAgg(RendererBase):
                 'RendererAgg.__init__ width=%s, height=%s' % (width, height),
                 'debug-annoying')
 
-        self._renderer = _RendererAgg(int(width), int(height), dpi, debug=False)
+        self._renderer = _RendererAgg(int(round(width)), int(round(height)),
+                                      dpi, debug=False)
+
         self._filter_renderers = []
 
         if __debug__: verbose.report('RendererAgg.__init__ _RendererAgg done',
