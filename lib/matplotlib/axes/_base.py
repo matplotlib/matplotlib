9706cc09335676bba7d40a56826ae131349f0b7d
Thomas A Caswell <tcaswell@gmail.com>
2015-05-05 17:21:19 -0400

    MNT : remove stale flags from Axes.draw
    The first pass at getting this to work put all of the logic to label
    artists as not stale in the Axes.draw method.  Now that all of the draw
    methods are marked up this is not needed.

--- lib/matplotlib/axes/_base.py
+++ lib/matplotlib/axes/_base.py
@@ -2064,7 +2064,6 @@ class _AxesBase(martist.Artist):
         # will draw the edges
         if self.axison and self._frameon:
             self.patch.draw(renderer)
-            self.patch.stale = False
 
         if _do_composite:
             # make a composite image, blending alpha
@@ -2097,18 +2096,15 @@ class _AxesBase(martist.Artist):
                 self.patch.get_transform()))
 
             renderer.draw_image(gc, round(l), round(b), im)
-            im.stale = False
             gc.restore()
 
         if dsu_rasterized:
             for zorder, a in dsu_rasterized:
                 a.draw(renderer)
-                a.stale = False
             renderer.stop_rasterizing()
 
         for zorder, a in dsu:
             a.draw(renderer)
-            a.stale = False
 
         renderer.close_group('axes')
         self._cachedRenderer = renderer
@@ -2125,7 +2121,6 @@ class _AxesBase(martist.Artist):
                    ' caches the render')
             raise AttributeError(msg)
         a.draw(self._cachedRenderer)
-        a.stale = False
 
     def redraw_in_frame(self):
         """
