a182a76207f21c4176e7b1422e19753fcb2927bb
James Pallister <james.pallister@bristol.ac.uk>
2015-05-26 12:05:15 +0100

    Whitespace fixes for PEP8 and the correct baseline image

--- lib/matplotlib/tests/test_backend_pdf.py
+++ lib/matplotlib/tests/test_backend_pdf.py
@@ -108,13 +108,14 @@ def test_composite_image():
         fig.savefig(pdf, format="pdf")
         assert len(pdf._file.images.keys()) == 2
 
+
 @image_comparison(baseline_images=['hatching_legend'],
                   extensions=['pdf'])
 def test_hatching_legend():
     """Test for correct hatching on patches in legend"""
-    fig = plt.figure(figsize=(1,2))
+    fig = plt.figure(figsize=(1, 2))
 
-    a = plt.Rectangle([0,0], 0, 0, facecolor="green", hatch="XXXX")
-    b = plt.Rectangle([0,0], 0, 0, facecolor="blue", hatch="XXXX")
+    a = plt.Rectangle([0, 0], 0, 0, facecolor="green", hatch="XXXX")
+    b = plt.Rectangle([0, 0], 0, 0, facecolor="blue", hatch="XXXX")
 
-    fig.legend([a,b,a,b], ["","","",""])
+    fig.legend([a, b, a, b], ["", "", "", ""])
