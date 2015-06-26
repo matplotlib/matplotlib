ce9c8e7112f4d80ab769ea3d7782cc5c67f81968
OceanWolf <juichenieder-tigger@yahoo.co.uk>
2015-06-26 18:33:09 +0200

    Revert old example and fix new one.

--- examples/user_interfaces/toolmanager_sgskip.py
+++ examples/user_interfaces/toolmanager_sgskip.py
@@ -19,10 +19,8 @@ import matplotlib
 matplotlib.use('GTK3Cairo')
 matplotlib.rcParams['toolbar'] = 'toolmanager'
 import matplotlib.pyplot as plt
-from matplotlib.backend_tools import (ToolBase, ToolToggleBase,
-                                      add_tools_to_container)
+from matplotlib.backend_tools import ToolBase, ToolToggleBase
 from gi.repository import Gtk, Gdk
-from random import uniform
 
 
 class ListTools(ToolBase):
@@ -68,40 +66,12 @@ class GroupHideTool(ToolToggleBase):
         self.set_lines_visibility(False)
 
     def set_lines_visibility(self, state):
+        gr_lines = []
         for ax in self.figure.get_axes():
             for line in ax.get_lines():
                 if line.get_gid() == self.gid:
                     line.set_visible(state)
         self.figure.canvas.draw()
-
-
-class LineTool(ToolBase):
-    description = 'Draw a random line'
-
-    def __init__(self, *args, **kwargs):
-        self.color = kwargs.pop('color')
-        ToolBase.__init__(self, *args, **kwargs)
-
-    def trigger(self, *args, **kwargs):
-        x0, y0, x1, y1 = (uniform(0, 2), uniform(1, 4), uniform(0, 2),
-                         uniform(1, 4))
-        fig = self.figure
-        fig.gca().plot([x0, x1], [y0, y1], color=self.color, gid=self.color)
-        fig.canvas.draw_idle()
-
-
-class DotTool(ToolBase):
-    description = 'Draw a random dot'
-
-    def __init__(self, *args, **kwargs):
-        self.color = kwargs.pop('color')
-        ToolBase.__init__(self, *args, **kwargs)
-
-    def trigger(self, *args, **kwargs):
-        x0, y0 = uniform(0, 2), uniform(1, 4)
-        fig = self.figure
-        fig.gca().plot([x0], [y0], 'o', color=self.color, gid=self.color)
-        fig.canvas.draw_idle()
 
 
 fig = plt.figure()
@@ -110,31 +80,19 @@ plt.plot([2, 3, 4], gid='unknown')
 plt.plot([3, 2, 1], gid='mygroup')
 
 # Add the custom tools that we created
-manager = fig.canvas.manager
-tool_mgr = manager.toolmanager
-tool_mgr.add_tool('List', ListTools)
-tool_mgr.add_tool('Hide', GroupHideTool, gid='mygroup')
+fig.canvas.manager.toolmanager.add_tool('List', ListTools)
+fig.canvas.manager.toolmanager.add_tool('Hide', GroupHideTool, gid='mygroup')
 
 
 # Add an existing tool to new group `foo`.
 # It can be added as many times as we want
-manager.toolbar.add_tool('zoom', 'foo')
+fig.canvas.manager.toolbar.add_tool('zoom', 'foo')
 
 # Remove the forward button
-tool_mgr.remove_tool('forward')
+fig.canvas.manager.toolmanager.remove_tool('forward')
 
 # To add a custom tool to the toolbar at specific location inside
 # the navigation group
-manager.toolbar.add_tool('Hide', 'navigation', 1)
-
-for i, c in enumerate(['yellowgreen', 'forestgreen']):
-    sidebar = manager.backend.Toolbar(tool_mgr)
-    sidebar.set_flow('vertical')
-    tools = [['shapes', [tool_mgr.add_tool('L%s' % i, LineTool, color=c),
-                         tool_mgr.add_tool('D%s' % i, DotTool, color=c)]],
-             ['hide', [tool_mgr.add_tool('H%s' % i, GroupHideTool, gid=c)]]]
-
-    manager.window.add_element(sidebar, False, 'west')
-    add_tools_to_container(sidebar, tools)
+fig.canvas.manager.toolbar.add_tool('Hide', 'navigation', 1)
 
 plt.show()
