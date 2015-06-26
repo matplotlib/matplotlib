66dd5507b9accf35fa561fc4a9fdaa3e1428762a
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-06-26 09:58:36 +0100

    PEP8 fixes

--- lib/matplotlib/testing/exceptions.py
+++ lib/matplotlib/testing/exceptions.py
@@ -1,10 +1,16 @@
 class KnownFailureTest(Exception):
-    '''Raise this exception to mark a test as a known failing test.'''
-    pass
+    """
+    Raise this exception to mark a test as a known failing test.
+    """
+
 
 class KnownFailureDidNotFailTest(Exception):
-    '''Raise this exception to mark a test should have failed but did not.'''
-    pass
+    """
+    Raise this exception to mark a test should have failed but did not.
+    """
+
 
 class ImageComparisonFailure(AssertionError):
-    '''Raise this exception to mark a test as a comparison between two images.'''
+    """
+    Raise this exception to mark a test as a comparison between two images.
+    """
