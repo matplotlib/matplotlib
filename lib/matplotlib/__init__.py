903b09bae4cf38077bd0147c0f26308b38c7d86a
Eugen Beck <me@curufinwe.org>
2015-07-06 12:14:44 +0200

    _create_tmp_config_dir() "mkdirs" the returned dir
    Fixes #4120

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -537,6 +537,7 @@ def _create_tmp_config_dir():
     """
     import getpass
     import tempfile
+    from matplotlib.cbook import mkdirs
 
     try:
         tempdir = tempfile.gettempdir()
@@ -546,6 +547,11 @@ def _create_tmp_config_dir():
         return None
     tempdir = os.path.join(tempdir, 'matplotlib-%s' % getpass.getuser())
     os.environ['MPLCONFIGDIR'] = tempdir
+
+    try:
+        mkdirs(tempdir)
+    except OSError:
+        pass
 
     return tempdir
 
