100e7e033429d5ec07d311aea5c1f3f133b4d2c3
Eric Ma <ericmajinglong@gmail.com>
2015-07-11 14:29:54 -0500

    MEP12 on tex_unicode_demo.py

--- examples/pylab_examples/tex_unicode_demo.py
+++ examples/pylab_examples/tex_unicode_demo.py
@@ -4,23 +4,27 @@
 This demo is tex_demo.py modified to have unicode. See that file for
 more information.
 """
+
 from __future__ import unicode_literals
-import matplotlib as mpl
-mpl.rcParams['text.usetex'] = True
-mpl.rcParams['text.latex.unicode'] = True
-from numpy import arange, cos, pi
-from matplotlib.pyplot import (figure, axes, plot, xlabel, ylabel, title,
-                               grid, savefig, show)
+import matplotlib
+matplotlib.rcParams['text.usetex'] = True
+matplotlib.rcParams['text.latex.unicode'] = True
 
-figure(1, figsize=(6, 4))
-ax = axes([0.1, 0.1, 0.8, 0.7])
-t = arange(0.0, 1.0 + 0.01, 0.01)
-s = cos(2*2*pi*t) + 2
-plot(t, s)
+import numpy as np
+# from numpy import arange, cos, pi
+# from matplotlib.pyplot import (figure, axes, plot, xlabel, ylabel, title,
+                               # grid, savefig, show)
+import matplotlib.pyplot as plt 
 
-xlabel(r'\textbf{time (s)}')
-ylabel(r'\textit{Velocity (\u00B0/sec)}', fontsize=16)
-title(r"\TeX\ is Number $\displaystyle\sum_{n=1}^\infty\frac{-e^{i\pi}}{2^n}$!",
+plt.figure(1, figsize=(6, 4))
+ax = plt.axes([0.1, 0.1, 0.8, 0.7])
+t = np.arange(0.0, 1.0 + 0.01, 0.01)
+s = np.cos(2*2*np.pi*t) + 2
+plt.plot(t, s)
+
+plt.xlabel(r'\textbf{time (s)}')
+plt.ylabel(r'\textit{Velocity (\u00B0/sec)}', fontsize=16)
+plt.title(r"\TeX\ is Number $\displaystyle\sum_{n=1}^\infty\frac{-e^{i\pi}}{2^n}$!",
       fontsize=16, color='r')
-grid(True)
-show()
+plt.grid(True)
+plt.show()
