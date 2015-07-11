2a1c19d4bfedfc8da9c248489dce112f7d50e859
Eric Ma <ericmajinglong@gmail.com>
2015-07-11 15:04:27 -0500

    MEP12 on step_demo.py

--- examples/pylab_examples/step_demo.py
+++ examples/pylab_examples/step_demo.py
@@ -1,25 +1,25 @@
 import numpy as np
 from numpy import ma
-from matplotlib.pyplot import step, legend, xlim, ylim, show
+import matplotlib.pyplot as plt
 
 x = np.arange(1, 7, 0.4)
 y0 = np.sin(x)
 y = y0.copy() + 2.5
 
-step(x, y, label='pre (default)')
+plt.step(x, y, label='pre (default)')
 
 y -= 0.5
-step(x, y, where='mid', label='mid')
+plt.step(x, y, where='mid', label='mid')
 
 y -= 0.5
-step(x, y, where='post', label='post')
+plt.step(x, y, where='post', label='post')
 
 y = ma.masked_where((y0 > -0.15) & (y0 < 0.15), y - 0.5)
-step(x, y, label='masked (pre)')
+plt.step(x, y, label='masked (pre)')
 
-legend()
+plt.legend()
 
-xlim(0, 7)
-ylim(-0.5, 4)
+plt.xlim(0, 7)
+plt.ylim(-0.5, 4)
 
-show()
+plt.show()
