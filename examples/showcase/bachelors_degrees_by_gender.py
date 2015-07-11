da2842ee20283f4c897bee1593ad40848a0df38a
Randy Olson <rso@randalolson.com>
2015-07-11 11:44:57 -0500

    pep8 enforcement, different figure call

--- examples/showcase/bachelors_degrees_by_gender.py
+++ examples/showcase/bachelors_degrees_by_gender.py
@@ -14,17 +14,16 @@ color_sequence = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
 # You typically want your plot to be ~1.33x wider than tall. This plot
 # is a rare exception because of the number of lines being plotted on it.
 # Common sizes: (10, 7.5) and (12, 9)
-plt.figure(figsize=(12, 14))
+fig, ax = plt.subplots(1, 1, figsize=(12, 14))
 
-# Remove the plot frame lines. They are unnecessary chartjunk.  
-ax = plt.subplot(111)
+# Remove the plot frame lines. They are unnecessary here.
 ax.spines['top'].set_visible(False)
 ax.spines['bottom'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.spines['left'].set_visible(False)
 
-# Ensure that the axis ticks only show up on the bottom and left of the plot.  
-# Ticks on the right and top of the plot are generally unnecessary chartjunk.  
+# Ensure that the axis ticks only show up on the bottom and left of the plot.
+# Ticks on the right and top of the plot are generally unnecessary.
 ax.get_xaxis().tick_bottom()
 ax.get_yaxis().tick_left()
 
@@ -36,18 +35,20 @@ plt.ylim(-0.25, 90)
 # Make sure your axis ticks are large enough to be easily read.
 # You don't want your viewers squinting to read your plot.
 plt.xticks(range(1970, 2011, 10), fontsize=14)
-plt.yticks(range(0, 91, 10), ['{}%'.format(x) for x in range(0, 91, 10)], fontsize=14)
+plt.yticks(range(0, 91, 10), ['{}%'.format(x)
+                              for x in range(0, 91, 10)], fontsize=14)
 
-# Provide tick lines across the plot to help your viewers trace along  
-# the axis ticks. Make sure that the lines are light and small so they  
-# don't obscure the primary data lines.  
-for y in range(10, 91, 10):  
+# Provide tick lines across the plot to help your viewers trace along
+# the axis ticks. Make sure that the lines are light and small so they
+# don't obscure the primary data lines.
+for y in range(10, 91, 10):
     plt.plot(range(1969, 2012), [y] * len(range(1969, 2012)), '--',
-             lw=0.5, color='black', alpha=0.3) 
+             lw=0.5, color='black', alpha=0.3)
 
-# Remove the tick marks; they are unnecessary with the tick lines we just plotted.  
-plt.tick_params(axis='both', which='both', bottom='off', top='off',  
-                labelbottom='on', left='off', right='off', labelleft='on')  
+# Remove the tick marks; they are unnecessary with the tick lines we just
+# plotted.
+plt.tick_params(axis='both', which='both', bottom='off', top='off',
+                labelbottom='on', left='off', right='off', labelleft='on')
 
 # Now that the plot is prepared, it's time to actually plot the data!
 # Note that I plotted the majors in order of the highest % in the final year.
@@ -91,7 +92,7 @@ for rank, column in enumerate(majors):
 # Note that if the title is descriptive enough, it is unnecessary to include
 # axis labels; they are self-evident, in this plot's case.
 plt.title('Percentage of Bachelor\'s degrees conferred to women in '
-         'the U.S.A. by major (1970-2011)\n', fontsize=18, ha='center')
+          'the U.S.A. by major (1970-2011)\n', fontsize=18, ha='center')
 
 # Finally, save the figure as a PNG.
 # You can also save it as a PDF, JPEG, etc.
