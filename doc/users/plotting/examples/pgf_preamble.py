714d18788320325d0bff75184f62d472f67ceb91
Ryan May <rmay31@gmail.com>
2015-07-11 14:12:02 -0500

    Move a few more imports of six to matplotlib.external.

--- doc/users/plotting/examples/pgf_preamble.py
+++ doc/users/plotting/examples/pgf_preamble.py
@@ -2,7 +2,7 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-import six
+from matplotlib.externals import six
 
 import matplotlib as mpl
 mpl.use("pgf")
