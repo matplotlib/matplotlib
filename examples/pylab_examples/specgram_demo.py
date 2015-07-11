cb3efadd489d4b26abe070354b438d66405047c3
Eric Ma <ericmajinglong@gmail.com>
2015-07-11 15:20:58 -0500

    MEP12 on shared-to-spectrum in pylab examples

--- examples/pylab_examples/specgram_demo.py
+++ examples/pylab_examples/specgram_demo.py
@@ -1,17 +1,17 @@
-#!/usr/bin/env python
-from pylab import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 dt = 0.0005
-t = arange(0.0, 20.0, dt)
-s1 = sin(2*pi*100*t)
-s2 = 2*sin(2*pi*400*t)
+t = np.arange(0.0, 20.0, dt)
+s1 = np.sin(2*np.pi*100*t)
+s2 = 2*np.sin(2*np.pi*400*t)
 
 # create a transient "chirp"
-mask = where(logical_and(t > 10, t < 12), 1.0, 0.0)
+mask = np.where(np.logical_and(t > 10, t < 12), 1.0, 0.0)
 s2 = s2 * mask
 
 # add some noise into the mix
-nse = 0.01*randn(len(t))
+nse = 0.01*np.random.random(size=len(t))
 
 x = s1 + s2 + nse  # the signal
 NFFT = 1024       # the length of the windowing segments
@@ -22,9 +22,9 @@ Fs = int(1.0/dt)  # the sampling frequency
 # the power is computed, and im is the matplotlib.image.AxesImage
 # instance
 
-ax1 = subplot(211)
-plot(t, x)
-subplot(212, sharex=ax1)
-Pxx, freqs, bins, im = specgram(x, NFFT=NFFT, Fs=Fs, noverlap=900,
-                                cmap=cm.gist_heat)
-show()
+ax1 = plt.subplot(211)
+plt.plot(t, x)
+plt.subplot(212, sharex=ax1)
+Pxx, freqs, bins, im = plt.specgram(x, NFFT=NFFT, Fs=Fs, noverlap=900,
+                                cmap=plt.cm.gist_heat)
+plt.show()
