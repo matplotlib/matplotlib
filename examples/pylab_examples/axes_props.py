5a5f458aaf850357a00590621aae85fc3d63bd0b
domspad <domspad@umich.edu>
2015-07-16 23:15:50 -0700

    pep8

--- examples/pylab_examples/axes_props.py
+++ examples/pylab_examples/axes_props.py
@@ -4,10 +4,10 @@ You can control the axis tick and grid properties
 """
 
 import matplotlib.pyplot as plt
-import numpy as np 
+import numpy as np
 
 t = np.arange(0.0, 2.0, 0.01)
-s = np.sin(2*pi*t)
+s = np.sin(2*np.pi*t)
 plt.plot(t, s)
 plt.grid(True)
 
