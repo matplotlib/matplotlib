cc350be55e23f581d69a2f00a154cc1e39c85c56
domspad <domspad@umich.edu>
2015-07-16 23:25:40 -0700

    pep8

--- examples/pylab_examples/arctest.py
+++ examples/pylab_examples/arctest.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import matplotlib.pyplot as plt
+import numpy as np
 
 def f(t):
     'a damped exponential'
-    s1 = np.cos(2*pi*t)
+    s1 = np.cos(2*np.pi*t)
     e1 = np.exp(-t)
     return np.multiply(s1, e1)
 
