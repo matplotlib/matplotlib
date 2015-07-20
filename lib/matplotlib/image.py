b252fe77ac1cbcf12ad49a232801a407fa025a44
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-07-20 11:01:53 -0700

    Stupid, don't user double underscore because of mangling

--- lib/matplotlib/image.py
+++ lib/matplotlib/image.py
@@ -35,7 +35,7 @@ from matplotlib.transforms import BboxBase, Bbox, IdentityTransform
 import matplotlib.transforms as mtransforms
 
 # map interpolation strings to module constants
-__interpd = {
+_interpd_ = {
     'none': _image.NEAREST,  # fall back to nearest when not supported
     'nearest': _image.NEAREST,
     'bilinear': _image.BILINEAR,
@@ -57,15 +57,15 @@ __interpd = {
 }
 
 
-interpolations_names = list(six.iterkeys(__interpd))
+interpolations_names = list(six.iterkeys(_interpd_))
 
 class _AxesImageBase(martist.Artist, cm.ScalarMappable):
     zorder = 0
 
     # the 3 following keys seem to be unused now, keep it for backward compat just in case. 
-    _interpd = __interpd
+    _interpd = _interpd_
     # reverse interp dict
-    _interpdr = dict([(v, k) for k, v in six.iteritems(__interpd)])
+    _interpdr = dict([(v, k) for k, v in six.iteritems(_interpd_)])
     iterpnames = interpolations_names
     # <end unused keys>
 
@@ -488,7 +488,7 @@ class _AxesImageBase(martist.Artist, cm.ScalarMappable):
         if s is None:
             s = rcParams['image.interpolation']
         s = s.lower()
-        if s not in __interpd:
+        if s not in _interpd_:
             raise ValueError('Illegal interpolation string')
         self._interpolation = s
         self.stale = True
@@ -622,7 +622,7 @@ class AxesImage(_AxesImageBase):
         numrows, numcols = im.get_size()
         if numrows < 1 or numcols < 1:   # out of range
             return None
-        im.set_interpolation(__interpd[self._interpolation])
+        im.set_interpolation(_interpd_[self._interpolation])
 
         im.set_resample(self._resample)
 
@@ -765,7 +765,7 @@ class NonUniformImage(AxesImage):
         im = _image.pcolor(self._Ax, self._Ay, A,
                            int(height), int(width),
                            (x0, x0+v_width, y0, y0+v_height),
-                           __interpd[self._interpolation])
+                           _interpd_[self._interpolation])
 
         fc = self.axes.patch.get_facecolor()
         bg = mcolors.colorConverter.to_rgba(fc, 0)
@@ -1191,7 +1191,7 @@ class BboxImage(_AxesImageBase):
         # image input dimensions
         im.reset_matrix()
 
-        im.set_interpolation(__interpd[self._interpolation])
+        im.set_interpolation(_interpd_[self._interpolation])
 
         im.set_resample(self._resample)
 
