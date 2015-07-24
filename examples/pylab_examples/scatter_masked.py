9e33f892d4b7d8f035912320ad6536491093cc75
Eric Ma <ericmjl@Erics-MacBook-Pro.local>
2015-07-23 20:38:50 -0400

    mep12 on scatter_masked.py

--- examples/pylab_examples/scatter_masked.py
+++ examples/pylab_examples/scatter_masked.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python
-from pylab import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 N = 100
 r0 = 0.6
-x = 0.9*rand(N)
-y = 0.9*rand(N)
-area = pi*(10 * rand(N))**2  # 0 to 10 point radiuses
-c = sqrt(area)
-r = sqrt(x*x + y*y)
-area1 = ma.masked_where(r < r0, area)
-area2 = ma.masked_where(r >= r0, area)
-scatter(x, y, s=area1, marker='^', c=c, hold='on')
-scatter(x, y, s=area2, marker='o', c=c)
+x = 0.9*np.random.rand(N)
+y = 0.9*np.random.rand(N)
+area = np.pi*(10 * np.random.rand(N))**2  # 0 to 10 point radiuses
+c = np.sqrt(area)
+r = np.sqrt(x*x + y*y)
+area1 = np.ma.masked_where(r < r0, area)
+area2 = np.ma.masked_where(r >= r0, area)
+plt.scatter(x, y, s=area1, marker='^', c=c, hold='on')
+plt.scatter(x, y, s=area2, marker='o', c=c)
 # Show the boundary between the regions:
-theta = arange(0, pi/2, 0.01)
-plot(r0*cos(theta), r0*sin(theta))
+theta = np.arange(0, np.pi/2, 0.01)
+plt.plot(r0*np.cos(theta), r0*np.sin(theta))
 
-show()
+plt.show()
