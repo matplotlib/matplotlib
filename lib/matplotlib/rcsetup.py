eb33e03f15ecfe8487c1077a5d75eaa6f212c0b9
Thomas A Caswell <tcaswell@gmail.com>
2015-07-25 02:48:01 -0400

    ENH: plotting methods can unpack labeled data
    After discussions with Brian Granger, Fernando Perez Peter Wang, Matthew
    Rocklin and Jake VanderPlas this is a proposal for how to deal with
    labeled data in matplotlib.
    
    The approach taken is that if the optional kwarg 'data' is passed in any
    other string args/kwargs to the function are replaced be the result
    `data[k]` if the key exists in `data`, else leave the value as-is.

--- lib/matplotlib/rcsetup.py
+++ lib/matplotlib/rcsetup.py
@@ -1003,7 +1003,7 @@ defaultParams = {
     'xtick.minor.pad':   [4, validate_float],    # distance to label in points
     'xtick.color':       ['k', validate_color],  # color of the xtick labels
     'xtick.minor.visible':   [False, validate_bool],    # visiablility of the x axis minor ticks
-    
+
     # fontsize of the xtick labels
     'xtick.labelsize':   ['medium', validate_fontsize],
     'xtick.direction':   ['in', six.text_type],            # direction of xticks
@@ -1161,7 +1161,8 @@ defaultParams = {
     'animation.convert_path':  ['convert', six.text_type],
      # Additional arguments for mencoder movie writer (using pipes)
 
-    'animation.convert_args':  [[], validate_stringlist]}
+    'animation.convert_args':  [[], validate_stringlist],
+    'unpack_labeled': [True, validate_bool], }
 
 
 if __name__ == '__main__':
