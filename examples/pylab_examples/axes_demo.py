237d43981d785a4d14690cde045de489fe6f2cf9
Eric Ma <ericmajinglong@gmail.com>
2015-07-30 15:20:27 -0400

    mep12 on axes_demo.py

--- examples/pylab_examples/axes_demo.py
+++ examples/pylab_examples/axes_demo.py
@@ -1,32 +1,33 @@
-#!/usr/bin/env python
-
-from pylab import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 # create some data to use for the plot
 dt = 0.001
-t = arange(0.0, 10.0, dt)
-r = exp(-t[:1000]/0.05)               # impulse response
-x = randn(len(t))
-s = convolve(x, r)[:len(x)]*dt  # colored noise
+t = np.arange(0.0, 10.0, dt)
+r = np.exp(-t[:1000]/0.05)               # impulse response
+x = np.random.randn(len(t))
+s = np.convolve(x, r)[:len(x)]*dt  # colored noise
 
 # the main axes is subplot(111) by default
-plot(t, s)
-axis([0, 1, 1.1*amin(s), 2*amax(s)])
-xlabel('time (s)')
-ylabel('current (nA)')
-title('Gaussian colored noise')
+plt.plot(t, s)
+plt.axis([0, 1, 1.1*np.amin(s), 2*np.amax(s)])
+plt.xlabel('time (s)')
+plt.ylabel('current (nA)')
+plt.title('Gaussian colored noise')
 
 # this is an inset axes over the main axes
-a = axes([.65, .6, .2, .2], axisbg='y')
-n, bins, patches = hist(s, 400, normed=1)
-title('Probability')
-setp(a, xticks=[], yticks=[])
+a = plt.axes([.65, .6, .2, .2], axisbg='y')
+n, bins, patches = plt.hist(s, 400, normed=1)
+plt.title('Probability')
+plt.xticks([])
+plt.yticks([])
 
 # this is another inset axes over the main axes
-a = axes([0.2, 0.6, .2, .2], axisbg='y')
-plot(t[:len(r)], r)
-title('Impulse response')
-setp(a, xlim=(0, .2), xticks=[], yticks=[])
+a = plt.axes([0.2, 0.6, .2, .2], axisbg='y')
+plt.plot(t[:len(r)], r)
+plt.title('Impulse response')
+plt.xlim(0, 0.2)
+plt.xticks([])
+plt.yticks([])
 
-
-show()
+plt.show()
