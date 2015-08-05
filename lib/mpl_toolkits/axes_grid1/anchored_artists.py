efb3f5ad446c3172eaf0af3343d3cc74e37c2c79
Joseph Jon Booker <joe@neoturbine.net>
2015-08-04 23:16:46 -0400

    Cleanup axes_grid1.anchored_artists
    Remove non-library (and unusable) code
    Remove unused imports
    Fixed pylint issues with indentation

--- lib/mpl_toolkits/axes_grid1/anchored_artists.py
+++ lib/mpl_toolkits/axes_grid1/anchored_artists.py
@@ -1,14 +1,9 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-from matplotlib.externals import six
-
+from matplotlib.offsetbox import (AnchoredOffsetbox, AuxTransformBox,
+                                  DrawingArea, TextArea, VPacker)
 from matplotlib.patches import Rectangle, Ellipse
-
-import numpy as np
-
-from matplotlib.offsetbox import AnchoredOffsetbox, AuxTransformBox, VPacker,\
-     TextArea, AnchoredText, DrawingArea, AnnotationBbox
 
 
 class AnchoredDrawingArea(AnchoredOffsetbox):
@@ -23,21 +18,18 @@ class AnchoredDrawingArea(AnchoredOffsetbox):
         *width*, *height*, *xdescent*, *ydescent* : the dimensions of the DrawingArea.
         *prop* : font property. This is only used for scaling the paddings.
         """
-
         self.da = DrawingArea(width, height, xdescent, ydescent)
         self.drawing_area = self.da
 
-        super(AnchoredDrawingArea, self).__init__(loc, pad=pad, borderpad=borderpad,
-                                                  child=self.da,
-                                                  prop=None,
-                                                  frameon=frameon,
-                                                  **kwargs)
+        super(AnchoredDrawingArea, self).__init__(
+            loc, pad=pad, borderpad=borderpad, child=self.da, prop=None,
+            frameon=frameon, **kwargs
+        )
 
 
 class AnchoredAuxTransformBox(AnchoredOffsetbox):
     def __init__(self, transform, loc,
                  pad=0.4, borderpad=0.5, prop=None, frameon=True, **kwargs):
-
         self.drawing_area = AuxTransformBox(transform)
 
         AnchoredOffsetbox.__init__(self, loc, pad=pad, borderpad=borderpad,
@@ -45,7 +37,6 @@ class AnchoredAuxTransformBox(AnchoredOffsetbox):
                                    prop=prop,
                                    frameon=frameon,
                                    **kwargs)
-
 
 
 class AnchoredEllipse(AnchoredOffsetbox):
@@ -57,7 +48,7 @@ class AnchoredEllipse(AnchoredOffsetbox):
         pad, borderpad in fraction of the legend font size (or prop)
         """
         self._box = AuxTransformBox(transform)
-        self.ellipse = Ellipse((0,0), width, height, angle)
+        self.ellipse = Ellipse((0, 0), width, height, angle)
         self._box.add_artist(self.ellipse)
 
         AnchoredOffsetbox.__init__(self, loc, pad=pad, borderpad=borderpad,
@@ -122,7 +113,6 @@ class AnchoredSizeBar(AnchoredOffsetbox):
         >>> bar = AnchoredSizeBar(ax.transData, 3, '3 units', 4, pad=0.5, sep=5, borderpad=0.5, frameon=False, size_vertical=0.5, color='white', fontproperties=fontprops)
 
         """
-
         self.size_bar = AuxTransformBox(transform)
         self.size_bar.add_artist(Rectangle((0, 0), size, size_vertical,
                                            fill=True, facecolor=color,
@@ -157,73 +147,3 @@ class AnchoredSizeBar(AnchoredOffsetbox):
                                    child=self._box,
                                    prop=fontproperties,
                                    frameon=frameon, **kwargs)
-
-
-if __name__ == "__main__":
-
-    import matplotlib.pyplot as plt
-
-    fig = plt.gcf()
-    fig.clf()
-    ax = plt.subplot(111)
-
-    offsetbox = AnchoredText("Test", loc=6, pad=0.3,
-                             borderpad=0.3, prop=None)
-    xy = (0.5, 0.5)
-    ax.plot([0.5], [0.5], "xk")
-    ab = AnnotationBbox(offsetbox, xy,
-                        xybox=(1., .5),
-                        xycoords='data',
-                        boxcoords=("axes fraction", "data"),
-                        arrowprops=dict(arrowstyle="->"))
-                        #arrowprops=None)
-
-    ax.add_artist(ab)
-
-
-    from matplotlib.patches import Circle
-    ada = AnchoredDrawingArea(20, 20, 0, 0,
-                              loc=6, pad=0.1, borderpad=0.3, frameon=True)
-    p = Circle((10, 10), 10)
-    ada.da.add_artist(p)
-
-    ab = AnnotationBbox(ada, (0.3, 0.4),
-                        xybox=(1., 0.4),
-                        xycoords='data',
-                        boxcoords=("axes fraction", "data"),
-                        arrowprops=dict(arrowstyle="->"))
-                        #arrowprops=None)
-
-    ax.add_artist(ab)
-
-
-    arr = np.arange(100).reshape((10,10))
-    im = AnchoredImage(arr,
-                       loc=4,
-                       pad=0.5, borderpad=0.2, prop=None, frameon=True,
-                       zoom=1,
-                       cmap = None,
-                       norm = None,
-                       interpolation=None,
-                       origin=None,
-                       extent=None,
-                       filternorm=1,
-                       filterrad=4.0,
-                       resample = False,
-                       )
-
-    ab = AnnotationBbox(im, (0.5, 0.5),
-                        xybox=(-10., 10.),
-                        xycoords='data',
-                        boxcoords="offset points",
-                        arrowprops=dict(arrowstyle="->"))
-                        #arrowprops=None)
-
-    ax.add_artist(ab)
-
-    ax.set_xlim(0, 1)
-    ax.set_ylim(0, 1)
-
-
-    plt.draw()
-    plt.show()
