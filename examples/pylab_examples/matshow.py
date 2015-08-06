cc61032f6ad5a114a91c1f687fb2a01db7495ff5
Eric Ma <ericmajinglong@gmail.com>
2015-08-05 20:32:26 -0400

    mep12 on matshow.py

--- examples/pylab_examples/matshow.py
+++ examples/pylab_examples/matshow.py
@@ -1,22 +1,22 @@
 """Simple matshow() example."""
-from matplotlib.pylab import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def samplemat(dims):
     """Make a matrix with all zeros and increasing elements on the diagonal"""
-    aa = zeros(dims)
+    aa = np.zeros(dims)
     for i in range(min(dims)):
         aa[i, i] = i
     return aa
 
-
 # Display 2 matrices of different sizes
 dimlist = [(12, 12), (15, 35)]
 for d in dimlist:
-    matshow(samplemat(d))
+    plt.matshow(samplemat(d))
 
 # Display a random matrix with a specified figure number and a grayscale
 # colormap
-matshow(rand(64, 64), fignum=100, cmap=cm.gray)
+plt.matshow(np.random.rand(64, 64), fignum=100, cmap=plt.cm.gray)
 
-show()
+plt.show()
