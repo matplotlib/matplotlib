a843e42e60b0895682a2304c0b26031ad77f9e89
Víctor Zabalza <vzabalza@gmail.com>
2015-08-07 02:26:36 +0100

    apply different rules for dropsub (integrals)

--- lib/matplotlib/tests/test_mathtext.py
+++ lib/matplotlib/tests/test_mathtext.py
@@ -100,6 +100,8 @@ math_tests = [
     r'$\dotplus$ $\doteq$ $\doteqdot$ $\ddots$',
     r'$x_kx^py^{p-2} d_i^jb_jc_kd x^j_i E^0 E^0_u$', # github issue #4873
     r'${x}_{k}{x}^{p}{y}^{p-2} {d}_{i}^{j}{b}_{j}{c}_{k}{d} {x}^{j}_{i}{E}^{0}{E}^0_u$',
+    r'${\int}_x^x x\oint_x^x x\int_{X}^{X}x\int_x x \int^x x \int_{x} x\int^{x}{\int}_{x} x{\int}^{x}_{x}x$',
+
 ]
 
 digits = "0123456789"
