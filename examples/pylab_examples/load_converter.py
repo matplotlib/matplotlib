209f4dc6c84a615c63e2bcf17e5c482d3e3fc63d
Eric Ma <ericmajinglong@gmail.com>
2015-08-15 19:52:18 -0400

    MEP12 on load_converter.py

--- examples/pylab_examples/load_converter.py
+++ examples/pylab_examples/load_converter.py
@@ -1,9 +1,18 @@
 from __future__ import print_function
-from matplotlib.dates import bytespdate2num
-#from matplotlib.mlab import load
 import numpy as np
-from pylab import figure, show
+import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
+import matplotlib.dates as mdates
+
+# Note: matplotlib.dates doesn't have bytespdate2num.
+# This function was copied off the internet.
+# Source: http://pythonprogramming.net/colors-fills-matplotlib-tutorial/
+def bytespdate2num(fmt, encoding='utf-8'):
+    strconverter = mdates.strpdate2num(fmt)
+    def bytesconverter(b):
+        s = b.decode(encoding)
+        return strconverter(s)
+    return bytesconverter
 
 datafile = cbook.get_sample_data('msft.csv', asfileobj=False)
 print('loading', datafile)
@@ -13,8 +22,8 @@ dates, closes = np.loadtxt(
     converters={0: bytespdate2num('%d-%b-%y')},
     skiprows=1, usecols=(0, 2), unpack=True)
 
-fig = figure()
+fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot_date(dates, closes, '-')
 fig.autofmt_xdate()
-show()
+plt.show()
