d4b27cc186da7b6ce086ad729d8893e6ef511286
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2015-08-16 14:56:07 +0100

    Add reference to faq
    In the framework error message within the OSX backend

--- src/_macosx.m
+++ src/_macosx.m
@@ -6283,7 +6283,9 @@ static bool verify_framework(void)
         "not be able to function correctly if Python is not installed as a "
         "framework. See the Python documentation for more information on "
         "installing Python as a framework on Mac OS X. Please either reinstall "
-        "Python as a framework, or try one of the other backends.");
+        "Python as a framework, or try one of the other backends. If you are "
+        "Working with Matplotlib in a virtual enviroment see 'Working with "
+        "Matplotlib in Virtual environments' in the Matplotlib FAQ");
     return false;
 }
 
