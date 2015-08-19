4c20aa822f42cca060517878b579a050b73f4d13
Eric Ma <ericmajinglong@gmail.com>
2015-08-18 20:15:32 -0400

    anti-pep8 fixes ^_^

--- examples/pylab_examples/image_origin.py
+++ examples/pylab_examples/image_origin.py
@@ -11,15 +11,15 @@ x = np.arange(100.0)
 x.shape = (10, 10)
 
 interp = 'bilinear'
-# interp = 'nearest'
+#interp = 'nearest'
 lim = -2, 11, -2, 6
 plt.subplot(211, axisbg='g')
 plt.title('blue should be up')
 plt.imshow(x, origin='upper', interpolation=interp, cmap='jet')
-# plt.axis(lim)
+#plt.axis(lim)
 
 plt.subplot(212, axisbg='y')
 plt.title('blue should be down')
 plt.imshow(x, origin='lower', interpolation=interp, cmap='jet')
-# plt.axis(lim)
+#plt.axis(lim)
 plt.show()
