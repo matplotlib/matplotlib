d8e77d4ddd179ea0bd4b12b3418c1c1e3be01e2e
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2015-08-19 21:45:13 +0100

    Add new colormaps to lightness
    Also partly pep8 and make the figure a bit smaller

--- doc/users/plotting/colormaps/lightness.py
+++ doc/users/plotting/colormaps/lightness.py
@@ -6,15 +6,17 @@ categories as in the original matplotlib gallery of colormaps.
 
 import colorconv as color
 #from skimage import color
-# we are using a local copy of colorconv from scikit-image to reduce dependencies. 
+# we are using a local copy of colorconv from scikit-image to
+# reduce dependencies.
 # You should probably use the one from scikit-image in most cases. 
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import matplotlib as mpl
 
-mpl.rcParams.update({'font.size': 14})
-mpl.rcParams['font.sans-serif'] = 'Arev Sans, Bitstream Vera Sans, Lucida Grande, Verdana, Geneva, Lucid, Helvetica, Avant Garde, sans-serif'
+mpl.rcParams.update({'font.size': 12})
+mpl.rcParams['font.sans-serif'] = ('Arev Sans, Bitstream Vera Sans,'
+'Lucida Grande, Verdana, Geneva, Lucid, Helvetica, Avant Garde, sans-serif')
 mpl.rcParams['mathtext.fontset'] = 'custom'
 mpl.rcParams['mathtext.cal'] = 'cursive'
 mpl.rcParams['mathtext.rm'] = 'sans'
@@ -24,15 +26,17 @@ mpl.rcParams['mathtext.bf'] = 'sans:bold'
 mpl.rcParams['mathtext.sf'] = 'sans'
 mpl.rcParams['mathtext.fallback_to_cm'] = 'True'
 
-# Have colormaps separated into categories: http://matplotlib.org/examples/color/colormaps_reference.html
+# Have colormaps separated into categories:
+# http://matplotlib.org/examples/color/colormaps_reference.html
 
 cmaps = [('Sequential',     ['Blues', 'BuGn', 'BuPu',
                              'GnBu', 'Greens', 'Greys', 'Oranges', 'OrRd',
                              'PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu',
                              'Reds', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd']),
          ('Sequential (2)', ['afmhot', 'autumn', 'bone', 'cool', 'copper',
-                             'gist_heat', 'gray', 'hot', 'pink',
-                             'spring', 'summer', 'winter']),
+                             'gist_heat', 'gray', 'hot', 'inferno', 'magma',
+                             'pink', 'plasma', 'spring', 'summer', 'viridis',
+                             'winter']),
          ('Diverging',      ['BrBG', 'bwr', 'coolwarm', 'PiYG', 'PRGn', 'PuOr',
                              'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'Spectral',
                              'seismic']),
@@ -60,14 +64,13 @@ for cmap_category, cmap_list in cmaps:
         dsub = 7
     nsubplots = int(np.ceil(len(cmap_list)/float(dsub)))
 
-    fig = plt.figure(figsize=(11.5,4*nsubplots))
+    fig = plt.figure(figsize=(7,2.6*nsubplots))
 
     for i, subplot in enumerate(range(nsubplots)):
 
         locs = [] # locations for text labels
 
         ax = fig.add_subplot(nsubplots, 1, i+1)
-        # pdb.set_trace()
 
         for j, cmap in enumerate(cmap_list[i*dsub:(i+1)*dsub]):
 
@@ -79,10 +82,12 @@ for cmap_category, cmap_list in cmaps:
 
             # Plot colormap L values
             # Do separately for each category so each plot can be pretty
-            # to make scatter markers change color along plot: http://stackoverflow.com/questions/8202605/matplotlib-scatterplot-colour-as-a-function-of-a-third-variable
+            # to make scatter markers change color along plot:
+            # http://stackoverflow.com/questions/8202605/matplotlib-scatterplot-colour-as-a-function-of-a-third-variable
             if cmap_category=='Sequential':
                 dc = 0.6 # spacing between colormaps
-                ax.scatter(x+j*dc, lab[0,::-1,0], c=x, cmap=cmap + '_r', s=300, linewidths=0.)
+                ax.scatter(x+j*dc, lab[0,::-1,0], c=x, cmap=cmap + '_r',
+                           s=300, linewidths=0.)
                 if i==2:
                     ax.axis([-0.1,4.1,0,100])
                 else:
@@ -91,27 +96,34 @@ for cmap_category, cmap_list in cmaps:
 
             elif cmap_category=='Sequential (2)':
                 dc = 1.15
-                ax.scatter(x+j*dc, lab[0,:,0], c=x, cmap=cmap, s=300, linewidths=0.)
+                ax.scatter(x+j*dc, lab[0,:,0], c=x, cmap=cmap,
+                           s=300, linewidths=0.)
                 ax.axis([-0.1,7.0,0,100])
-                locs.append(x[-1]+j*dc) # store locations for colormap labels
+                # store locations for colormap labels
+                locs.append(x[-1]+j*dc)
 
             elif cmap_category=='Diverging':
                 dc = 1.2
-                ax.scatter(x+j*dc, lab[0,:,0], c=x, cmap=cmap, s=300, linewidths=0.)
+                ax.scatter(x+j*dc, lab[0,:,0], c=x, cmap=cmap,
+                           s=300, linewidths=0.)
                 ax.axis([-0.1,7.1,0,100])
-                locs.append(x[int(x.size/2.)]+j*dc) # store locations for colormap labels
-
+                # store locations for colormap labels
+                locs.append(x[int(x.size/2.)]+j*dc)
             elif cmap_category=='Qualitative':
                 dc = 1.3
-                ax.scatter(x+j*dc, lab[0,:,0], c=x, cmap=cmap, s=300, linewidths=0.)
+                ax.scatter(x+j*dc, lab[0,:,0], c=x, cmap=cmap,
+                           s=300, linewidths=0.)
                 ax.axis([-0.1,6.3,0,100])
-                locs.append(x[int(x.size/2.)]+j*dc) # store locations for colormap labels
+                # store locations for colormap labels
+                locs.append(x[int(x.size/2.)]+j*dc)
 
             elif cmap_category=='Miscellaneous':
                 dc = 1.25
-                ax.scatter(x+j*dc, lab[0,:,0], c=x, cmap=cmap, s=300, linewidths=0.)
+                ax.scatter(x+j*dc, lab[0,:,0], c=x, cmap=cmap,
+                           s=300, linewidths=0.)
                 ax.axis([-0.1,6.1,0,100])
-                locs.append(x[int(x.size/2.)]+j*dc) # store locations for colormap labels
+                # store locations for colormap labels
+                locs.append(x[int(x.size/2.)]+j*dc)
 
             # Set up labels for colormaps
             ax.xaxis.set_ticks_position('top')
@@ -123,8 +135,9 @@ for cmap_category, cmap_list in cmaps:
             for label in labels:
                 label.set_rotation(60)
 
-    ax.set_xlabel(cmap_category + ' colormaps', fontsize=22)
-    fig.text(-0.005, 0.55, 'Lightness $L^*$', fontsize=18, transform=fig.transFigure, rotation=90)
+    ax.set_xlabel(cmap_category + ' colormaps', fontsize=14)
+    fig.text(0.0, 0.55, 'Lightness $L^*$', fontsize=12,
+             transform=fig.transFigure, rotation=90)
 
-    fig.tight_layout(h_pad=0.05)
+    fig.tight_layout(h_pad=0.05, pad=1.5)
     plt.show()
