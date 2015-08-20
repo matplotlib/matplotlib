bb3955a6bfcc274512dd12cfa9167b720bba6ced
Steven Silvester <steven.silvester@ieee.org>
2015-08-20 09:32:15 -0500

    Remove debug print

--- lib/matplotlib/tests/test_widgets.py
+++ lib/matplotlib/tests/test_widgets.py
@@ -193,7 +193,7 @@ def test_rectangle_handles():
     do_event(tool, 'press', xdata=132, ydata=132)
     do_event(tool, 'onmove', xdata=120, ydata=120)
     do_event(tool, 'release', xdata=120, ydata=120)
-    assert tool.extents == (108, 138, 108, 138), tool.extents
+    assert tool.extents == (108, 138, 108, 138)
 
     # create a new rectangle
     do_event(tool, 'press', xdata=10, ydata=10)
