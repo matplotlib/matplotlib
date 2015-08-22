b60a84d960363a4f7afa0a531adae3cd01449b97
Steven Silvester <steven.silvester@ieee.org>
2015-08-22 16:49:12 -0500

    Add docstring for EllipseSelector and use print() statements

--- lib/matplotlib/widgets.py
+++ lib/matplotlib/widgets.py
@@ -1634,17 +1634,17 @@ class RectangleSelector(_SelectorWidget):
 
         def onselect(eclick, erelease):
           'eclick and erelease are matplotlib events at press and release'
-          print ' startposition : (%f, %f)' % (eclick.xdata, eclick.ydata)
-          print ' endposition   : (%f, %f)' % (erelease.xdata, erelease.ydata)
-          print ' used button   : ', eclick.button
+          print(' startposition : (%f, %f)' % (eclick.xdata, eclick.ydata))
+          print(' endposition   : (%f, %f)' % (erelease.xdata, erelease.ydata))
+          print(' used button   : ', eclick.button)
 
         def toggle_selector(event):
-            print ' Key pressed.'
+            print(' Key pressed.')
             if event.key in ['Q', 'q'] and toggle_selector.RS.active:
-                print ' RectangleSelector deactivated.'
+                print(' RectangleSelector deactivated.')
                 toggle_selector.RS.set_active(False)
             if event.key in ['A', 'a'] and not toggle_selector.RS.active:
-                print ' RectangleSelector activated.'
+                print(' RectangleSelector activated.')
                 toggle_selector.RS.set_active(True)
 
         x = arange(100)/(99.0)
@@ -2024,7 +2024,42 @@ class RectangleSelector(_SelectorWidget):
 
 
 class EllipseSelector(RectangleSelector):
+    """
+    Select an elliptical region of an axes.
 
+    For the cursor to remain responsive you much keep a reference to
+    it.
+
+    Example usage::
+
+        from matplotlib.widgets import  EllipseSelector
+        from pylab import *
+
+        def onselect(eclick, erelease):
+          'eclick and erelease are matplotlib events at press and release'
+          print(' startposition : (%f, %f)' % (eclick.xdata, eclick.ydata))
+          print(' endposition   : (%f, %f)' % (erelease.xdata, erelease.ydata))
+          print(' used button   : ', eclick.button)
+
+        def toggle_selector(event):
+            print(' Key pressed.')
+            if event.key in ['Q', 'q'] and toggle_selector.ES.active:
+                print(' EllipseSelector deactivated.')
+                toggle_selector.RS.set_active(False)
+            if event.key in ['A', 'a'] and not toggle_selector.ES.active:
+                print(' EllipseSelector activated.')
+                toggle_selector.ES.set_active(True)
+
+        x = arange(100)/(99.0)
+        y = sin(x)
+        fig = figure
+        ax = subplot(111)
+        ax.plot(x,y)
+
+        toggle_selector.ES = EllipseSelector(ax, onselect, drawtype='line')
+        connect('key_press_event', toggle_selector)
+        show()
+    """
     _shape_klass = Ellipse
 
     def draw_shape(self, extents):
