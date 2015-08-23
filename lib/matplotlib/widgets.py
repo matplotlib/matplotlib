321bb33eb4ffa8cc8f6276e476cfe6d492c916a2
Steven Silvester <steven.silvester@ieee.org>
2015-08-23 17:29:04 -0500

    Remove extra draw_idle trigger

--- lib/matplotlib/widgets.py
+++ lib/matplotlib/widgets.py
@@ -1796,7 +1796,6 @@ class RectangleSelector(_SelectorWidget):
         if self.interactive and self.to_draw.get_visible():
             self._set_active_handle(event)
 
-        self.set_visible(self.visible)
         if self.active_handle is None or not self.interactive:
             # Clear previous rectangle before drawing new rectangle.
             self.update()
