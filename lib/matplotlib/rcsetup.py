47b8b89bbd416493b9c941ce64c4c6aee6212392
Benjamin Root <ben.v.root@gmail.com>
2015-08-23 13:23:03 -0400

    reduce() isn't built-in in py3k. go figure...

--- lib/matplotlib/rcsetup.py
+++ lib/matplotlib/rcsetup.py
@@ -18,6 +18,7 @@ from __future__ import (absolute_import, division, print_function,
 
 from matplotlib.externals import six
 
+from functools import reduce
 import operator
 import os
 import warnings
