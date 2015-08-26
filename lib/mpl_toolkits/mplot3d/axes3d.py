5f24e08a30390e8ad48aa41ca20b76ca29d3e035
Thomas A Caswell <tcaswell@gmail.com>
2015-08-25 23:48:40 -0400

    FIX: jumping axes
    The first time the Axes3D is drawn the x,y,z axis are drawn before the
    aspect has been adjusted so they are wrong (the double zaxis the
    previous commit removed was actually the correct one).
    
    This copies the relevant code up to the Axes3D level.  This is not great
    as there is code-duplication, but the 'correct' solution is to make the
    Axes.draw method much more pluggable which is a very large job.

--- lib/mpl_toolkits/mplot3d/axes3d.py
+++ lib/mpl_toolkits/mplot3d/axes3d.py
@@ -247,6 +247,18 @@ class Axes3D(Axes):
         self.axesPatch.draw(renderer)
         self._frameon = False
 
+        # first, set the aspect
+        # this is duplicated from `axes._base._AxesBase.draw`
+        # but must be called before any of the artist are drawn as
+        # it adjusts the view limits and the size of the bounding box
+        # of the axes
+        locator = self.get_axes_locator()
+        if locator:
+            pos = locator(self, renderer)
+            self.apply_aspect(pos)
+        else:
+            self.apply_aspect()
+
         # add the projection matrix to the renderer
         self.M = self.get_proj()
         renderer.M = self.M
