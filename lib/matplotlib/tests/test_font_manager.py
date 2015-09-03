3008a7a481b82632abf3f35a42c423589b4c8e40
Jouni K. Seppänen <jks@iki.fi>
2015-09-03 21:29:25 +0300

    Test case of FontManager serialization

--- lib/matplotlib/tests/test_font_manager.py
+++ lib/matplotlib/tests/test_font_manager.py
@@ -5,8 +5,11 @@ from nose.tools import assert_equal
 from matplotlib.externals import six
 
 import os
+import tempfile
+import warnings
 
-from matplotlib.font_manager import findfont, FontProperties
+from matplotlib.font_manager import (
+    findfont, FontProperties, fontManager, json_dump, json_load)
 from matplotlib import rc_context
 
 
@@ -17,3 +20,17 @@ def test_font_priority():
         font = findfont(
             FontProperties(family=["sans-serif"]))
     assert_equal(os.path.basename(font), 'cmmi10.ttf')
+
+
+def test_json_serialization():
+    with tempfile.NamedTemporaryFile() as temp:
+        json_dump(fontManager, temp.name)
+        copy = json_load(temp.name)
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', 'findfont: Font family.*not found')
+        for prop in ({'family': 'STIXGeneral'},
+                     {'family': 'Bitstream Vera Sans', 'weight': 700},
+                     {'family': 'no such font family'}):
+            fp = FontProperties(**prop)
+            assert_equal(fontManager.findfont(fp, rebuild_if_missing=False),
+                         copy.findfont(fp, rebuild_if_missing=False))
