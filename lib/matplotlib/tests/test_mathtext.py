7d732e82e2f08b0a01f4df9a663b4c916eec00b2
Víctor Zabalza <vzabalza@gmail.com>
2015-09-03 15:52:12 +0100

    add - to binary operators, do not space binary operators at start of string

--- lib/matplotlib/tests/test_mathtext.py
+++ lib/matplotlib/tests/test_mathtext.py
@@ -102,7 +102,8 @@ math_tests = [
     r'${xyz}^k{x}_{k}{x}^{p}{y}^{p-2} {d}_{i}^{j}{b}_{j}{c}_{k}{d} {x}^{j}_{i}{E}^{0}{E}^0_u$',
     r'${\int}_x^x x\oint_x^x x\int_{X}^{X}x\int_x x \int^x x \int_{x} x\int^{x}{\int}_{x} x{\int}^{x}_{x}x$',
     r'testing$^{123}$',
-    ' '.join('$\\' + p + '$' for p in sorted(mathtext.Parser._snowflake))
+    ' '.join('$\\' + p + '$' for p in sorted(mathtext.Parser._snowflake)),
+    r'$6-2$; $-2$; $ -2$; ${-2}$; ${  -2}$; $20^{+3}_{-2}$',
 ]
 
 digits = "0123456789"
