fc7683b230a8be1957a3a55952d7dee2d113c450
Thomas A Caswell <tcaswell@gmail.com>
2015-09-14 20:10:12 -0400

    BLD: tweak version style

--- lib/matplotlib/_version.py
+++ lib/matplotlib/_version.py
@@ -35,7 +35,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep440-post"
     cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "matplotlib-"
     cfg.versionfile_source = "lib/matplotlib/_version.py"
