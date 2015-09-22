99e1fced86b00be05e278eed304392192cad19e1
Eric Ma <ericmajinglong@gmail.com>
2015-09-22 15:45:45 -0400

    mep12 on ellipse_rotated.py

--- examples/pylab_examples/ellipse_rotated.py
+++ examples/pylab_examples/ellipse_rotated.py
@@ -1,19 +1,20 @@
-from pylab import *
+import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.patches import Ellipse
 
 delta = 45.0  # degrees
 
-angles = arange(0, 360 + delta, delta)
+angles = np.arange(0, 360 + delta, delta)
 ells = [Ellipse((1, 1), 4, 2, a) for a in angles]
 
-a = subplot(111, aspect='equal')
+a = plt.subplot(111, aspect='equal')
 
 for e in ells:
     e.set_clip_box(a.bbox)
     e.set_alpha(0.1)
     a.add_artist(e)
 
-xlim(-2, 4)
-ylim(-1, 3)
+plt.xlim(-2, 4)
+plt.ylim(-1, 3)
 
-show()
+plt.show()
