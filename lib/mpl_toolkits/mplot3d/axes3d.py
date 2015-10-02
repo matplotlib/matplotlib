fc64e7588a8816dbc2935aa39b4576ef6bdac528
Till Stensitzki <tillsten@zedat.fu-berlin.de>
2015-10-02 18:50:11 +0200

    also check in plot_wireframe

--- lib/mpl_toolkits/mplot3d/axes3d.py
+++ lib/mpl_toolkits/mplot3d/axes3d.py
@@ -1756,7 +1756,8 @@ class Axes3D(Axes):
         cstride = kwargs.pop("cstride", 1)
 
         had_data = self.has_data()
-        Z = np.atleast_2d(Z)
+        if Z.ndim != 2:
+            raise ValueError("Argument Z must be 2-dimensional.")
         # FIXME: Support masked arrays
         X, Y, Z = np.broadcast_arrays(X, Y, Z)
         rows, cols = Z.shape
