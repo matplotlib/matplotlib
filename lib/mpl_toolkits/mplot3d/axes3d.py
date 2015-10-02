d98f92f18a95f9fc4669936aa303c0891b8df8d2
Till Stensitzki <tillsten@zedat.fu-berlin.de>
2015-10-02 16:26:21 +0200

    Don't allow 1d arrays in plot_surface.

--- lib/mpl_toolkits/mplot3d/axes3d.py
+++ lib/mpl_toolkits/mplot3d/axes3d.py
@@ -1581,7 +1581,8 @@ class Axes3D(Axes):
 
         had_data = self.has_data()
 
-        Z = np.atleast_2d(Z)
+        if Z.ndim != 2:
+            raise ValueError("Argument Z must be 2-dimensional.")
         # TODO: Support masked arrays
         X, Y, Z = np.broadcast_arrays(X, Y, Z)
         rows, cols = Z.shape
