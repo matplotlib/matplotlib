e3bdf44a4db6709590d8fd360dcacedd787d2470
Michael Droettboom <mdboom@gmail.com>
2015-10-20 13:20:59 -0400

    Add new category for perceptually uniform colormaps
    Refactor to reduce duplication

--- examples/color/colormaps_reference.py
+++ examples/color/colormaps_reference.py
@@ -35,25 +35,7 @@ Miscellaneous:
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-cmaps = [('Sequential',     ['Blues', 'BuGn', 'BuPu',
-                             'GnBu', 'Greens', 'Greys', 'Oranges', 'OrRd',
-                             'PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu',
-                             'Reds', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd']),
-         ('Sequential (2)', ['afmhot', 'autumn', 'bone', 'cool', 'copper',
-                             'gist_heat', 'gray', 'hot', 'inferno', 'magma',
-                             'pink', 'plasma', 'spring', 'summer', 'viridis',
-                             'winter']),
-         ('Diverging',      ['BrBG', 'bwr', 'coolwarm', 'PiYG', 'PRGn', 'PuOr',
-                             'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'Spectral',
-                             'seismic']),
-         ('Qualitative',    ['Accent', 'Dark2', 'Paired', 'Pastel1',
-                             'Pastel2', 'Set1', 'Set2', 'Set3']),
-         ('Miscellaneous',  ['gist_earth', 'terrain', 'ocean', 'gist_stern',
-                             'brg', 'CMRmap', 'cubehelix',
-                             'gnuplot', 'gnuplot2', 'gist_ncar',
-                             'nipy_spectral', 'jet', 'rainbow',
-                             'gist_rainbow', 'hsv', 'flag', 'prism'])]
+from colormaps import cmaps
 
 
 nrows = max(len(cmap_list) for cmap_category, cmap_list in cmaps)
