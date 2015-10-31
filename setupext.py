f39b388a251b7c959f22f4a81751f8cbb3dff6e1
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2015-10-31 09:33:44 +0000

    Remove python2.6 backport in setupext

--- setupext.py
+++ setupext.py
@@ -9,6 +9,7 @@ import multiprocessing
 import os
 import re
 import subprocess
+from subprocess import check_output
 import sys
 import warnings
 from textwrap import fill
@@ -17,32 +18,6 @@ import versioneer
 
 
 PY3 = (sys.version_info[0] >= 3)
-
-
-try:
-    from subprocess import check_output
-except ImportError:
-    # check_output is not available in Python 2.6
-    def check_output(*popenargs, **kwargs):
-        """
-        Run command with arguments and return its output as a byte
-        string.
-
-        Backported from Python 2.7 as it's implemented as pure python
-        on stdlib.
-        """
-        process = subprocess.Popen(
-            stdout=subprocess.PIPE, *popenargs, **kwargs)
-        output, unused_err = process.communicate()
-        retcode = process.poll()
-        if retcode:
-            cmd = kwargs.get("args")
-            if cmd is None:
-                cmd = popenargs[0]
-            error = subprocess.CalledProcessError(retcode, cmd)
-            error.output = output
-            raise error
-        return output
 
 
 if sys.platform != 'win32':
