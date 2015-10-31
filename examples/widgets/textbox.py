606d4a31acabac24ae13ec92f6255cf49faff3b7
Hastings Greer <hastings.greer@gmail.com>
2015-10-31 19:03:22 -0400

    fixed PEP8 formatting

--- examples/widgets/textbox.py
+++ examples/widgets/textbox.py
@@ -9,6 +9,7 @@ s = t ** 2
 initial_text = "t ** 2"
 l, = plt.plot(t, s, lw=2)
 
+
 def submit(text):
     ydata = eval(text)
     l.set_ydata(ydata)
@@ -16,7 +17,7 @@ def submit(text):
     plt.draw()
 
 axbox = plt.axes([0.1, 0.05, 0.8, 0.075])
-text_box = TextBox(axbox, 'Evaluate', initial = initial_text)
+text_box = TextBox(axbox, 'Evaluate', initial=initial_text)
 text_box.on_submit(submit)
 
 plt.show()
