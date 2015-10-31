ff8afc30e2265fc92ac6be3b9585bf45c20cce0e
QuadmasterXLII <hastings.greer@gmail.com>
2015-10-31 17:20:04 -0400

    Removed a newline that was accidentally commited

--- lib/matplotlib/widgets.py
+++ lib/matplotlib/widgets.py
@@ -1161,7 +1161,6 @@ class SubplotTool(Widget):
             self.targetfig.canvas.draw()
 
 
-
 class Cursor(AxesWidget):
     """
     A horizontal and vertical line that spans the axes and moves with
