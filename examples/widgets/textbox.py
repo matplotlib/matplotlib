c8fac35bf0b73fb4cbcaaa0f9cdb07826ee077d0
Hastings Greer <hastings.greer@gmail.com>
2015-10-31 16:58:16 -0400

    Added a text box example: evaluates any string inputs as y(x).

---
+++ examples/widgets/textbox.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import TextBox
+fig, ax = plt.subplots()
+plt.subplots_adjust(bottom=0.2)
+t = np.arange(-2.0, 2.0, 0.001)
+s = t ** 2
+initial_text = "t ** 2"
+l, = plt.plot(t, s, lw=2)
+
+
+def submit(text):
+    ydata = eval(text)
+    l.set_ydata(ydata)
+    ax.set_ylim(np.min(ydata), np.max(ydata))
+    plt.draw()
+
+axbox = plt.axes([0.1, 0.05, 0.8, 0.075])
+text_box = TextBox(axbox, 'Evaluate', initial=initial_text)
+text_box.on_submit(submit)
+
+plt.show()
