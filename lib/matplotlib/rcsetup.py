915cef7bce2331237f489620ede538219a61e842
Michael Droettboom <mdboom@gmail.com>
2015-11-17 08:52:44 -0500

    Use Vega collection10 colors instead

--- lib/matplotlib/rcsetup.py
+++ lib/matplotlib/rcsetup.py
@@ -965,14 +965,18 @@ defaultParams = {
     # This entry can be either a cycler object or a
     # string repr of a cycler-object, which gets eval()'ed
     # to create the object.
-    'axes.prop_cycle': [ccycler('color', 'bgrcmyk'),
-                        validate_cycler],
-    'axes.xmargin': [0, ValidateInterval(0, 1,
-                                         closedmin=True,
-                                         closedmax=True)],  # margin added to xaxis
-    'axes.ymargin': [0, ValidateInterval(0, 1,
-                                         closedmin=True,
-                                         closedmax=True)],# margin added to yaxis
+    'axes.prop_cycle': [
+        ccycler('color',
+                ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
+                 '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
+                 '#bcbd22', '#17becf']),
+        validate_cycler],
+    'axes.xmargin': [0.05, ValidateInterval(0, 1,
+                                            closedmin=True,
+                                            closedmax=True)],  # margin added to xaxis
+    'axes.ymargin': [0.05, ValidateInterval(0, 1,
+                                            closedmin=True,
+                                            closedmax=True)],# margin added to yaxis
 
     'polaraxes.grid': [True, validate_bool],  # display polar grid or
                                                      # not
