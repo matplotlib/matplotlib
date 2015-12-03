5a7e3b47816ee1abccc3cca6bb9a25bce8448cd6
Jan Schulz <jasc@gmx.net>
2015-12-03 17:12:00 +0100

    CI: test in windows and build packages
    This builds windows conda packages and wheels for some python versions and
    runs the tests on these python versions on windows. It uses the AppVeyor CI
    servive and the packages are available as artefacts to download.
    
    Appveyor based on
    http://tjelvarolsson.com/blog/how-to-continuously-test-your-python-code-on-windows-using-appveyor/
    https://packaging.python.org/en/latest/appveyor/
    https://github.com/rmcgibbo/python-appveyor-conda-example
    
    Conda stuff based on
    https://github.com/conda/conda-recipes/tree/master/matplotlib
    
    ...and a lot of trial and error...

---
+++ ci/conda_recipe/README.md
@@ -0,0 +1,3 @@
+# conda package
+
+Up to now, this is mainly used to build a test conda package on windows on appveyor.
---
+++ ci/conda_recipe/bld.bat
@@ -0,0 +1,29 @@
+mkdir lib
+copy %LIBRARY_LIB%\zlibstatic.lib lib\z.lib
+if errorlevel 1 exit 1
+copy %LIBRARY_LIB%\libpng_static.lib lib\png.lib
+if errorlevel 1 exit 1
+
+set LIB=%LIB%;.\lib
+set LIBPATH=%LIBPATH%;.\lib
+set INCLUDE=%LIBRARY_INC%;%LIBRARY_INC%\freetype2;%INCLUDE%
+
+:: debug...
+set
+
+copy setup.cfg.template setup.cfg
+if errorlevel 1 exit 1
+
+python setup.py install
+if errorlevel 1 exit 1
+
+rd /s /q %SP_DIR%\dateutil
+rd /s /q %SP_DIR%\numpy
+
+if "%ARCH%"=="64" (
+    set PLAT=win-amd64
+) else (
+    set PLAT=win32
+)
+
+::copy C:\Tcl%ARCH%\bin\t*.dll %SP_DIR%\matplotlib-%PKG_VERSION%-py%PY_VER%-%PLAT%.egg\matplotlib\backends
