1179df765bfb999e91d3087bdc92c2b7216e21e5
Jan Schulz <jasc@gmx.net>
2015-12-03 17:55:01 +0100

    TST: workaround for windows

--- lib/matplotlib/tests/test_font_manager.py
+++ lib/matplotlib/tests/test_font_manager.py
@@ -29,9 +29,13 @@ def test_font_priority():
 
 
 def test_json_serialization():
-    with tempfile.NamedTemporaryFile() as temp:
-        json_dump(fontManager, temp.name)
-        copy = json_load(temp.name)
+    # on windows, we can't open a file twice, so save the name and unlink
+    # manually...
+    with tempfile.NamedTemporaryFile(delete=False) as temp:
+        name = temp.name
+    json_dump(fontManager, name)
+    copy = json_load(name)
+    os.remove(name)
     with warnings.catch_warnings():
         warnings.filterwarnings('ignore', 'findfont: Font family.*not found')
         for prop in ({'family': 'STIXGeneral'},
