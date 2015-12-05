8f1339fcd30cfe51723fc077b7fd571400c662f1
Thomas A Caswell <tcaswell@gmail.com>
2015-12-04 23:14:56 -0500

    TST: add shotgun test of marker fill styles
    Based on code that used to be in
    examples/pylab_examples/filledmarker_demo.py

--- lib/matplotlib/tests/test_lines.py
+++ lib/matplotlib/tests/test_lines.py
@@ -5,7 +5,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from matplotlib.externals import six
-
+import itertools
 import matplotlib.lines as mlines
 import nose
 from nose.tools import assert_true, assert_raises
@@ -125,6 +125,34 @@ def test_set_line_coll_dash_image():
 
     np.random.seed(0)
     cs = ax.contour(np.random.randn(20, 30), linestyles=[(0, (3, 3))])
+
+
+@image_comparison(baseline_images=['marker_fill_styles'], remove_text=True,
+                  extensions=['png'])
+def test_marker_fill_styles():
+    colors = itertools.cycle(['b', 'g', 'r', 'c', 'm', 'y', 'k'])
+    altcolor = 'lightgreen'
+
+    y = np.array([1, 1])
+    x = np.array([0, 9])
+    fig, ax = plt.subplots()
+
+    for j, marker in enumerate(mlines.Line2D.filled_markers):
+        for i, fs in enumerate(mlines.Line2D.fillStyles):
+            color = next(colors)
+            ax.plot(j * 10 + x, y + i + .5 * (j % 2),
+                    marker=marker,
+                    markersize=20,
+                    markerfacecoloralt=altcolor,
+                    fillstyle=fs,
+                    label=fs,
+                    linewidth=5,
+                    color=color,
+                    markeredgecolor=color,
+                    markeredgewidth=2)
+
+    ax.set_ylim([0, 7.5])
+    ax.set_xlim([-5, 135])
 
 
 def test_nan_is_sorted():
