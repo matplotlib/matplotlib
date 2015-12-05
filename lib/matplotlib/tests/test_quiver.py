93725330d66a2d7ede290f1739744b32452b8b86
Eric Firing <efiring@hawaii.edu>
2015-12-04 19:32:54 -1000

    Merge pull request #5620 from tacaswell/fix_quiverkey
    FIX: quiver key pivot location

--- lib/matplotlib/tests/test_quiver.py
+++ lib/matplotlib/tests/test_quiver.py
@@ -92,6 +92,22 @@ def test_quiver_copy():
     assert q0.V[0] == 2.0
 
 
+@image_comparison(baseline_images=['quiver_key_pivot'],
+                  extensions=['png'], remove_text=True)
+def test_quiver_key_pivot():
+    fig, ax = plt.subplots()
+
+    u, v = np.mgrid[0:2*np.pi:10j, 0:2*np.pi:10j]
+
+    q = ax.quiver(np.sin(u), np.cos(v))
+    ax.set_xlim(-2, 11)
+    ax.set_ylim(-2, 11)
+    ax.quiverkey(q, 0.5, 1, 1, 'N', labelpos='N')
+    ax.quiverkey(q, 1, 0.5, 1, 'E', labelpos='E')
+    ax.quiverkey(q, 0.5, 0, 1, 'S', labelpos='S')
+    ax.quiverkey(q, 0, 0.5, 1, 'W', labelpos='W')
+
+
 if __name__ == '__main__':
     import nose
     nose.runmodule()
