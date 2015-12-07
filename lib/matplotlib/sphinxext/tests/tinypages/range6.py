d984d2b9a8ab71fa4effbe8711609899f6aaa55d
Hans Moritz Günther <moritz.guenther@gmx.de>
2015-12-07 15:10:07 -0500

    fix pep8

--- lib/matplotlib/sphinxext/tests/tinypages/range6.py
+++ lib/matplotlib/sphinxext/tests/tinypages/range6.py
@@ -1,8 +1,10 @@
 from matplotlib import pyplot as plt
 
+
 def range4():
-    '''This function should never be called if the plot_directive works as expected.'''
+    '''This is never be called if plot_directive works as expected.'''
     raise NotImplementedError
+
 
 def range6():
     plt.figure()
