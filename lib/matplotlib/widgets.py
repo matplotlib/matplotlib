a8934dcc74f6fd877bd95e1fff944e37033ee2ce
Thomas A Caswell <tcaswell@gmail.com>
2015-12-14 20:22:04 -0500

    ENH: when re-drawing the span clear previous span
    Force a full re-draw on the last span is not in the blit background

--- lib/matplotlib/widgets.py
+++ lib/matplotlib/widgets.py
@@ -1488,7 +1488,10 @@ class SpanSelector(_SelectorWidget):
         self.rect.set_visible(self.visible)
         if self.span_stays:
             self.stay_rect.set_visible(False)
-
+            # really force a draw so that the stay rect is not in
+            # the blit background
+            if self.useblit:
+                self.canvas.draw()
         xdata, ydata = self._get_data(event)
         if self.direction == 'horizontal':
             self.pressv = xdata
