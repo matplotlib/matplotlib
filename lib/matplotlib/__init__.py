4604517b1615e3d8450a2b276483269a114bdfb2
Thomas A Caswell <tcaswell@gmail.com>
2015-12-21 21:23:35 -0500

    FIX: Container remove method
    It was not properly looping over sub-children of the container.
    
    closes #5692

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -1440,6 +1440,7 @@ default_test_modules = [
     'matplotlib.tests.test_colorbar',
     'matplotlib.tests.test_colors',
     'matplotlib.tests.test_compare_images',
+    'matplotlib.tests.test_container',
     'matplotlib.tests.test_contour',
     'matplotlib.tests.test_dates',
     'matplotlib.tests.test_delaunay',
