76bdae4bfebdc0a5fbcdc0c57c823bc89cb0a243
Julien Lhermitte <jrmlhermitte@gmail.com>
2015-12-23 15:48:00 -0500

    changed to use set_xlim instead, also added tests

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -177,6 +177,48 @@ def test_twin_inherit_autoscale_setting():
     assert ax_y_on.get_autoscaley_on()
     assert not ax_y_off.get_autoscaley_on()
 
+@cleanup
+def test_inverted_cla():
+    # Github PR #5450. Setting autoscale should reset 
+    # axes to be non-inverted.
+    # plotting an image, then 1d graph, axis is now down
+    fig = plt.figure(0);
+    ax = fig.gca()
+    # test that a new axis is not inverted per default
+    assert not(ax.xaxis_inverted())
+    assert not(ax.yaxis_inverted())
+    img = np.random.random((100,100))
+    ax.imshow(img)
+    # test that a image axis is inverted
+    assert not(ax.xaxis_inverted())
+    assert ax.yaxis_inverted()
+    ax.cla()
+    x = np.linspace(0,2*np.pi,100);
+    ax.plot(x,np.cos(x))
+    assert not(ax.xaxis_inverted())
+    assert not(ax.yaxis_inverted())
+
+    # autoscaling should not bring back axes to normal
+    ax.cla()
+    ax.imshow(img)
+    plt.autoscale()
+    assert not(ax.xaxis_inverted())
+    assert ax.yaxis_inverted()
+
+    # two shared axes. Clearing the master axis should bring axes in shared
+    # axies back to normal
+    ax0 = plt.subplot(211)
+    ax1 = plt.subplot(212, sharey=ax0)
+    ax0.imshow(img)
+    ax1.plot(x,np.cos(x))
+    ax0.cla()
+    assert not(ax1.yaxis_inverted())
+    ax1.cla()
+    # clearing the nonmaster should not touch limits
+    ax0.imshow(img)
+    ax1.plot(x,np.cos(x))
+    ax1.cla()
+    assert ax.yaxis_inverted()
 
 @image_comparison(baseline_images=["minorticks_on_rcParams_both"],
                   extensions=['png'])
