3783bf366ff682b373efbe0085d26f51cf5f9e00
Julian V. Modesto <julianvmodesto@gmail.com>
2016-01-20 20:01:34 -0500

    Check for consistent height and width ratios

--- lib/matplotlib/tests/test_gridspec.py
+++ lib/matplotlib/tests/test_gridspec.py
@@ -1,8 +1,13 @@
 import matplotlib.gridspec as gridspec
-from nose.tools import assert_equal
+from nose.tools import assert_raises, assert_equal
 
 
 def test_equal():
     gs = gridspec.GridSpec(2, 1)
     assert_equal(gs[0, 0], gs[0, 0])
     assert_equal(gs[:, 0], gs[:, 0])
+
+
+def test_ratios():
+    assert_raises(ValueError, gridspec.GridSpec,
+                  1, 2, width_ratios=[2, 1, 3], height_ratios=[3, 4])
