fc7747a763afc9ec9d4d8d35daa894d8f3b3cfea
Julian V. Modesto <julianvmodesto@gmail.com>
2016-01-23 17:26:55 -0500

    Add tests for both height and width ratios

--- lib/matplotlib/tests/test_gridspec.py
+++ lib/matplotlib/tests/test_gridspec.py
@@ -8,10 +8,19 @@ def test_equal():
     assert_equal(gs[:, 0], gs[:, 0])
 
 
-def test_ratios():
+def test_width_ratios():
     """
     Addresses issue #5835.
     See at https://github.com/matplotlib/matplotlib/issues/5835.
     """
     assert_raises(ValueError, gridspec.GridSpec,
-                  1, 2, width_ratios=[2, 1, 3], height_ratios=[3, 4])
+                  1, 1, width_ratios=[2, 1, 3])
+
+
+def test_height_ratios():
+    """
+    Addresses issue #5835.
+    See at https://github.com/matplotlib/matplotlib/issues/5835.
+    """
+    assert_raises(ValueError, gridspec.GridSpec,
+                  1, 1, height_ratios=[2, 1, 3])
