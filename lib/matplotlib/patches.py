8052a71b25b5237a4748ff325707cab484b58b9e
Thomas A Caswell <tcaswell@gmail.com>
2016-01-24 13:05:18 -0500

    Merge pull request #5872 from jenshnielsen/sphinx134
    DOC: Fix issue with Sphinx 1.3.4
    
    Discarded changes to .travis.yml from #5872

--- lib/matplotlib/patches.py
+++ lib/matplotlib/patches.py
@@ -2992,15 +2992,21 @@ class ConnectionStyle(_Style):
 
         def __init__(self, armA=0., armB=0., fraction=0.3, angle=None):
             """
-            *armA* : minimum length of armA
+            Parameters
+            ----------
+            armA : float
+                minimum length of armA
 
-            *armB* : minimum length of armB
+            armB : float
+                minimum length of armB
 
-            *fraction* : a fraction of the distance between two points that
-                         will be added to armA and armB.
+            fraction : float
+                a fraction of the distance between two points that
+                will be added to armA and armB.
 
-            *angle* : angle of the connecting line (if None, parallel to A
-                      and B)
+            angle : float or None
+                angle of the connecting line (if None, parallel
+                to A and B)
             """
             self.armA = armA
             self.armB = armB
