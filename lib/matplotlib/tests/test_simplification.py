a20fccaa4eda35a5b8ed57c521c2505c4f266316
Thomas A Caswell <tcaswell@gmail.com>
2016-02-22 15:31:34 -0500

    Merge pull request #5911 from mdboom/clipping-fix
    Fix #5895: Properly clip MOVETO commands
     Conflicts:
    	lib/matplotlib/tests/baseline_images/test_axes/log_scales.svg
    
    	kept master version of file

--- lib/matplotlib/tests/test_simplification.py
+++ lib/matplotlib/tests/test_simplification.py
@@ -212,6 +212,22 @@ def test_clipping_with_nans():
     ax.set_ylim(-0.25, 0.25)
 
 
+def test_clipping_full():
+    p = path.Path([[1e30, 1e30]] * 5)
+    simplified = list(p.iter_segments(clip=[0, 0, 100, 100]))
+    assert simplified == []
+
+    p = path.Path([[50, 40], [75, 65]], [1, 2])
+    simplified = list(p.iter_segments(clip=[0, 0, 100, 100]))
+    assert ([(list(x), y) for x, y in simplified] ==
+            [([50, 40], 1), ([75, 65], 2)])
+
+    p = path.Path([[50, 40]], [1])
+    simplified = list(p.iter_segments(clip=[0, 0, 100, 100]))
+    assert ([(list(x), y) for x, y in simplified] ==
+            [([50, 40], 1)])
+
+
 if __name__=='__main__':
     import nose
     nose.runmodule(argv=['-s','--with-doctest'], exit=False)
