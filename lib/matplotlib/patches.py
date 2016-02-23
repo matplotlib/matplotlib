85cecf172fe78de870d1c24abc7abd8d6302ece2
afvincent <vincent.adrien@gmail.com>
2016-02-24 00:37:14 +0100

    Removed whitespaces (for PEP8)

--- lib/matplotlib/patches.py
+++ lib/matplotlib/patches.py
@@ -4056,7 +4056,7 @@ class FancyArrowPatch(Patch):
 
         self._mutation_scale = mutation_scale
         self._mutation_aspect = mutation_aspect
-        
+
         self.set_dpi_cor(dpi_cor)
         #self._draw_in_display_coordinate = True
 
@@ -4068,7 +4068,7 @@ class FancyArrowPatch(Patch):
 
         self._dpi_cor = dpi_cor
         self.stale = True
-        
+
     def get_dpi_cor(self):
         """
         dpi_cor is currently used for linewidth-related things and
