007a4ca46db97f3d849fe6396d5e499184b145e5
Manuel Jung <mjung@astrophysik.uni-kiel.de>
2016-02-26 01:56:22 +0100

    New streamplot option integration_direction added.

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -4605,8 +4605,8 @@ or tuple of floats
                          label_namer=None)
     def streamplot(self, x, y, u, v, density=1, linewidth=None, color=None,
                    cmap=None, norm=None, arrowsize=1, arrowstyle='-|>',
-                   minlength=0.1, maxlength=2.0, transform=None, zorder=None,
-                   start_points=None):
+                   minlength=0.1, maxlength=4.0, transform=None, zorder=None,
+                   start_points=None,integration_direction='both'):
         if not self._hold:
             self.cla()
         stream_container = mstream.streamplot(self, x, y, u, v,
@@ -4621,6 +4621,7 @@ or tuple of floats
                                               maxlength=maxlength,
                                               start_points=start_points,
                                               transform=transform,
+                                              integration_direction=integration_direction,
                                               zorder=zorder)
         return stream_container
     streamplot.__doc__ = mstream.streamplot.__doc__
