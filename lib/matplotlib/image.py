FIX: do invalid masking in to_rgba not set_data
The way that color mapping used to work was:

 - mask invalid
 - normalize
 - cmap (which uses the mask information fill in 'bad' values)
 - interpolate

The new order is:

 - mask invalid
 - interpolate (converting mask -> nan)
 - normalize (which passes through nan)
 - cmap

which results in cmap seeing the `nan` which it seems to map to
0.

This does the masking on the interpolated array just prior to
normalizing and removes it on the way in.

Fixes #6069

lib/matplotlib/image.py
@@ -503,9 +503,9 @@ class _ImageBase(martist.Artist, cm.ScalarMappable):
         """
         # check if data is PIL Image without importing Image
         if hasattr(A, 'getpixel'):
-            self._A = pil_to_array(A)
-        else:
-            self._A = cbook.safe_masked_invalid(A)
+            A = pil_to_array(A)
+
+        self._A = A
 
         if (self._A.dtype != np.uint8 and
                 not np.can_cast(self._A.dtype, np.float)):