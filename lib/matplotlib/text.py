6f8f2660932a472290ec402b53a0d7bcdcf89e10
Bearstrong <eric.cyr@mail.utoronto.ca>
2016-02-29 18:08:07 -0800

    Included fix for issue #698

--- lib/matplotlib/text.py
+++ lib/matplotlib/text.py
@@ -760,7 +760,7 @@ class Text(Artist):
             self._renderer = renderer
         if not self.get_visible():
             return
-        if self.get_text().strip == '':
+        if self.get_text().strip() == '':
             return
 
         renderer.open_group('text', self.get_gid())
@@ -976,7 +976,7 @@ class Text(Artist):
         if dpi is not None:
             dpi_orig = self.figure.dpi
             self.figure.dpi = dpi
-        if self.get_text().strip == '':
+        if self.get_text().strip() == '':
             tx, ty = self._get_xy_display()
             return Bbox.from_bounds(tx, ty, 0, 0)
 
@@ -2238,7 +2238,7 @@ class Annotation(Text, _AnnotationBase):
                     self.arrow_patch.set_patchA(self._bbox_patch)
                 else:
                     pad = renderer.points_to_pixels(4)
-                    if self.get_text().strip == "":
+                    if self.get_text().strip() == "":
                         self.arrow_patch.set_patchA(None)
                         return
 
