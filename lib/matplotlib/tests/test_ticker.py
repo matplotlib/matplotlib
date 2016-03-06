66061adac67ff71a0c5ffcaa05641138ad219485
Vincent Vandalon <vincent@vandalon.nl>
2016-03-06 15:39:06 +0100

    axis.formatter.useoffset is still honored, however, it is not directly returned by get_useOffset(). get_useOffset() returns whether an offset has been set either by the user or by _set_offset()

--- lib/matplotlib/tests/test_ticker.py
+++ lib/matplotlib/tests/test_ticker.py
@@ -351,7 +351,7 @@ def test_logformatter_pprint():
 
 
 def test_use_offset():
-    for use_offset in [True, False]:
+    for use_offset in [False]:
         with matplotlib.rc_context({'axes.formatter.useoffset': use_offset}):
             tmp_form = mticker.ScalarFormatter()
             nose.tools.assert_equal(use_offset, tmp_form.get_useOffset())
