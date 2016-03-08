2d3de46e9475636ecc13d0db4a71e3c00d1319ee
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2016-03-08 17:23:59 +0000

    Remove duplicated imports

--- examples/lines_bars_and_markers/barh_demo.py
+++ examples/lines_bars_and_markers/barh_demo.py
@@ -2,9 +2,8 @@
 Simple demo of a horizontal bar chart.
 """
 import matplotlib.pyplot as plt
-plt.rcdefaults()
 import numpy as np
-import matplotlib.pyplot as plt
+plt.rcdefaults()
 
 
 # Example data
