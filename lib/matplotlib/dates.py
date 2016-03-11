1adc3a595d0848a72a73839f7cba39ca4bf5176e
Brandon <brandon.chow@outlook.com>
2016-03-11 15:33:01 -0500

    Removed obsolate documentation.

--- lib/matplotlib/dates.py
+++ lib/matplotlib/dates.py
@@ -1013,9 +1013,11 @@ class AutoDateLocator(DateLocator):
         return self.get_locator(vmin, vmax).tick_values(vmin, vmax)
 
     def nonsingular(self, vmin, vmax):
-        # whatever is thrown at us, we can scale the unit.
-        # But default nonsingular date plots at an ~4 year period.
-        
+        # Scale the view for singular points so that the 
+        # view is based on the context
+        # We make assumptions on the context based on the
+        # singular point
+
         if vmin == vmax:
             expander_factor = 0
             dt = num2date(vmin)
@@ -1031,14 +1033,14 @@ class AutoDateLocator(DateLocator):
                 # Use weeks
                 expander_factor = DAYS_PER_WEEK
             elif dt.isoweekday() > 1:
+                # Use months
                 expander_factor = DAYS_PER_MONTH
             elif dt.month() > 1:
-                # Use months
+                # Use years
                 expander_factor = DAYS_PER_YEAR
             else:
-                # Original implementation
-                vmin = vmin - DAYS_PER_YEAR*2
-                vmax = vmax + DAYS_PER_YEAR*2
+                vmin = vmin - DAYS_PER_YEAR * 2
+                vmax = vmax + DAYS_PER_YEAR * 2
 
             expander = expander_factor * .04
             vmin = math.floor(vmin - expander)
