9b9ed321ba4af06ea2fdcaaa9cbbf1026cacbba8
Trish Gillett-Kawamoto <discardthree@gmail.com>
2016-03-16 15:23:16 -0400

    Issue #1702 transpose bugfix

--- examples/mplot3d/hist3d_demo.py
+++ examples/mplot3d/hist3d_demo.py
@@ -1,3 +1,6 @@
+# Note: np.meshgrid produces arrays in (ny, nx), so flatten is called with 'F'.
+# For numpy >= 1.7, this can be avoided by calling meshgrid with indexing='ij'.
+
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,12 +10,11 @@ ax = fig.add_subplot(111, projection='3d')
 x, y = np.random.rand(2, 100) * 4
 hist, xedges, yedges = np.histogram2d(x, y, bins=4)
 
-elements = (len(xedges) - 1) * (len(yedges) - 1)
 xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25)
+xpos = xpos.flatten('F')
+ypos = ypos.flatten('F')
+zpos = np.zeros_like(xpos)
 
-xpos = xpos.flatten()
-ypos = ypos.flatten()
-zpos = np.zeros(elements)
 dx = 0.5 * np.ones_like(zpos)
 dy = dx.copy()
 dz = hist.flatten()
