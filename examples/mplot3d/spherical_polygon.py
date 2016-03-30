b569f08893f4fe7f30eede55b4d7ded611816963
Nikolai <mail@nikno.de>
2016-03-30 18:27:54 +0100

    initial commit adding spherical polygons

---
+++ examples/mplot3d/spherical_polygon.py
@@ -0,0 +1,28 @@
+from mpl_toolkits.mplot3d import proj3d
+from scipy.spatial import ConvexHull
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.spherical import SphericalPolygon
+import numpy as np
+
+# plot unit sphere for reference
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+u = np.linspace(0, 2 * np.pi, 100)
+v = np.linspace(0, np.pi, 100)
+x = np.outer(np.cos(u), np.sin(v))
+y = np.outer(np.sin(u), np.sin(v))
+z = np.outer(np.ones(np.size(u)), np.cos(v))
+ax.plot_surface(x, y, z, rstride=5, cstride=5, color='y', alpha=0.1)
+
+# plot spherical triangle
+points_triangle = np.array([[0, 1., 0], [0, 0, 1.], [-1., 0, 0]])
+sp_triangle = SphericalPolygon(points_triangle)
+sp_triangle.add_to_ax(ax, alpha=0.8, color='r')
+
+# plot spherical square
+points_square = np.array([[0.7, 1., 1.], [0.5, -1., 1.], [0.5, -1., -1.], [0.5, 1., -1], ])
+points_square = np.array([p / np.linalg.norm(p) for p in points_square])
+sp = SphericalPolygon(points_square, ConvexHull(points_square).simplices)
+sp.add_to_ax(ax, alpha=0.8, color='b')
+
+plt.show()
---
+++ lib/mpl_toolkits/mplot3d/spherical.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+
+# This module adds functionality to plot spherical polygons and is a result of
+# the discussion of issue #5294 (Handling Spherical Polygons) on github. The
+# following people contributed to this discussion:
+# Nikolai Nowaczyk, Tyler Reddy, Nicolas P. Rougier, Phillip Wolfram,
+# Thomas A Caswell
+
+import numpy as np
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+
+class SphericalPolygon(object):
+    """
+     Plots a spherical polygon onto a sphere.
+    """
+
+    def __init__(self,
+                 vertices,
+                 tri=[[0, 1, 2]],
+                 center=np.array([0, 0, 0]),
+                 radius=1.,
+                 subdivisions=None):
+        """
+        Parameters
+        ----------
+        vertices: an array of points in 3D assumed to be on a sphere
+        tri: a list [[i,j,k], ...] of lists [i,j,k] of integers i,j,k, where
+            i, j, k are the indices of the vertices defining the face. (If left
+            empty, class will assume only 3 vertices in clockwise order.)
+        center: a point in 3D defining the center of the sphere
+        radius: a positive double representing the radius of the sphere
+        subdivisions: an integer defining how often the triangulation of
+                      the polygon is subdivided before it is plotted. (If left
+                      empty, the class will try to guess this value such that
+                      the result looks nice.)
+        """
+
+        self.vertices = vertices
+        self.center = center
+        self.radius = radius
+        self.subdivisions = subdivisions
+        self.tri = tri
+        self._refine_triangulation()
+
+    def _project_to_sphere(self, point):
+        """
+        Projects point onto the sphere defined by self.center and self.radius.
+
+        Parameters
+        ----------
+        point : array of floats of length ndim
+
+        returns: array of floats of length ndim
+        """
+
+        length = np.linalg.norm(point - self.center)
+        return (point - self.center) / length * self.radius + self.center
+
+    def _subdivide(self):
+        """
+         Subdivides the triangulation once by replacing every triangle
+         by four smaller triangles defined by the vertices of the original
+         triangle and the midpoints of its edges.
+        """
+
+        faces_subdivided = []
+
+        for face in self.tri:
+            # create three new vertices at the midpoints of each edge
+            a = self.vertices[face[0]]
+            b = self.vertices[face[1]]
+            c = self.vertices[face[2]]
+
+            # add vertices to list
+            k = len(self.vertices)
+            self.vertices = np.insert(self.vertices, k,
+                                      self._project_to_sphere(a + b), axis=0)
+            self.vertices = np.insert(self.vertices, k + 1,
+                                      self._project_to_sphere(b + c), axis=0)
+            self.vertices = np.insert(self.vertices, k + 2,
+                                      self._project_to_sphere(a + c), axis=0)
+
+            # add new faces
+            faces_subdivided.append([face[0], k, k + 2])
+            faces_subdivided.append([k, face[1], k + 1])
+            faces_subdivided.append([k + 1, face[2], k + 2])
+            faces_subdivided.append([k, k + 1, k + 2])
+
+        self.tri = faces_subdivided
+
+    def _refine_triangulation(self):
+        """
+         Refines the triangulation of the vertices.
+        """
+
+        if self.subdivisions is None:
+            while np.min(np.array([min(
+                np.linalg.norm(self.vertices[face[0]] - self.vertices[face[1]]),
+                np.linalg.norm(self.vertices[face[0]] - self.vertices[face[2]]),
+                np.linalg.norm(self.vertices[face[1]] - self.vertices[face[2]]))
+                                   for face in self.tri])) > self.radius * 0.2:
+                self._subdivide()
+
+        else:
+            for n in range(0, self.subdivisions):
+                self._subdivide()
+
+    def add_to_ax(self, ax, *args, **kwargs):
+        """
+         Adds each triangle of the triangulation to an ax object.
+        """
+        for face in self.tri:
+            ax.add_collection3d(
+                Poly3DCollection([self.vertices[face]], *args, **kwargs))
