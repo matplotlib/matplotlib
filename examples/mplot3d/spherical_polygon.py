72cb6793a6c2e0842f5ae9ccac44ab9e91e6929f
Nikolai <mail@nikno.de>
2016-03-30 22:27:23 +0100

    simplified plotting of the sphere in example

--- examples/mplot3d/spherical_polygon.py
+++ examples/mplot3d/spherical_polygon.py
@@ -10,7 +10,7 @@ u = np.linspace(0, 2 * np.pi, 100)
 v = np.linspace(0, np.pi, 100)
 x = np.outer(np.cos(u), np.sin(v))
 y = np.outer(np.sin(u), np.sin(v))
-z = np.outer(np.ones(np.size(u)), np.cos(v))
+z = np.outer(np.ones_like(u), np.cos(v))
 ax.plot_surface(x, y, z, rstride=5, cstride=5, color='y', alpha=0.1)
 
 # plot spherical triangle
