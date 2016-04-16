0be94eef80b4b1b47163abc8db936f6b869ab281
J. Goutin <JGoutin@users.noreply.github.com>
2016-04-16 10:49:54 +0200

    Update backend_qt4.py
    Key auto-repeat Off by default for QT4.

--- lib/matplotlib/backends/backend_qt4.py
+++ lib/matplotlib/backends/backend_qt4.py
@@ -76,6 +76,9 @@ class FigureCanvasQT(FigureCanvasQT5):
         w, h = self.get_width_height()
         self.resize(w, h)
 
+        # Key auto-repeat disabled by default
+        self._keyautorepeat = False
+
     def wheelEvent(self, event):
         x = event.x()
         # flipy so y=0 is bottom of canvas
