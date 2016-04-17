ef29e676b4f24f9a9213aac7996ea5ae3cef0180
BTWS <brecht.willems@ugent.be>
2016-04-17 10:10:55 +0200

    Make checkbuttons with all plotted lines with correct visibility automatically
    Previously you had to manually change the variables in func. Now it's done automatically based on the properties of the lines.

--- examples/widgets/check_buttons.py
+++ examples/widgets/check_buttons.py
@@ -8,23 +8,23 @@ s1 = np.sin(4*np.pi*t)
 s2 = np.sin(6*np.pi*t)
 
 fig, ax = plt.subplots()
-l0, = ax.plot(t, s0, visible=False, lw=2)
-l1, = ax.plot(t, s1, lw=2)
-l2, = ax.plot(t, s2, lw=2)
+ax.plot(t, s0, visible=False, lw=2, color='k', label='2 Hz')
+ax.plot(t, s1, lw=2, color='r', label='4 Hz')
+ax.plot(t, s2, lw=2, color='g', label='6 Hz')
 plt.subplots_adjust(left=0.2)
 
-rax = plt.axes([0.05, 0.4, 0.1, 0.15])
-check = CheckButtons(rax, ('2 Hz', '4 Hz', '6 Hz'), (False, True, True))
+lines = ax.get_lines()
 
+# Make checkbuttons with all plotted lines with correct visibility
+rax = plt.axes([0.05, 0.4, 0.1, 0.15])
+labels = [str(graph.get_label()) for graph in lines]
+visibility = [graph.get_visible() for graph in lines]
+check = CheckButtons(rax, labels, visibility)
 
 def func(label):
-    if label == '2 Hz':
-        l0.set_visible(not l0.get_visible())
-    elif label == '4 Hz':
-        l1.set_visible(not l1.get_visible())
-    elif label == '6 Hz':
-        l2.set_visible(not l2.get_visible())
+    lines[labels.index(label)].set_visible(not lines[labels.index(label)].get_visible())
     plt.draw()
+
 check.on_clicked(func)
 
 plt.show()
