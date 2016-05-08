61fd0bec25ed012a000bd8d3a79ca7a835d50984
Eric Firing <efiring@hawaii.edu>
2016-05-07 16:47:13 -1000

    ENH: Figure.show() raises figure with qt backends

--- lib/matplotlib/backends/backend_qt5.py
+++ lib/matplotlib/backends/backend_qt5.py
@@ -529,6 +529,8 @@ class FigureManagerQT(FigureManagerBase):
 
     def show(self):
         self.window.show()
+        self.window.activateWindow()
+        self.window.raise_()
 
     def destroy(self, *args):
         # check for qApp first, as PySide deletes it in its atexit handler
