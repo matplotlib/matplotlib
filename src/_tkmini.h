3f5407a79b77eb9c66e971bb08b83c095bc3da5b
Matthew Brett <matthew.brett@gmail.com>
2016-05-21 13:44:36 -0400

    RF: check, edit, refactor Tcl / tk mini header
    Move typedefs into Tcl / tk mini header.
    
    Rename typedefs for clarity.
    
    Strip older definition of Tcl_Interp.
    
    Check all defines compatible with Tcl / Tk 8.5 and current trunk (as of
    21 May 2016).

--- src/_tkmini.h
+++ src/_tkmini.h
@@ -57,16 +57,24 @@
  * license
  */
 
+/*
+ * Unless otherwise noted, these definitions are stable from Tcl / Tk 8.5
+ * through Tck / Tk master as of 21 May 2016
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* Tcl header excerpts */
 #define TCL_OK 0
 #define TCL_ERROR 1
 
-typedef struct Tcl_Interp
-{
-    char *result;
-    void (*freeProc) (char *);
-    int errorLine;
-} Tcl_Interp;
+/*
+ * Users of versions of Tcl >= 8.6 encouraged to tread Tcl_Interp as an opaque
+ * pointer.  The following definition results when TCL_NO_DEPRECATED defined.
+ */
+typedef struct Tcl_Interp Tcl_Interp;
 
 typedef struct Tcl_Command_ *Tcl_Command;
 typedef void *ClientData;
@@ -74,6 +82,15 @@ typedef void *ClientData;
 typedef int (Tcl_CmdProc) (ClientData clientData, Tcl_Interp
         *interp, int argc, const char *argv[]);
 typedef void (Tcl_CmdDeleteProc) (ClientData clientData);
+
+/* Typedefs derived from function signatures in Tcl header */
+/* Tcl_CreateCommand */
+typedef Tcl_Command (*Tcl_CreateCommand_t)(Tcl_Interp *interp,
+        const char *cmdName, Tcl_CmdProc *proc,
+        ClientData clientData,
+        Tcl_CmdDeleteProc *deleteProc);
+/* Tcl_AppendResult */
+typedef void (*Tcl_AppendResult_t) (Tcl_Interp *interp, ...);
 
 /* Tk header excerpts */
 typedef struct Tk_Window_ *Tk_Window;
@@ -89,3 +106,23 @@ typedef struct Tk_PhotoImageBlock
     int pixelSize;
     int offset[4];
 } Tk_PhotoImageBlock;
+
+/* Typedefs derived from function signatures in Tk header */
+/* Tk_MainWindow */
+typedef Tk_Window (*Tk_MainWindow_t) (Tcl_Interp *interp);
+typedef Tk_PhotoHandle (*Tk_FindPhoto_t) (Tcl_Interp *interp, const char
+        *imageName);
+/* Tk_PhotoPutBLock_NoComposite typedef */
+typedef void (*Tk_PhotoPutBlock_NoComposite_t) (Tk_PhotoHandle handle,
+        Tk_PhotoImageBlock *blockPtr, int x, int y,
+        int width, int height);
+/* Tk_PhotoBlank */
+typedef void (*Tk_PhotoBlank_t) (Tk_PhotoHandle handle);
+
+/*
+ * end block for C++
+ */
+
+#ifdef __cplusplus
+}
+#endif
