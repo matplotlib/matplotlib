e4acb775bae22d3773910ab58f73de9a3caadcd7
Chen Karako <chen.karako@shopify.com>
2016-05-26 20:31:49 -0400

    Cleaned up: added docstrings and switched mixcase variables to comply with PEP 8

--- examples/api/barchart_demo.py
+++ examples/api/barchart_demo.py
@@ -1,20 +1,22 @@
-# a bar plot with errorbars
+"""
+A bar plot with errorbars and height labels on individual bars
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 N = 5
-menMeans = (20, 35, 30, 35, 27)
-menStd = (2, 3, 4, 1, 2)
+men_means = (20, 35, 30, 35, 27)
+men_std = (2, 3, 4, 1, 2)
 
 ind = np.arange(N)  # the x locations for the groups
 width = 0.35       # the width of the bars
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(ind, menMeans, width, color='r', yerr=menStd)
+rects1 = ax.bar(ind, men_means, width, color='r', yerr=men_std)
 
-womenMeans = (25, 32, 34, 20, 25)
-womenStd = (3, 5, 2, 3, 3)
-rects2 = ax.bar(ind + width, womenMeans, width, color='y', yerr=womenStd)
+women_means = (25, 32, 34, 20, 25)
+women_std = (3, 5, 2, 3, 3)
+rects2 = ax.bar(ind + width, women_means, width, color='y', yerr=women_std)
 
 # add some text for labels, title and axes ticks
 ax.set_ylabel('Scores')
@@ -26,7 +28,9 @@ ax.legend((rects1[0], rects2[0]), ('Men', 'Women'))
 
 
 def autolabel(rects):
-    # attach some text labels
+    """
+    Attach a text label above each bar displaying its height
+    """
     for rect in rects:
         height = rect.get_height()
         ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
