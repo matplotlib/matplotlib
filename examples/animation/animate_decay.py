9619f22cc2a04f6a0864cfaba16b764d6abe8dd9
ashley <awesomeashley527@gmail.com>
2016-05-26 22:08:01 -0400

    changed while loop to for loop in animate_decay.py

--- examples/animation/animate_decay.py
+++ examples/animation/animate_decay.py
@@ -12,9 +12,7 @@ def data_gen(t=0):
 
     Generates a decaying sine wave.
     """
-    count = 0
-    while count < 1000:
-        count += 1
+    for count in range(1000):
         t += 0.1
         yield t, np.sin(2*np.pi*t) * np.exp(-t/10.)
 
