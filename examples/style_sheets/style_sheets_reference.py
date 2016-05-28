50a516e2d7a143ba0fdc7d80f2fa2bab5876c700
Adrien F Vincent <vincent.adrien@gmail.com>
2016-05-28 23:54:35 +0200

    Docstrings corrections/Code clarification/Add a 2nd scatter cloud

--- examples/style_sheets/style_sheets_reference.py
+++ examples/style_sheets/style_sheets_reference.py
@@ -8,13 +8,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_scatter(ax, prng, nb_samples=200):
+def plot_scatter(ax, prng, nb_samples=100):
     """ Scatter plot.
-
-    NB: `plt.scatter` doesn't use default colors.
     """
-    x, y = prng.normal(size=(2, nb_samples))
-    ax.plot(x, y, 'o')
+    for mu, sigma, marker in [(-.5, 0.75, 'o'), (0.75, 1., 's')]:
+        x, y = prng.normal(loc=mu, scale=sigma, size=(2, nb_samples))
+        ax.plot(x, y, marker=marker, ls='')
     ax.set_xlabel('X-label')
     ax.set_ylabel('Y-label')
     return ax
@@ -25,8 +24,8 @@ def plot_colored_sinusoidal_lines(ax):
     """
     L = 2*np.pi
     x = np.linspace(0, L)
-    ncolors = len(plt.rcParams['axes.color_cycle'])
-    shift = np.linspace(0, L, ncolors, endpoint=False)
+    nb_colors = len(plt.rcParams['axes.prop_cycle'])
+    shift = np.linspace(0, L, nb_colors, endpoint=False)
     for s in shift:
         ax.plot(x, np.sin(x + s), '-')
     ax.margins(0)
@@ -40,7 +39,7 @@ def plot_bar_graphs(ax, prng, min_value=5, max_value=25, nb_samples=5):
     ya, yb = prng.randint(min_value, max_value, size=(2, nb_samples))
     width = 0.25
     ax.bar(x, ya, width)
-    ax.bar(x + width, yb, width, color=plt.rcParams['axes.color_cycle'][2])
+    ax.bar(x + width, yb, width, color='C2')
     ax.set_xticks(x + width)
     ax.set_xticklabels(['a', 'b', 'c', 'd', 'e'])
     return ax
@@ -53,15 +52,13 @@ def plot_colored_circles(ax, prng, nb_samples=15):
     the color cycle, because different styles may have different numbers of
     colors.
     """
-    list_of_colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
-    max_idx = min(nb_samples, len(list_of_colors))
-    for color in list_of_colors[0:max_idx]:
+    for sty_dict, j in zip(plt.rcParams['axes.prop_cycle'], range(nb_samples)):
         ax.add_patch(plt.Circle(prng.normal(scale=3, size=2),
-                     radius=1.0, color=color))
-    # Force the limits to be the same accross the styles (because different
+                                radius=1.0, color=sty_dict['color']))
+    # Force the limits to be the same across the styles (because different
     # styles may have different numbers of available colors).
-    ax.set_xlim([-5., 7.])
-    ax.set_ylim([-8.5, 3.5])
+    ax.set_xlim([-4, 7])
+    ax.set_ylim([-5, 6])
     ax.set_aspect('equal', adjustable='box')  # to plot circles as circles
     return ax
 
@@ -79,9 +76,10 @@ def plot_histograms(ax, prng, nb_samples=10000):
     """ Plot 4 histograms and a text annotation.
     """
     params = ((10, 10), (4, 12), (50, 12), (6, 55))
-    for values in (prng.beta(a, b, size=nb_samples) for a, b in params):
+    for a, b in params:
+        values = prng.beta(a, b, size=nb_samples)
         ax.hist(values, histtype="stepfilled", bins=30, alpha=0.8, normed=True)
-    # Add a small annotation
+    # Add a small annotation.
     ax.annotate('Annotation', xy=(0.25, 4.25), xycoords='data',
                 xytext=(0.4, 10), textcoords='data',
                 bbox=dict(boxstyle="round", alpha=0.2),
@@ -95,25 +93,24 @@ def plot_histograms(ax, prng, nb_samples=10000):
 def plot_figure(style_label=None):
     """
     Setup and plot the demonstration figure with the style `style_label`.
-    If `style_label`, fall back to the `default` style.
+    If `style_label` is None, fall back to the `default` style.
     """
     if style_label is None:
         style_label = 'default'
 
     # Use a dedicated RandomState instance to draw the same "random" values
-    # across the different figures
-    prng = np.random.RandomState(145236987)
+    # across the different figures.
+    prng = np.random.RandomState(96917002)
 
     fig, axes = plt.subplots(ncols=3, nrows=2, num=style_label)
     fig.suptitle(style_label)
 
-    axes_list = axes.ravel()  # for convenience
-    plot_scatter(axes_list[0], prng)
-    plot_image_and_patch(axes_list[1], prng)
-    plot_bar_graphs(axes_list[2], prng)
-    plot_colored_circles(axes_list[3], prng)
-    plot_colored_sinusoidal_lines(axes_list[4])
-    plot_histograms(axes_list[5], prng)
+    plot_scatter(axes[0, 0], prng)
+    plot_image_and_patch(axes[0, 1], prng)
+    plot_bar_graphs(axes[0, 2], prng)
+    plot_colored_circles(axes[1, 0], prng)
+    plot_colored_sinusoidal_lines(axes[1, 1])
+    plot_histograms(axes[1, 2], prng)
 
     return fig
 
