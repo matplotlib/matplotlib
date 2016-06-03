2b389246c483c4fb07b1235f02ce1c5e93f1ce8f
Jan Schulz <jasc@gmx.net>
2016-06-03 12:04:05 +0200

    Update dependencies in tests and wheelbuild and remove pyqt
    * Use versions from conda-forge (in sync with the conda package build)
    * remove pyqt (which right now is installed from default) as the dependencies of that package clashes with packages in conda-forge. See here:
    
    https://github.com/conda-forge/conda-forge.github.io/issues/157#issuecomment-223536381

--- build_alllocal.cmd
+++ build_alllocal.cmd
@@ -1,7 +1,9 @@
 :: This assumes you have installed all the dependencies via conda packages:
 :: # create a new environment with the required packages
-:: conda create  -n "matplotlib_build" python=3.4 numpy python-dateutil pyparsing pytz tornado pyqt cycler tk libpng zlib freetype
+:: conda create  -n "matplotlib_build" python=3.4 numpy python-dateutil pyparsing pytz tornado "cycler>=0.10" tk libpng zlib freetype
 :: activate matplotlib_build
+:: if you want qt backend, you also have to install pyqt
+:: conda install pyqt
 :: # this package is only available in the conda-forge channel
 :: conda install -c conda-forge msinttypes
 :: if you build on py2.7:
