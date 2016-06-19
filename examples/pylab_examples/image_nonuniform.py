723f33e66add829c42143c8b613b6aee5575a432
Eric Firing <efiring@hawaii.edu>
2016-06-18 14:20:33 -1000

    image_nonuniform: eliminate crowding

--- examples/pylab_examples/image_nonuniform.py
+++ examples/pylab_examples/image_nonuniform.py
@@ -1,25 +1,30 @@
 '''
-This illustrates the NonUniformImage class, which still needs
-an axes method interface; either a separate interface, or a
-generalization of imshow.
+This illustrates the NonUniformImage class.  It is not
+available via an Axes method but it is easily added to an
+Axes instance as shown here.
 '''
 
-from matplotlib.pyplot import figure, show
 import numpy as np
+import matplotlib.pyplot as plt
 from matplotlib.image import NonUniformImage
 from matplotlib import cm
 
 interp = 'nearest'
 
+# Linear x array for cell centers:
 x = np.linspace(-4, 4, 9)
+
+# Highly nonlinear x array:
 x2 = x**3
+
 y = np.linspace(-4, 4, 9)
-#print('Size %d points' % (len(x) * len(y)))
+
 z = np.sqrt(x[np.newaxis, :]**2 + y[:, np.newaxis]**2)
 
-fig = figure()
-fig.suptitle('NonUniformImage class')
-ax = fig.add_subplot(221)
+fig, axs = plt.subplots(nrows=2, ncols=2)
+fig.subplots_adjust(bottom=0.07, hspace=0.3)
+fig.suptitle('NonUniformImage class', fontsize='large')
+ax = axs[0, 0]
 im = NonUniformImage(ax, interpolation=interp, extent=(-4, 4, -4, 4),
                      cmap=cm.Purples)
 im.set_data(x, y, z)
@@ -28,7 +33,7 @@ ax.set_xlim(-4, 4)
 ax.set_ylim(-4, 4)
 ax.set_title(interp)
 
-ax = fig.add_subplot(222)
+ax = axs[0, 1]
 im = NonUniformImage(ax, interpolation=interp, extent=(-64, 64, -4, 4),
                      cmap=cm.Purples)
 im.set_data(x2, y, z)
@@ -39,7 +44,7 @@ ax.set_title(interp)
 
 interp = 'bilinear'
 
-ax = fig.add_subplot(223)
+ax = axs[1, 0]
 im = NonUniformImage(ax, interpolation=interp, extent=(-4, 4, -4, 4),
                      cmap=cm.Purples)
 im.set_data(x, y, z)
@@ -48,7 +53,7 @@ ax.set_xlim(-4, 4)
 ax.set_ylim(-4, 4)
 ax.set_title(interp)
 
-ax = fig.add_subplot(224)
+ax = axs[1, 1]
 im = NonUniformImage(ax, interpolation=interp, extent=(-64, 64, -4, 4),
                      cmap=cm.Purples)
 im.set_data(x2, y, z)
@@ -57,4 +62,4 @@ ax.set_xlim(-64, 64)
 ax.set_ylim(-4, 4)
 ax.set_title(interp)
 
-show()
+plt.show()
