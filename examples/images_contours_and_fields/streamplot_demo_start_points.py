6f5f34737894504256032da44d1cdd047dfa1905
Adrien F Vincent <vincent.adrien@gmail.com>
2016-07-01 21:27:30 +0200

    Remove index on 'fig0' and 'ax0' (only ones of their kind)

--- examples/images_contours_and_fields/streamplot_demo_start_points.py
+++ examples/images_contours_and_fields/streamplot_demo_start_points.py
@@ -14,13 +14,13 @@ U, V = np.mgrid[-3:3:100j, 0:0:100j]
 
 seed_points = np.array([[-2, 0, 1], [-2, 0, 1]])
 
-fig0, ax0 = plt.subplots()
-strm = ax0.streamplot(X, Y, U, V, color=U, linewidth=2,
-                      cmap=plt.cm.autumn, start_points=seed_points.T)
-fig0.colorbar(strm.lines)
+fig, ax = plt.subplots()
+strm = ax.streamplot(X, Y, U, V, color=U, linewidth=2,
+                     cmap=plt.cm.autumn, start_points=seed_points.T)
+fig.colorbar(strm.lines)
 
-ax0.plot(seed_points[0], seed_points[1], 'bo')
+ax.plot(seed_points[0], seed_points[1], 'bo')
 
-ax0.axis((-3, 3, -3, 3))
+ax.axis((-3, 3, -3, 3))
 
 plt.show()
