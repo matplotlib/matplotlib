341483968bc7d12598b48aa9407ceebcb30fcda8
Jan Schulz <jasc@gmx.net>
2016-07-03 17:07:09 +0200

    Fix local windows build script due to conda changes

--- build_alllocal.cmd
+++ build_alllocal.cmd
@@ -17,20 +17,20 @@ IF [%1]==[] (
     echo Using user supplied target: %TARGET%
 )
 
-IF NOT DEFINED CONDA_DEFAULT_ENV (
+IF NOT DEFINED CONDA_PREFIX (
     echo No Conda env activated: you need to create a conda env with the right packages and activate it!
     GOTO:eof
 )
 
 :: copy the libs which have "wrong" names
-set LIBRARY_LIB=%CONDA_DEFAULT_ENV%\Library\lib
+set LIBRARY_LIB=%CONDA_PREFIX%\Library\lib
 mkdir lib || cmd /c "exit /b 0"
 copy %LIBRARY_LIB%\zlibstatic.lib lib\z.lib
 copy %LIBRARY_LIB%\libpng_static.lib lib\png.lib
 
 :: Make the header files and the rest of the static libs available during the build
-:: CONDA_DEFAULT_ENV is a env variable which is set to the currently active environment path
-set MPLBASEDIRLIST=%CONDA_DEFAULT_ENV%\Library\;.
+:: CONDA_PREFIX is a env variable which is set to the currently active environment path
+set MPLBASEDIRLIST=%CONDA_PREFIX%\Library\;.
 
 :: build the target
 python setup.py %TARGET%
