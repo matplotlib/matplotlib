70b851fef0ca4044c16f2802090a2a1c1f059f01
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2016-07-04 16:10:56 -0700

    Merge pull request #6688 from afvincent/fix_polar_scatter_demo
    DOC: fix radial increase of size & OO style in polar_scatter_demo

--- examples/pie_and_polar_charts/polar_scatter_demo.py
+++ examples/pie_and_polar_charts/polar_scatter_demo.py
@@ -1,8 +1,8 @@
 """
 Demo of scatter plot on a polar axis.
 
-Size increases radially in this example and color increases with angle (just to
-verify the symbols are being scattered correctly).
+Size increases radially in this example and color increases with angle
+(just to verify the symbols are being scattered correctly).
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,11 +11,11 @@ import matplotlib.pyplot as plt
 N = 150
 r = 2 * np.random.rand(N)
 theta = 2 * np.pi * np.random.rand(N)
-area = 200 * r**2 * np.random.rand(N)
+area = 200 * r**2
 colors = theta
 
 ax = plt.subplot(111, projection='polar')
-c = plt.scatter(theta, r, c=colors, s=area, cmap=plt.cm.hsv)
+c = ax.scatter(theta, r, c=colors, s=area, cmap=plt.cm.hsv)
 c.set_alpha(0.75)
 
 plt.show()
