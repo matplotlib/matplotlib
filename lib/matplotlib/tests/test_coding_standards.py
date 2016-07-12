5613f2bf4bcadd1f28235e153c92b4de8ed1074a
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2016-07-12 09:08:31 +0100

    compat/subprocess is now pep8 complient

--- lib/matplotlib/tests/test_coding_standards.py
+++ lib/matplotlib/tests/test_coding_standards.py
@@ -217,7 +217,6 @@ def test_pep8_conformance_installed_files():
                           'tests/test_subplots.py',
                           'tests/test_tightlayout.py',
                           'tests/test_triangulation.py',
-                          'compat/subprocess.py',
                           'backends/__init__.py',
                           'backends/backend_agg.py',
                           'backends/backend_cairo.py',
