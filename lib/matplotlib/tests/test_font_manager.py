eecdf69bb2011fc86f96a7a5883f4c178a30fdbb
Thomas A Caswell <tcaswell@gmail.com>
2016-07-11 21:56:22 -0500

    TST: add test for CFF / OTF files
    follow up to #6714

--- lib/matplotlib/tests/test_font_manager.py
+++ lib/matplotlib/tests/test_font_manager.py
@@ -5,8 +5,9 @@ from nose.tools import assert_equal
 import six
 
 import os
-
-from matplotlib.font_manager import (findfont, FontProperties, get_font)
+import os.path
+from matplotlib.font_manager import (findfont, FontProperties, get_font,
+                                     is_opentype_cff_font, fontManager as fm)
 from matplotlib import rc_context
 
 
@@ -23,3 +24,15 @@ def test_font_priority():
     cmap = font.get_charmap()
     assert len(cmap) == 131
     assert cmap[8729] == 30
+
+
+def test_otf():
+    fname = '/usr/share/fonts/opentype/freefont/FreeMono.otf'
+    if os.path.exists(fname):
+        assert is_opentype_cff_font(fname)
+
+    otf_files = [f for f in fm.ttffiles if 'otf' in f]
+    for f in otf_files:
+        with open(f, 'rb') as fd:
+            res = fd.read(4) == b'OTTO'
+        assert res == is_opentype_cff_font(f)
