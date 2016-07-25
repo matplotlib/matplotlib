6a2ffbb260d14beeb822701cf4b47e9ffa4d610d
Thomas A Caswell <tcaswell@gmail.com>
2016-07-25 15:10:24 -0400

    Merge pull request #6817 from afvincent/typo_pyplot_scales_demo
    DOC: Fix a few typos and formulations

--- doc/pyplots/pyplot_scales.py
+++ doc/pyplots/pyplot_scales.py
@@ -41,11 +41,11 @@ plt.plot(x, y)
 plt.yscale('logit')
 plt.title('logit')
 plt.grid(True)
-# Format the minor tick labels of the y-axis into empty string with
-# the `NullFormatter`, to avoid cumbering it with too many labels.
+# Format the minor tick labels of the y-axis into empty strings with
+# `NullFormatter`, to avoid cumbering the axis with too many labels.
 plt.gca().yaxis.set_minor_formatter(NullFormatter())
-# Adjust the subplot layout, because the logit one may take more space than
-# usual, due to y-tick labels like "1 - 10^3"
+# Adjust the subplot layout, because the logit one may take more space
+# than usual, due to y-tick labels like "1 - 10^{-3}"
 plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
                     wspace=0.35)
 
