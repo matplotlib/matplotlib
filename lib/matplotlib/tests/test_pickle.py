c9c2bfe2fc7bcde5f26b151c1a3c0f9940bb5ebd
Thomas A Caswell <tcaswell@gmail.com>
2016-07-31 00:01:51 -0400

    Revert "Merge pull request #6840 from Kojoley/fix-test_pickle-test_complete"
    This reverts commit aaeaffd9a0bc7d02cf3eab1566b41beac55b95e8.

--- lib/matplotlib/tests/test_pickle.py
+++ lib/matplotlib/tests/test_pickle.py
@@ -127,6 +127,7 @@ def test_simple():
     pickle.dump(fig, BytesIO(), pickle.HIGHEST_PROTOCOL)
 
 
+@cleanup
 @image_comparison(baseline_images=['multi_pickle'],
                   extensions=['png'], remove_text=True)
 def test_complete():
