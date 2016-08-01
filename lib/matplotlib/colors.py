Merge pull request #6837 from anntzer/fix-integer-normalization
FIX: Normalize(<signed integer array>).

closes #6825

lib/matplotlib/colors.py
@@ -811,8 +811,9 @@ class Normalize(object):
         if is_scalar:
             value = [value]
         dtype = np.min_scalar_type(value)
-        dtype = (np.float32 if dtype.itemsize <= 2
-                 else np.promote_types(dtype, float))
+        if np.issubdtype(dtype, np.integer) or dtype.type is np.bool_:
+            # bool_/int8/int16 -> float32; int32/int64 -> float64
+            dtype = np.promote_types(dtype, np.float32)
         result = np.ma.array(value, dtype=dtype, copy=True)
         return result, is_scalar
 
@@ -830,7 +831,9 @@ class Normalize(object):
         result, is_scalar = self.process_value(value)
 
         self.autoscale_None(result)
-        vmin, vmax = self.vmin, self.vmax
+        # Convert at least to float, without losing precision.
+        (vmin,), _ = self.process_value(self.vmin)
+        (vmax,), _ = self.process_value(self.vmax)
         if vmin == vmax:
             result.fill(0)   # Or should it be all masked?  Or 0.5?
         elif vmin > vmax:
@@ -854,7 +857,8 @@ class Normalize(object):
     def inverse(self, value):
         if not self.scaled():
             raise ValueError("Not invertible until scaled")
-        vmin, vmax = self.vmin, self.vmax
+        (vmin,), _ = self.process_value(self.vmin)
+        (vmax,), _ = self.process_value(self.vmax)
 
         if cbook.iterable(value):
             val = ma.asarray(value)