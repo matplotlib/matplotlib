05392052102ac1615c11e22d8827f3e0111871a1
Heiko Oberdiek <heiko.oberdiek@iqs.de>
2016-08-02 10:32:43 +0200

    Line break in title now uses implicit string concatenation. Also the quotes are changed to single quotes to follow the quote symbol of the other strings.

--- examples/pylab_examples/tex_unicode_demo.py
+++ examples/pylab_examples/tex_unicode_demo.py
@@ -19,8 +19,7 @@ plt.plot(t, s)
 
 plt.xlabel(r'\textbf{time (s)}')
 plt.ylabel('\\textit{Velocity (\u00B0/sec)}', fontsize=16)
-plt.title(r"\TeX\ is Number \
-          $\displaystyle\sum_{n=1}^\infty\frac{-e^{i\pi}}{2^n}$!",
-          fontsize=16, color='r')
+plt.title(r'\TeX\ is Number $\displaystyle\sum_{n=1}^\infty'
+          r'\frac{-e^{i\pi}}{2^n}$!', fontsize=16, color='r')
 plt.grid(True)
 plt.show()
