beaaff52e25803e0c8ffb8d71fec0c221d6aec6f
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2016-08-07 20:58:27 +0100

    Add missing $

--- lib/matplotlib/tests/test_usetex.py
+++ lib/matplotlib/tests/test_usetex.py
@@ -16,7 +16,7 @@ def test_usetex():
             # \left[ ... \right\} draw some variable-height characters,
             # \sqrt and \frac draw horizontal rules, \mathrm changes the font
             r'\LaTeX\ $\left[\int\limits_e^{2e}'
-            r'\sqrt\frac{\log^3 x}{x}\,\mathrm{d}x \right\}',
+            r'\sqrt\frac{\log^3 x}{x}\,\mathrm{d}x \right\}$',
             fontsize=24)
     ax.set_xticks([])
     ax.set_yticks([])
