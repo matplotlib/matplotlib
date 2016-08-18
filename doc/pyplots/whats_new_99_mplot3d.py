3e25df74ba61e72a559afc8c109e966fa5540e1f
Paul Hobson <pmhobson@gmail.com>
2016-08-18 16:11:04 -0700

    DOC: move a few examples away from pylab

--- doc/pyplots/whats_new_99_mplot3d.py
+++ doc/pyplots/whats_new_99_mplot3d.py
@@ -1,17 +1,18 @@
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-import pylab
 import random
-import numpy as np
 
-fig = pylab.figure()
-ax = Axes3D(fig)
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
 X = np.arange(-5, 5, 0.25)
 Y = np.arange(-5, 5, 0.25)
 X, Y = np.meshgrid(X, Y)
 R = np.sqrt(X**2 + Y**2)
 Z = np.sin(R)
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet)
 
-pylab.show()
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.viridis)
 
+plt.show()
