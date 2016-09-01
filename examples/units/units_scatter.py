d5af573389cc0b4985a2d341b9039abc0dca0b49
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2016-08-31 22:15:59 -0400

    Merge pull request #6957 from phobson/partial-update-pylab-examples
    DOC: clearing out some instances of using pylab in the docs

--- examples/units/units_scatter.py
+++ examples/units/units_scatter.py
@@ -9,28 +9,25 @@ The example below shows support for unit conversions over masked
 arrays.
 """
 import numpy as np
+import matplotlib.pyplot as plt
 from basic_units import secs, hertz, minutes
-from matplotlib.pylab import figure, show
 
 # create masked array
+data = (1, 2, 3, 4, 5, 6, 7, 8)
+mask = (1, 0, 1, 0, 0, 0, 1, 0)
+xsecs = secs * np.ma.MaskedArray(data, mask, float)
 
-
-xsecs = secs*np.ma.MaskedArray((1, 2, 3, 4, 5, 6, 7, 8), (1, 0, 1, 0, 0, 0, 1, 0), np.float)
-#xsecs = secs*np.arange(1,10.)
-
-fig = figure()
-ax1 = fig.add_subplot(3, 1, 1)
+fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, sharex=True)
 ax1.scatter(xsecs, xsecs)
-#ax1.set_ylabel('seconds')
+ax1.yaxis.set_units(secs)
 ax1.axis([0, 10, 0, 10])
 
-ax2 = fig.add_subplot(3, 1, 2, sharex=ax1)
 ax2.scatter(xsecs, xsecs, yunits=hertz)
 ax2.axis([0, 10, 0, 1])
 
-ax3 = fig.add_subplot(3, 1, 3, sharex=ax1)
 ax3.scatter(xsecs, xsecs, yunits=hertz)
 ax3.yaxis.set_units(minutes)
 ax3.axis([0, 10, 0, 1])
 
-show()
+fig.tight_layout()
+plt.show()
