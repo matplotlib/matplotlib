19cb23d5dddac7df1845aeb986c10ebba9a24582
Hakan Kucukdereli <hkucukdereli@gmail.com>
2016-09-07 18:08:35 +0100

    Documentation changes to _axes.py

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -1832,7 +1832,6 @@ class Axes(_AxesBase):
         ----
         Additional parameters are the same as those for 
         :func:`~matplotlib.pyplot.plot`.
-
         """
 
         where = kwargs.pop('where', 'pre')
