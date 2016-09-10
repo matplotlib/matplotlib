aabd045d50fad5c37e7b60b2531151f6e5a4d950
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2016-09-10 10:00:13 +0100

    Add tick locator example

---
+++ examples/ticks_and_spines/tick-locators.py
@@ -0,0 +1,81 @@
+# Show the different tick locators
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+# Setup a plot such that only the bottom spine is shown
+def setup(ax):
+    ax.spines['right'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.yaxis.set_major_locator(ticker.NullLocator())
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.tick_params(which='major', width=1.00)
+    ax.tick_params(which='major', length=5)
+    ax.tick_params(which='minor', width=0.75)
+    ax.tick_params(which='minor', length=2.5)
+    ax.set_xlim(0, 5)
+    ax.set_ylim(0, 1)
+    ax.patch.set_alpha(0.0)
+
+
+plt.figure(figsize=(8, 8))
+n = 7
+
+# Null Locator
+ax = plt.subplot(n, 1, 1)
+setup(ax)
+ax.xaxis.set_major_locator(ticker.NullLocator())
+ax.xaxis.set_minor_locator(ticker.NullLocator())
+ax.text(0.0, 0.5, "Null locator", fontsize=16, transform=ax.transAxes)
+
+# Multiple Locator
+ax = plt.subplot(n, 1, 2)
+setup(ax)
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1.0))
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.5))
+ax.text(0.0, 0.5, "Multiple locator", fontsize=16, transform=ax.transAxes)
+
+# Fixed Locator
+ax = plt.subplot(n, 1, 3)
+setup(ax)
+majors = [0, 1, 5]
+ax.xaxis.set_major_locator(ticker.FixedLocator(majors))
+minors = np.linspace(0, 1, 11)[1:-1]
+ax.xaxis.set_minor_locator(ticker.FixedLocator(minors))
+ax.text(0.0, 0.5, "Fixed locator", fontsize=16, transform=ax.transAxes)
+
+# Linear Locator
+ax = plt.subplot(n, 1, 4)
+setup(ax)
+ax.xaxis.set_major_locator(ticker.LinearLocator(3))
+ax.xaxis.set_minor_locator(ticker.LinearLocator(31))
+ax.text(0.0, 0.5, "Linear locator", fontsize=16, transform=ax.transAxes)
+
+# Auto Locator
+ax = plt.subplot(n, 1, 5)
+setup(ax)
+ax.xaxis.set_major_locator(ticker.AutoLocator())
+ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
+ax.text(0.0, 0.5, "Auto locator", fontsize=16, transform=ax.transAxes)
+
+# MaxN Locator
+ax = plt.subplot(n, 1, 6)
+setup(ax)
+ax.xaxis.set_major_locator(ticker.MaxNLocator(5))
+ax.xaxis.set_minor_locator(ticker.MaxNLocator(50))
+ax.text(0.0, 0.5, "MaxN locator", fontsize=16, transform=ax.transAxes)
+
+# Log Locator
+ax = plt.subplot(n, 1, 7)
+setup(ax)
+ax.set_xlim(10**3, 10**10)
+ax.set_xscale('log')
+ax.xaxis.set_major_locator(ticker.LogLocator(base=10.0, numticks=15))
+ax.text(0.0, 0.5, "Log locator", fontsize=16, transform=ax.transAxes)
+
+plt.tight_layout()
+# plt.savefig("tick-locators.pdf")
+# plt.savefig("tick-locators.png", dpi=150)
+plt.show()
