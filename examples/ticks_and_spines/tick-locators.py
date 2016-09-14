b784528a0cc0cc1ec261aacc72a8aa82ff638e8c
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2016-09-14 17:11:03 +0200

    Changed index locator range

--- examples/ticks_and_spines/tick-locators.py
+++ examples/ticks_and_spines/tick-locators.py
@@ -60,10 +60,9 @@ ax.text(0.0, 0.5, "LinearLocator(3)", fontsize=14, transform=ax.transAxes)
 # Index Locator
 ax = plt.subplot(n, 1, 5)
 setup(ax)
-ax.set_xlim(5, 55)
-ax.plot(range(5, 55), [0]*50, color='White')
-ax.xaxis.set_major_locator(ticker.IndexLocator(base=5, offset=2))
-ax.text(0.0, 0.5, "IndexLocator (base=5, offset=2)",
+ax.plot(range(0, 5), [0]*5, color='White')
+ax.xaxis.set_major_locator(ticker.IndexLocator(base=.5, offset=.25))
+ax.text(0.0, 0.5, "IndexLocator(base=0.5, offset=0.25)",
         fontsize=14, transform=ax.transAxes)
 
 # Auto Locator
@@ -78,7 +77,7 @@ ax = plt.subplot(n, 1, 7)
 setup(ax)
 ax.xaxis.set_major_locator(ticker.MaxNLocator(4))
 ax.xaxis.set_minor_locator(ticker.MaxNLocator(40))
-ax.text(0.0, 0.5, "MaxNLocator (n=4)", fontsize=14, transform=ax.transAxes)
+ax.text(0.0, 0.5, "MaxNLocator(n=4)", fontsize=14, transform=ax.transAxes)
 
 # Log Locator
 ax = plt.subplot(n, 1, 8)
@@ -86,7 +85,7 @@ setup(ax)
 ax.set_xlim(10**3, 10**10)
 ax.set_xscale('log')
 ax.xaxis.set_major_locator(ticker.LogLocator(base=10.0, numticks=15))
-ax.text(0.0, 0.5, "LogLocator(10,15)", fontsize=15, transform=ax.transAxes)
+ax.text(0.0, 0.5, "LogLocator(base=10, numticks=15)", fontsize=15, transform=ax.transAxes)
 
 plt.tight_layout()
 # plt.savefig("tick-locators.pdf")
