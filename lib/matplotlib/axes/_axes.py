aaca81daf7190f6613604344cb051f916ced4e9c
Benjamin Congdon <bencongdon96@gmail.com>
2016-09-27 06:32:26 -0500

    Remove hard-coded streamplot zorder and let `mlinesLine2D.zorder` to be the default

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -4613,9 +4613,12 @@ or tuple of floats
                          label_namer=None)
     def streamplot(self, x, y, u, v, density=1, linewidth=None, color=None,
                    cmap=None, norm=None, arrowsize=1, arrowstyle='-|>',
-                   minlength=0.1, transform=None, zorder=2, start_points=None):
+                   minlength=0.1, transform=None, zorder=None,
+                   start_points=None):
         if not self._hold:
             self.cla()
+        if zorder is None:
+            zorder = mlines.Line2D.zorder
         stream_container = mstream.streamplot(self, x, y, u, v,
                                               density=density,
                                               linewidth=linewidth,
