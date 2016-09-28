86bf3359d5af20fe912f5e75915e820d6900cfd1
Adrien VINCENT <vincent.adrien@gmail.com>
2016-09-28 10:24:21 +0200

    DOC: switch to pyplot OO interface

--- examples/pylab_examples/mri_with_eeg.py
+++ examples/pylab_examples/mri_with_eeg.py
@@ -6,33 +6,36 @@ faster*
 from __future__ import division, print_function
 
 import numpy as np
-
-from matplotlib.pyplot import *
-from matplotlib.collections import LineCollection
+import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
+import matplotlib.cm as cm
+
+from matplotlib.collections import LineCollection
 # I use if 1 to break up the different regions of code visually
+
+fig = plt.figure("MRI_with_EEG")
 
 if 1:   # load the data
     # data are 256x256 16 bit integers
     dfile = cbook.get_sample_data('s1045.ima.gz')
     im = np.fromstring(dfile.read(), np.uint16).astype(float)
-    im.shape = 256, 256
+    im.shape = (256, 256)
 
 if 1:  # plot the MRI in pcolor
-    subplot(221)
-    imshow(im, cmap=cm.gray)
-    axis('off')
+    ax0 = fig.add_subplot(2, 2, 1)
+    ax0.imshow(im, cmap=cm.gray)
+    ax0.axis('off')
 
 if 1:  # plot the histogram of MRI intensity
-    subplot(222)
+    ax1 = fig.add_subplot(2, 2, 2)
     im = np.ravel(im)
     im = im[np.nonzero(im)]  # ignore the background
-    im = im/(2.0**15)  # normalize
-    hist(im, 100)
-    xticks([-1, -.5, 0, .5, 1])
-    yticks([])
-    xlabel('intensity')
-    ylabel('MRI density')
+    im = im / (2**15)  # normalize
+    ax1.hist(im, 100)
+    ax1.set_xticks([-1, -0.5, 0, 0.5, 1])
+    ax1.set_yticks([])
+    ax1.set_xlabel('intensity')
+    ax1.set_ylabel('MRI density')
 
 if 1:   # plot the EEG
     # load the data
@@ -41,37 +44,34 @@ if 1:   # plot the EEG
     eegfile = cbook.get_sample_data('eeg.dat', asfileobj=False)
     print('loading eeg %s' % eegfile)
     data = np.fromstring(open(eegfile, 'rb').read(), float)
-    data.shape = numSamples, numRows
-    t = 10.0 * np.arange(numSamples, dtype=float)/numSamples
+    data.shape = (numSamples, numRows)
+    t = 10.0 * np.arange(numSamples) / numSamples
     ticklocs = []
-    ax = subplot(212)
-    xlim(0, 10)
-    xticks(np.arange(10))
+    ax2 = fig.add_subplot(2, 1, 2)
+    ax2.set_xlim(0, 10)
+    ax2.set_xticks(np.arange(10))
     dmin = data.min()
     dmax = data.max()
-    dr = (dmax - dmin)*0.7  # Crowd them a bit.
+    dr = (dmax - dmin) * 0.7  # Crowd them a bit.
     y0 = dmin
     y1 = (numRows - 1) * dr + dmax
-    ylim(y0, y1)
+    ax2.set_ylim(y0, y1)
 
     segs = []
     for i in range(numRows):
         segs.append(np.hstack((t[:, np.newaxis], data[:, i, np.newaxis])))
-        ticklocs.append(i*dr)
+        ticklocs.append(i * dr)
 
     offsets = np.zeros((numRows, 2), dtype=float)
     offsets[:, 1] = ticklocs
 
-    lines = LineCollection(segs, offsets=offsets,
-                           transOffset=None,
-                           )
-
-    ax.add_collection(lines)
+    lines = LineCollection(segs, offsets=offsets, transOffset=None)
+    ax2.add_collection(lines)
 
     # set the yticks to use axes coords on the y axis
-    ax.set_yticks(ticklocs)
-    ax.set_yticklabels(['PG3', 'PG5', 'PG7', 'PG9'])
+    ax2.set_yticks(ticklocs)
+    ax2.set_yticklabels(['PG3', 'PG5', 'PG7', 'PG9'])
 
-    xlabel('time (s)')
+    ax2.set_xlabel('time (s)')
 
-show()
+plt.show()
