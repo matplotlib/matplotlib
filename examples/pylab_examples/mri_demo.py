21ee5eb65f0a94a5492cb0ee4fc9682f28a98782
Adrien F Vincent <vincent.adrien@gmail.com>
2016-10-02 11:52:02 +0200

    Switch mri_demo.py to OO interface

--- examples/pylab_examples/mri_demo.py
+++ examples/pylab_examples/mri_demo.py
@@ -2,7 +2,10 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
+import matplotlib.cm as cm
 import numpy as np
+
+fig, ax = plt.subplots(num="MRI_demo")
 
 # Data are 256x256 16 bit integers
 dfile = cbook.get_sample_data('s1045.ima.gz')
@@ -10,7 +13,7 @@ im = np.fromstring(dfile.read(), np.uint16).astype(float)
 im.shape = (256, 256)
 dfile.close()
 
-plt.imshow(im, cmap=plt.cm.gray)
-plt.axis('off')
+ax.imshow(im, cmap=cm.gray)
+ax.axis('off')
 
 plt.show()
