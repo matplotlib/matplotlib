17fab809cd863571be2dcc0c07209971312cd440
Thomas A Caswell <tcaswell@gmail.com>
2016-10-03 19:01:48 -0400

    Merge pull request #7192 from afvincent/doc_pylab_examples_mri_with_eeg
    DOC: switch pylab example `mri_with_eeg.py` to OO interface + cosmetic fixes

--- examples/pylab_examples/mri_demo.py
+++ examples/pylab_examples/mri_demo.py
@@ -1,13 +1,19 @@
-from __future__ import print_function
+"""Displays an MRI image."""
+
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
+import matplotlib.cm as cm
 import numpy as np
-# data are 256x256 16 bit integers
+
+fig, ax = plt.subplots(num="MRI_demo")
+
+# Data are 256x256 16 bit integers
 dfile = cbook.get_sample_data('s1045.ima.gz')
 im = np.fromstring(dfile.read(), np.uint16).astype(float)
-im.shape = 256, 256
+im.shape = (256, 256)
+dfile.close()
 
-plt.imshow(im, cmap=plt.cm.gray)
-plt.axis('off')
+ax.imshow(im, cmap=cm.gray)
+ax.axis('off')
 
 plt.show()
