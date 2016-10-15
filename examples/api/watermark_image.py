3f80a06c2bf3fb916cece16234dca6281326a08d
AndersonDaniel <Daniel.Anderson.D@gmail.com>
2016-10-15 19:15:47 +0300

    Fix image watermark example where image was hidden by axes (#7265)

--- examples/api/watermark_image.py
+++ examples/api/watermark_image.py
@@ -16,6 +16,6 @@ fig, ax = plt.subplots()
 
 ax.plot(np.random.rand(20), '-o', ms=20, lw=2, alpha=0.7, mfc='orange')
 ax.grid()
-fig.figimage(im, 10, 10)
+fig.figimage(im, 10, 10, zorder=3)
 
 plt.show()
