55465523622f27b33fc14a9baafb9197f7aecb69
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2016-11-27 10:40:55 -0800

    Merge pull request #7520 from dstansby/table-bug
    Fix table.py bug

--- lib/matplotlib/tests/test_table.py
+++ lib/matplotlib/tests/test_table.py
@@ -5,11 +5,18 @@ import six
 
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.testing.decorators import image_comparison
+from matplotlib.testing.decorators import image_comparison, cleanup
 
 from matplotlib.table import CustomCell
 from matplotlib.path import Path
 from nose.tools import assert_equal
+
+
+@cleanup
+def test_non_square():
+    # Check that creating a non-square table works
+    cellcolors = ['b', 'r']
+    plt.table(cellColours=cellcolors)
 
 
 @image_comparison(baseline_images=['table_zorder'],
