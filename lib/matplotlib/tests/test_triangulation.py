4300eaded63d57e11eb0dd505fb85ee5b3f58421
Eric Firing <efiring@hawaii.edu>
2016-11-28 12:05:38 -1000

    test_triangulation: increase tolerance on Windows

--- lib/matplotlib/tests/test_triangulation.py
+++ lib/matplotlib/tests/test_triangulation.py
@@ -773,7 +773,7 @@ def test_tri_smooth_contouring():
 
 @image_comparison(baseline_images=['tri_smooth_gradient'],
                   extensions=['png'], remove_text=True,
-                  tol=0.015 if on_win else 0)
+                  tol=0.03 if on_win else 0)
 def test_tri_smooth_gradient():
     # Image comparison based on example trigradient_demo.
 
