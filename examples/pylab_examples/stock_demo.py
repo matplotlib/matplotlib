4954a4557c71ca34d7b615d74dae0537b5da3c16
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2016-11-29 14:22:42 -0800

    FIX the example now works properly + formatting

--- examples/pylab_examples/stock_demo.py
+++ examples/pylab_examples/stock_demo.py
@@ -1,18 +1,21 @@
 import matplotlib.pyplot as plt
+"""
+================
+Stock Demo Plots
+================
+
+The following example displays Matplotlibs capabilities of creating
+graphs that can be used for stocks. The example specfically uses
+Apple and Intel stock data and graphs the normalized prices on the
+same plot.
+"""
+
+
 import numpy as np
 
 import matplotlib.cbook as cbook
 from matplotlib.ticker import MultipleLocator
 
-"""
-Stock Demo Plots
-
-The following example displays Matplotlibs capabilities of creating
-graphs that can be used for stocks. The example specfically uses
-Apple and Intel stock data and graphs the normalized prices on the
-same plot. 
-
-"""
 
 def get_two_stock_data():
     """
@@ -20,32 +23,31 @@ def get_two_stock_data():
     (d1,p1,d2,p2) are the trade time (in days) and prices for stocks 1
     and 2 (intc and aapl)
     """
-    ticker1, ticker2 = 'INTC', 'AAPL'
-
     file1 = cbook.get_sample_data('INTC.dat.gz')
     file2 = cbook.get_sample_data('AAPL.dat.gz')
-    M1 = fromstring(file1.read(), '<d')
+    M1 = np.fromstring(file1.read(), '<d')
 
-    M1 = resize(M1, (M1.shape[0]//2, 2))
+    M1 = np.resize(M1, (M1.shape[0]//2, 2))
 
-    M2 = fromstring(file2.read(), '<d')
-    M2 = resize(M2, (M2.shape[0]//2, 2))
+    M2 = np.fromstring(file2.read(), '<d')
+    M2 = np.resize(M2, (M2.shape[0]//2, 2))
 
     d1, p1 = M1[:, 0], M1[:, 1]
     d2, p2 = M2[:, 0], M2[:, 1]
     return (d1, p1, d2, p2)
 
+
 d1, p1, d2, p2 = get_two_stock_data()
 
 fig, ax = plt.subplots()
-lines1 = plt.plot(d1, p1, 'b', label="INTC")
-lines2 = plt.plot(d2, p2, 'r', label="AAPL")
-plt.xlabel('Days')
-plt.ylabel('Normalized price')
-plt.xlim(0, 3)
+lines1 = ax.plot(d1, p1, 'b', label="INTC")
+lines2 = ax.plot(d2, p2, 'r', label="AAPL")
+ax.set_xlabel('Days')
+ax.set_ylabel('Normalized price')
+ax.set_xlim(0, 3)
 ax.xaxis.set_major_locator(MultipleLocator(1))
 
-plt.title('INTC vs AAPL')
-plt.legend()
+ax.set_title('INTC vs AAPL')
+ax.legend()
 
 plt.show()
