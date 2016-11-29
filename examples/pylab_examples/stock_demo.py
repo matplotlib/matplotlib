fd1406321d475c271adbdd5e3fb92423138e7acc
Salil Vanvari <salilvanvari@berkeley.edu>
2016-11-28 22:13:06 -0800

    fixed style to more greatly represent style

--- examples/pylab_examples/stock_demo.py
+++ examples/pylab_examples/stock_demo.py
@@ -7,13 +7,14 @@ from data_helper import get_two_stock_data
 d1, p1, d2, p2 = get_two_stock_data()
 
 fig, ax = plt.subplots()
-lines = plt.plot(d1, p1, 's', d2, p2, 'o')
+lines1 = plt.plot(d1, p1, 'b', label="INTC")
+lines2 = plt.plot(d2, p2, 'r', label="AAPL")
 plt.xlabel('Days')
 plt.ylabel('Normalized price')
 plt.xlim(0, 3)
 ax.xaxis.set_major_locator(MultipleLocator(1))
 
 plt.title('INTC vs AAPL')
-plt.legend(('INTC', 'AAPL'))
+plt.legend()
 
 plt.show()
