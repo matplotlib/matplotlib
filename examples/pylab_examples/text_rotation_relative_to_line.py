4b6fe150694d0293b0f56210c55f116be28fb1e5
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2016-12-04 02:20:13 -0500

    Use anchor rotation_mode in text rotation example.
    This keeps the location in a relatively normal place. Also, fix the text
    to actually be different between the two objects.

--- examples/pylab_examples/text_rotation_relative_to_line.py
+++ examples/pylab_examples/text_rotation_relative_to_line.py
@@ -26,12 +26,12 @@ l2 = np.array((5, 5))
 # Rotate angle
 angle = 45
 trans_angle = plt.gca().transData.transform_angles(np.array((45,)),
-                                               l2.reshape((1, 2)))[0]
+                                                   l2.reshape((1, 2)))[0]
 
 # Plot text
 th1 = plt.text(l1[0], l1[1], 'text not rotated correctly', fontsize=16,
-           rotation=angle)
-th2 = plt.text(l2[0], l2[1], 'text not rotated correctly', fontsize=16,
-           rotation=trans_angle)
+               rotation=angle, rotation_mode='anchor')
+th2 = plt.text(l2[0], l2[1], 'text rotated correctly', fontsize=16,
+               rotation=trans_angle, rotation_mode='anchor')
 
 plt.show()
