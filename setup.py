171d07067de36fe1c10dcd7a000cfa50791e09a3
Christoph Deil <Deil.Christoph@gmail.com>
2016-12-31 15:22:00 +0100

    Declare Python 3.6 support via classifier in setup.py

--- setup.py
+++ setup.py
@@ -128,6 +128,7 @@ classifiers = [
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Topic :: Scientific/Engineering :: Visualization',
     ]
 
