dda23a54e6f474104c3439983571cb82904b396c
Naoya Kanai <naopon@gmail.com>
2017-01-03 22:18:05 -0800

    Save PNG in correct orientation with imsave

--- lib/matplotlib/image.py
+++ lib/matplotlib/image.py
@@ -521,7 +521,8 @@ class _ImageBase(martist.Artist, cm.ScalarMappable):
 
     def write_png(self, fname):
         """Write the image to png file with fname"""
-        im = self.to_rgba(self._A, bytes=True, norm=True)
+        im = self.to_rgba(self._A[::-1] if self.origin == 'lower' else self._A,
+                          bytes=True, norm=True)
         _png.write_png(im, fname)
 
     def set_data(self, A):
