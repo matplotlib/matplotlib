698e5bf6e1e11aeb3c3dba9db28e30860d7c6c55
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2017-01-15 19:02:11 -0800

    Merge pull request #7821 from vollbier/remove_showcase_piechart
    DOC: Changes to screenshots plots.

--- examples/pylab_examples/scatter_demo2.py
+++ examples/pylab_examples/scatter_demo2.py
@@ -29,8 +29,8 @@ close = 0.003 * price_data.close[:-2] / 0.003 * price_data.open[:-2]
 fig, ax = plt.subplots()
 ax.scatter(delta1[:-1], delta1[1:], c=close, s=volume, alpha=0.5)
 
-ax.set_xlabel(r'$\Delta_i$', fontsize=20)
-ax.set_ylabel(r'$\Delta_{i+1}$', fontsize=20)
+ax.set_xlabel(r'$\Delta_i$', fontsize=15)
+ax.set_ylabel(r'$\Delta_{i+1}$', fontsize=15)
 ax.set_title('Volume and percent change')
 
 ax.grid(True)
