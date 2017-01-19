d353c71864257d0fcf2c21501f1e47ace8bcfb9a
Björn Dahlgren <bjodah@gmail.com>
2017-01-19 19:25:42 +0100

    Update vlines example with axes wide lines.

--- examples/pylab_examples/vline_hline_demo.py
+++ examples/pylab_examples/vline_hline_demo.py
@@ -3,6 +3,7 @@ Small demonstration of the hlines and vlines plots.
 """
 
 import matplotlib.pyplot as plt
+from matplotlib.transforms import blended_transform_factory as btf
 import numpy as np
 import numpy.random as rnd
 
@@ -22,6 +23,7 @@ hax = fig.add_subplot(122)
 
 vax.plot(t, s + nse, '^')
 vax.vlines(t, [0], s)
+vax.vlines([1, 2], 0, 1, transform=btf(vax.transData, vax.transAxes), colors='r')
 vax.set_xlabel('time (s)')
 vax.set_title('Vertical lines demo')
 
