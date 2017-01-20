32b93cd3697d80ee6d3aa949ea48244dc20c909a
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2017-01-20 11:37:32 -0800

    MAINT moved test_image to pytest

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -1498,7 +1498,6 @@ default_test_modules = [
     'matplotlib.tests.test_figure',
     'matplotlib.tests.test_font_manager',
     'matplotlib.tests.test_gridspec',
-    'matplotlib.tests.test_image',
     'matplotlib.tests.test_legend',
     'matplotlib.tests.test_lines',
     'matplotlib.tests.test_mathtext',
--- lib/matplotlib/tests/test_gridspec.py
+++ lib/matplotlib/tests/test_gridspec.py
@@ -1,5 +1,5 @@
 import matplotlib.gridspec as gridspec
-from nose.tools import assert_raises, assert_equal
+from numpy.testing import assert_raises, assert_equal
 
 
 def test_equal():
