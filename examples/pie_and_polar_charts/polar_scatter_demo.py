69a634a791878cd69d43a4c2ee671b3f9ee623ae
Naoya Kanai <naopon@gmail.com>
2017-01-20 11:41:24 -0800

    MEP12ify pie and polar chart examples

--- examples/pie_and_polar_charts/polar_scatter_demo.py
+++ examples/pie_and_polar_charts/polar_scatter_demo.py
@@ -1,4 +1,8 @@
 """
+==========================
+Scatter plot on polar axis
+==========================
+
 Demo of scatter plot on a polar axis.
 
 Size increases radially in this example and color increases with angle
@@ -10,7 +14,7 @@ import matplotlib.pyplot as plt
 # Fixing random state for reproducibility
 np.random.seed(19680801)
 
-
+# Compute areas and colors
 N = 150
 r = 2 * np.random.rand(N)
 theta = 2 * np.pi * np.random.rand(N)
@@ -18,7 +22,6 @@ area = 200 * r**2
 colors = theta
 
 ax = plt.subplot(111, projection='polar')
-c = ax.scatter(theta, r, c=colors, s=area, cmap=plt.cm.hsv)
-c.set_alpha(0.75)
+c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.75)
 
 plt.show()
