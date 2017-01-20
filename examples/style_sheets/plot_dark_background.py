69eee924c89a54a1e8ac287637d549a8d485ad69
Naoya Kanai <naopon@gmail.com>
2017-01-20 01:40:16 -0800

    Convert style sheet examples to MEP12

--- examples/style_sheets/plot_dark_background.py
+++ examples/style_sheets/plot_dark_background.py
@@ -1,7 +1,11 @@
 """
+===========================
+Dark background style sheet
+===========================
+
 This example demonstrates the "dark_background" style, which uses white for
-elements that are typically black (text, borders, etc). Note, however, that not
-all plot elements default to colors defined by an rc parameter.
+elements that are typically black (text, borders, etc). Note that not all plot
+elements default to colors defined by an rc parameter.
 
 """
 import numpy as np
@@ -10,14 +14,16 @@ import matplotlib.pyplot as plt
 
 plt.style.use('dark_background')
 
+fig, ax = plt.subplots()
+
 L = 6
 x = np.linspace(0, L)
 ncolors = len(plt.rcParams['axes.prop_cycle'])
 shift = np.linspace(0, L, ncolors, endpoint=False)
 for s in shift:
-    plt.plot(x, np.sin(x + s), 'o-')
-plt.xlabel('x-axis')
-plt.ylabel('y-axis')
-plt.title('title')
+    ax.plot(x, np.sin(x + s), 'o-')
+ax.set_xlabel('x-axis')
+ax.set_ylabel('y-axis')
+ax.set_title("'dark_background' style sheet")
 
 plt.show()
