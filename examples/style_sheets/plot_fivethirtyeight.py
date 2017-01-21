14cd2339b11e05af5905331f9a2d2939b49719a2
Paul Hobson <pmhobson@gmail.com>
2017-01-20 19:32:50 -0800

    Merge pull request #7890 from naoyak/mep12-style-sheets
    DOC: Convert style sheet examples to MEP12

--- examples/style_sheets/plot_fivethirtyeight.py
+++ examples/style_sheets/plot_fivethirtyeight.py
@@ -1,4 +1,8 @@
 """
+===========================
+FiveThirtyEight style sheet
+===========================
+
 This shows an example of the "fivethirtyeight" styling, which
 tries to replicate the styles from FiveThirtyEight.com.
 """
@@ -9,13 +13,19 @@ import numpy as np
 
 x = np.linspace(0, 10)
 
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+fig, ax = plt.subplots()
+
 with plt.style.context('fivethirtyeight'):
-    plt.plot(x, np.sin(x) + x + np.random.randn(50))
-    plt.plot(x, np.sin(x) + 0.5 * x + np.random.randn(50))
-    plt.plot(x, np.sin(x) + 2 * x + np.random.randn(50))
-    plt.plot(x, np.sin(x) - 0.5 * x + np.random.randn(50))
-    plt.plot(x, np.sin(x) - 2 * x + np.random.randn(50))
-    plt.plot(x, np.sin(x) + np.random.randn(50))
+    ax.plot(x, np.sin(x) + x + np.random.randn(50))
+    ax.plot(x, np.sin(x) + 0.5 * x + np.random.randn(50))
+    ax.plot(x, np.sin(x) + 2 * x + np.random.randn(50))
+    ax.plot(x, np.sin(x) - 0.5 * x + np.random.randn(50))
+    ax.plot(x, np.sin(x) - 2 * x + np.random.randn(50))
+    ax.plot(x, np.sin(x) + np.random.randn(50))
+    ax.set_title("'fivethirtyeight' style sheet")
 
 
 plt.show()
