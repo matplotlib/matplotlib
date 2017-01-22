556fd82eeac1ed8bbc4d6b8bd4e155357124b510
Jouni K. Seppänen <jks@iki.fi>
2017-01-22 11:08:27 +0200

    Communicate node-specific cache reports to master if using xdist
    See:
    https://github.com/pytest-dev/pytest-xdist/blob/master/xdist/newhooks.py
    http://pytest.org/latest/writing_plugins.html#optionally-using-hooks-from-3rd-party-plugins
    https://github.com/pytest-dev/pytest-xdist/blob/e5d80645347fc7e67efdceb97df8e95d140b283d/testing/acceptance_test.py#L221

--- lib/matplotlib/testing/conftest.py
+++ lib/matplotlib/testing/conftest.py
@@ -1,6 +1,7 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+from six.moves import reduce
 import pytest
 
 import matplotlib
@@ -26,6 +27,8 @@ def pytest_configure(config):
             ccache.ConversionCache(max_size=int(max_size))
     else:
         ccache.conversion_cache = ccache.ConversionCache()
+    if config.pluginmanager.hasplugin('xdist'):
+        config.pluginmanager.register(DeferPlugin())
 
 
 def pytest_unconfigure(config):
@@ -33,10 +36,22 @@ def pytest_unconfigure(config):
     matplotlib._called_from_pytest = False
 
 
+def pytest_sessionfinish(session):
+    if hasattr(session.config, 'slaveoutput'):
+        session.config.slaveoutput['cache-report'] = ccache.conversion_cache.report()
+
+
 def pytest_terminal_summary(terminalreporter):
     tr = terminalreporter
-    data = ccache.conversion_cache.report()
-    tr.write_sep('-', 'Image conversion cache report')
+    if hasattr(tr.config, 'cache_reports'):
+        reports = tr.config.cache_reports
+        data = {'hits': reduce(lambda x, y: x.union(y),
+                               (rep['hits'] for rep in reports)),
+                'gets': reduce(lambda x, y: x.union(y),
+                               (rep['gets'] for rep in reports))}
+    else:
+        data = ccache.conversion_cache.report()
+    tr.write_sep('=', 'Image conversion cache report')
     tr.write_line('Hit rate: %d/%d' % (len(data['hits']), len(data['gets'])))
     if tr.config.getoption('--conversion-cache-report-misses'):
         tr.write_line('Missed files:')
@@ -81,3 +96,10 @@ def mpl_test_settings(request):
             plt.switch_backend(prev_backend)
         _do_cleanup(original_units_registry,
                     original_settings)
+
+
+class DeferPlugin(object):
+    def pytest_testnodedown(self, node, error):
+        if not hasattr(node.config, 'cache_reports'):
+            node.config.cache_reports = []
+        node.config.cache_reports.append(node.slaveoutput['cache-report'])
