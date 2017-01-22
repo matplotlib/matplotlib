ff71b3632fc76b68110394b373295ba854b05b45
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2017-01-22 14:19:30 -0800

    MAINT np.testing.assert_raises depends on nose

--- lib/matplotlib/tests/test_image.py
+++ lib/matplotlib/tests/test_image.py
@@ -9,6 +9,7 @@ import warnings
 from nose.plugins.attrib import attr
 
 import numpy as np
+from numpy.testing import assert_array_equal
 
 from matplotlib.testing.decorators import (image_comparison,
                                            knownfailureif, cleanup)
