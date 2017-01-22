dc350eea444078b3ada902ff17545e2ce363504e
Jouni K. Seppänen <jks@iki.fi>
2017-01-22 09:32:04 +0200

    Skip fontconfig test on win32

--- lib/matplotlib/tests/test_font_manager.py
+++ lib/matplotlib/tests/test_font_manager.py
@@ -5,17 +5,15 @@ from nose.tools import assert_equal
 import six
 
 import os
-
+import sys
 import tempfile
 import warnings
 
 from matplotlib.font_manager import (
     findfont, FontProperties, fontManager, json_dump, json_load, get_font,
     get_fontconfig_fonts, is_opentype_cff_font, fontManager as fm)
-import os.path
-
-
 from matplotlib import rc_context
+from matplotlib.testing.decorators import skipif
 
 
 def test_font_priority():
@@ -67,5 +65,6 @@ def test_otf():
         assert res == is_opentype_cff_font(f)
 
 
+@skipif(sys.platform == 'win32', reason='no fontconfig on Windows')
 def test_get_fontconfig_fonts():
     assert len(get_fontconfig_fonts()) > 1
