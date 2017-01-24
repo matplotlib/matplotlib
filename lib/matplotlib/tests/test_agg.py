481c826c2ca3546d21fece282272509c4d0ebd60
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2017-01-23 21:30:06 -0500

    Remove nose/pytest compatibility functions.
    They've never been released and are no longer used.

--- lib/matplotlib/tests/test_agg.py
+++ lib/matplotlib/tests/test_agg.py
@@ -11,7 +11,6 @@ import pytest
 from matplotlib.image import imread
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
-from matplotlib.testing import skip
 from matplotlib.testing.decorators import cleanup, image_comparison
 from matplotlib import pyplot as plt
 from matplotlib import collections
@@ -181,7 +180,7 @@ def test_agg_filter():
             return t2
 
     if LooseVersion(np.__version__) < LooseVersion('1.7.0'):
-        skip('Disabled on Numpy < 1.7.0')
+        pytest.skip('Disabled on Numpy < 1.7.0')
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
