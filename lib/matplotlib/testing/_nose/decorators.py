ca4d87a61bc7e4b688766ba060a5c8842d20f9a6
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2017-01-23 19:03:14 -0500

    Remove matplotlib.decorators.skipif.
    It's a compatibility wrapper around nose/pytest, but has never been
    released.

--- lib/matplotlib/testing/_nose/decorators.py
+++ lib/matplotlib/testing/_nose/decorators.py
@@ -9,45 +9,6 @@ from . import knownfail
 from .exceptions import KnownFailureDidNotFailTest
 
 
-def skipif(skip_condition, *args, **kwargs):
-    if isinstance(skip_condition, bool) and 'reason' not in kwargs:
-        raise ValueError("you need to specify reason=STRING "
-                         "when using booleans as conditions.")
-
-    def skip_decorator(func):
-        import inspect
-
-        def skipper(*_args, **_kwargs):
-            condition, msg = skip_condition, kwargs.get('reason')  # local copy
-            if isinstance(condition, six.string_types):
-                globs = {'os': os, 'sys': sys}
-                try:
-                    globs.update(func.__globals__)
-                except AttributeError:
-                    globs.update(func.func_globals)
-                if msg is None:
-                    msg = condition
-                condition = eval(condition, globs)
-            else:
-                condition = bool(condition)
-
-            if condition:
-                skip(msg)
-            else:
-                return func(*_args, **_kwargs)
-
-        if inspect.isclass(func):
-            setup = getattr(func, 'setup_class', classmethod(lambda _: None))
-            setup = skip_decorator(setup.__func__)
-            setup = setup.__get__(func)
-            setattr(func, 'setup_class', setup)
-            return func
-
-        return copy_metadata(func, skipper)
-
-    return skip_decorator
-
-
 def knownfailureif(fail_condition, msg=None, known_exception_class=None):
     # based on numpy.testing.dec.knownfailureif
     if msg is None:
