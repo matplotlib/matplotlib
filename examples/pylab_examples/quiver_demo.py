7a8ea739ffc536aaca49dc80c3c35515d0cc9d15
David Stansby <dstansby@gmail.com>
2017-01-28 12:44:27 +0000

    Simplify remaining quiver examples

--- examples/pylab_examples/quiver_demo.py
+++ examples/pylab_examples/quiver_demo.py
@@ -15,35 +15,26 @@ U = np.cos(X)
 V = np.sin(Y)
 
 plt.figure()
-plt.title('scales with plot width, not view')
+plt.title('Arrows scale with plot width, not view')
 Q = plt.quiver(X, Y, U, V, units='width')
-qk = plt.quiverkey(Q, 0.9, 0.95, 2, r'$2 \frac{m}{s}$',
-                   labelpos='E',
-                   coordinates='figure',
-                   fontproperties={'weight': 'bold'})
-plt.axis([-1, 7, -1, 7])
+qk = plt.quiverkey(Q, 0.9, 0.9, 2, r'$2 \frac{m}{s}$', labelpos='E',
+                   coordinates='figure')
 
 plt.figure()
 plt.title("pivot='mid'; every third arrow; units='inches'")
 Q = plt.quiver(X[::3, ::3], Y[::3, ::3], U[::3, ::3], V[::3, ::3],
-               pivot='mid', color='r', units='inches')
-qk = plt.quiverkey(Q, 0.5, 0.03, 1, r'$1 \frac{m}{s}$',
-                   fontproperties={'weight': 'bold'})
-plt.plot(X[::3, ::3], Y[::3, ::3], 'k.')
-plt.axis([-1, 7, -1, 7])
+               pivot='mid', units='inches')
+qk = plt.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$', labelpos='E',
+                   coordinates='figure')
+plt.scatter(X[::3, ::3], Y[::3, ::3], color='r', s=5)
 
 plt.figure()
-plt.title("scales with x view; pivot='tip'")
+plt.title("pivot='tip'; scales with x view")
 M = np.hypot(U, V)
-Q = plt.quiver(X, Y, U, V, M,
-               units='x',
-               pivot='tip',
-               width=0.022,
+Q = plt.quiver(X, Y, U, V, M, units='x', pivot='tip', width=0.022,
                scale=1 / 0.15)
-qk = plt.quiverkey(Q, 0.9, 1.05, 1, r'$1 \frac{m}{s}$',
-                   labelpos='E',
-                   fontproperties={'weight': 'bold'})
-plt.plot(X, Y, 'k.', markersize=2)
-plt.axis([-1, 7, -1, 7])
+qk = plt.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$', labelpos='E',
+                   coordinates='figure')
+plt.scatter(X, Y, color='k', s=5)
 
 plt.show()
