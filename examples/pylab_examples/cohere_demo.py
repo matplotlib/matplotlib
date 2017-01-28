9f5c27f85c1fc45eaa3e7575d9f7285d2c8b5165
David Stansby <dstansby@gmail.com>
2017-01-28 13:11:42 +0000

    Fix cohere-demo

--- examples/pylab_examples/cohere_demo.py
+++ examples/pylab_examples/cohere_demo.py
@@ -7,31 +7,28 @@ import matplotlib.pyplot as plt
 # Fixing random state for reproducibility
 np.random.seed(19680801)
 
-
-# make a little extra space between the subplots
-plt.subplots_adjust(wspace=0.5)
-
 dt = 0.01
 t = np.arange(0, 30, dt)
 nse1 = np.random.randn(len(t))                 # white noise 1
 nse2 = np.random.randn(len(t))                 # white noise 2
-r = np.exp(-t/0.05)
+r = np.exp(-t / 0.05)
 
-cnse1 = np.convolve(nse1, r, mode='same')*dt   # colored noise 1
-cnse2 = np.convolve(nse2, r, mode='same')*dt   # colored noise 2
+cnse1 = np.convolve(nse1, r, mode='same') * dt   # colored noise 1
+cnse2 = np.convolve(nse2, r, mode='same') * dt   # colored noise 2
 
 # two signals with a coherent part and a random part
-s1 = 0.01*np.sin(2*np.pi*10*t) + cnse1
-s2 = 0.01*np.sin(2*np.pi*10*t) + cnse2
+s1 = 0.01 * np.sin(2 * np.pi * 10 * t) + cnse1
+s2 = 0.01 * np.sin(2 * np.pi * 10 * t) + cnse2
 
-plt.subplot(211)
-plt.plot(t, s1, t, s2)
-plt.xlim(0, 5)
-plt.xlabel('time')
-plt.ylabel('s1 and s2')
-plt.grid(True)
+fig, axs = plt.subplots(2, 1)
+axs[0].plot(t, s1, t, s2)
+axs[0].set_xlim(0, 5)
+axs[0].set_xlabel('time')
+axs[0].set_ylabel('s1 and s2')
+axs[0].grid(True)
 
-plt.subplot(212)
-cxy, f = plt.cohere(s1, s2, 256, 1./dt)
-plt.ylabel('coherence')
+cxy, f = axs[1].cohere(s1, s2, 256, 1. / dt)
+axs[1].set_ylabel('coherence')
+
+fig.tight_layout()
 plt.show()
