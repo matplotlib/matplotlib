019d651b91d7e37da5578e7b75ce4d014ec799cd
Levi Kilcher <levi.kilcher@gmail.com>
2017-01-29 11:25:30 -0700

    Correct test comment.

--- lib/matplotlib/tests/test_marker.py
+++ lib/matplotlib/tests/test_marker.py
@@ -15,6 +15,6 @@ def test_markers_valid():
 def test_markers_invalid():
     marker_style = markers.MarkerStyle()
     mrk_array = np.array([[-0.5, 0,  1, 2, 3]])
-    # Checking this doesn't fail.
+    # Checking this does fail.
     with pytest.raises(ValueError):
         marker_style.set_marker(mrk_array)
