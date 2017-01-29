68296f8dc7535a8f51b163cd096b8a2d41cfc287
hannah <story645@gmail.com>
2017-01-29 14:48:02 -0500

    Merge pull request #7913 from dstansby/quiver-doc
    [MRG+2] Clean up quiver docstring + add simple quiver example

--- examples/pylab_examples/quiver_demo.py
+++ examples/pylab_examples/quiver_demo.py
@@ -1,12 +1,12 @@
 '''
-Demonstration of quiver and quiverkey functions. This is using the
-new version coming from the code in quiver.py.
+========================================================
+Demonstration of advanced quiver and quiverkey functions
+========================================================
 
 Known problem: the plot autoscaling does not take into account
 the arrows, so those on the boundaries are often out of the picture.
 This is *not* an easy problem to solve in a perfectly general way.
 The workaround is to manually expand the axes.
-
 '''
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,81 +16,27 @@ X, Y = np.meshgrid(np.arange(0, 2 * np.pi, .2), np.arange(0, 2 * np.pi, .2))
 U = np.cos(X)
 V = np.sin(Y)
 
-# 1
 plt.figure()
-Q = plt.quiver(U, V)
-qk = plt.quiverkey(Q, 0.5, 0.98, 2, r'$2 \frac{m}{s}$', labelpos='W',
-                   fontproperties={'weight': 'bold'})
-l, r, b, t = plt.axis()
-dx, dy = r - l, t - b
-plt.axis([l - 0.05*dx, r + 0.05*dx, b - 0.05*dy, t + 0.05*dy])
-
-plt.title('Minimal arguments, no kwargs')
-
-# 2
-plt.figure()
+plt.title('Arrows scale with plot width, not view')
 Q = plt.quiver(X, Y, U, V, units='width')
-qk = plt.quiverkey(Q, 0.9, 0.95, 2, r'$2 \frac{m}{s}$',
-                   labelpos='E',
-                   coordinates='figure',
-                   fontproperties={'weight': 'bold'})
-plt.axis([-1, 7, -1, 7])
-plt.title('scales with plot width, not view')
+qk = plt.quiverkey(Q, 0.9, 0.9, 2, r'$2 \frac{m}{s}$', labelpos='E',
+                   coordinates='figure')
 
-# 3
 plt.figure()
-Q = plt.quiver(X[::3, ::3], Y[::3, ::3], U[::3, ::3], V[::3, ::3],
-               pivot='mid', color='r', units='inches')
-qk = plt.quiverkey(Q, 0.5, 0.03, 1, r'$1 \frac{m}{s}$',
-                   fontproperties={'weight': 'bold'})
-plt.plot(X[::3, ::3], Y[::3, ::3], 'k.')
-plt.axis([-1, 7, -1, 7])
 plt.title("pivot='mid'; every third arrow; units='inches'")
-
-# 4
-plt.figure()
-M = np.hypot(U, V)
-Q = plt.quiver(X, Y, U, V, M,
-               units='x',
-               pivot='tip',
-               width=0.022,
-               scale=1 / 0.15)
-qk = plt.quiverkey(Q, 0.9, 1.05, 1, r'$1 \frac{m}{s}$',
-                   labelpos='E',
-                   fontproperties={'weight': 'bold'})
-plt.plot(X, Y, 'k.', markersize=2)
-plt.axis([-1, 7, -1, 7])
-plt.title("scales with x view; pivot='tip'")
-
-# 5
-plt.figure()
 Q = plt.quiver(X[::3, ::3], Y[::3, ::3], U[::3, ::3], V[::3, ::3],
-               color='r', units='x',
-               linewidths=(0.5,), edgecolors=('k'), headaxislength=5)
-qk = plt.quiverkey(Q, 0.5, 0.03, 1, r'$1 \frac{m}{s}$',
-                   fontproperties={'weight': 'bold'})
-plt.axis([-1, 7, -1, 7])
-plt.title("triangular head; scale with x view; black edges")
+               pivot='mid', units='inches')
+qk = plt.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$', labelpos='E',
+                   coordinates='figure')
+plt.scatter(X[::3, ::3], Y[::3, ::3], color='r', s=5)
 
-# 6
 plt.figure()
-M = np.zeros(U.shape, dtype='bool')
-XMaskStart = U.shape[0]//3
-YMaskStart = U.shape[1]//3
-XMaskStop = 2*U.shape[0]//3
-YMaskStop = 2*U.shape[1]//3
-
-M[XMaskStart:XMaskStop,
-  YMaskStart:YMaskStop] = True
-U = ma.masked_array(U, mask=M)
-V = ma.masked_array(V, mask=M)
-Q = plt.quiver(U, V)
-qk = plt.quiverkey(Q, 0.5, 0.98, 2, r'$2 \frac{m}{s}$', labelpos='W',
-                   fontproperties={'weight': 'bold'})
-l, r, b, t = plt.axis()
-dx, dy = r - l, t - b
-plt.axis([l - 0.05 * dx, r + 0.05 * dx, b - 0.05 * dy, t + 0.05 * dy])
-plt.title('Minimal arguments, no kwargs - masked values')
-
+plt.title("pivot='tip'; scales with x view")
+M = np.hypot(U, V)
+Q = plt.quiver(X, Y, U, V, M, units='x', pivot='tip', width=0.022,
+               scale=1 / 0.15)
+qk = plt.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$', labelpos='E',
+                   coordinates='figure')
+plt.scatter(X, Y, color='k', s=5)
 
 plt.show()
---
+++ examples/pylab_examples/quiver_simple_demo.py
@@ -0,0 +1,18 @@
+'''
+==================================================
+A simple example of a quiver plot with a quiverkey
+==================================================
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+
+X = np.arange(-10, 10, 1)
+Y = np.arange(-10, 10, 1)
+U, V = np.meshgrid(X, Y)
+
+fig, ax = plt.subplots()
+q = ax.quiver(X, Y, U, V)
+ax.quiverkey(q, X=0.3, Y=1.1, U=10,
+             label='Quiver key, length = 10', labelpos='E')
+
+plt.show()
