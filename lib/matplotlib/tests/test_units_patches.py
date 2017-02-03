ab57debf599e095c98c3cc4913f4d3c0ff87ba36
Ted Drain <ted.drain@gmail.com>
2017-02-02 18:35:55 -0800

    Update test_units_patches.py

--- lib/matplotlib/tests/test_units_patches.py
+++ lib/matplotlib/tests/test_units_patches.py
@@ -4,16 +4,10 @@ Tests using patches with units.
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-from matplotlib.externals import six
-
-import numpy as np
-from numpy.testing import assert_array_equal
-from numpy.testing import assert_equal
-from numpy.testing import assert_almost_equal
-
-from matplotlib.testing.decorators import image_comparison, cleanup
+from matplotlib.testing.decorators import image_comparison
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+
 
 @image_comparison(baseline_images=['units_rectangle'], extensions=['png'])
 def test_units_rectangle():
@@ -27,7 +21,3 @@ def test_units_rectangle():
     ax.add_patch(p)
     ax.set_xlim([4*U.km, 7*U.km])
     ax.set_ylim([5*U.km, 9*U.km])
-
-if __name__ == '__main__':
-    import nose
-    nose.runmodule(argv=['-s', '--with-doctest'], exit=False)
