29b0410ffc9c2fe0406536620dfe0f110364b6af
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2017-02-02 23:11:57 -0500

    Template-ize visual_tests.py.

--- visual_tests.py
+++ visual_tests.py
@@ -12,26 +12,50 @@ import six
 
 from collections import defaultdict
 
+
+html_template = """<html><head><style media="screen" type="text/css">
+img{{
+  width:100%;
+  max-width:800px;
+}}
+</style>
+</head><body>
+{failed}
+{body}
+</body></html>
+"""
+
+subdir_template = """<h2>{subdir}</h2><table>
+<thead><td>name</td><td>actual</td><td>expected</td><td>diff</td></thead>
+{rows}
+</table>
+"""
+
+failed_template = """<h2>Only Failed</h2><table>
+<thead><td>name</td><td>actual</td><td>expected</td><td>diff</td></thead>
+{rows}
+</table>
+"""
+
+row_template = ('<tr>'
+                '<td>{0} {1}</td>'
+                '<td>{2}</td>'
+                '<td><a href="{3}"><img src="{3}"></a></td>'
+                '<td>{4}</td>'
+                '</tr>')
+
+linked_image_template = '<a href="{0}"><img src="{0}"></a>'
+
+
 def run():
     # Build a website for visual comparison
     image_dir = "result_images"
     # build the website
-    _html = ""
-    _html += """<html><head><style media="screen" type="text/css">
-    img{
-        width:100%;
-        max-width:800px;
-    }
-    </style>
-    </head><body>\n"""
     _subdirs = [name for name in os.listdir(image_dir) if os.path.isdir(os.path.join(image_dir, name))]
     # loop over all pictures
-    _row = '<tr><td>{0} {1}</td><td>{2}</td><td><a href="{3}"><img src="{3}"></a></td><td>{4}</td>\n'
-    _failed = ""
-    _failed += "<h2>Only Failed</h2>"
-    _failed += "<table>\n<thead><td>name</td><td>actual</td><td>expected</td><td>diff</td></thead>\n"
     _has_failure = False
-    _body = ""
+    failed_rows = []
+    body_sections = []
     for subdir in _subdirs:
         if subdir == "test_compare_images":
             # these are the image which test the image comparison functions...
@@ -51,37 +75,46 @@ def run():
             else:
                 pictures[fn]["c"] = "/".join((subdir, file))
 
-        _body += "<h2>{0}</h2>".format(subdir)
-        _body += "<table>\n<thead><td>name</td><td>actual</td><td>expected</td><td>diff</td></thead>\n"
+        subdir_rows = []
         for name, test in six.iteritems(pictures):
             if test.get("f", None):
                 # a real failure in the image generation, resulting in different images
                 _has_failure = True
                 s = "(failed)"
                 failed = '<a href="{0}">diff</a>'.format(test.get("f", ""))
-                current = '<a href="{0}"><img src="{0}"></a>'.format(test.get("c", ""))
-                _failed += _row.format(name, "", current, test.get("e", ""), failed)
+                current = linked_image_template.format(test.get("c", ""))
+                failed_rows.append(row_template.format(name, "", current,
+                                                       test.get("e", ""),
+                                                       failed))
             elif test.get("c", None) is None:
                 # A failure in the test, resulting in no current image
                 _has_failure = True
                 s = "(failed)"
                 failed = '--'
                 current = '(Failure in test, no image produced)'
-                _failed += _row.format(name, "", current, test.get("e", ""), failed)
+                failed_rows.append(row_template.format(name, "", current,
+                                                       test.get("e", ""),
+                                                       failed))
             else:
                 s = "(passed)"
                 failed = '--'
-                current = '<a href="{0}"><img src="{0}"></a>'.format(test.get("c", ""))
-            _body += _row.format(name, "", current, test.get("e", ""), failed)
-        _body += "</table>\n"
-    _failed += "</table>\n"
+                current = linked_image_template.format(test.get("c", ""))
+            subdir_rows.append(row_template.format(name, "", current,
+                                                   test.get("e", ""), failed))
+
+        body_sections.append(
+            subdir_template.format(subdir=subdir, rows='\n'.join(subdir_rows)))
+
     if _has_failure:
-        _html += _failed
-    _html += _body
-    _html += "\n</body></html>"
+        failed = failed_template.format(rows='\n'.join(failed_rows))
+    else:
+        failed = ''
+    body = ''.join(body_sections)
+    html = html_template.format(failed=failed, body=body)
     index = os.path.join(image_dir, "index.html")
     with open(index, "w") as f:
-        f.write(_html)
+        f.write(html)
+
     try:
         import webbrowser
         webbrowser.open(index)
