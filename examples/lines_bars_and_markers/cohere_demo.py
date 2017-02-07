f406d7dd494048a339fe5c48f26d2cbe6e629d40
David Stansby <dstansby@gmail.com>
2017-02-07 23:00:05 +0000

    Remove convolution from cohere_demo

--- examples/lines_bars_and_markers/cohere_demo.py
+++ examples/lines_bars_and_markers/cohere_demo.py
@@ -15,18 +15,14 @@ dt = 0.01
 t = np.arange(0, 30, dt)
 nse1 = np.random.randn(len(t))                 # white noise 1
 nse2 = np.random.randn(len(t))                 # white noise 2
-r = np.exp(-t / 0.05)
 
-cnse1 = np.convolve(nse1, r, mode='same') * dt   # colored noise 1
-cnse2 = np.convolve(nse2, r, mode='same') * dt   # colored noise 2
-
-# two signals with a coherent part and a random part
-s1 = 0.01 * np.sin(2 * np.pi * 10 * t) + cnse1
-s2 = 0.01 * np.sin(2 * np.pi * 10 * t) + cnse2
+# Two signals with a coherent part at 10Hz and a random part
+s1 = np.sin(2 * np.pi * 10 * t) + nse1
+s2 = np.sin(2 * np.pi * 10 * t) + nse2
 
 fig, axs = plt.subplots(2, 1)
 axs[0].plot(t, s1, t, s2)
-axs[0].set_xlim(0, 5)
+axs[0].set_xlim(0, 2)
 axs[0].set_xlabel('time')
 axs[0].set_ylabel('s1 and s2')
 axs[0].grid(True)
