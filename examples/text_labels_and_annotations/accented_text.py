1122dc5799619f9edad55807951e55d5c4c85ff7
David Stansby <dstansby@gmail.com>
2017-02-17 10:30:28 +0000

    Update accented text sentences@

--- examples/text_labels_and_annotations/accented_text.py
+++ examples/text_labels_and_annotations/accented_text.py
@@ -23,12 +23,13 @@ ax.set_title(r'$\ddot{o}\acute{e}\grave{e}\hat{O}'
 
 # Shorthand is also supported and curly's are optional
 ax.set_xlabel(r"""$\"o\ddot o \'e\`e\~n\.x\^y$""", fontsize=20)
+ax.text(4, 0.5, r"$F=m\ddot{x}$")
 fig.tight_layout()
 
 # Unicode demo
 fig, ax = plt.subplots()
-ax.set_title('Développés et fabriqués')
-ax.set_xlabel("réactivité nous permettent d'être sélectionnés et adoptés")
+ax.set_title("GISCARD CHAHUTÉ À L'ASSEMBLÉE")
+ax.set_xlabel("LE COUP DE DÉ DE DE GAULLE")
 ax.set_ylabel('André was here!')
 ax.text(0.2, 0.8, 'Institut für Festkörperphysik', rotation=45)
 ax.text(0.4, 0.2, 'AVA (check kerning)')
