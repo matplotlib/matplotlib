07b3b520f8c4444298ef50bce6df9eea22c4cc91
David Stansby <dstansby@gmail.com>
2017-02-21 18:45:22 +0000

    Curlies --> curly braces

--- examples/text_labels_and_annotations/accented_text.py
+++ examples/text_labels_and_annotations/accented_text.py
@@ -21,7 +21,7 @@ ax.plot(range(10))
 ax.set_title(r'$\ddot{o}\acute{e}\grave{e}\hat{O}'
              r'\breve{i}\bar{A}\tilde{n}\vec{q}$', fontsize=20)
 
-# Shorthand is also supported and curly's are optional
+# Shorthand is also supported and curly braces are optional
 ax.set_xlabel(r"""$\"o\ddot o \'e\`e\~n\.x\^y$""", fontsize=20)
 ax.text(4, 0.5, r"$F=m\ddot{x}$")
 fig.tight_layout()
