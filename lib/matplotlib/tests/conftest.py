996f22b90276472257ccd405b35645bfff855f61
Jouni K. Seppänen <jks@iki.fi>
2017-02-22 11:53:53 +0200

    pep8 fix

--- lib/matplotlib/tests/conftest.py
+++ lib/matplotlib/tests/conftest.py
@@ -4,4 +4,3 @@ from __future__ import (absolute_import, division, print_function,
 from matplotlib.testing.conftest import (
     mpl_test_settings, pytest_configure, pytest_unconfigure,
     pytest_sessionfinish, pytest_terminal_summary)
-
