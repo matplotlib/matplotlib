e5184bbe8fbfc38beb211ee749fd7d4425f6b40b
Jouni K. Seppänen <jks@iki.fi>
2017-02-22 10:07:03 +0200

    Omit unnecessary tmpdir deletions
    It seems that the tmpdir fixture will take care of them after
    three test runs.
    http://doc.pytest.org/en/latest/tmpdir.html#the-default-base-temporary-directory

--- lib/matplotlib/tests/test_cache.py
+++ lib/matplotlib/tests/test_cache.py
@@ -19,33 +19,30 @@ def test_cache_basic(tmpdir):
     def fname(f):
         return str(intmp(f))
 
-    try:
-        cache = _ConversionCache(fname('cache'))
-        intmp('fake.pdf').write_binary(b'this is a fake pdf file')
-        intmp('fake.svg').write_binary(b'this pretends to be an svg file')
-        assert not cache.get(fname('fake.pdf'), fname('fakepdf.png'))
-        assert not cache.get(fname('fake.svg'), fname('fakesvg.png'))
-        assert cache.report() == \
-            {'gets': {fname('fake.pdf'), fname('fake.svg')},
-             'hits': set()}
+    cache = _ConversionCache(fname('cache'))
+    intmp('fake.pdf').write_binary(b'this is a fake pdf file')
+    intmp('fake.svg').write_binary(b'this pretends to be an svg file')
+    assert not cache.get(fname('fake.pdf'), fname('fakepdf.png'))
+    assert not cache.get(fname('fake.svg'), fname('fakesvg.png'))
+    assert cache.report() == \
+        {'gets': {fname('fake.pdf'), fname('fake.svg')},
+         'hits': set()}
 
-        intmp('fakepdf.png').write_binary(b'generated from the pdf file')
-        cache.put(fname('fake.pdf'), fname('fakepdf.png'))
-        assert cache.get(fname('fake.pdf'), fname('copypdf.png'))
-        assert intmp('copypdf.png').read() == 'generated from the pdf file'
-        assert cache.report() == \
-            {'gets': {fname('fake.pdf'), fname('fake.svg')},
-             'hits': {fname('fake.pdf')}}
+    intmp('fakepdf.png').write_binary(b'generated from the pdf file')
+    cache.put(fname('fake.pdf'), fname('fakepdf.png'))
+    assert cache.get(fname('fake.pdf'), fname('copypdf.png'))
+    assert intmp('copypdf.png').read() == 'generated from the pdf file'
+    assert cache.report() == \
+        {'gets': {fname('fake.pdf'), fname('fake.svg')},
+         'hits': {fname('fake.pdf')}}
 
-        intmp('fakesvg.png').write_binary(b'generated from the svg file')
-        cache.put(fname('fake.svg'), fname('fakesvg.png'))
-        assert cache.get(fname('fake.svg'), fname('copysvg.png'))
-        assert intmp('copysvg.png').read() == 'generated from the svg file'
-        assert cache.report() == \
-            {'gets': {fname('fake.pdf'), fname('fake.svg')},
-             'hits': {fname('fake.pdf'), fname('fake.svg')}}
-    finally:
-        tmpdir.remove(rec=1)
+    intmp('fakesvg.png').write_binary(b'generated from the svg file')
+    cache.put(fname('fake.svg'), fname('fakesvg.png'))
+    assert cache.get(fname('fake.svg'), fname('copysvg.png'))
+    assert intmp('copysvg.png').read() == 'generated from the svg file'
+    assert cache.report() == \
+        {'gets': {fname('fake.pdf'), fname('fake.svg')},
+         'hits': {fname('fake.pdf'), fname('fake.svg')}}
 
 
 def test_cache_expire(tmpdir):
@@ -55,33 +52,29 @@ def test_cache_expire(tmpdir):
     def fname(*f):
         return str(intmp(*f))
 
-    try:
-        cache = _ConversionCache(fname('cache'), 10)
-        for i in range(5):
-            pngfile = intmp('cache', 'file%d.png' % i)
-            pngfile.write_binary(b'1234')
-            os.utime(str(pngfile), (i*1000, i*1000))
+    cache = _ConversionCache(fname('cache'), 10)
+    for i in range(5):
+        pngfile = intmp('cache', 'file%d.png' % i)
+        pngfile.write_binary(b'1234')
+        os.utime(str(pngfile), (i*1000, i*1000))
 
-        cache.expire()
-        assert not os.path.exists(fname('cache', 'file0.png'))
-        assert not os.path.exists(fname('cache', 'file1.png'))
-        assert not os.path.exists(fname('cache', 'file2.png'))
-        assert os.path.exists(fname('cache', 'file3.png'))
-        assert os.path.exists(fname('cache', 'file4.png'))
+    cache.expire()
+    assert not os.path.exists(fname('cache', 'file0.png'))
+    assert not os.path.exists(fname('cache', 'file1.png'))
+    assert not os.path.exists(fname('cache', 'file2.png'))
+    assert os.path.exists(fname('cache', 'file3.png'))
+    assert os.path.exists(fname('cache', 'file4.png'))
 
-        intmp('cache', 'onemore.png').write_binary(b'x' * 11)
-        os.utime(fname('cache', 'onemore.png'), (5000, 5000))
+    intmp('cache', 'onemore.png').write_binary(b'x' * 11)
+    os.utime(fname('cache', 'onemore.png'), (5000, 5000))
 
-        cache.expire()
-        assert not os.path.exists(fname('cache', 'file0.png'))
-        assert not os.path.exists(fname('cache', 'file1.png'))
-        assert not os.path.exists(fname('cache', 'file2.png'))
-        assert not os.path.exists(fname('cache', 'file3.png'))
-        assert not os.path.exists(fname('cache', 'file4.png'))
-        assert not os.path.exists(fname('cache', 'onemore.png'))
-
-    finally:
-        tmpdir.remove(rec=1)
+    cache.expire()
+    assert not os.path.exists(fname('cache', 'file0.png'))
+    assert not os.path.exists(fname('cache', 'file1.png'))
+    assert not os.path.exists(fname('cache', 'file2.png'))
+    assert not os.path.exists(fname('cache', 'file3.png'))
+    assert not os.path.exists(fname('cache', 'file4.png'))
+    assert not os.path.exists(fname('cache', 'onemore.png'))
 
 
 def test_cache_default_dir():
@@ -96,10 +89,7 @@ def test_cache_default_dir():
             side_effect=OSError)
 def test_cache_mkdir_error(mkdirs, tmpdir):
     with pytest.raises(_CacheError):
-        try:
-            c = _ConversionCache(str(tmpdir.join('cache')))
-        finally:
-            tmpdir.remove(rec=1)
+        c = _ConversionCache(str(tmpdir.join('cache')))
 
 
 @mock.patch('matplotlib.testing._conversion_cache.os.access',
@@ -108,7 +98,4 @@ def test_cache_unwritable_error(access, tmpdir):
     with pytest.raises(_CacheError):
         cachedir = tmpdir.join('cache')
         cachedir.ensure(dir=True)
-        try:
-            c = _ConversionCache(str(cachedir))
-        finally:
-            tmpdir.remove(rec=1)
+        c = _ConversionCache(str(cachedir))
