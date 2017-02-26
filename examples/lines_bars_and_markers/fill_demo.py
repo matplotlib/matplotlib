e391d695fade01931eb0abcef778742017ba60e1
patniharshit <patniharshit@gmail.com>
2017-02-26 16:57:34 +0530

    merge fill_demo and fill_demo_features

--- examples/lines_bars_and_markers/fill_demo.py
+++ examples/lines_bars_and_markers/fill_demo.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 """
 ==================
 A simple Fill plot
@@ -5,9 +8,6 @@ A simple Fill plot
 
 This example showcases the most basic fill plot a user can do with matplotlib.
 """
-import numpy as np
-import matplotlib.pyplot as plt
-
 x = np.linspace(0, 1, 500)
 y = np.sin(4 * np.pi * x) * np.exp(-5 * x)
 
@@ -15,4 +15,23 @@ fig, ax = plt.subplots()
 
 ax.fill(x, y, zorder=10)
 ax.grid(True, zorder=5)
+plt.show()
+
+"""
+========================
+A more complex fill demo
+========================
+
+In addition to the basic fill plot, this demo shows a few optional features:
+
+    * Multiple curves with a single command.
+    * Setting the fill color.
+    * Setting the opacity (alpha value).
+"""
+x = np.linspace(0, 2 * np.pi, 500)
+y1 = np.sin(x)
+y2 = np.sin(3 * x)
+
+fig, ax = plt.subplots()
+ax.fill(x, y1, 'b', x, y2, 'r', alpha=0.3)
 plt.show()
