ac09d6073b931277f3bd284a972a6c9f46fec1d5
Dietrich Brunn <Dietrich.Brunn@web.de>
2017-02-26 20:08:55 +0100

    Fix layout of spectrum_demo.py

--- examples/pylab_examples/spectrum_demo.py
+++ examples/pylab_examples/spectrum_demo.py
@@ -1,32 +1,43 @@
+"""
+========================
+Spectrum Representations
+========================
+
+The plots show different spectrum representations of a sine signal with
+additive noise. A (frequency) spectrum of a discrete-time signal is calculated
+by utilizing the fast Fourier transform (FFT).
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 np.random.seed(0)
 
-dt = 0.01
-Fs = 1/dt
+dt = 0.01  # sampling interval
+Fs = 1/dt  # sampling frequency
 t = np.arange(0, 10, dt)
+
+# generate noise:
 nse = np.random.randn(len(t))
 r = np.exp(-t/0.05)
-
 cnse = np.convolve(nse, r)*dt
 cnse = cnse[:len(t)]
-s = 0.1*np.sin(2*np.pi*t) + cnse
 
-plt.subplot(3, 2, 1)
-plt.plot(t, s)
+s = 0.1*np.sin(2*np.pi*t) + cnse  # the signal
 
-plt.subplot(3, 2, 3)
-plt.magnitude_spectrum(s, Fs=Fs)
+fig, axx = plt.subplots(3, 2)
 
-plt.subplot(3, 2, 4)
-plt.magnitude_spectrum(s, Fs=Fs, scale='dB')
+# plot time signal:
+axx[0, 0].plot(t, s)
+axx[0, 0].set_xlabel("Time $t$")
+axx[0, 0].set_ylabel("Signal $s(t)$")
 
-plt.subplot(3, 2, 5)
-plt.angle_spectrum(s, Fs=Fs)
+# plot different spectrum types:
+axx[1, 0].magnitude_spectrum(s, Fs=Fs)
+axx[2, 0].phase_spectrum(s, Fs=Fs)
+axx[0, 1].remove()  # don't display empty ax
+axx[1, 1].magnitude_spectrum(s, Fs=Fs, scale='dB')
+axx[2, 1].angle_spectrum(s, Fs=Fs)
 
-plt.subplot(3, 2, 6)
-plt.phase_spectrum(s, Fs=Fs)
-
+fig.tight_layout()
 plt.show()
