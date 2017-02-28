73890e14f43d3c747f55deed55f60e00819dd2b9
David Stansby <dstansby@gmail.com>
2017-02-28 17:42:55 +0000

    Put boundarynorm example back in

--- examples/lines_bars_and_markers/multicolored_line.py
+++ examples/lines_bars_and_markers/multicolored_line.py
@@ -10,6 +10,7 @@ is colored based on its derivative.
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
+from matplotlib.colors import ListedColormap, BoundaryNorm
 
 x = np.linspace(0, 3 * np.pi, 500)
 y = np.sin(x)
@@ -22,18 +23,26 @@ dydx = np.cos(0.5 * (x[:-1] + x[1:]))  # first derivative
 points = np.array([x, y]).T.reshape(-1, 1, 2)
 segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
-# Create a norm to map from data points to colors
+fig, axs = plt.subplots(2, 1, sharex=True, sharey=True)
+
+# Create a continuous norm to map from data points to colors
 norm = plt.Normalize(dydx.min(), dydx.max())
 lc = LineCollection(segments, cmap='viridis', norm=norm)
 # Set the values used for colormapping
 lc.set_array(dydx)
 lc.set_linewidth(2)
+line = axs[0].add_collection(lc)
+fig.colorbar(line, ax=axs[0])
 
-fig, ax = plt.subplots()
-line = ax.add_collection(lc)
-fig.colorbar(line)
+# Use a boundary norm instead
+cmap = ListedColormap(['r', 'g', 'b'])
+norm = BoundaryNorm([-1, -0.5, 0.5, 1], cmap.N)
+lc = LineCollection(segments, cmap=cmap, norm=norm)
+lc.set_array(dydx)
+lc.set_linewidth(2)
+line = axs[1].add_collection(lc)
+fig.colorbar(line, ax=axs[1])
 
-ax.set_xlim(x.min(), x.max())
-ax.set_ylim(-1.1, 1.1)
-
+axs[0].set_xlim(x.min(), x.max())
+axs[0].set_ylim(-1.1, 1.1)
 plt.show()
