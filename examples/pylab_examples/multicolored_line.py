6f4d1cd45a07c78f2291ec509a6cbafe0ebb5537
David Stansby <dstansby@gmail.com>
2017-02-28 17:15:31 +0000

    Simplify multicolored_line example

--- examples/pylab_examples/multicolored_line.py
+++ examples/pylab_examples/multicolored_line.py
@@ -1,20 +1,19 @@
 '''
-Color parts of a line based on its properties, e.g., slope.
+==================
+Multicolored lines
+==================
+
+This example shows how to make a multi-colored line. In this example, we color
+the line based on its derivate.
 '''
 
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
-from matplotlib.colors import ListedColormap, BoundaryNorm
 
 x = np.linspace(0, 3 * np.pi, 500)
 y = np.sin(x)
-z = np.cos(0.5 * (x[:-1] + x[1:]))  # first derivative
-
-# Create a colormap for red, green and blue and a norm to color
-# f' < -0.5 red, f' > 0.5 blue, and the rest green
-cmap = ListedColormap(['r', 'g', 'b'])
-norm = BoundaryNorm([-1, -0.5, 0.5, 1], cmap.N)
+dydx = np.cos(0.5 * (x[:-1] + x[1:]))  # first derivative
 
 # Create a set of line segments so that we can color them individually
 # This creates the points as a N x 1 x 2 array so that we can stack points
@@ -23,31 +22,17 @@ norm = BoundaryNorm([-1, -0.5, 0.5, 1], cmap.N)
 points = np.array([x, y]).T.reshape(-1, 1, 2)
 segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
-# Create the line collection object, setting the colormapping parameters.
-# Have to set the actual values used for colormapping separately.
-lc = LineCollection(segments, cmap=cmap, norm=norm)
-lc.set_array(z)
-lc.set_linewidth(3)
+norm = plt.Normalize(dydx.min(), dydx.max())
+lc = LineCollection(segments, cmap='viridis', norm=norm)
+# Set the values used for colormapping
+lc.set_array(dydx)
+lc.set_linewidth(2)
 
-fig1 = plt.figure()
-plt.gca().add_collection(lc)
-plt.xlim(x.min(), x.max())
-plt.ylim(-1.1, 1.1)
+fig, ax = plt.subplots()
+line = ax.add_collection(lc)
+fig.colorbar(line)
 
-# Now do a second plot coloring the curve using a continuous colormap
-t = np.linspace(0, 10, 200)
-x = np.cos(np.pi * t)
-y = np.sin(t)
-points = np.array([x, y]).T.reshape(-1, 1, 2)
-segments = np.concatenate([points[:-1], points[1:]], axis=1)
+ax.set_xlim(x.min(), x.max())
+ax.set_ylim(-1.1, 1.1)
 
-lc = LineCollection(segments, cmap=plt.get_cmap('copper'),
-                    norm=plt.Normalize(0, 10))
-lc.set_array(t)
-lc.set_linewidth(3)
-
-fig2 = plt.figure()
-plt.gca().add_collection(lc)
-plt.xlim(-1, 1)
-plt.ylim(-1, 1)
 plt.show()
