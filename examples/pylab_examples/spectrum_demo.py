f2bc54bfbda234b5fc76a43328cf0437db483448
Dietrich Brunn <Dietrich.Brunn@web.de>
2017-03-02 00:18:04 +0100

    Added plot titles to spectrum_demo.py

--- examples/pylab_examples/spectrum_demo.py
+++ examples/pylab_examples/spectrum_demo.py
@@ -23,20 +23,29 @@ r = np.exp(-t/0.05)
 cnse = np.convolve(nse, r)*dt
 cnse = cnse[:len(t)]
 
-s = 0.1*np.sin(2*np.pi*t) + cnse  # the signal
+s = 0.1*np.sin(4*np.pi*t) + cnse  # the signal
 
-fig, axes = plt.subplots(nrows=3, ncols=2)
+fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(7, 7))
 
 # plot time signal:
-axes[0, 0].plot(t, s)
+axes[0, 0].set_title("Signal")
+axes[0, 0].plot(t, s, color='C0')
 axes[0, 0].set_xlabel("Time")
 axes[0, 0].set_ylabel("Amplitude")
 
 # plot different spectrum types:
-axes[1, 0].magnitude_spectrum(s, Fs=Fs)
-axes[1, 1].magnitude_spectrum(s, Fs=Fs, scale='dB')
-axes[2, 0].phase_spectrum(s, Fs=Fs)
-axes[2, 1].angle_spectrum(s, Fs=Fs)
+axes[1, 0].set_title("Magnitude Spectrum")
+axes[1, 0].magnitude_spectrum(s, Fs=Fs, color='C1')
+
+axes[1, 1].set_title("Log. Magnitude Spectrum")
+axes[1, 1].magnitude_spectrum(s, Fs=Fs, scale='dB', color='C1')
+
+axes[2, 0].set_title("Phase Spectrum ")
+axes[2, 0].phase_spectrum(s, Fs=Fs, color='C2')
+
+axes[2, 1].set_title("Angle Spectrum")
+axes[2, 1].angle_spectrum(s, Fs=Fs, color='C2')
+
 axes[0, 1].remove()  # don't display empty ax
 
 fig.tight_layout()
