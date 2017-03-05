a0f91fa6ae94ed6955e79e0bdf8b98627db098ad
helmiriawan <helmiriawan@live.com>
2017-03-05 21:36:38 +0100

    merged the tex_X.py files into a single file tex.py

--- examples/pylab_examples/tex.py
+++ examples/pylab_examples/tex.py
@@ -13,11 +13,14 @@ time, the run may be silent, as a lot of the information is cached in
 ~/.tex.cache
 
 """
+from __future__ import unicode_literals
 import numpy as np
+import matplotlib
+matplotlib.rcParams['text.usetex'] = True
+matplotlib.rcParams['text.latex.unicode'] = True
 import matplotlib.pyplot as plt
 
 
-plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 plt.figure(1, figsize=(6, 4))
 ax = plt.axes([0.1, 0.1, 0.8, 0.7])
@@ -27,8 +30,23 @@ plt.plot(t, s)
 
 plt.xlabel(r'\textbf{time (s)}')
 plt.ylabel(r'\textit{voltage (mV)}', fontsize=16)
-plt.title(r"\TeX\ is Number $\displaystyle\sum_{n=1}^\infty"
-          r"\frac{-e^{i\pi}}{2^n}$!", fontsize=16, color='r')
+plt.title(r'\TeX\ is Number $\displaystyle\sum_{n=1}^\infty'
+          r'\frac{-e^{i\pi}}{2^n}$!', fontsize=16, color='r')
 plt.grid(True)
 plt.savefig('tex_demo')
+plt.show()
+
+
+
+plt.figure(2, figsize=(6, 4))
+ax = plt.axes([0.1, 0.1, 0.8, 0.7])
+t = np.arange(0.0, 1.0 + 0.01, 0.01)
+s = np.cos(2*2*np.pi*t) + 2
+plt.plot(t, s)
+
+plt.xlabel(r'\textbf{time (s)}')
+plt.ylabel('\\textit{Velocity (\u00B0/sec)}', fontsize=16)
+plt.title(r'\TeX\ is Number $\displaystyle\sum_{n=1}^\infty'
+          r'\frac{-e^{i\pi}}{2^n}$!', fontsize=16, color='r')
+plt.grid(True)
 plt.show()
