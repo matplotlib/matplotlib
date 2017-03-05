c03df1feab8f954c8179c6c77a5e9e410cbd1d52
Adrien F Vincent <vincent.adrien@gmail.com>
2017-03-05 11:39:30 +0100

    Add a non-picture test dedicated to the 'colors' parameter

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -27,6 +27,7 @@ import matplotlib.patches as mpatches
 import matplotlib.colors as mcolors
 from numpy.testing import assert_allclose, assert_array_equal
 from matplotlib.cbook import IgnoredKeywordWarning
+from matplotlib.cbook._backports import broadcast_to
 
 # Note: Some test cases are run twice: once normally and once with labeled data
 #       These two must be defined in the same test function or need to have
@@ -2908,6 +2909,33 @@ def test_eventplot_defaults():
     axobj = fig.add_subplot(111)
     colls = axobj.eventplot(data)
 
+
+@pytest.mark.parametrize(('colors'), [
+    ('0.5',),  # string color with multiple characters: not OK before #8193 fix
+    ('tab:orange', 'tab:pink', 'tab:cyan', 'bLacK'),  # case-insensitive
+    ('red', (0, 1, 0), None, (1, 0, 1, 0.5)),  # a tricky case mixing types
+    ('rgbk',)  # len('rgbk') == len(data) and each character is a valid color
+])
+def test_eventplot_colors(colors):
+    '''Test the *colors* parameter of eventplot. Inspired by the issue #8193.
+    '''
+    data = [[i] for i in range(4)]  # 4 successive events of different nature
+
+    # Build the list of the expected colors
+    expected = [c if c is not None else 'C0' for c in colors]
+    # Convert the list into an array of RGBA values
+    # NB: ['rgbk'] is not a valid argument for to_rgba_array, while 'rgbk' is.
+    if len(expected) == 1:
+        expected = expected[0]
+    expected = broadcast_to(mcolors.to_rgba_array(expected), (len(data), 4))
+
+    fig, ax = plt.subplots()
+    if len(colors) == 1:  # tuple with a single string (like '0.5' or 'rgbk')
+        colors = colors[0]
+    collections = ax.eventplot(data, colors=colors)
+
+    for coll, color in zip(collections, expected):
+        assert_allclose(coll.get_color(), color)
 
 @image_comparison(baseline_images=['test_eventplot_problem_kwargs'],
                   extensions=['png'], remove_text=True)
