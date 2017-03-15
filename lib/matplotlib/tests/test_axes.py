1a55203717d1115db8b0693d4b5d67470217798a
Thomas A Caswell <tcaswell@gmail.com>
2017-03-15 07:42:06 -0400

    STY: fix whitespace in the tests

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -1246,8 +1246,10 @@ def test_bar_tick_label_multiple():
     ax.bar([1, 2.5], [1, 2], width=[0.2, 0.5], tick_label=['a', 'b'],
            align='center')
 
-@image_comparison(baseline_images=['bar_tick_label_multiple_old_label_alignment'],
-                  extensions=['png'])
+
+@image_comparison(
+    baseline_images=['bar_tick_label_multiple_old_label_alignment'],
+    extensions=['png'])
 def test_bar_tick_label_multiple_old_alignment():
     # Test that the algnment for class is backward compatible
     matplotlib.rcParams["ytick.alignment"] = "center"
