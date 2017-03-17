3ed55f480ccc62eba4177d21f7a0cf065eed9bd5
Vidur Satija <vidursatija@gmail.com>
2017-03-17 10:01:36 +0530

    Added __copy__ to Colormap

--- lib/matplotlib/colors.py
+++ lib/matplotlib/colors.py
@@ -529,7 +529,18 @@ class Colormap(object):
         if vtype == 'scalar':
             rgba = tuple(rgba[0, :])
         return rgba
-
+    
+    def __copy__(self):
+        """Create new object with the same class and update attributes
+           If object is initialized, copy the elements of _lut list
+        """
+        cls = self.__class__
+    	newCMapObj = cls.__new__(cls)
+    	newCMapObj.__dict__.update(self.__dict__)
+        if self._isinit:
+            newCMapObj._lut = np.copy(self._lut)
+        return newCMapObj
+    
     def set_bad(self, color='k', alpha=None):
         """Set color to be used for masked values.
         """
