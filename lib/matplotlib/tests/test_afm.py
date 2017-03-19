1667a0c01394ff268aa43412d1174b0cbafed21b
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2017-03-19 11:59:42 -0700

    FIX reviewers's comment

--- lib/matplotlib/tests/test_afm.py
+++ lib/matplotlib/tests/test_afm.py
@@ -2,12 +2,11 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import matplotlib.afm as afm
-import six
 
 
 def test_nonascii_str():
     # This tests that we also decode bytes as utf-8 properly.
-    # Else, font files with non ascii caracters fail to load.
+    # Else, font files with non ascii characters fail to load.
     inp_str = "привет"
     byte_str = inp_str.encode("utf8")
 
