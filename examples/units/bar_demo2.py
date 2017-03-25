6a18d9c0083c2b692cf8d0e42f936e3ddd079c2b
David Stansby <dstansby@gmail.com>
2017-03-25 16:21:34 +0000

    Clean up unit examples:

--- examples/units/bar_demo2.py
+++ examples/units/bar_demo2.py
@@ -3,12 +3,12 @@
 Bar demo with units
 ===================
 
-plot using a variety of cm vs inches conversions.  The example shows
-how default unit instrospection works (ax1), how various keywords can
+A plot using a variety of centimetre vs inches conversions.  This example shows
+how default unit introspection works (ax1), how various keywords can
 be used to set the x and y units to override the defaults (ax2, ax3,
 ax4) and how one can set the xlimits using scalars (ax3, current units
 assumed) or units (conversions applied to get the numbers to current
-units)
+units).
 
 """
 import numpy as np
--- examples/units/radian_demo.py
+++ examples/units/radian_demo.py
@@ -3,7 +3,8 @@
 Radian ticks
 ============
 
-Plot with radians from the basic_units mockup example package
+Plot with radians from the basic_units mockup example package.
+
 This example shows how the unit class can determine the tick locating,
 formatting and axis labeling.
 """
