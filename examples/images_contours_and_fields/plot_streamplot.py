913fdb7331c0b5d6ddc0ea8f07e1a19ba7585b74
patniharshit <patniharshit@gmail.com>
2017-03-28 18:25:33 +0530

    Remove data regeneration

--- examples/images_contours_and_fields/plot_streamplot.py
+++ examples/images_contours_and_fields/plot_streamplot.py
@@ -3,14 +3,14 @@
 Streamplot
 ==========
 
-A stream plot, or stream line plot, is used to display 2D vector fields. This
-example shows a few features of the stream plot function:
+A stream plot, or streamline plot, is used to display 2D vector fields. This
+example shows a few features of the streamplot function:
 
-    * Varying the color along a stream line.
-    * Varying the density of stream lines.
-    * Varying the line width along a stream line.
-    * Controlling the starting points of stream lines.
-    * Stream lines skipping masked regions and NaN values.
+    * Varying the color along a streamline.
+    * Varying the density of streamlines.
+    * Varying the line width along a streamline.
+    * Controlling the starting points of streamlines.
+    * Streamlines skipping masked regions and NaN values.
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,7 +34,7 @@ ax0.set_title('Varying Density')
 ax1 = fig.add_subplot(gs[0, 1])
 strm = ax1.streamplot(X, Y, U, V, color=U, linewidth=2, cmap='autumn')
 fig.colorbar(strm.lines)
-ax1.set_title('Varying color')
+ax1.set_title('Varying Color')
 
 #  Varying line width along a streamline
 ax2 = fig.add_subplot(gs[1, 0])
@@ -43,9 +43,6 @@ ax2.streamplot(X, Y, U, V, density=0.6, color='k', linewidth=lw)
 ax2.set_title('Varying Line Width')
 
 # Controlling the starting points of the streamlines
-X, Y = (np.linspace(-3, 3, 100),
-        np.linspace(-3, 3, 100))
-U, V = np.mgrid[-3:3:100j, 0:0:100j]
 seed_points = np.array([[-2, -1, 0, 1, 2, -1], [-2, -1,  0, 1, 2, 2]])
 
 ax3 = fig.add_subplot(gs[1, 1])
@@ -59,12 +56,6 @@ ax3.plot(seed_points[0], seed_points[1], 'bo')
 ax3.axis((-w, w, -w, w))
 
 # Create a mask
-w = 3
-Y, X = np.mgrid[-w:w:100j, -w:w:100j]
-U = -1 - X**2 + Y
-V = 1 + X - Y**2
-speed = np.sqrt(U*U + V*V)
-
 mask = np.zeros(U.shape, dtype=bool)
 mask[40:60, 40:60] = True
 U[:20, :20] = np.nan
