72e581a55673ffaebc90983782887aa416d9b629
Antony Lee <anntzer.lee@gmail.com>
2017-04-13 20:15:25 -0700

    More robust check for numpoints in legend_handler.

--- lib/matplotlib/legend_handler.py
+++ lib/matplotlib/legend_handler.py
@@ -149,15 +149,14 @@ class HandlerNpoints(HandlerBase):
         numpoints = self.get_numpoints(legend)
 
         if numpoints > 1:
-            # we put some pad here to compensate the size of the
-            # marker
+            # we put some pad here to compensate the size of the marker
             pad = self._marker_pad * fontsize
             xdata = np.linspace(-xdescent + pad,
                                 -xdescent + width - pad,
                                 numpoints)
             xdata_marker = xdata
-        elif numpoints == 1:
-            xdata = np.linspace(-xdescent, -xdescent+width, 2)
+        else:
+            xdata = np.linspace(-xdescent, -xdescent + width, 2)
             xdata_marker = [-xdescent + 0.5 * width]
 
         return xdata, xdata_marker
