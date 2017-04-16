428dc54d15c4fe3630df2e54fb79acf6c9206236
Keerysanth Sribaskaran <keerush@live.com>
2017-04-16 19:52:05 -0400

    Changed test to look at text extents, and removed baseline image.

--- lib/matplotlib/tests/test_text.py
+++ lib/matplotlib/tests/test_text.py
@@ -423,10 +423,22 @@ def test_font_scaling():
         ax.text(0.1, i*30, "{fs} pt font size".format(fs=fs), fontsize=fs)
 
 
-@pytest.mark.parametrize('spacing1, spacing2', [(2, 0.4), (0.4, 2)])
-@image_comparison(baseline_images=['line_spacing'], extensions=['png'],
-                  tol=0.01)
-def two_2line_texts(spacing1, spacing2):
+@pytest.mark.parametrize('spacing1, spacing2', [(0.4, 2), (2, 0.4), (2, 2)])
+def test_two_2line_texts(spacing1, spacing2):
     text_string = 'line1\nline2'
-    plt.text(0.5, 0.5, text_string, linespacing=spacing1, alpha=0.5)
-    plt.text(0.5, 0.5, text_string, linespacing=spacing2, alpha=0.5)
+    fig = plt.figure()
+    renderer = fig.canvas.get_renderer()
+
+    text1 = plt.text(0.25, 0.5, text_string, linespacing=spacing1)
+    text2 = plt.text(0.25, 0.5, text_string, linespacing=spacing2)
+    fig.canvas.draw()
+
+    box1 = text1.get_window_extent(renderer=renderer)
+    box2 = text2.get_window_extent(renderer=renderer)
+
+    # line spacing only affects height
+    assert box1.width == box2.width
+    if (spacing1 == spacing2):
+        assert box1.height == box2.height
+    else:
+        assert box1.height != box2.height
