d0cfe37ec401bd3ad80ea2dfae20e986ccc80ada
Ryan Watkins <watkinrt@gmail.com>
2017-07-04 13:45:18 -0700

    With the resent merging of the sphinx-gallery branch, I went through and updated the docstring to appropriately escape the backslashes.

--- examples/text_labels_and_annotations/dfrac_demo.py
+++ examples/text_labels_and_annotations/dfrac_demo.py
@@ -1,19 +1,19 @@
 r"""
 =========================================
-The difference between \dfrac and \frac
+The difference between \\dfrac and \\frac
 =========================================
 
-In this example, the differences between the \dfrac and \frac TeX macros are
+In this example, the differences between the \\dfrac and \\frac TeX macros are
 illustrated; in particular, the difference between display style and text style
 fractions when using Mathtex.
 
 .. versionadded:: 2.1
 
 .. note::
-    To use \dfrac with the LaTeX engine (text.usetex : True), you need to
+    To use \\dfrac with the LaTeX engine (text.usetex : True), you need to
     import the amsmath package with the text.latex.preamble rc, which is
     an unsupported feature; therefore, it is probably a better idea to just
-    use the \displaystyle option before the \frac macro to get this behavior
+    use the \\displaystyle option before the \\frac macro to get this behavior
     with the LaTeX engine.
 
 """
