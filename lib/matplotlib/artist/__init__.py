410e191c82e3fb4f5b3c32a80843dc2cdbfb3b29
Kaitlyn Chait <katiec1029@gmail.com>
2017-07-07 20:05:30 -0400

    added artist directory & artist.py

---
+++ lib/matplotlib/artist/__init__.py
@@ -0,0 +1,5 @@
+# from .artist import Artist
+# from .artist import allow_rasterization
+# from .artist import kwdoc
+from .artist import *
+# from .artist.artist import Artist, allow_rasterization, kwdoc
