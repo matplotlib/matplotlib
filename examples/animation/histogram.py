7197efc14f99cce20e565a156e9d89cea3a38295
John Hoffman <jah5@princeton.edu>
2017-07-15 17:11:45 -0500

    minor documentation changes, color_cycle has been moved from examples to tutorials, edit to animations/histogram

--- examples/animation/histogram.py
+++ examples/animation/histogram.py
@@ -14,8 +14,6 @@ import matplotlib.patches as patches
 import matplotlib.path as path
 import matplotlib.animation as animation
 
-fig, ax = plt.subplots()
-
 # Fixing random state for reproducibility
 np.random.seed(19680801)
 
@@ -30,12 +28,20 @@ bottom = np.zeros(len(left))
 top = bottom + n
 nrects = len(left)
 
-# here comes the tricky part -- we have to set up the vertex and path
-# codes arrays using moveto, lineto and closepoly
-
-# for each rect: 1 for the MOVETO, 3 for the LINETO, 1 for the
-# CLOSEPOLY; the vert for the closepoly is ignored but we still need
-# it to keep the codes aligned with the vertices
+###############################################################################
+# Here comes the tricky part -- we have to set up the vertex and path codes
+# arrays using `plt.Path.MOVETO`, `plt.Path.LINETO` and `plt.Path.CLOSEPOLY`
+# for each rect.
+#
+# * We need 1 `MOVETO` per rectangle, which sets the initial point.
+# * We need 3 `LINETO`'s, which tell `matplotlib` to draw lines from vertex 1
+#   to vertex 2, v2 to v3, and v3 to v4.
+# * We then need one `CLOSEPOLY` which tells matplotlib to draw a line from the
+#   v4 to our initial vertex (the `MOVETO` vertex), in order to close the
+#   polygon.
+#
+# Note: the vertex for `CLOSEPOLY` is ignored, but we still need a placeholder
+# in the `verts` array to keep the codes aligned with the vertices.
 nverts = nrects*(1 + 3 + 1)
 verts = np.zeros((nverts, 2))
 codes = np.ones(nverts, int) * path.Path.LINETO
@@ -50,13 +56,12 @@ verts[2::5, 1] = top
 verts[3::5, 0] = right
 verts[3::5, 1] = bottom
 
-barpath = path.Path(verts, codes)
-patch = patches.PathPatch(
-    barpath, facecolor='green', edgecolor='yellow', alpha=0.5)
-ax.add_patch(patch)
-
-ax.set_xlim(left[0], right[-1])
-ax.set_ylim(bottom.min(), top.max())
+###############################################################################
+# To animate the histogram, we need an `animate` function, which generates
+# a random set of numbers and updates the locations of the vertices for the
+# histogram (in this case, only the heights of each rectangle). `patch` will
+# eventually be a `Patch` object.
+patch = None
 
 
 def animate(i):
@@ -68,5 +73,18 @@ def animate(i):
     verts[2::5, 1] = top
     return [patch, ]
 
+###############################################################################
+# And now we build the `Path` and `Patch` instances for the histogram using
+# our vertices and codes. We add the patch to the `Axes` instance, and setup
+# the `FuncAnimation` with our animate function.
+fig, ax = plt.subplots()
+barpath = path.Path(verts, codes)
+patch = patches.PathPatch(
+    barpath, facecolor='green', edgecolor='yellow', alpha=0.5)
+ax.add_patch(patch)
+
+ax.set_xlim(left[0], right[-1])
+ax.set_ylim(bottom.min(), top.max())
+
 ani = animation.FuncAnimation(fig, animate, 100, repeat=False, blit=True)
-plt.show()
+# plt.show()
