b2a2dccef7052753b8e2c1d99cf0183a03494c17
Dana <dmgt@users.noreply.github.com>
2017-07-16 12:34:13 -0500

    Updated simple plot example to OO format

--- examples/pylab_examples/simple_plot.py
+++ examples/pylab_examples/simple_plot.py
@@ -7,13 +7,19 @@ Simple Plot
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Data for plotting
 t = np.arange(0.0, 2.0, 0.01)
 s = 1 + np.sin(2*np.pi*t)
-plt.plot(t, s)
 
-plt.xlabel('time (s)')
-plt.ylabel('voltage (mV)')
-plt.title('About as simple as it gets, folks')
-plt.grid(True)
-plt.savefig("test.png")
+# Note that using plt.subplots below is equivilent to using
+# fig = plt.figure and then ax = fig.add_subplot(111)
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='About as simple as it gets, folks')
+ax.grid()
+
+fig.savefig("test.png")
 plt.show()
+
