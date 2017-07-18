ac2abfab50cc30597d03213aca5ae41715237be0
tonyyli <tony.y.li@gmail.com>
2017-07-17 22:39:14 -0700

    Edits for formatting / indentation

--- examples/api/sankey_basics.py
+++ examples/api/sankey_basics.py
@@ -13,6 +13,7 @@ from matplotlib.sankey import Sankey
 
 ###############################################################################
 # Example 1 -- Mostly defaults
+#
 # This demonstrates how to create a simple diagram by implicitly calling the
 # Sankey.add() method and by appending finish() to the call to the class.
 
@@ -23,24 +24,28 @@ plt.title("The default settings produce a diagram like this.")
 
 ###############################################################################
 # Notice:
-#   1. Axes weren't provided when Sankey() was instantiated, so they were
-#      created automatically.
-#   2. The scale argument wasn't necessary since the data was already
-#      normalized.
-#   3. By default, the lengths of the paths are justified.
+#
+# 1. Axes weren't provided when Sankey() was instantiated, so they were
+#    created automatically.
+# 2. The scale argument wasn't necessary since the data was already
+#    normalized.
+# 3. By default, the lengths of the paths are justified.
+
 
 ###############################################################################
 # Example 2
+#
 # This demonstrates:
-#   1. Setting one path longer than the others
-#   2. Placing a label in the middle of the diagram
-#   3. Using the scale argument to normalize the flows
-#   4. Implicitly passing keyword arguments to PathPatch()
-#   5. Changing the angle of the arrow heads
-#   6. Changing the offset between the tips of the paths and their labels
-#   7. Formatting the numbers in the path labels and the associated unit
-#   8. Changing the appearance of the patch and the labels after the figure is
-#      created
+#
+# 1. Setting one path longer than the others
+# 2. Placing a label in the middle of the diagram
+# 3. Using the scale argument to normalize the flows
+# 4. Implicitly passing keyword arguments to PathPatch()
+# 5. Changing the angle of the arrow heads
+# 6. Changing the offset between the tips of the paths and their labels
+# 7. Formatting the numbers in the path labels and the associated unit
+# 8. Changing the appearance of the patch and the labels after the figure is
+#    created
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1, xticks=[], yticks=[],
@@ -60,18 +65,22 @@ diagrams[0].text.set_fontweight('bold')
 
 ###############################################################################
 # Notice:
-#   1. Since the sum of the flows is nonzero, the width of the trunk isn't
-#      uniform.  If verbose.level is helpful (in matplotlibrc), a message is
-#      given in the terminal window.
-#   2. The second flow doesn't appear because its value is zero.  Again, if
-#      verbose.level is helpful, a message is given in the terminal window.
+#
+# 1. Since the sum of the flows is nonzero, the width of the trunk isn't
+#    uniform.  If verbose.level is helpful (in matplotlibrc), a message is
+#    given in the terminal window.
+# 2. The second flow doesn't appear because its value is zero.  Again, if
+#    verbose.level is helpful, a message is given in the terminal window.
+
 
 ###############################################################################
 # Example 3
+#
 # This demonstrates:
-#   1. Connecting two systems
-#   2. Turning off the labels of the quantities
-#   3. Adding a legend
+#
+# 1. Connecting two systems
+# 2. Turning off the labels of the quantities
+# 3. Adding a legend
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1, xticks=[], yticks=[], title="Two Systems")
