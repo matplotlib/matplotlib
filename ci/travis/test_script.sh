0909c06ae3f93b88cecc474259ff3b2893718f2e
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2017-07-21 16:55:19 -0400

    Build  docs with Circle CI (#8893)
    * Remove documentation builds from Travis.
    
    * Enable docs build on CircleCI.
    
    * Ensure we have tags on CircleCI.
    
    This helps to provide a reasonable version number.
    
    * Attempt to use CircleCI 2.0.
    
    * Remove mencoder from doc build.
    
    It's not available on jessie, the distro on which CircleCI's docker
    image is based.
    
    * ci: Install doc requirements in user directory.
    
    The docker image does not appear to be running as root.
    
    * ci: Use explicit path to linkchecker.
    
    * ci: Try to remove redundancy in CircleCI config.
    
    * ci: Store doc artifacts after CircleCI build.
    
    * ci: Rename CircleCI build names to be clearer.
    
    Add 'doc-' prefix so that it will be clear on the GitHub status list
    what these builds are doing.
    
    * ci: Print out documentation link at end of build.
    
    * ci: Re-add documentation deploy script on CircleCI.
    
    * ci: Delete existing files before deploying docs.
    
    Otherwise, random files might be left over, such as the old gallery.html
    that was removed when switching to sphinx-gallery.
    
    * ci: Bundle and store sphinx-gallery artifacts also.

--- ci/travis/test_script.sh
+++ ci/travis/test_script.sh
@@ -11,31 +11,18 @@ set -ev
 # Travis VM to run out of memory (since so many copies of inkscape and
 # ghostscript are running at the same time).
 
-if [[ $BUILD_DOCS == false ]]; then
-  if [[ $DELETE_FONT_CACHE == 1 ]]; then
-    rm -rf ~/.cache/matplotlib
-  fi
-  # Workaround for pytest-xdist flaky collection order
-  # https://github.com/pytest-dev/pytest/issues/920
-  # https://github.com/pytest-dev/pytest/issues/1075
-  export PYTHONHASHSEED=$(python -c 'import random; print(random.randint(1, 4294967295))')
-  echo PYTHONHASHSEED=$PYTHONHASHSEED
+if [[ $DELETE_FONT_CACHE == 1 ]]; then
+  rm -rf ~/.cache/matplotlib
+fi
+# Workaround for pytest-xdist flaky collection order
+# https://github.com/pytest-dev/pytest/issues/920
+# https://github.com/pytest-dev/pytest/issues/1075
+export PYTHONHASHSEED=$(python -c 'import random; print(random.randint(1, 4294967295))')
+echo PYTHONHASHSEED=$PYTHONHASHSEED
 
-  echo The following args are passed to pytest $PYTEST_ARGS $RUN_PEP8
-  if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    pytest $PYTEST_ARGS $RUN_PEP8
-  else
-    gdb -return-child-result -batch -ex r -ex bt --args python $PYTHON_ARGS -m pytest $PYTEST_ARGS $RUN_PEP8
-  fi
+echo The following args are passed to pytest $PYTEST_ARGS $RUN_PEP8
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+  pytest $PYTEST_ARGS $RUN_PEP8
 else
-  cd doc
-  python make.py html
-  # We don't build the LaTeX docs here, so linkchecker will complain
-  touch build/html/Matplotlib.pdf
-  # Linkchecker only works with python 2.7 for the time being
-  source ~/virtualenv/python2.7/bin/activate
-  pip install pip --upgrade
-  # linkchecker is currently broken with requests 2.10.0 so force an earlier version
-  pip install $PRE requests==2.9.2 linkchecker
-  linkchecker build/html/index.html
+  gdb -return-child-result -batch -ex r -ex bt --args python $PYTHON_ARGS -m pytest $PYTEST_ARGS $RUN_PEP8
 fi
