ac8f340c9a771871c894aecdc12a9111958271dd
keithbriggs <keith.briggs@bt.com>
2017-08-01 09:47:03 +0100

    Update colorbar.py

--- lib/matplotlib/colorbar.py
+++ lib/matplotlib/colorbar.py
@@ -52,7 +52,7 @@ make_axes_kw_doc = '''
     *fraction*    0.15; fraction of original axes to use for colorbar
     *pad*         0.05 if vertical, 0.15 if horizontal; fraction
                   of original axes between colorbar and new image axes
-    *shrink*      1.0; fraction by which to multiply the size of  the colorbar
+    *shrink*      1.0; fraction by which to multiply the size of the colorbar
     *aspect*      20; ratio of long to short dimensions
     *anchor*      (0.0, 0.5) if vertical; (0.5, 1.0) if horizontal;
                   the anchor point of the colorbar axes
