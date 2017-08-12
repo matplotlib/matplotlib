e289d774ff8de9fadbb7e72039b2da6ae56d9eec
gnaggnoyil <gnaggnoyil@gmail.com>
2017-08-13 07:09:42 +0800

    correct test_RRuleLocator_dayrange

--- lib/matplotlib/tests/test_dates.py
+++ lib/matplotlib/tests/test_dates.py
@@ -130,8 +130,8 @@ def test_RRuleLocator():
 
 def test_RRuleLocator_dayrange():
     loc = mdates.DayLocator()
-    x1 = datetime.datetime(year=1, month=1, day=1)
-    y1 = datetime.datetime(year=1, month=1, day=16)
+    x1 = datetime.datetime(year=1, month=1, day=1, tzinfo=pytz.UTC)
+    y1 = datetime.datetime(year=1, month=1, day=16, tzinfo=pytz.UTC)
     loc.tick_values(x1, y1)
     # On success, no overflow error shall be thrown
 
