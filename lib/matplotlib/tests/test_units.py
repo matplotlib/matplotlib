a90064ebac99a8a0f135a2b264de55e7d74dc3e4
Ryan May <rmay31@gmail.com>
2017-08-17 11:26:27 -0600

    ENH: Add test for masked units behavior

--- lib/matplotlib/tests/test_units.py
+++ lib/matplotlib/tests/test_units.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.testing.decorators import image_comparison
 import matplotlib.units as munits
 import numpy as np
 
@@ -9,24 +10,25 @@ except ImportError:
     from mock import MagicMock
 
 
+# Basic class that wraps numpy array and has units
+class Quantity(object):
+    def __init__(self, data, units):
+        self.magnitude = data
+        self.units = units
+
+    def to(self, new_units):
+        return Quantity(self.magnitude, new_units)
+
+    def __getattr__(self, attr):
+        return getattr(self.magnitude, attr)
+
+    def __getitem__(self, item):
+        return self.magnitude[item]
+
+
 # Tests that the conversion machinery works properly for classes that
 # work as a facade over numpy arrays (like pint)
 def test_numpy_facade():
-    # Basic class that wraps numpy array and has units
-    class Quantity(object):
-        def __init__(self, data, units):
-            self.magnitude = data
-            self.units = units
-
-        def to(self, new_units):
-            return Quantity(self.magnitude, new_units)
-
-        def __getattr__(self, attr):
-            return getattr(self.magnitude, attr)
-
-        def __getitem__(self, item):
-            return self.magnitude[item]
-
     # Create an instance of the conversion interface and
     # mock so we can check methods called
     qc = munits.ConversionInterface()
@@ -55,3 +57,15 @@ def test_numpy_facade():
     assert qc.convert.called
     assert qc.axisinfo.called
     assert qc.default_units.called
+
+
+# Tests gh-8908
+@image_comparison(baseline_images=['plot_masked_units'],
+                  extensions=['png'], remove_text=True, style='mpl20')
+def test_plot_masked_units():
+    data = np.linspace(-5, 5)
+    data_masked = np.ma.array(data, mask=(data > -2) & (data < 2))
+    data_masked_units = Quantity(data_masked, 'meters')
+
+    fig, ax = plt.subplots()
+    ax.plot(data_masked_units)
