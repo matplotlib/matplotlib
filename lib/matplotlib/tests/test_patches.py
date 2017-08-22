03fba6b4c784067636e59ac23a25fa4baaacafca
Paul Ganssle <paul@ganssle.io>
2017-08-22 15:44:09 -0400

    Add a test to ensure that datetimes cannot be used as deltas.

--- lib/matplotlib/tests/test_patches.py
+++ lib/matplotlib/tests/test_patches.py
@@ -374,3 +374,16 @@ def test_datetime_rectangle():
 
     fig, ax = plt.subplots()
     ax.add_patch(patch)
+
+
+def test_datetime_datetime_fails():
+    from datetime import datetime
+
+    start = datetime(2017, 1, 1, 0, 0, 0)
+    dt_delta = datetime(1970, 1, 5)    # Will be 5 days if units are done wrong
+
+    with pytest.raises(TypeError):
+        mpatches.Rectangle((start, 0), dt_delta, 1)
+
+    with pytest.raises(TypeError):
+        mpatches.Rectangle((0, start), 1, dt_delta)
