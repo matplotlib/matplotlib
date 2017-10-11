b30362211565c608c9b7b93928af3ddf337802d1
Antony Lee <anntzer.lee@gmail.com>
2017-10-11 15:28:57 -0700

    Merge pull request #9376 from wjmallard/fix-docstring-typo
    Fix docstring typo in Rectangle, Ellipse, and Spine.

--- lib/matplotlib/spines.py
+++ lib/matplotlib/spines.py
@@ -136,7 +136,7 @@ class Spine(mpatches.Patch):
     def _recompute_transform(self):
         """NOTE: This cannot be called until after this has been added
                  to an Axes, otherwise unit conversion will fail. This
-                 maxes it very important to call the accessor method and
+                 makes it very important to call the accessor method and
                  not directly access the transformation member variable.
         """
         assert self._patch_type in ('arc', 'circle')
