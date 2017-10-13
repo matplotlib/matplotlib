7aaa222f7df7cc1bad4f4a8fd3c9c2bfe9057e05
Jody Klymak <jklymak@gmail.com>
2017-10-12 21:48:02 -0700

    Add pcolor and contour to tutorial/introductory/sample_plots

--- tutorials/introductory/sample_plots.py
+++ tutorials/introductory/sample_plots.py
@@ -36,14 +36,14 @@ Multiple axes (i.e. subplots) are created with the
 
    Subplot
 
-.. _screenshots_pcolormesh_demo
+.. _screenshots_pcolormesh_demo:
 
 Contouring and pseudocolor
 ==========================
 
 The :func:`~matplotlib.pyplot.pcolormesh` command can make a colored
 representation of a two-dimensional array.  The
-:func:`~matplotlib.pyplot.contour` command is another way to representation
+:func:`~matplotlib.pyplot.contour` command is another way to represent
 the same data:
 
 .. figure:: ../../gallery/images_contours_and_fields/images/sphx_glr_pcolormesh_levels_001.png
@@ -51,7 +51,7 @@ the same data:
    :align: center
    :scale: 50
 
-   Contour and Pcolormesh 
+   Contour and Pcolormesh
 
 .. _screenshots_histogram_demo:
 
