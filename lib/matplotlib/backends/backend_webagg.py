b14f1e0eaab46fa67ab3c85e222504b221439354
cgohlke <cjgohlke@gmail.com>
2017-10-16 12:55:18 -0700

    Remove explicit return in __init__

--- lib/matplotlib/backends/backend_webagg.py
+++ lib/matplotlib/backends/backend_webagg.py
@@ -78,8 +78,8 @@ class WebAggApplication(tornado.web.Application):
     class SingleFigurePage(tornado.web.RequestHandler):
         def __init__(self, application, request, **kwargs):
             self.url_prefix = kwargs.pop('url_prefix', '')
-            return tornado.web.RequestHandler.__init__(self, application,
-                                                       request, **kwargs)
+            tornado.web.RequestHandler.__init__(self, application,
+                                                request, **kwargs)
 
         def get(self, fignum):
             fignum = int(fignum)
@@ -98,8 +98,8 @@ class WebAggApplication(tornado.web.Application):
     class AllFiguresPage(tornado.web.RequestHandler):
         def __init__(self, application, request, **kwargs):
             self.url_prefix = kwargs.pop('url_prefix', '')
-            return tornado.web.RequestHandler.__init__(self, application,
-                                                       request, **kwargs)
+            tornado.web.RequestHandler.__init__(self, application,
+                                                request, **kwargs)
 
         def get(self):
             ws_uri = 'ws://{req.host}{prefix}/'.format(req=self.request,
