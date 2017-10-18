7a54c1e57838c9db6d4f58cb67949500ec31a7f4
Luca Verginer <luca@verginer.eu>
2017-10-18 23:42:31 +0200

    Wrap long line

--- lib/matplotlib/contour.py
+++ lib/matplotlib/contour.py
@@ -1672,7 +1672,8 @@ class QuadContourSet(ContourSet):
           contour(Z,N)
           contour(X,Y,Z,N)
 
-        contour up to *N+1* automatically chosed contour levels (*N* intervals).
+        contour up to *N+1* automatically chosed contour levels
+        (*N* intervals).
 
         ::
 
