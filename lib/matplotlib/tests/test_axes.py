b5bc6aa85f466e2841ab6bcd8e4dce2cab5f28c6
Thomas A Caswell <tcaswell@gmail.com>
2017-10-17 23:55:15 -0400

    FIX: provide __ne__ implementation for transforms in py2
    Closes #9455
    
    The issue is that due to `!=` not defaulting to `not a == b` in
    python2 (unlike python3) an internal check was failing and the y
    ticks (aka the radial ticks) were being regenerated on every draw
    and the alpha settings did not properly propagate.
    
    This fix is a band-aid to make the behavior the same on all versions
    of python.

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -5353,3 +5353,19 @@ def test_patch_deprecations():
         assert fig.patch == fig.figurePatch
 
     assert len(w) == 2
+
+
+def test_polar_gridlines():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, polar=True)
+
+    # make all major grid lines lighter, only x grid lines set in 2.1.0
+    ax.grid(alpha=0.2)
+
+    # hide y tick labels, no effect in 2.1.0
+    plt.setp(ax.yaxis.get_ticklabels(), visible=False)
+
+    fig.canvas.draw()
+
+    assert ax.xaxis.majorTicks[0].gridline.get_alpha() == .2
+    assert ax.yaxis.majorTicks[0].gridline.get_alpha() == .2
