6a685d2d3e6be4271db55c56eceb90c2a973c511
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2017-10-20 20:45:37 -0400

    Add tests that hinting_factor works.

--- lib/matplotlib/tests/test_text.py
+++ lib/matplotlib/tests/test_text.py
@@ -2,6 +2,8 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import six
+
+import io
 import warnings
 
 import numpy as np
@@ -448,3 +450,17 @@ def test_nonfinite_pos():
     ax.text(0, np.nan, 'nan')
     ax.text(np.inf, 0, 'inf')
     fig.canvas.draw()
+
+
+def test_hinting_factor_backends():
+    plt.rcParams['text.hinting_factor'] = 1
+    fig = plt.figure()
+    t = fig.text(0.5, 0.5, 'some text')
+
+    fig.savefig(io.BytesIO(), format='svg')
+    expected = t.get_window_extent().intervalx
+
+    fig.savefig(io.BytesIO(), format='png')
+    # Backends should apply hinting_factor consistently (within 10%).
+    np.testing.assert_allclose(t.get_window_extent().intervalx, expected,
+                               rtol=0.1)
