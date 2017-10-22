179ca3b518317e34eebdd24a47f0c12bedb6d61e
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2017-10-22 12:38:21 -0700

    DOC accessing named colors

--- examples/color/named_colors.py
+++ examples/color/named_colors.py
@@ -1,9 +1,13 @@
 """
-========================
-Visualizing named colors
-========================
+================
+Color references
+================
 
-Simple plot example with the named colors and its visual representation.
+All the named colors and what they look like.
+
+
+Matplotlib contains a number of color palettes. Here is what they look like,
+and how they are named.
 """
 from __future__ import division
 
@@ -11,45 +15,84 @@ import matplotlib.pyplot as plt
 from matplotlib import colors as mcolors
 
 
-colors = dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS)
+def plot_colors(colors):
+    # Sort colors by hue, saturation, value and name.
+    by_hsv = sorted(
+        (tuple(mcolors.rgb_to_hsv(mcolors.to_rgba(color)[:3])), name)
+        for name, color in colors.items())
+    sorted_names = [name for hsv, name in by_hsv]
 
-# Sort colors by hue, saturation, value and name.
-by_hsv = sorted((tuple(mcolors.rgb_to_hsv(mcolors.to_rgba(color)[:3])), name)
-                for name, color in colors.items())
-sorted_names = [name for hsv, name in by_hsv]
+    n = len(sorted_names)
+    if n > 400:
+        ncols = 8
+    else:
+        ncols = 4
+    nrows = n // ncols + 1
+    figsize = (2.4 * ncols, 0.2 * nrows)
+    fig, ax = plt.subplots(figsize=figsize)
 
-n = len(sorted_names)
-ncols = 4
-nrows = n // ncols + 1
+    # Get height and width
+    X, Y = fig.get_dpi() * fig.get_size_inches()
+    h = Y / (nrows + 1)
+    w = X / ncols
 
-fig, ax = plt.subplots(figsize=(8, 5))
+    for i, name in enumerate(sorted_names):
+        col = i % ncols
+        row = i // ncols
+        y = Y - (row * h) - h
 
-# Get height and width
-X, Y = fig.get_dpi() * fig.get_size_inches()
-h = Y / (nrows + 1)
-w = X / ncols
+        xi_line = w * (col + 0.05)
+        xf_line = w * (col + 0.25)
+        xi_text = w * (col + 0.3)
 
-for i, name in enumerate(sorted_names):
-    col = i % ncols
-    row = i // ncols
-    y = Y - (row * h) - h
+        ax.text(xi_text, y, name, fontsize=(h * 0.5),
+                horizontalalignment='left',
+                verticalalignment='center')
 
-    xi_line = w * (col + 0.05)
-    xf_line = w * (col + 0.25)
-    xi_text = w * (col + 0.3)
+        ax.hlines(y + h * 0.1, xi_line, xf_line,
+                  color=colors[name], linewidth=(h * 0.5))
 
-    ax.text(xi_text, y, name, fontsize=(h * 0.8),
-            horizontalalignment='left',
-            verticalalignment='center')
+    ax.set_xlim(0, X)
+    ax.set_ylim(0, Y)
+    ax.set_axis_off()
 
-    ax.hlines(y + h * 0.1, xi_line, xf_line,
-              color=colors[name], linewidth=(h * 0.6))
+    fig.subplots_adjust(left=0, right=1,
+                        top=1, bottom=0,
+                        hspace=0, wspace=0)
 
-ax.set_xlim(0, X)
-ax.set_ylim(0, Y)
-ax.set_axis_off()
 
-fig.subplots_adjust(left=0, right=1,
-                    top=1, bottom=0,
-                    hspace=0, wspace=0)
-plt.show()
+###############################################################################
+# Base colors
+# ===========
+#
+# Available in Matplotlib since the ages of dawn, the base colors offer the
+# user easy access to a large color palette.
+colors = dict(mcolors.BASE_COLORS)
+plot_colors(colors)
+
+
+###############################################################################
+# Tableau colors
+# ==============
+#
+# The tableau colors are the default the default in Matplotlib since 2.0, and
+# can be accessed directly through their names:
+colors = dict(mcolors.TABLEAU_COLORS)
+plot_colors(colors)
+
+###############################################################################
+# Short names
+# ===========
+#
+# For fast access, these 1-letter names cover the basics colors!
+colors = dict(mcolors.CSS4_COLORS)
+plot_colors(colors)
+
+###############################################################################
+# XKCD colors
+# ===========
+#
+# In case you think all the above aren't enough to suit your needs, the XKCD
+# color palette contains an insane number of colors... Check them out.
+colors = dict(mcolors.XKCD_COLORS)
+plot_colors(colors)
