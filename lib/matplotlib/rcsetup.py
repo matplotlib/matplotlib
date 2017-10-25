75856e36cdb137225216c06b6d4b687b0276b1e8
Antony Lee <anntzer.lee@gmail.com>
2017-10-25 01:54:30 -0700

    Don't bother with checking backends at setup time.
    We can just default to trying all backends in order until one loads.
    Only backends actually requiring a compiled component need to be handled
    in setupext.py now.
    
    Move the `mpl-data/*.glade` entry into a recursive glob for the
    Matplotlib SetupPackage (as there are no more entries for Gtk3).
    
    As a side benefit, we don't need any multiprocessing craziness in
    setupext.py anymore...

--- lib/matplotlib/rcsetup.py
+++ lib/matplotlib/rcsetup.py
@@ -941,9 +941,13 @@ def _validate_linestyle(ls):
 
 # a map from key -> value, converter
 defaultParams = {
-    'backend':           ['Agg', validate_backend],  # agg is certainly
-                                                      # present
-    'backend_fallback':  [True, validate_bool],  # agg is certainly present
+    'backend':           [["macosx",
+                           "qt5agg", "qt4agg",
+                           "gtk3agg", "gtk3cairo", "gtkagg",
+                           "tkagg",
+                           "wxagg",
+                           "agg", "cairo"], validate_backend],
+    'backend_fallback':  [True, validate_bool],
     'backend.qt4':       ['PyQt4', validate_qt4],
     'backend.qt5':       ['PyQt5', validate_qt5],
     'webagg.port':       [8988, validate_int],
