c8b3a813c3f7394c88e2b07d45c32e384f724721
Antony Lee <anntzer.lee@gmail.com>
2017-10-30 23:08:17 -0700

    Implement Qt4 backend by fully reexporting Qt5 backend.

--- lib/matplotlib/backends/backend_qt4.py
+++ lib/matplotlib/backends/backend_qt4.py
@@ -2,20 +2,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import six
-from six import unichr
-import os
-import re
-import signal
-import sys
-
-from matplotlib._pylab_helpers import Gcf
-from matplotlib.backend_bases import (
-    FigureCanvasBase, FigureManagerBase, NavigationToolbar2, TimerBase,
-    cursors)
-from matplotlib.figure import Figure
-from matplotlib.widgets import SubplotTool
-
-from .qt_compat import QtCore, QtWidgets, _getSaveFileName, __version__
 
 from .backend_qt5 import (
     backend_version, SPECIAL_KEYS, SUPER, ALT, CTRL, SHIFT, MODIFIER_KEYS,
@@ -23,24 +9,7 @@ from .backend_qt5 import (
     NavigationToolbar2QT, SubplotToolQt, error_msg_qt, exception_handler)
 from .backend_qt5 import FigureCanvasQT as FigureCanvasQT5
 
-DEBUG = False
-
-
-class FigureCanvasQT(FigureCanvasQT5):
-
-    def wheelEvent(self, event):
-        x = event.x()
-        # flipy so y=0 is bottom of canvas
-        y = self.figure.bbox.height - event.y()
-        # from QWheelEvent::delta doc
-        steps = event.delta()/120
-        if (event.orientation() == QtCore.Qt.Vertical):
-            FigureCanvasBase.scroll_event(self, x, y, steps)
-            if DEBUG:
-                print('scroll event: delta = %i, '
-                      'steps = %i ' % (event.delta(), steps))
-
 
 @_BackendQT5.export
 class _BackendQT4(_BackendQT5):
-    FigureCanvas = FigureCanvasQT
+    pass
--- lib/matplotlib/backends/backend_qt4agg.py
+++ lib/matplotlib/backends/backend_qt4agg.py
@@ -6,25 +6,10 @@ from __future__ import (absolute_import, division, print_function,
 
 import six
 
-from .backend_agg import FigureCanvasAgg
-from .backend_qt4 import (
-    QtCore, _BackendQT4, FigureCanvasQT, FigureManagerQT, NavigationToolbar2QT)
-from .backend_qt5agg import FigureCanvasQTAggBase
+from .backend_qt5agg import (
+    _BackendQT5Agg, FigureCanvasQTAgg, FigureManagerQT, NavigationToolbar2QT)
 
 
-class FigureCanvasQTAgg(FigureCanvasQTAggBase, FigureCanvasQT):
-    """
-    The canvas the figure renders into.  Calls the draw and print fig
-    methods, creates the renderers, etc...
-
-    Attributes
-    ----------
-    figure : `matplotlib.figure.Figure`
-        A high-level Figure instance
-
-    """
-
-
-@_BackendQT4.export
-class _BackendQT4Agg(_BackendQT4):
-    FigureCanvas = FigureCanvasQTAgg
+@_BackendQT5Agg.export
+class _BackendQT4Agg(_BackendQT5Agg):
+    pass
