04edaa3bb1aae9e16f5f1eab7dbc7d8d46960e76
Antony Lee <anntzer.lee@gmail.com>
2017-11-20 20:50:19 -0500

    MNT: re-work how units are handled in axes._base
    Reattach the unitful data to Line2D.

--- lib/matplotlib/cbook/__init__.py
+++ lib/matplotlib/cbook/__init__.py
@@ -2312,8 +2312,8 @@ def index_of(y):
     try:
         return y.index.values, y.values
     except AttributeError:
-        y = _check_1d(y)
-        return np.arange(y.shape[0], dtype=float), y
+        # Ensure that scalar y gives x == [0].
+        return np.arange((np.shape(y) or (1,))[0], dtype=float), y
 
 
 def safe_first_element(obj):
