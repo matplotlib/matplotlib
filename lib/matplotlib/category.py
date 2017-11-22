2bd832d261021cdf7f4773bd366a179766faf487
Thomas A Caswell <tcaswell@gmail.com>
2017-11-21 22:21:09 -0500

    API: reduce scope of StringCategorical & simplify internal structure
     - Expect that `axis.units` has a `_mapping` attribute.
     - Allow only strings.  This prevents issues with mixed input where
       plotting different sub-sets of the data will or will not trigger
       the correct converter / unit behavior.
     - Do not allow missing data.  Do not special case nan or inf for the
       same reasons as above.
     - deprecate `Axis.unit_data`.  Axis already has a `units` attribute
       which hold a UnitData instance.

--- lib/matplotlib/category.py
+++ lib/matplotlib/category.py
@@ -18,42 +18,20 @@ import itertools
 
 import numpy as np
 
-def to_str(value):
-    if LooseVersion(np.__version__) < LooseVersion('1.7.0'):
-        if (isinstance(value, (six.text_type, np.unicode))):
-            value = value.encode('utf-8', 'ignore').decode('utf-8')
-    if isinstance(value, (bytes, np.bytes_, six.binary_type)):
-        value = value.decode(encoding='utf-8')
-    elif isinstance(value, (bytes, np.bytes_, six.binary_type)):
-        return value.decode(encoding='utf-8')
-    elif not isinstance(value, (str, np.str_, six.text_type)):
-        value = str(value)
-    return value
-
 
 class StrCategoryConverter(units.ConversionInterface):
     @staticmethod
     def convert(value, unit, axis):
-        """Uses axis.unit_data map to encode
-        data as floats
-        """
-        if isinstance(value, six.string_types):
-            return axis.unit_data._mapping[value]
+        """Use axis.units mapping tncode data as floats."""
 
-        # dtype=object preserves 42, '42' distinction on scatter
-        values = np.atleast_1d(np.array(value, dtype=object))
-        if units.ConversionInterface.is_numlike(value):
-            return np.array([axis.unit_data._mapping.get(v, v)
-                             for v in values])
-
-        if hasattr(axis.unit_data, 'update'):
-            axis.unit_data.update(values)
-
-        str2idx = np.vectorize(axis.unit_data._mapping.__getitem__,
-                               otypes=[float])
-
-        mapped_value = str2idx(values)
-        return mapped_value
+        # We also need to pass numbers through.
+        if np.issubdtype(np.asarray(value).dtype.type, np.number):
+            return value
+        else:
+            axis.units.update(value)
+            str2idx = np.vectorize(axis.units._mapping.__getitem__,
+                                   otypes=[float])
+            return str2idx(value)
 
     @staticmethod
     def axisinfo(unit, axis):
@@ -63,13 +41,8 @@ class StrCategoryConverter(units.ConversionInterface):
 
     @staticmethod
     def default_units(data, axis):
-        # the conversion call stack is:
-        # default_units->axis_info->convert
-        if axis.unit_data is None:
-            axis.unit_data = UnitData(data)
-        else:
-            axis.unit_data.update(data)
-        return None
+        return UnitData()
+
 
 
 class StrCategoryLocator(ticker.FixedLocator):
@@ -85,45 +58,45 @@ class StrCategoryFormatter(ticker.FixedFormatter):
 
 
 class UnitData(object):
-    def __init__(self, data):
-        """Create mapping between unique categorical values
-        and numerical identifier
+    def __init__(self, data=None):
+        """Create mapping between unique categorical values and numerical id.
 
         Parameters
         ----------
-        data: iterable
-              sequence of values
-        """
-        # seq, loc need to be pass by reference or there needs to be
-        # a callback from Locator/Formatter on update
-        self._seq, self._locs = [], []
-        self._mapping = OrderedDict()
-        self._counter = itertools.count()
-        self.update(data)
+        data : Mapping[str, int]
+            The initial categories.  May be `None`.
 
-    def _update_mapping(self, value):
-        if value in self._mapping:
-            return
-        if isinstance(value, (float, complex)) and np.isnan(value):
-            self._mapping[value] = np.nan
+        """
+        self._vals = []
+        if data is None:
+            data = ()
+        self._mapping = OrderedDict(data)
+        for k, v in self._mapping.items():
+            if not isinstance(k, six.text_type):
+                raise TypeError("{val!r} is not a string".format(val=k))
+            self._mapping[k] = int(v)
+        if self._mapping:
+            start = max(self._mapping.values()) + 1
         else:
-            self._mapping[value] = next(self._counter)
-        self._seq.append(to_str(value))
-        self._locs.append(self._mapping[value])
-        return
+            start = 0
+        self._counter = itertools.count(start=start)
 
     def update(self, data):
-        if (isinstance(data, six.string_types) or
-                not isinstance(data, Iterable)):
-            self._update_mapping(data)
-        else:
-            unsorted_unique = OrderedDict.fromkeys(data)
-            for ns in unsorted_unique:
-                self._update_mapping(ns)
+        if isinstance(data, six.string_types):
+            data = [data]
+        sorted_unique = OrderedDict.fromkeys(data)
+        for val in sorted_unique:
+            if val in self._mapping:
+                continue
+            if not isinstance(val, six.text_type):
+                raise TypeError("{val!r} is not a string".format(val))
+            self._vals.append(val)
+            self._mapping[val] = next(self._counter)
+
 
 # Connects the convertor to matplotlib
 units.registry[str] = StrCategoryConverter()
-units.registry[np.str_] = StrCategoryConverter()
-units.registry[six.text_type] = StrCategoryConverter()
 units.registry[bytes] = StrCategoryConverter()
+units.registry[np.str_] = StrCategoryConverter()
 units.registry[np.bytes_] = StrCategoryConverter()
+units.registry[six.text_type] = StrCategoryConverter()
