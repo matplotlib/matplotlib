6f73c245b125856cad0a72f885b6c14891bb3eb9
Antony Lee <anntzer.lee@gmail.com>
2017-11-22 23:05:47 -0500

    TST: add additional tests to categorical classes

--- lib/matplotlib/tests/test_category.py
+++ lib/matplotlib/tests/test_category.py
@@ -216,3 +216,54 @@ class TestUpdatePlot(object):
         unitmap = cat.UnitData(list(zip(labels, ticks)))
 
         axis_test(ax.xaxis, ticks, labels, unitmap)
+
+
+@pytest.fixture
+def ax():
+    return plt.figure().subplots()
+
+
+@pytest.mark.parametrize(
+    "data, expected_indices, expected_labels",
+    [(["Здравствуйте мир"], [0], ["Здравствуйте мир"]),
+     (["a", "b", "b", "a", "c", "c"], [0, 1, 1, 0, 2, 2], ["a", "b", "c"]),
+     (["foo", "bar"], range(2), ["foo", "bar"]),
+     (np.array(["1", "11", "3"]), range(3), ["1", "11", "3"])])
+def test_simple(ax, data, expected_indices, expected_labels):
+    l, = ax.plot(data)
+    assert_array_equal(l.get_ydata(orig=False), expected_indices)
+    assert isinstance(ax.yaxis.major.locator, cat.StrCategoryLocator)
+    assert isinstance(ax.yaxis.major.formatter, cat.StrCategoryFormatter)
+    ax.figure.canvas.draw()
+    labels = [label.get_text() for label in ax.yaxis.get_majorticklabels()]
+    assert labels == expected_labels
+
+
+def test_default_units(ax):
+    ax.plot(["a"])
+    du = ax.yaxis.converter.default_units(["a"], ax.yaxis)
+    assert isinstance(du, cat.UnitData)
+
+
+def test_update(ax):
+    l1, = ax.plot(["a", "d"])
+    l2, = ax.plot(["b", "d", "e"])
+    assert_array_equal(l1.get_ydata(orig=False), [0, 1])
+    assert_array_equal(l2.get_ydata(orig=False), [2, 1, 3])
+    assert ax.yaxis.units._vals == ["a", "d", "b", "e"]
+    assert ax.yaxis.units._mapping == {"a": 0, "d": 1, "b": 2, "e": 3}
+
+
+@pytest.mark.parametrize("plotter", [Axes.plot, Axes.scatter, Axes.bar])
+def test_StrCategoryLocator(ax, plotter):
+    ax.plot(["a", "b", "c"])
+    assert_array_equal(ax.yaxis.major.locator(), range(3))
+
+
+@pytest.mark.parametrize("plotter", [Axes.plot, Axes.scatter, Axes.bar])
+def test_StrCategoryFormatter(ax, plotter):
+    plotter(ax, range(2), ["hello", "мир"])
+    assert ax.yaxis.major.formatter(0, 0) == "hello"
+    assert ax.yaxis.major.formatter(1, 1) == "мир"
+    assert ax.yaxis.major.formatter(2, 2) == ""
+    assert ax.yaxis.major.formatter(0, None) == ""
