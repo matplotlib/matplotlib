587b553e23c66ccec93ccf357b5be5e924045fd1
Antony Lee <anntzer.lee@gmail.com>
2017-12-28 20:27:40 -0800

    Move axisartist examples to their folder.

--- examples/axes_grid1/simple_axisline4.py
+++ examples/axes_grid1/simple_axisline4.py
@@ -6,7 +6,6 @@ Simple Axisline4
 """
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import host_subplot
-import mpl_toolkits.axisartist as AA
 import numpy as np
 
 ax = host_subplot(111)
