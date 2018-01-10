24dba88a6220373a523a48d4186e4ecf00e00ed1
Antony Lee <anntzer.lee@gmail.com>
2018-01-10 13:05:32 -0800

    Don't use gdb on Travis (as it breaks the build).

--- ci/travis/test_script.sh
+++ ci/travis/test_script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-set -ev
+set -ex
 
 # This script is meant to be called by the "script" step defined in
 # .travis.yml. See http://docs.travis-ci.com/ for more details.
@@ -15,9 +15,4 @@ if [[ $DELETE_FONT_CACHE == 1 ]]; then
   rm -rf ~/.cache/matplotlib
 fi
 
-echo The following args are passed to pytest $PYTEST_ARGS $RUN_PEP8
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  pytest $PYTEST_ARGS $RUN_PEP8
-else
-  gdb -return-child-result -batch -ex r -ex bt --args python $PYTHON_ARGS -m pytest $PYTEST_ARGS $RUN_PEP8
-fi
+pytest $PYTEST_ARGS $RUN_PEP8
