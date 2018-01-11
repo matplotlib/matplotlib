7216b8c193db6683c944786dc1475415b9d621fb
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2018-01-11 01:55:56 -0500

    TST: Increase some tolerances for Windows.
    The problem arises in some other location that is more difficult to
    determine than the previous one.

--- lib/matplotlib/tests/test_streamplot.py
+++ lib/matplotlib/tests/test_streamplot.py
@@ -1,10 +1,15 @@
 from __future__ import absolute_import, division, print_function
 
+import sys
+
 import numpy as np
 from numpy.testing import assert_array_almost_equal
 import matplotlib.pyplot as plt
 from matplotlib.testing.decorators import image_comparison
 import matplotlib.transforms as mtransforms
+
+
+on_win = (sys.platform == 'win32')
 
 
 def velocity_field():
@@ -52,7 +57,8 @@ def test_linewidth():
                    linewidth=lw)
 
 
-@image_comparison(baseline_images=['streamplot_masks_and_nans'])
+@image_comparison(baseline_images=['streamplot_masks_and_nans'],
+                  tol=0.04 if on_win else 0)
 def test_masks_and_nans():
     X, Y, U, V = velocity_field()
     mask = np.zeros(U.shape, dtype=bool)
