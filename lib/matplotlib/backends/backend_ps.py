01d03dddef440e03a73b6f80cf6c42e6b9b575d6
Jody Klymak <jklymak@gmail.com>
2018-01-22 16:07:06 -0800

    FIX: don't autorotate xpdf images

--- lib/matplotlib/backends/backend_ps.py
+++ lib/matplotlib/backends/backend_ps.py
@@ -1544,7 +1544,7 @@ def xpdf_distill(tmpfile, eps=False, ptype='letter', bbox=None, rotated=False):
     """
     pdffile = tmpfile + '.pdf'
     psfile = tmpfile + '.ps'
-
+    
     if eps:
         paper_option = "-dEPSCrop"
     else:
@@ -1557,12 +1557,14 @@ def xpdf_distill(tmpfile, eps=False, ptype='letter', bbox=None, rotated=False):
         command = [str("ps2pdf"), "-dAutoFilterColorImages#false",
                    "-dAutoFilterGrayImages#false",
                    "-sGrayImageFilter#FlateEncode",
+                   "-dAutoRotatePages=false",
                    "-sColorImageFilter#FlateEncode", paper_option, tmpfile,
                    pdffile]
     else:
         command = [str("ps2pdf"), "-dAutoFilterColorImages=false",
                    "-dAutoFilterGrayImages=false",
                    "-sGrayImageFilter=FlateEncode",
+                   "-dAutoRotatePages=false",
                    "-sColorImageFilter=FlateEncode", paper_option, tmpfile,
                    pdffile]
     _log.debug(command)
