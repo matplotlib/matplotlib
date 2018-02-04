cd456b5c1f2f84dd36cb83db56ff7b1173ec7d96
Jody Klymak <jklymak@gmail.com>
2018-02-04 14:37:30 -0800

    FIX: Qt4/5 deprecation, part deux

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -845,7 +845,8 @@ _deprecated_map = {}
 
 _deprecated_ignore_map = {'nbagg.transparent': 'figure.facecolor'}
 
-_obsolete_set = {'plugins.directory', 'text.dvipnghack'}
+_obsolete_set = {'plugins.directory', 'text.dvipnghack', 'backend.qt4',
+        'backend.qt5'}
 
 # The following may use a value of None to suppress the warning.
 _deprecated_set = {'axes.hold'}  # do NOT include in _all_deprecated
