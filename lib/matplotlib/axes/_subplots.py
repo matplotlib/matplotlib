e767003d9662865cb1aa2a4efed1f84f709f2032
stonebig <stonebig34@gmail.com>
2018-02-27 23:03:15 +0100

    make seaborn great again on Matplotlib-2.2
    fix for https://github.com/matplotlib/matplotlib/issues/10585

--- lib/matplotlib/axes/_subplots.py
+++ lib/matplotlib/axes/_subplots.py
@@ -172,8 +172,8 @@ class SubplotBase(object):
         Make a twinx axes of self. This is used for twinx and twiny.
         """
         from matplotlib.projections import process_projection_requirements
-        if 'sharex' in kwargs and 'sharey' in kwargs:
-            raise ValueError("Twinned Axes may share only one axis.")
+        if kwargs["sharex"] is not self and kwargs["sharey"] is not self:
+           raise ValueError("Twinned Axes may share only one axis.")
         kl = (self.get_subplotspec(),) + kl
         projection_class, kwargs, key = process_projection_requirements(
             self.figure, *kl, **kwargs)
