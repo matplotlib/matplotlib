a16182fbfcc0fb90bcaa07188d04d09c9d82e3ca
Osarumwense <osaru97@yahoo.com>
2018-03-06 16:15:15 -0500

    adding test case for creating a 3d graph from 2d [5724]

--- lib/mpl_toolkits/tests/test_mplot3d.py
+++ lib/mpl_toolkits/tests/test_mplot3d.py
@@ -174,6 +174,14 @@ def test_scatter3d_color():
     ax.scatter(np.arange(10, 20), np.arange(10, 20), np.arange(10, 20),
                color='b', marker='s')
 
+@image_comparison(baseline_images=['plot_3d_from_2d'], remove_text=True,
+                  extensions=['png'])
+def test_plot_3d_from_2d():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    xs = np.arange(0, 5)
+    ys = np.arange(5, 10)
+    ax.plot(xs, ys, zs=0, zdir='x')
 
 @image_comparison(baseline_images=['surface3d'], remove_text=True)
 def test_surface3d():
