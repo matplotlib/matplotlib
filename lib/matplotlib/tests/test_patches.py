0f1e0ea9d5951ad3fce0bdf2b09cb1f4397bf78e
KonradAdamczyk <konradadamczyk01@o2.pl>
2018-03-14 23:25:19 +0100

    test to ensure #10575 is fixed
    test set_alpha after update_from changes only alpha in color

--- lib/matplotlib/tests/test_patches.py
+++ lib/matplotlib/tests/test_patches.py
@@ -410,3 +410,23 @@ def test_contains_points():
     expected = path.contains_points(points, transform, radius)
     result = ell.contains_points(points)
     assert np.all(result == expected)
+
+    
+def test_when_update_from_and_set_alpha_then_only_alpha_changes():
+    # given
+    source_facecolor = (0.234, 0.123, 0.135, 0.322)
+    source_egdecolor = (0.728, 0.682, 0.945, 0.268)
+    source = mpatches.Rectangle((0, 0), 1., 1., facecolor=source_facecolor, edgecolor=source_egdecolor)
+    updated = mpatches.Rectangle((1., 0), 1., 1., facecolor='pink', edgecolor="green")
+    # when
+    updated.update_from(source)
+    # then
+    assert updated.get_facecolor() == source_facecolor
+    assert updated.get_edgecolor() == source_egdecolor
+    # when
+    updated.set_alpha(0.777)
+    # then
+    expected_facecolor = source_facecolor[0:3] + (0.777,)
+    expected_edgecolor = source_egdecolor[0:3] + (0.777,)
+    assert updated.get_facecolor() == expected_facecolor
+    assert updated.get_edgecolor() == expected_edgecolor
