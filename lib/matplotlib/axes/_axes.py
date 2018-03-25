7f157b58f0e2b810bb10a70b481157232b1b528e
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2018-03-25 00:35:27 -0400

    Backport PR #10877: Fix invalid escape sequence in docstring.

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -1844,7 +1844,7 @@ class Axes(_AxesBase):
     @_preprocess_data(replace_names=["x", "y"], label_namer="y")
     def xcorr(self, x, y, normed=True, detrend=mlab.detrend_none,
               usevlines=True, maxlags=10, **kwargs):
-        """
+        r"""
         Plot the cross correlation between *x* and *y*.
 
         The correlation with lag k is defined as sum_n x[n+k] * conj(y[n]).
