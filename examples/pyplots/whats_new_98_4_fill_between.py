8b0b534906d77089a77bb52a66e27f0cc4d26ee1
Matthew Bell <bellmj@g.cofc.edu>
2018-04-05 21:15:24 -0400

    updated line lenght and added a single space after the comma in the fill_between method

--- examples/pyplots/whats_new_98_4_fill_between.py
+++ examples/pyplots/whats_new_98_4_fill_between.py
@@ -2,7 +2,8 @@
 =============================
 Whats New 0.98.4 Fill Between
 =============================
- generates an example of using the fill_between() method with opposing two quadratics
+generates an example of using the fill_between() method with opposing
+two quadratics
 """
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,8 +14,8 @@ y2 = 5*x*x + x
 
 fig, ax = plt.subplots()
 ax.plot(x, y1, x, y2, color='black')
-ax.fill_between(x, y1, y2, where=y2>y1, facecolor='yellow',alpha=0.5)
-ax.fill_between(x, y1, y2, where=y2<=y1, facecolor='red',alpha=0.5)
+ax.fill_between(x, y1, y2, where=y2>y1, facecolor='yellow', alpha=0.5)
+ax.fill_between(x, y1, y2, where=y2<=y1, facecolor='red', alpha=0.5)
 ax.set_title('Highlight Between and Intercept')
 
 plt.show()
