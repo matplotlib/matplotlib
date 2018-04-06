db32fecbceca2a90624579b3844952f59105d001
Matthew Bell <bellmj@g.cofc.edu>
2018-04-06 14:55:43 -0400

    added white space to pass PEP8-check

--- examples/pyplots/fill_between.py
+++ examples/pyplots/fill_between.py
@@ -8,13 +8,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.arange(-5, 5, 0.01)
-y1 = -5*x*x + x + 10
-y2 = 5*x*x + x
+y1 = -5 * x * x + x + 10
+y2 = 5 * x * x + x
 
 fig, ax = plt.subplots()
-ax.plot(x, y1, x, y2, color='black')
-ax.fill_between(x, y1, y2, where=y2>y1, facecolor='yellow', alpha=0.5)
-ax.fill_between(x, y1, y2, where=y2<=y1, facecolor='red', alpha=0.5)
+ax.plot(x, y1, x, y2, color = 'black')
+ax.fill_between(x, y1, y2, where = y2 > y1, facecolor = 'yellow', alpha = 0.5)
+ax.fill_between(x, y1, y2, where = y2 < = y1, facecolor = 'red', alpha = 0.5)
 ax.set_title('Fill Between')
 
 plt.show()
