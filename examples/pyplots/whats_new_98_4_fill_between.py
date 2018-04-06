f5459d17f4c819e7a2af179d3da0b4327f1e7480
Matthew Bell <bellmj@g.cofc.edu>
2018-04-05 20:41:57 -0400

    updated the pyplot fill_between example to elucidate the premise;making the chart more readable and removing unnecessary confusing code

--- examples/pyplots/whats_new_98_4_fill_between.py
+++ examples/pyplots/whats_new_98_4_fill_between.py
@@ -2,19 +2,19 @@
 =============================
 Whats New 0.98.4 Fill Between
 =============================
-
+ generates an example of using the fill_between() method with opposing two quadratics
 """
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arange(0.0, 2, 0.01)
-y1 = np.sin(2*np.pi*x)
-y2 = 1.2*np.sin(4*np.pi*x)
+x = np.arange(-5, 5, 0.01)
+y1 = -5*x*x + x + 10
+y2 = 5*x*x + x
 
 fig, ax = plt.subplots()
 ax.plot(x, y1, x, y2, color='black')
-ax.fill_between(x, y1, y2, where=y2>y1, facecolor='green')
-ax.fill_between(x, y1, y2, where=y2<=y1, facecolor='red')
-ax.set_title('fill between where')
+ax.fill_between(x, y1, y2, where=y2>y1, facecolor='yellow',alpha=0.5)
+ax.fill_between(x, y1, y2, where=y2<=y1, facecolor='red',alpha=0.5)
+ax.set_title('Highlight Between and Intercept')
 
 plt.show()
