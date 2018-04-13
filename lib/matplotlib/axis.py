e16b493352bd3d8ec11dfb4ca6d8d3dc0cd88890
Jason Neal <jason.neal@astro.up.pt>
2018-04-13 10:21:00 +0100

    Doc: unreference specific axis in get_ticklabels
    get_ticklabels is not just for x ticks.

--- lib/matplotlib/axis.py
+++ lib/matplotlib/axis.py
@@ -1245,7 +1245,7 @@ class Axis(artist.Artist):
 
     def get_ticklabels(self, minor=False, which=None):
         """
-        Get the x tick labels as a list of :class:`~matplotlib.text.Text`
+        Get the tick labels as a list of :class:`~matplotlib.text.Text`
         instances.
 
         Parameters
