2b40cf3390de9dca07da1f38f1e636c8516d421c
Thomas A Caswell <tcaswell@gmail.com>
2018-04-18 17:54:11 -0400

    Backport PR #11063: Enable sphinx.ext.viewcode

--- doc/conf.py
+++ doc/conf.py
@@ -34,6 +34,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.inheritance_diagram',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
     'IPython.sphinxext.ipython_console_highlighting',
     'IPython.sphinxext.ipython_directive',
     'numpydoc',  # Needs to be loaded *after* autodoc.
