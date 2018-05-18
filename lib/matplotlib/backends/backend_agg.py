076653f784691173e739a4c30d3658ba4781aa3e
Zac-HD <zac.hatfield.dodds@gmail.com>
2018-05-19 00:32:29 +1000

    Delete commented-out code

--- lib/matplotlib/backends/backend_agg.py
+++ lib/matplotlib/backends/backend_agg.py
@@ -418,15 +418,11 @@ class FigureCanvasAgg(FigureCanvasBase):
 
         toolbar = self.toolbar
         try:
-            # if toolbar:
-            #     toolbar.set_cursor(cursors.WAIT)
             self.figure.draw(self.renderer)
             # A GUI class may be need to update a window using this draw, so
             # don't forget to call the superclass.
             super().draw()
         finally:
-            # if toolbar:
-            #     toolbar.set_cursor(toolbar._lastCursor)
             RendererAgg.lock.release()
 
     def get_renderer(self, cleared=False):
