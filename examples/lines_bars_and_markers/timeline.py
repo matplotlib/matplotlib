4e60dfbcabdea700fc1f4c4c6884d88a53c0d8d5
Chris Holdgraf <choldgraf@berkeley.edu>
2018-06-09 13:39:44 -0700

    removing pandas dependency

--- examples/lines_bars_and_markers/timeline.py
+++ examples/lines_bars_and_markers/timeline.py
@@ -12,7 +12,8 @@ of Matplotlib. First, we'll pull the data from GitHub.
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+import matplotlib.dates as mdates
+from datetime import datetime
 import urllib.request
 import json
 
@@ -20,14 +21,14 @@ import json
 url = 'https://api.github.com/repos/matplotlib/matplotlib/releases'
 data = json.loads(urllib.request.urlopen(url).read().decode())
 
-releases = []
+names = []
+dates = []
 for irelease in data:
-    releases.append((irelease['tag_name'], irelease['published_at']))
-releases = pd.DataFrame(releases, columns=['name', 'date'])
-releases['date'] = pd.to_datetime(releases['date'])
-# Remove release candidates and betas
-releases = releases.loc[['rc' not in nm for nm in releases['name']]]
-releases = releases.loc[['b' not in nm for nm in releases['name']]]
+    if 'rc' not in irelease['tag_name'] and 'b' not in irelease['tag_name']:
+        names.append(irelease['tag_name'])
+        # Convert date strings (e.g. 2014-10-18T18:56:23Z) to datetime
+        dates.append(datetime.strptime(irelease['published_at'],
+                     "%Y-%m-%dT%H:%M:%SZ"))
 
 ##############################################################################
 # Next, we'll iterate through each date and plot it on a horizontal line.
@@ -39,29 +40,29 @@ levels = np.array([-5, 5, -3, 3, -1, 1])
 fig, ax = plt.subplots(figsize=(20, 5))
 
 # Create the base line
-start = releases['date'].min()
-stop = releases['date'].max()
+start = min(dates)
+stop = max(dates)
 ax.plot((start, stop), (0, 0), 'k', alpha=.5)
 
 # Iterate through releases annotating each one
-for ix, (iname, idate) in releases.iterrows():
-    level = levels[ix % 6]
+for ii, (iname, idate) in enumerate(zip(names, dates)):
+    level = levels[ii % 6]
     vert = 'top' if level < 0 else 'bottom'
 
     ax.scatter(idate, 0, s=100, facecolor='w', edgecolor='k', zorder=9999)
     # Plot a line up to the text
-    ax.plot((idate, idate), (0, level),
-            c='r', alpha=.7)
+    ax.plot((idate, idate), (0, level), c='r', alpha=.7)
     # Give the text a faint background and align it properly
     ax.text(idate, level, iname,
             horizontalalignment='right', verticalalignment=vert, fontsize=14,
             backgroundcolor=(1., 1., 1., .3))
 ax.set(title="Matplotlib release dates")
 # Set the xticks formatting
-xticks = pd.date_range(start, stop, freq='3M')
-ax.set_xticks(xticks)
-ax.set_xticklabels(xticks.strftime("%b %Y"),
-                   rotation=45, horizontalalignment='right', fontsize=14)
+# format xaxis with 3 month intervals
+ax.get_xaxis().set_major_locator(mdates.MonthLocator(interval=3))
+ax.get_xaxis().set_major_formatter(mdates.DateFormatter("%b %Y"))
+fig.autofmt_xdate()
+
 # Remove components for a cleaner look
 plt.setp((ax.get_yticklabels() + ax.get_yticklines() +
           list(ax.spines.values())), visible=False)
