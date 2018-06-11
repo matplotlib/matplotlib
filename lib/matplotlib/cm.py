963d7f6986e82641faa3e7309fa4e6b368db756e
Eric Firing <efiring@hawaii.edu>
2018-06-10 19:55:10 -1000

    Use Sphinx link in cm.py docstring.

--- lib/matplotlib/cm.py
+++ lib/matplotlib/cm.py
@@ -1,5 +1,8 @@
 """
-This is a compatibility shim for the renamed colormaps module.
+This provides the contents of :mod:`~matplotlib.colormaps` under its original
+name, for compatibility with existing code.  For new code, use::
+
+    import matplotlib.colormaps as cm
 
 """
 from matplotlib.colormaps import *
