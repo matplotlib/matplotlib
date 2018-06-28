d15c344aae80504cbe4d32b9becb4b237160de45
Taras Kuzyo <kuzyo.taras@gmail.com>
2018-06-28 19:32:53 +0300

    Ensuring both x and y attrs of LocationEvent are int

--- lib/matplotlib/backend_bases.py
+++ lib/matplotlib/backend_bases.py
@@ -1365,8 +1365,8 @@ class LocationEvent(Event):
         *x*, *y* in figure coords, 0,0 = bottom, left
         """
         Event.__init__(self, name, canvas, guiEvent=guiEvent)
-        self.x = x          # x position - pixels from left of canvas
-        self.y = y          # y position - pixels from right of canvas
+        self.x = int(x)          # x position - pixels from left of canvas
+        self.y = int(y)          # y position - pixels from right of canvas
         self.inaxes = None  # the Axes instance if mouse us over axes
         self.xdata = None   # x coord of mouse in data coords
         self.ydata = None   # y coord of mouse in data coords
