7a5af736bd0502e5e1ca816a94c79f8497816f8d
Thomas A Caswell <tcaswell@gmail.com>
2018-06-28 16:26:08 -0400

    BLD: add 3.7 to classifiers

--- setup.py
+++ setup.py
@@ -114,6 +114,7 @@ classifiers = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Topic :: Scientific/Engineering :: Visualization',
     ]
 
