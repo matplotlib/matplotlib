aedc80e279bd10d0918ac4fa62488ea91bce1f67
Taras Kuzyo <kuzyo.taras@gmail.com>
2018-07-04 13:38:31 +0300

    added a test case and baseline images for xkcd plot with markers

--- lib/matplotlib/tests/test_path.py
+++ lib/matplotlib/tests/test_path.py
@@ -122,6 +122,22 @@ def test_xkcd():
         ax.plot(x, y)
 
 
+@image_comparison(baseline_images=['xkcd_marker'], remove_text=True)
+def test_xkcd_marker():
+    np.random.seed(0)
+
+    x = np.linspace(0, 5, 8)
+    y1 = x
+    y2 = 5 - x
+    y3 = 2.5 * np.ones(8)
+
+    with plt.xkcd():
+        fig, ax = plt.subplots()
+        ax.plot(x, y1, '+', ms=10)
+        ax.plot(x, y2, 'o', ms=10)
+        ax.plot(x, y3, '^', ms=10)
+
+
 @image_comparison(baseline_images=['marker_paths'], extensions=['pdf'],
                   remove_text=True)
 def test_marker_paths_pdf():
