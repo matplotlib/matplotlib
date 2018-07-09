ec77ca215b2f306a9fa38e58b57587d7803015cf
Antony Lee <anntzer.lee@gmail.com>
2018-07-09 10:05:11 +0200

    Lazy-init the OSX event loop.

--- src/_macosx.m
+++ src/_macosx.m
@@ -267,6 +267,39 @@ static int wait_for_stdin(void)
 
 /* ---------------------------- Python classes ---------------------------- */
 
+static bool backend_inited = false;
+
+static void lazy_init(void) {
+    if (backend_inited) {
+        return;
+    }
+    backend_inited = true;
+
+    NSApp = [NSApplication sharedApplication];
+
+    PyOS_InputHook = wait_for_stdin;
+
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+    WindowServerConnectionManager* connectionManager = [WindowServerConnectionManager sharedManager];
+    NSWorkspace* workspace = [NSWorkspace sharedWorkspace];
+    NSNotificationCenter* notificationCenter = [workspace notificationCenter];
+    [notificationCenter addObserver: connectionManager
+                           selector: @selector(launch:)
+                               name: NSWorkspaceDidLaunchApplicationNotification
+                             object: nil];
+    [pool release];
+}
+
+static PyObject*
+event_loop_is_running(PyObject* self)
+{
+    if (backend_inited) {
+        Py_RETURN_TRUE;
+    } else {
+        Py_RETURN_FALSE;
+    }
+}
+
 static CGFloat _get_device_scale(CGContextRef cr)
 {
     CGSize pixelSize = CGContextConvertSizeToDeviceSpace(cr, CGSizeMake(1, 1));
@@ -281,6 +314,7 @@ typedef struct {
 static PyObject*
 FigureCanvas_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
+    lazy_init();
     FigureCanvas *self = (FigureCanvas*)type->tp_alloc(type, 0);
     if (!self) return NULL;
     self->view = [View alloc];
@@ -641,6 +675,7 @@ typedef struct {
 static PyObject*
 FigureManager_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
+    lazy_init();
     Window* window = [Window alloc];
     if (!window) return NULL;
     FigureManager *self = (FigureManager*)type->tp_alloc(type, 0);
@@ -1076,6 +1111,7 @@ typedef struct {
 static PyObject*
 NavigationToolbar2_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
+    lazy_init();
     NavigationToolbar2Handler* handler = [NavigationToolbar2Handler alloc];
     if (!handler) return NULL;
     NavigationToolbar2 *self = (NavigationToolbar2*)type->tp_alloc(type, 0);
@@ -2311,16 +2347,6 @@ static int _copy_agg_buffer(CGContextRef cr, PyObject *renderer)
 @end
 
 static PyObject*
-event_loop_is_running(PyObject* self)
-{
-    if ([NSApp isRunning]) {
-        Py_RETURN_TRUE;
-    } else {
-        Py_RETURN_FALSE;
-    }
-}
-
-static PyObject*
 show(PyObject* self)
 {
     [NSApp activateIgnoringOtherApps: YES];
@@ -2346,6 +2372,7 @@ typedef struct {
 static PyObject*
 Timer_new(PyTypeObject* type, PyObject *args, PyObject *kwds)
 {
+    lazy_init();
     Timer* self = (Timer*)type->tp_alloc(type, 0);
     if (!self) return NULL;
     self->timer = NULL;
@@ -2572,7 +2599,7 @@ static struct PyMethodDef methods[] = {
    {"event_loop_is_running",
     (PyCFunction)event_loop_is_running,
     METH_NOARGS,
-    "Return whether the NSApp main event loop is currently running."
+    "Return whether the OSX backend has set up the NSApp main event loop."
    },
    {"show",
     (PyCFunction)show,
@@ -2617,13 +2644,12 @@ PyObject* PyInit__macosx(void)
      || PyType_Ready(&TimerType) < 0)
         return NULL;
 
-    NSApp = [NSApplication sharedApplication];
-
     if (!verify_framework())
         return NULL;
 
     module = PyModule_Create(&moduledef);
-    if (module==NULL) return NULL;
+    if (!module)
+        return NULL;
 
     Py_INCREF(&FigureCanvasType);
     Py_INCREF(&FigureManagerType);
@@ -2634,16 +2660,5 @@ PyObject* PyInit__macosx(void)
     PyModule_AddObject(module, "NavigationToolbar2", (PyObject*) &NavigationToolbar2Type);
     PyModule_AddObject(module, "Timer", (PyObject*) &TimerType);
 
-    PyOS_InputHook = wait_for_stdin;
-
-    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-    WindowServerConnectionManager* connectionManager = [WindowServerConnectionManager sharedManager];
-    NSWorkspace* workspace = [NSWorkspace sharedWorkspace];
-    NSNotificationCenter* notificationCenter = [workspace notificationCenter];
-    [notificationCenter addObserver: connectionManager
-                           selector: @selector(launch:)
-                               name: NSWorkspaceDidLaunchApplicationNotification
-                             object: nil];
-    [pool release];
     return module;
 }
