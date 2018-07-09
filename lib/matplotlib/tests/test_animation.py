f8758ebb346fd653b969b2375705ada525c11c09
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2018-07-09 00:27:08 -0400

    TST: Place animation files in a temp dir.

--- lib/matplotlib/tests/test_animation.py
+++ lib/matplotlib/tests/test_animation.py
@@ -215,9 +215,10 @@ def test_movie_writer_registry():
     not animation.writers.is_available(mpl.rcParams["animation.writer"]),
     reason="animation writer not installed")
 @pytest.mark.parametrize("method_name", ["to_html5_video", "to_jshtml"])
-def test_embed_limit(method_name, caplog):
-    with mpl.rc_context({"animation.embed_limit": 1e-6}):  # ~1 byte.
-        getattr(make_animation(frames=1), method_name)()
+def test_embed_limit(method_name, caplog, tmpdir):
+    with tmpdir.as_cwd():
+        with mpl.rc_context({"animation.embed_limit": 1e-6}):  # ~1 byte.
+            getattr(make_animation(frames=1), method_name)()
     assert len(caplog.records) == 1
     record, = caplog.records
     assert (record.name == "matplotlib.animation"
