d2ec53b4ef9635056d943576239466e41b90c246
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2018-07-11 02:57:38 +0200

    Minor docstring fixes

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -273,7 +273,7 @@ class Axes(_AxesBase):
     @docstring.dedent_interpd
     def legend(self, *args, **kwargs):
         """
-        Places a legend on the axes.
+        Place a legend on the axes.
 
         Call signatures::
 
