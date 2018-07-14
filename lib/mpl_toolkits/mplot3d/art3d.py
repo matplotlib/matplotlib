Fix broadcasting

lib/mpl_toolkits/mplot3d/art3d.py
@@ -290,7 +290,7 @@ class Patch3D(Patch):
 
     def set_3d_properties(self, verts, zs=0, zdir='z'):
         zs = np.broadcast_to(zs, len(verts))
-        verts = np.hstack([verts, zs])
+        verts = np.hstack([verts, zs[:, None]])
         self._segment3d = _juggle_axes_vec(verts.T, zdir)
         self._facecolor3d = Patch.get_facecolor(self)
 

lib/mpl_toolkits/mplot3d/axes3d.py
@@ -2354,7 +2354,7 @@ class Axes3D(Axes):
 
         verts = np.vstack([list(zip(*verts)), verts_zs])
 
-        xs, ys, verts_zs = art3d.juggle_axes_vec(verts, zdir)
+        xs, ys, verts_zs = art3d._juggle_axes_vec(verts, zdir)
         self.auto_scale_xyz(xs, ys, verts_zs, had_data)
 
         return patches