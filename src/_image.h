ebc2d66e4189e555767c45341427ee2f43099137
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2018-07-16 19:40:30 -0400

    Remove unused code.

--- src/_image.h
+++ src/_image.h
@@ -21,8 +21,6 @@ void _bin_indices_middle_linear(float *arows,
                                 float dy,
                                 float y_min);
 void _bin_indices(int *irows, int nrows, const double *y, unsigned long ny, double sc, double offs);
-void _bin_indices_linear(
-    float *arows, int *irows, int nrows, double *y, unsigned long ny, double sc, double offs);
 
 template <class CoordinateArray, class ColorArray, class OutputArray>
 void pcolor(CoordinateArray &x,
