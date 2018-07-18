efafd31f252c01592c2ecbe761b5a9e5c6885a74
Antony Lee <anntzer.lee@gmail.com>
2018-07-18 13:48:53 +0200

    Avoid using private APIs in examples.
    Most fixes are trivial, except the removal of the call to
    _update_patch_transform() in widgets/menu.py: this is OK because calling
    Rectangle.draw() will itself call _update_patch_transform() via
    get_patch_transform().

--- examples/text_labels_and_annotations/demo_text_path.py
+++ examples/text_labels_and_annotations/demo_text_path.py
@@ -66,9 +66,7 @@ if 1:
 
     ax = plt.subplot(211)
 
-    from matplotlib._png import read_png
-    fn = get_sample_data("grace_hopper.png", asfileobj=False)
-    arr = read_png(fn)
+    arr = plt.imread(get_sample_data("grace_hopper.png"))
 
     text_path = TextPath((0, 0), "!?", size=150)
     p = PathClippedImagePatch(text_path, arr, ec="k",
