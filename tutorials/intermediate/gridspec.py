9b61044d38fb243462c3fcb1f89b502bd6c46d6d
Thomas A Caswell <tcaswell@gmail.com>
2018-08-04 21:30:56 -0400

    MNT: remove extraneous 10

--- tutorials/intermediate/gridspec.py
+++ tutorials/intermediate/gridspec.py
@@ -232,7 +232,7 @@ from itertools import product
 def squiggle_xy(a, b, c, d, i=np.arange(0.0, 2*np.pi, 0.05)):
     return np.sin(i*a)*np.cos(i*b), np.sin(i*c)*np.cos(i*d)
 
-10
+
 fig11 = plt.figure(figsize=(8, 8), constrained_layout=False)
 
 # gridspec inside gridspec
