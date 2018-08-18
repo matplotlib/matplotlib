846f9ab131ec11102b7b7d8c2ffbfa3901f786fd
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2018-08-18 10:36:16 -0700

    DOC refer to DOI for colormap IBM paper.

--- tutorials/colors/colormaps.py
+++ tutorials/colors/colormaps.py
@@ -417,4 +417,4 @@ for cmap_category, cmap_list in cmaps.items():
 # .. [bw] http://www.tannerhelland.com/3643/grayscale-image-algorithm-vb6/
 # .. [colorblindness] http://www.color-blindness.com/
 # .. [vischeck] http://www.vischeck.com/vischeck/
-# .. [IBM] https://archive.is/fmYgZ
+# .. [IBM] https://ieeexplore.ieee.org/document/480803/
