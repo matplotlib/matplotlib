5020747eb4d12c67ac2230aa48477ad31ac86af6
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2018-08-18 12:15:27 -0700

    Merge pull request #11799 from saketkc/patch-3
    DOC: Update link to IBM's colormap selection paper

--- tutorials/colors/colormaps.py
+++ tutorials/colors/colormaps.py
@@ -417,4 +417,4 @@ for cmap_category, cmap_list in cmaps.items():
 # .. [bw] http://www.tannerhelland.com/3643/grayscale-image-algorithm-vb6/
 # .. [colorblindness] http://www.color-blindness.com/
 # .. [vischeck] http://www.vischeck.com/vischeck/
-# .. [IBM] http://www.research.ibm.com/people/l/lloydt/color/color.HTM
+# .. [IBM] https://ieeexplore.ieee.org/document/480803/
