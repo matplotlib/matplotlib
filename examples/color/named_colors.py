72716abc84b391753348fa63c78f2243d4bc8a29
Joses W. Ho <joseshowh@gmail.com>
2018-08-27 00:55:39 +0800

    align swatches; standardize swatch size and font size

--- examples/color/named_colors.py
+++ examples/color/named_colors.py
@@ -16,7 +16,7 @@ from matplotlib import colors as mcolors
 
 # First, we define a custom plotting function that accepts a dictionary
 # from one of matplotlib's named color palettes.
-def plot_colors(colors, title, fig_size, sort_colors=True, ncols=4):
+def plot_colors(colors, title, swatch_width=3, swatch_height=1, sort_colors=True, ncols=4):
 
     import matplotlib.pyplot as plt
     from matplotlib.colors import rgb_to_hsv, to_rgba
@@ -30,8 +30,12 @@ def plot_colors(colors, title, fig_size, sort_colors=True, ncols=4):
 
     n = len(names)
     nrows = n // ncols
+    if n % ncols > 0:
+        nrows += 1
 
-    fig, ax = plt.subplots(figsize=fig_size)
+    x_inches = swatch_width * ncols
+    y_inches = swatch_height * nrows
+    fig, ax = plt.subplots(figsize=(x_inches, y_inches))
 
     # Get height and width
     X, Y = fig.get_dpi() * fig.get_size_inches()
@@ -43,16 +47,17 @@ def plot_colors(colors, title, fig_size, sort_colors=True, ncols=4):
         col = i // nrows
         y = Y - (row * h) - h
 
-        xi_line = w * (col + 0.05)
-        xf_line = w * (col + 0.25)
-        xi_text = w * (col + 0.3)
+        swatch_start_x = w * (col + 0.05)
+        swatch_end_x = w * (col + 0.3)
+        text_pos_x = w * (col + 0.35)
 
-        ax.text(xi_text, y, name, fontsize=(h * 0.5),
+        ax.text(text_pos_x, y, name, fontsize=15,
                 horizontalalignment='left',
                 verticalalignment='center')
 
-        ax.hlines(y + h * 0.1, xi_line, xf_line,
-                  color=colors[name], linewidth=(h * 0.6))
+        ax.hlines(y,
+                  swatch_start_x, swatch_end_x,
+                  color=colors[name], linewidth=20)
 
     ax.set_xlim(0, X)
     ax.set_ylim(0, Y)
@@ -62,23 +67,19 @@ def plot_colors(colors, title, fig_size, sort_colors=True, ncols=4):
                         top=1, bottom=0,
                         hspace=0, wspace=0)
 
-    fig.suptitle(title, y=1.07, fontsize=20)
+    plt.title(title, fontsize=25)
 
     plt.tight_layout()
     plt.show()
 
 
-plot_colors(mcolors.BASE_COLORS, "Base Colors",
-            fig_size=(9, 1.5))
+plot_colors(mcolors.BASE_COLORS, "Base Colors")
 
-plot_colors(mcolors.CSS4_COLORS, "CSS Colors",
-            fig_size=(9, 10))
+plot_colors(mcolors.CSS4_COLORS, "CSS Colors")
 
-plot_colors(mcolors.SOLARIZED_COLORS, "Solarized Palette",
-            fig_size=(9, 4), ncols=2, sort_colors=False)
+plot_colors(mcolors.SOLARIZED_COLORS, "Solarized Palette", sort_colors=False)
 
-plot_colors(mcolors.TABLEAU_COLORS, "Tableau Palette",
-            fig_size=(9, 3.2), ncols=2)
+plot_colors(mcolors.TABLEAU_COLORS, "Tableau Palette")
 
 #############################################################################
 #
