b31c7366f41888525b4b22f1baf19c82269f4913
DietmarSchwertberger <github@schwertberger.de>
2018-08-26 23:11:51 +0200

    fix bug in previous commit

--- lib/matplotlib/backends/backend_wx.py
+++ lib/matplotlib/backends/backend_wx.py
@@ -861,6 +861,7 @@ class _FigureCanvasWxBase(FigureCanvasBase, wx.Panel):
         left   = min(point[0] for point in points)
         bottom = max(point[1] for point in points)
         right  = max(point[0] for point in points)
+
         if previous_rubberband:
             # extend by one pixel to avoid residuals on Mac OS
             if left > 0:
@@ -869,8 +870,8 @@ class _FigureCanvasWxBase(FigureCanvasBase, wx.Panel):
                 top -= 1
             if bottom < self.figure.bbox.height - 1:
                 bottom += 1
-            if top < self.figure.bbox.width - 1:
-                top += 1
+            if right < self.figure.bbox.width - 1:
+                right += 1
 
         rect = wx.Rect(topLeft=wx.Point(left,top), bottomRight=wx.Point(right, bottom))
         self.Refresh(False, rect)
