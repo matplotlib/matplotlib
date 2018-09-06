5c788936a972fbba385a29d8dccede78da191ad4
Jody Klymak <jklymak@gmail.com>
2018-09-06 14:31:32 -0700

    Backport PR #12047: Remove asserting about current backend at the end of mpl_test_settings.

--- lib/matplotlib/testing/conftest.py
+++ lib/matplotlib/testing/conftest.py
@@ -53,8 +53,6 @@ def mpl_test_settings(request):
             if backend is not None:
                 plt.switch_backend(prev_backend)
 
-    assert matplotlib.get_backend() == 'agg'
-
 
 @pytest.fixture
 def mpl_image_comparison_parameters(request, extension):
