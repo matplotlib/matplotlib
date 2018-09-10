91808a754b46988e7194060f0e74e573ea24eaa7
dabana <david.banville2712@gmail.com>
2018-09-10 13:11:34 -0400

    pass flake 8 compliance check

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -5345,10 +5345,10 @@ class Axes(_AxesBase):
             'lanczos'.
 
             If *interpolation* is 'none', then no interpolation is performed
-            on the Agg, ps, pdf and svg backends. Other backends will fall back to
-            'nearest'. Note that most SVG renders perform interpolation at
+            on the Agg, ps, pdf and svg backends. Other backends will fall back
+            to 'nearest'. Note that most SVG renders perform interpolation at
             rendering and that the default interpolation method they implement
-            may differ. 
+            may differ.
 
             See
             :doc:`/gallery/images_contours_and_fields/interpolation_methods`
