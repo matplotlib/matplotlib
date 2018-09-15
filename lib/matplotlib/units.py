Fix inferring units for zero-sized arrays

lib/matplotlib/units.py
@@ -160,13 +160,16 @@ class Registry(dict):
             x = x.values
 
         # If x is an array, look inside the array for data with units
-        if isinstance(x, np.ndarray) and x.size:
+        if isinstance(x, np.ndarray):
+            # If there are no elements in x, infer the units from its dtype
+            if not x.size:
+                return self.get_converter(np.array([0], dtype=x.dtype))
             xravel = x.ravel()
             try:
                 # pass the first value of x that is not masked back to
                 # get_converter
                 if not np.all(xravel.mask):
-                    # some elements are not masked
+                    # Get first non-masked item
                     converter = self.get_converter(
                         xravel[np.argmin(xravel.mask)])
                     return converter