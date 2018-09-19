7ede68c3f6617c67b07af33b893b33d5fbc0700d
David Stansby <dstansby@gmail.com>
2018-09-19 18:15:57 +0100

    Remove mlab.less_simple_linear_interpolation

--- lib/matplotlib/mlab.py
+++ lib/matplotlib/mlab.py
@@ -2559,47 +2559,6 @@ def rec2csv(r, fname, delimiter=',', formatd=None, missing='',
 ##################################################
 # Linear interpolation algorithms
 ##################################################
-@cbook.deprecated("2.2", alternative="numpy.interp")
-def less_simple_linear_interpolation(x, y, xi, extrap=False):
-    """
-    This function provides simple (but somewhat less so than
-    :func:`cbook.simple_linear_interpolation`) linear interpolation.
-    :func:`simple_linear_interpolation` will give a list of point
-    between a start and an end, while this does true linear
-    interpolation at an arbitrary set of points.
-
-    This is very inefficient linear interpolation meant to be used
-    only for a small number of points in relatively non-intensive use
-    cases.  For real linear interpolation, use scipy.
-    """
-    x = np.asarray(x)
-    y = np.asarray(y)
-    xi = np.atleast_1d(xi)
-
-    s = list(y.shape)
-    s[0] = len(xi)
-    yi = np.tile(np.nan, s)
-
-    for ii, xx in enumerate(xi):
-        bb = x == xx
-        if np.any(bb):
-            jj, = np.nonzero(bb)
-            yi[ii] = y[jj[0]]
-        elif xx < x[0]:
-            if extrap:
-                yi[ii] = y[0]
-        elif xx > x[-1]:
-            if extrap:
-                yi[ii] = y[-1]
-        else:
-            jj, = np.nonzero(x < xx)
-            jj = max(jj)
-
-            yi[ii] = y[jj] + (xx-x[jj])/(x[jj+1]-x[jj]) * (y[jj+1]-y[jj])
-
-    return yi
-
-
 @cbook.deprecated("2.2")
 def slopes(x, y):
     """
