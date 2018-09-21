ac0fc0849898bb555d1dbfd3dfb479f808efc64b
David Stansby <dstansby@gmail.com>
2018-09-21 12:00:10 +0100

    Remove mlab.rec_append_fields

--- lib/matplotlib/mlab.py
+++ lib/matplotlib/mlab.py
@@ -1544,41 +1544,6 @@ def safe_isinf(x):
 
 
 @cbook.deprecated("2.2")
-def rec_append_fields(rec, names, arrs, dtypes=None):
-    """
-    Return a new record array with field names populated with data
-    from arrays in *arrs*.  If appending a single field, then *names*,
-    *arrs* and *dtypes* do not have to be lists. They can just be the
-    values themselves.
-    """
-    if (not isinstance(names, str) and np.iterable(names)
-            and len(names) and isinstance(names[0], str)):
-        if len(names) != len(arrs):
-            raise ValueError("number of arrays do not match number of names")
-    else:  # we have only 1 name and 1 array
-        names = [names]
-        arrs = [arrs]
-    arrs = list(map(np.asarray, arrs))
-    if dtypes is None:
-        dtypes = [a.dtype for a in arrs]
-    elif not np.iterable(dtypes):
-        dtypes = [dtypes]
-    if len(arrs) != len(dtypes):
-        if len(dtypes) == 1:
-            dtypes = dtypes * len(arrs)
-        else:
-            raise ValueError("dtypes must be None, a single dtype or a list")
-    old_dtypes = rec.dtype.descr
-    newdtype = np.dtype(old_dtypes + list(zip(names, dtypes)))
-    newrec = np.recarray(rec.shape, dtype=newdtype)
-    for field in rec.dtype.fields:
-        newrec[field] = rec[field]
-    for name, arr in zip(names, arrs):
-        newrec[name] = arr
-    return newrec
-
-
-@cbook.deprecated("2.2")
 def csv2rec(fname, comments='#', skiprows=0, checkrows=0, delimiter=',',
             converterd=None, names=None, missing='', missingd=None,
             use_mrecords=False, dayfirst=False, yearfirst=False):
