83aac0d99dc8b17272ecaa93a3920b80f71e18c5
Jody Klymak <jklymak@gmail.com>
2018-09-28 08:39:51 -0700

    Backport PR #12322: Fix the docs build.

--- doc/sphinxext/mock_gui_toolkits.py
+++ doc/sphinxext/mock_gui_toolkits.py
@@ -6,23 +6,8 @@ class MyCairoCffi(MagicMock):
     __name__ = "cairocffi"
 
 
-class MyWX(MagicMock):
-    class Panel(object):
-        pass
-
-    class ToolBar(object):
-        pass
-
-    class Frame(object):
-        pass
-
-    class StatusBar(object):
-        pass
-
-
 def setup(app):
     sys.modules.update(
         cairocffi=MyCairoCffi(),
-        wx=MyWX(),
     )
     return {'parallel_read_safe': True, 'parallel_write_safe': True}
