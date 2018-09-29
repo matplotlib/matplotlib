a4e7ddd8584f147083d0ee9e765932877dfb0eaf
Jody Klymak <jklymak@gmail.com>
2018-09-29 16:16:48 -0700

    TST: add test for first draw having correct annotation position

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -5931,3 +5931,20 @@ def test_scatter_empty_data():
     # making sure this does not raise an exception
     plt.scatter([], [])
     plt.scatter([], [], s=[], c=[])
+
+
+@image_comparison(baseline_images=['annotate_across_transforms'],
+        style='mpl20', extensions=['png'], remove_text=True)
+def test_annotate_across_transforms():
+    x = np.linspace(0, 10, 200)
+    y = np.exp(-x) * np.sin(x)
+
+    fig, ax = plt.subplots(figsize=(3.39, 3))
+    ax.plot(x, y)
+    axins = ax.inset_axes([0.4, 0.5, 0.3, 0.3])
+    axins.set_aspect(0.2)
+    axins.xaxis.set_visible(False)
+    axins.yaxis.set_visible(False)
+    ax.annotate("", xy=(x[150], y[150]), xycoords=ax.transData,
+            xytext=(1, 0), textcoords=axins.transAxes,
+            arrowprops=dict(arrowstyle="->"))
