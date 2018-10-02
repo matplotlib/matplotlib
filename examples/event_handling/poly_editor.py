0466b93dba3eca1836436e894b43daba9c92d640
David Stansby <dstansby@gmail.com>
2018-10-02 10:47:42 +0100

    Move methods to functions in poly_editor.py

--- examples/event_handling/poly_editor.py
+++ examples/event_handling/poly_editor.py
@@ -11,6 +11,34 @@ from matplotlib.lines import Line2D
 from matplotlib.artist import Artist
 
 
+def dist(x, y):
+    """
+    Return the distance between two points.
+    """
+    d = x - y
+    return np.sqrt(np.dot(d, d))
+
+
+def dist_point_to_segment(p, s0, s1):
+    """
+    Get the distance of a point to a segment.
+      *p*, *s0*, *s1* are *xy* sequences
+    This algorithm from
+    http://geomalgorithms.com/a02-_lines.html
+    """
+    v = s1 - s0
+    w = p - s0
+    c1 = np.dot(w, v)
+    if c1 <= 0:
+        return dist(p, s0)
+    c2 = np.dot(v, v)
+    if c2 <= c1:
+        return dist(p, s1)
+    b = c1 / c2
+    pb = s0 + b * v
+    return dist(p, pb)
+
+
 class PolygonInteractor(object):
     """
     A polygon editor.
@@ -84,32 +112,6 @@ class PolygonInteractor(object):
 
         return ind
 
-    def dist(self, x, y):
-        """
-        Return the distance between two points.
-        """
-        d = x-y
-        return np.sqrt(np.dot(d, d))
-
-    def dist_point_to_segment(self, p, s0, s1):
-        """
-        Get the distance of a point to a segment.
-          *p*, *s0*, *s1* are *xy* sequences
-        This algorithm from
-        http://geomalgorithms.com/a02-_lines.html
-        """
-        v = s1 - s0
-        w = p - s0
-        c1 = np.dot(w, v)
-        if c1 <= 0:
-            return self.dist(p, s0)
-        c2 = np.dot(v, v)
-        if c2 <= c1:
-            return self.dist(p, s1)
-        b = c1 / c2
-        pb = s0 + b * v
-        return self.dist(p, pb)
-
     def button_press_callback(self, event):
         'whenever a mouse button is pressed'
         if not self.showverts:
@@ -149,7 +151,7 @@ class PolygonInteractor(object):
             for i in range(len(xys) - 1):
                 s0 = xys[i]
                 s1 = xys[i + 1]
-                d = self.dist_point_to_segment(p, s0, s1)
+                d = dist_point_to_segment(p, s0, s1)
                 if d <= self.epsilon:
                     self.poly.xy = np.insert(
                         self.poly.xy, i+1,
