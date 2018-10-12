8acb82e2f7d7b47630e43ba11be4b9bfbb3f54a4
Anubhav Shrimal <anubhavshrimal@gmail.com>
2018-10-12 15:14:44 +0530

    Rectified plot error
    The plot shown on the page: https://matplotlib.org/tutorials/introductory/usage.html#sphx-glr-tutorials-introductory-usage-py
    
    Will be possible only if plt.show() is outside the loop

--- tutorials/introductory/usage.py
+++ tutorials/introductory/usage.py
@@ -611,7 +611,7 @@ import matplotlib.pyplot as plt
 plt.ioff()
 for i in range(3):
     plt.plot(np.random.rand(10))
-    plt.show()
+plt.show()
 
 ###############################################################################
 # which makes three plots, one at a time.
