62297d167c2b7ac8305ba6a60ec0191c6e7a09af
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2018-10-12 23:04:51 -0700

    Backport PR #12478: MAINT: numpy deprecates asscalar in 1.16 (#12508)

--- lib/matplotlib/colors.py
+++ lib/matplotlib/colors.py
@@ -98,7 +98,7 @@ def _sanitize_extrema(ex):
     if ex is None:
         return ex
     try:
-        ret = np.asscalar(ex)
+        ret = ex.item()
     except AttributeError:
         ret = float(ex)
     return ret
