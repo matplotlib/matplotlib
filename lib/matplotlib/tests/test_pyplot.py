f42c0163a71b5514a873579aa29516bf6f52bd0f
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2018-10-16 22:34:00 +0200

    Improve error message on failing test_pyplot_up_to_date

--- lib/matplotlib/tests/test_pyplot.py
+++ lib/matplotlib/tests/test_pyplot.py
@@ -1,3 +1,4 @@
+import difflib
 import subprocess
 import sys
 from pathlib import Path
@@ -16,7 +17,19 @@ def test_pyplot_up_to_date():
     try:
         subprocess.run([sys.executable, str(gen_script)], check=True)
         new_contents = Path(plt.__file__).read_text()
-        assert orig_contents == new_contents
+
+        if orig_contents != new_contents:
+            diff_msg = '\n'.join(
+                difflib.unified_diff(
+                    orig_contents.split('\n'), new_contents.split('\n'),
+                    fromfile='found pyplot.py',
+                    tofile='expected pyplot.py',
+                    n=0, lineterm=''))
+            raise AssertionError(
+                "pyplot.py is not up-to-date. Please run "
+                "'python tools/boilerplate.py' to update pyplot.py. "
+                "Here is a diff of unexpected differences:\n%s" % diff_msg
+            )
     finally:
         Path(plt.__file__).write_text(orig_contents)
 
