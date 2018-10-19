0741bc701542c48a8f5b6bad376f88b9527fe0e4
Joseph Albert <4261275+jcalbert@users.noreply.github.com>
2018-10-19 18:39:30 -0400

    modified errorbar_subsample example to demonstrate feature

--- examples/lines_bars_and_markers/errorbar_subsample.py
+++ examples/lines_bars_and_markers/errorbar_subsample.py
@@ -12,22 +12,29 @@ import matplotlib.pyplot as plt
 
 # example data
 x = np.arange(0.1, 4, 0.1)
-y = np.exp(-x)
+y = np.exp(np.vstack([-1.0 * x, -.5 * x]))
 
 # example variable error bar values
-yerr = 0.1 + 0.1 * np.sqrt(x)
+yerr = 0.1 + 0.1 * np.sqrt(np.vstack([x, x/2]))
 
 
 # Now switch to a more OO interface to exercise more features.
-fig, axs = plt.subplots(nrows=1, ncols=2, sharex=True)
+fig, axs = plt.subplots(nrows=1, ncols=3, sharex=True, figsize=(12, 6))
 ax = axs[0]
-ax.errorbar(x, y, yerr=yerr)
+for i in range(2):
+    ax.errorbar(x, y[i], yerr=yerr[i])
+
 ax.set_title('all errorbars')
 
 ax = axs[1]
-ax.errorbar(x, y, yerr=yerr, errorevery=5)
-ax.set_title('only every 5th errorbar')
+for i in range(2):
+    ax.errorbar(x, y[i], yerr=yerr[i], errorevery=6)
+ax.set_title('only every 6th errorbar')
 
+ax = axs[2]
+for i in range(2):
+    ax.errorbar(x, y[i], yerr=yerr[i], errorevery=(3 * i, 6))
+ax.set_title('second series shifted by 3')
 
 fig.suptitle('Errorbar subsampling for better appearance')
 
