0b7bc40610aa574d1286e567095a2c5d1bd98f1d
thoo <theinhtay@gmail.com>
2018-10-21 22:41:01 -0400

    Modify linestyles file

--- examples/lines_bars_and_markers/linestyles.py
+++ examples/lines_bars_and_markers/linestyles.py
@@ -3,31 +3,44 @@
 Linestyles
 ==========
 
-This examples showcases different linestyles copying those of Tikz/PGF.
+This example showcases different linestyles copying those of Tikz/PGF.
+Linestyle can be provided as simple as *solid* , *dotted* or *dashed*.
+Moreover, the dashing of the line can be controlled by a dash tuple
+such as (offset, (on_off_seq)) as mentioned in `.Line2D.set_linestyle`.
+For e.g., ``(0, (3, 10, 1, 15))`` means 3pt-line,10pt-space,1pt-line,15pt-space
+with no offset.
+
+*Note*: The dash style can also be configured via `.Line2D.set_dashes`
+as shown in :doc:`/gallery/lines_bars_and_markers/line_demo_dash_control`
+and passing a list of dash sequences using the keyword *dashes* to the
+cycler in :doc:`property_cycle </tutorials/intermediate/color_cycle>`.
 """
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 from matplotlib.transforms import blended_transform_factory
 
-linestyles = OrderedDict(
-    [('solid',               (0, ())),
+linestyles = [
+     ('solid',               ('-')),     # Same as (0, ())
+     ('dotted',              (':')),     # Same as (0, (1, 1))
+     ('dashed',              ('--')),
+     ('dashdot',             ('-.')),
+
      ('loosely dotted',      (0, (1, 10))),
-     ('dotted',              (0, (1, 5))),
      ('densely dotted',      (0, (1, 1))),
 
      ('loosely dashed',      (0, (5, 10))),
-     ('dashed',              (0, (5, 5))),
      ('densely dashed',      (0, (5, 1))),
 
      ('loosely dashdotted',  (0, (3, 10, 1, 10))),
-     ('dashdotted',          (0, (3, 5, 1, 5))),
      ('densely dashdotted',  (0, (3, 1, 1, 1))),
 
+     ('dashdotdotted',       (0, (3, 5, 1, 5, 1, 5))),
      ('loosely dashdotdotted', (0, (3, 10, 1, 10, 1, 10))),
-     ('dashdotdotted',         (0, (3, 5, 1, 5, 1, 5))),
-     ('densely dashdotdotted', (0, (3, 1, 1, 1, 1, 1)))])
+     ('densely dashdotdotted', (0, (3, 1, 1, 1, 1, 1)))]
 
+# Reverse the list so that plots and linestyles are in same order.
+linestyles = OrderedDict(linestyles[::-1])
 
 plt.figure(figsize=(10, 6))
 ax = plt.subplot(1, 1, 1)
@@ -44,7 +57,7 @@ plt.xticks([])
 # the reference point (0 in Axes coords, y tick value in Data coords).
 reference_transform = blended_transform_factory(ax.transAxes, ax.transData)
 for i, (name, linestyle) in enumerate(linestyles.items()):
-    ax.annotate(str(linestyle), xy=(0.0, i), xycoords=reference_transform,
+    ax.annotate(repr(linestyle), xy=(0.0, i), xycoords=reference_transform,
                 xytext=(-6, -12), textcoords='offset points', color="blue",
                 fontsize=8, ha="right", family="monospace")
 
