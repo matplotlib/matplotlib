bcc63a40334f9dbb6c88428747971c9f72d3c8cb
thoo <theinhtay@gmail.com>
2018-10-24 16:34:54 -0400

    Split into two subplots

--- examples/lines_bars_and_markers/linestyles.py
+++ examples/lines_bars_and_markers/linestyles.py
@@ -19,12 +19,14 @@ import matplotlib.pyplot as plt
 from collections import OrderedDict
 from matplotlib.transforms import blended_transform_factory
 
-linestyles = [
+
+linestyle_str = [
      ('solid',                 ('solid')),    # Same as (0, ()) or '-'
      ('dotted',                ('dotted')),   # Same as (0, (1, 1)) or '.'
      ('dashed',                ('dashed')),   # Same as '--'
-     ('dashdot',               ('dashdot')),  # Same as '-.'
+     ('dashdot',               ('dashdot'))]  # Same as '-.'
 
+linestyle_tuple = [
      ('loosely dotted',        (0, (1, 10))),
      ('densely dotted',        (0, (1, 1))),
 
@@ -38,24 +40,31 @@ linestyles = [
      ('loosely dashdotdotted', (0, (3, 10, 1, 10, 1, 10))),
      ('densely dashdotdotted', (0, (3, 1, 1, 1, 1, 1)))]
 
-# Reverse the list so that plots and linestyles are in same order.
-linestyles = OrderedDict(linestyles[::-1])
+
+def simple_plot(ax, linestyles):
+    yticklabels = []
+    for i, (name, linestyle) in enumerate(linestyles):
+        ax.plot(X, Y+i, linestyle=linestyle, linewidth=1.5, color='black')
+        yticklabels.append(name)
+
+    ax.set(xticks=[], ylim=(-0.5, len(linestyles)-0.5),
+           yticks=np.arange(len(linestyles)), yticklabels=yticklabels)
+
+    # For each line style, add a text annotation with a small offset from
+    # the reference point (0 in Axes coords, y tick value in Data coords).
+    reference_transform = blended_transform_factory(ax.transAxes, ax.transData)
+    for i, (name, linestyle) in enumerate(linestyles):
+        ax.annotate(repr(linestyle), xy=(0.0, i), xycoords=reference_transform,
+                    xytext=(-6, -12), textcoords='offset points', color="blue",
+                    fontsize=8, ha="right", family="monospace")
+
+
 X, Y = np.linspace(0, 100, 10), np.zeros(10)
+fig, ax = plt.subplots(2, 1, gridspec_kw={'height_ratios': [1, 2]},
+                       figsize=(10, 6))
 
-fig, ax = plt.subplots(figsize=(10, 6))
-for i, (name, linestyle) in enumerate(linestyles.items()):
-    ax.plot(X, Y+i, linestyle=linestyle, linewidth=1.5, color='black')
-
-ax.set(xticks=[], ylim=(-0.5, len(linestyles)-0.5),
-       yticks=np.arange(len(linestyles)), yticklabels=linestyles.keys())
-
-# For each line style, add a text annotation with a small offset from
-# the reference point (0 in Axes coords, y tick value in Data coords).
-reference_transform = blended_transform_factory(ax.transAxes, ax.transData)
-for i, (name, linestyle) in enumerate(linestyles.items()):
-    ax.annotate(repr(linestyle), xy=(0.0, i), xycoords=reference_transform,
-                xytext=(-6, -12), textcoords='offset points', color="blue",
-                fontsize=8, ha="right", family="monospace")
+simple_plot(ax[0], linestyle_str[::-1])
+simple_plot(ax[1], linestyle_tuple[::-1])
 
 plt.tight_layout()
 plt.show()
