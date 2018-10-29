d2e6e9260c0d25567a55f44d050d355684804044
thoo <theinhtay@gmail.com>
2018-10-29 16:29:03 -0400

    changes as recommended

--- examples/lines_bars_and_markers/linestyles.py
+++ examples/lines_bars_and_markers/linestyles.py
@@ -3,11 +3,11 @@
 Linestyles
 ==========
 
-Linestyle can be provided as simple as *solid* , *dotted*, *dashed*
-or *dashdot*. Moreover, the dashing of the line can be controlled by
-a dash tuple such as (offset, (on_off_seq)) as mentioned in
-`.Line2D.set_linestyle`. For example, ``(0, (3, 10, 1, 15))`` means
-3pt-line,10pt-space,1pt-line,15pt-space with no offset.
+Simple linestyles can be defined using the strings "solid", "dotted", "dashed"
+or "dashdot". More refined control can be achieved by providing a dash tuple
+``(offset, (on_off_seq))``. For example, ``(0, (3, 10, 1, 15))`` means
+(3pt line, 10pt space, 1pt line, 15pt space) with no offset. See also
+`.Line2D.set_linestyle`.
 
 *Note*: The dash style can also be configured via `.Line2D.set_dashes`
 as shown in :doc:`/gallery/lines_bars_and_markers/line_demo_dash_control`
@@ -26,12 +26,15 @@ linestyle_str = [
 
 linestyle_tuple = [
      ('loosely dotted',        (0, (1, 10))),
+     ('dotted',                (0, (1, 1))),
      ('densely dotted',        (0, (1, 1))),
 
      ('loosely dashed',        (0, (5, 10))),
+     ('dashed',                (0, (5, 5))),
      ('densely dashed',        (0, (5, 1))),
 
      ('loosely dashdotted',    (0, (3, 10, 1, 10))),
+     ('dashdotted',            (0, (3, 5, 1, 5))),
      ('densely dashdotted',    (0, (3, 1, 1, 1))),
 
      ('dashdotdotted',         (0, (3, 5, 1, 5, 1, 5))),
@@ -39,8 +42,10 @@ linestyle_tuple = [
      ('densely dashdotdotted', (0, (3, 1, 1, 1, 1, 1)))]
 
 
-def simple_plot(ax, linestyles):
+def plot_linestyles(ax, linestyles):
+    X, Y = np.linspace(0, 100, 10), np.zeros(10)
     yticklabels = []
+
     for i, (name, linestyle) in enumerate(linestyles):
         ax.plot(X, Y+i, linestyle=linestyle, linewidth=1.5, color='black')
         yticklabels.append(name)
@@ -56,12 +61,12 @@ def simple_plot(ax, linestyles):
                     xytext=(-6, -12), textcoords='offset points', color="blue",
                     fontsize=8, ha="right", family="monospace")
 
-X, Y = np.linspace(0, 100, 10), np.zeros(10)
-fig, ax = plt.subplots(2, 1, gridspec_kw={'height_ratios': [1, 2]},
-                       figsize=(10, 6))
 
-simple_plot(ax[0], linestyle_str[::-1])
-simple_plot(ax[1], linestyle_tuple[::-1])
+fig, (ax0, ax1) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [1, 3]},
+                               figsize=(10, 8))
+
+plot_linestyles(ax0, linestyle_str[::-1])
+plot_linestyles(ax1, linestyle_tuple[::-1])
 
 plt.tight_layout()
 plt.show()
