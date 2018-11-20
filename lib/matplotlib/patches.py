23dd2ad4c2338b2eb185663a79392d125a1b9158
Antony Lee <anntzer.lee@gmail.com>
2018-11-20 23:46:50 +0100

    Keep Collections and Patches property aliases in sync. (#12851)
    I know there's some desire to completely get rid of the shorthand
    aliases, but until this happens, it seems better to have the same
    aliases available for all classes when that makes sense (and this won't
    make the deprecation period, if it happens, any harder).

--- lib/matplotlib/patches.py
+++ lib/matplotlib/patches.py
@@ -20,8 +20,8 @@ from .path import Path
     "antialiased": ["aa"],
     "edgecolor": ["ec"],
     "facecolor": ["fc"],
-    "linewidth": ["lw"],
     "linestyle": ["ls"],
+    "linewidth": ["lw"],
 })
 class Patch(artist.Artist):
     """
