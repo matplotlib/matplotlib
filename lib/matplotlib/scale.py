82f7ce20af17e9b0f8c0446f0bf3d50f7b9fe917
Elan Ernest <23121882+ImportanceOfBeingErnest@users.noreply.github.com>
2018-12-10 01:57:48 +0100

    Backport PR #12938: Fix xtick.minor.visible only acting on the xaxis

--- lib/matplotlib/scale.py
+++ lib/matplotlib/scale.py
@@ -69,7 +69,8 @@ class LinearScale(ScaleBase):
         axis.set_major_formatter(ScalarFormatter())
         axis.set_minor_formatter(NullFormatter())
         # update the minor locator for x and y axis based on rcParams
-        if rcParams['xtick.minor.visible']:
+        if (axis.axis_name == 'x' and rcParams['xtick.minor.visible']
+            or axis.axis_name == 'y' and rcParams['ytick.minor.visible']):
             axis.set_minor_locator(AutoMinorLocator())
         else:
             axis.set_minor_locator(NullLocator())
