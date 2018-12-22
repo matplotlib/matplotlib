6b634075f5b884a607f46fb1796a127d7905c933
Christoph Reiter <reiter.christoph@gmail.com>
2018-12-22 18:26:20 +0100

    cairo: speed up the cairo append_path() slow path
    Using the Gtk3Cairo backend with wire3d_animation_sgskip.py:
    
    Before: 9.15 fps
    After: 10.93 fps
    
    Converting the code to int makes the comparisons faster.

--- lib/matplotlib/backends/backend_cairo.py
+++ lib/matplotlib/backends/backend_cairo.py
@@ -90,6 +90,8 @@ def _append_paths_slow(ctx, paths, transforms, clip=None):
     for path, transform in zip(paths, transforms):
         for points, code in path.iter_segments(
                 transform, remove_nans=True, clip=clip):
+            # int is a lot faster than uint8 when comparing
+            code = int(code)
             if code == Path.MOVETO:
                 ctx.move_to(*points)
             elif code == Path.CLOSEPOLY:
