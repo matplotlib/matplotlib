a986013f212b3c8ff55d714ac11139a6045c2eec
Jody Klymak <jklymak@gmail.com>
2019-01-04 10:40:55 -0800

    ahem... spelling of _warn_external

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -2638,7 +2638,7 @@ class Axes(_AxesBase):
             self.add_collection(stemlines)
         # Old behaviour is to plot each of the lines individually
         else:
-            cbook._warn_extrnal(
+            cbook._warn_external(
                 'In Matplotlib 3.3 individual lines on a stem plot will be '
                 'added as a LineCollection instead of individual lines.\n'
                 'This significantly improves the performance of a stem plot.\n'
