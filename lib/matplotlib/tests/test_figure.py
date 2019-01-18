4724634aceca40294a71569e2a945224452a3d09
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2019-01-18 18:37:55 -0500

    TST: Fix incorrect call to pytest.raises. (#13194)
    The `message` parameter is actually the message to print when it fails,
    but we want to check the exception message, which is the `match`
    parameter.

--- lib/matplotlib/tests/test_figure.py
+++ lib/matplotlib/tests/test_figure.py
@@ -366,8 +366,8 @@ def test_subplots_shareax_loglabels():
 
 def test_savefig():
     fig = plt.figure()
-    msg = "savefig() takes 2 positional arguments but 3 were given"
-    with pytest.raises(TypeError, message=msg):
+    msg = r"savefig\(\) takes 2 positional arguments but 3 were given"
+    with pytest.raises(TypeError, match=msg):
         fig.savefig("fname1.png", "fname2.png")
 
 
