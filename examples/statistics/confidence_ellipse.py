47a87f8f446c085e6379750c45a50c2b4fe1e69c
Carsten <carstenschelp@mp.nl>
2019-03-09 15:46:46 +0100

    Have ellipse plotted "over" scattered dataset using zorder=0

--- examples/statistics/confidence_ellipse.py
+++ examples/statistics/confidence_ellipse.py
@@ -209,12 +209,12 @@ ax_kwargs.axvline(c='grey', lw=1)
 ax_kwargs.axhline(c='grey', lw=1)
 
 x, y = get_correlated_dataset(500, dependency_kwargs, mu, scale)
-# Now plot the dataset first ("under" the ellipse) in order to
-# demonstrate the transparency of the ellipse (alpha).
-ax_kwargs.scatter(x, y, s=0.5)
+# Plot the ellipse with zorder=0 in order to demonstrate
+# its transparency (caused by the use of alpha).
 confidence_ellipse(x, y, ax_kwargs,
-    alpha=0.5, facecolor='pink', edgecolor='purple')
+    alpha=0.5, facecolor='pink', edgecolor='purple', zorder=0)
 
+ax_kwargs.scatter(x, y, s=0.5)
 ax_kwargs.scatter(mu[0], mu[1], c='red', s=3)
 ax_kwargs.set_title(f'Using kwargs')
 
