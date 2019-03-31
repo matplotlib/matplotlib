192f4887a97a9ef30d628a98fd58b96253b45689
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2019-03-31 15:16:13 +0200

    Backport PR #13817: Remove borders from barcode example

--- examples/images_contours_and_fields/barcode_demo.py
+++ examples/images_contours_and_fields/barcode_demo.py
@@ -14,17 +14,18 @@ np.random.seed(19680801)
 # the bar
 x = np.random.rand(500) > 0.7
 
-axprops = dict(xticks=[], yticks=[])
 barprops = dict(aspect='auto', cmap='binary', interpolation='nearest')
 
 fig = plt.figure()
 
 # a vertical barcode
-ax1 = fig.add_axes([0.1, 0.3, 0.1, 0.6], **axprops)
+ax1 = fig.add_axes([0.1, 0.1, 0.1, 0.8])
+ax1.set_axis_off()
 ax1.imshow(x.reshape((-1, 1)), **barprops)
 
 # a horizontal barcode
-ax2 = fig.add_axes([0.3, 0.1, 0.6, 0.1], **axprops)
+ax2 = fig.add_axes([0.3, 0.4, 0.6, 0.2])
+ax2.set_axis_off()
 ax2.imshow(x.reshape((1, -1)), **barprops)
 
 plt.show()
