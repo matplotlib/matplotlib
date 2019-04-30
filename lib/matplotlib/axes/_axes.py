5552d3d669350a2e6164a6ba6da680d02dfd042c
sasoripathos <sasori.pan.jerry@gmail.com>
2019-04-30 15:26:33 -0400

    Fix code style

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -7890,7 +7890,7 @@ optional.
     @_preprocess_data(replace_names=["dataset"])
     def violinplot(self, dataset, positions=None, vert=True, widths=0.5,
                    showmeans=False, showextrema=True, showmedians=False,
-                    percentiles=None, points=100, bw_method=None):
+                   percentiles=None, points=100, bw_method=None):
         """
         Make a violin plot.
 
