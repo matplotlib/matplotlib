bb54fd171ddb54ddcd9e4eca993612b0821d8e9f
Chris Barnes <barnesc@janelia.hhmi.org>
2019-05-20 13:17:55 -0400

    TST: Axes.indicate_inset failure
    See #14275
    
    inset_ax argument is meant to be optional, but currently fails if not
    given.

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -5935,6 +5935,33 @@ def test_tick_padding_tightbbox():
     assert bb.y0 < bb2.y0
 
 
+def test_inset():
+    """Regression test: failure described in for issue #14275"""
+    dx, dy = 0.05, 0.05
+    # generate 2 2d grids for the x & y bounds
+    y, x = np.mgrid[slice(1, 5 + dy, dy),
+                    slice(1, 5 + dx, dx)]
+    z = np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
+
+    fig, ax = plt.subplots()
+    ax.pcolormesh(x, y, z)
+    ax.set_aspect(1.)
+    ax.apply_aspect()
+    # we need to apply_aspect to make the drawing below work.
+
+    xlim = [1.5, 2.15]
+    ylim = [2, 2.5]
+
+    rect = [xlim[0], ylim[0], xlim[1] - xlim[0], ylim[1] - ylim[0]]
+
+    rec, connectors = ax.indicate_inset(bounds=rect)
+    assert connectors == []
+    fig.canvas.draw()
+    xx = np.array([[1.5, 2.],
+                   [2.15, 2.5]])
+    assert (np.all(rec.get_bbox().get_points() == xx))
+
+
 def test_zoom_inset():
     dx, dy = 0.05, 0.05
     # generate 2 2d grids for the x & y bounds
