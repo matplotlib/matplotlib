2cd914d5894fa240a737c0d17f6b35ad534b08b9
Antony Lee <anntzer.lee@gmail.com>
2019-06-07 01:04:09 +0200

    Move most of pytest's conf to conftest.py.
    pytest.ini is not found if running the test suite from anywhere but
    a source checkout root.  For example, it is not found if running
    `matplotlib.test()` from an installed Matplotlib (that includes test
    data). mplcairo's test suite also relies on being able to run the
    Matplotlib test suite from not-the-source-tree (via `pytest --pyargs
    matplotlib`).
    
    This is particularly problematic now that pytest requires markers to be
    explicitly declared, and emits a warning when they are not.
    
    Instead, declare most relevant options in pytest_configure().  (Note
    that this actually only helps mplcairo when used in conjunction of
    pytest>=5.0 due to pytest issue 5078, but the patch doesn't bump the
    pytest version requirement for matplotlib.)
    
    See also pytest issue 4039.

--- lib/matplotlib/testing/conftest.py
+++ lib/matplotlib/testing/conftest.py
@@ -5,6 +5,21 @@ from matplotlib import cbook
 
 
 def pytest_configure(config):
+    # config is initialized here rather than in pytest.ini so that `pytest
+    # --pyargs matplotlib` (which would not find pytest.ini) works.  The only
+    # entries in pytest.ini set minversion (which is checked earlier) and
+    # testpaths/python_files, as they are required to properly find the tests.
+    for key, value in [
+        ("markers", "flaky: (Provided by pytest-rerunfailures.)"),
+        ("markers", "timeout: (Provided by pytest-timeout.)"),
+        ("markers", "backend: Set alternate Matplotlib backend temporarily."),
+        ("markers", "style: Set alternate Matplotlib style temporarily."),
+        ("markers", "baseline_images: Compare output against references."),
+        ("markers", "pytz: Tests that require pytz to be installed."),
+        ("filterwarnings", "error"),
+    ]:
+        config.addinivalue_line(key, value)
+
     matplotlib.use('agg', force=True)
     matplotlib._called_from_pytest = True
     matplotlib._init_tests()
