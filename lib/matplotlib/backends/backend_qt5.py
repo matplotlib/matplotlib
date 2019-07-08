3edda06d65c846178bd279c24b9c3cbe33e8a2b7
Antony Lee <anntzer.lee@gmail.com>
2019-07-08 12:03:56 +0200

    Make Qt navtoolbar more robust against removal of either pan or zoom.
    ... by using a QButtonGroup to ensure that a button gets unchecked when
    the other gets checked.

--- lib/matplotlib/backends/backend_qt5.py
+++ lib/matplotlib/backends/backend_qt5.py
@@ -680,6 +680,8 @@ class NavigationToolbar2QT(NavigationToolbar2, QtWidgets.QToolBar):
 
     def _init_toolbar(self):
         self.basedir = str(cbook._get_data_path('images'))
+        # Ensure that zoom and pan are mutually exclusive.
+        self._button_group = QtWidgets.QButtonGroup()
 
         for text, tooltip_text, image_file, callback in self.toolitems:
             if text is None:
@@ -690,6 +692,7 @@ class NavigationToolbar2QT(NavigationToolbar2, QtWidgets.QToolBar):
                 self._actions[callback] = a
                 if callback in ['zoom', 'pan']:
                     a.setCheckable(True)
+                    self._button_group.addButton(self.widgetForAction(a))
                 if tooltip_text is not None:
                     a.setToolTip(tooltip_text)
                 if text == 'Subplots':
@@ -763,18 +766,13 @@ class NavigationToolbar2QT(NavigationToolbar2, QtWidgets.QToolBar):
             ax = axes[titles.index(item)]
         figureoptions.figure_edit(ax, self)
 
-    def _update_buttons_checked(self):
-        # sync button checkstates to match active mode
-        self._actions['pan'].setChecked(self._active == 'PAN')
-        self._actions['zoom'].setChecked(self._active == 'ZOOM')
-
     def pan(self, *args):
         super().pan(*args)
-        self._update_buttons_checked()
+        self._actions['pan'].setChecked(self._active == 'PAN')
 
     def zoom(self, *args):
         super().zoom(*args)
-        self._update_buttons_checked()
+        self._actions['zoom'].setChecked(self._active == 'ZOOM')
 
     def set_message(self, s):
         self.message.emit(s)
