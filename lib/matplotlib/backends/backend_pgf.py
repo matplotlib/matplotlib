87131255371829eee7d0f6b8f8cdb8f2c5106180
Antony Lee <anntzer.lee@gmail.com>
2019-07-12 19:49:57 +0200

    Fix get_canvas_width_height() for pgf backend.
    The pgf backend's get_canvas_width_height returns the canvas size in
    inches, but it should return it in display units ("pixels", or
    inches*dpi) instead.
    
    get_canvas_width_height is actually barely used (only in text.py for
    some backends (not pgf), in collections.py as an optimization, and in
    BboxImage -- the only really relevant case), so to show the failure,
    apply
    
        diff --git i/examples/text_labels_and_annotations/demo_text_path.py w/examples/text_labels_and_annotations/demo_text_path.py
        index 47ed5dde6..d45a922f4 100644
        --- i/examples/text_labels_and_annotations/demo_text_path.py
        +++ w/examples/text_labels_and_annotations/demo_text_path.py
        @@ -61,6 +61,7 @@ class PathClippedImagePatch(mpatches.PathPatch):
    
        if __name__ == "__main__":
    
        +    import matplotlib; matplotlib.use("pgf")
            usetex = plt.rcParams["text.usetex"]
    
            fig = plt.figure()
        @@ -156,4 +157,5 @@ if __name__ == "__main__":
            ax.set_xlim(0, 1)
            ax.set_ylim(0, 1)
    
        +    plt.savefig("/tmp/test.pdf")
            plt.show()
    
    and observe that the image is incorrectly drawn before the patch, but
    correctly after.

--- lib/matplotlib/backends/backend_pgf.py
+++ lib/matplotlib/backends/backend_pgf.py
@@ -736,7 +736,8 @@ class RendererPgf(RendererBase):
 
     def get_canvas_width_height(self):
         # docstring inherited
-        return self.figure.get_figwidth(), self.figure.get_figheight()
+        return (self.figure.get_figwidth() * self.dpi,
+                self.figure.get_figheight() * self.dpi)
 
     def points_to_pixels(self, points):
         # docstring inherited
