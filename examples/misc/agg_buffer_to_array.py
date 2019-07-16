719281bc8ea34877ebb012cf45180de2b29a55e9
Thomas A Caswell <tcaswell@gmail.com>
2019-07-15 23:35:43 -0400

    DOC: create a figure without pyplot in buffer example
    closes #14796

--- examples/misc/agg_buffer_to_array.py
+++ examples/misc/agg_buffer_to_array.py
@@ -7,15 +7,21 @@ Convert a rendered figure to its image (NumPy array) representation.
 """
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvas
 
-# make an agg figure
-fig, ax = plt.subplots()
+# Create a figure that pyplot does not know about.
+fig = Figure()
+# attach a non-interactive Agg canvas to the figure
+# (as a side-effect of the ``__init__``)
+canvas = FigureCanvas(fig)
+ax = fig.subplots()
 ax.plot([1, 2, 3])
 ax.set_title('a simple figure')
-fig.canvas.draw()
-
+# Force a draw so we can grab the pixel buffer
+canvas.draw()
 # grab the pixel buffer and dump it into a numpy array
-X = np.array(fig.canvas.renderer.buffer_rgba())
+X = np.array(canvas.renderer.buffer_rgba())
 
 # now display the array X as an Axes in a new figure
 fig2 = plt.figure()
