32249948e16df666fb5046f7c22f98a5b5aacb38
Antony Lee <anntzer.lee@gmail.com>
2019-07-16 18:48:52 +0200

    Simplify text_layout example.
    By working with figure-level artists rather than axes-level ones (where
    the axes cover the whole figure...) we don't have do specify the
    transform again and again.

--- examples/pyplots/text_layout.py
+++ examples/pyplots/text_layout.py
@@ -5,82 +5,51 @@ Text Layout
 
 Create text with different alignment and rotation.
 """
+
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-# build a rectangle in axes coords
+fig = plt.figure()
+
 left, width = .25, .5
 bottom, height = .25, .5
 right = left + width
 top = bottom + height
 
-fig = plt.figure()
-ax = fig.add_axes([0,0,1,1])
+# Draw a rectangle in figure coordinates ((0, 0) is bottom left and (1, 1) is
+# upper right).
+p = patches.Rectangle((left, bottom), width, height, fill=False)
+fig.add_artist(p)
 
-# axes coordinates are 0,0 is bottom left and 1,1 is upper right
-p = patches.Rectangle(
-    (left, bottom), width, height,
-    fill=False, transform=ax.transAxes, clip_on=False
-    )
+# Figure.text (aka. plt.figtext) behaves like Axes.text (aka. plt.text), with
+# the sole exception that the coordinates are relative to the figure ((0, 0) is
+# bottom left and (1, 1) is upper right).
+fig.text(left, bottom, 'left top',
+         horizontalalignment='left', verticalalignment='top')
+fig.text(left, bottom, 'left bottom',
+         horizontalalignment='left', verticalalignment='bottom')
+fig.text(right, top, 'right bottom',
+         horizontalalignment='right', verticalalignment='bottom')
+fig.text(right, top, 'right top',
+         horizontalalignment='right', verticalalignment='top')
+fig.text(right, bottom, 'center top',
+         horizontalalignment='center', verticalalignment='top')
+fig.text(left, 0.5*(bottom+top), 'right center',
+         horizontalalignment='right', verticalalignment='center',
+         rotation='vertical')
+fig.text(left, 0.5*(bottom+top), 'left center',
+         horizontalalignment='left', verticalalignment='center',
+         rotation='vertical')
+fig.text(0.5*(left+right), 0.5*(bottom+top), 'middle',
+         horizontalalignment='center', verticalalignment='center',
+         fontsize=20, color='red')
+fig.text(right, 0.5*(bottom+top), 'centered',
+         horizontalalignment='center', verticalalignment='center',
+         rotation='vertical')
+fig.text(left, top, 'rotated\nwith newlines',
+         horizontalalignment='center', verticalalignment='center',
+         rotation=45)
 
-ax.add_patch(p)
-
-ax.text(left, bottom, 'left top',
-        horizontalalignment='left',
-        verticalalignment='top',
-        transform=ax.transAxes)
-
-ax.text(left, bottom, 'left bottom',
-        horizontalalignment='left',
-        verticalalignment='bottom',
-        transform=ax.transAxes)
-
-ax.text(right, top, 'right bottom',
-        horizontalalignment='right',
-        verticalalignment='bottom',
-        transform=ax.transAxes)
-
-ax.text(right, top, 'right top',
-        horizontalalignment='right',
-        verticalalignment='top',
-        transform=ax.transAxes)
-
-ax.text(right, bottom, 'center top',
-        horizontalalignment='center',
-        verticalalignment='top',
-        transform=ax.transAxes)
-
-ax.text(left, 0.5*(bottom+top), 'right center',
-        horizontalalignment='right',
-        verticalalignment='center',
-        rotation='vertical',
-        transform=ax.transAxes)
-
-ax.text(left, 0.5*(bottom+top), 'left center',
-        horizontalalignment='left',
-        verticalalignment='center',
-        rotation='vertical',
-        transform=ax.transAxes)
-
-ax.text(0.5*(left+right), 0.5*(bottom+top), 'middle',
-        horizontalalignment='center',
-        verticalalignment='center',
-        fontsize=20, color='red',
-        transform=ax.transAxes)
-
-ax.text(right, 0.5*(bottom+top), 'centered',
-        horizontalalignment='center',
-        verticalalignment='center',
-        rotation='vertical',
-        transform=ax.transAxes)
-
-ax.text(left, top, 'rotated\nwith newlines',
-        horizontalalignment='center',
-        verticalalignment='center',
-        rotation=45,
-        transform=ax.transAxes)
-
-ax.set_axis_off()
 plt.show()
 
 #############################################################################
@@ -94,5 +63,5 @@ plt.show()
 # in this example:
 
 import matplotlib
-matplotlib.axes.Axes.text
-matplotlib.pyplot.text
+matplotlib.figure.Figure.add_artist
+matplotlib.figure.Figure.text
