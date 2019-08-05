8d27eeb735f25c12118ca3e0e6d05d872a5f42a5
Thomas A Caswell <tcaswell@gmail.com>
2019-08-05 15:49:30 -0400

    Merge pull request #14915 from AWhetter/fix_14585
    BLD: Don't redownload jquery when installing from an sdist

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ if __name__ == '__main__':
 from io import BytesIO
 import os
 from string import Template
+import shutil
 from zipfile import ZipFile
 
 from setuptools import setup
@@ -147,6 +148,16 @@ cmdclass['build_ext'] = BuildExtraLibraries
 
 
 def _download_jquery_to(dest):
+    if os.path.exists(os.path.join(dest, "jquery-ui-1.12.1")):
+        return
+
+    # If we are installing from an sdist, use the already downloaded jquery-ui
+    sdist_src = os.path.join(
+        "lib/matplotlib/backends/web_backend", "jquery-ui-1.12.1")
+    if os.path.exists(sdist_src):
+        shutil.copytree(sdist_src, os.path.join(dest, "jquery-ui-1.12.1"))
+        return
+
     # Note: When bumping the jquery-ui version, also update the versions in
     # single_figure.html and all_figures.html.
     url = "https://jqueryui.com/resources/download/jquery-ui-1.12.1.zip"
