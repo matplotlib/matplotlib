d9c4b3befe1936d139758736f6faceb85208f607
Avni Sharma <16avnisharma@gmail.com>
2019-08-09 17:50:34 +0530

    Moved the example under step_demo.py

--- examples/lines_bars_and_markers/step_demo.py
+++ examples/lines_bars_and_markers/step_demo.py
@@ -29,6 +29,15 @@ plt.plot(x, y, 'C2o', alpha=0.5)
 plt.legend(title='Parameter where:')
 plt.show()
 
+# The following example demonstrates the drawstyle parameter that can be used 
+# to modify how the points are connected.
+x = np.arange(1,14,0.1)
+
+fig, ax = plt.subplots(figsize=(15,5))
+_ = ax.plot(x, y, drawstyle='steps-post')
+_ = ax.set_aspect('equal')
+plt.show()
+
 #############################################################################
 #
 # ------------
