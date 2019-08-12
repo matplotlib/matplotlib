2e78285feeeb8a753031e124e6defcca48e7a72a
Joy Bhalla <joybhalla9@gmail.com>
2019-08-12 16:39:25 +0530

    Added minimum python version check

--- doc/conf.py
+++ doc/conf.py
@@ -17,6 +17,9 @@ import sys
 import matplotlib
 import sphinx
 
+if sys.version_info < (3, 0, 0):
+    print("You're using python 2.x, conf.py works with python3+ only.")
+    exit()
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
