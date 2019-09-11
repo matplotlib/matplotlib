df3834e90d922ff1d36a2b5b2685b41a255dc1a7
Antony Lee <anntzer.lee@gmail.com>
2019-09-11 10:51:09 +0200

    Fix incorrect calls to warn_deprecated.
    1) `message` is keyword-only.
    2) it's `%(since)s`, not `%(version)s`.

--- lib/matplotlib/offsetbox.py
+++ lib/matplotlib/offsetbox.py
@@ -1308,7 +1308,7 @@ class AnchoredText(AnchoredOffsetbox):
             cbook.warn_deprecated(
                 "3.1", message="Mixing horizontalalignment or "
                 "verticalalignment with AnchoredText is not supported, "
-                "deprecated since %(version)s, and will raise an exception "
+                "deprecated since %(since)s, and will raise an exception "
                 "%(removal)s.")
 
         self.txt = TextArea(s, textprops=prop, minimumdescent=False)
