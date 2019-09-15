9cea8eac747932920f5e7a36d3f548e1fc0aea0c
Julian Mehne <julian.mehne@posteo.de>
2019-09-15 14:58:05 +0200

    Embedding in Tk example: Fix toolbar being clipped. (#15266)
    Calling `pack` without options puts the widget at the end of the package
    list. Space is allocated to widgets sequentially. If a window is too
    small, there may be no space left for widgets. By packing the canvas
    last, we essentially give the toolbar and the quit button priority in
    the packing order.
    
    Fixes #13485.

--- examples/user_interfaces/embedding_in_tk_sgskip.py
+++ examples/user_interfaces/embedding_in_tk_sgskip.py
@@ -25,11 +25,9 @@ fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
 
 canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
 canvas.draw()
-canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
 toolbar = NavigationToolbar2Tk(canvas, root)
 toolbar.update()
-canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
 
 def on_key_press(event):
@@ -40,6 +38,12 @@ def on_key_press(event):
 canvas.mpl_connect("key_press_event", on_key_press)
 
 button = tkinter.Button(master=root, text="Quit", command=root.quit)
+
+# Packing order is important. Widgets are processed sequentially and if there
+# is no space left, because the window is too small, they are not displayed.
+# The canvas is rather flexible in its size, so we pack it last which makes
+# sure the UI controls are displayed as long as possible.
 button.pack(side=tkinter.BOTTOM)
+canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
 tkinter.mainloop()
