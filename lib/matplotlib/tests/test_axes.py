0e25db2cdfa8163e397c055c2d4be52c4842ddec
Alex Rudy <alex.rudy@gmail.com>
2019-09-29 12:10:37 -0700

    Add datetime64 check

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -1752,20 +1752,27 @@ def test_hist_datetime_datasets():
     ax.hist(data, stacked=True)
     ax.hist(data, stacked=False)
 
-@pytest.mark.parametrize("bins_preprocess", [lambda bins: None, mpl.dates.date2num, lambda bins: bins], 
-                         ids=['None', 'date2num', 'datetime.datetime'])
+
+@pytest.mark.parametrize("bins_preprocess",
+                         [lambda bins: None,
+                          mpl.dates.date2num,
+                          lambda bins: bins,
+                          lambda bins: np.asarray(bins).astype('datetime64')],
+                         ids=['None', 'date2num', 'datetime.datetime',
+                              'np.datetime64'])
 def test_hist_datetime_datasets_bins(bins_preprocess):
-    data = [[datetime.datetime(2019, 1, 5), datetime.datetime(2019, 1, 11), 
+    data = [[datetime.datetime(2019, 1, 5), datetime.datetime(2019, 1, 11),
              datetime.datetime(2019, 2, 1), datetime.datetime(2019, 3, 1)],
-            [datetime.datetime(2019, 1, 11), datetime.datetime(2019, 2, 5), 
+            [datetime.datetime(2019, 1, 11), datetime.datetime(2019, 2, 5),
              datetime.datetime(2019, 2, 18), datetime.datetime(2019, 3, 1)]]
 
-    date_edges = [datetime.datetime(2019, 1, 1), datetime.datetime(2019, 2, 1), 
-                  datetime.datetime(2019, 3, 1),] 
-    
+    date_edges = [datetime.datetime(2019, 1, 1), datetime.datetime(2019, 2, 1),
+                  datetime.datetime(2019, 3, 1)]
+
     fig, ax = plt.subplots()
     ax.hist(data, bins=bins_preprocess(date_edges), stacked=True)
     ax.hist(data, bins=bins_preprocess(date_edges), stacked=False)
+
 
 @pytest.mark.parametrize('data, expected_number_of_hists',
                          [([], 1),
