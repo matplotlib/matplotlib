8f4ce304a9bc443e0f8f5ab43a19d9a007a2d022
Emlyn Price <emlyn.je.price@gmail.com>
2019-10-01 21:23:14 +0100

    Add 'step' to histtype demo
    Add the step histogram type to the histtype demo

--- examples/statistics/histogram_histtypes.py
+++ examples/statistics/histogram_histtypes.py
@@ -21,15 +21,18 @@ mu = 200
 sigma = 25
 x = np.random.normal(mu, sigma, size=100)
 
-fig, (ax0, ax1) = plt.subplots(ncols=2, figsize=(8, 4))
+fig, (ax0, ax1, ax2) = plt.subplots(ncols=3, figsize=(8, 4))
 
 ax0.hist(x, 20, density=True, histtype='stepfilled', facecolor='g', alpha=0.75)
 ax0.set_title('stepfilled')
 
+ax1.hist(x, 20, density=True, histtype='step', facecolor='g', alpha=0.75)
+ax1.set_title('step')
+
 # Create a histogram by providing the bin edges (unequally spaced).
 bins = [100, 150, 180, 195, 205, 220, 250, 300]
-ax1.hist(x, bins, density=True, histtype='bar', rwidth=0.8)
-ax1.set_title('unequal bins')
+ax2.hist(x, bins, density=True, histtype='bar', rwidth=0.8)
+ax2.set_title('bar, unequal bins')
 
 fig.tight_layout()
 plt.show()
