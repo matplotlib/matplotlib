bb138156f17728219baf8b542838abd49c51ba0e
Hanno Rein <hanno@hanno-rein.de>
2019-10-22 18:58:09 -0400

    Fixes an error in the documentation of Ellipse

--- lib/matplotlib/patches.py
+++ lib/matplotlib/patches.py
@@ -1420,7 +1420,7 @@ class Ellipse(Patch):
 
     def get_path(self):
         """
-        Return the vertices of the rectangle
+        Return the vertices of the ellipse
         """
         return self._path
 
