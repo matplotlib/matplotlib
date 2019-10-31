fd9f102e1a5fd6dacb9af697c8249b3493450ae6
Thomas A Caswell <tcaswell@gmail.com>
2019-10-30 22:00:03 -0400

    TST: skip the nbagg test if we don't have IPython installed

--- lib/matplotlib/tests/test_backend_webagg.py
+++ lib/matplotlib/tests/test_backend_webagg.py
@@ -3,13 +3,17 @@ import sys
 import pytest
 
 
-@pytest.mark.parametrize('backend', ['webagg', 'nbagg'])
+@pytest.mark.parametrize("backend", ["webagg", "nbagg"])
 def test_webagg_fallback(backend):
-    test_code = ("import os;" +
-                 f"os.environ['MPLBACKEND'] = '{backend}';" +
-                 "import matplotlib.pyplot as plt; " +
-                 "print(plt.get_backend());"
-                 f"assert '{backend}' == plt.get_backend().lower();")
+    if backend == "nbagg":
+        pytest.importorskip("IPython")
+    test_code = (
+        "import os;"
+        + f"os.environ['MPLBACKEND'] = '{backend}';"
+        + "import matplotlib.pyplot as plt; "
+        + "print(plt.get_backend());"
+        f"assert '{backend}' == plt.get_backend().lower();"
+    )
     ret = subprocess.call(
         [sys.executable, "-c", test_code],
         env={"MPLBACKEND": backend, "DISPLAY": ""}
