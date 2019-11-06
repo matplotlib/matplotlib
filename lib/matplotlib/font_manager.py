66439ae8af4518e39dc0428d338192cbacb6466a
Colton Lathrop <colathro@microsoft.com>
2019-11-06 13:15:51 -0600

    tests

--- lib/matplotlib/font_manager.py
+++ lib/matplotlib/font_manager.py
@@ -1279,6 +1279,11 @@ class FontManager:
                 best_font = font
             if score == 0:
                 break
+        
+        if best_font is not None:
+            if abs(map_weight_name_to_score(prop.get_weight()) - map_weight_name_to_score(best_font.weight)) > 100:
+                _log.warning('findfont: Failed to find font weight %s, now using %s.',
+                            prop.get_weight(), best_font.weight)
 
         if best_font is None or best_score >= 10.0:
             if fallback_to_default:
@@ -1296,10 +1301,6 @@ class FontManager:
             _log.debug('findfont: Matching %s to %s (%r) with score of %f.',
                        prop, best_font.name, best_font.fname, best_score)
             result = best_font.fname
-        
-        if abs(map_weight_name_to_score(prop.get_weight()) - map_weight_name_to_score(best_font.weight)) > 100:
-            _log.warning('findfont: Failed to find font weight %s, now using %s.',
-                        prop.get_weight(), best_font.weight)
 
         if not os.path.isfile(result):
             if rebuild_if_missing:
