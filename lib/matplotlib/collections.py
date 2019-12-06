e51a8d89bb83182692a4e566e4ab711174e88295
Thomas A Caswell <tcaswell@gmail.com>
2019-12-05 23:44:06 -0500

    Update lib/matplotlib/collections.py
    Co-Authored-By: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>

--- lib/matplotlib/collections.py
+++ lib/matplotlib/collections.py
@@ -475,7 +475,7 @@ class Collection(artist.Artist, cm.ScalarMappable):
 
         Parameters
         ----------
-        offsets : array-like (N, 2) or (2, )
+        offsets : array-like (N, 2) or (2,)
         """
         offsets = np.asanyarray(offsets, float)
         if offsets.shape == (2,):  # Broadcast (2,) -> (1, 2) but nothing else.
