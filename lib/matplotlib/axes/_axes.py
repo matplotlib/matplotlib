1e230033a8a630d648f77b7da704ed230d471c5b
Inception95 <yangyiheng@inspur.com>
2019-12-13 16:53:57 -0800

    Raise an ValueError when Axes.pie accepts negative values

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -2921,6 +2921,10 @@ class Axes(_AxesBase):
                 "and will be removed %(removal)s; pass a 1D array instead.")
             x = np.atleast_1d(x.squeeze())
 
+        for part in x:
+            if part < 0:
+                raise ValueError("wedge sizes must be non negative values")
+
         sx = x.sum()
         if sx > 1:
             x = x / sx
