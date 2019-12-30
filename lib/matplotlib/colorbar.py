ccbb825ceae6ea754c2f423eda386148e747713b
Antony Lee <anntzer.lee@gmail.com>
2019-12-30 22:12:29 +0100

    Document that colorbar() takes a label kwarg.
    This is easier than adding a separate call to set_label().

--- lib/matplotlib/colorbar.py
+++ lib/matplotlib/colorbar.py
@@ -116,6 +116,8 @@ colormap_kw_doc = """
                   An alternative `~.ticker.Formatter` may be given instead.
     *drawedges*   bool
                   Whether to draw lines at color boundaries.
+    *label*       str
+                  The label on the colorbar's long axis.
     ============  ====================================================
 
     The following will probably be useful only in the context of
@@ -177,8 +179,7 @@ use_gridspec : bool, optional
 Returns
 -------
 colorbar : `~matplotlib.colorbar.Colorbar`
-    See also its base class, `~matplotlib.colorbar.ColorbarBase`.  Use
-    `~.ColorbarBase.set_label` to label the colorbar.
+    See also its base class, `~matplotlib.colorbar.ColorbarBase`.
 
 Notes
 -----
