9859fa29e42fa3af117b47a52de749160f044702
Jody Klymak <jklymak@gmail.com>
2020-01-28 15:20:18 -0800

    Backport PR #16347: FIX: catch warnings from pandas in cbook._check_1d
    Merge pull request #16347 from tacaswell/fix_pandas_index_deprecation
    
    FIX: catch warnings from pandas in cbook._check_1d
     Conflicts:
    	lib/matplotlib/tests/test_axes.py
             - new test in middle of section of tests not on v2.2.x yet, only kept
               new test

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -1472,6 +1472,15 @@ def test_hist_log():
     ax.hist(data, fill=False, log=True)
 
 
+def test_pandas_minimal_plot(pd):
+    # smoke test that series and index objcets do not warn
+    x = pd.Series([1, 2], dtype="float64")
+    plt.plot(x, x)
+    plt.plot(x.index, x)
+    plt.plot(x)
+    plt.plot(x.index)
+
+
 @image_comparison(baseline_images=['hist_bar_empty'], remove_text=True,
                   extensions=['png'])
 def test_hist_bar_empty():
