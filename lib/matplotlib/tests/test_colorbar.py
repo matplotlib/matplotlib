5d99e151be80bcb0b3b6d081fd3038330f573d94
Thomas A Caswell <tcaswell@gmail.com>
2020-01-28 22:12:51 -0500

    Backport PR #16344: Cast vmin/vmax to floats before nonsingular-expanding them.

--- lib/matplotlib/tests/test_colorbar.py
+++ lib/matplotlib/tests/test_colorbar.py
@@ -580,3 +580,14 @@ def test_colorbar_label():
 
     cbar3 = fig.colorbar(im, orientation='horizontal', label='horizontal cbar')
     assert cbar3.ax.get_xlabel() == 'horizontal cbar'
+
+
+@pytest.mark.parametrize("clim", [(-20000, 20000), (-32768, 0)])
+def test_colorbar_int(clim):
+    # Check that we cast to float early enough to not
+    # overflow ``int16(20000) - int16(-20000)`` or
+    # run into ``abs(int16(-32768)) == -32768``.
+    fig, ax = plt.subplots()
+    im = ax.imshow([[*map(np.int16, clim)]])
+    fig.colorbar(im)
+    assert (im.norm.vmin, im.norm.vmax) == clim
