dbb78a0265c23cb05cbe5df2b79eab22b53501fc
Thomas A Caswell <tcaswell@gmail.com>
2020-03-05 22:10:14 -0500

    TST: add the request fixture to check_figures_equal
    If you stacked `pytest.mark.parametrize` with check_figures_equal
    every set of parameters would write to the same file.  This makes
    post-hoc debugging hard and causes intermittent CI failures.

--- lib/matplotlib/testing/decorators.py
+++ lib/matplotlib/testing/decorators.py
@@ -388,15 +388,14 @@ def check_figures_equal(*, extensions=("png", "pdf", "svg"), tol=0):
         _, result_dir = _image_directories(func)
 
         @pytest.mark.parametrize("ext", extensions)
-        def wrapper(*args, ext, **kwargs):
+        def wrapper(*args, ext, request, **kwargs):
+            fn = request.node.name
             try:
                 fig_test = plt.figure("test")
                 fig_ref = plt.figure("reference")
                 func(*args, fig_test=fig_test, fig_ref=fig_ref, **kwargs)
-                test_image_path = result_dir / (func.__name__ + "." + ext)
-                ref_image_path = result_dir / (
-                    func.__name__ + "-expected." + ext
-                )
+                test_image_path = result_dir / (fn + "." + ext)
+                ref_image_path = result_dir / (fn + "-expected." + ext)
                 fig_test.savefig(test_image_path)
                 fig_ref.savefig(ref_image_path)
                 _raise_on_image_difference(
@@ -411,7 +410,9 @@ def check_figures_equal(*, extensions=("png", "pdf", "svg"), tol=0):
             parameters=([param
                          for param in sig.parameters.values()
                          if param.name not in {"fig_test", "fig_ref"}]
-                        + [inspect.Parameter("ext", POSITIONAL_OR_KEYWORD)])
+                        + [inspect.Parameter("ext", POSITIONAL_OR_KEYWORD),
+                           inspect.Parameter("request", POSITIONAL_OR_KEYWORD),
+                          ])
         )
         wrapper.__signature__ = new_sig
 
