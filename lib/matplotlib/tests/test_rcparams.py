e923f7ac55b65c6cd959b85a548c57614b6792a4
MuhammadFarooq1234 <farooq.muhammad41@yahoo.com>
2020-03-08 15:46:28 -0400

    Add test cases for #16583

--- lib/matplotlib/tests/test_rcparams.py
+++ lib/matplotlib/tests/test_rcparams.py
@@ -534,3 +534,48 @@ def test_backend_fallback_headful(tmpdir):
     # The actual backend will depend on what's installed, but at least tkagg is
     # present.
     assert backend.strip().lower() != "agg"
+
+# Test cases for Bug 16583 - matplotlibrc validates some parameters wrongly
+
+def test_xtickalign_validation_rcparams():
+    with pytest.raises(ValueError):
+        mpl.rcParams['xtick.alignment'] = 'top'
+
+@check_figures_equal()
+def test_left_xtick(fig_test, fig_ref):
+    labels = range(0, 100, 10)
+
+    fig_test.subplots()
+    ax1 = fig_test.axes
+    ax1[0].set_xticklabels(labels, ha='left')
+
+    mpl.rcParams['xtick.alignment'] = 'left'
+    fig_ref.subplots()
+    ax2 = fig_ref.axes
+    ax2[0].set_xticklabels(labels)
+
+@check_figures_equal()
+def test_right_xtick(fig_test, fig_ref):
+    labels = range(0, 100, 10)
+
+    fig_test.subplots()
+    ax1 = fig_test.axes
+    ax1[0].set_xticklabels(labels, ha='right')
+
+    mpl.rcParams['xtick.alignment'] = 'right'
+    fig_ref.subplots()
+    ax2 = fig_ref.axes
+    ax2[0].set_xticklabels(labels)
+
+@check_figures_equal()
+def test_center_xtick(fig_test, fig_ref):
+    labels = range(0, 100, 10)
+
+    fig_test.subplots()
+    ax1 = fig_test.axes
+    ax1[0].set_xticklabels(labels, ha='center')
+
+    mpl.rcParams['xtick.alignment'] = 'center'
+    fig_ref.subplots()
+    ax2 = fig_ref.axes
+    ax2[0].set_xticklabels(labels)
