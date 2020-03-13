7c13faebff06bc5e206c1d4bc30e21204aad93a8
Jody Klymak <jklymak@gmail.com>
2020-03-13 13:02:07 -0700

    Backport PR #16745: Allow numbers to set uvc for all arrows in quiver.set_UVC, fixes #16743

--- lib/matplotlib/tests/test_quiver.py
+++ lib/matplotlib/tests/test_quiver.py
@@ -252,3 +252,15 @@ def test_quiverkey_angles():
     # The arrows are only created when the key is drawn
     fig.canvas.draw()
     assert len(qk.verts) == 1
+
+
+def test_quiver_setuvc_numbers():
+    """Check that it is possible to set all arrow UVC to the same numbers"""
+
+    fig, ax = plt.subplots()
+
+    X, Y = np.meshgrid(np.arange(2), np.arange(2))
+    U = V = np.ones_like(X)
+
+    q = ax.quiver(X, Y, U, V)
+    q.set_UVC(0, 1)
