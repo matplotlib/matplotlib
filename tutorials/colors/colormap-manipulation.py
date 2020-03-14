1270fc24a01d77ec8cef3d19d9add444ea196eb1
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2020-03-14 02:05:33 -0400

    STY: Rename some camelCase variables.

--- tutorials/colors/colormap-manipulation.py
+++ tutorials/colors/colormap-manipulation.py
@@ -138,8 +138,8 @@ plot_examples([viridis, newcmp])
 # middle 0.5 of the colormap.  However, we need to interpolate from a larger
 # colormap, otherwise the new colormap will have repeated values.
 
-viridisBig = cm.get_cmap('viridis', 512)
-newcmp = ListedColormap(viridisBig(np.linspace(0.25, 0.75, 256)))
+viridis_big = cm.get_cmap('viridis', 512)
+newcmp = ListedColormap(viridis_big(np.linspace(0.25, 0.75, 256)))
 plot_examples([viridis, newcmp])
 
 ##############################################################################
