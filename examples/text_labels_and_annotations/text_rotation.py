6925bf6050a9702f1709fbe5b7a98927cc38c333
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2020-03-17 03:50:39 -0400

    DOC: Fix calls to Axes.set_xticks with minor ticks.
    These were incorrect conversions of `plt.xticks`.

--- examples/text_labels_and_annotations/text_rotation.py
+++ examples/text_labels_and_annotations/text_rotation.py
@@ -30,6 +30,7 @@ def addtext(ax, props):
     for x in range(0, 5):
         ax.scatter(x + 0.5, 0.5, color='r', alpha=0.5)
     ax.set_yticks([0, .5, 1])
+    ax.set_xticks(np.arange(0, 5.1, 0.5))
     ax.set_xlim(0, 5)
     ax.grid(True)
 
@@ -37,14 +38,12 @@ def addtext(ax, props):
 # the text bounding box
 bbox = {'fc': '0.8', 'pad': 0}
 
-fig, axs = plt.subplots(2, 1)
+fig, axs = plt.subplots(2, 1, sharex=True)
 
 addtext(axs[0], {'ha': 'center', 'va': 'center', 'bbox': bbox})
-axs[0].set_xticks(np.arange(0, 5.1, 0.5), [])
 axs[0].set_ylabel('center / center')
 
 addtext(axs[1], {'ha': 'left', 'va': 'bottom', 'bbox': bbox})
-axs[1].set_xticks(np.arange(0, 5.1, 0.5))
 axs[1].set_ylabel('left / bottom')
 
 plt.show()
