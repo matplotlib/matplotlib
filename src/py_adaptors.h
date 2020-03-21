f1fdca066442a177f609ecbd60059e241b6112d7
Adam Paszke <adam.paszke@gmail.com>
2020-03-21 19:26:35 +0100

    Make PathIterator more generic
    Previously PathIterator has kept vertices and codes as (owned) PyArrayObjects,
    while after this patch is keeps them as strided memory regions. This
    allows to generalize the kinds of objects those iterators can be
    constructed from, opening up opportunities for optimized path
    collections.

--- src/py_adaptors.h
+++ src/py_adaptors.h
@@ -21,6 +21,84 @@ int convert_path(PyObject *obj, void *pathp);
 namespace py
 {
 
+template<typename T, size_t Dim>
+class StridedMemoryBase {
+protected:
+    char *m_data;
+    npy_intp *m_strides;
+
+    explicit StridedMemoryBase(char *data, npy_intp *strides)
+        : m_data(data), m_strides(strides)
+    {
+    }
+
+public:
+    StridedMemoryBase()
+        : m_data(nullptr), m_strides(nullptr)
+    {
+    }
+
+    explicit StridedMemoryBase(PyArrayObject *array)
+    {
+        if (PyArray_NDIM(array) != Dim) {
+            PyErr_SetString(PyExc_ValueError, "Invalid array dimensionality");
+            throw py::exception();
+        }
+        m_data = PyArray_BYTES(array);
+        m_strides = PyArray_STRIDES(array);
+    }
+
+    operator bool() const {
+        return m_data != nullptr;
+    }
+
+    void reset() {
+        m_data = nullptr;
+        m_strides = nullptr;
+    }
+
+    T* data() {
+        return reinterpret_cast<T*>(m_data);
+    }
+};
+
+#define INHERIT_CONSTRUCTORS(CLS, DIM)                                         \
+  CLS() {}                                                                     \
+  explicit CLS(char *data, npy_intp *strides): StridedMemoryBase<T, DIM>(data, strides) {} \
+  explicit CLS(PyArrayObject *array): StridedMemoryBase<T, DIM>(array) {}
+
+
+template<typename T>
+class StridedMemory1D : public StridedMemoryBase<T, 1> {
+public:
+    INHERIT_CONSTRUCTORS(StridedMemory1D, 1)
+    T operator[](size_t idx) const {
+        return *reinterpret_cast<T*>(this->m_data + *this->m_strides * idx);
+    }
+};
+
+template<typename T>
+class StridedMemory2D : public StridedMemoryBase<T, 2> {
+public:
+    INHERIT_CONSTRUCTORS(StridedMemory2D, 2)
+    StridedMemory1D<T> operator[](size_t idx) const {
+        return StridedMemory1D<T>(this->m_data + *this->m_strides * idx,
+                                  this->m_strides + 1);
+    }
+};
+
+template<typename T>
+class StridedMemory3D : public StridedMemoryBase<T, 3> {
+public:
+    INHERIT_CONSTRUCTORS(StridedMemory3D, 3)
+    StridedMemory2D<T> operator[](size_t idx) const {
+        return StridedMemory2D<T>(this->m_data + *this->m_strides * idx,
+                                  this->m_strides + 1);
+    }
+};
+
+#undef INHERIT_CONSTRUCTORS
+
 /************************************************************
  * py::PathIterator acts as a bridge between Numpy and Agg.  Given a
  * pair of Numpy arrays, vertices and codes, it iterates over
@@ -31,12 +109,12 @@ namespace py
  */
 class PathIterator
 {
-    /* We hold references to the Python objects, not just the
-       underlying data arrays, so that Python reference counting
-       can work.
+    /* XXX: This class does not own the data! It should really be used as an
+       iterator, where it is the container that manages the lifetime of the
+       paths.
     */
-    PyArrayObject *m_vertices;
-    PyArrayObject *m_codes;
+    StridedMemory2D<double> m_vertices;
+    StridedMemory1D<uint8_t> m_codes;
 
     unsigned m_iterator;
     unsigned m_total_vertices;
@@ -50,8 +128,8 @@ class PathIterator
 
   public:
     inline PathIterator()
-        : m_vertices(NULL),
-          m_codes(NULL),
+        : m_vertices(),
+          m_codes(),
           m_iterator(0),
           m_total_vertices(0),
           m_should_simplify(false),
@@ -63,14 +141,18 @@ class PathIterator
                         PyObject *codes,
                         bool should_simplify,
                         double simplify_threshold)
-        : m_vertices(NULL), m_codes(NULL), m_iterator(0)
+        : m_vertices(),
+          m_codes(),
+          m_iterator(0)
     {
         if (!set(vertices, codes, should_simplify, simplify_threshold))
             throw py::exception();
     }
 
     inline PathIterator(PyObject *vertices, PyObject *codes)
-        : m_vertices(NULL), m_codes(NULL), m_iterator(0)
+        : m_vertices(),
+          m_codes(),
+          m_iterator(0)
     {
         if (!set(vertices, codes))
             throw py::exception();
@@ -78,10 +160,7 @@ class PathIterator
 
     inline PathIterator(const PathIterator &other)
     {
-        Py_XINCREF(other.m_vertices);
         m_vertices = other.m_vertices;
-
-        Py_XINCREF(other.m_codes);
         m_codes = other.m_codes;
 
         m_iterator = 0;
@@ -91,39 +170,32 @@ class PathIterator
         m_simplify_threshold = other.m_simplify_threshold;
     }
 
-    ~PathIterator()
-    {
-        Py_XDECREF(m_vertices);
-        Py_XDECREF(m_codes);
-    }
-
     inline int
     set(PyObject *vertices, PyObject *codes, bool should_simplify, double simplify_threshold)
     {
         m_should_simplify = should_simplify;
         m_simplify_threshold = simplify_threshold;
 
-        Py_XDECREF(m_vertices);
-        m_vertices = (PyArrayObject *)PyArray_FromObject(vertices, NPY_DOUBLE, 2, 2);
-
-        if (!m_vertices || PyArray_DIM(m_vertices, 1) != 2) {
+        PyArrayObject *vertices_arr =
+            (PyArrayObject *)PyArray_FromObject(vertices, NPY_DOUBLE, 2, 2);
+        if (!vertices_arr || PyArray_DIM(vertices_arr, 1) != 2) {
             PyErr_SetString(PyExc_ValueError, "Invalid vertices array");
             return 0;
         }
-
-        Py_XDECREF(m_codes);
-        m_codes = NULL;
+        m_vertices = StridedMemory2D<double>(vertices_arr);
 
         if (codes != NULL && codes != Py_None) {
-            m_codes = (PyArrayObject *)PyArray_FromObject(codes, NPY_UINT8, 1, 1);
-
-            if (!m_codes || PyArray_DIM(m_codes, 0) != PyArray_DIM(m_vertices, 0)) {
+            PyArrayObject *codes_arr = (PyArrayObject *)PyArray_FromObject(codes, NPY_UINT8, 1, 1);
+            if (!codes_arr || PyArray_DIM(codes_arr, 0) != PyArray_DIM(vertices_arr, 0)) {
                 PyErr_SetString(PyExc_ValueError, "Invalid codes array");
                 return 0;
             }
+            m_codes = StridedMemory1D<uint8_t>(codes_arr);
+        } else {
+            m_codes.reset();
         }
 
-        m_total_vertices = (unsigned)PyArray_DIM(m_vertices, 0);
+        m_total_vertices = (unsigned)PyArray_DIM(vertices_arr, 0);
         m_iterator = 0;
 
         return 1;
@@ -144,12 +216,12 @@ class PathIterator
 
         const size_t idx = m_iterator++;
 
-        char *pair = (char *)PyArray_GETPTR2(m_vertices, idx, 0);
-        *x = *(double *)pair;
-        *y = *(double *)(pair + PyArray_STRIDE(m_vertices, 1));
+        StridedMemory1D<double> vertex = m_vertices[idx];
+        *x = vertex[0];
+        *y = vertex[1];
 
-        if (m_codes != NULL) {
-            return (unsigned)(*(char *)PyArray_GETPTR1(m_codes, idx));
+        if (m_codes) {
+            return static_cast<unsigned>(m_codes[idx]);
         } else {
             return idx == 0 ? agg::path_cmd_move_to : agg::path_cmd_line_to;
         }
@@ -177,12 +249,12 @@ class PathIterator
 
     inline bool has_curves() const
     {
-        return m_codes != NULL;
+        return m_codes;
     }
 
     inline void *get_id()
     {
-        return (void *)m_vertices;
+        return (void *)m_vertices.data();
     }
 };
 
