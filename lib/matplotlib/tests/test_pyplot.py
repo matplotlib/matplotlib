f4d42f70bfa0e6b7e951ac8563450f825b1cc918
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2020-03-28 20:08:25 -0400

    TST: Don't modify actual pyplot file for boilerplate test.
    This should prevent git/versioneer from thinking the working tree is
    dirty if some parallel test happens to run while this one is mid-write.

--- lib/matplotlib/tests/test_pyplot.py
+++ lib/matplotlib/tests/test_pyplot.py
@@ -10,31 +10,32 @@ from matplotlib import pyplot as plt
 from matplotlib.cbook import MatplotlibDeprecationWarning
 
 
-def test_pyplot_up_to_date():
+def test_pyplot_up_to_date(tmpdir):
     gen_script = Path(mpl.__file__).parents[2] / "tools/boilerplate.py"
     if not gen_script.exists():
         pytest.skip("boilerplate.py not found")
     orig_contents = Path(plt.__file__).read_text()
-    try:
-        subprocess.run([sys.executable, str(gen_script)], check=True)
-        new_contents = Path(plt.__file__).read_text()
+    plt_file = tmpdir.join('pyplot.py')
+    plt_file.write_text(orig_contents, 'utf-8')
 
-        if orig_contents != new_contents:
-            diff_msg = '\n'.join(
-                difflib.unified_diff(
-                    orig_contents.split('\n'), new_contents.split('\n'),
-                    fromfile='found pyplot.py',
-                    tofile='expected pyplot.py',
-                    n=0, lineterm=''))
-            pytest.fail(
-                "pyplot.py is not up-to-date. Please run "
-                "'python tools/boilerplate.py' to update pyplot.py. "
-                "This needs to be done from an environment where your "
-                "current working copy is installed (e.g. 'pip install -e'd). "
-                "Here is a diff of unexpected differences:\n%s" % diff_msg
-            )
-    finally:
-        Path(plt.__file__).write_text(orig_contents)
+    subprocess.run([sys.executable, str(gen_script), str(plt_file)],
+                   check=True)
+    new_contents = plt_file.read_text('utf-8')
+
+    if orig_contents != new_contents:
+        diff_msg = '\n'.join(
+            difflib.unified_diff(
+                orig_contents.split('\n'), new_contents.split('\n'),
+                fromfile='found pyplot.py',
+                tofile='expected pyplot.py',
+                n=0, lineterm=''))
+        pytest.fail(
+            "pyplot.py is not up-to-date. Please run "
+            "'python tools/boilerplate.py' to update pyplot.py. "
+            "This needs to be done from an environment where your "
+            "current working copy is installed (e.g. 'pip install -e'd). "
+            "Here is a diff of unexpected differences:\n%s" % diff_msg
+        )
 
 
 def test_copy_docstring_and_deprecators(recwarn):
