d8561bd335ef490095e58310d83960286beab4b7
Sidharth Bansal <20972099+SidharthBansal@users.noreply.github.com>
2020-04-03 22:40:26 +0530

    Divided comment across 2 lines to pass Travis

--- lib/matplotlib/patches.py
+++ lib/matplotlib/patches.py
@@ -720,7 +720,8 @@ class Rectangle(Patch):
         Parameters
         ----------
         xy : (float, float)
-            The rectangle extends from xy[0] to xy[0] + width in x-direction and from xy[1] to xy[1] + height in y-direction.
+            The rectangle extends from xy[0] to xy[0] + width in
+            x-direction and from xy[1] to xy[1] + height in y-direction.
         width : float
             Rectangle width
         height : float
