d99bf41f23f357e2e61279e6aa2fade003e88291
vishalBindal <vishalbindal.06@gmail.com>
2020-04-05 04:00:53 +0530

    Change documentation of get_minor_ticks to list return type

--- lib/matplotlib/axis.py
+++ lib/matplotlib/axis.py
@@ -1334,7 +1334,7 @@ class Axis(martist.Artist):
         return self.majorTicks[:numticks]
 
     def get_minor_ticks(self, numticks=None):
-        """Get the minor tick instances; grow as necessary."""
+        r"""Return the list of minor `.Tick`\s."""
         if numticks is None:
             numticks = len(self.get_minorticklocs())
 
