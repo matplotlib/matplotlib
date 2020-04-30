2c27a02f5206b02c30266ee0f535261d761166b3
Léonard Gérard <leolchat@gmail.com>
2020-04-29 22:13:10 -0700

    flake8 fixes

--- lib/matplotlib/tests/test_dates.py
+++ lib/matplotlib/tests/test_dates.py
@@ -453,7 +453,10 @@ def test_concise_formatter_subsecond():
     locator = mdates.AutoDateLocator(interval_multiples=True)
     formatter = mdates.ConciseDateFormatter(locator)
     year_1996 = 9861.0
-    strings = formatter.format_ticks([year_1996, year_1996 + 500 / mdates.MUSECONDS_PER_DAY, year_1996 + 900 / mdates.MUSECONDS_PER_DAY])
+    strings = formatter.format_ticks([
+        year_1996,
+        year_1996 + 500 / mdates.MUSECONDS_PER_DAY,
+        year_1996 + 900 / mdates.MUSECONDS_PER_DAY])
     assert strings == ['00:00', '00.0005', '00.0009']
 
 
