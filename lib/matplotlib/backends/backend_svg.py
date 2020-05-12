8e5aac9693a22af0eb70ffe48ec0f9c51c9b74b9
Po <22870592+cover-me@users.noreply.github.com>
2020-05-12 11:53:57 -0400

    Update backend_svg.py

--- lib/matplotlib/backends/backend_svg.py
+++ lib/matplotlib/backends/backend_svg.py
@@ -874,7 +874,8 @@ class RendererSVG(RendererBase):
 
             attrib['transform'] = generate_transform(
                 [('matrix', flipped.frozen())])
-            attrib['style'] = 'image-rendering:crisp-edges;image-rendering:pixelated'
+            attrib['style'] = ('image-rendering:crisp-edges;'
+                'image-rendering:pixelated')
             self.writer.element(
                 'image',
                 width=short_float_fmt(w), height=short_float_fmt(h),
