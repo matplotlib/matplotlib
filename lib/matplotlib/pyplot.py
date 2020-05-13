aaacd25b2add603f29c9a0bccec3d17619b9bcb6
Antony Lee <anntzer.lee@gmail.com>
2020-05-13 19:45:36 +0200

    Remove qt4 backends from backend fallback candidates.
    This avoids triggering a deprecation warning whenever backend fallback
    occurs, even if the fallback mechanism was not going to select qt4foo.

--- lib/matplotlib/pyplot.py
+++ lib/matplotlib/pyplot.py
@@ -234,8 +234,7 @@ def switch_backend(newbackend):
         # Don't try to fallback on the cairo-based backends as they each have
         # an additional dependency (pycairo) over the agg-based backend, and
         # are of worse quality.
-        for candidate in [
-                "macosx", "qt5agg", "qt4agg", "gtk3agg", "tkagg", "wxagg"]:
+        for candidate in ["macosx", "qt5agg", "gtk3agg", "tkagg", "wxagg"]:
             try:
                 switch_backend(candidate)
             except ImportError:
