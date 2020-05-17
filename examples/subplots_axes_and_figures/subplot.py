a664c8af6b148790a625dccbc2e8719efe30c534
Asaf Maman <asafmaman@icloud.com>
2020-05-17 09:46:59 +0300

    adding both methods how to generate multiple plots

--- examples/subplots_axes_and_figures/subplot.py
+++ examples/subplots_axes_and_figures/subplot.py
@@ -26,3 +26,34 @@ ax2.set_xlabel('time (s)')
 ax2.set_ylabel('Undamped')
 
 plt.show()
+
+#############################################################################
+#
+#
+# Alternative Method For Creating Multiple Plots
+# """"""""""
+#
+# Subplots can also be generated using `subplot()` as in the following example:
+#
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x1 = np.linspace(0.0, 5.0)
+x2 = np.linspace(0.0, 2.0)
+
+y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
+y2 = np.cos(2 * np.pi * x2)
+
+plt.subplot(2, 1, 1)
+plt.plot(x1, y1, 'o-')
+plt.title('A tale of 2 subplots')
+plt.ylabel('Damped oscillation')
+
+plt.subplot(2, 1, 2)
+plt.plot(x2, y2, '.-')
+plt.xlabel('time (s)')
+plt.ylabel('Undamped')
+
+plt.show()
