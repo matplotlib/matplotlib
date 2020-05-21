f2886583e43fc6e7d41f651033b89aef63a85f0a
Antony Lee <anntzer.lee@gmail.com>
2020-05-21 13:34:11 +0200

    Cleanup RectangleSelector example.
    - Don't use "q" or "a" as keys given that they are already mapped to
      "quit" and "all_axes" (even though the latter is deprecated).
    - Document the interactivity directly as the axes title.
    - No need to plot too many things in the axes; they're irrelevant.

--- lib/matplotlib/widgets.py
+++ lib/matplotlib/widgets.py
@@ -1896,35 +1896,9 @@ class RectangleSelector(_SelectorWidget):
 
     For the cursor to remain responsive you must keep a reference to it.
 
-    Example usage::
-
-        import numpy as np
-        import matplotlib.pyplot as plt
-        from matplotlib.widgets import RectangleSelector
-
-        def onselect(eclick, erelease):
-            "eclick and erelease are matplotlib events at press and release."
-            print('startposition: (%f, %f)' % (eclick.xdata, eclick.ydata))
-            print('endposition  : (%f, %f)' % (erelease.xdata, erelease.ydata))
-            print('used button  : ', eclick.button)
-
-        def toggle_selector(event):
-            print('Key pressed.')
-            if event.key in ['Q', 'q'] and toggle_selector.RS.active:
-                print('RectangleSelector deactivated.')
-                toggle_selector.RS.set_active(False)
-            if event.key in ['A', 'a'] and not toggle_selector.RS.active:
-                print('RectangleSelector activated.')
-                toggle_selector.RS.set_active(True)
-
-        x = np.arange(100.) / 99
-        y = np.sin(x)
-        fig, ax = plt.subplots()
-        ax.plot(x, y)
-
-        toggle_selector.RS = RectangleSelector(ax, onselect, drawtype='line')
-        fig.canvas.mpl_connect('key_press_event', toggle_selector)
-        plt.show()
+    Examples
+    --------
+    :doc:`/gallery/widgets/rectangle_selector`
     """
 
     _shape_klass = Rectangle
