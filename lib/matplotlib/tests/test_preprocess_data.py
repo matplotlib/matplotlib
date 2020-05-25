58a59ed86116619840e05452299dad033beff977
hannah <story645@gmail.com>
2020-05-25 11:02:37 -0400

    reverted double quotes on string

--- lib/matplotlib/tests/test_preprocess_data.py
+++ lib/matplotlib/tests/test_preprocess_data.py
@@ -85,7 +85,7 @@ def test_function_call_with_dict_input(func):
 @pytest.mark.parametrize('func', all_funcs, ids=all_func_ids)
 def test_function_call_with_dict_data(func):
     """Test with dict data -> label comes from the value of 'x' parameter."""
-    data = {'a': [1, 2], 'b': [8, 9], "w": "NOT"}
+    data = {"a": [1, 2], "b": [8, 9], "w": "NOT"}
     assert (func(None, "a", "b", data=data) ==
             "x: [1, 2], y: [8, 9], ls: x, w: xyz, label: b")
     assert (func(None, x="a", y="b", data=data) ==
