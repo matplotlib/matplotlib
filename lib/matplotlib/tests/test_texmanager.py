e930a568285fb9caeadfc3ad181233d6eda013fd
Dag Evensberget <dag.evensberget@gmail.com>
2020-05-30 11:46:41 +1000

    Add test of rc parameter text.latex.textcomp_full

--- lib/matplotlib/tests/test_texmanager.py
+++ lib/matplotlib/tests/test_texmanager.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 from matplotlib.texmanager import TexManager
 
+import pytest
+
 
 def test_fontconfig_preamble():
     """
@@ -16,3 +18,30 @@ def test_fontconfig_preamble():
     font_config2 = tm2.get_font_config()
 
     assert font_config1 != font_config2
+
+
+@pytest.mark.parametrize('textcomp_full, expected_result',
+                         [(True, 'usepackage[full]{textcomp}'),
+                          (False, 'usepackage{textcomp}')])
+def test_textcomp_full(textcomp_full, expected_result):
+    """
+    Addresses issue #9118.
+    See https://github.com/matplotlib/matplotlib/issues/9118.
+
+    Test that the [full] option is set for the textcomp package
+    in _font_preamble if the rcParam text.latex.textcomp_full
+    is set to True, and that the option is not set otherwise.
+    """
+    plt.rcParams['text.usetex'] = True
+    plt.rcParams['text.latex.textcomp_full'] = textcomp_full
+
+    tm = TexManager()
+    tm.get_font_config()  # This also sets tm1._font_preamble
+
+    font_preamble = tm.get_font_preamble()
+    if 'textcomp' not in font_preamble:
+        # Do not test for anything if the textcomp package
+        # is not in the font preamble.
+        return
+
+    assert expected_result in font_preamble
