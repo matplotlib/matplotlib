5a916187e56b1443e112c12750feb3707a33186b
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2020-06-09 19:32:22 -0400

    Backport PR #17560: FIX: do not let no-op monkey patches to renderer leak out
    Merge pull request #17560 from tacaswell/fix_noop_tight_bbox
    
    FIX: do not let no-op monkey patches to renderer leak out
    
     Conflicts:
    	lib/matplotlib/backend_bases.py
              - trivial conflicting imports
              - did not back port other changes to tight box
    	lib/matplotlib/tight_layout.py
              - ended up not backporting any of the changes

--- lib/matplotlib/tests/test_bbox_tight.py
+++ lib/matplotlib/tests/test_bbox_tight.py
@@ -108,3 +108,26 @@ def test_tight_pcolorfast():
     # Previously, the bbox would include the area of the image clipped out by
     # the axes, resulting in a very tall image given the y limits of (0, 0.1).
     assert width > height
+
+
+def test_noop_tight_bbox():
+    from PIL import Image
+    x_size, y_size = (10, 7)
+    dpi = 100
+    # make the figure just the right size up front
+    fig = plt.figure(frameon=False, dpi=dpi, figsize=(x_size/dpi, y_size/dpi))
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    fig.add_axes(ax)
+    ax.set_axis_off()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    data = np.arange(x_size * y_size).reshape(y_size, x_size)
+    ax.imshow(data)
+    out = BytesIO()
+    fig.savefig(out, bbox_inches='tight', pad_inches=0)
+    out.seek(0)
+    im = np.asarray(Image.open(out))
+    assert (im[:, :, 3] == 255).all()
+    assert not (im[:, :, :3] == 255).all()
+    assert im.shape == (7, 10, 4)
