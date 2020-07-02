matplotlib/matplotlib

commit ac018af76cae81054dd27c2d4039dfd5467a1366

Rewrite _reshape_2D to not use ragged ndarrays.
This is a raising a deprecation warning in NumPy 1.19, may go away some
time later, and is not strictly necessary for the implementation.

lib/matplotlib/cbook/__init__.py
@@ -1316,24 +1316,48 @@ def _reshape_2D(X, name):
     Use Fortran ordering to convert ndarrays and lists of iterables to lists of
     1D arrays.
 
-    Lists of iterables are converted by applying `np.asarray` to each of their
-    elements.  1D ndarrays are returned in a singleton list containing them.
-    2D ndarrays are converted to the list of their *columns*.
+    Lists of iterables are converted by applying `np.asanyarray` to each of
+    their elements.  1D ndarrays are returned in a singleton list containing
+    them.  2D ndarrays are converted to the list of their *columns*.
 
     *name* is used to generate the error message for invalid inputs.
     """
-    # Iterate over columns for ndarrays, over rows otherwise.
-    X = np.atleast_1d(X.T if isinstance(X, np.ndarray) else np.asarray(X))
+    # Iterate over columns for ndarrays.
+    if isinstance(X, np.ndarray):
+        X = X.T
+
+        if len(X) == 0:
+            return [[]]
+        elif X.ndim == 1 and np.ndim(X[0]) == 0:
+            # 1D array of scalars: directly return it.
+            return [X]
+        elif X.ndim in [1, 2]:
+            # 2D array, or 1D array of iterables: flatten them first.
+            return [np.reshape(x, -1) for x in X]
+        else:
+            raise ValueError(f'{name} must have 2 or fewer dimensions')
+
+    # Iterate over list of iterables.
     if len(X) == 0:
         return [[]]
-    elif X.ndim == 1 and np.ndim(X[0]) == 0:
+
+    result = []
+    is_1d = True
+    for xi in X:
+        xi = np.asanyarray(xi)
+        nd = np.ndim(xi)
+        if nd > 1:
+            raise ValueError(f'{name} must have 2 or fewer dimensions')
+        elif nd == 1 and len(xi) != 1:
+            is_1d = False
+        result.append(xi.reshape(-1))
+
+    if is_1d:
         # 1D array of scalars: directly return it.
-        return [X]
-    elif X.ndim in [1, 2]:
-        # 2D array, or 1D array of iterables: flatten them first.
-        return [np.reshape(x, -1) for x in X]
+        return [np.reshape(result, -1)]
     else:
-        raise ValueError("{} must have 2 or fewer dimensions".format(name))
+        # 2D array, or 1D array of iterables: use flattened version.
+        return result
 
 
 def violin_stats(X, method, points=100, quantiles=None):

commit 41bf4ea9843f704c3af01484dc1130be7ca6e1c1

FIX: Keep lists of lists of one scalar each 2D in _reshape_2D
This fixes a regression introduced by commit
ac018af76cae81054dd27c2d4039dfd5467a1366.

Closes #17820

lib/matplotlib/cbook/__init__.py
@@ -1372,12 +1372,12 @@ def _reshape_2D(X, name):
     result = []
     is_1d = True
     for xi in X:
+        if isinstance(xi, collections.abc.Iterable):
+            is_1d = False
         xi = np.asanyarray(xi)
         nd = np.ndim(xi)
         if nd > 1:
             raise ValueError(f'{name} must have 2 or fewer dimensions')
-        elif nd == 1 and len(xi) != 1:
-            is_1d = False
         result.append(xi.reshape(-1))
 
     if is_1d:

lib/matplotlib/tests/test_cbook.py
@@ -502,6 +502,14 @@ def test_reshape2d():
     xnew = cbook._reshape_2D(x, 'x')
     assert np.shape(xnew) == (5, 3)
 
+    # Test a list of lists which are all of length 1
+    x = [[1], [2], [3]]
+    xnew = cbook._reshape_2D(x, 'x')
+    assert isinstance(xnew, list)
+    assert isinstance(xnew[0], np.ndarray) and xnew[0].shape == (1,)
+    assert isinstance(xnew[1], np.ndarray) and xnew[1].shape == (1,)
+    assert isinstance(xnew[2], np.ndarray) and xnew[2].shape == (1,)
+
     # Now test with a list of lists with different lengths, which means the
     # array will internally be converted to a 1D object array of lists
     x = [[1, 2, 3], [3, 4], [2]]