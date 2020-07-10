78b4f74c3e9c1500f5370923eef71a1ea5b7bc5e
Forrest <11200528+forrestm@users.noreply.github.com>
2020-07-10 18:38:34 -0500

    Corrected for PEP8

--- examples/shapes_and_collections/hatches_styles.py
+++ examples/shapes_and_collections/hatches_styles.py
@@ -4,7 +4,7 @@ Hatches Styles Demo
 ==========
 
 Hatching (pattern filled polygons) is supported currently in the PS,
-PDF, SVG and Agg backends only. 
+PDF, SVG and Agg backends only.
 """
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,8 +21,8 @@ text_pos = pos + [0.9, -0.5]
 hashes = ['/', '\\', '|', '-', '+', 'x', 'o', 'O', '.', '*']
 
 for h in range(len(hashes)):
-  ax.add_patch(Rectangle(pos[h], 2, 2, fill=False, hatch=hashes[h]))
-  ax.text(text_pos[h][0], text_pos[h][1], hashes[h], fontsize=15)
+    ax.add_patch(Rectangle(pos[h], 2, 2, fill=False, hatch=hashes[h]))
+    ax.text(text_pos[h][0], text_pos[h][1], hashes[h], fontsize=15)
 
 plt.axis('equal')
 plt.axis('off')
