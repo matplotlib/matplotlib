9d4633874ddbf6939d75e0fd02d2a14a8da682f1
Bradley Dice <bdice@bradleydice.com>
2020-07-12 11:56:20 -0500

    Enforce stricter tests for _repr_png_.

--- lib/matplotlib/tests/test_colors.py
+++ lib/matplotlib/tests/test_colors.py
@@ -1,7 +1,9 @@
 import copy
 import itertools
 
+from io import BytesIO
 import numpy as np
+from PIL import Image
 import pytest
 
 from numpy.testing import assert_array_equal, assert_array_almost_equal
@@ -1141,6 +1143,13 @@ def test_repr_png():
     cmap = plt.get_cmap('viridis')
     png = cmap._repr_png_()
     assert len(png) > 0
+    img = Image.open(BytesIO(png))
+    assert img.width > 0
+    assert img.height > 0
+    assert 'Title' in img.text
+    assert 'Description' in img.text
+    assert 'Author' in img.text
+    assert 'Software' in img.text
 
 
 def test_repr_html():
