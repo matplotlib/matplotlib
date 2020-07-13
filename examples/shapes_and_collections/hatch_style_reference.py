0be6c5df6a2f11f822d3765374df9c0376d9c6a8
Forrest <11200528+forrestm@users.noreply.github.com>
2020-07-13 14:44:32 -0500

    Moved title to bottom of plots

--- examples/shapes_and_collections/hatch_style_reference.py
+++ examples/shapes_and_collections/hatch_style_reference.py
@@ -16,7 +16,7 @@ hatches = ['/', '\\', '|', '-', '+', 'x', 'o', 'O', '.', '*']
 
 def hatches_plot(ax, h):
     ax.add_patch(Rectangle((0, 0), 2, 2, fill=False, hatch=h))
-    ax.set_title(f"' {h} '")
+    ax.text(1, -0.5, f"' {h} '", size=15, ha="center")
     ax.axis('equal')
     ax.axis('off')
 
