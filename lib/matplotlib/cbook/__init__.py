1aab9adb3cef05ea6bdd45910edaabe3b8550148
DangoMelon <gerardo_art@me.com>
2020-07-14 12:43:14 -0500

    Use f-strings and more explicit variable names

--- lib/matplotlib/cbook/__init__.py
+++ lib/matplotlib/cbook/__init__.py
@@ -2249,31 +2249,31 @@ def _check_in_list(_values, *, _print_supported_values=True, **kwargs):
     Parameters
     ----------
     _values : iterable
-        Sequence of values to check on
+        Sequence of values to check on.
     _print_supported_values : bool, default: True
-        Whether to print *_values* when raising ValueError
-    **kwargs : dict-like
-        *key, value* pairs as keyword arguments to find in *_values*
+        Whether to print *_values* when raising ValueError.
+    **kwargs : dict
+        *key, value* pairs as keyword arguments to find in *_values*.
 
     Raises
     ------
     ValueError
-        If any *value* in *kwargs* is not found in *_values*
+        If any *value* in *kwargs* is not found in *_values*.
 
     Examples
     --------
     >>> cbook._check_in_list(["foo", "bar"], arg=arg, other_arg=other_arg)
     """
     values = _values
-    for k, v in kwargs.items():
-        if v not in values:
+    for key, val in kwargs.items():
+        if val not in values:
             if _print_supported_values:
                 raise ValueError(
-                    "{!r} is not a valid value for {}; supported values are {}"
-                    .format(v, k, ', '.join(map(repr, values))))
+                    f"{val!r} is not a valid value for {key}; "
+                    f"supported values are {', '.join(map(repr, values))}")
             else:
                 raise ValueError(
-                    "{!r} is not a valid value for {}".format(v, k))
+                    f"{val!r} is not a valid value for {key}")
 
 
 def _check_shape(_shape, **kwargs):
