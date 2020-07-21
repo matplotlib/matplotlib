8b04e35d0ad3c21d13334a95f535e9573bcb90a8
Antony Lee <anntzer.lee@gmail.com>
2020-07-21 19:55:19 +0200

    Fix typo in mathtext_data.
    As can be checked with
    ```
    rcParams["ps.useafm"] = True
    figtext(.5, .5, "$\\Lambda\varphi$")
    savefig("/tmp/test.ps")
    ```
    and inspecting the resulting file.

--- lib/matplotlib/mathtext.py
+++ lib/matplotlib/mathtext.py
@@ -464,12 +464,13 @@ class Fonts:
         self.mathtext_backend.set_canvas_size(
             self.width, self.height, self.depth)
 
-    def render_glyph(self, ox, oy, facename, font_class, sym, fontsize, dpi):
+    @cbook._rename_parameter("3.4", "facename", "font")
+    def render_glyph(self, ox, oy, font, font_class, sym, fontsize, dpi):
         """
         At position (*ox*, *oy*), draw the glyph specified by the remaining
         parameters (see `get_metrics` for their detailed description).
         """
-        info = self._get_info(facename, font_class, sym, fontsize, dpi)
+        info = self._get_info(font, font_class, sym, fontsize, dpi)
         self.used_characters.setdefault(info.font.fname, set()).add(info.num)
         self.mathtext_backend.render_glyph(ox, oy, info)
 
