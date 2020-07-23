3fc463319da0cc338db03c84ba65e5531480ba26
Mary Chris Go <marychrisgo@yahoo.com.ph>
2020-07-23 11:14:41 +0800

    Add purpose of demo, edit consistency of python code

--- examples/misc/rasterization_demo.py
+++ examples/misc/rasterization_demo.py
@@ -2,33 +2,40 @@
 ==================
 Rasterization Demo
 ==================
-
 """
 
-# Rasterization is a method where an image described in a vector graphics format is being converted into a raster image (pixels).  
+# Rasterization is a method where an image described in a vector graphics
+# format is being converted into a raster image (pixels).
+
+
+# This demo is showing how you may want to balance most things such as text
+# and lines being vectorized to get all the benefits of vector graphics, but
+# turning some artists into rasterized images. This method will save
+# computational resources because embedding a 2k by 2k mesh in SVG
+# results in a huge file that takes a long time to both save and load.
+
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Set 'd' to be the scalar 2-D array where the values will be color-mapped
 d = np.arange(100).reshape(10, 10)
-# x, y return 2-D grid coordinates based on the coordinates contained 
+# x, y return 2-D grid coordinates based on the coordinates contained
 x, y = np.meshgrid(np.arange(11), np.arange(11))
 
 theta = 0.25*np.pi
-xx = x*np.cos(theta) - y*np.sin(theta) #rotates x by -theta
-yy = x*np.sin(theta) + y*np.cos(theta) #rotates y by -theta
-
+xx = x*np.cos(theta) - y*np.sin(theta)  # rotates x by -theta
+yy = x*np.sin(theta) + y*np.cos(theta)  # rotates y by -theta
 
 # Plot the rasterized and non-rasterized plot
 fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 
-# Create a pseudocolor non-rastertized plot with a non-regular rectangular grid 
+# Create a pseudocolor non-rastertized plot with a non-regular rectangular grid
 ax1.set_aspect(1)
 ax1.pcolormesh(xx, yy, d)
 ax1.set_title("No Rasterization")
 
-# Create a pseudocolor rastertized plot with a non-regular rectangular grid 
+# Create a pseudocolor rastertized plot with a non-regular rectangular grid
 ax2.set_aspect(1)
 ax2.set_title("Rasterization")
 m = ax2.pcolormesh(xx, yy, d)
@@ -49,7 +56,7 @@ m.set_zorder(-20)
 ax4.text(0.5, 0.5, "Text", alpha=0.2,
          zorder=-15,
          va="center", ha="center", size=50, transform=ax4.transAxes)
-# Set zorder value which artists will be rasterized
+# Set zorder value below which artists will be rasterized
 ax4.set_rasterization_zorder(-10)
 ax4.set_title("Rasterization z$<-10$")
 # ax2.title.set_rasterized(True) # should display a warning
