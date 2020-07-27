be60f79ea705841c50799bf0ee9ea851490bb2aa
Jouni K. Seppänen <jks@iki.fi>
2020-07-27 16:34:29 +0300

    Add a test
    The GNU Free Serif font has a wide variety of characters with which
    we can exercise the linear transforms used in TTF composite glyphs.

--- lib/matplotlib/tests/test_ttconv.py
+++ lib/matplotlib/tests/test_ttconv.py
@@ -15,3 +15,17 @@ def test_truetype_conversion(recwarn):
             font=Path(__file__).with_name("mpltest.ttf"), fontsize=80)
     ax.set_xticks([])
     ax.set_yticks([])
+
+
+@image_comparison(["ttconv_transforms"], extensions=["pdf"])
+def test_ttconv_transforms():
+    matplotlib.rcParams['pdf.fonttype'] = 3
+    fig, ax = plt.subplots()
+    kw = {'font': Path(__file__).with_name("FreeSerifSubset.ttf"), 'fontsize': 14}
+    # characters where Free Serif uses various scales and linear transforms
+    # e.g. the right paren is a reflected left paren
+    ax.text(.1, .1, "parens, FAX, u with two diacritics: ()℻ǘ", **kw)
+    ax.text(.1, .2, "double arrows LURD, single arrows LURD: ⇐⇑⇒⇓←↑→↓", **kw)
+    ax.text(.1, .3, "corner arrows, wreath product: ↴↵≀", **kw)
+    ax.set_xticks([])
+    ax.set_yticks([])
