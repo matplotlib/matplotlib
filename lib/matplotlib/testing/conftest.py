501b30e42f282419bb4993b22b22f44e7d624190
Jouni K. Seppänen <jks@iki.fi>
2020-08-01 18:55:54 +0300

    Filter out just the py23 warning

--- lib/matplotlib/testing/conftest.py
+++ lib/matplotlib/testing/conftest.py
@@ -19,7 +19,8 @@ def pytest_configure(config):
         ("markers", "pytz: Tests that require pytz to be installed."),
         ("markers", "network: Tests that reach out to the network."),
         ("filterwarnings", "error"),
-        #("filterwarnings", "error"),  # fontTools.subset raises a pointless DeprecationWarning
+        ("filterwarnings",
+         "ignore:.*The py23 module has been deprecated:DeprecationWarning"),
     ]:
         config.addinivalue_line(key, value)
 
