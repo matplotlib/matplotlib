8f779f016e0a3bdbe2fd998851db915d2f419a47
Antony Lee <anntzer.lee@gmail.com>
2020-08-02 12:24:41 +0200

    Cleanup menu example.

--- examples/widgets/menu.py
+++ examples/widgets/menu.py
@@ -5,12 +5,12 @@ Menu
 
 """
 import numpy as np
-import matplotlib.colors as colors
-import matplotlib.patches as patches
-import matplotlib.mathtext as mathtext
-import matplotlib.pyplot as plt
 import matplotlib.artist as artist
+import matplotlib.colors as colors
 import matplotlib.image as image
+import matplotlib.mathtext as mathtext
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
 
 
 class ItemProperties:
@@ -20,9 +20,6 @@ class ItemProperties:
         self.labelcolor = labelcolor
         self.bgcolor = bgcolor
         self.alpha = alpha
-
-        self.labelcolor_rgb = colors.to_rgba(labelcolor)[:3]
-        self.bgcolor_rgb = colors.to_rgba(bgcolor)[:3]
 
 
 class MenuItem(artist.Artist):
@@ -37,58 +34,42 @@ class MenuItem(artist.Artist):
         self.set_figure(fig)
         self.labelstr = labelstr
 
-        if props is None:
-            props = ItemProperties()
-
-        if hoverprops is None:
-            hoverprops = ItemProperties()
-
-        self.props = props
-        self.hoverprops = hoverprops
+        self.props = props if props is not None else ItemProperties()
+        self.hoverprops = (
+            hoverprops if hoverprops is not None else ItemProperties())
+        if self.props.fontsize != self.hoverprops.fontsize:
+            raise NotImplementedError(
+                'support for different font sizes not implemented')
 
         self.on_select = on_select
 
         x, self.depth = self.parser.to_mask(
             labelstr, fontsize=props.fontsize, dpi=fig.dpi)
 
-        if props.fontsize != hoverprops.fontsize:
-            raise NotImplementedError(
-                'support for different font sizes not implemented')
-
         self.labelwidth = x.shape[1]
         self.labelheight = x.shape[0]
 
-        self.labelArray = np.zeros((x.shape[0], x.shape[1], 4))
-        self.labelArray[:, :, -1] = x/255.
+        mask = np.zeros((*x.shape, 4))
+        mask[:, :, -1] = x / 255
+        self.label = image.FigureImage(fig, origin='upper', array=mask)
 
-        self.label = image.FigureImage(fig, origin='upper')
-        self.label.set_array(self.labelArray)
-
-        # we'll update these later
-        self.rect = patches.Rectangle((0, 0), 1, 1)
+        self.rect = patches.Rectangle((0, 0), 1, 1)  # Will be updated later.
 
         self.set_hover_props(False)
 
         fig.canvas.mpl_connect('button_release_event', self.check_select)
 
     def check_select(self, event):
-        over, junk = self.rect.contains(event)
+        over, _ = self.rect.contains(event)
         if not over:
             return
-
         if self.on_select is not None:
             self.on_select(self)
 
     def set_extent(self, x, y, w, h):
-        print(x, y, w, h)
-        self.rect.set_x(x)
-        self.rect.set_y(y)
-        self.rect.set_width(w)
-        self.rect.set_height(h)
-
+        self.rect.set(x=x, y=y, width=w, height=h)
         self.label.ox = x + self.padx
-        self.label.oy = y - self.depth + self.pady/2.
-
+        self.label.oy = y - self.depth + self.pady/2
         self.hover = False
 
     def draw(self, renderer):
@@ -96,29 +77,18 @@ class MenuItem(artist.Artist):
         self.label.draw(renderer)
 
     def set_hover_props(self, b):
-        if b:
-            props = self.hoverprops
-        else:
-            props = self.props
-
-        r, g, b = props.labelcolor_rgb
-        self.labelArray[:, :, 0] = r
-        self.labelArray[:, :, 1] = g
-        self.labelArray[:, :, 2] = b
-        self.label.set_array(self.labelArray)
+        props = self.hoverprops if b else self.props
+        self.label.get_array()[..., :3] = colors.to_rgb(props.labelcolor)
         self.rect.set(facecolor=props.bgcolor, alpha=props.alpha)
 
     def set_hover(self, event):
         """
         Update the hover status of event and return whether it was changed.
         """
-        b, junk = self.rect.contains(event)
-
+        b, _ = self.rect.contains(event)
         changed = (b != self.hover)
-
         if changed:
             self.set_hover_props(b)
-
         self.hover = b
         return changed
 
@@ -126,10 +96,8 @@ class MenuItem(artist.Artist):
 class Menu:
     def __init__(self, fig, menuitems):
         self.figure = fig
-        fig.suppressComposite = True
 
         self.menuitems = menuitems
-        self.numitems = len(menuitems)
 
         maxw = max(item.labelwidth for item in menuitems)
         maxh = max(item.labelheight for item in menuitems)
@@ -152,12 +120,8 @@ class Menu:
         fig.canvas.mpl_connect('motion_notify_event', self.on_move)
 
     def on_move(self, event):
-        draw = False
-        for item in self.menuitems:
-            draw = item.set_hover(event)
-            if draw:
-                self.figure.canvas.draw()
-                break
+        if any(item.set_hover(event) for item in self.menuitems):
+            self.figure.canvas.draw()
 
 
 fig = plt.figure()
