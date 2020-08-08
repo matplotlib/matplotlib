6a545caec1b5e75946397587edfc97bc02061a7d
sarthakforwet <sarthakkhandelwal032000@gmail.com>
2020-08-09 02:35:43 +0530

    autoscale_tutorial.py: improved style & formatting of the code

--- tutorials/intermediate/autoscale_tutorial.py
+++ tutorials/intermediate/autoscale_tutorial.py
@@ -1,16 +1,14 @@
 """
 Matplotlib used to recompute autoscaled limits after every plotting (plot(),
 bar(), etc.) call. It now only does so when actually rendering the canvas, or
-when the user queries the Axes limits and that is possible through **autoscale**
-feature.
-This is an improvement over the previous case when user has to manually
-autoscale the Axes(or axis) according to data.
-This particular method is a part of matplotlib.axes.Axes class. It is used to
-scale the Axes(or axis) according to data limits.
-Before autoscale one could have to manually struggle with the Axes to scale
-itself according to data.
-Whenever we use autoscale method the axes(or axis if specified only one axis)
-does recalculate its limits with respect to data.
+when the user queries the Axes limits and that is possible through autoscale
+feature. This is an improvement over the previous case when user has to
+manually autoscale the Axes (or axis) according to the data. This particular
+method is a part of matplotlib.axes.Axes class. It is used to scale the
+Axes (or axis) according to the data limits. Before autoscale one could
+have to manually struggle with the Axes to scale itself according to
+data. Whenever we use autoscale, the Axes (or axis) recalculate its
+limits with respect to the data.
 """
 
 import matplotlib.pyplot as plt
@@ -21,146 +19,144 @@ from matplotlib.collections import EllipseCollection
 
 
 def autoscale():
-    x = np.sin(np.linspace(0,6))
-    fig,ax =plt.subplots(ncols=2)
+    """Experiment autoscale feature by switching it on/off for a curve"""
+    curve = np.sin(np.linspace(0, 6))
+    _, axis = plt.subplots(ncols=2)
 
-    # Firstly we will explicitly enable autoscaling (However it is by-default enabled for pyplot artist.)
-    ax[0].autoscale(True)
-    ax[0].plot(x)
+    # At First we explicitly enable autoscaling (However it is by-default
+    # enabled for pyplot artist.)
+    axis[0].autoscale(True)
+    axis[0].plot(curve)
 
-    # Now we disable it .
-    ax[1].autoscale(False)
-    ax[1].plot(x)
+    # Now let's disable it.
+    axis[1].autoscale(False)
+    axis[1].plot(curve)
 
 
-# Utility function to describe autoscaling feature with Margins.
-def autoscale_and_margins(autoscale=False):
-    t = np.arange(-np.pi/2,(3 * np.pi)/2,0.1)
-    f_t = np.cos(t)
-    fig,ax = plt.subplots(ncols=2)
-    for axis in [ax[0],ax[1]]:
-        axis.plot(t,f_t,color="red")
-        axis.axhline(y=0,color="magenta",alpha=0.7)
-        axis.margins(0.2,0.2)
+def autoscale_and_margins():
+    """Utility function to describe autoscaling feature with Margins."""
+    arr = np.arange(-np.pi / 2, (3 * np.pi) / 2, 0.1)
+    f_t = np.cos(arr)
+    _, axis = plt.subplots(ncols=2)
+    for a_x in [axis[0], axis[1]]:
+        a_x.plot(arr, f_t, color="red")
+        a_x.axhline(y=0, color="magenta", alpha=0.7)
+        a_x.margins(0.2, 0.2)
 
-    ax[1].autoscale(tight=True)    # tight in ax.margins is by default True but unable to autoscale itself when Zoomed in or Zoomed out.
+    axis[1].autoscale(tight=True)
+    # "tight" argument in ax.margins is by default set to True but unable
+    # to automatically autoscale the plot when zoomed-in or zoomed-out.
     plt.tight_layout()
 
 
-
 def autoscale_and_collections():
-    fig,ax = plt.subplots(ncols=2)
-    x = np.arange(10)
-    y = np.arange(15)
-    X, Y = np.meshgrid(x, y)
+    """Function to showcase the relation between autoscale feature and
+    matplotlib's collections objects.
+    """
+    fig, axis = plt.subplots(ncols=2)
+    x_arr = np.arange(10)
+    y_arr = np.arange(15)
+    x_arr, y_arr = np.meshgrid(x_arr, y_arr)
 
-    XY = np.column_stack((X.ravel(), Y.ravel()))
+    x_y = np.column_stack((x_arr.ravel(), y_arr.ravel()))
+    ec_1 = EllipseCollection(
+        10,
+        10,
+        5,
+        units="y",
+        offsets=x_y * 0.5,
+        transOffset=axis[0].transData,
+        cmap="jet",
+    )
+    ec_1.set_array((x_arr * y_arr).ravel())
+    ec_2 = EllipseCollection(
+        10,
+        10,
+        5,
+        units="y",
+        offsets=x_y * 0.5,
+        transOffset=axis[1].transData,
+        cmap="jet",
+    )
+    ec_2.set_array((x_arr * y_arr).ravel())
 
-    ec_1 = EllipseCollection(10, 10, 5, units ='y',
-                          offsets = XY * 0.5,           # when autoscaling is enabled then axis is autoscaled to this offset.
-                          transOffset = ax[0].transData,
-                          cmap ="jet")
-    ec_1.set_array((X * Y).ravel())
-
-    ec_2 = EllipseCollection(10, 10, 5, units ='y',
-                          offsets = XY * 0.5,           # when autoscaling is enabled then axis is autoscaled to this offset.
-                          transOffset = ax[1].transData,
-                          cmap ="jet")
-    ec_2.set_array((X * Y).ravel())
-
-    ax[0].add_collection(ec_1)
-    ax[1].add_collection(ec_2)
-    ax[1].autoscale_view()
+    # when autoscaling is enabled then axis is autoscaled to the value
+    # specified in the offsets argument in the above two commands.
+    axis[0].add_collection(ec_1)
+    axis[1].add_collection(ec_2)
+    axis[1].autoscale_view()
     fig.canvas.draw()
 
 
 def autoscale_and_patches():
-    fig,ax = plt.subplots(ncols=2)
-    vertices = [(0,0),(0,3),(1,0),(0,0)]
-    codes = [Path.MOVETO] + [Path.LINETO]*2 + [Path.MOVETO]
-    vertices = np.array(vertices,float)
-    path_1 = Path(vertices,codes)
-    patches_1 = PathPatch(path_1,facecolor="magenta",alpha=0.7)
+    """Function to showcase relation between autoscale feature and matplotlib's
+    patches objects.
+    """
+    fig, axis = plt.subplots(ncols=2)
+    vertices = [(0, 0), (0, 3), (1, 0), (0, 0)]
+    codes = [Path.MOVETO] + [Path.LINETO] * 2 + [Path.MOVETO]
+    vertices = np.array(vertices, float)
+    path_1 = Path(vertices, codes)
+    patches_1 = PathPatch(path_1, facecolor="magenta", alpha=0.7)
     # matplotlib.patches.Patch is Base class of PathPatch and it
-    # does not support autoscaling.
-    # creating two because re-using of artists not supported.
-    path_2 = Path(vertices,codes)
-    patches_2 = PathPatch(path_2,facecolor="magenta",alpha=0.7)
-    # matplotlib.patches.Patch is Base class of PathPatch and it
-    # does not support autoscaling.
+    # does not support autoscaling by-default.
 
-    ax[0].add_patch(patches_1)
-    ax[1].add_patch(patches_2)
-    ax[1].autoscale()
+    path_2 = Path(vertices, codes)
+    patches_2 = PathPatch(path_2, facecolor="magenta", alpha=0.7)
+
+    # As re-using of the artists is not supported, hence creating two
+    # different Path and PathPatch objects.
+
+    axis[0].add_patch(patches_1)
+    axis[1].add_patch(patches_2)
+    axis[1].autoscale()
     fig.canvas.draw()
 
+
 def autoscale_disable():
-    x = np.arange(10)
+    """Function to explicitly disable autoscale feature"""
+    arr = np.arange(10)
+
     # Disable Autoscaling
     plt.autoscale(False)
+    plt.xlim(0, 2)
+    plt.plot(arr)
 
-    plt.xlim(0,2)
-    plt.plot(x)
-"""
-autoscale
-=============
-There are some cases when we have to explicitly enable or disable autoscaling
-feature and we would see some of those cases in the following tutorial. Lets
-just discuss how we can explicitly enable or disable autoscaling.
-"""
+
+# There are some cases when we have to explicitly enable or disable autoscaling
+# feature and we would see some of those cases later on in this tutorial. Let's
+# just discuss how we can explicitly enable or disable autoscaling:
 
 autoscale()
 
-"""
-autoscale and margins
-=====================
-Whenever we set margins our axes remains invariant of the change caused by
-it.Hence we use autoscaling if we want data to be bound with the axes
-irrespective of the margin set.
-"""
+# Whenever we set margins our axes remains invariant of the change caused by
+# it. Hence we use autoscaling if we want data to be bound with the axes
+# irrespective of the margin set. Let's look at an example to understand this:
 
 autoscale_and_margins()
 
-"""
-Artist with autoscale
-=====================
-Collection and Patch subclasses of Artist class does not support autoscaling by
-default. If one wants to enable autoscaling he have to explicitly enable it. See
-Axes limits in the below plots.
-For further reference on relation of Autoscale feature with Collection Class
-see-
-https://matplotlib.org/3.2.1/api/prev_api_changes/api_changes_3.2.0/behavior.html#autoscaling
-"""
+# Collection and Patch subclasses of Artist class does not support autoscaling
+# by-default. Autoscaling should be explicitly enabled for Collection and Patch
+# objects to support it. See Axes limits in the below plots. For further
+# reference on relation of Autoscale feature with Collection Class see-
+# https://matplotlib.org/3.2.1/api/prev_api_changes/api_changes_3.2.0/behavior.html#autoscaling
 
-"""
-autoscale and collections
-=========================
-"""
+# Let's look at the relation between autoscale feature and Collection object:
 autoscale_and_collections()
 
-"""
-autoscale and patches
-=====================
-"""
+
+# Let's look at the relation between autoscale feature and Patch object:
 autoscale_and_patches()
 
-"""
-Some of the subclasses under Collection class are :
-https://matplotlib.org/3.1.1/_images/inheritance-1d05647d989bf64e3e438a24b19fee19432184da.png
-reference site : https://matplotlib.org/3.1.1/api/collections_api.html
-"""
+# Some of the subclasses under Collection class are :
+# https://matplotlib.org/3.1.1/_images/inheritance-1d05647d989bf64e3e438a24b19fee19432184da.png
+# reference site : https://matplotlib.org/3.1.1/api/collections_api.html. Till
+# now we have seen how we can enable autoscaling. Now lets just discuss under
+# which case we possibly need to disable it:
 
-"""
-Till now we have seen how we can enable autoscaling. Now lets just discuss under
-which case we possibly need to disable it.
-Suppose we want to plot a line at 45 degrees to x and y axes and we want data to
-be shown within a given range out of the whole and hence we have to set limits
-for x and y axes and in that case we have to first disable autoscaling and then
-we can set the limits .
-"""
-"""
-autoscale disable
-=================
-"""
+# Suppose we want to plot a line at 45 degrees to x and y axes and we want to
+# plot the data from a specified range and hence we have to set limits for x
+# and y axes and in that case we have to first disable autoscaling and then
+# we can set the limits:
 
 autoscale_disable()
