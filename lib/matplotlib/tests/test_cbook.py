993176eb0ea743d252338ac2f95a330590e18616
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2020-08-21 21:04:58 -0400

    Capture output of CallbackRegistry exception test.
    If running `pytest -s`, the default exception handler will print out the
    traceback, appearing as if there were an error. But this is exactly what
    this test is checking, so capture output and check it.

--- lib/matplotlib/tests/test_cbook.py
+++ lib/matplotlib/tests/test_cbook.py
@@ -226,16 +226,23 @@ class Test_callback_registry:
                        "callbacks")
 
 
-def test_callbackregistry_default_exception_handler(monkeypatch):
+def test_callbackregistry_default_exception_handler(capsys, monkeypatch):
     cb = cbook.CallbackRegistry()
     cb.connect("foo", lambda: None)
+
     monkeypatch.setattr(
         cbook, "_get_running_interactive_framework", lambda: None)
     with pytest.raises(TypeError):
         cb.process("foo", "argument mismatch")
+    outerr = capsys.readouterr()
+    assert outerr.out == outerr.err == ""
+
     monkeypatch.setattr(
         cbook, "_get_running_interactive_framework", lambda: "not-none")
     cb.process("foo", "argument mismatch")  # No error in that case.
+    outerr = capsys.readouterr()
+    assert outerr.out == ""
+    assert "takes 0 positional arguments but 1 was given" in outerr.err
 
 
 def raising_cb_reg(func):
