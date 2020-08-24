bb7d8a0ccd8f192d6314849e86f06ac297ea2373
SidharthBansal <bansal.sidharth2996@gmail.com>
2020-08-24 09:03:50 +0530

    ENH: Added Pytest Plugin to run image generation

---
+++ lib/matplotlib/testing/generate_images_plugin.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+            "--generate_images",
+             action="store_true",
+             default=False,
+             help="run matplotlib baseline image generation tests"
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--generate_images"):
+        skip_non_matplotlib_baseline_image_generation_tests = pytest.mark.skip(
+            reason="No need to run non image generation tests"
+        )
+        for item in items:
+            if "generate_images" not in item.keywords:
+                item.add_marker(skip_non_matplotlib_baseline_image_generation_tests)
