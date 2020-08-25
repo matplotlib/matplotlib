6c3e5bbe67dd190759b34595eb4db02a9a0b2ad5
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2020-08-26 00:29:03 +0200

    Better error message for unescaped underscores

--- lib/matplotlib/tests/test_texmanager.py
+++ lib/matplotlib/tests/test_texmanager.py
@@ -1,5 +1,12 @@
-import matplotlib.pyplot as plt
+import pytest
+
+from matplotlib import pyplot as plt, checkdep_usetex
 from matplotlib.texmanager import TexManager
+
+
+needs_usetex = pytest.mark.skipif(
+    not checkdep_usetex(True),
+    reason="This test needs a TeX installation")
 
 
 def test_fontconfig_preamble():
@@ -16,3 +23,15 @@ def test_fontconfig_preamble():
     font_config2 = tm2.get_font_config()
 
     assert font_config1 != font_config2
+
+
+@needs_usetex
+def test_usetex_missing_underscore():
+    """
+    Test that failed TeX rendering due to an unescaped underscore has a
+    custom error message.
+    """
+    with pytest.raises(RuntimeError,
+                       match='caused by an unescaped underscore'):
+        plt.text(0, 0, 'foo_bar', usetex=True)
+        plt.draw()  # TeX rendering is done at draw time
