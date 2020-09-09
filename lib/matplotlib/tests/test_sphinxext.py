1d6e939c937971d2e408fe4539653066c9a470a0
ulijh <ujh@posteo.de>
2020-09-09 07:47:49 +0200

    Update lib/matplotlib/tests/test_sphinxext.py
    Fix typo in comment.
    
    Co-authored-by: Elliott Sales de Andrade <quantum.analyst@gmail.com>

--- lib/matplotlib/tests/test_sphinxext.py
+++ lib/matplotlib/tests/test_sphinxext.py
@@ -53,7 +53,7 @@ def test_tinypages(tmpdir):
     assert filecmp.cmp(range_6, html_dir / 'range6.png')
     # check if figure caption made it into html file
     assert b'This is the caption for plot 15.' in html_contents
-    # check if figure caption usin :caption: made it into html file
+    # check if figure caption using :caption: made it into html file
     assert b'Plot 17 uses the caption option.' in html_contents
     # check if figure caption made it into html file
     assert b'This is the caption for plot 18.' in html_contents
