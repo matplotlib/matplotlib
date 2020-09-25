9dc40460fdf963e1ea018327ab290c39696ddbf6
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2020-09-25 19:55:06 -0400

    Support binary comms in nbagg.
    Fixes #4382.

--- lib/matplotlib/backends/backend_nbagg.py
+++ lib/matplotlib/backends/backend_nbagg.py
@@ -197,11 +197,14 @@ class CommSocket:
         self.comm.send({'data': json.dumps(content)})
 
     def send_binary(self, blob):
-        # The comm is ascii, so we always send the image in base64
-        # encoded data URL form.
-        data = b64encode(blob).decode('ascii')
-        data_uri = "data:image/png;base64,{0}".format(data)
-        self.comm.send({'data': data_uri})
+        if self.supports_binary:
+            self.comm.send({'blob': 'image/png'}, buffers=[blob])
+        else:
+            # The comm is ASCII, so we send the image in base64 encoded data
+            # URL form.
+            data = b64encode(blob).decode('ascii')
+            data_uri = "data:image/png;base64,{0}".format(data)
+            self.comm.send({'data': data_uri})
 
     def on_message(self, message):
         # The 'supports_binary' message is relevant to the
