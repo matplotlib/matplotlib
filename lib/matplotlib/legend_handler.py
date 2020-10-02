3418472526f328de231264f0eaa5a08630e0b574
Wojciech Lange <wojtek.lange.gd@gmail.com>
2020-10-02 13:55:03 +0200

    flake8

--- lib/matplotlib/legend_handler.py
+++ lib/matplotlib/legend_handler.py
@@ -770,21 +770,22 @@ class HandlerPolyCollection(HandlerBase):
         p.set_transform(trans)
         return [p]
 
+
 class HandlerFancyArrowPatch(HandlerPatch):
-	    """
-	    Handler for FancyArrowPatch instances.
-	    """
-	    def _create_patch(self, legend, orig_handle, xdescent, ydescent, width,
-	                      height, fontsize):
-	        arrow = FancyArrowPatch([-xdescent,
-	                                 -ydescent + height / 2],
-	                                [-xdescent + width,
-	                                 -ydescent + height / 2],
-	                                mutation_scale=width / 3)
-	        arrow.set_arrowstyle(orig_handle.get_arrowstyle())
-	        return arrow
-	
-	
+    """
+    Handler for FancyArrowPatch instances.
+    """
+    def _create_patch(self, legend, orig_handle, xdescent, ydescent, width,
+                            height, fontsize):
+        arrow = FancyArrowPatch([-xdescent,
+                                    -ydescent + height / 2],
+                                    [-xdescent + width,
+                                     -ydescent + height / 2],
+                                    mutation_scale=width / 3)
+        arrow.set_arrowstyle(orig_handle.get_arrowstyle())
+        return arrow
+
+
 class HandlerAnnotation(HandlerBase):
     """
     Handler for Annotation instances.
@@ -800,26 +801,25 @@ class HandlerAnnotation(HandlerBase):
     Must be of length 2.
     Default is [1,4].
     """
-	
+
     def __init__(self, pad=None, width_ratios=[1, 4], **kwargs):
-	
-	self._pad = pad
+
+        self._pad = pad
         self._width_ratios = width_ratios
-	
-	HandlerBase.__init__(self, **kwargs)
-	
-	def create_artists(self, legend, orig_handle, xdescent,
-	                       ydescent, width, height, fontsize,
-	                       trans,
-                          ):
-	
-	        if orig_handle.arrow_patch is not None:
-	
-	            # Arrow without text
-	
-	            handler = HandlerFancyArrowPatch()
-	            handle = orig_handle.arrow_patch
-	
-	        return handler.create_artists(legend, handle, xdescent,
-	                                      ydescent, width, height,
-	                                      fontsize, trans)
+
+        HandlerBase.__init__(self, **kwargs)
+
+    def create_artists(self, legend, orig_handle, xdescent,
+                           ydescent, width, height, fontsize,
+                           trans):
+
+        if orig_handle.arrow_patch is not None:
+
+            # Arrow without text
+
+            handler = HandlerFancyArrowPatch()
+            handle = orig_handle.arrow_patch
+
+        return handler.create_artists(legend, handle, xdescent,
+                                      ydescent, width, height,
+                                      fontsize, trans)
