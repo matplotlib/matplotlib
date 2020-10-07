ae6305d5d6b585737aad4a3df688cf6871b22a65
liensuitnatsnoc <18105757+grrlic@users.noreply.github.com>
2020-10-07 14:00:13 +0700

    replace backticks and kwargs keyword

--- lib/mpl_toolkits/axes_grid1/anchored_artists.py
+++ lib/mpl_toolkits/axes_grid1/anchored_artists.py
@@ -52,7 +52,7 @@ class AnchoredDrawingArea(AnchoredOffsetbox):
             Font property used as a reference for paddings.
 
         frameon : bool, default: True
-            If True, add a frame and solid background for the artist. The frame's appearance can be changed with the `facecolor` and `edgecolor` kwargs.
+            If True, add a frame and solid background for the artist. The frame's appearance can be changed with the *facecolor* and *edgecolor* keyword arguments.
             If False, possibly allow image with proper transparency.
 
         **kwargs
