bf167113a3da842ecc805b5efd8d94945dbe59be
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2020-10-14 22:56:17 -0400

    Add ResizeObserver polyfill license to our directory.

--- tools/embed_js.py
+++ tools/embed_js.py
@@ -2,16 +2,25 @@
 Script to embed JavaScript dependencies in mpl.js.
 """
 
+from collections import namedtuple
 from pathlib import Path
 import re
+import shutil
 import subprocess
 import sys
 
 
+Package = namedtuple('Package', [
+    # The package to embed, in some form that `npm install` can use.
+    'name',
+    # The path to the source file within the package to embed.
+    'source',
+    # The path to the license file within the package to embed.
+    'license'])
 # The list of packages to embed, in some form that `npm install` can use.
 JAVASCRIPT_PACKAGES = [
     # Polyfill/ponyfill for ResizeObserver.
-    '@jsxtools/resize-observer',
+    Package('@jsxtools/resize-observer', 'index.js', 'LICENSE.md'),
 ]
 # This is the magic line that must exist in mpl.js, after which the embedded
 # JavaScript will be appended.
@@ -27,28 +36,37 @@ def safe_name(name):
     return '_'.join(re.split(r'[@/-]', name)).upper()
 
 
-def gen_embedded_lines(web_backend_path):
-    for pkg in JAVASCRIPT_PACKAGES:
-        index = web_backend_path / 'node_modules' / pkg / 'index.js'
-        if not index.exists():
-            # Exact version should already be saved in package.json, so we use
-            # --no-save here.
-            try:
-                subprocess.run(['npm', 'install', '--no-save', pkg],
-                               cwd=web_backend_path)
-            except FileNotFoundError as err:
-                raise ValueError(
-                    f'npm must be installed to fetch {pkg}') from err
-        name = safe_name(pkg)
-        print('Embedding', index, 'as', name)
-        yield '// prettier-ignore\n'
-        for line in index.read_text().splitlines(keepends=True):
-            line = line.replace('module.exports=function',
-                                f'var {name}=function')
-            yield line
+def prep_package(web_backend_path, pkg):
+    source = web_backend_path / 'node_modules' / pkg.name / pkg.source
+    license = web_backend_path / 'node_modules' / pkg.name / pkg.license
+    if not source.exists():
+        # Exact version should already be saved in package.json, so we use
+        # --no-save here.
+        try:
+            subprocess.run(['npm', 'install', '--no-save', pkg.name],
+                           cwd=web_backend_path)
+        except FileNotFoundError as err:
+            raise ValueError(
+                f'npm must be installed to fetch {pkg.name}') from err
+    if not source.exists():
+        raise ValueError(
+            f'{pkg.name} package is missing source in {pkg.source}')
+    elif not license.exists():
+        raise ValueError(
+            f'{pkg.name} package is missing license in {pkg.license}')
+
+    return source, license
 
 
-def build_mpljs(web_backend_path):
+def gen_embedded_lines(pkg, source):
+    name = safe_name(pkg.name)
+    print('Embedding', source, 'as', name)
+    yield '// prettier-ignore\n'
+    for line in source.read_text().splitlines(keepends=True):
+        yield line.replace('module.exports=function', f'var {name}=function')
+
+
+def build_mpljs(web_backend_path, license_path):
     mpljs_path = web_backend_path / "js/mpl.js"
     mpljs_orig = mpljs_path.read_text().splitlines(keepends=True)
     try:
@@ -60,7 +78,13 @@ def build_mpljs(web_backend_path):
 
     with mpljs_path.open('w') as mpljs:
         mpljs.writelines(mpljs_orig)
-        mpljs.writelines(gen_embedded_lines(web_backend_path))
+
+        for pkg in JAVASCRIPT_PACKAGES:
+            source, license = prep_package(web_backend_path, pkg)
+            mpljs.writelines(gen_embedded_lines(pkg, source))
+
+            shutil.copy(license,
+                        license_path / f'LICENSE{safe_name(pkg.name)}')
 
 
 if __name__ == '__main__':
@@ -70,4 +94,8 @@ if __name__ == '__main__':
     else:
         web_backend_path = (Path(__file__).parent.parent /
                             "lib/matplotlib/backends/web_backend")
-    build_mpljs(web_backend_path)
+    if len(sys.argv) > 2:
+        license_path = Path(sys.argv[2])
+    else:
+        license_path = Path(__file__).parent.parent / "LICENSE"
+    build_mpljs(web_backend_path, license_path)
