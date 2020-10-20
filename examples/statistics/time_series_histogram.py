22a1392e9e344698cb42d48da921a29c25349d24
ecotner <2.71828cotner@gmail.com>
2020-10-20 00:03:46 -0400

    change random noise to random walk, use jet cmap on log color scale plot

--- examples/statistics/time_series_histogram.py
+++ examples/statistics/time_series_histogram.py
@@ -21,18 +21,21 @@ from matplotlib.colors import LogNorm
 
 _, axes = plt.subplots(nrows=3, figsize=(10, 6 * 3))
 
-# Make some data; lots of random noise + small fraction of sine waves
+# Make some data; a 1D random walk + small fraction of sine waves
 num_series = 10000
 num_points = 100
-SNR = 0.05  # Signal to Noise Ratio
+SNR = 0.10  # Signal to Noise Ratio
 x = np.linspace(0, 4 * np.pi, num_points)
-# random noise
-Y = np.random.randn(num_series, num_points)
+# random walk
+Y = np.cumsum(np.random.randn(num_series, num_points), axis=-1)
 # sinusoidal signal
 num_signal = int(round(SNR * num_series))
-phi = (0.25 * np.pi) * np.random.randn(num_signal, 1)
-Y[-num_signal:] = np.sin(x[None, :] - phi) + 0.1 * \
-    np.random.randn(num_signal, num_points)
+phi = (np.pi / 8) * np.random.randn(num_signal, 1)  # small random offest
+Y[-num_signal:] = ((
+    np.sqrt(np.arange(num_points))[None, :]
+    * np.sin(x[None, :] - phi))
+    + 0.05 * np.random.randn(num_signal, num_points)
+)
 
 # Plot it using `plot` and the lowest nonzero value of alpha (1/256).
 # With this view it is extremely difficult to observe the sinusoidal behavior
@@ -43,7 +46,7 @@ for i in range(Y.shape[0]):
     axes[0].plot(x, Y[i], color="C0", alpha=1 / 256)
 toc = time.time()
 axes[0].set_title(
-    r"Standard time series visualization using `plt.plot`")
+    r"Standard time series visualization using line plot")
 print(f"{toc-tic:.2f} sec. elapsed")  # ~4 seconds
 
 
@@ -51,7 +54,7 @@ print(f"{toc-tic:.2f} sec. elapsed")  # ~4 seconds
 # the hidden signal be more visible, but it is also a much quicker procedure.
 tic = time.time()
 # linearly interpolate between the points in each time series
-num_fine = 1000
+num_fine = 400 * 3
 x_fine = np.linspace(x.min(), x.max(), num_fine)
 y_fine = np.zeros((num_series, num_fine))
 for i in range(num_series):
@@ -62,19 +65,21 @@ x_fine = np.matlib.repmat(x_fine, num_series, 1).flatten()
 
 # Plot (x, y) points in 2d histogram with log colorscale
 # It is pretty evident that there is some kind of structure under the noise
-# that has a periodicity of about ~6 and oscillates between +1/-1.
-cmap = copy(plt.cm.Blues)
+cmap = copy(plt.cm.jet)
 cmap.set_bad(cmap(0))
-h, xedges, yedges = np.histogram2d(x_fine, y_fine, bins=[200, 200])
+h, xedges, yedges = np.histogram2d(x_fine, y_fine, bins=[400, 100])
 axes[1].pcolormesh(xedges, yedges, h.T, cmap=cmap, norm=LogNorm())
 axes[1].set_title(
-    r"Alternative time series vis. using `plt.hist2d` and log color scale")
+    r"Alternative time series vis. using 2d histogram and log color scale")
 
-# It is even visible on a linear color scale
-h, xedges, yedges = np.histogram2d(x_fine, y_fine, bins=[200, 200])
-axes[2].pcolormesh(xedges, yedges, h.T, cmap=cmap)
+# Same thing on linear color scale but with different (more visible) cmap
+cmap = copy(plt.cm.Blues)
+cmap.set_bad(cmap(0))
+h, xedges, yedges = np.histogram2d(x_fine, y_fine, bins=[400, 100])
+# tune vmax to make signal more visible
+axes[2].pcolormesh(xedges, yedges, h.T, cmap=cmap, vmax=3e3)
 axes[2].set_title(
-    r"Alternative time series vis. using `plt.hist2d` and linear color scale")
+    r"Alternative time series vis. using 2d histogram and linear color scale")
 toc = time.time()
 print(f"{toc-tic:.2f} sec. elapsed")  # ~1 sec for both plots + interpolation
 
