1a17bc1fff518f25e2e4c6cecc23161fe64e92e3
Jakub Klus <jakub.klus@innovatrics.com>
2020-10-31 17:01:10 +0100

    Condensed  arguments formatting

--- tutorials/intermediate/autoscale.py
+++ tutorials/intermediate/autoscale.py
@@ -186,7 +186,8 @@ print(ax.margins())
 # limits to the figure.
 
 fig, ax = plt.subplots()
-collection = mpl.collections.StarPolygonCollection(5, 0, [250, ],
+collection = mpl.collections.StarPolygonCollection(
+    5, 0, [250, ],  # five point star, zero angle, size 250px
     offsets=np.column_stack([x, y]),  # Set the positions
     transOffset=ax.transData,  # Propagate transformations of the Axes
 )
