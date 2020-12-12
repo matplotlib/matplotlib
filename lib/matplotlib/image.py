1b56d96a1fbe0af2b8e0eec8a2aa071ba792a982
Jaroza727 <jaroza@umich.edu>
2020-12-11 20:47:07 -0500

    added format_cursor_data function to NonUniformImage class

--- lib/matplotlib/image.py
+++ lib/matplotlib/image.py
@@ -1038,7 +1038,6 @@ class AxesImage(_ImageBase):
 
 
 class NonUniformImage(AxesImage):
-    mouseover = False  # This class still needs its own get_cursor_data impl.
 
     def __init__(self, ax, *, interpolation='nearest', **kwargs):
         """
@@ -1189,6 +1188,19 @@ class NonUniformImage(AxesImage):
         if self._A is not None:
             raise RuntimeError('Cannot change colors after loading data')
         super().set_cmap(cmap)
+
+    def get_cursor_data(self, event):
+        # docstring inherited
+        x, y = event.xdata, event.ydata
+        if (x < self._Ax[0] or x > self._Ax[-1] or
+                y < self._Ay[0] or y > self._Ay[-1]):
+            return None
+        j = np.searchsorted(self._Ax, x) - 1
+        i = np.searchsorted(self._Ay, y) - 1
+        try:
+            return self._A[i, j]
+        except IndexError:
+            return None
 
 
 class PcolorImage(AxesImage):
