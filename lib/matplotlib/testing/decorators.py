9b5ecff85553b580ab2036438ebb3f523ce2b63b
David Stansby <dstansby@gmail.com>
2020-12-27 16:47:50 +0000

    Check that no new figures are created in image comparison tests

--- lib/matplotlib/testing/decorators.py
+++ lib/matplotlib/testing/decorators.py
@@ -408,6 +408,12 @@ def check_figures_equal(*, extensions=("png", "pdf", "svg"), tol=0):
     tol : float
         The RMS threshold above which the test is considered failed.
 
+    Raises
+    ------
+    RuntimeError
+        If any new figures are created (and not subsequently closed) inside
+        the test function.
+
     Examples
     --------
     Check that calling `.Axes.plot` with a single argument plots it against
@@ -421,6 +427,7 @@ def check_figures_equal(*, extensions=("png", "pdf", "svg"), tol=0):
     """
     ALLOWED_CHARS = set(string.digits + string.ascii_letters + '_-[]()')
     KEYWORD_ONLY = inspect.Parameter.KEYWORD_ONLY
+
     def decorator(func):
         import pytest
 
@@ -444,7 +451,16 @@ def check_figures_equal(*, extensions=("png", "pdf", "svg"), tol=0):
             try:
                 fig_test = plt.figure("test")
                 fig_ref = plt.figure("reference")
+                # Keep track of number of open figures, to make sure test
+                # doesn't create any new ones
+                n_figs = len(plt.get_fignums())
                 func(*args, fig_test=fig_test, fig_ref=fig_ref, **kwargs)
+                if len(plt.get_fignums()) > n_figs:
+                    raise RuntimeError('Number of open figures changed during '
+                                       'test. Make sure you are plotting to '
+                                       'fig_test or fig_ref, or if this is '
+                                       'deliberate explicitly close the '
+                                       'new figure(s) inside the test.')
                 test_image_path = result_dir / (file_name + "." + ext)
                 ref_image_path = result_dir / (file_name + "-expected." + ext)
                 fig_test.savefig(test_image_path)
