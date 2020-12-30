58d8f5ce4b3ca1a2aa5154c192bec1ebbe1e3c84
Charles <c@harl.es>
2020-12-30 13:54:00 -0800

    Reduced size of long line

--- lib/matplotlib/tests/test_sankey.py
+++ lib/matplotlib/tests/test_sankey.py
@@ -18,5 +18,7 @@ def test_format_using_callable():
     def show_three_decimal_places(value):
         return f'{value:.3f}'
 
-    s = Sankey(flows=[0.25], labels=['First'], orientations=[-1], format=show_three_decimal_places)
+    s = Sankey(flows=[0.25], labels=['First'], orientations=[-1],
+               format=show_three_decimal_places)
+
     assert s.diagrams[0].texts[0].get_text() == 'First\n0.250'
