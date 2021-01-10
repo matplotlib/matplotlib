411bdde424c85f9b77390ef27ea312bbe45a7a09
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-01-10 10:28:47 -0500

    Simplify explanation and remove valstep from example

--- examples/widgets/slider_demo.py
+++ examples/widgets/slider_demo.py
@@ -3,13 +3,10 @@
 Slider
 ======
 
-Using the slider widget to control visual properties of your plot.
-
 In this example, sliders are used to control the frequency and amplitude of
-a sine wave. You can control many continuously-varying properties of your plot
-in this way.
+a sine wave.
 
-For a more detailed example of value snapping see
+For an example of having the slider snap to discrete values see
 :doc:`/gallery/widgets/slider_snap_demo`.
 
 For an example of using a `matplotlib.widgets.RangeSlider` to define a range
@@ -30,7 +27,7 @@ t = np.arange(0.0, 1.0, 0.001)
 init_amplitude = 5
 init_frequency = 3
 
-# Create the figure and the `~.Line2D` that we will manipulate
+# Create the figure and the line that we will manipulate
 fig, ax = plt.subplots()
 line, = plt.plot(t, f(t, init_amplitude, init_frequency), lw=2)
 
@@ -41,7 +38,6 @@ ax.margins(x=0)
 plt.subplots_adjust(left=0.25, bottom=0.25)
 
 # Make a horizontal slider to control the frequency.
-# This slider will snap to discrete values as defind by ``valstep``.
 axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
 freq_slider = Slider(
     ax=axfreq,
@@ -49,7 +45,6 @@ freq_slider = Slider(
     valmin=0.1,
     valmax=30.0,
     valinit=init_amplitude,
-    valstep=5.0
 )
 
 # Make a vertically oriented slider to control the amplitude
@@ -64,11 +59,13 @@ amp_slider = Slider(
 )
 
 
+# The function to be called anytime a slider's value changes
 def update(val):
     line.set_ydata(f(t, amp_slider.val, freq_slider.val))
     fig.canvas.draw_idle()
 
 
+# register the update function with each slider
 freq_slider.on_changed(update)
 amp_slider.on_changed(update)
 
