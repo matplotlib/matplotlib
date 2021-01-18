4e30764eeb3e2f48dceb8093fe8c14bbd69144c9
MahmoudElhalwany <mahmoud.a.elhalwany@gmail.com>
2021-01-18 15:29:30 +0300

    Reduce whitespace in Choosing Colormaps tutorial plots

--- tutorials/colors/colormaps.py
+++ tutorials/colors/colormaps.py
@@ -199,17 +199,16 @@ cmaps['Miscellaneous'] = [
 # First, we'll show the range of each colormap. Note that some seem
 # to change more "quickly" than others.
 
-nrows = max(len(cmap_list) for cmap_category, cmap_list in cmaps.items())
 gradient = np.linspace(0, 1, 256)
 gradient = np.vstack((gradient, gradient))
 
 
 def plot_color_gradients(cmap_category, cmap_list, nrows):
-    fig, axs = plt.subplots(nrows=nrows)
-    fig.subplots_adjust(top=0.95, bottom=0.01, left=0.2, right=0.99)
-    axs[0].set_title(cmap_category + ' colormaps', fontsize=14)
+    fig, axes = plt.subplots(nrows=nrows)
+    fig.subplots_adjust(top=(0.0522*nrows), bottom=0.01, left=0.2, right=0.99)
+    axes[0].set_title(cmap_category + ' colormaps', fontsize=14)
 
-    for ax, name in zip(axs, cmap_list):
+    for ax, name in zip(axes, cmap_list):
         ax.imshow(gradient, aspect='auto', cmap=plt.get_cmap(name))
         pos = list(ax.get_position().bounds)
         x_text = pos[0] - 0.01
@@ -217,12 +216,12 @@ def plot_color_gradients(cmap_category, cmap_list, nrows):
         fig.text(x_text, y_text, name, va='center', ha='right', fontsize=10)
 
     # Turn off *all* ticks & spines, not just the ones with colormaps.
-    for ax in axs:
+    for ax in axes:
         ax.set_axis_off()
 
 
 for cmap_category, cmap_list in cmaps.items():
-    plot_color_gradients(cmap_category, cmap_list, nrows)
+    plot_color_gradients(cmap_category, cmap_list, len(cmap_list))
 
 plt.show()
 
