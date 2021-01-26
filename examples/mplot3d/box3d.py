0eb24d9fa1d6f3136fee16c7e7f01e16a129ec7e
iury simoes-sousa <iuryt@pm.me>
2021-01-26 09:56:06 -0500

    Update box3d.py
    Incorporate pflakes suggestions

--- examples/mplot3d/box3d.py
+++ examples/mplot3d/box3d.py
@@ -18,95 +18,101 @@ import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
 
-#Define dimensions
+# Define dimensions
 Nx, Ny, Nz = 100, 300, 500
 X = xr.DataArray(np.arange(Nx), dims=['X'])
 Y = xr.DataArray(np.arange(Ny), dims=['Y'])
 Z = xr.DataArray(-np.arange(Nz), dims=['Z'])
 
-#Create fake data
-#This code works for da => ZYX.
-#For da => XYZ or YXZ or ZYX you may have to change the plotting part.
+# Create fake data
+# This code works for da => ZYX.
+# For da => XYZ or YXZ or ZYX you may have to change the plotting part.
 # or transpose the data before plotting
 da = xr.DataArray(
     ((X+100)**2 + (Y-20)**2 + 2*Z)/1000+1,
     coords={'X': X, 'Y': Y, 'Z': Z},
     dims=['X', 'Y', 'Z']
-).T   #.T invert from XYZ to ZYX
+).T   # .T invert from XYZ to ZYX
 
 
 vmin = da.min().values
 vmax = da.max().values
-#Key arguments for contour plots
+# Key arguments for contour plots
 kw = {
     'vmin': vmin,
     'vmax': vmax,
-    'levels': np.linspace(vmin,vmax,10),
+    'levels': np.linspace(vmin, vmax, 10),
     'cmap': 'viridis',
 }
 
-#Create a figure with 3D ax
-fig = plt.figure(figsize=(7,4))
+# Create a figure with 3D ax
+fig = plt.figure(figsize=(7, 4))
 ax = fig.add_subplot(111, projection='3d')
 
-#Upper surface--
-#Select the surface at Z=0
+# Upper surface--
+# Select the surface at Z=0
 di = da.sel(Z=0, method='nearest')
-#Create the grid for plotting (required for 3D plots)
+# Create the grid for plotting (required for 3D plots)
 dims = np.meshgrid(di[di.dims[1]].values, di[di.dims[0]].values)
-#Plot surface
-#zdir sets the normal axis and
-#offset is the surface offset at this normal axis
+# Plot surface
+# zdir sets the normal axis and
+# offset is the surface offset at this normal axis
 C = ax.contourf(dims[0], dims[1], di.values, zdir='z', offset=0, **kw)
 # --
 
 
-#South surface--
-#Select the face at Y=0
+# South surface--
+# Select the face at Y=0
 di = da.sel(Y=0)
-#Create the grid for plotting (required for 3D plots)
+# Create the grid for plotting (required for 3D plots)
 dims = np.meshgrid(di[di.dims[1]].values, di[di.dims[0]].values)
-#Plot surface
-#zdir sets the normal axis and
-#offset is the surface offset at this normal axis
-C = ax.contourf(dims[0], di.values, dims[1], zdir='y', offset=di.Y.values, **kw)
+# Plot surface
+# zdir sets the normal axis and
+# offset is the surface offset at this normal axis
+C = ax.contourf(
+    dims[0], di.values, dims[1], zdir='y',
+    offset=di.Y.values, **kw,
+)
 
-#East surface--
-#Select the face at X=X.max()
+# East surface--
+# Select the face at X=X.max()
 di = da.sel(X=da.X.max(), method='nearest')
-#Create the grid for plotting (required for 3D plots)
+# Create the grid for plotting (required for 3D plots)
 dims = np.meshgrid(di[di.dims[1]].values, di[di.dims[0]].values)
-#Plot surface
-#zdir sets the normal axis and
-#offset is the surface offset at this normal axis
-C = ax.contourf(di.values, dims[0], dims[1], zdir='x', offset=di.X.values, **kw)
+# Plot surface
+# zdir sets the normal axis and
+# offset is the surface offset at this normal axis
+C = ax.contourf(
+    di.values, dims[0], dims[1],
+    zdir='x', offset=di.X.values, **kw
+)
 
-#Set limits of the plot from coord limits
+# Set limits of the plot from coord limits
 ax.set(
     xlim=[da.X.min(), da.X.max()],
     ylim=[da.Y.min(), da.Y.max()],
     zlim=[da.Z.min(), da.Z.max()],
 )
 
-color = '0.4' #color of the line of the corners
-#Get xlim,ylim and zlim
-xlim,ylim,zlim = list(map(np.array,[ax.get_xlim(),ax.get_ylim(),ax.get_zlim()]))
-#Plot corners
-ax.plot(xlim*0+xlim[1],ylim,zlim*0,color,linewidth=1,zorder=1e4)
-ax.plot(xlim,ylim*0+ylim[0],zlim*0,color,linewidth=1,zorder=1e4)
-ax.plot(xlim*0+xlim[1],ylim*0+ylim[0],zlim,color,linewidth=1,zorder=1e4)
+color = '0.4'  # color of the line of the corners
+# Get xlim,ylim and zlim
+xlim, ylim, zlim = list(map(np.array, [ax.get_xlim(), ax.get_ylim(), ax.get_zlim()]))
+# Plot corners
+ax.plot(xlim*0+xlim[1], ylim,zlim*0, color, linewidth=1, zorder=1e4)
+ax.plot(xlim, ylim*0+ylim[0], zlim*0, color, linewidth=1, zorder=1e4)
+ax.plot(xlim*0+xlim[1], ylim*0+ylim[0], zlim, color, linewidth=1, zorder=1e4)
 
-#Set labels and zticks
+# Set labels and zticks
 ax.set(
     xlabel='\n X [km]',
     ylabel='\n Y [km]',
     zlabel='\n Z [m]',
-    zticks=[0,-150,-300,-450],
+    zticks=[0, -150, -300, -450],
 )
 
-#Set distance and angle view
+# Set distance and angle view
 ax.view_init(40, -30)
 ax.dist = 11
 
-#Colorbar
+# Colorbar
 fig.colorbar(C, ax=ax, fraction=0.02, pad=0.1, label='Name [units]')
