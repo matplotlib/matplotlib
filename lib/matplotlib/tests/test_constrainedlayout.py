40f78483f7266749eb6007b1c7cf1b55db9ec037
Nora Moseman <nmoseman@post.harvard.edu>
2021-01-26 17:38:11 -0700

    fixed formatting

--- lib/matplotlib/tests/test_constrainedlayout.py
+++ lib/matplotlib/tests/test_constrainedlayout.py
@@ -522,19 +522,22 @@ def test_bbox():
     fig, ax = plt.subplots(constrained_layout=True)
     ax.set_aspect(1.)
 
+
 def test_align_labels():
     """
-    Tests for a bug in which constrained layout and align_ylabels on three
-    unevenly sized subplots, one of whose y tick labels include negative
-    numbers, drives the non-negative subplots' y labels off the edge of the plot
+    Tests for a bug in which constrained layout and align_ylabels on
+    three unevenly sized subplots, one of whose y tick labels include
+    negative numbers, drives the non-negative subplots' y labels off
+    the edge of the plot
 
     """
 
-    data = [0,1]
+    data = [0, 1]
 
     fig, (ax3, ax1, ax2) = plt.subplots(3, 1, constrained_layout=True,
-                                        figsize=(6.4,8),
-                                        gridspec_kw={"height_ratios":(1, 1, 0.7)})
+                                        figsize=(6.4, 8),
+                                        gridspec_kw={"height_ratios": (1, 1,
+                                                                       0.7)})
 
     ax1.plot(data, data)
     ax1.set_ylabel("Label")
@@ -552,5 +555,8 @@ def test_align_labels():
     after_align = (ax1.yaxis.label.get_window_extent(),
                    ax2.yaxis.label.get_window_extent(),
                    ax3.yaxis.label.get_window_extent())
-
-    assert round(after_align[0].x0, 5) == round(after_align[1].x0, 5) == round(after_align[2].x0, 5) > 1
+    #ensure labels are approximately aligned
+    np.testing.assert_allclose([after_align[0].x0, after_align[2].x0],
+                               after_align[1].x0, rtol=0, atol=1e-05)
+    #ensure labels do not go off the edge
+    assert after_align[0].x0 >= 1
