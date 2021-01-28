2ab0e05399d1d156283166f97e84304adff5113b
Nora Moseman <nmoseman@post.harvard.edu>
2021-01-28 10:50:14 -0700

    do not call plot(), set explicit positive y_lim for subplots, fix formatting

--- lib/matplotlib/tests/test_constrainedlayout.py
+++ lib/matplotlib/tests/test_constrainedlayout.py
@@ -529,34 +529,29 @@ def test_align_labels():
     three unevenly sized subplots, one of whose y tick labels include
     negative numbers, drives the non-negative subplots' y labels off
     the edge of the plot
-
     """
-
-    data = [0, 1]
-
     fig, (ax3, ax1, ax2) = plt.subplots(3, 1, constrained_layout=True,
                                         figsize=(6.4, 8),
                                         gridspec_kw={"height_ratios": (1, 1,
                                                                        0.7)})
 
-    ax1.plot(data, data)
+    ax1.set_ylim(0, 1)
     ax1.set_ylabel("Label")
 
-    ax2.plot(data, data)
     ax2.set_ylim(-1.5, 1.5)
     ax2.set_ylabel("Label")
 
-    ax3.plot(data, data)
+    ax3.set_ylim(0, 1)
     ax3.set_ylabel("Label")
 
     fig.align_ylabels(axs=(ax3, ax1, ax2))
 
     fig.canvas.draw()
-    after_align = (ax1.yaxis.label.get_window_extent(),
+    after_align = [ax1.yaxis.label.get_window_extent(),
                    ax2.yaxis.label.get_window_extent(),
-                   ax3.yaxis.label.get_window_extent())
-    #ensure labels are approximately aligned
+                   ax3.yaxis.label.get_window_extent()]
+    # ensure labels are approximately aligned
     np.testing.assert_allclose([after_align[0].x0, after_align[2].x0],
                                after_align[1].x0, rtol=0, atol=1e-05)
-    #ensure labels do not go off the edge
+    # ensure labels do not go off the edge
     assert after_align[0].x0 >= 1
