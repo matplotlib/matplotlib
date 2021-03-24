6924c26ebf8b14bc92bbf2a5c7df5648dc8d5409
Jody Klymak <jklymak@gmail.com>
2021-03-24 14:11:55 -0700

    FIX: fix taking suptitle (and friends) out of get_tightbbox

--- lib/matplotlib/tests/test_bbox_tight.py
+++ lib/matplotlib/tests/test_bbox_tight.py
@@ -61,6 +61,14 @@ def test_bbox_inches_tight_suptile_legend():
     plt.xlabel('X axis')
 
 
+@image_comparison(['bbox_inches_tight_suptile_non_default.png'],
+                  remove_text=False, savefig_kwarg={'bbox_inches': 'tight'},
+                  tol=0.1)  # large tolerance because only testing clipping.
+def test_bbox_inches_tight_suptitle_non_default():
+    fig, ax = plt.subplots()
+    fig.suptitle('Booo', x=0.5, y=1.1)
+
+
 @image_comparison(['bbox_inches_tight_clipping'],
                   remove_text=True, savefig_kwarg={'bbox_inches': 'tight'})
 def test_bbox_inches_tight_clipping():
