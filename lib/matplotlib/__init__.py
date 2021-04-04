4b417052aabc8ba225c654a15234fbff0d3d78e5
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2021-04-04 22:41:52 +0200

    Cleanup docstrings related to interactive mode

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -1137,7 +1137,14 @@ def interactive(b):
 
 
 def is_interactive():
-    """Return whether to redraw after every plotting command."""
+    """
+    Return whether to redraw after every plotting command.
+
+    .. note::
+
+        This function is only intended for use in backends. End users should
+        use `.pyplot.isinteractive` instead.
+    """
     return rcParams['interactive']
 
 
