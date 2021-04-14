9418774423c291411c76dc41ef6b9d1ade8c95a7
richardsheridan <richard.sheridan@gmail.com>
2021-04-14 14:39:24 -0400

    linting

--- lib/matplotlib/backends/_backend_tk.py
+++ lib/matplotlib/backends/_backend_tk.py
@@ -362,9 +362,11 @@ class FigureCanvasTk(FigureCanvasBase):
         if timeout > 0:
             milliseconds = int(1000 * timeout)
             if milliseconds > 0:
-                self._event_loop_id = self._tkcanvas.after(milliseconds, self.stop_event_loop)
+                self._event_loop_id = self._tkcanvas.after(
+                    milliseconds, self.stop_event_loop)
             else:
-                self._event_loop_id = self._tkcanvas.after_idle(self.stop_event_loop)
+                self._event_loop_id = self._tkcanvas.after_idle(
+                    self.stop_event_loop)
         self._master.mainloop()
 
     def stop_event_loop(self):
