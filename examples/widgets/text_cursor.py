e989183725dc3e0091ea8ea74a6701ea730b32ea
Joschua Conrad <joschua.conrad@uni-ulm.de>
2021-04-18 20:21:00 +0200

    Docstring and code format changes
    The hint on the missing cursor in example's preview is now a note in the
    docstring.
    FIxed format of multiline examples of Cursor instantiation

--- examples/widgets/text_cursor.py
+++ examples/widgets/text_cursor.py
@@ -9,6 +9,11 @@ is close to the mouse pointer.
 The new cursor inherits from `~matplotlib.widgets.Cursor` and demonstrates the
 creation of new widgets and their event callbacks.
 
+.. note::
+    The figure related to this example does not show the cursor, because that
+    figure is automatically created in a build queue, where the first mouse
+    movement, which triggers the cursor creation, is missing.
+
 """
 from matplotlib.widgets import Cursor
 import numpy as np
@@ -265,6 +270,7 @@ ax = fig.add_subplot(111, facecolor='#FFFFCC')
 
 # A linearly growing x vector.
 x = np.linspace(-5, 5, 1000)
+
 # A non biunique function. dataaxis='y' will cause trouble.
 y = (x ** 2)
 
@@ -284,7 +290,9 @@ cursor = TextCursor(
     line=lin[0],
     numberformat="{0:.2f}\n{1:.2f}",
     dataaxis='x', offset=[10, 10],
-    textprops={'color': 'blue', 'fontweight': 'bold'}, ax=ax, useblit=True,
+    textprops={'color': 'blue', 'fontweight': 'bold'},
+    ax=ax,
+    useblit=True,
     color='red',
     linewidth=2)
 
@@ -293,27 +301,13 @@ cursor = TextCursor(
 # instead of vice versa. Hover you cursor to y=4. There are two x values
 # producing this y value: -2 and 2. The function is only unique,
 # but not biunique. Only one value is shown in the text.
-# cursor = TextCursor(line=lin[0], numberformat="{0:.2f}\n{1:.2f}",
-#    dataaxis='y', offset=[10, 10],
-#    textprops={'color':'blue', 'fontweight':'bold'}, ax=ax, useblit=True,
-#    color='red', linewidth=2)
-
-# In the gallery picture, we cannot see the cursor.
-# The cursor is displayed after the first mouse move event.
-# There is no mouse move when using automatic scripts forgenerating the
-# documentation. The onmove event needs to occur after the plt.show() command,
-# because this one creates a fresh and clean canvas. Without the cursor or
-# its text. And after the plt.show(), we cannot execute more instructions.
-# The following code draws the figure (without showing it) and calls the mouse
-# event manually. The resulting figure is saved to a file. But it cannot be
-# shown, because this first clears the canvas and therefore removes the cursor.
-# from matplotlib.backend_bases import MouseEvent
-# location = np.array([0, 10])
-# location = ax.transData.transform(location)
-# event = MouseEvent(name='motion_notify_event', button=None, key=None,
-#    x=location[0], y=location[1], canvas=ax.figure.canvas)
-# ax.figure.canvas.draw()
-# cursor.onmove(event)
-# fig.savefig('text_cursor.png')
+# cursor = TextCursor(
+#     line=lin[0],
+#     numberformat="{0:.2f}\n{1:.2f}",
+#     dataaxis='y', offset=[10, 10],
+#     textprops={'color': 'blue', 'fontweight': 'bold'},
+#     ax=ax,
+#     useblit=True,
+#     color='red', linewidth=2)
 
 plt.show()
