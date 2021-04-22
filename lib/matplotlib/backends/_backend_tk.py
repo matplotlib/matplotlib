559c095b82c5916f9b09f34a0b132f144ad54b74
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-04-22 01:14:21 -0400

    Set Windows DPI awareness to best possible setting.

--- lib/matplotlib/backends/_backend_tk.py
+++ lib/matplotlib/backends/_backend_tk.py
@@ -866,7 +866,7 @@ class _BackendTk(_Backend):
         with _restore_foreground_window_at_end():
             if cbook._get_running_interactive_framework() is None:
                 cbook._setup_new_guiapp()
-                _c_internal_utils.Win32_SetDpiAwareness()
+                _c_internal_utils.Win32_SetProcessDpiAwareness_max()
             window = tk.Tk(className="matplotlib")
             window.withdraw()
 
