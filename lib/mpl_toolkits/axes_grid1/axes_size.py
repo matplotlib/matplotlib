eb12b029ffe2f110540a4338684d1a729d1ddfc5
Antony Lee <anntzer.lee@gmail.com>
2021-04-25 13:40:45 +0200

    Document, test, and simplify impl. of auto_adjustable_area.
    Document behavior of auto_adjustable_area, and slightly modernize the
    example.
    
    Simplify its implementation: `Padded` is just size addition and
    `GetExtentHelper` and `SizeFromFunc` can reasonably be fused into a
    single class; none of them are used anywhere else, so just deprecate
    them as public APIs.
    
    Add a test.

--- lib/mpl_toolkits/axes_grid1/axes_size.py
+++ lib/mpl_toolkits/axes_grid1/axes_size.py
@@ -204,6 +204,7 @@ class Fraction(_Base):
             return rel_size, abs_size
 
 
+@_api.deprecated("3.6", alternative="size + pad")
 class Padded(_Base):
     """
     Return a instance where the absolute part of *size* is
@@ -238,6 +239,7 @@ def from_any(size, fraction_ref=None):
     raise ValueError("Unknown format")
 
 
+@_api.deprecated("3.6")
 class SizeFromFunc(_Base):
     def __init__(self, func):
         self._func = func
@@ -252,6 +254,7 @@ class SizeFromFunc(_Base):
         return rel_size, abs_size
 
 
+@_api.deprecated("3.6")
 class GetExtentHelper:
     _get_func_map = {
         "left":   lambda self, axes_bbox: axes_bbox.xmin - self.xmin,
@@ -271,3 +274,31 @@ class GetExtentHelper:
                        ax.bbox)
               for ax in self._ax_list]
         return max(vl)
+
+
+class _AxesDecorationsSize(_Base):
+    """
+    Fixed size, corresponding to the size of decorations on a given Axes side.
+    """
+
+    _get_size_map = {
+        "left":   lambda tight_bb, axes_bb: axes_bb.xmin - tight_bb.xmin,
+        "right":  lambda tight_bb, axes_bb: tight_bb.xmax - axes_bb.xmax,
+        "bottom": lambda tight_bb, axes_bb: axes_bb.ymin - tight_bb.ymin,
+        "top":    lambda tight_bb, axes_bb: tight_bb.ymax - axes_bb.ymax,
+    }
+
+    def __init__(self, ax, direction):
+        self._get_size = _api.check_getitem(
+            self._get_size_map, direction=direction)
+        self._ax_list = [ax] if isinstance(ax, Axes) else ax
+
+    def get_size(self, renderer):
+        sz = max([
+            self._get_size(ax.get_tightbbox(renderer, call_axes_locator=False),
+                           ax.bbox)
+            for ax in self._ax_list])
+        dpi = renderer.points_to_pixels(72)
+        abs_size = sz / dpi
+        rel_size = 0
+        return rel_size, abs_size
