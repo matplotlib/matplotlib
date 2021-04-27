matplotlib/matplotlib

commit f93091eb50b6aae820e25d1836def5a3896b65a9

Simplified search for plot datat index matching mouse cursor position
Co-authored-by: Elliott Sales de Andrade <quantum.analyst@gmail.com>

examples/widgets/annotated_cursor.py
@@ -240,22 +240,16 @@ class AnnotatedCursor(Cursor):
             raise ValueError(f"The data axis specifier {self.dataaxis} should "
                              f"be 'x' or 'y'")
 
-        # If position is valid
-        if pos is not None:
-            # And in valid plot data range
-            if pos >= lim[0] and pos <= lim[-1]:
-                # Convert given positon to numpy array,
-                # so numpy function can be used.
-                findme = np.array([pos])
-                # Find closest x value in sorted x vector.
-                # This is the code line,
-                # which requires the plotted data to be sorted.
-                index = np.searchsorted(data, findme)
-                # Return none, if this index is out of range.
-                if (index < 0) or (index >= len(data)):
-                    return None
-                # Return plot point as tuple.
-                return (xdata[index][0], ydata[index][0])
+        # If position is valid and in valid plot data range.
+        if pos is not None and lim[0] <= pos <= lim[-1]:
+            # Find closest x value in sorted x vector.
+            # This requires the plotted data to be sorted.
+            index = np.searchsorted(data, pos)
+            # Return none, if this index is out of range.
+            if index < 0 or index >= len(data):
+                return None
+            # Return plot point as tuple.
+            return (xdata[index][0], ydata[index][0])
 
         # Return none if there is no good related point for this x position.
         return None

commit d027645ba81649eccaa127a739235cae5efc8c0a

Fixed bug introduced with f93091eb50b6aae820e25d1836def5a3896b65a9

examples/widgets/annotated_cursor.py
@@ -250,7 +250,7 @@ class AnnotatedCursor(Cursor):
             if index < 0 or index >= len(data):
                 return None
             # Return plot point as tuple.
-            return (xdata[index][0], ydata[index][0])
+            return (xdata[index], ydata[index])
 
         # Return none if there is no good related point for this x position.
         return None