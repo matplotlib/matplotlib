af1ce2f0c577dcfb26a0fdb570d9044b1c42e565
ain-soph <ain-soph@live.com>
2021-04-28 17:10:44 -0400

    fix argument order issue

--- lib/matplotlib/text.py
+++ lib/matplotlib/text.py
@@ -176,9 +176,10 @@ class Text(Artist):
         kwargs = dict(kwargs)
         sentinel = object()  # bbox can be None, so use another sentinel.
         # Update fontproperties first, as it has lowest priority.
-        fontproperties = kwargs.pop("fontproperties", sentinel)
-        if fontproperties is not sentinel:
-            self.set_fontproperties(fontproperties)
+        for alias in ["fontproperties", "font", "font_properties"]:
+            fontproperties = kwargs.pop(alias, sentinel)
+            if fontproperties is not sentinel:
+                self.set_fontproperties(fontproperties)
         # Update bbox last, as it depends on font properties.
         bbox = kwargs.pop("bbox", sentinel)
         super().update(kwargs)
