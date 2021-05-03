f2cb087ada044955428bfe643347e89c387ae5b3
Sacha Jungerman <jungerm2@illinois.edu>
2021-05-03 10:50:58 -0500

    add svgfuncanim and tests

--- lib/matplotlib/animation.py
+++ lib/matplotlib/animation.py
@@ -21,7 +21,7 @@
 import abc
 import base64
 import contextlib
-from io import BytesIO, TextIOWrapper
+from io import BytesIO, TextIOWrapper, StringIO
 import itertools
 import logging
 from pathlib import Path
@@ -31,13 +31,17 @@ import sys
 from tempfile import TemporaryDirectory
 import uuid
 import warnings
+from html import unescape
+from xml.dom import minidom
+from functools import lru_cache
 
 import numpy as np
 from PIL import Image
 
 import matplotlib as mpl
 from matplotlib._animation_data import (
-    DISPLAY_TEMPLATE, INCLUDED_FRAMES, JS_INCLUDE, STYLE_INCLUDE)
+    DISPLAY_TEMPLATE, INCLUDED_FRAMES, JS_INCLUDE, STYLE_INCLUDE, SVG_DISPLAY_TEMPLATE, SVG_JS_INCLUDE)
+from matplotlib.backends.backend_svg import MixedModeRenderer, RendererSVG, XMLWriter
 from matplotlib import _api, cbook
 import matplotlib.colors as mcolors
 
@@ -54,6 +58,13 @@ subprocess_creation_flags = (
 # * Wrap x264 API:
 
 # (https://stackoverflow.com/q/2940671/)
+
+
+def get_all_children(artist):
+    if isinstance(artist, mpl.artist.Artist):
+        for child in artist.get_children():
+            yield from get_all_children(child)
+        yield artist
 
 
 def adjusted_figsize(w, h, dpi, n):
@@ -1798,3 +1809,357 @@ def _validate_grabframe_kwargs(savefig_kwargs):
             raise TypeError(
                 f"grab_frame got an unexpected keyword argument {k!r}"
             )
+
+class SVGFuncAnimation:
+    """
+    Makes an SVG animation by repeatedly calling a function *func* which return modified artists.
+
+    Parameters
+    ----------
+    fig : `~matplotlib.figure.Figure`
+        The figure object used to get needed events, such as draw or resize.
+
+    func : callable
+        The function to call at each frame.  The first argument will
+        be the next value in *frames*.   Any additional positional
+        arguments can be supplied via the *fargs* parameter.
+
+        The required signature is::
+
+            def func(frame, *fargs) -> iterable_of_artists
+
+        *func* must return an iterable of all artists that were modified or
+        created. This information is used by the blitting algorithm to determine
+        which parts of the figure have to be updated.
+
+    frames : iterable, int, generator function, or None, optional
+        Source of data to pass *func* and each frame of the animation
+
+        - If an iterable, then simply use the values provided.  If the
+          iterable has a length, it will override the *save_count* kwarg.
+
+        - If an integer, then equivalent to passing ``range(frames)``
+
+        - If a generator function, then must have the signature::
+
+             def gen_function() -> obj
+
+        - If *None*, then equivalent to passing ``itertools.count``.
+
+        In all of these cases, the values in *frames* is simply passed through
+        to the user-supplied *func* and thus can be of any type.
+
+    init_func : callable, optional
+        A function used to draw a clear frame. If not given, the results of
+        drawing from the first item in the frames sequence will be used. This
+        function will be called once before the first frame.
+
+        The required signature is::
+
+            def init_func() -> iterable_of_artists
+
+        *init_func* must return an iterable of artists to be re-drawn. This
+        information is used by the blitting algorithm to determine which parts
+        of the figure have to be updated.
+
+    fargs : tuple or None, optional
+        Additional arguments to pass to each call to *func*.
+
+    fkwargs : dictionary or None, optional
+        Additional keyword arguments to pass to each call to *func*.
+
+    default_mode: one of 'loop', 'once', 'reflect'. default: 'loop'
+        Specifies the default end-of-animation behavior.
+
+    save_count : int, default: 100
+        Fallback for the number of values from *frames* to cache. This is
+        only used if the number of frames cannot be inferred from *frames*,
+        i.e. when it's an iterator without length or a generator.
+
+    interval : int, default: 200
+        Delay between frames in milliseconds.
+    """
+    def __init__(
+        self,
+        fig,
+        func,
+        frames,
+        init_func=None,
+        fargs=None,
+        fkwargs=None,
+        default_mode="loop",
+        save_count=100,
+        interval=200,
+        embed_limit=None,
+        blit=True,
+    ):
+        self._fig = fig
+        self._func = func
+        self._init_func = init_func
+        self._args = fargs if fargs else ()
+        self._kwargs = fkwargs if fkwargs else {}
+        self._default_mode = default_mode.lower()
+        _api.check_in_list(['loop', 'once', 'reflect'], default_mode=self._default_mode)
+        self._save_count = save_count
+        self._interval = interval
+        self._blit = blit
+
+        self._total_bytes = 0
+        self._html_representation = ""
+        self._base_document = None
+        self._embedded_frames = []
+        self._vector_renderer = None
+        self._renderer = None
+
+        if not self._blit:
+            raise NotImplementedError(
+                "SVGFuncAnimation requires the provided animation "
+                "function to return the artists it has changed, blitting "
+                "must be enabled."
+            )
+
+        # Save embed limit, which is given in MB
+        if embed_limit is None:
+            self._bytes_limit = mpl.rcParams['animation.embed_limit']
+        else:
+            self._bytes_limit = embed_limit
+        # Convert from MB to bytes
+        self._bytes_limit *= 1024 * 1024
+
+        if frames is None:
+            self._iter_gen = lambda: iter(range(self._save_count))
+        elif callable(frames):
+            self._iter_gen = frames
+        elif np.iterable(frames):
+            self._iter_gen = lambda: iter(frames)
+            if hasattr(frames, '__len__'):
+                self._save_count = len(frames)
+        else:
+            self._iter_gen = lambda: iter(range(frames))
+            self._save_count = frames
+
+    @staticmethod
+    def _find_by_attr(dom, value, attr="id", return_child=True):
+        # this could be done better with a more advanced XML parser but has been
+        # done like so to minimize external dependencies. ElementTree re-writes
+        # and changes the namespaces so we use minidom instead.
+        for index, child in enumerate(dom.childNodes):
+            if isinstance(child, minidom.Element):
+                if child.getAttribute(attr) == value:
+                    if return_child:
+                        return child
+                    return index, dom
+                else:
+                    retval = SVGFuncAnimation._find_by_attr(
+                        child, value, attr=attr, return_child=return_child
+                    )
+                    if retval:
+                        return retval
+
+    def _validate_artists(self, artists, name="animation function", set_animated=False):
+        # Both `_init_func` and `_func` should return an iterable of artists
+        # if blit is True. Otherwise the return value is not used.
+        if self._blit:
+            err = RuntimeError(
+                f"The {name} must return a sequence of Artist objects."
+            )
+            try:
+                # check if a sequence
+                iter(artists)
+            except TypeError:
+                raise err from None
+
+            # check each item if is artist
+            for i in artists:
+                if not isinstance(i, mpl.artist.Artist):
+                    raise err
+
+            if set_animated:
+                for a in artists:
+                    a.set_animated(self._blit)
+
+            if not artists:
+                raise err
+
+            return sorted(artists, key=lambda x: x.get_zorder())
+        return []
+
+    @lru_cache
+    def _grab_frames(self):
+        # Clear previous data
+        self._base_document = None
+        self._embedded_frames = []
+        self._vector_renderer = None
+        self._renderer = None
+
+        with StringIO() as f:
+            # Init figure by adding all artists returned by init_func to the figure
+            # And marking them as visible and not animated. This makes sure they get
+            # drawn in the first frame. We later mark them as animated for better blitting.
+            if self._init_func:
+                init_artists = self._init_func()
+                init_artists = self._validate_artists(
+                    init_artists, name="init_func", set_animated=False
+                )
+                for artist in init_artists:
+                    self._fig.add_artist(artist)
+                    artist.set_animated(False)
+                    artist.set_visible(True)
+            else:
+                init_artists = []
+
+            # Set the gid of every artist to a uuid, the idea here is that
+            # when an artist is drawn in SVG it will be encased in a group with
+            # an id equal to the artist's gid and the gid of an artist doesn't
+            # change when the artist's data changes.
+            for artist in get_all_children(self._fig):
+                artist.set_gid(f"{artist.__class__.__name__}_{uuid.uuid4().hex}")
+
+            # Now we can save the initial figure, without finalizing it's renderer.
+            # This keeps the renderer._defs from being written until we know all of them.
+            # Also keep a ref to the writer in order to swap it out for each artist redraw.
+            dpi = self._fig.get_dpi()
+            self._fig.set_dpi(72)
+            width, height = self._fig.get_size_inches()
+            w, h = width * 72, height * 72
+
+            self._vector_renderer = RendererSVG(w, h, f, None, dpi)
+            self._renderer = MixedModeRenderer(
+                self._fig, width, height, dpi, self._vector_renderer
+            )
+
+            self._fig.draw(self._renderer)
+            base_writer = self._vector_renderer.writer
+
+            for artist in init_artists:
+                artist.set_animated(self._blit)
+
+            # These group ids will help us detect if an artist that we haven't
+            # encountered before gets drawn. These artists are problematic
+            # because the SVG group associated with them won't be in the
+            # base document so we won't be able to update it properly.
+            known_groups = self._vector_renderer._groupids
+
+            # Get all subsequent frames by only drawing
+            # the artists returned by the user's func
+            for framedata in self._iter_gen():
+                drawn_artists = {}
+
+                # Get all artists that the user returned, if there
+                # aren't any, then _validate_artists will throw an error
+                artists = self._func(framedata, *self._args, **self._kwargs)
+                artists = self._validate_artists(
+                    artists, name="animation function", set_animated=True
+                )
+
+                for artist in artists:
+                    artist_gid = artist.get_gid()
+
+                    # Check that this artist is known
+                    if artist_gid not in known_groups:
+                        raise ValueError(
+                            f"Artist {artist}, with gid={artist.get_gid()}, not recognized. "
+                            f"This usually occurs when the animation function returns a new artist."
+                        )
+
+                    # By switching out the underlying writer we can capture the
+                    # new data but any new defs get captured by the base document.
+                    with StringIO() as artist_f:
+                        writer = XMLWriter(artist_f)
+                        self._vector_renderer.writer = writer
+
+                        self._fig.draw_artist(artist)
+                        drawn_artist = artist_f.getvalue()
+                        self._total_bytes += len(drawn_artist)
+
+                    drawn_artists[artist_gid] = drawn_artist
+
+                if self._total_bytes >= self._bytes_limit:
+                    _log.warning(
+                        "Animation size has reached %s bytes, exceeding the limit "
+                        "of %s. If you're sure you want a larger animation "
+                        "embedded, set the animation.embed_limit rc parameter to "
+                        "a larger value (in MB). This and further frames will be "
+                        "dropped.", self._total_bytes, self._bytes_limit)
+                    break
+                else:
+                    self._embedded_frames.append(drawn_artists)
+
+            # Swap back in the original writer and finalize to get all defs.
+            self._vector_renderer.writer = base_writer
+            self._renderer.finalize()
+            self._base_document = f.getvalue()
+
+    def grab_frame(self, index):
+        """
+        Get a single frame as a valid SVG document.
+
+        The full animation isn't simply created by calling this repeatedly, instead the final
+        animation reuses parts that are common between frames. This method is useful for debugging
+        and extracting still frames but shouldn't be used to generate all animation frames.
+
+        Parameters
+        ----------
+        index : int
+            Index of frame to grab
+        """
+        self._grab_frames()
+        # Note: we use minidom instead of etree as etree messes up the namespaces
+        base = minidom.parseString(self._base_document)
+        for gid, data in self._embedded_frames[index].items():
+            index, parent = self._find_by_attr(base, gid, return_child=False)
+            # Slightly abuse text nodes to inject XML chunks into doc (requires unescaping)
+            parent.childNodes[index] = base.createTextNode(data)
+        return unescape(base.toxml())
+
+    def save(self, filename, fps=None, default_mode=None):
+        """
+        Save the animation as a movie file by drawing every frame.
+
+        Parameters
+        ----------
+        filename : str
+            The output filename, e.g., :file:`myanim.svg`.
+
+        fps : int, optional
+            Movie frame rate (per second).  If not set, the frame rate from the
+            animation's frame interval.
+
+        default_mode: one of 'loop', 'once', 'reflect'.
+            Specifies the default end-of-animation behavior. If not set,
+            the animation's default value is used.
+        """
+        self._grab_frames()
+
+        # Convert interval in ms to frames per second
+        interval = self._interval if fps is None else np.floor(1000/fps)
+        default_mode = self._default_mode if default_mode is None else default_mode
+
+        mode_dict = dict(once_checked="", loop_checked="", reflect_checked="")
+        mode_dict[default_mode + "_checked"] = "checked"
+
+        with open(filename, "w", encoding="utf-8") as of:
+            of.write(SVG_JS_INCLUDE + STYLE_INCLUDE)
+            of.write(
+                SVG_DISPLAY_TEMPLATE.format(
+                    id=uuid.uuid4().hex,
+                    Nframes=len(self._embedded_frames),
+                    fill_frames=self._embedded_frames,
+                    base_document=self._base_document,
+                    interval=interval,
+                    **mode_dict,
+                )
+            )
+
+    def to_jshtml(self, fps=None, default_mode=None):
+        """Generate HTML representation of the animation"""
+        if not self._html_representation:
+            with TemporaryDirectory() as tmpdir:
+                path = Path(tmpdir, "temp.html")
+                self.save(str(path), fps=fps, default_mode=default_mode)
+                self._html_representation = path.read_text()
+        return self._html_representation
+
+    def _repr_html_(self):
+        """IPython display hook for rendering."""
+        return self.to_jshtml()
--- lib/matplotlib/tests/test_animation.py
+++ lib/matplotlib/tests/test_animation.py
@@ -6,6 +6,10 @@ import shutil
 import subprocess
 import sys
 import weakref
+import uuid
+import xml
+import base64
+import functools
 
 import numpy as np
 import pytest
@@ -14,6 +18,9 @@ import matplotlib as mpl
 from matplotlib import pyplot as plt
 from matplotlib import animation
 from matplotlib.testing.decorators import check_figures_equal
+from matplotlib.backends.backend_svg import RendererSVG
+from matplotlib.testing.decorators import _raise_on_image_difference
+from matplotlib.testing.compare import convert
 
 
 @pytest.fixture()
@@ -551,3 +558,192 @@ def test_movie_writer_invalid_path(anim):
     with pytest.raises(FileNotFoundError, match=match_str):
         anim.save("/foo/bar/aardvark/thiscannotreallyexist.mp4",
                   writer=animation.FFMpegFileWriter())
+
+
+def get_frames(anim, size, tmpdir):
+    if isinstance(anim, animation.SVGFuncAnimation):
+        return [anim.grab_frame(i) for i in range(size)]
+    elif isinstance(anim, animation.FuncAnimation):
+        with mpl.rc_context({"animation.frame_format": "svg"}):
+            path = Path(tmpdir, "temp.html")
+            writer = animation.HTMLWriter(embed_frames=True)
+            # The savefig kwargs are needed because savefig overwrites the fig's facecolor
+            anim.save(str(path), writer=writer, savefig_kwargs={'facecolor': anim._fig.get_facecolor()})
+            return [base64.b64decode(f).decode("ascii") for f in writer._saved_frames[:size]]
+
+
+def get_line_anim(constructor, size, fmt="r-", use_init=False):
+    np.random.seed(0)
+
+    fig = plt.figure()
+    data = np.random.rand(size)
+    (l,) = plt.plot([], [], fmt)
+    plt.xlim(0, size - 1)
+    plt.ylim(0, 1)
+
+    def init():
+        l.set_data([], [])
+        return (l,)
+
+    def update_line(num, data, line):
+        line.set_data(range(num+1), data[:num+1])
+        return (line,)
+
+    anim = constructor(fig, update_line, range(size), init_func=init if use_init else None, fargs=(data, l),)
+    plt.close(fig)
+    return anim
+
+
+@functools.lru_cache
+def get_line_anim_frames(constructor, size, tmpdir, fmt="r-", use_init=False):
+    anim = get_line_anim(constructor, size, fmt=fmt, use_init=use_init)
+    return get_frames(anim, size, tmpdir)
+
+
+@functools.lru_cache
+def get_text_anim_frames(constructor, size, tmpdir, init_text="", use_init=False, math_mode=False):
+    np.random.seed(0)
+    simple_text = ["First", "Second", "Third"]
+    math_text = [r"$\sum_{i=0}^\infty x_i$", r"$E=mc^2$", r"$c=\sqrt{a^2+b^2}$"]
+
+    fig = plt.figure()
+    x, y = np.random.rand(2, size)
+    txt = plt.text(0.5, 0.5, init_text, fontsize=15)
+    text = math_text if math_mode else simple_text
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+
+    def init():
+        txt.set_text(init_text)
+        return (txt,)
+
+    def update_text(num):
+        txt.set_text(text[num % 3])
+        txt.set_x(x[num])
+        txt.set_y(y[num])
+        return (txt,)
+
+    anim = constructor(fig, update_text, range(size), init_func=init if use_init else None)
+    plt.close(fig)
+    return get_frames(anim, size, tmpdir)
+
+
+def compare_svgs(tmpdir, expected, actual, tol=0):
+    expected_path = Path(tmpdir, "expected.svg")
+    actual_path = Path(tmpdir, "actual.svg")
+
+    with open(expected_path, "w") as f:
+        f.write(expected)
+
+    with open(actual_path, "w") as f:
+        f.write(actual)
+
+    new_expected_path = convert(expected_path, False)
+    new_actual_path = convert(actual_path, False)
+
+    _raise_on_image_difference(new_expected_path, new_actual_path, tol=tol)
+
+
+@pytest.mark.parametrize("index", [0, 4, 9])
+def test_svganim_svg_validity(tmpdir, index):
+    svg_frame = get_line_anim_frames(animation.SVGFuncAnimation, 10, tmpdir, fmt="r-")[index]
+    parser = xml.parsers.expat.ParserCreate()
+    parser.Parse(svg_frame)  # this will raise ExpatError if the svg is invalid
+
+
+def test_svganim_cleanup_temporaries(tmpdir):
+    with tmpdir.as_cwd():
+        get_line_anim_frames(animation.SVGFuncAnimation, 10, tmpdir, fmt="r-")
+        assert list(Path(str(tmpdir)).iterdir()) == []
+
+
+@pytest.mark.parametrize("index", range(3))
+@pytest.mark.parametrize("anim_type", [get_line_anim_frames, get_text_anim_frames])
+def test_svganim_init_func(tmpdir, anim_type, index):
+    svg_init_frame = anim_type(animation.SVGFuncAnimation, 3, tmpdir, use_init=True)[index]
+    svg_frame = anim_type(animation.SVGFuncAnimation, 3, tmpdir, use_init=False)[index]
+    compare_svgs(tmpdir, svg_frame, svg_init_frame, tol=0)
+
+
+@pytest.mark.parametrize("index", [0, 4, 9])
+@pytest.mark.parametrize("marker", ["bo", "g^", "r1", "cp", "m*", "yX", "kD"])
+def test_svganim_line_animation(tmpdir, marker, index):
+    func_frame = get_line_anim_frames(animation.FuncAnimation, 10, tmpdir, fmt=marker)[index]
+    svg_frame = get_line_anim_frames(animation.SVGFuncAnimation, 10, tmpdir, fmt=marker)[index]
+    compare_svgs(tmpdir, func_frame, svg_frame, tol=0)
+
+
+@pytest.mark.parametrize("index", range(3))
+@pytest.mark.parametrize("math_mode", [True, False])
+@pytest.mark.parametrize("init_text", ["", "non-empty-text"])
+def test_svganim_text_animation(tmpdir, init_text, math_mode, index):
+    func_frame = get_text_anim_frames(animation.FuncAnimation, 3, tmpdir, init_text=init_text, math_mode=math_mode)[index]
+    svg_frame = get_text_anim_frames(animation.SVGFuncAnimation, 3, tmpdir, init_text=init_text, math_mode=math_mode)[index]
+    compare_svgs(tmpdir, func_frame, svg_frame, tol=0)
+
+
+@pytest.mark.parametrize("frames, save_count", [
+    [10, None], [range(10), None], [iter(range(10)), 10], [lambda: range(10), 10]
+])
+def test_svganim_frames_param_type(monkeypatch, frames, save_count):
+    def mock_uuid(*args, **kwargs):
+        class DummyUUID:
+            hex = 'ABCDEF'
+        return DummyUUID()
+
+    def mock_make_id(*args, **kwargs):
+        return 'dummyid1234'
+
+    def get_anim(frames, save_count, size=10):
+        np.random.seed(0)
+        fig = plt.figure()
+        data = np.random.rand(size)
+        (l,) = plt.plot([], [], 'r-')
+        plt.xlim(0, size-1)
+        plt.ylim(0, 1)
+        index = 0
+
+        def update_line(ununsed):
+            nonlocal index
+            l.set_data(range(index + 1), data[:index + 1])
+            index += 1
+            return (l,)
+
+        anim = animation.SVGFuncAnimation(fig, update_line, frames, save_count=save_count)
+        anim._grab_frames()
+        plt.close(fig)
+        return anim._embedded_frames
+
+    # Remove all randomness associated with unique ids in the end SVG
+    # this enables us to compare the SVGs directly without inkscape
+    monkeypatch.setattr(uuid, 'uuid4', mock_uuid)
+    monkeypatch.setattr(RendererSVG, '_make_id', mock_make_id)
+    assert get_anim(range(10), 10) == get_anim(frames, save_count)
+
+
+def test_svganim_embed_limit(caplog, tmpdir):
+    caplog.set_level("WARNING")
+    with tmpdir.as_cwd():
+        with mpl.rc_context({"animation.embed_limit": 1e-6}):  # ~1 byte.
+            anim = get_line_anim(animation.SVGFuncAnimation, 2, fmt='r-')
+            anim._grab_frames()
+    assert len(caplog.records) == 1
+    record, = caplog.records
+    assert (record.name == "matplotlib.animation"
+            and record.levelname == "WARNING")
+
+
+def test_svganim_requires_blit():
+    with pytest.raises(NotImplementedError, match='.*blitting must be enabled.*'):
+        animation.SVGFuncAnimation(None, lambda: 0, 10, blit=False)
+
+
+def test_svganim_unrecognized_artist():
+    fig = plt.figure()
+
+    def update_line(ununsed):
+        return plt.plot([], [], 'r-')
+
+    with pytest.raises(ValueError, match='Artist .* not recognized.*'):
+        anim = animation.SVGFuncAnimation(fig, update_line, 10)
+        anim.to_jshtml()
