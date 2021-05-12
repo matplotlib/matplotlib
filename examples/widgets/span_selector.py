609a8402a4e51e272fd75d63a44e78ad55cc86a6
Jash Shah <55907269+Jashshah6086@users.noreply.github.com>
2021-05-13 02:23:53 +0530

    Update span_selector(Issue#20200)

--- examples/widgets/span_selector.py
+++ examples/widgets/span_selector.py
@@ -29,10 +29,10 @@ line2, = ax2.plot([], [])
 def onselect(xmin, xmax):
     indmin, indmax = np.searchsorted(x, (xmin, xmax))
     indmax = min(len(x) - 1, indmax)
-    
+
     region_x = x[indmin:indmax]
     region_y = y[indmin:indmax]
-    
+
     if len(region_x) >= 2:
         line2.set_data(region_x, region_y)
         ax2.set_xlim(region_x[0], region_x[-1])
