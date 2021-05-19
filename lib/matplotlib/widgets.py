94a2f46f3cf0a6a621833fd3ac31b958e36e4910
Eric Prestat <eric.prestat@gmail.com>
2021-05-19 12:06:47 +0100

    Fix draw recursive loop when resizing figure containing Selector using blit.

--- lib/matplotlib/widgets.py
+++ lib/matplotlib/widgets.py
@@ -1737,7 +1737,8 @@ class _SelectorWidget(AxesWidget):
                 self.canvas.draw()
             self.background = self.canvas.copy_from_bbox(self.ax.bbox)
         if needs_redraw:
-            self.update()
+            for artist in self.artists:
+                self.ax.draw_artist(artist)
 
     def connect_default_events(self):
         """Connect the major canvas events to methods."""
