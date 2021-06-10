1368c6bdb9dc90d709f907b1b3e8372095036904
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-06-10 00:14:04 -0400

    Ensure Ctrl+C out of IPython doesn't break the GTK application.

--- lib/matplotlib/backends/backend_gtk3.py
+++ lib/matplotlib/backends/backend_gtk3.py
@@ -51,12 +51,26 @@ except TypeError as exc:
 _application = None
 
 
+def _shutdown_application(app):
+    # The application might prematurely shut down if Ctrl-C'd out of IPython,
+    # so close all windows.
+    for win in app.get_windows():
+        win.destroy()
+    # The PyGObject wrapper incorrectly thinks that None is not allowed, or we
+    # would call this:
+    # Gio.Application.set_default(None)
+    # Instead, we set this property and ignore default applications with it:
+    app._created_by_matplotlib = True
+    global _application
+    _application = None
+
+
 def _create_application():
     global _application
 
     if _application is None:
         app = Gio.Application.get_default()
-        if app is None:
+        if app is None or getattr(app, '_created_by_matplotlib'):
             # display_is_valid returns False only if on Linux and neither X11
             # nor Wayland display can be opened.
             if not mpl._c_internal_utils.display_is_valid():
@@ -66,6 +80,7 @@ def _create_application():
             # The activate signal must be connected, but we don't care for
             # handling it, since we don't do any remote processing.
             _application.connect('activate', lambda *args, **kwargs: None)
+            _application.connect('shutdown', _shutdown_application)
             _application.register()
             cbook._setup_new_guiapp()
         else:
@@ -851,6 +866,8 @@ class _BackendGTK3(_Backend):
         if _application is None:
             return
 
-        _application.run()  # Quits when all added windows close.
-        # Running after quit is undefined, so create a new one next time.
-        _application = None
+        try:
+            _application.run()  # Quits when all added windows close.
+        finally:
+            # Running after quit is undefined, so create a new one next time.
+            _application = None
