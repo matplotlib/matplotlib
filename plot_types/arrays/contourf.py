6b842125cdd126b85bb50d1c48b4ce2ad13288fb
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2021-06-25 00:11:23 +0200

    Cleanup plot types

--- plot_types/arrays/contourf.py
+++ plot_types/arrays/contourf.py
@@ -12,14 +12,14 @@ plt.style.use('mpl_plot_gallery')
 
 # make data
 X, Y = np.meshgrid(np.linspace(-3, 3, 256), np.linspace(-3, 3, 256))
-Z = (1 - X/2. + X**5 + Y**3)*np.exp(-X**2-Y**2)
+Z = (1 - X/2. + X**5 + Y**3) * np.exp(-X**2 - Y**2)
 Z = Z - Z.min()
-levs = np.linspace(np.min(Z), np.max(Z), 7)
+levels = np.linspace(np.min(Z), np.max(Z), 7)
 
 # plot
 fig, ax = plt.subplots()
 
-plt.contourf(X, Y, Z, levels=levs)
-plt.contour(X, Y, Z, levels=levs, colors="white", linewidths=0.5)
+ax.contourf(X, Y, Z, levels=levels)
+ax.contour(X, Y, Z, levels=levels, colors="white", linewidths=0.5)
 
 plt.show()
