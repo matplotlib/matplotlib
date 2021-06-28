cc197649640cb00d86a425006d3854bebc320792
Antony Lee <anntzer.lee@gmail.com>
2021-06-28 19:18:07 +0200

    Fix ScalarFormatter.format_ticks for non-ordered tick locations.
    Non-ordered "ticks" locations can be used when using ScalarFormatter to
    format contour lines.  Perhaps ContourLabeler.get_text could arrange for
    the "ticks" to be ordered, but it's much easier to just support
    non-ordered ticks in ScalarFormatter.

--- lib/matplotlib/tests/test_contour.py
+++ lib/matplotlib/tests/test_contour.py
@@ -464,3 +464,13 @@ def test_contour_line_start_on_corner_edge():
     cbar = fig.colorbar(filled)
     lines = ax.contour(x, y, z, corner_mask=True, colors='k')
     cbar.add_lines(lines)
+
+
+@pytest.mark.style("default")
+def test_contour_autolabel_beyond_powerlimits():
+    ax = plt.figure().add_subplot()
+    cs = plt.contour(np.geomspace(1e-6, 1e-4, 100).reshape(10, 10),
+                     levels=[.25e-5, 1e-5, 4e-5])
+    ax.clabel(cs)
+    # Currently, the exponent is missing, but that may be fixed in the future.
+    assert {text.get_text() for text in ax.texts} == {"0.25", "1.00", "4.00"}
