f1d7ac8ab90fbd7e28ea92ec8ff4eb9acf6237f8
Eric Prestat <eric.prestat@gmail.com>
2021-07-07 12:46:40 +0100

    Move RectangleSelector from __init__ to class docstring.

--- lib/matplotlib/widgets.py
+++ lib/matplotlib/widgets.py
@@ -2499,10 +2499,84 @@ class ToolHandles:
 
 
 class RectangleSelector(_SelectorWidget):
-    """
+    r"""
     Select a rectangular region of an axes.
 
     For the cursor to remain responsive you must keep a reference to it.
+
+    Parameters
+    ----------
+    ax : `~matplotlib.axes.Axes`
+        The parent axes for the widget.
+
+    onselect : function
+        A callback function that is called after a selection is completed.
+        It must have the signature::
+
+            def onselect(eclick: MouseEvent, erelease: MouseEvent)
+
+        where *eclick* and *erelease* are the mouse click and release
+        `.MouseEvent`\s that start and complete the selection.
+
+    drawtype : {"box", "line", "none"}, default: "box"
+        Whether to draw the full rectangle box, the diagonal line of the
+        rectangle, or nothing at all.
+
+    minspanx : float, default: 0
+        Selections with an x-span less than *minspanx* are ignored.
+
+    minspany : float, default: 0
+        Selections with an y-span less than *minspany* are ignored.
+
+    useblit : bool, default: False
+        Whether to use blitting for faster drawing (if supported by the
+        backend).
+
+    lineprops : dict, optional
+        Properties with which the line is drawn, if ``drawtype == "line"``.
+        Default::
+
+            dict(color="black", linestyle="-", linewidth=2, alpha=0.5)
+
+    rectprops : dict, optional
+        Properties with which the rectangle is drawn, if ``drawtype ==
+        "box"``.  Default::
+
+            dict(facecolor="red", edgecolor="black", alpha=0.2, fill=True)
+
+    spancoords : {"data", "pixels"}, default: "data"
+        Whether to interpret *minspanx* and *minspany* in data or in pixel
+        coordinates.
+
+    button : `.MouseButton`, list of `.MouseButton`, default: all buttons
+        Button(s) that trigger rectangle selection.
+
+    handle_grab_distance : float, default: 10
+        Distance in pixels within which the interactive tool handles can be
+        activated.
+
+    handle_props : dict
+        Properties with which the interactive handles are drawn.
+
+    interactive : bool, default: False
+        Whether to draw a set of handles that allow interaction with the
+        widget after it is drawn.
+
+    state_modifier_keys : dict, optional
+        Keyboard modifiers which affect the widget's behavior.  Values
+        amend the defaults.
+
+        - "move": Move the existing shape, default: no modifier.
+        - "clear": Clear the current shape, default: "escape".
+        - "square": Makes the shape square, default: "shift".
+        - "center": Make the initial point the center of the shape,
+          default: "ctrl".
+
+        "square" and "center" can be combined.
+
+    drag_from_anywhere : bool, optional
+        If `True`, the widget can be moved by clicking anywhere within
+        its bounds.
 
     Examples
     --------
@@ -2521,81 +2595,6 @@ class RectangleSelector(_SelectorWidget):
                  button=None, handle_grab_distance=10, handle_props=None,
                  interactive=False, state_modifier_keys=None,
                  drag_from_anywhere=False):
-        r"""
-        Parameters
-        ----------
-        ax : `~matplotlib.axes.Axes`
-            The parent axes for the widget.
-
-        onselect : function
-            A callback function that is called after a selection is completed.
-            It must have the signature::
-
-                def onselect(eclick: MouseEvent, erelease: MouseEvent)
-
-            where *eclick* and *erelease* are the mouse click and release
-            `.MouseEvent`\s that start and complete the selection.
-
-        drawtype : {"box", "line", "none"}, default: "box"
-            Whether to draw the full rectangle box, the diagonal line of the
-            rectangle, or nothing at all.
-
-        minspanx : float, default: 0
-            Selections with an x-span less than *minspanx* are ignored.
-
-        minspany : float, default: 0
-            Selections with an y-span less than *minspany* are ignored.
-
-        useblit : bool, default: False
-            Whether to use blitting for faster drawing (if supported by the
-            backend).
-
-        lineprops : dict, optional
-            Properties with which the line is drawn, if ``drawtype == "line"``.
-            Default::
-
-                dict(color="black", linestyle="-", linewidth=2, alpha=0.5)
-
-        rectprops : dict, optional
-            Properties with which the rectangle is drawn, if ``drawtype ==
-            "box"``.  Default::
-
-                dict(facecolor="red", edgecolor="black", alpha=0.2, fill=True)
-
-        spancoords : {"data", "pixels"}, default: "data"
-            Whether to interpret *minspanx* and *minspany* in data or in pixel
-            coordinates.
-
-        button : `.MouseButton`, list of `.MouseButton`, default: all buttons
-            Button(s) that trigger rectangle selection.
-
-        handle_grab_distance : float, default: 10
-            Distance in pixels within which the interactive tool handles can be
-            activated.
-
-        handle_props : dict
-            Properties with which the interactive handles are drawn.
-
-        interactive : bool, default: False
-            Whether to draw a set of handles that allow interaction with the
-            widget after it is drawn.
-
-        state_modifier_keys : dict, optional
-            Keyboard modifiers which affect the widget's behavior.  Values
-            amend the defaults.
-
-            - "move": Move the existing shape, default: no modifier.
-            - "clear": Clear the current shape, default: "escape".
-            - "square": Makes the shape square, default: "shift".
-            - "center": Make the initial point the center of the shape,
-              default: "ctrl".
-
-            "square" and "center" can be combined.
-
-        drag_from_anywhere : bool, optional
-            If `True`, the widget can be moved by clicking anywhere within
-            its bounds.
-        """
         super().__init__(ax, onselect, useblit=useblit, button=button,
                          state_modifier_keys=state_modifier_keys)
 
