feda49775781c5fbfe2cdd1a5c75e8250f2c1201
Antony Lee <anntzer.lee@gmail.com>
2021-07-08 10:13:15 +0200

    Merge EllipseSelector example together with RectangleSelector.
    The old EllipseSelector example had similar issues as those fixed in
    f288658 for RectangleSelector.
    
    I'm not too happy with the explanation text, but that can be fixed
    lated.

--- lib/matplotlib/widgets.py
+++ lib/matplotlib/widgets.py
@@ -3017,31 +3017,7 @@ class EllipseSelector(RectangleSelector):
 
     Examples
     --------
-    >>> import numpy as np
-    >>> import matplotlib.pyplot as plt
-    >>> from matplotlib.widgets import EllipseSelector
-    >>> def onselect(eclick, erelease):
-    ...     "eclick and erelease are matplotlib events at press and release."
-    ...     print(f'startposition: {eclick.xdata}, {eclick.ydata})
-    ...     print(f'endposition  : {erelease.xdata}, {erelease.ydata})
-    ...     print('used button  : ', eclick.button)
-    ...
-    >>> def toggle_selector(event):
-    ...     print(' Key pressed.')
-    ...     if event.key in ['Q', 'q'] and toggle_selector.ES.active:
-    ...         print('EllipseSelector deactivated.')
-    ...         toggle_selector.RS.set_active(False)
-    ...     if event.key in ['A', 'a'] and not toggle_selector.ES.active:
-    ...         print('EllipseSelector activated.')
-    ...         toggle_selector.ES.set_active(True)
-    ...
-    >>> x = np.arange(100.) / 99
-    >>> y = np.sin(x)
-    >>> fig, ax = plt.subplots()
-    >>> ax.plot(x, y)
-    >>> toggle_selector.ES = EllipseSelector(ax, onselect)
-    >>> fig.canvas.mpl_connect('key_press_event', toggle_selector)
-    >>> plt.show()
+    :doc:`/gallery/widgets/rectangle_selector`
     """
 
     _shape_klass = Ellipse
