b528c7d1df7949a2592908fb4b5d3c55af216b67
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2021-07-18 22:45:07 +0200

    Use ax instead of axs when the object is clearly a single Axes

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -6439,7 +6439,7 @@ def test_secondary_formatter():
         secax.xaxis.get_major_formatter(), mticker.ScalarFormatter)
 
 
-def color_boxes(fig, axs):
+def color_boxes(fig, ax):
     """
     Helper for the tests below that test the extents of various axes elements
     """
@@ -6447,7 +6447,7 @@ def color_boxes(fig, axs):
 
     renderer = fig.canvas.get_renderer()
     bbaxis = []
-    for nn, axx in enumerate([axs.xaxis, axs.yaxis]):
+    for nn, axx in enumerate([ax.xaxis, ax.yaxis]):
         bb = axx.get_tightbbox(renderer)
         if bb:
             axisr = plt.Rectangle(
@@ -6459,7 +6459,7 @@ def color_boxes(fig, axs):
 
     bbspines = []
     for nn, a in enumerate(['bottom', 'top', 'left', 'right']):
-        bb = axs.spines[a].get_window_extent(renderer)
+        bb = ax.spines[a].get_window_extent(renderer)
         spiner = plt.Rectangle(
             (bb.x0, bb.y0), width=bb.width, height=bb.height,
             linewidth=0.7, edgecolor="green", facecolor="none", transform=None,
@@ -6467,7 +6467,7 @@ def color_boxes(fig, axs):
         fig.add_artist(spiner)
         bbspines += [bb]
 
-    bb = axs.get_window_extent()
+    bb = ax.get_window_extent()
     rect2 = plt.Rectangle(
         (bb.x0, bb.y0), width=bb.width, height=bb.height,
         linewidth=1.5, edgecolor="magenta", facecolor="none", transform=None,
@@ -6475,7 +6475,7 @@ def color_boxes(fig, axs):
     fig.add_artist(rect2)
     bbax = bb
 
-    bb2 = axs.get_tightbbox(renderer)
+    bb2 = ax.get_tightbbox(renderer)
     rect2 = plt.Rectangle(
         (bb2.x0, bb2.y0), width=bb2.width, height=bb2.height,
         linewidth=3, edgecolor="red", facecolor="none", transform=None,
