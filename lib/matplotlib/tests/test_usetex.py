4733fff3ad31bbadddf91eff6e1f0e49230630b6
Andrea PIERRÉ <andrea_pierre@brown.edu>
2021-07-22 00:14:12 -0400

    refactor: move test

--- lib/matplotlib/tests/test_usetex.py
+++ lib/matplotlib/tests/test_usetex.py
@@ -105,3 +105,13 @@ def test_textcomp_full():
     fig = plt.figure()
     fig.text(.5, .5, "hello, world", usetex=True)
     fig.canvas.draw()
+
+
+def test_usetex_with_underscore():
+    plt.rcParams["text.usetex"] = True
+    df = {"a_b": range(5)[::-1], "c": range(5)}
+    fig, ax = plt.subplots()
+    ax.plot("c", "a_b", data=df)
+    ax.legend()
+    ax.text(0, 0, "foo_bar", usetex=True)
+    plt.draw()  # TeX rendering is done at draw time
