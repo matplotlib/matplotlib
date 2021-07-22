32b594e266ea9682aa3dc8dbaef57aa75d7b190d
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2021-07-22 07:56:04 +0200

    Backport PR #20710: Fix tests with Inkscape 1.1.

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -1370,7 +1370,7 @@ def test_markevery_line():
     ax.legend()
 
 
-@image_comparison(['markevery_linear_scales'], remove_text=True)
+@image_comparison(['markevery_linear_scales'], remove_text=True, tol=0.001)
 def test_markevery_linear_scales():
     cases = [None,
              8,
