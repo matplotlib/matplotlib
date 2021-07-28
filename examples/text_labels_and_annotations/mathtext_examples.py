0d925a5cccdf6376729adffc5add6788f5d56835
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-07-28 16:06:59 -0400

    Merge dictionaries in mathtext example.
    There were two only because we needed that to preserve order, but that's
    guaranteed for dictionaries now, so they can be combined.

--- examples/text_labels_and_annotations/mathtext_examples.py
+++ examples/text_labels_and_annotations/mathtext_examples.py
@@ -10,49 +10,48 @@ import subprocess
 import sys
 import re
 
-# Selection of features following "Writing mathematical expressions" tutorial
-mathtext_titles = {
-    0: "Header demo",
-    1: "Subscripts and superscripts",
-    2: "Fractions, binomials and stacked numbers",
-    3: "Radicals",
-    4: "Fonts",
-    5: "Accents",
-    6: "Greek, Hebrew",
-    7: "Delimiters, functions and Symbols"}
-n_lines = len(mathtext_titles)
+# Selection of features following "Writing mathematical expressions" tutorial,
+# with randomly picked examples.
+mathtext_demos = {
+    "Header demo":
+        r"$W^{3\beta}_{\delta_1 \rho_1 \sigma_2} = "
+        r"U^{3\beta}_{\delta_1 \rho_1} + \frac{1}{8 \pi 2} "
+        r"\int^{\alpha_2}_{\alpha_2} d \alpha^\prime_2 \left[\frac{ "
+        r"U^{2\beta}_{\delta_1 \rho_1} - \alpha^\prime_2U^{1\beta}_"
+        r"{\rho_1 \sigma_2} }{U^{0\beta}_{\rho_1 \sigma_2}}\right]$",
 
-# Randomly picked examples
-mathext_demos = {
-    0: r"$W^{3\beta}_{\delta_1 \rho_1 \sigma_2} = "
-    r"U^{3\beta}_{\delta_1 \rho_1} + \frac{1}{8 \pi 2} "
-    r"\int^{\alpha_2}_{\alpha_2} d \alpha^\prime_2 \left[\frac{ "
-    r"U^{2\beta}_{\delta_1 \rho_1} - \alpha^\prime_2U^{1\beta}_"
-    r"{\rho_1 \sigma_2} }{U^{0\beta}_{\rho_1 \sigma_2}}\right]$",
+    "Subscripts and superscripts":
+        r"$\alpha_i > \beta_i,\ "
+        r"\alpha_{i+1}^j = {\rm sin}(2\pi f_j t_i) e^{-5 t_i/\tau},\ "
+        r"\ldots$",
 
-    1: r"$\alpha_i > \beta_i,\ "
-    r"\alpha_{i+1}^j = {\rm sin}(2\pi f_j t_i) e^{-5 t_i/\tau},\ "
-    r"\ldots$",
+    "Fractions, binomials and stacked numbers":
+        r"$\frac{3}{4},\ \binom{3}{4},\ \genfrac{}{}{0}{}{3}{4},\ "
+        r"\left(\frac{5 - \frac{1}{x}}{4}\right),\ \ldots$",
 
-    2: r"$\frac{3}{4},\ \binom{3}{4},\ \genfrac{}{}{0}{}{3}{4},\ "
-    r"\left(\frac{5 - \frac{1}{x}}{4}\right),\ \ldots$",
+    "Radicals":
+        r"$\sqrt{2},\ \sqrt[3]{x},\ \ldots$",
 
-    3: r"$\sqrt{2},\ \sqrt[3]{x},\ \ldots$",
+    "Fonts":
+        r"$\mathrm{Roman}\ , \ \mathit{Italic}\ , \ \mathtt{Typewriter} \ "
+        r"\mathrm{or}\ \mathcal{CALLIGRAPHY}$",
 
-    4: r"$\mathrm{Roman}\ , \ \mathit{Italic}\ , \ \mathtt{Typewriter} \ "
-    r"\mathrm{or}\ \mathcal{CALLIGRAPHY}$",
+    "Accents":
+        r"$\acute a,\ \bar a,\ \breve a,\ \dot a,\ \ddot a, \ \grave a, \ "
+        r"\hat a,\ \tilde a,\ \vec a,\ \widehat{xyz},\ \widetilde{xyz},\ "
+        r"\ldots$",
 
-    5: r"$\acute a,\ \bar a,\ \breve a,\ \dot a,\ \ddot a, \ \grave a, \ "
-    r"\hat a,\ \tilde a,\ \vec a,\ \widehat{xyz},\ \widetilde{xyz},\ "
-    r"\ldots$",
+    "Greek, Hebrew":
+        r"$\alpha,\ \beta,\ \chi,\ \delta,\ \lambda,\ \mu,\ "
+        r"\Delta,\ \Gamma,\ \Omega,\ \Phi,\ \Pi,\ \Upsilon,\ \nabla,\ "
+        r"\aleph,\ \beth,\ \daleth,\ \gimel,\ \ldots$",
 
-    6: r"$\alpha,\ \beta,\ \chi,\ \delta,\ \lambda,\ \mu,\ "
-    r"\Delta,\ \Gamma,\ \Omega,\ \Phi,\ \Pi,\ \Upsilon,\ \nabla,\ "
-    r"\aleph,\ \beth,\ \daleth,\ \gimel,\ \ldots$",
-
-    7: r"$\coprod,\ \int,\ \oint,\ \prod,\ \sum,\ "
-    r"\log,\ \sin,\ \approx,\ \oplus,\ \star,\ \varpropto,\ "
-    r"\infty,\ \partial,\ \Re,\ \leftrightsquigarrow, \ \ldots$"}
+    "Delimiters, functions and Symbols":
+        r"$\coprod,\ \int,\ \oint,\ \prod,\ \sum,\ "
+        r"\log,\ \sin,\ \approx,\ \oplus,\ \star,\ \varpropto,\ "
+        r"\infty,\ \partial,\ \Re,\ \leftrightsquigarrow, \ \ldots$",
+}
+n_lines = len(mathtext_demos)
 
 
 def doall():
@@ -73,32 +72,31 @@ def doall():
     # Gap between lines in axes coords
     line_axesfrac = 1 / n_lines
 
-    # Plotting header demonstration formula
-    full_demo = mathext_demos[0]
+    # Plot header demonstration formula.
+    full_demo = mathtext_demos['Header demo']
     ax.annotate(full_demo,
                 xy=(0.5, 1. - 0.59 * line_axesfrac),
                 color='tab:orange', ha='center', fontsize=20)
 
-    # Plotting features demonstration formulae
-    for i_line in range(1, n_lines):
+    # Plot feature demonstration formulae.
+    for i_line, (title, demo) in enumerate(mathtext_demos.items()):
+        print(i_line, demo)
+        if i_line == 0:
+            continue
+
         baseline = 1 - i_line * line_axesfrac
         baseline_next = baseline - line_axesfrac
-        title = mathtext_titles[i_line] + ":"
         fill_color = ['white', 'tab:blue'][i_line % 2]
         ax.fill_between([0, 1], [baseline, baseline],
                         [baseline_next, baseline_next],
                         color=fill_color, alpha=0.2)
-        ax.annotate(title,
+        ax.annotate(f'{title}:',
                     xy=(0.07, baseline - 0.3 * line_axesfrac),
                     color=mpl_grey_rgb, weight='bold')
-        demo = mathext_demos[i_line]
         ax.annotate(demo,
                     xy=(0.05, baseline - 0.75 * line_axesfrac),
                     color=mpl_grey_rgb, fontsize=16)
 
-    for i in range(n_lines):
-        s = mathext_demos[i]
-        print(i, s)
     plt.show()
 
 
@@ -111,8 +109,7 @@ if '--latex' in sys.argv:
         fd.write("\\begin{document}\n")
         fd.write("\\begin{enumerate}\n")
 
-        for i in range(n_lines):
-            s = mathext_demos[i]
+        for s in mathtext_demos.values():
             s = re.sub(r"(?<!\\)\$", "$$", s)
             fd.write("\\item %s\n" % s)
 
