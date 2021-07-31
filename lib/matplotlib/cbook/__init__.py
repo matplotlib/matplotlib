0b724bf3ea06dee2f73d487aa09406a77e117b2d
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-07-31 01:33:37 -0400

    Add a GTK4 backend.

--- lib/matplotlib/cbook/__init__.py
+++ lib/matplotlib/cbook/__init__.py
@@ -50,8 +50,8 @@ def _get_running_interactive_framework():
     Returns
     -------
     Optional[str]
-        One of the following values: "qt", "gtk3", "wx", "tk", "macosx",
-        "headless", ``None``.
+        One of the following values: "qt", "gtk3", "gtk4", "wx", "tk",
+        "macosx", "headless", ``None``.
     """
     # Use ``sys.modules.get(name)`` rather than ``name in sys.modules`` as
     # entries can also have been explicitly set to None.
@@ -64,8 +64,13 @@ def _get_running_interactive_framework():
     if QtWidgets and QtWidgets.QApplication.instance():
         return "qt"
     Gtk = sys.modules.get("gi.repository.Gtk")
-    if Gtk and Gtk.main_level():
-        return "gtk3"
+    if Gtk:
+        if Gtk.MAJOR_VERSION == 4:
+            from gi.repository import GLib
+            if GLib.main_depth():
+                return "gtk4"
+        if Gtk.MAJOR_VERSION == 3 and Gtk.main_level():
+            return "gtk3"
     wx = sys.modules.get("wx")
     if wx and wx.GetApp():
         return "wx"
