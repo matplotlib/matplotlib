3d180e74ec3ca2f48047e22c8195dcaa49e4ae15
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2021-08-24 22:46:30 +0200

    Backport PR #20693: Fix setting artists properties of selectors

--- lib/matplotlib/tests/test_widgets.py
+++ lib/matplotlib/tests/test_widgets.py
@@ -90,6 +90,34 @@ def test_rectangle_drag(drag_from_anywhere, new_center):
     assert tool.center == (180, 190)
 
 
+def test_rectangle_selector_set_props_handle_props():
+    ax = get_ax()
+
+    def onselect(epress, erelease):
+        pass
+
+    tool = widgets.RectangleSelector(ax, onselect, interactive=True,
+                                     props=dict(facecolor='b', alpha=0.2),
+                                     handle_props=dict(alpha=0.5))
+    # Create rectangle
+    do_event(tool, 'press', xdata=0, ydata=10, button=1)
+    do_event(tool, 'onmove', xdata=100, ydata=120, button=1)
+    do_event(tool, 'release', xdata=100, ydata=120, button=1)
+
+    artist = tool._selection_artist
+    assert artist.get_facecolor() == mcolors.to_rgba('b', alpha=0.2)
+    tool.set_props(facecolor='r', alpha=0.3)
+    assert artist.get_facecolor() == mcolors.to_rgba('r', alpha=0.3)
+
+    for artist in tool._handles_artists:
+        assert artist.get_markeredgecolor() == 'black'
+        assert artist.get_alpha() == 0.5
+    tool.set_handle_props(markeredgecolor='r', alpha=0.3)
+    for artist in tool._handles_artists:
+        assert artist.get_markeredgecolor() == 'r'
+        assert artist.get_alpha() == 0.3
+
+
 def test_ellipse():
     """For ellipse, test out the key modifiers"""
     ax = get_ax()
@@ -185,9 +213,9 @@ def test_rectangle_handles():
 
     # Check that marker_props worked.
     assert mcolors.same_color(
-        tool._corner_handles.artist.get_markerfacecolor(), 'r')
+        tool._corner_handles.artists[0].get_markerfacecolor(), 'r')
     assert mcolors.same_color(
-        tool._corner_handles.artist.get_markeredgecolor(), 'b')
+        tool._corner_handles.artists[0].get_markeredgecolor(), 'b')
 
 
 @pytest.mark.parametrize('interactive', [True, False])
@@ -402,6 +430,34 @@ def test_span_selector_direction():
 
     with pytest.raises(ValueError):
         tool.direction = 'invalid_string'
+
+
+def test_span_selector_set_props_handle_props():
+    ax = get_ax()
+
+    def onselect(epress, erelease):
+        pass
+
+    tool = widgets.SpanSelector(ax, onselect, 'horizontal', interactive=True,
+                                props=dict(facecolor='b', alpha=0.2),
+                                handle_props=dict(alpha=0.5))
+    # Create rectangle
+    do_event(tool, 'press', xdata=0, ydata=10, button=1)
+    do_event(tool, 'onmove', xdata=100, ydata=120, button=1)
+    do_event(tool, 'release', xdata=100, ydata=120, button=1)
+
+    artist = tool._selection_artist
+    assert artist.get_facecolor() == mcolors.to_rgba('b', alpha=0.2)
+    tool.set_props(facecolor='r', alpha=0.3)
+    assert artist.get_facecolor() == mcolors.to_rgba('r', alpha=0.3)
+
+    for artist in tool._handles_artists:
+        assert artist.get_color() == 'b'
+        assert artist.get_alpha() == 0.5
+    tool.set_handle_props(color='r', alpha=0.3)
+    for artist in tool._handles_artists:
+        assert artist.get_color() == 'r'
+        assert artist.get_alpha() == 0.3
 
 
 def test_tool_line_handle():
@@ -779,6 +835,43 @@ def test_polygon_selector():
                       + polygon_place_vertex(50, 150)
                       + polygon_place_vertex(50, 50))
     check_polygon_selector(event_sequence, expected_result, 1)
+
+
+def test_polygon_selector_set_props_handle_props():
+    ax = get_ax()
+
+    ax._selections_count = 0
+
+    def onselect(vertices):
+        ax._selections_count += 1
+        ax._current_result = vertices
+
+    tool = widgets.PolygonSelector(ax, onselect,
+                                   props=dict(color='b', alpha=0.2),
+                                   handle_props=dict(alpha=0.5))
+
+    event_sequence = (polygon_place_vertex(50, 50)
+                      + polygon_place_vertex(150, 50)
+                      + polygon_place_vertex(50, 150)
+                      + polygon_place_vertex(50, 50))
+
+    for (etype, event_args) in event_sequence:
+        do_event(tool, etype, **event_args)
+
+    artist = tool._selection_artist
+    assert artist.get_color() == 'b'
+    assert artist.get_alpha() == 0.2
+    tool.set_props(color='r', alpha=0.3)
+    assert artist.get_color() == 'r'
+    assert artist.get_alpha() == 0.3
+
+    for artist in tool._handles_artists:
+        assert artist.get_color() == 'b'
+        assert artist.get_alpha() == 0.5
+    tool.set_handle_props(color='r', alpha=0.3)
+    for artist in tool._handles_artists:
+        assert artist.get_color() == 'r'
+        assert artist.get_alpha() == 0.3
 
 
 @pytest.mark.parametrize(
