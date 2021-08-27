5fa8b29a4efa59a93054485941210d5a8389731c
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-08-27 18:35:20 -0400

    Backport PR #20889: Fix clearing selector

--- lib/matplotlib/tests/test_widgets.py
+++ lib/matplotlib/tests/test_widgets.py
@@ -460,6 +460,82 @@ def test_span_selector_set_props_handle_props():
         assert artist.get_alpha() == 0.3
 
 
+@pytest.mark.parametrize('selector', ['span', 'rectangle'])
+def test_selector_clear(selector):
+    ax = get_ax()
+
+    def onselect(*args):
+        pass
+
+    kwargs = dict(ax=ax, onselect=onselect, interactive=True)
+    if selector == 'span':
+        Selector = widgets.SpanSelector
+        kwargs['direction'] = 'horizontal'
+    else:
+        Selector = widgets.RectangleSelector
+
+    tool = Selector(**kwargs)
+    do_event(tool, 'press', xdata=10, ydata=10, button=1)
+    do_event(tool, 'onmove', xdata=100, ydata=120, button=1)
+    do_event(tool, 'release', xdata=100, ydata=120, button=1)
+
+    # press-release event outside the selector to clear the selector
+    do_event(tool, 'press', xdata=130, ydata=130, button=1)
+    do_event(tool, 'release', xdata=130, ydata=130, button=1)
+    assert not tool._selection_completed
+
+    ax = get_ax()
+    kwargs['ignore_event_outside'] = True
+    tool = Selector(**kwargs)
+    assert tool.ignore_event_outside
+    do_event(tool, 'press', xdata=10, ydata=10, button=1)
+    do_event(tool, 'onmove', xdata=100, ydata=120, button=1)
+    do_event(tool, 'release', xdata=100, ydata=120, button=1)
+
+    # press-release event outside the selector ignored
+    do_event(tool, 'press', xdata=130, ydata=130, button=1)
+    do_event(tool, 'release', xdata=130, ydata=130, button=1)
+    assert tool._selection_completed
+
+    do_event(tool, 'on_key_press', key='escape')
+    assert not tool._selection_completed
+
+
+@pytest.mark.parametrize('selector', ['span', 'rectangle'])
+def test_selector_clear_method(selector):
+    ax = get_ax()
+
+    def onselect(*args):
+        pass
+
+    if selector == 'span':
+        tool = widgets.SpanSelector(ax, onselect, 'horizontal',
+                                    interactive=True,
+                                    ignore_event_outside=True)
+    else:
+        tool = widgets.RectangleSelector(ax, onselect, interactive=True)
+    do_event(tool, 'press', xdata=10, ydata=10, button=1)
+    do_event(tool, 'onmove', xdata=100, ydata=120, button=1)
+    do_event(tool, 'release', xdata=100, ydata=120, button=1)
+    assert tool._selection_completed
+    assert tool.visible
+    if selector == 'span':
+        assert tool.extents == (10, 100)
+
+    tool.clear()
+    assert not tool._selection_completed
+    assert not tool.visible
+
+    # Do another cycle of events to make sure we can
+    do_event(tool, 'press', xdata=10, ydata=10, button=1)
+    do_event(tool, 'onmove', xdata=50, ydata=120, button=1)
+    do_event(tool, 'release', xdata=50, ydata=120, button=1)
+    assert tool._selection_completed
+    assert tool.visible
+    if selector == 'span':
+        assert tool.extents == (10, 50)
+
+
 def test_tool_line_handle():
     ax = get_ax()
 
