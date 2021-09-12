41808430ace77c5dfdbb815c1d2e26ad3c1e8181
Jakub Klus <jakub.klus@gmail.com>
2021-09-12 23:40:33 +0200

    Fix errors discovered by CI

--- examples/lines_bars_and_markers/multivariate_marker_plot.py
+++ examples/lines_bars_and_markers/multivariate_marker_plot.py
@@ -18,9 +18,9 @@ from matplotlib.textpath import TextPath
 from matplotlib.colors import Normalize
 
 SUCCESS_SYMBOLS = [
-    TextPath((0,0), "☹"), 
-    TextPath((0,0), "😒"), 
-    TextPath((0,0), "☺"), 
+    TextPath((0, 0), "☹"),
+    TextPath((0, 0), "😒"),
+    TextPath((0, 0), "☺"),
 ]
 
 N = 25
@@ -40,6 +40,6 @@ for skill, takeoff, thrust, mood, pos in data:
     m = MarkerStyle(SUCCESS_SYMBOLS[mood], transform=t)
     ax.plot(pos[0], pos[1], marker=m, color=cmap(thrust))
 fig.colorbar(plt.cm.ScalarMappable(norm=Normalize(0, 1), cmap=cmap),
-    ax=ax, label="Normalized Thrust [a.u.]")
+             ax=ax, label="Normalized Thrust [a.u.]")
 ax.set_xlabel("X position [m]")
 ax.set_ylabel("Y position [m]")
