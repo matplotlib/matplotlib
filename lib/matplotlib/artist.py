0b9a6239e8f213ed98fce760ff053c859a62a3dc
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2021-09-16 20:29:43 +0200

    Backport PR #21089: Update sticky_edges docstring to new behavior.

--- lib/matplotlib/artist.py
+++ lib/matplotlib/artist.py
@@ -1119,6 +1119,11 @@ class Artist:
         where one usually expects no margin on the bottom edge (0) of the
         histogram.
 
+        Moreover, margin expansion "bumps" against sticky edges and cannot
+        cross them.  For example, if the upper data limit is 1.0, the upper
+        view limit computed by simple margin application is 1.2, but there is a
+        sticky edge at 1.1, then the actual upper view limit will be 1.1.
+
         This attribute cannot be assigned to; however, the ``x`` and ``y``
         lists can be modified in place as needed.
 
