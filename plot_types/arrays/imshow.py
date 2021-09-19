cb41936915edd738da9167c7a0a057b1b1c57a50
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2021-09-20 00:28:51 +0200

    Minor style fixes in plot types

--- plot_types/arrays/imshow.py
+++ plot_types/arrays/imshow.py
@@ -13,7 +13,7 @@ plt.style.use('_mpl-gallery-nogrid')
 
 # make data
 X, Y = np.meshgrid(np.linspace(-3, 3, 256), np.linspace(-3, 3, 256))
-Z = (1 - X/2. + X**5 + Y**3) * np.exp(-X**2 - Y**2)
+Z = (1 - X/2 + X**5 + Y**3) * np.exp(-X**2 - Y**2)
 Z = Z[::16, ::16]
 
 # plot
