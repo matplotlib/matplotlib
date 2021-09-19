9dd1441ed36dc8a0273925052960dd5c6c49baa7
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2021-09-20 00:47:12 +0200

    Simplify unstructured data generation
    There is no need to generate a regular grid and statistically choose
    points from that. Instead, we can immediately choose random points.

--- plot_types/unstructured/tripcolor.py
+++ plot_types/unstructured/tripcolor.py
@@ -10,17 +10,11 @@ import numpy as np
 
 plt.style.use('_mpl-gallery-nogrid')
 
-# make structured data
-X, Y = np.meshgrid(np.linspace(-3, 3, 256), np.linspace(-3, 3, 256))
-Z = (1 - X/2 + X**5 + Y**3) * np.exp(-X**2 - Y**2)
-
-# sample it to make unstructured x, y, z
+# make data:
 np.random.seed(1)
-ysamp = np.random.randint(0, 256, size=250)
-xsamp = np.random.randint(0, 256, size=250)
-y = Y[:, 0][ysamp]
-x = X[0, :][xsamp]
-z = Z[ysamp, xsamp]
+x = np.random.uniform(-3, 3, 256)
+y = np.random.uniform(-3, 3, 256)
+z = (1 - x/2 + x**5 + y**3) * np.exp(-x**2 - y**2)
 
 # plot:
 fig, ax = plt.subplots()
