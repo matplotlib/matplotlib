203c5de8a68bb8c22fa06c6c516b7617a1a056f2
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-10-01 19:01:54 -0400

    Ensure internal FreeType uses same compiler as Python
    Otherwise, mixed compilers might break things when compiled together
    into the extension.
    
    Fixes #21202

--- setupext.py
+++ setupext.py
@@ -605,9 +605,16 @@ class FreeType(SetupPackage):
         if (src_path / 'objs' / '.libs' / libfreetype).is_file():
             return  # Bail out because we have already built FreeType.
 
+        cc = get_ccompiler()
+
         print(f"Building freetype in {src_path}")
         if sys.platform != 'win32':  # compilation on non-windows
-            env = {**env, "CFLAGS": "{} -fPIC".format(env.get("CFLAGS", ""))}
+            env = {
+                **env,
+                "CC": (shlex.join(cc.compiler) if sys.version_info >= (3, 8)
+                       else " ".join(shlex.quote(x) for x in cc.compiler)),
+                "CFLAGS": "{} -fPIC".format(env.get("CFLAGS", "")),
+            }
             subprocess.check_call(
                 ["./configure", "--with-zlib=no", "--with-bzip2=no",
                  "--with-png=no", "--with-harfbuzz=no", "--enable-static",
@@ -660,7 +667,6 @@ class FreeType(SetupPackage):
                 f.truncate()
                 f.write(vcxproj)
 
-            cc = get_ccompiler()
             cc.initialize()  # Get msbuild in the %PATH% of cc.spawn.
             cc.spawn(["msbuild", str(sln_path),
                       "/t:Clean;Build",
