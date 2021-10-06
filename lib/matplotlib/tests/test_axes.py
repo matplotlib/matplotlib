8837c9f07b36ce6cc9bc341b0b69862926094e55
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-10-05 22:14:49 -0400

    Fix snap argument to pcolormesh
    If it's passed as a keyword argument, then it needs to be removed from
    `kwargs`, or not passed again explicitly. Other handling of `snap` uses
    `setdefault`, so do that here too.

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -1249,22 +1249,23 @@ def test_pcolorflaterror():
         ax.pcolormesh(x, y, Z, shading='flat')
 
 
+@pytest.mark.parametrize('snap', [False, True])
 @check_figures_equal(extensions=["png"])
-def test_pcolorauto(fig_test, fig_ref):
+def test_pcolorauto(fig_test, fig_ref, snap):
     ax = fig_test.subplots()
     x = np.arange(0, 10)
     y = np.arange(0, 4)
     np.random.seed(19680801)
     Z = np.random.randn(3, 9)
     # this is the same as flat; note that auto is default
-    ax.pcolormesh(x, y, Z)
+    ax.pcolormesh(x, y, Z, snap=snap)
 
     ax = fig_ref.subplots()
     # specify the centers
     x2 = x[:-1] + np.diff(x) / 2
     y2 = y[:-1] + np.diff(y) / 2
     # this is same as nearest:
-    ax.pcolormesh(x2, y2, Z)
+    ax.pcolormesh(x2, y2, Z, snap=snap)
 
 
 @image_comparison(['canonical'])
