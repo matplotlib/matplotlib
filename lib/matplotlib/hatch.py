d6678ee56486506becaf26769ab9ea31148a5052
Aditya Singh <66196917+singhaditya28@users.noreply.github.com>
2021-10-09 10:30:46 +0530

    Update hatch.py

--- lib/matplotlib/hatch.py
+++ lib/matplotlib/hatch.py
@@ -222,3 +222,12 @@ def get_path(hatchpattern, density=6):
             cursor += pattern.num_vertices
 
     return Path(vertices, codes)
+def _set_hatch_linewidth(gc, hatch_linewidth):
+    if hasattr(gc, "set_hatch_linewidth"):
+        gc.set_hatch_linewidth(hatch_linewidth)
+    else:
+        _api.warn_deprecated(
+            "3.6", message="The current backend does not define "
+            "GraphicsContextRenderer.set_hatch_linewidth; support for such "
+            "backends is deprecated since %(since)s and will be removed "
+            "%(removal)s.")
