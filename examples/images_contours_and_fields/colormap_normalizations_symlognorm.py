e0dcff7ff5ad7f9d39efec900ab3af06895f22f4
Richard Penney <rwp@rwpenney.uk>
2021-10-17 10:54:42 +0100

    Tweaked flake8 issues

--- examples/images_contours_and_fields/colormap_normalizations_symlognorm.py
+++ examples/images_contours_and_fields/colormap_normalizations_symlognorm.py
@@ -22,15 +22,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
+
+def rbf(x, y):
+    return 1.0 / (1 + 5 * ((x ** 2) + (y ** 2)))
+
 N = 200
 gain = 8
 X, Y = np.mgrid[-3:3:complex(0, N), -2:2:complex(0, N)]
-rbf = lambda x, y: 1.0 / (1 + 5 * ((x ** 2) + (y ** 2)))
 Z1 = rbf(X + 0.5, Y + 0.5)
 Z2 = rbf(X - 0.5, Y - 0.5)
 Z = gain * Z1 - Z2
 
-shadeopts = { 'cmap': 'PRGn', 'shading': 'gouraud' }
+shadeopts = {'cmap': 'PRGn', 'shading': 'gouraud'}
 colormap = 'PRGn'
 lnrwidth = 0.2
 
@@ -66,14 +69,14 @@ fig, ax = plt.subplots(2, 1, sharex=True, sharey=True)
 
 pcm = ax[0].pcolormesh(X, Y, Z,
                        norm=colors.SymLogNorm(linthresh=lnrwidth, linscale=1,
-                                              vmin=-gain, vmax=gain, base=10),
+                                              vmin=-2, vmax=gain, base=10),
                        **shadeopts)
 fig.colorbar(pcm, ax=ax[0], extend='both')
 ax[0].text(-2.5, 1.5, 'symlog')
 
 pcm = ax[1].pcolormesh(X, Y, Z,
                        norm=colors.AsinhNorm(linear_width=lnrwidth,
-                                             vmin=-gain, vmax=gain),
+                                             vmin=-2, vmax=gain),
                        **shadeopts)
 fig.colorbar(pcm, ax=ax[1], extend='both')
 ax[1].text(-2.5, 1.5, 'asinh')
