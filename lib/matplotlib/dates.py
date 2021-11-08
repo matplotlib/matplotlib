8858d8cc0594b450ce88a1c8bf30561a5f56027b
Simon Hoxbro <simon.hansen@me.com>
2021-11-08 09:55:26 +0100

    Enhance support for tex dates

--- lib/matplotlib/dates.py
+++ lib/matplotlib/dates.py
@@ -595,8 +595,9 @@ def _wrap_in_tex(text):
     p = r'([a-zA-Z]+)'
     ret_text = re.sub(p, r'}$\1$\\mathdefault{', text)
 
-    # Braces ensure dashes are not spaced like binary operators.
-    ret_text = '$\\mathdefault{'+ret_text.replace('-', '{-}')+'}$'
+    # Braces ensure symbols are not spaced like binary operators.
+    ret_text = ret_text.replace('-', '{-}').replace(':', '{:}').replace(' ', r'\;')
+    ret_text = '$\\mathdefault{' + ret_text + '}$'
     ret_text = ret_text.replace('$\\mathdefault{}$', '')
     return ret_text
 
