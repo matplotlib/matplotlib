faabfd9a7c195f01996ce8d1341416c608d110a4
Antony Lee <anntzer.lee@gmail.com>
2021-11-22 21:20:54 +0100

    Simplify macosx toolbar init.
    Pass `canvas` as argument to `__init__`, relying on PyArg_ParseTuple to
    check type (and non-NULL-ness).
    
    Also declare some variables closer to point of use, and use
    NSMakeRect/NSMakeSize to create NSRects and NSSizes.

--- lib/matplotlib/backends/backend_macosx.py
+++ lib/matplotlib/backends/backend_macosx.py
@@ -86,11 +86,10 @@ class FigureManagerMac(_macosx.FigureManager, FigureManagerBase):
 class NavigationToolbar2Mac(_macosx.NavigationToolbar2, NavigationToolbar2):
 
     def __init__(self, canvas):
-        self.canvas = canvas  # Needed by the _macosx __init__.
         data_path = cbook._get_data_path('images')
         _, tooltips, image_names, _ = zip(*NavigationToolbar2.toolitems)
         _macosx.NavigationToolbar2.__init__(
-            self,
+            self, canvas,
             tuple(str(data_path / image_name) + ".pdf"
                   for image_name in image_names if image_name is not None),
             tuple(tooltip for tooltip in tooltips if tooltip is not None))
