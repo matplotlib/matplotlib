a527add1e287f745187c9f5cc03c7b2841ea648a
sveneschlbeck <sven.eschlbeck@t-online.de>
2021-12-16 12:05:10 +0100

    Added test for color value error

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -2212,6 +2212,8 @@ class TestScatter:
             plt.scatter([1, 2], [1, 2], color=[0.1, 0.2])
         with pytest.raises(ValueError):
             plt.scatter([1, 2, 3], [1, 2, 3], color=[1, 2, 3])
+        with pytest.raises(ValueError):
+            plt.scatter([1, 2], [1, 2], color='blue\n')
 
     def test_scatter_unfilled(self):
         coll = plt.scatter([0, 1, 2], [1, 3, 2], c=['0.1', '0.3', '0.5'],
