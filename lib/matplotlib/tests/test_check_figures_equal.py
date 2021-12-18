58398b1efd8b9bef8792310cc54d1285c98a08f2
gajendra0180 <gajendrahitz@gmail.com>
2021-12-18 19:40:57 +0530

    flake8 Formatting

--- lib/matplotlib/tests/test_check_figures_equal.py
+++ lib/matplotlib/tests/test_check_figures_equal.py
@@ -1,5 +1,9 @@
-import matplotlib.pyplot as plt
-def test_check_figures_equal():
-    res1=plt.triplot([0,2,1],[0,0,1],[[0,1,2]], ls='--')
-    res2=plt.triplot([0,2,1],[0,0,1],[[0,1,2]], linestyle='--')
 
+
+import matplotlib.pyplot as plt
+
+
+def test_check_figures_equal():
+
+    res1 = plt.triplot([0, 2, 1], [0, 0, 1], [[0, 1, 2]], ls='--')
+    res2 = plt.triplot([0, 2, 1], [0, 0, 1], [[0, 1, 2]], linestyle='--')
