222968c8705a92d0e38d86bf0ac6db8749604fd8
gajendra0180 <gajendrahitz@gmail.com>
2021-12-19 11:31:37 +0530

    check_figures_equal decorator added

--- lib/matplotlib/tests/test_check_figures_equal.py
+++
@@ -1,9 +0,0 @@
-
-
-import matplotlib.pyplot as plt
-
-
-def test_check_figures_equal():
-
-    res1 = plt.triplot([0, 2, 1], [0, 0, 1], [[0, 1, 2]], ls='--')
-    res2 = plt.triplot([0, 2, 1], [0, 0, 1], [[0, 1, 2]], linestyle='--')
