4630a4a28b2afbdb41e37a45ef440c403d39d9f5
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-12-21 01:18:00 -0500

    Backport PR #22009: FIX: Prevent set_alpha from changing color of legend patch

--- lib/matplotlib/legend_handler.py
+++ lib/matplotlib/legend_handler.py
@@ -815,6 +815,8 @@ class HandlerPolyCollection(HandlerBase):
         # Directly set Patch color attributes (must be RGBA tuples).
         legend_handle._facecolor = first_color(orig_handle.get_facecolor())
         legend_handle._edgecolor = first_color(orig_handle.get_edgecolor())
+        legend_handle._original_facecolor = orig_handle._original_facecolor
+        legend_handle._original_edgecolor = orig_handle._original_edgecolor
         legend_handle._fill = orig_handle.get_fill()
         legend_handle._hatch = orig_handle.get_hatch()
         # Hatch color is anomalous in having no getters and setters.
