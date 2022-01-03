462a38a9711b4c2005b51bb6a20e6e4627a383e1
Jody Klymak <jklymak@gmail.com>
2022-01-03 21:52:20 +0100

    FIX: aspect ratio locked when colorbar created, not axes

--- lib/matplotlib/colorbar.py
+++ lib/matplotlib/colorbar.py
@@ -261,7 +261,6 @@ class _ColorbarAxesLocator:
         # we need to reset the aspect ratio of the axes to account
         # of the extends...
 
-        aspect = ax.get_box_aspect()
         ax._colorbar_info['aspect'] = aspect
         # now shrink and/or offset to take into account the
         # extend tri/rectangles.
