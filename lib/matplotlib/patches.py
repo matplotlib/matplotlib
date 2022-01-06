7328fe4f6b1e3779692e0996064c6b1ecb6937cb
David Stansby <dstansby@gmail.com>
2022-01-06 12:04:33 +0000

    Add edge midpoint helper methods

--- lib/matplotlib/patches.py
+++ lib/matplotlib/patches.py
@@ -810,6 +810,14 @@ class Rectangle(Patch):
         return self.get_patch_transform().transform(
             [(0, 0), (1, 0), (1, 1), (0, 1)])
 
+    def _get_edge_midpoints(self):
+        """
+        Return the edge midpoints of the rectangle, moving anti-clockwise from
+        the center of the left-hand edge.
+        """
+        return self.get_patch_transform().transform(
+            [(0, 0.5), (0.5, 0), (1, 0.5), (0.5, 1)])
+
     def get_center(self):
         """Return the centre of the rectangle."""
         return self.get_patch_transform().transform((0.5, 0.5))
@@ -1674,6 +1682,14 @@ class Ellipse(Patch):
         """
         return self.get_patch_transform().transform(
             [(-1, -1), (1, -1), (1, 1), (-1, 1)])
+
+    def _get_edge_midpoints(self):
+        """
+        Return the corners of the ellipse, moving anti-clockwise from
+        the center of the left-hand edge before rotation.
+        """
+        return self.get_patch_transform().transform(
+            [(-1, 0), (0, -1), (1, 0), (0, 1)])
 
 
 class Annulus(Patch):
