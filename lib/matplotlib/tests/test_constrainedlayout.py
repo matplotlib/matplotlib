cc034ad668991531554b70db36beceeb9112e8f3
Jody Klymak <jklymak@gmail.com>
2022-01-07 15:17:13 +0100

    FIX: add test for identical subgridspec

--- lib/matplotlib/tests/test_constrainedlayout.py
+++ lib/matplotlib/tests/test_constrainedlayout.py
@@ -115,6 +115,26 @@ def test_constrained_layout6():
                  ticks=ticker.MaxNLocator(nbins=5))
 
 
+def test_identical_subgridspec():
+
+    fig = plt.figure(constrained_layout=True)
+
+    GS = fig.add_gridspec(2, 1)
+
+    GSA = GS[0].subgridspec(1, 3)
+    GSB = GS[1].subgridspec(1, 3)
+
+    axa = []
+    axb = []
+    for i in range(3):
+        axa += [fig.add_subplot(GSA[i])]
+        axb += [fig.add_subplot(GSB[i])]
+
+    fig.draw_without_rendering()
+    # chech first row above second
+    assert axa[0].get_position().y0 > axb[0].get_position().y1
+
+
 def test_constrained_layout7():
     """Test for proper warning if fig not set in GridSpec"""
     with pytest.warns(
