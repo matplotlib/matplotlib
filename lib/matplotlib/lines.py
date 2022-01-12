4690f0d792a495c3169661c73ae7da4957d00400
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2022-01-12 19:20:34 +0100

    Backport PR #22209: DOC: Document default join style

--- lib/matplotlib/lines.py
+++ lib/matplotlib/lines.py
@@ -1293,6 +1293,8 @@ class Line2D(Artist):
         """
         How to join segments of the line if it `~Line2D.is_dashed`.
 
+        The default joinstyle is :rc:`lines.dash_joinstyle`.
+
         Parameters
         ----------
         s : `.JoinStyle` or %(JoinStyle)s
@@ -1306,6 +1308,8 @@ class Line2D(Artist):
     def set_solid_joinstyle(self, s):
         """
         How to join segments if the line is solid (not `~Line2D.is_dashed`).
+
+        The default joinstyle is :rc:`lines.solid_joinstyle`.
 
         Parameters
         ----------
