ecb53b6bf7445e50915b73bb9bf0c9642ab9b4cd
unknown <>
2022-01-19 23:06:26 +0100

    Expanded documentation of Axis.set_ticks()

--- lib/matplotlib/axis.py
+++ lib/matplotlib/axis.py
@@ -1826,6 +1826,21 @@ class Axis(martist.Artist):
             List of tick locations.
         labels : list of str, optional
             List of tick labels. If not set, the labels show the data value.
+            
+            .. warning::
+                Setting *labels=None* does not guarantee that labels for all
+                set ticks will be automatically generated - this depends
+                on the :class:`~matplotlib.ticker.Formatter` of the axis.
+                Some tick label formatters, e.g.
+                :class:`~matplotlib.ticker.ScalarFormatter` (default for
+                plots made with :meth:`~.Axes.plot`) will make
+                labels for custom ticks, while
+                others like e.g.
+                :class:`~matplotlib.ticker.LogFormatterSciNotation`
+                (default for :meth:`~.Axes.semilogy` plots) may not.
+                Supply all tick labels in the *labels* parameter if you
+                want to be sure.
+
         minor : bool, default: False
             If ``False``, set the major ticks; if ``True``, the minor ticks.
         **kwargs
@@ -1838,6 +1853,22 @@ class Axis(martist.Artist):
         choice to prevent the surprise of a non-visible tick. If you need
         other limits, you should set the limits explicitly after setting the
         ticks.
+        
+        This method implicitly calls :meth:`.set_major_locator` (or
+        :meth:`.set_minor_locator` if *minor=True*) with a newly-constructed
+        :class:`~matplotlib.ticker.FixedLocator` initialized to the provided
+        tick locations.
+        
+        Do not add ticks manually to an automatically ticked axis
+        by e.g. doing::
+        
+            special_ticks = [1.1, 2.5, 3.7]
+            ticks = axes.get_yticks()
+            ticks_ext = np.append(ticklist, special_ticks)
+            axes.set_yticks(ticks_ext)
+            
+        as this may give unpredictable results.
+        See :ghissue:`22262` for discussion.
         """
         result = self._set_tick_locations(ticks, minor=minor)
         if labels is not None:
