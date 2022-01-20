cbbebe2401ecaf69052983d87aa326a53c163cdb
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2022-01-21 00:43:35 +0100

    Merge "Scatter Symbol" and "Scatter Custom Symbol" examples
    Remove "Scatter plot with pie chart markers". This is only a variant of
    path-as-custom-symbol, which is already covered here. Also, all pie
    markers in the scatter plot have the same data distribution, so the
    pie is more a style thing and cannot convey additional
    datapoint-specific information.
    
    Also closes #22266 by using *sizes* instead of *s* and spelling out the
    color to make their meaning clearer.

--- examples/lines_bars_and_markers/scatter_custom_symbol.py
+++ examples/lines_bars_and_markers/scatter_custom_symbol.py
@@ -1,17 +1,42 @@
 """
-=====================
-Scatter Custom Symbol
-=====================
+=================================
+Scatter plots with custom symbols
+=================================
 
-Creating a custom ellipse symbol in scatter plot.
-
+.. redirect-from:: /gallery/lines_bars_and_markers/scatter_symbol
+.. redirect-from:: /gallery/lines_bars_and_markers/scatter_piecharts
 """
+
+##############################################################################
+# Using TeX symbols
+# -----------------
+# An easy way to customize scatter symbols is passing a TeX symbol name
+# enclosed in $-signs as a marker. Below we use ``marker=r'$\clubsuit$'``.
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 # Fixing random state for reproducibility
 np.random.seed(19680801)
+
+
+x = np.arange(0.0, 50.0, 2.0)
+y = x ** 1.3 + np.random.rand(*x.shape) * 30.0
+sizes = np.random.rand(*x.shape) * 800 + 500
+
+fig, ax = plt.subplots()
+ax.scatter(x, y, sizes, c="green", alpha=0.5, marker=r'$\clubsuit$',
+           label="Luck")
+ax.set_xlabel("Leprechauns")
+ax.set_ylabel("Gold")
+ax.legend()
+plt.show()
+
+##############################################################################
+# Using a custom path
+# -------------------
+# Alternatively, one can also pass a custom path of N vertices as a Nx2 array
+# of x, y values as *marker*.
 
 # unit area ellipse
 rx, ry = 3., 1.
