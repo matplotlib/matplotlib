a9d12dedd174eb384da4c88facaadcb428c2c3f9
Antony Lee <anntzer.lee@gmail.com>
2022-01-29 17:14:59 +0100

    Fix "trailing" whitespace in C docstrings.
    and harmonize description of functions that are noops on some OSes.

--- src/_c_internal_utils.c
+++ src/_c_internal_utils.c
@@ -183,31 +183,32 @@ static PyMethodDef functions[] = {
      "Check whether the current X11 or Wayland display is valid.\n\n"
      "On Linux, returns True if either $DISPLAY is set and XOpenDisplay(NULL)\n"
      "succeeds, or $WAYLAND_DISPLAY is set and wl_display_connect(NULL)\n"
-     "succeeds.  On other platforms, always returns True."},
+     "succeeds.\n\n"
+     "On other platforms, always returns True."},
     {"Win32_GetCurrentProcessExplicitAppUserModelID",
      (PyCFunction)mpl_GetCurrentProcessExplicitAppUserModelID, METH_NOARGS,
      "Win32_GetCurrentProcessExplicitAppUserModelID()\n--\n\n"
-     "Wrapper for Windows's GetCurrentProcessExplicitAppUserModelID.  On \n"
-     "non-Windows platforms, always returns None."},
+     "Wrapper for Windows's GetCurrentProcessExplicitAppUserModelID.\n\n"
+     "On non-Windows platforms, always returns None."},
     {"Win32_SetCurrentProcessExplicitAppUserModelID",
      (PyCFunction)mpl_SetCurrentProcessExplicitAppUserModelID, METH_O,
      "Win32_SetCurrentProcessExplicitAppUserModelID(appid, /)\n--\n\n"
-     "Wrapper for Windows's SetCurrentProcessExplicitAppUserModelID.  On \n"
-     "non-Windows platforms, a no-op."},
+     "Wrapper for Windows's SetCurrentProcessExplicitAppUserModelID.\n\n"
+     "On non-Windows platforms, does nothing."},
     {"Win32_GetForegroundWindow",
      (PyCFunction)mpl_GetForegroundWindow, METH_NOARGS,
      "Win32_GetForegroundWindow()\n--\n\n"
-     "Wrapper for Windows' GetForegroundWindow.  On non-Windows platforms, \n"
-     "always returns None."},
+     "Wrapper for Windows' GetForegroundWindow.\n\n"
+     "On non-Windows platforms, always returns None."},
     {"Win32_SetForegroundWindow",
      (PyCFunction)mpl_SetForegroundWindow, METH_O,
      "Win32_SetForegroundWindow(hwnd, /)\n--\n\n"
-     "Wrapper for Windows' SetForegroundWindow.  On non-Windows platforms, \n"
-     "a no-op."},
+     "Wrapper for Windows' SetForegroundWindow.\n\n"
+     "On non-Windows platforms, does nothing."},
     {"Win32_SetProcessDpiAwareness_max",
      (PyCFunction)mpl_SetProcessDpiAwareness_max, METH_NOARGS,
      "Win32_SetProcessDpiAwareness_max()\n--\n\n"
-     "Set Windows' process DPI awareness to best option available.\n"
+     "Set Windows' process DPI awareness to best option available.\n\n"
      "On non-Windows platforms, does nothing."},
     {NULL, NULL}};  // sentinel.
 static PyModuleDef util_module = {
