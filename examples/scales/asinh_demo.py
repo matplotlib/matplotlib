5a03297072a62f479196ccd79ed08288425402c3
Richard Penney <rwpenney@users.noreply.github.com>
2022-02-03 16:33:37 +0000

    Update examples/scales/asinh_demo.py
    Co-authored-by: Jody Klymak <jklymak@gmail.com>

--- examples/scales/asinh_demo.py
+++ examples/scales/asinh_demo.py
@@ -36,6 +36,8 @@ transition between linear and logarithmic regions of the plot.
 
 .. note::
    `.scale.AsinhScale` is experimental, and the API may change.
+.. note::
+   `.scale.AsinhScale` is experimental, and the API may change.
 See `~.scale.AsinhScale`, `~.scale.SymmetricalLogScale`.
 """
 
