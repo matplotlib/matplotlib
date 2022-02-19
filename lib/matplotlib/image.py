b0ac2bef9d5d22af2ee74f24c6f06549629f658a
Lumijek <lumijekpr@gmail.com>
2022-02-19 12:26:13 -0800

    Added get_shape method

--- lib/matplotlib/image.py
+++ lib/matplotlib/image.py
@@ -277,8 +277,8 @@ class _ImageBase(martist.Artist, cm.ScalarMappable):
 
     def __str__(self):
         try:
-            size = self.get_size()
-            return f"{type(self).__name__}(size={size!r})"
+            shape = self.get_shape()
+            return f"{type(self).__name__}(shape={shape!r})"
         except RuntimeError:
             return type(self).__name__
 
@@ -292,6 +292,15 @@ class _ImageBase(martist.Artist, cm.ScalarMappable):
             raise RuntimeError('You must first set the image array')
 
         return self._A.shape[:2]
+
+    def get_shape(self):
+        """Return the shape of the image as
+        tuple (numrows, numcols, channels).
+        """
+        if self._A is None:
+            raise RuntimeError('You must first set the image array')
+
+        return self._A.shape
 
     def set_alpha(self, alpha):
         """
