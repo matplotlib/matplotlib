84c0c55a5b154fcc859c0660c07b0918c97e0e1c
wqh17101 <597935261@qq.com>
2022-04-02 09:13:43 +0800

    Update setupext.py
    flask8  line too long

--- setupext.py
+++ setupext.py
@@ -617,11 +617,14 @@ class FreeType(SetupPackage):
                 },
                 **env,
             }
-            configure_ac = os.path.join(src_path, "builds", "unix", "configure.ac")
+            configure_ac = os.path.join(
+                src_path, "builds", "unix", "configure.ac")
             if os.path.exists(os.path.join(src_path, "autogen.sh")) \
                     and not os.path.exists(configure_ac):
-                print(f"{configure_ac} does not exist. Using sh autogen.sh to generate.")
-                subprocess.check_call(["sh", "./autogen.sh"], env=env, cwd=src_path)
+                print(f"{configure_ac} does not exist. "
+                      f"Using sh autogen.sh to generate.")
+                subprocess.check_call(
+                    ["sh", "./autogen.sh"], env=env, cwd=src_path)
             env["CFLAGS"] = env.get("CFLAGS", "") + " -fPIC"
             configure = [
                 "./configure", "--with-zlib=no", "--with-bzip2=no",
