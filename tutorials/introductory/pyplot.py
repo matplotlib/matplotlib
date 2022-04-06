ad64412a6ba0f1929e16bb45321c5a00e773d891
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2022-04-06 09:45:46 +0200

    Use bool for bool keyword arguments

--- tutorials/introductory/pyplot.py
+++ tutorials/introductory/pyplot.py
@@ -327,7 +327,7 @@ mu, sigma = 100, 15
 x = mu + sigma * np.random.randn(10000)
 
 # the histogram of the data
-n, bins, patches = plt.hist(x, 50, density=1, facecolor='g', alpha=0.75)
+n, bins, patches = plt.hist(x, 50, density=True, facecolor='g', alpha=0.75)
 
 
 plt.xlabel('Smarts')
--- tutorials/introductory/quick_start.py
+++ tutorials/introductory/quick_start.py
@@ -288,7 +288,7 @@ mu, sigma = 115, 15
 x = mu + sigma * np.random.randn(10000)
 fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
 # the histogram of the data
-n, bins, patches = ax.hist(x, 50, density=1, facecolor='C0', alpha=0.75)
+n, bins, patches = ax.hist(x, 50, density=True, facecolor='C0', alpha=0.75)
 
 ax.set_xlabel('Length [cm]')
 ax.set_ylabel('Probability')
