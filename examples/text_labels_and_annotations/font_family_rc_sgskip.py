761f3d61d8388f8a1259670f41992b416bcf7ad8
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2022-04-25 10:29:04 +0200

    new example for rcParams font

--- examples/text_labels_and_annotations/font_family_rc_sgskip.py
+++ examples/text_labels_and_annotations/font_family_rc_sgskip.py
@@ -30,8 +30,21 @@ import matplotlib.pyplot as plt
 plt.rcParams['font.family'] = 'sans-serif'
 plt.rcParams['font.sans-serif'] = ['Tahoma']
 
-fig, ax = plt.subplots()
-ax.plot([1, 2, 3], label='test')
+fig, ax = plt.subplots(figsize=(6, 1), facecolor="#eefade")
+ax.text(0.01, 0.2, "Hello World! 01", size = 40)
+ax.axis("off")
+plt.show()
 
-ax.legend()
+
+"""
+And here a second example:
+"""
+
+plt.rcParams['font.family'] = 'monospace'
+# plt.rcParams['font.monospace'] = ['Computer Modern Typewriter'] # this line gives an error currently, therefore the 
+# question: How can one access the fonts from https://github.com/matplotlib/matplotlib/blob/main/lib/matplotlib/mpl-data/matplotlibrc#L273 
+# the correct way?
+fig, ax = plt.subplots(figsize=(6, 1), facecolor="#eefade")
+ax.text(0.01, 0.2, "Hello World! 01", size = 40)
+ax.axis("off")
 plt.show()
