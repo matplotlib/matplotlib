763a0fce37e00d893873bddd69329710f38c5501
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-04-25 21:44:57 +0200

    rename and add further examples

--- examples/text_labels_and_annotations/font_family_rc.py
+++ examples/text_labels_and_annotations/font_family_rc.py
@@ -26,30 +26,48 @@ in order::
 
 .. redirect-from:: /examples/font_family_rc_sgskip
 
-First example:
+# First example, chooses default sans-serif font:
 """
 
 import matplotlib.pyplot as plt
 
+
+def print_text(text):
+    fig, ax = plt.subplots(figsize=(6, 1), facecolor="#eefade")
+    ax.text(0.01, 0.2, text, size=40)
+    ax.axis("off")
+    plt.show()
+
+
 plt.rcParams["font.family"] = "sans-serif"
-plt.rcParams["font.sans-serif"] = ["Tahoma"]
-
-fig, ax = plt.subplots(figsize=(6, 1), facecolor="#eefade")
-ax.text(0.01, 0.2, "Hello World! 01", size=40)
-ax.axis("off")
-plt.show()
-
-
+print_text("Hello World! 01")
 #############################################################################
 #
 # Second example:
 
+plt.rcParams["font.family"] = "sans-serif"
+plt.rcParams["font.sans-serif"] = "Tahoma"
+print_text("Hello World! 02")
+
+#############################################################################
+#
+# Third example:
+
+plt.rcParams["font.family"] = "sans-serif"
+plt.rcParams["font.sans-serif"] = "Lucida Grande"
+print_text("Hello World! 03")
+
+#############################################################################
+#
+# Fourth example, chooses default monospace font:
 
 plt.rcParams["font.family"] = "monospace"
-# plt.rcParams['font.monospace'] = ['Computer Modern Typewriter'] # this line gives an error currently, therefore the
-# question: How can one access the fonts from https://github.com/matplotlib/matplotlib/blob/main/lib/matplotlib/mpl-data/matplotlibrc#L273
-# the correct way?
-fig, ax = plt.subplots(figsize=(6, 1), facecolor="#eefade")
-ax.text(0.01, 0.2, "Hello World! 01", size=40)
-ax.axis("off")
-plt.show()
+print_text("Hello World! 04")
+
+#############################################################################
+#
+# Fifth example:
+
+plt.rcParams["font.family"] = "monospace"
+plt.rcParams["font.monospace"] = "Menlo"
+print_text("Hello World! 05")
