a0f8fdcd0013d6ead724ef33abbaeafcc15b10fc
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2022-04-29 12:55:29 +0200

    Update examples/text_labels_and_annotations/font_family_rc.py
    Co-authored-by: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>

--- examples/text_labels_and_annotations/font_family_rc.py
+++ examples/text_labels_and_annotations/font_family_rc.py
@@ -35,7 +35,7 @@ import matplotlib.pyplot as plt
 
 def print_text(text):
     fig, ax = plt.subplots(figsize=(6, 1), facecolor="#eefade")
-    ax.text(0.01, 0.2, text, size=40)
+    ax.text(0.5, 0.5, text, ha='center', va='center', size=40)
     ax.axis("off")
     plt.show()
 
