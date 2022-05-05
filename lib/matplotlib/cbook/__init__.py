29ce2c226aafc170cb9b874e81b158c832322cf7
Jody Klymak <jklymak@gmail.com>
2022-05-05 04:38:49 +0000

    FIX: fix check_1d to also check for ndim
    Arrays sometimes don't have all the methods arrays should have, so
    add another check here.  Plot requires both ndim and shape and this
    will extract the numpy array if x does not have those attributes.
    Otherwise leave the object alone, because unit support (currently only
    in plot) requires the object to retain the unit info.

--- lib/matplotlib/cbook/__init__.py
+++ lib/matplotlib/cbook/__init__.py
@@ -1333,7 +1333,12 @@ def _check_1d(x):
     """Convert scalars to 1D arrays; pass-through arrays as is."""
     # Unpack in case of e.g. Pandas or xarray object
     x = _unpack_to_numpy(x)
-    if not hasattr(x, 'shape') or len(x.shape) < 1:
+    # plot requires `shape` and `ndim`.  If passed an
+    # object that doesn't provide them, then force to numpy array.
+    # Note this will strip unit information.
+    if (not hasattr(x, 'shape') or
+            not hasattr(x, 'ndim') or
+            len(x.shape) < 1):
         return np.atleast_1d(x)
     else:
         return x
