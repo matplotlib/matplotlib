d57af62d399a990126b84d4acb018077d16c037d
kkoutris <kkoutris4@gmail.com>
2022-05-10 11:37:55 +0300

    Fix code to be on par with flake

--- examples/text_labels_and_annotations/angle_annotation.py
+++ examples/text_labels_and_annotations/angle_annotation.py
@@ -237,14 +237,14 @@ line1, = ax.plot(*zip(*p1))
 line2, = ax.plot(*zip(*p2))
 point, = ax.plot(*center, marker="o")
 
-am1 = AngleAnnotation(center, p1[1], p2[1], ax=ax, size=75, textposition="inside",
-                        text=r"$\alpha$")
-am2 = AngleAnnotation(center, p2[1], p1[0], ax=ax, size=35, textposition="inside", 
-                        text=r"$\beta$")
-am3 = AngleAnnotation(center, p1[0], p2[0], ax=ax, size=75, textposition="inside", 
-                        text=r"$\gamma$")
-am4 = AngleAnnotation(center, p2[0], p1[1], ax=ax, size=35, textposition="inside", 
-                        text=r"$\theta$")
+am1 = AngleAnnotation(center, p1[1], p2[1], ax=ax, size=75,
+                        textposition="inside", text=r"$\alpha$")
+am2 = AngleAnnotation(center, p2[1], p1[0], ax=ax, size=35,
+                        textposition="inside", text=r"$\beta$")
+am3 = AngleAnnotation(center, p1[0], p2[0], ax=ax, size=75,
+                        textposition="inside", text=r"$\gamma$")
+am4 = AngleAnnotation(center, p2[0], p1[1], ax=ax, size=35,
+                        textposition="inside", text=r"$\theta$")
 
 
 # Showcase some styling options for the angle arc, as well as the text.
