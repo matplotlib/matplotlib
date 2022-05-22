5fd189bb041e69ea898cbc5640f8b274bd3454ec
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2022-05-22 17:05:45 +0200

    Try to unbreak CI by xfailing OSX Tk tests
    Stopgap solution for #23094

--- lib/matplotlib/tests/test_backend_tk.py
+++ lib/matplotlib/tests/test_backend_tk.py
@@ -37,6 +37,10 @@ def _isolated_tk_test(success_count, func=None):
         sys.platform == "linux" and not _c_internal_utils.display_is_valid(),
         reason="$DISPLAY and $WAYLAND_DISPLAY are unset"
     )
+    @pytest.xfail(  # GitHub issue #23094
+        reason="Tk version mismatch on OSX CI",
+        condition=sys.platform == 'darwin'
+    )
     @functools.wraps(func)
     def test_func():
         # even if the package exists, may not actually be importable this can
