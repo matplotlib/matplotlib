f9fa791e121bf74a0aa9ea3b61b40f8733208ff9
Thomas A Caswell <tcaswell@gmail.com>
2022-05-24 14:51:49 -0400

    Backport PR #23122: Remove redundant rcparam default

--- examples/images_contours_and_fields/interpolation_methods.py
+++ examples/images_contours_and_fields/interpolation_methods.py
@@ -10,12 +10,12 @@ If *interpolation* is None, it defaults to the :rc:`image.interpolation`.
 If the interpolation is ``'none'``, then no interpolation is performed for the
 Agg, ps and pdf backends. Other backends will default to ``'antialiased'``.
 
-For the Agg, ps and pdf backends, ``interpolation = 'none'`` works well when a
-big image is scaled down, while ``interpolation = 'nearest'`` works well when
+For the Agg, ps and pdf backends, ``interpolation='none'`` works well when a
+big image is scaled down, while ``interpolation='nearest'`` works well when
 a small image is scaled up.
 
 See :doc:`/gallery/images_contours_and_fields/image_antialiasing` for a
-discussion on the default ``interpolation="antialiased"`` option.
+discussion on the default ``interpolation='antialiased'`` option.
 """
 
 import matplotlib.pyplot as plt
