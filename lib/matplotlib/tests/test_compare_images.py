62f949a9329736d95c9ddc405d8d515fd19379fb
Nickolaos Giannatos <t8190025@aueb.gr>
2022-06-15 18:14:14 +0300

    Change documentation

--- lib/matplotlib/tests/test_compare_images.py
+++ lib/matplotlib/tests/test_compare_images.py
@@ -57,11 +57,8 @@ def test_image_comparison_expect_rms(im1, im2, tol, expect_rms, tmp_path,
     """
     monkeypatch.chdir(tmp_path)
     baseline_dir, result_dir = map(Path, _image_directories(lambda: "dummy"))
-    # Copy both "baseline" and "test" image to result_dir, so that 1)
-    # compare_images writes the diff to result_dir, rather than to the source
-    # tree and 2) the baseline image doesn't appear missing to triage_tests.py.
-    # result_im1 = make_test_filename(result_dir / im1, "expected")
-    # shutil.copyfile(baseline_dir / im1, result_im1)
+    # Copy "test" image to result_dir, so that compare_images writes
+    # the diff to result_dir, rather than to the source tree
     result_im2 = result_dir / im1
     shutil.copyfile(baseline_dir / im2, result_im2)
     results = compare_images(
