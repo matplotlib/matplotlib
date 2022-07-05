c06f3bd577dde8fee9c9a8115242188088fe9305
Jody Klymak <jklymak@gmail.com>
2022-07-05 19:18:55 +0200

    FIX: make colorbars for log normed contour plots have logscale

--- lib/matplotlib/tests/test_contour.py
+++ lib/matplotlib/tests/test_contour.py
@@ -8,7 +8,7 @@ from numpy.testing import (
     assert_array_almost_equal, assert_array_almost_equal_nulp)
 import matplotlib as mpl
 from matplotlib.testing.decorators import image_comparison
-from matplotlib import pyplot as plt, rc_context
+from matplotlib import pyplot as plt, rc_context, ticker
 from matplotlib.colors import LogNorm, same_color
 import pytest
 
@@ -153,6 +153,28 @@ def test_given_colors_levels_and_extends():
                            levels=levels, extend=extend)
 
         plt.colorbar(c, ax=ax)
+
+
+@image_comparison(['contour_log_locator.svg'], style='mpl20',
+                  remove_text=False)
+def test_log_locator_levels():
+
+    fig, ax = plt.subplots()
+
+    N = 100
+    x = np.linspace(-3.0, 3.0, N)
+    y = np.linspace(-2.0, 2.0, N)
+
+    X, Y = np.meshgrid(x, y)
+
+    Z1 = np.exp(-X**2 - Y**2)
+    Z2 = np.exp(-(X * 10)**2 - (Y * 10)**2)
+    data = Z1 + 50 * Z2
+
+    c = ax.contourf(data, locator=ticker.LogLocator())
+    assert_array_almost_equal(c.levels, np.power(10.0, np.arange(-6, 3)))
+    cb = fig.colorbar(c, ax=ax)
+    assert_array_almost_equal(cb.ax.get_yticks(), c.levels)
 
 
 @image_comparison(['contour_datetime_axis.png'], style='mpl20')
