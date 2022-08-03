8dd068eb51edf2249978c765bb4d67784278b465
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2022-08-02 22:23:03 -0400

    Add general test for WebAgg backend

--- lib/matplotlib/tests/test_backend_webagg.py
+++ lib/matplotlib/tests/test_backend_webagg.py
@@ -1,14 +1,33 @@
 import os
+import shutil
+import subprocess
 import sys
+
 import pytest
 
 import matplotlib.backends.backend_webagg_core
 from matplotlib.testing import subprocess_run_for_testing
 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_webagg import WebAggApplication
+from matplotlib.testing.compare import compare_images
+from matplotlib.testing.decorators import _image_directories
+from matplotlib.testing.exceptions import ImageComparisonFailure
+
+
+pytest.importorskip("tornado")
+
+
+try:
+    import pytest_playwright  # noqa
+except ImportError:
+    @pytest.fixture
+    def page():
+        pytest.skip(reason='Missing pytest-playwright')
+
 
 @pytest.mark.parametrize("backend", ["webagg", "nbagg"])
 def test_webagg_fallback(backend):
-    pytest.importorskip("tornado")
     if backend == "nbagg":
         pytest.importorskip("IPython")
     env = dict(os.environ)
@@ -30,3 +49,43 @@ def test_webagg_fallback(backend):
 def test_webagg_core_no_toolbar():
     fm = matplotlib.backends.backend_webagg_core.FigureManagerWebAgg
     assert fm._toolbar2_class is None
+
+
+@pytest.mark.backend('webagg')
+def test_webagg_general(page):
+    from playwright.sync_api import expect
+
+    # Listen for all console logs.
+    page.on('console', lambda msg: print(f'CONSOLE: {msg.text}'))
+
+    fig, ax = plt.subplots(facecolor='w')
+
+    # Don't start the Tornado event loop, but use the existing event loop
+    # started by the `page` fixture.
+    WebAggApplication.initialize()
+    WebAggApplication.started = True
+
+    page.goto(f'http://{WebAggApplication.address}:{WebAggApplication.port}/')
+    expect(page).to_have_title('MPL | WebAgg current figures')
+
+    # Check title.
+    expect(page.locator('div.ui-dialog-title')).to_have_text('Figure 1')
+
+    # Check toolbar buttons.
+    assert page.locator('button.mpl-widget').count() == len([
+        name for name, *_ in fig.canvas.manager.ToolbarCls.toolitems
+        if name is not None])
+
+    # Check canvas actually contains something.
+    baseline_dir, result_dir = _image_directories(test_webagg_general)
+    browser = page.context.browser.browser_type.name
+    actual = result_dir / f'{browser}.png'
+    expected = result_dir / f'{browser}-expected.png'
+
+    canvas = page.locator('canvas.mpl-canvas')
+    canvas.screenshot(path=actual)
+    shutil.copyfile(baseline_dir / f'{browser}.png', expected)
+
+    err = compare_images(expected, actual, tol=0)
+    if err:
+        raise ImageComparisonFailure(err)
