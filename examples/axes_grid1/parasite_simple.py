935976cf782440413c586e86c47568d7a14dd789
Antony Lee <anntzer.lee@gmail.com>
2022-08-07 11:30:44 +0200

    Cleanup parasite_simple example.
    - Use `legend(labelcolor=...)` instead of setting colors manually.
    - Use `ax.legend` instead of `plt.legend`.
    - Order imports.

--- examples/axes_grid1/parasite_simple.py
+++ examples/axes_grid1/parasite_simple.py
@@ -2,13 +2,12 @@
 ===============
 Parasite Simple
 ===============
-
 """
-from mpl_toolkits.axes_grid1 import host_subplot
+
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import host_subplot
 
 host = host_subplot(111)
-
 par = host.twinx()
 
 host.set_xlabel("Distance")
@@ -18,12 +17,9 @@ par.set_ylabel("Temperature")
 p1, = host.plot([0, 1, 2], [0, 1, 2], label="Density")
 p2, = par.plot([0, 1, 2], [0, 3, 2], label="Temperature")
 
-leg = plt.legend()
+host.legend(labelcolor="linecolor")
 
 host.yaxis.get_label().set_color(p1.get_color())
-leg.texts[0].set_color(p1.get_color())
-
 par.yaxis.get_label().set_color(p2.get_color())
-leg.texts[1].set_color(p2.get_color())
 
 plt.show()
