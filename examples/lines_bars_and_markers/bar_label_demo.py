39980d45e57854e7c2383d8887a572be8ad14c80
SourajitaDewasi <sourajita19100@iiitnr.edu.in>
2022-08-09 23:38:45 +0530

    changing bar examples to tea and coffee

--- examples/lines_bars_and_markers/bar_label_demo.py
+++ examples/lines_bars_and_markers/bar_label_demo.py
@@ -21,25 +21,25 @@ import numpy as np
 # Define the data
 
 N = 5
-menMeans = (20, 35, 30, 35, -27)
-womenMeans = (25, 32, 34, 20, -25)
-menStd = (2, 3, 4, 1, 2)
-womenStd = (3, 5, 2, 3, 3)
+coffeeMeans = (20, 25, -10, 32, 10)
+teaMeans = (30, 13, -14, 21, 17)
+coffeeStd = (3, 2, 4, 1, 2)
+teaStd = (4, 3, 2, 3, 5)
 ind = np.arange(N)    # the x locations for the groups
-width = 0.35       # the width of the bars: can also be len(x) sequence
+width = 0.25       # the width of the bars: can also be len(x) sequence
 
 ###############################################################################
 # Stacked bar plot with error bars
 
 fig, ax = plt.subplots()
 
-p1 = ax.bar(ind, menMeans, width, yerr=menStd, label='Men')
-p2 = ax.bar(ind, womenMeans, width,
-            bottom=menMeans, yerr=womenStd, label='Women')
+p1 = ax.bar(ind, coffeeMeans, width, yerr=coffeeStd, label='Coffee')
+p2 = ax.bar(ind, teaMeans, width,
+            bottom=coffeeMeans, yerr=teaStd, label='Tea')
 
 ax.axhline(0, color='grey', linewidth=0.8)
 ax.set_ylabel('Scores')
-ax.set_title('Scores by group and gender')
+ax.set_title('Scores by group and their beverage choices')
 ax.set_xticks(ind, labels=['G1', 'G2', 'G3', 'G4', 'G5'])
 ax.legend()
 
