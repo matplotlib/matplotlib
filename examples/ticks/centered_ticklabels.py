5d367deb88915902872b60b6035fbf1570fa48ec
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2022-08-11 13:02:37 +0200

    Update example to use tick_params where possible

--- examples/ticks/centered_ticklabels.py
+++ examples/ticks/centered_ticklabels.py
@@ -7,7 +7,7 @@ Ticklabels are aligned relative to their associated tick. The alignment
 'center', 'left', or 'right' can be controlled using the horizontal alignment
 property::
 
-    for label in ax.xaxis.get_xticklabels():
+    for label in ax.get_xticklabels():
         label.set_horizontalalignment('right')
 
 However there is no direct way to center the labels between ticks. To fake
@@ -23,7 +23,7 @@ import matplotlib.dates as dates
 import matplotlib.ticker as ticker
 import matplotlib.pyplot as plt
 
-# load some financial data; Google's stock price
+# Load some financial data; Google's stock price
 r = (cbook.get_sample_data('goog.npz', np_load=True)['price_data']
      .view(np.recarray))
 r = r[-250:]  # get the last 250 days
@@ -38,11 +38,12 @@ ax.xaxis.set_minor_locator(dates.MonthLocator(bymonthday=16))
 ax.xaxis.set_major_formatter(ticker.NullFormatter())
 ax.xaxis.set_minor_formatter(dates.DateFormatter('%b'))
 
-for tick in ax.xaxis.get_minor_ticks():
-    tick.tick1line.set_markersize(0)
-    tick.tick2line.set_markersize(0)
-    tick.label1.set_horizontalalignment('center')
+# Remove the tick lines
+ax.tick_params(axis='x', which='minor', tick1On=False, tick2On=False)
 
+# Align the minor tick label
+for label in ax.get_xticklabels(minor=True):
+    label.set_horizontalalignment('center')
 imid = len(r) // 2
 ax.set_xlabel(str(r.date[imid].item().year))
 plt.show()
