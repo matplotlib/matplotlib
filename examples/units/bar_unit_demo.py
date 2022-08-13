a9bcdcf56f1f47d5ebc5f21cee982bc7f38be20f
SourajitaDewasi <sourajita19100@iiitnr.edu.in>
2022-08-13 13:59:09 +0530

    changing bar unit examples from men and women to bream_fish length and parkki_fish length

--- examples/units/bar_unit_demo.py
+++ examples/units/bar_unit_demo.py
@@ -24,14 +24,14 @@ bream_length_std = [2*cm, 3*cm, 1*cm, 1*cm, 4*cm]
 fig, ax = plt.subplots()
 
 ind = np.arange(N)    # the x locations for the groups
-width = 0.25         # the width of the bars
+width = 0.35         # the width of the bars
 ax.bar(ind, bream_length_means, width, bottom=0*cm, 
          yerr=bream_length_std, label='Bream_Fish')
 
 parkki_length_means = (14*cm, 13*cm, 17*cm, 16*cm, 20*cm)
 parkki_length_std = (1*cm, 2*cm, 2*cm, 3*cm, 4*cm)
 ax.bar(ind + width, parkki_length_means, width, bottom=0*cm, 
-      yerr=parkki_length_std, label='Parkki_Fish')
+         yerr=parkki_length_std, label='Parkki_Fish')
 
 ax.set_title('Scores by group and length of each fish type')
 ax.set_xticks(ind + width / 2, labels=['G1', 'G2', 'G3', 'G4', 'G5'])
