edbaa8394183c21231aced671aeb52b8e2ad06ff
SourajitaDewasi <sourajita19100@iiitnr.edu.in>
2022-08-13 14:16:24 +0530

    changing bar unit examples from men and women to bream_fish length and parkki_fish length

--- examples/units/bar_unit_demo.py
+++ examples/units/bar_unit_demo.py
@@ -16,24 +16,22 @@ import numpy as np
 from basic_units import cm, inch
 import matplotlib.pyplot as plt
 
-
-N = 5
-bream_length_means = [26*cm, 27*cm, 28*cm, 29*cm, 29*cm]
-bream_length_std = [2*cm, 3*cm, 1*cm, 1*cm, 4*cm]
+N=5
+bream_means = [26*cm, 27*cm, 28*cm, 29*cm, 29*cm]
+bream_std = [2*cm, 3*cm, 1*cm, 1*cm, 4*cm]
 
 fig, ax = plt.subplots()
 
 ind = np.arange(N)    # the x locations for the groups
 width = 0.35         # the width of the bars
-ax.bar(ind, bream_length_means, width, bottom=0*cm, 
-         yerr=bream_length_std, label='Bream_Fish')
+ax.bar(ind, bream_means, width, bottom=0*cm, yerr=bream_std, label='Bream')
 
-parkki_length_means = (14*cm, 13*cm, 17*cm, 16*cm, 20*cm)
-parkki_length_std = (1*cm, 2*cm, 2*cm, 3*cm, 4*cm)
-ax.bar(ind + width, parkki_length_means, width, bottom=0*cm, 
-         yerr=parkki_length_std, label='Parkki_Fish')
+parkki_means = (14*cm, 13*cm, 17*cm, 16*cm, 20*cm)
+parkki_std = (1*cm, 2*cm, 4*cm, 3*cm, 2*cm)
+ax.bar(ind + width, parkki_means, width, bottom=0*cm, yerr=parkki_std,
+       label='Parkki')
 
-ax.set_title('Scores by group and length of each fish type')
+ax.set_title('Scores by group and length of Bream and Parkki fish type')
 ax.set_xticks(ind + width / 2, labels=['G1', 'G2', 'G3', 'G4', 'G5'])
 
 ax.legend()
