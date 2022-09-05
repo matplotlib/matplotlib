a2b7c7899f2f46e9fcf80d510e91cc4921b546dd
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2022-09-05 17:34:42 -0400

    Add examples to bar_label_demo.py

--- examples/lines_bars_and_markers/bar_label_demo.py
+++ examples/lines_bars_and_markers/bar_label_demo.py
@@ -94,6 +94,30 @@ ax.set_xlim(right=16)
 
 plt.show()
 
+###############################################################################
+# Bar labels using {}-style format string
+
+fruit_names = ['Coffee', 'Salted Caramel', 'Pistachio']
+fruit_counts = [4000, 2000, 7000]
+
+fig, ax = plt.subplots()
+bar_container = ax.bar(fruit_names, fruit_counts)
+ax.set(ylabel='pints sold', title='Gelato sales by flavor', ylim=(0, 8000))
+ax.bar_label(bar_container, fmt='{:,.0f}')
+
+###############################################################################
+# Bar labels using a callable
+
+animal_names = ['Lion', 'Gazelle', 'Cheetah']
+mph_speed = [50, 60, 75]
+
+fig, ax = plt.subplots()
+bar_container = ax.bar(animal_names, mph_speed)
+ax.set(ylabel='speed in MPH', title='Running speeds', ylim=(0, 80))
+ax.bar_label(
+    bar_container, fmt=lambda x: '{:.1f} km/h'.format(x * 1.61)
+)
+
 #############################################################################
 #
 # .. admonition:: References
