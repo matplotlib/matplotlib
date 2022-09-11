8ecd60dc48e8767282656c8633415fda1be8beeb
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2022-09-11 12:55:11 +0200

    Add test and example for VBoxDivider

--- examples/axes_grid1/demo_axes_hbox_divider.py
+++ examples/axes_grid1/demo_axes_hbox_divider.py
@@ -1,43 +1,54 @@
 """
-===================
-`.HBoxDivider` demo
-===================
+================================
+HBoxDivider and VBoxDivider demo
+================================
 
 Using an `.HBoxDivider` to arrange subplots.
+
+Note that both axes' location are adjusted so that they have
+equal heights while maintaining their aspect ratios.
+
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1.axes_divider import HBoxDivider
+from mpl_toolkits.axes_grid1.axes_divider import HBoxDivider, VBoxDivider
 import mpl_toolkits.axes_grid1.axes_size as Size
 
 
-def make_heights_equal(fig, rect, ax1, ax2, pad):
-    # pad in inches
-    divider = HBoxDivider(
-        fig, rect,
-        horizontal=[Size.AxesX(ax1), Size.Fixed(pad), Size.AxesX(ax2)],
-        vertical=[Size.AxesY(ax1), Size.Scaled(1), Size.AxesY(ax2)])
-    ax1.set_axes_locator(divider.new_locator(0))
-    ax2.set_axes_locator(divider.new_locator(2))
+arr1 = np.arange(20).reshape((4, 5))
+arr2 = np.arange(20).reshape((5, 4))
 
+fig, (ax1, ax2) = plt.subplots(1, 2)
+ax1.imshow(arr1)
+ax2.imshow(arr2)
 
-if __name__ == "__main__":
+pad = 0.5  # pad in inches
+divider = HBoxDivider(
+    fig, 111,
+    horizontal=[Size.AxesX(ax1), Size.Fixed(pad), Size.AxesX(ax2)],
+    vertical=[Size.AxesY(ax1), Size.Scaled(1), Size.AxesY(ax2)])
+ax1.set_axes_locator(divider.new_locator(0))
+ax2.set_axes_locator(divider.new_locator(2))
 
-    arr1 = np.arange(20).reshape((4, 5))
-    arr2 = np.arange(20).reshape((5, 4))
+plt.show()
 
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.imshow(arr1)
-    ax2.imshow(arr2)
+###############################################################################
+# Using a `.VBoxDivider` to arrange subplots.
+#
+# Note that both axes' location are adjusted so that they have
+# equal widths while maintaining their aspect ratios.
 
-    make_heights_equal(fig, 111, ax1, ax2, pad=0.5)
+fig, (ax1, ax2) = plt.subplots(2, 1)
+ax1.imshow(arr1)
+ax2.imshow(arr2)
 
-    fig.text(.5, .5,
-             "Both axes' location are adjusted\n"
-             "so that they have equal heights\n"
-             "while maintaining their aspect ratios",
-             va="center", ha="center",
-             bbox=dict(boxstyle="round, pad=1", facecolor="w"))
+divider = VBoxDivider(
+    fig, 111,
+    horizontal=[Size.AxesX(ax1), Size.Scaled(1), Size.AxesX(ax2)],
+    vertical=[Size.AxesY(ax1), Size.Fixed(pad), Size.AxesY(ax2)])
 
-    plt.show()
+ax1.set_axes_locator(divider.new_locator(0))
+ax2.set_axes_locator(divider.new_locator(2))
+
+plt.show()
