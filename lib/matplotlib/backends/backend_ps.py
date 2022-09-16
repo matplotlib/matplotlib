d24be50e70ba6cc4680a2debb76850f462b6f804
Thomas A Caswell <tcaswell@gmail.com>
2022-09-16 11:18:17 -0400

    FIX: do not append None to stream in ps
    This was reported as an issue and this seems like the problem, but we do not
    have a good test yet.

--- lib/matplotlib/backends/backend_ps.py
+++ lib/matplotlib/backends/backend_ps.py
@@ -665,8 +665,9 @@ grestore
                 curr_stream[1].append(
                     (item.x, item.ft_object.get_glyph_name(item.glyph_idx))
                 )
-            # append the last entry
-            stream.append(curr_stream)
+            if curr_stream is not None:
+                # append the last entry
+                stream.append(curr_stream)
 
         self.set_color(*gc.get_rgb())
 
