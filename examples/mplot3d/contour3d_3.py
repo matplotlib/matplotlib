85e6634134510e7d968212e23258368b45c1eacf
baharev <ali.baharev@gmail.com>
2022-09-19 21:19:48 +0200

    Changing the edge color and the line width in response to the comments

--- examples/mplot3d/contour3d_3.py
+++ examples/mplot3d/contour3d_3.py
@@ -11,20 +11,21 @@ See :doc:`contourf3d_2` for the filled version.
 
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
-from matplotlib import cm
+from matplotlib.cm import coolwarm as cmap
 
 ax = plt.figure().add_subplot(projection='3d')
 X, Y, Z = axes3d.get_test_data(0.05)
 
 # Plot the 3D surface
-ax.plot_surface(X, Y, Z, edgecolor='black', rstride=8, cstride=8, alpha=0.3)
+ax.plot_surface(X, Y, Z, edgecolor=cmap(0), lw=0.5, rstride=8, cstride=8,
+                alpha=0.3)
 
 # Plot projections of the contours for each dimension.  By choosing offsets
 # that match the appropriate axes limits, the projected contours will sit on
 # the 'walls' of the graph.
-ax.contour(X, Y, Z, zdir='z', offset=-100, cmap=cm.coolwarm)
-ax.contour(X, Y, Z, zdir='x', offset=-40, cmap=cm.coolwarm)
-ax.contour(X, Y, Z, zdir='y', offset=40, cmap=cm.coolwarm)
+ax.contour(X, Y, Z, zdir='z', offset=-100, cmap=cmap)
+ax.contour(X, Y, Z, zdir='x', offset=-40, cmap=cmap)
+ax.contour(X, Y, Z, zdir='y', offset=40, cmap=cmap)
 
 ax.set(xlim=(-40, 40), ylim=(-40, 40), zlim=(-100, 100),
        xlabel='X', ylabel='Y', zlabel='Z')
