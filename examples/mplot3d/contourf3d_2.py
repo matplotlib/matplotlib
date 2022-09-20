ff7c43aa406ab1a922a9a4b396310ba7a89b6bf9
baharev <ali.baharev@gmail.com>
2022-09-20 09:58:28 +0200

    Specifying the color map and the edge color as strings

--- examples/mplot3d/contourf3d_2.py
+++ examples/mplot3d/contourf3d_2.py
@@ -2,30 +2,27 @@
 ===================================
 Project filled contour onto a graph
 ===================================
-
 Demonstrates displaying a 3D surface while also projecting filled contour
 'profiles' onto the 'walls' of the graph.
-
 See :doc:`contour3d_3` for the unfilled version.
 """
 
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
-from matplotlib.cm import coolwarm as cmap
 
 ax = plt.figure().add_subplot(projection='3d')
 X, Y, Z = axes3d.get_test_data(0.05)
 
 # Plot the 3D surface
-ax.plot_surface(X, Y, Z, edgecolor=cmap(0), lw=0.5, rstride=8, cstride=8,
+ax.plot_surface(X, Y, Z, edgecolor='royalblue', lw=0.5, rstride=8, cstride=8,
                 alpha=0.3)
 
 # Plot projections of the contours for each dimension.  By choosing offsets
 # that match the appropriate axes limits, the projected contours will sit on
 # the 'walls' of the graph
-ax.contourf(X, Y, Z, zdir='z', offset=-100, cmap=cmap)
-ax.contourf(X, Y, Z, zdir='x', offset=-40, cmap=cmap)
-ax.contourf(X, Y, Z, zdir='y', offset=40, cmap=cmap)
+ax.contourf(X, Y, Z, zdir='z', offset=-100, cmap='coolwarm')
+ax.contourf(X, Y, Z, zdir='x', offset=-40, cmap='coolwarm')
+ax.contourf(X, Y, Z, zdir='y', offset=40, cmap='coolwarm')
 
 ax.set(xlim=(-40, 40), ylim=(-40, 40), zlim=(-100, 100),
        xlabel='X', ylabel='Y', zlabel='Z')
