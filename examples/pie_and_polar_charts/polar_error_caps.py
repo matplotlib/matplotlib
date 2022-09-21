b7c27d5387b2fe4d08d95e6b9582058b410b94aa
Jakub Klus <jakub.klus@gmail.com>
2022-09-21 16:56:55 +0200

    More elaborate example of polar error caps.

--- examples/pie_and_polar_charts/polar_error_caps.py
+++ examples/pie_and_polar_charts/polar_error_caps.py
@@ -4,16 +4,43 @@ Error bar rendering on polar axis
 =================================
 
 Demo of error bar plot in polar coordinates.
+Theta error bars are curved lines ended with caps oriented towards the
+center.
+Radius error bars are straight lines oriented towards center with
+perpendicular caps.
 """
 import numpy as np
 import matplotlib.pyplot as plt
 
-fig = plt.figure(figsize=(10, 10))
-ax = plt.subplot(111, projection='polar')
-theta = np.arange(0, 2*np.pi, np.pi / 4)
+theta = np.arange(0, 2 * np.pi, np.pi / 4)
 r = theta / np.pi / 2 + 0.5
-ax.errorbar(theta, r, xerr=0.25, yerr=0.1, capsize=7, fmt="o")
+
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(projection='polar')
+ax.errorbar(theta, r, xerr=0.25, yerr=0.1, capsize=7, fmt="o", c="seagreen")
+ax.set_title("Pretty polar error bars")
 plt.show()
+
+#############################################################################
+# Please acknowledge that large theta error bars will be overlapping.
+# This may reduce readability of the output plot. See example figure below:
+
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(projection='polar')
+ax.errorbar(theta, r, xerr=5.25, yerr=0.1, capsize=7, fmt="o", c="darkred")
+ax.set_title("Overlapping theta error bars")
+plt.show()
+
+#############################################################################
+# On the other hand, large radius error bars will never overlap, they just
+# lead to unwanted scale in the data, reducing the displayed range.
+
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(projection='polar')
+ax.errorbar(theta, r, xerr=0.25, yerr=10.1, capsize=7, fmt="o", c="orangered")
+ax.set_title("Large radius error bars")
+plt.show()
+
 
 #############################################################################
 #
