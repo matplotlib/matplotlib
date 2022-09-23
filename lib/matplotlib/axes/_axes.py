c6339567de1cbe1a75e97020ef93388364b0261b
erykoff <erykoff@stanford.edu>
2022-09-22 21:39:13 -0700

    Backport PR #23944: FIX: ValueError when hexbin is run with empty arrays and log scaling.

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -4927,7 +4927,9 @@ default: :rc:`scatter.edgecolors`
         # autoscale the norm with current accum values if it hasn't been set
         if norm is not None:
             if norm.vmin is None and norm.vmax is None:
-                norm.autoscale(accum)
+                norm.autoscale_None(accum)
+            norm.vmin = np.ma.masked if norm.vmin is None else norm.vmin
+            norm.vmax = np.ma.masked if norm.vmax is None else norm.vmax
 
         if bins is not None:
             if not np.iterable(bins):
