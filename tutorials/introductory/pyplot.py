4a6c18166fe206f372d5012fc1642d199a5c82c9
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2022-09-25 21:28:35 +0200

    Backport PR #24004: Increase consistency in tutorials and examples

--- tutorials/introductory/pyplot.py
+++ tutorials/introductory/pyplot.py
@@ -295,8 +295,10 @@ plt.show()
 #     plt.figure(2)                # a second figure
 #     plt.plot([4, 5, 6])          # creates a subplot() by default
 #
-#     plt.figure(1)                # figure 1 current; subplot(212) still current
-#     plt.subplot(211)             # make subplot(211) in figure1 current
+#     plt.figure(1)                # first figure current;
+#                                  # subplot(212) still current
+#     plt.subplot(211)             # make subplot(211) in the first figure
+#                                  # current
 #     plt.title('Easy as 1, 2, 3') # subplot 211 title
 #
 # You can clear the current figure with `~.pyplot.clf`
