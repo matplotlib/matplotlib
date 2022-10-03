3fee7584e2f03049f4dec0ccdf4055cb0e38e05b
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2022-10-03 20:13:14 +0200

    Add facecolor to axisline style

--- lib/mpl_toolkits/axisartist/tests/test_axislines.py
+++ lib/mpl_toolkits/axisartist/tests/test_axislines.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.testing.decorators import image_comparison
 from matplotlib.transforms import IdentityTransform
 
-from mpl_toolkits.axisartist.axislines import SubplotZero, Subplot
+from mpl_toolkits.axisartist.axislines import AxesZero, SubplotZero, Subplot
 from mpl_toolkits.axisartist import Axes, SubplotHost
 
 
@@ -90,3 +90,30 @@ def test_ParasiteAxesAuxTrans():
         ax1.set_ylim((0, 5))
 
     ax2.contour(xx, yy, data, colors='k')
+
+
+@image_comparison(['axisline_style.png'], remove_text=True, style='mpl20')
+def test_axisline_style():
+    fig = plt.figure(figsize=(2, 2))
+    ax = fig.add_subplot(axes_class=AxesZero)
+    ax.axis["xzero"].set_axisline_style("-|>")
+    ax.axis["xzero"].set_visible(True)
+    ax.axis["yzero"].set_axisline_style("->")
+    ax.axis["yzero"].set_visible(True)
+
+    for direction in ("left", "right", "bottom", "top"):
+        ax.axis[direction].set_visible(False)
+
+
+@image_comparison(['axisline_style_size_color.png'], remove_text=True,
+                  style='mpl20')
+def test_axisline_style_size_color():
+    fig = plt.figure(figsize=(2, 2))
+    ax = fig.add_subplot(axes_class=AxesZero)
+    ax.axis["xzero"].set_axisline_style("-|>", size=2.0, facecolor='r')
+    ax.axis["xzero"].set_visible(True)
+    ax.axis["yzero"].set_axisline_style("->, size=1.5")
+    ax.axis["yzero"].set_visible(True)
+
+    for direction in ("left", "right", "bottom", "top"):
+        ax.axis[direction].set_visible(False)
