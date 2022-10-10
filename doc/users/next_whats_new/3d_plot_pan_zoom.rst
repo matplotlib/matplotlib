4896ec1a2cfb8c454e385632d8df213c915ced52
Scott Shambaugh <14363975+scottshambaugh@users.noreply.github.com>
2022-10-10 01:38:29 -0600

    Add pan and zoom toolbar handling to 3D Axes (Replaces PR#22614) (#23449)
    * ENH: Add pan and zoom toolbar handling to 3D Axes
    
    1) This moves the pan logic that was already in the mouse move handler
    into the "drag_pan" method to make it available from the toolbar.
    
    2) This expands upon the panning logic to enable a zoom-to-box feature.
    The zoom-to-box is done relative to the Axes, so it shrinks/expands
    the box as a fraction of each delta, from lower-left Axes to lower-left
    zoom-box. Thus, it tries to handle non-centered zooms, which adds more
    cases to handle versus the current right-click zoom only scaling from
    the center of the projection.
    
    * Rewrite zooming with bounding box
    
    * Rewrite 3d panning to work with a roll angle
    
    * Whats new for zoom and pan buttons
    
    * Make pan button configurable
    
    * Do not jump when zooming and mouse goes over other subplot
    
    * Rework zooming for 3d plots
    
    * Handle x/y lock when zooming and panning
    
    * Update tests
    
    * Docstrings
    
    * Dont assume a scale_z
    
    * Limit zoom box
    
    * Test zoom pan key modifiers
    
    * Save some calculation by saving view axes
    
    * Deprecation warnings for Axes3D.eye, .vvec
    
    * Remove Axes3D._prepare_view_from_bbox for now
    
    * Comments and docstrings
    
    * Switch from uvn to uvw
    
    * Save aspect to axes
    
    * Constrain zooming with mouse when one of the equal aspect ratios is set
    
    * Cleanup
    
    * Cleanup
    
    * Consolidate finding equal aspect axis indices
    
    * linting
    
    * More intuitive scaling
    
    * Box zoom keeps existing aspect ratios
    
    * Linting
    
    * Code review comments
    
    * Revert parameters for view_transformation
    
    * Fix new 3d pan/zoom view going on view stack twice
    
    * Better clipping
    
    * Test 3d toolbar navigation
    
    * Privatize helper functions
    
    * Deprecations
    
    * Code review changes
    
    * Deprecation note
    
    * Undeprecate proj3d.view_transformation
    
    * Undeprecate proj3d.view_transformation
    
    * Update doc/api/next_api_changes/deprecations/23449-SS.rst
    
    
    Co-authored-by: Greg Lucas <greg.m.lucas@gmail.com>
    Co-authored-by: Scott Shambaugh <scottshambaugh@users.noreply.github.com>
    Co-authored-by: Oscar Gustafsson <oscar.gustafsson@gmail.com>

---
+++ doc/users/next_whats_new/3d_plot_pan_zoom.rst
@@ -0,0 +1,8 @@
+3D plot pan and zoom buttons
+----------------------------
+
+The pan and zoom buttons in the toolbar of 3D plots are now enabled.
+Unselect both to rotate the plot. When the zoom button is pressed,
+zoom in by using the left mouse button to draw a bounding box, and
+out by using the right mouse button to draw the box. When zooming a
+3D plot, the current view aspect ratios are kept fixed.
--- lib/mpl_toolkits/tests/test_mplot3d.py
+++ lib/mpl_toolkits/tests/test_mplot3d.py
@@ -5,7 +5,8 @@ import pytest
 
 from mpl_toolkits.mplot3d import Axes3D, axes3d, proj3d, art3d
 import matplotlib as mpl
-from matplotlib.backend_bases import MouseButton
+from matplotlib.backend_bases import (MouseButton, MouseEvent,
+                                      NavigationToolbar2)
 from matplotlib import cm
 from matplotlib import colors as mcolors, patches as mpatch
 from matplotlib.testing.decorators import image_comparison, check_figures_equal
@@ -977,7 +978,8 @@ def _test_proj_make_M():
     R = np.array([100, 100, 100])
     V = np.array([0, 0, 1])
     roll = 0
-    viewM = proj3d.view_transformation(E, R, V, roll)
+    u, v, w = proj3d._view_axes(E, R, V, roll)
+    viewM = proj3d._view_transformation_uvw(u, v, w, E)
     perspM = proj3d.persp_transformation(100, -100, 1)
     M = np.dot(perspM, viewM)
     return M
@@ -1043,7 +1045,8 @@ def test_proj_axes_cube_ortho():
     R = np.array([0, 0, 0])
     V = np.array([0, 0, 1])
     roll = 0
-    viewM = proj3d.view_transformation(E, R, V, roll)
+    u, v, w = proj3d._view_axes(E, R, V, roll)
+    viewM = proj3d._view_transformation_uvw(u, v, w, E)
     orthoM = proj3d.ortho_transformation(-1, 1)
     M = np.dot(orthoM, viewM)
 
@@ -1688,6 +1691,82 @@ def test_pan():
     assert x_center != pytest.approx(x_center0)
     assert y_center != pytest.approx(y_center0)
     assert z_center != pytest.approx(z_center0)
+
+
+@pytest.mark.parametrize("tool,button,key,expected",
+                         [("zoom", MouseButton.LEFT, None,  # zoom in
+                          ((0.00, 0.06), (0.01, 0.07), (0.02, 0.08))),
+                          ("zoom", MouseButton.LEFT, 'x',  # zoom in
+                          ((-0.01, 0.10), (-0.03, 0.08), (-0.06, 0.06))),
+                          ("zoom", MouseButton.LEFT, 'y',  # zoom in
+                          ((-0.07, 0.04), (-0.03, 0.08), (0.00, 0.11))),
+                          ("zoom", MouseButton.RIGHT, None,  # zoom out
+                          ((-0.09, 0.15), (-0.07, 0.17), (-0.06, 0.18))),
+                          ("pan", MouseButton.LEFT, None,
+                          ((-0.70, -0.58), (-1.03, -0.91), (-1.27, -1.15))),
+                          ("pan", MouseButton.LEFT, 'x',
+                          ((-0.96, -0.84), (-0.58, -0.46), (-0.06, 0.06))),
+                          ("pan", MouseButton.LEFT, 'y',
+                          ((0.20, 0.32), (-0.51, -0.39), (-1.27, -1.15)))])
+def test_toolbar_zoom_pan(tool, button, key, expected):
+    # NOTE: The expected zoom values are rough ballparks of moving in the view
+    #       to make sure we are getting the right direction of motion.
+    #       The specific values can and should change if the zoom movement
+    #       scaling factor gets updated.
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(0, 0, 0)
+    fig.canvas.draw()
+    xlim0, ylim0, zlim0 = ax.get_xlim3d(), ax.get_ylim3d(), ax.get_zlim3d()
+
+    # Mouse from (0, 0) to (1, 1)
+    d0 = (0, 0)
+    d1 = (1, 1)
+    # Convert to screen coordinates ("s").  Events are defined only with pixel
+    # precision, so round the pixel values, and below, check against the
+    # corresponding xdata/ydata, which are close but not equal to d0/d1.
+    s0 = ax.transData.transform(d0).astype(int)
+    s1 = ax.transData.transform(d1).astype(int)
+
+    # Set up the mouse movements
+    start_event = MouseEvent(
+        "button_press_event", fig.canvas, *s0, button, key=key)
+    stop_event = MouseEvent(
+        "button_release_event", fig.canvas, *s1, button, key=key)
+
+    tb = NavigationToolbar2(fig.canvas)
+    if tool == "zoom":
+        tb.zoom()
+        tb.press_zoom(start_event)
+        tb.drag_zoom(stop_event)
+        tb.release_zoom(stop_event)
+    else:
+        tb.pan()
+        tb.press_pan(start_event)
+        tb.drag_pan(stop_event)
+        tb.release_pan(stop_event)
+
+    # Should be close, but won't be exact due to screen integer resolution
+    xlim, ylim, zlim = expected
+    assert ax.get_xlim3d() == pytest.approx(xlim, abs=0.01)
+    assert ax.get_ylim3d() == pytest.approx(ylim, abs=0.01)
+    assert ax.get_zlim3d() == pytest.approx(zlim, abs=0.01)
+
+    # Ensure that back, forward, and home buttons work
+    tb.back()
+    assert ax.get_xlim3d() == pytest.approx(xlim0)
+    assert ax.get_ylim3d() == pytest.approx(ylim0)
+    assert ax.get_zlim3d() == pytest.approx(zlim0)
+
+    tb.forward()
+    assert ax.get_xlim3d() == pytest.approx(xlim, abs=0.01)
+    assert ax.get_ylim3d() == pytest.approx(ylim, abs=0.01)
+    assert ax.get_zlim3d() == pytest.approx(zlim, abs=0.01)
+
+    tb.home()
+    assert ax.get_xlim3d() == pytest.approx(xlim0)
+    assert ax.get_ylim3d() == pytest.approx(ylim0)
+    assert ax.get_zlim3d() == pytest.approx(zlim0)
 
 
 @mpl.style.context('default')
