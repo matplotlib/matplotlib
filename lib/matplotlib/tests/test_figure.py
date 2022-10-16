57a0a01ef976b14aebb69bd89f4b770d55c05ba7
Joshua Barrass <josh.barrass.work@gmail.com>
2022-10-16 15:30:21 +0100

    Add test case for height_ratios

--- lib/matplotlib/tests/test_figure.py
+++ lib/matplotlib/tests/test_figure.py
@@ -932,6 +932,16 @@ class TestSubplotMosaic:
         assert axd["A"].get_gridspec().get_width_ratios() == width_ratios
         assert axd["B"].get_gridspec().get_width_ratios() != width_ratios
 
+    def test_nested_height_ratios(self):
+        x = [["A", [["B"],
+                    ["C"]]], ["D", "D"]]
+        height_ratios = [1, 2]
+
+        fig, axd = plt.subplot_mosaic(x, height_ratios=height_ratios)
+
+        assert axd["D"].get_gridspec().get_height_ratios() == height_ratios
+        assert axd["B"].get_gridspec().get_height_ratios() != height_ratios
+
     @check_figures_equal(extensions=["png"])
     @pytest.mark.parametrize(
         "x, empty_sentinel",
