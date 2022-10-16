666ab555b670e4ef01fd0ef24f6fbd4b9f87a770
Joshua Barrass <josh.barrass.work@gmail.com>
2022-10-16 15:28:26 +0100

    Add test case for width_ratios

--- lib/matplotlib/tests/test_figure.py
+++ lib/matplotlib/tests/test_figure.py
@@ -922,6 +922,16 @@ class TestSubplotMosaic:
         fig_ref.subplot_mosaic([["F"], [x]])
         fig_test.subplot_mosaic([["F"], [xt]])
 
+    def test_nested_width_ratios(self):
+        x = [["A", [["B"],
+                    ["C"]]]]
+        width_ratios = [2, 1]
+
+        fig, axd = plt.subplot_mosaic(x, width_ratios=width_ratios)
+
+        assert axd["A"].get_gridspec().get_width_ratios() == width_ratios
+        assert axd["B"].get_gridspec().get_width_ratios() != width_ratios
+
     @check_figures_equal(extensions=["png"])
     @pytest.mark.parametrize(
         "x, empty_sentinel",
