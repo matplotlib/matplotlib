bcaede7f68650a87be10ba3d91cf3f9479a84575
Saksham Gupta <95184568+26Saksham@users.noreply.github.com>
2022-10-22 01:38:17 +0530

    Adding new feature
    Looking amazing and easy to visualize

--- plot_types/basic/scatter_plot.py
+++ plot_types/basic/scatter_plot.py
@@ -8,22 +8,25 @@ See `~matplotlib.axes.Axes.scatter`.
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.style.use('_mpl-gallery')
+
 
 # make the data
 np.random.seed(3)
 x = 4 + np.random.normal(0, 2, 24)
 y = 4 + np.random.normal(0, 2, len(x))
 # size and color:
-sizes = np.random.uniform(15, 80, len(x))
+sizes = 5*np.random.uniform(15, 80, len(x))
+
 colors = np.random.uniform(15, 80, len(x))
 
 # plot
 fig, ax = plt.subplots()
 
-ax.scatter(x, y, s=sizes, c=colors, vmin=0, vmax=100)
+ax.scatter(x, y, s=sizes,alpha=0.5, cmap='nipy_spectral', c=colors, vmin=0, vmax=100,marker='+')
 
-ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-       ylim=(0, 8), yticks=np.arange(1, 8))
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
 
+
+plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
 plt.show()
