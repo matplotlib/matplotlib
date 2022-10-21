0199a66f37667304c1ba3c308b151c2c6d92b4cc
Saksham Gupta <95184568+26Saksham@users.noreply.github.com>
2022-10-22 00:41:45 +0530

    Draw a scatter Plot
    Two scatter plot draw with some property like marker,label,color

---
+++ examples/pyplots/pyplot_scatter.py
@@ -0,0 +1,14 @@
+#Draw scatter plot 
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+plt.scatter(x,y,color="green",marker="*")
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
+y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
+plt.scatter(x, y,color='red',marker='+')
+plt.show()
