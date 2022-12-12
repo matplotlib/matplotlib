631b286c7a31b595eb926194e117f2ff1e6a9f72
John Paul Jepko <jpjepko@umich.edu>
2022-12-12 02:22:28 -0500

    Implement nested four-level cache
    Prevents putting too many files in a single folder

--- lib/matplotlib/texmanager.py
+++ lib/matplotlib/texmanager.py
@@ -176,8 +176,15 @@ class TexManager:
         Return a filename based on a hash of the string, fontsize, and dpi.
         """
         src = cls._get_tex_source(tex, fontsize) + str(dpi)
-        return os.path.join(
-            cls.texcache, hashlib.md5(src.encode('utf-8')).hexdigest())
+        filehash = hashlib.md5(src.encode('utf-8')).hexdigest()
+        filepath = Path(cls.texcache)
+
+        num_letters, num_levels = 2, 2
+        for i in range(0, num_letters*num_levels, num_letters):
+            filepath = filepath / Path(filehash[i:i+2])
+
+        filepath.mkdir(parents=True, exist_ok=True)
+        return os.path.join(filepath, filehash)
 
     @classmethod
     def get_font_preamble(cls):
