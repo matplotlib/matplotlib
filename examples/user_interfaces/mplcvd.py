e6eecba865851530b6284945f4b877d5fc19aba3
Thomas A Caswell <tcaswell@gmail.com>
2022-12-16 17:15:19 -0500

    DOC: add demo code to example

--- examples/user_interfaces/mplcvd.py
+++ examples/user_interfaces/mplcvd.py
@@ -51,7 +51,7 @@ def _get_color_filter(name):
 
         def filter(input: np.ndarray[M, N, D])-> np.ndarray[M, N, D]
 
-        where (M, N) are the image dimentions, and D is the color depth (3 for
+        where (M, N) are the image dimensions, and D is the color depth (3 for
         RGB, 4 for RGBA). Alpha is passed through unchanged and otherwise
         ignored.
     """
@@ -252,3 +252,48 @@ def _setup_wx(tb):
             id=item.Id,
         )
     tb.SetDropdownMenu(tool.Id, menu)
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    from matplotlib import cbook
+
+    plt.rcParams['figure.hooks'].append('mplcvd:setup')
+
+    fig, axd = plt.subplot_mosaic(
+        [
+            ['viridis', 'turbo'],
+            ['photo', 'lines']
+        ]
+    )
+
+    delta = 0.025
+    x = y = np.arange(-3.0, 3.0, delta)
+    X, Y = np.meshgrid(x, y)
+    Z1 = np.exp(-X**2 - Y**2)
+    Z2 = np.exp(-(X - 1)**2 - (Y - 1)**2)
+    Z = (Z1 - Z2) * 2
+
+    imv = axd['viridis'].imshow(
+        Z, interpolation='bilinear',
+        origin='lower', extent=[-3, 3, -3, 3],
+        vmax=abs(Z).max(), vmin=-abs(Z).max()
+    )
+    fig.colorbar(imv)
+    imt = axd['turbo'].imshow(
+        Z, interpolation='bilinear', cmap='turbo',
+        origin='lower', extent=[-3, 3, -3, 3],
+        vmax=abs(Z).max(), vmin=-abs(Z).max()
+    )
+    fig.colorbar(imt)
+
+    # A sample image
+    with cbook.get_sample_data('grace_hopper.jpg') as image_file:
+        photo = plt.imread(image_file)
+    axd['photo'].imshow(photo)
+
+    th = np.linspace(0, 2*np.pi, 1024)
+    for j in [1, 2, 4, 6]:
+        axd['lines'].plot(th, np.sin(th * j), label=f'$\\omega={j}$')
+    axd['lines'].legend(ncol=2, loc='upper right')
+    plt.show()
