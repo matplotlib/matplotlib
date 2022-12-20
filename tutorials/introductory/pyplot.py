5282404f4c771b997fab856397ac361fbfb0ea42
Arup Jana <96692541+Arup3201@users.noreply.github.com>
2022-12-20 16:16:52 +0530

    changed the table style
    I changed the style to show the different options in ~.Line.Line2D properties and made it a markdown table style because in the jupyter notebook, it is very much difficult to see what is happening. Maybe thos change should have been more better in the actual ipynb file.

--- tutorials/introductory/pyplot.py
+++ tutorials/introductory/pyplot.py
@@ -182,41 +182,39 @@ plt.show()
 #
 # Here are the available `~.lines.Line2D` properties.
 #
-# ======================  ==================================================
-# Property                Value Type
-# ======================  ==================================================
-# alpha                   float
-# animated                [True | False]
-# antialiased or aa       [True | False]
-# clip_box                a matplotlib.transform.Bbox instance
-# clip_on                 [True | False]
-# clip_path               a Path instance and a Transform instance, a Patch
-# color or c              any matplotlib color
-# contains                the hit testing function
-# dash_capstyle           [``'butt'`` | ``'round'`` | ``'projecting'``]
-# dash_joinstyle          [``'miter'`` | ``'round'`` | ``'bevel'``]
-# dashes                  sequence of on/off ink in points
-# data                    (np.array xdata, np.array ydata)
-# figure                  a matplotlib.figure.Figure instance
-# label                   any string
-# linestyle or ls         [ ``'-'`` | ``'--'`` | ``'-.'`` | ``':'`` | ``'steps'`` | ...]
-# linewidth or lw         float value in points
-# marker                  [ ``'+'`` | ``','`` | ``'.'`` | ``'1'`` | ``'2'`` | ``'3'`` | ``'4'`` ]
-# markeredgecolor or mec  any matplotlib color
-# markeredgewidth or mew  float value in points
-# markerfacecolor or mfc  any matplotlib color
-# markersize or ms        float
-# markevery               [ None | integer | (startind, stride) ]
-# picker                  used in interactive line selection
-# pickradius              the line pick selection radius
-# solid_capstyle          [``'butt'`` | ``'round'`` | ``'projecting'``]
-# solid_joinstyle         [``'miter'`` | ``'round'`` | ``'bevel'``]
-# transform               a matplotlib.transforms.Transform instance
-# visible                 [True | False]
-# xdata                   np.array
-# ydata                   np.array
-# zorder                  any number
-# ======================  ==================================================
+#| Property               | Value Type|
+#|------------------------|------------------------------------------------------------------|
+#|alpha                   |float|
+#|animated                |[True &#124; False]|
+#|antialiased or aa       |[True &#124; False]|
+#|clip_box                |a matplotlib.transform.Bbox instance|
+#|clip_on                 |[True &#124; False]|
+#|clip_path               |a Path instance and a Transform instance, a Patch|
+#|color or c              |any matplotlib color|
+#|contains                |the hit testing function|
+#|dash_capstyle           |[``'butt'`` &#124; ``'round'`` &#124; ``'projecting'``]|
+#|dash_joinstyle          |[``'miter'`` &#124; ``'round'`` &#124; ``'bevel'``]|
+#|dashes                  |sequence of on/off ink in points|
+#|data                    |(np.array xdata, np.array ydata)|
+#|figure                  |a matplotlib.figure.Figure instance|
+#|label                   |any string|
+#|linestyle or ls         |[ ``'-'`` &#124; ``'--'`` &#124; ``'-.'`` &#124; ``':'`` &#124; ``'steps'`` &#124; ...]|
+#|linewidth or lw         |float value in points|
+#|marker                  |[ ``'+'`` &#124; ``','`` &#124; ``'.'`` &#124; ``'1'`` &#124; ``'2'`` &#124; ``'3'`` &#124; ``'4'`` ]|
+#|markeredgecolor or mec  |any matplotlib color|
+#|markeredgewidth or mew  |float value in points|
+#|markerfacecolor or mfc  |any matplotlib color|
+#|markersize or ms        |float|
+#|markevery               |[ None &#124; integer &#124; (startind, stride) ]|
+#|picker                  |used in interactive line selection|
+#|pickradius              |the line pick selection radius|
+#|solid_capstyle          |[``'butt'`` &#124; ``'round'`` &#124; ``'projecting'``]|
+#|solid_joinstyle         |[``'miter'`` &#124; ``'round'`` &#124; ``'bevel'``]|
+#|transform               |a matplotlib.transforms.Transform instance|
+#|visible                 |[True &#124; False]|
+#|xdata                   |np.array|
+#|ydata                   |np.array|
+#|zorder                  |any number|
 #
 # To get a list of settable line properties, call the
 # `~.pyplot.setp` function with a line or lines as argument
