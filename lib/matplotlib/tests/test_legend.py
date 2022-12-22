fdb52149e3eaadd5045848d8567a90d04a32c533
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2022-12-22 22:23:18 +0100

    Add a test for checking the displayed legend is reversed

--- lib/matplotlib/tests/test_legend.py
+++ lib/matplotlib/tests/test_legend.py
@@ -294,7 +294,8 @@ def test_legend_remove():
     assert ax.get_legend() is None
 
 
-def test_reverse_legend():
+def test_reverse_legend_handles_and_labels():
+    """Check that the legend handles and labels are reversed."""
     fig, ax = plt.subplots()
     x = 1
     y = 1
@@ -307,8 +308,22 @@ def test_reverse_legend():
     leg = ax.legend(reverse=True)
     actual_labels = [t.get_text() for t in leg.get_texts()]
     actual_markers = [h.get_marker() for h in leg.legend_handles]
-    assert (actual_labels, actual_markers) == (list(reversed(labels)),
-           (list(reversed(markers))))
+    assert actual_labels == list(reversed(labels))
+    assert actual_markers == list(reversed(markers))
+
+
+@check_figures_equal(extensions=["png"])
+def test_reverse_legend_display(fig_test, fig_ref):
+    """Check that the rendered legend entries are reversed"""
+    ax = fig_test.subplots()
+    ax.plot([1], 'ro', label="first")
+    ax.plot([2], 'bx', label="second")
+    ax.legend(reverse=True)
+
+    ax = fig_ref.subplots()
+    ax.plot([2], 'bx', label="second")
+    ax.plot([1], 'ro', label="first")
+    ax.legend()
 
 
 class TestLegendFunction:
