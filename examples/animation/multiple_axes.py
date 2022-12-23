1e602a6ad47a95d2732cd37c0c4805d3019e58f4
Thomas A Caswell <tcaswell@gmail.com>
2022-12-22 22:05:35 -0500

    DOC: fix docs

--- examples/animation/multiple_axes.py
+++ examples/animation/multiple_axes.py
@@ -48,12 +48,12 @@ fig.add_artist(con)
 
 
 def animate(i):
-    pos = np.cos(i), np.sin(i)
-    point.set_data(*pos)
     x = np.linspace(0, i, int(i * 25 / np.pi))
     sine.set_data(x, np.sin(x))
-    con.xy1 = pos
-    con.xy2 = i, pos[1]
+    x, y = np.cos(i), np.sin(i)
+    point.set_data([x], [y])
+    con.xy1 = x, y
+    con.xy2 = i, y
     return point, sine, con
 
 
