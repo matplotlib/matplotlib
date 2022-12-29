6614b8c9ebdbcbe57b369cb2fb2bcf111657bd33
Olivier C <1868182+ocastany@users.noreply.github.com>
2022-12-29 23:49:32 +0100

    Update lib/mpl_toolkits/mplot3d/tests/test_axes3d.py
    Co-authored-by: Greg Lucas <greg.m.lucas@gmail.com>

--- lib/mpl_toolkits/mplot3d/tests/test_axes3d.py
+++ lib/mpl_toolkits/mplot3d/tests/test_axes3d.py
@@ -597,7 +597,6 @@ def test_plot_surface_None_arg(fig_test, fig_ref):
     z = x + y
     ax_test = fig_test.add_subplot(projection='3d')
     ax_test.plot_surface(x, y, z, facecolors=None)
-    
     ax_ref = fig_ref.add_subplot(projection='3d')
     ax_ref.plot_surface(x, y, z)
 
