526cb71e20561171bb79997bf5b452f38dce499c
David Stansby <dstansby@gmail.com>
2023-01-23 15:30:22 +0000

    Include midpoint in tests

--- lib/matplotlib/tests/test_colors.py
+++ lib/matplotlib/tests/test_colors.py
@@ -665,7 +665,7 @@ def test_TwoSlopeNorm_scale():
 def test_TwoSlopeNorm_scaleout_center():
     # test the vmin never goes above vcenter
     norm = mcolors.TwoSlopeNorm(vcenter=0)
-    norm([1, 2, 3, 5])
+    norm([0, 1, 2, 3, 5])
     assert norm.vmin == -5
     assert norm.vmax == 5
 
@@ -673,7 +673,7 @@ def test_TwoSlopeNorm_scaleout_center():
 def test_TwoSlopeNorm_scaleout_center_max():
     # test the vmax never goes below vcenter
     norm = mcolors.TwoSlopeNorm(vcenter=0)
-    norm([-1, -2, -3, -5])
+    norm([0, -1, -2, -3, -5])
     assert norm.vmax == 5
     assert norm.vmin == -5
 
