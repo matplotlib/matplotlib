4beb17bb02792db3e26e583de55b18bf179152bc
RadostW <51670923+RadostW@users.noreply.github.com>
2023-02-02 20:37:52 +0100

    remove traling whitespace

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -3722,10 +3722,10 @@ class Axes(_AxesBase):
         """
         Draw a box and whisker plot.
 
-        The box extends from the first quartile (Q1) to the third 
-        quartile (Q3) of the data, with a line at the median. The 
-        whiskers extend from the box to the farthest point within 
-        1.5x the inter-quartile range (IQR) distance. Flier points 
+        The box extends from the first quartile (Q1) to the third
+        quartile (Q3) of the data, with a line at the median. The
+        whiskers extend from the box to the farthest point within
+        1.5x the inter-quartile range (IQR) distance. Flier points
         are those past the end of the whiskers.
         See https://en.wikipedia.org/wiki/Box_plot for reference.
 
