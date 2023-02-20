d78bc01e4eaf17fccfbc37fd5404a8312e4ff3b5
Jody Klymak <jklymak@gmail.com>
2023-02-20 14:03:31 -0800

    Auto backport of pr 25109 on v3.7.0 doc (#25216)
    * Merge pull request #25109 from jklymak/doc-figure-cross-ref
    
    DOC: add more cross-ref and explanatory image
    (cherry picked from commit 76dd07e529b2bc5169ac7867b85cf8bc783a9460)
    
    * Backport PR #25109: DOC: add more cross-ref and explanatory image
    
    ---------
    
    Co-authored-by: hannah <story645@gmail.com>

--- tutorials/introductory/quick_start.py
+++ tutorials/introductory/quick_start.py
@@ -30,7 +30,12 @@ import numpy as np
 fig, ax = plt.subplots()  # Create a figure containing a single axes.
 ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
 
-###############################################################################
+# %%
+#
+# Note that to get this Figure to display, you may have to call ``plt.show()``,
+# depending on your backend.  For more details of Figures and backends, see
+# :ref:`figure_explanation`.
+#
 # .. _figure_parts:
 #
 # Parts of a Figure
@@ -53,11 +58,16 @@ ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
 #    fig = plt.figure()  # an empty figure with no Axes
 #    fig, ax = plt.subplots()  # a figure with a single Axes
 #    fig, axs = plt.subplots(2, 2)  # a figure with a 2x2 grid of Axes
+#    # a figure with one axes on the left, and two on the right:
+#    fig, axs = plt.subplot_mosaic([['left', 'right-top'],
+#                                   ['left', 'right_bottom]])
 #
 # It is often convenient to create the Axes together with the Figure, but you
 # can also manually add Axes later on.  Note that many
 # :doc:`Matplotlib backends </users/explain/backends>` support zooming and
 # panning on figure windows.
+#
+# For more on Figures, see :ref:`figure_explanation`.
 #
 # :class:`~matplotlib.axes.Axes`
 # ------------------------------
