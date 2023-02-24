4554f2159fef26d2cf0cc4ae98139bbf57f4fcd0
Larry Bradley <larry.bradley@gmail.com>
2023-02-24 13:33:36 -0500

    DOC: fix typo

--- galleries/examples/lines_bars_and_markers/fill_between_demo.py
+++ galleries/examples/lines_bars_and_markers/fill_between_demo.py
@@ -52,7 +52,7 @@ N = 21
 x = np.linspace(0, 10, 11)
 y = [3.9, 4.4, 10.8, 10.3, 11.2, 13.1, 14.1,  9.9, 13.9, 15.1, 12.5]
 
-# fit a linear curve an estimate its y-values and their error.
+# fit a linear curve and estimate its y-values and their error.
 a, b = np.polyfit(x, y, deg=1)
 y_est = a * x + b
 y_err = x.std() * np.sqrt(1/len(x) +
