4fcbd7596d0797fa811307f084a28a437544fc3c
pontusnorman123 <pontusnorman1@gmail.com>
2023-02-27 10:40:26 +0100

    ENH: added example file #7

---
+++ galleries/examples/shapes_and_collections/hollow_scatter.py
@@ -0,0 +1,31 @@
+"""
+============
+Scatter plot
+============
+
+This example showcases a simple scatter plot.
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+
+N = 50
+x = np.random.rand(N)
+y = np.random.rand(N)
+colors = np.random.rand(N)
+area = (30 * np.random.rand(N))**2  # 0 to 15 point radii
+
+plt.scatter(x, y, s=area, c=colors, alpha=0.5)
+plt.show()
+
+# %%
+#
+# .. admonition:: References
+#
+#    The use of the following functions, methods, classes and modules is shown
+#    in this example:
+#
+#    - `matplotlib.axes.Axes.scatter` / `matplotlib.pyplot.scatter`
