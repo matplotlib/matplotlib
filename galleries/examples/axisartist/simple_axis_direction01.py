e20ed5b05d12442f56985bdcb4ac37e41d19e5ec
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2023-03-05 12:29:18 +0100

    Better axis labels for examples

--- galleries/examples/axisartist/simple_axis_direction01.py
+++ galleries/examples/axisartist/simple_axis_direction01.py
@@ -1,7 +1,7 @@
 """
-=======================
-Simple Axis Direction01
-=======================
+=====================
+Simple axis direction
+=====================
 
 """
 import matplotlib.pyplot as plt
@@ -13,10 +13,10 @@ ax1 = fig.add_subplot(axes_class=axisartist.Axes)
 fig.subplots_adjust(right=0.8)
 
 ax1.axis["left"].major_ticklabels.set_axis_direction("top")
-ax1.axis["left"].label.set_text("Label")
+ax1.axis["left"].label.set_text("Left label")
 
 ax1.axis["right"].label.set_visible(True)
-ax1.axis["right"].label.set_text("Label")
+ax1.axis["right"].label.set_text("Right label")
 ax1.axis["right"].label.set_axis_direction("left")
 
 plt.show()
