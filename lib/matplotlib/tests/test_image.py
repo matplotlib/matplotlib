bfd3ccf4481ef38b1ab7eb368d76f9dda1c44461
saranti <tsarantis@proton.me>
2023-03-11 16:08:49 +1100

    merge tests and add exception test

--- lib/matplotlib/tests/test_image.py
+++ lib/matplotlib/tests/test_image.py
@@ -1470,19 +1470,13 @@ def test__resample_valid_output():
         resample(np.zeros((9, 9)), np.zeros((9, 9)).T)
 
 
-def test_axesimage_get_size():
-    # generate dummy image to test get_size method
-    ax = plt.gca()
-    im = AxesImage(ax)
-    z = np.arange(12, dtype=float).reshape((4, 3))
-    im.set_data(z)
-    assert im.get_size() == (4, 3)
-
-
 def test_axesimage_get_shape():
     # generate dummy image to test get_shape method
     ax = plt.gca()
     im = AxesImage(ax)
+    with pytest.raises(RuntimeError):
+        im.get_shape()
     z = np.arange(12, dtype=float).reshape((4, 3))
     im.set_data(z)
     assert im.get_shape() == (4, 3)
+    assert im.get_size() == im.get_shape()
