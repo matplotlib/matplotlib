a7ba07ca7a22e27dd958863d7ade5a35554eddc6
Tom Sarantis <tsarantis@proton.me>
2023-03-18 02:52:54 +1100

    Added get_shape as an alias for get_size + tests (#25425)
    * Added get_shapes method as alias to get_size
    
    * merge tests and add exception test
    
    * match the correct runtime error
    
    * remove reduntant lines by making get_size call get_shape
    
    * simplify further
    
    * fix previous commit error

--- lib/matplotlib/image.py
+++ lib/matplotlib/image.py
@@ -275,8 +275,8 @@ class _ImageBase(martist.Artist, cm.ScalarMappable):
 
     def __str__(self):
         try:
-            size = self.get_size()
-            return f"{type(self).__name__}(size={size!r})"
+            shape = self.get_shape()
+            return f"{type(self).__name__}(shape={shape!r})"
         except RuntimeError:
             return type(self).__name__
 
@@ -286,10 +286,16 @@ class _ImageBase(martist.Artist, cm.ScalarMappable):
 
     def get_size(self):
         """Return the size of the image as tuple (numrows, numcols)."""
+        return self.get_shape()[:2]
+
+    def get_shape(self):
+        """
+        Return the shape of the image as tuple (numrows, numcols, channels).
+        """
         if self._A is None:
             raise RuntimeError('You must first set the image array')
 
-        return self._A.shape[:2]
+        return self._A.shape
 
     def set_alpha(self, alpha):
         """
