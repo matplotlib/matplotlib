b18beae53acfa539da473ba555577130eca2713a
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2023-04-15 01:09:02 +0200

    Update watermark example
    Note: this replaces mpl-data/sample_data/logo2.png with the current
    logo. I think that's ok and we do not guarantee content stability of
    that data (internally, it's only used for the watermark example). But
    we could also add it as logo3.png at the cost of having 22kB more
    data.

--- galleries/examples/images_contours_and_fields/watermark_image.py
+++ galleries/examples/images_contours_and_fields/watermark_image.py
@@ -3,7 +3,8 @@
 Watermark image
 ===============
 
-Using a PNG file as a watermark.
+Overlay an image on a plot by moving it to the front (``zorder=3``) and making it
+semi-transparent (``alpha=0.7``).
 """
 
 import matplotlib.pyplot as plt
@@ -17,9 +18,13 @@ with cbook.get_sample_data('logo2.png') as file:
 
 fig, ax = plt.subplots()
 
-ax.plot(np.sin(10 * np.linspace(0, 1)), '-o', ms=20, alpha=0.7, mfc='orange')
+np.random.seed(19680801)
+x = np.arange(30)
+y = x + np.random.randn(30)
+ax.bar(x, y, color='#6bbc6b')
 ax.grid()
-fig.figimage(im, 10, 10, zorder=3, alpha=.5)
+
+fig.figimage(im, 25, 25, zorder=3, alpha=.7)
 
 plt.show()
 
