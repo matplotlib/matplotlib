e51f4e8daeb13efd0b72bfa7f3031c6feef2584f
Daniil Aleschechkin <aleshechkin.daniil@gmail.com>
2023-04-26 07:00:40 -0600

    Added extra check to ensure we're not wrapping inherited wrapped functions

--- lib/matplotlib/ticker.py
+++ lib/matplotlib/ticker.py
@@ -208,8 +208,8 @@ class TickHelper:
                 self._repr = \
                     TickHelper._get_representation(child.__name__, [], {})
 
-        if (child.__init__):
-            child.__init__ = _init_repr_wrapper
+        if (child.__init__ and not hasattr(child.__init__, "__wrapped__")):
+                child.__init__ = _init_repr_wrapper
         else:
             child.__init__ = _default_init
 
