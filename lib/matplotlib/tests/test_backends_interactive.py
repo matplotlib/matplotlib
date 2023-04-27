b64444c177d143d7b5935489737f52b75c3c8297
Kyle Sunden <git@ksunden.space>
2023-04-26 19:00:48 -0500

    Explicitly test for missing qt bindings

--- lib/matplotlib/tests/test_backends_interactive.py
+++ lib/matplotlib/tests/test_backends_interactive.py
@@ -343,6 +343,26 @@ def test_qt5backends_uses_qt5():
     _run_helper(_implcore, timeout=_test_timeout)
 
 
+def _impl_missing():
+    import sys
+    # Simulate uninstalled
+    sys.modules["PyQt6"] = None
+    sys.modules["PyQt5"] = None
+    sys.modules["PySide2"] = None
+    sys.modules["PySide6"] = None
+
+    import matplotlib.pyplot as plt
+    with pytest.raises(ImportError, match="Failed to import any of the following Qt"):
+        plt.switch_backend("qtagg")
+    # Specifically ensure that Pyside6/Pyqt6 are not in the error message for qt5agg
+    with pytest.raises(ImportError, match="^(?:(?!(PySide6|PyQt6)).)*$"):
+        plt.switch_backend("qt5agg")
+
+
+def test_qt_missing():
+    _run_helper(_impl_missing, timeout=_test_timeout)
+
+
 def _impl_test_cross_Qt_imports():
     import sys
     import importlib
