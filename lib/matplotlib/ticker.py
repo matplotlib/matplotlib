68252ae64fdda47af2d8f45c50a78e3b3e27e79a
Daniil Aleschechkin <aleshechkin.daniil@gmail.com>
2023-04-26 20:48:54 -0600

    Removed overriden default init for the wrapper

--- lib/matplotlib/ticker.py
+++ lib/matplotlib/ticker.py
@@ -206,16 +206,7 @@ class TickHelper:
                 self._repr = \
                     TickHelper._get_representation(child.__name__, args, kwargs)
 
-        def _default_init(self):
-            if (not hasattr(self, "_repr")):
-                self._repr = \
-                    TickHelper._get_representation(child.__name__, [], {})
-
-        if (child.__init__):
-            child.__init__ = _init_repr_wrapper
-        else:
-            child.__init__ = _default_init
-
+        child.__init__ = _init_repr_wrapper
     def __repr__(self):
         return self._repr
 
