de4e43cdc340928f16631802f5b9b6f557907324
Antony Lee <anntzer.lee@gmail.com>
2023-05-14 00:52:19 +0200

    Cleanup demo_axes_grid{,2}.
    - No point in tweaking the patch of the inner_titles because the patch
      is set to invisible (frameon=False).
    - No need to toggle the colorbar labels on (they are on by default).
    - Use clim=(min, max) instead of constructing an explicit Normalize
      instance.
    - Group axes tweaking in set() calls.
    - Remove unnecessary subplots_adjust.
    - Some reformatting.

--- galleries/examples/axes_grid1/demo_axes_grid2.py
+++ galleries/examples/axes_grid1/demo_axes_grid2.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from matplotlib import cbook
-import matplotlib.colors
 from mpl_toolkits.axes_grid1 import ImageGrid
 
 
@@ -35,16 +34,11 @@ ZS = [Z[i::3, :] for i in range(3)]
 extent = extent[0], extent[1]/3., extent[2], extent[3]
 
 # *** Demo 1: colorbar at each axes ***
-grid = ImageGrid(fig, 211,  # similar to subplot(211)
-                 nrows_ncols=(1, 3),
-                 axes_pad=0.05,
-                 label_mode="1",
-                 share_all=True,
-                 cbar_location="top",
-                 cbar_mode="each",
-                 cbar_size="7%",
-                 cbar_pad="1%",
-                 )
+grid = ImageGrid(
+    # 211 = at the position of fig.add_subplot(211)
+    fig, 211, nrows_ncols=(1, 3), axes_pad=0.05, label_mode="1", share_all=True,
+    cbar_location="top", cbar_mode="each", cbar_size="7%", cbar_pad="1%")
+grid[0].set(xticks=[-2, 0], yticks=[-2, 0, 2])
 
 for i, (ax, z) in enumerate(zip(grid, ZS)):
     im = ax.imshow(z, origin="lower", extent=extent)
@@ -54,46 +48,22 @@ for i, (ax, z) in enumerate(zip(grid, ZS)):
         cb.set_ticks([-1, 0, 1])
 
 for ax, im_title in zip(grid, ["Image 1", "Image 2", "Image 3"]):
-    t = add_inner_title(ax, im_title, loc='lower left')
-    t.patch.set_alpha(0.5)
-
-for ax, z in zip(grid, ZS):
-    ax.cax.toggle_label(True)
-
-grid[0].set_xticks([-2, 0])
-grid[0].set_yticks([-2, 0, 2])
+    add_inner_title(ax, im_title, loc='lower left')
 
 # *** Demo 2: shared colorbar ***
-grid2 = ImageGrid(fig, 212,
-                  nrows_ncols=(1, 3),
-                  axes_pad=0.05,
-                  label_mode="1",
-                  share_all=True,
-                  cbar_location="right",
-                  cbar_mode="single",
-                  cbar_size="10%",
-                  cbar_pad=0.05,
-                  )
+grid2 = ImageGrid(
+    fig, 212, nrows_ncols=(1, 3), axes_pad=0.05, label_mode="1", share_all=True,
+    cbar_location="right", cbar_mode="single", cbar_size="10%", cbar_pad=0.05)
+grid2[0].set(xlabel="X", ylabel="Y", xticks=[-2, 0], yticks=[-2, 0, 2])
 
-grid2[0].set_xlabel("X")
-grid2[0].set_ylabel("Y")
-
-vmax, vmin = np.max(ZS), np.min(ZS)
-norm = matplotlib.colors.Normalize(vmax=vmax, vmin=vmin)
-
+clim = (np.min(ZS), np.max(ZS))
 for ax, z in zip(grid2, ZS):
-    im = ax.imshow(z, norm=norm, origin="lower", extent=extent)
+    im = ax.imshow(z, clim=clim, origin="lower", extent=extent)
 
 # With cbar_mode="single", cax attribute of all axes are identical.
 ax.cax.colorbar(im)
-ax.cax.toggle_label(True)
 
 for ax, im_title in zip(grid2, ["(a)", "(b)", "(c)"]):
-    t = add_inner_title(ax, im_title, loc='upper left')
-    t.patch.set_ec("none")
-    t.patch.set_alpha(0.5)
-
-grid2[0].set_xticks([-2, 0])
-grid2[0].set_yticks([-2, 0, 2])
+    add_inner_title(ax, im_title, loc='upper left')
 
 plt.show()
