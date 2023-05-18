bdad4286deb6763342217bfd54177edf03ba4f1a
Thomas A Caswell <tcaswell@gmail.com>
2023-05-18 18:04:09 -0400

    TST: put the missing path in the error message

--- lib/matplotlib/tests/__init__.py
+++ lib/matplotlib/tests/__init__.py
@@ -3,14 +3,17 @@ import os
 
 
 if (base_path := os.environ.get("MPLTESTIMAGEPATH", None)) is None:
-    base_path = Path(__file__).parent
+    base_path = Path(__file__).parent / 'baseline_images'
 else:
-    base_path = Path(base_path) / 'matplotlib' / 'tests'
+    base_path = Path(base_path) / 'matplotlib' / 'tests' / 'baseline_images'
+
 
 # Check that the test directories exist.
-if not (base_path / 'baseline_images').exists():
+if not (base_path).exists():
     raise OSError(
-        'The baseline image directory does not exist. '
+        f'The baseline image directory ({base_path!r}) does not exist. '
         'This is most likely because the test data is not installed. '
         'You may need to install matplotlib from source to get the '
         'test data.')
+
+del base_path
