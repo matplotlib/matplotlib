17376eb9dd87c8baa50b330f39b47a77e27b24c4
Smeet nagda <81572407+smeet07@users.noreply.github.com>
2023-05-19 11:32:11 +0530

    formatting

--- doc/conf.py
+++ doc/conf.py
@@ -74,7 +74,7 @@ if 'skip_sub_dirs=1' in sys.argv:
 # Parse year using SOURCE_DATE_EPOCH, falling back to current time.
 # https://reproducible-builds.org/specs/source-date-epoch/
 sourceyear = datetime.fromtimestamp(
-    int(os.environ.get('SOURCE_DATE_EPOCH', time.time())),datetime.timezone.utc).year
+    int(os.environ.get('SOURCE_DATE_EPOCH', time.time())), datetime.timezone.utc).year
 
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it
