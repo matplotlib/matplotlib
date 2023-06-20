fcaf784dafd7aa57a599e9b3f5d82c212cbe9119
Tanvi <Tanvi@tanvishindodkar44@gmail.com>
2023-06-20 18:00:59 +0530

    change

--- galleries/examples/pyplots/pyplot_text.py
+++ galleries/examples/pyplots/pyplot_text.py
@@ -20,7 +20,7 @@ t = np.arange(0.0, 2.0, 0.01)
 s = np.sin(2*np.pi*t)
 
 plt.plot(t, s)
-plt.text(0, -1, r'Hello, world!', fontsize=15)
+plt.text(0, -1, r'Hello, world!', fontsize=15, weight='bold')
 plt.title(r'$\mathcal{A}\sin(\omega t)$', fontsize=20)
 plt.xlabel('Time [s]')
 plt.ylabel('Voltage [mV]')
