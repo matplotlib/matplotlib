99cb86c967163cfba1bda78393c13ec0112db906
NikosNikolaidis02 <t8200120@aueb.gr>
2023-06-26 13:04:09 +0300

    image.py back to initial

--- lib/matplotlib/image.py
+++ lib/matplotlib/image.py
@@ -553,11 +553,9 @@ class _ImageBase(martist.Artist, cm.ScalarMappable):
             else:
                 if A.ndim == 2:  # _interpolation_stage == 'rgba'
                     self.norm.autoscale_None(A)
-                    A = self.to_rgba(A, alpha=self.get_alpha())
-                elif A.shape[2] == 3:
+                    A = self.to_rgba(A)
+                if A.shape[2] == 3:
                     A = _rgb_to_rgba(A)
-                    if (alpha := self.get_alpha()) is not None:
-                        A[:, :, 3] = self.get_alpha()
                 alpha = self._get_scalar_alpha()
                 output_alpha = _resample(  # resample alpha channel
                     self, A[..., 3], out_shape, t, alpha=alpha)
