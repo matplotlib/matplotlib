b812e775d38516a9ffb708b61e314c55ba3b974a
Saunak <76943154+Saunakghosh10@users.noreply.github.com>
2023-07-15 15:13:28 +0530

    This commit introduces a custom bar plot function

--- galleries/examples/lines_bars_and_markers/bar_colors.py
+++ galleries/examples/lines_bars_and_markers/bar_colors.py
@@ -8,6 +8,7 @@ using the *color* and *label* parameters of `~matplotlib.pyplot.bar`.
 Note that labels with a preceding underscore won't show up in the legend.
 """
 
+import numpy as np
 import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots()
@@ -24,3 +25,36 @@ ax.set_title('Fruit supply by kind and color')
 ax.legend(title='Fruit color')
 
 plt.show()
+
+
+def custom_bar_plot(labels, values, colors=None):
+    """
+    Custom bar plot function that plots bars with optional custom colors.
+
+    Parameters:
+        labels (array-like): Labels for each bar.
+        values (array-like): Values for each bar.
+        colors (array-like, optional): Colors for each bar. If not provided, default colors are used.
+
+    Returns:
+        None
+    """
+    x = np.arange(len(labels))
+    if colors is None:
+        colors = plt.cm.viridis(np.linspace(0, 1, len(labels)))
+
+    plt.bar(x, values, color=colors)
+    plt.xlabel('Categories')
+    plt.ylabel('Values')
+    plt.title('Custom Bar Plot')
+    plt.xticks(x, labels)
+    plt.grid(True)
+    plt.show()
+
+
+# Example usage
+labels = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5']
+values = [15, 8, 12, 10, 5]
+colors = ['red', 'blue', 'green', 'orange', 'purple']
+
+custom_bar_plot(labels, values, colors)
