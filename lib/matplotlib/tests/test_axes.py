57aff05c55db13a21e5fd0c165cf811deef3c633
Colin Caprani <colin.caprani@monash.edu>
2023-07-22 09:19:38 +1000

    Fix remove_text

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -8133,8 +8133,7 @@ def test_ylabel_ha_with_position(ha):
     assert ax.yaxis.get_label().get_ha() == ha
 
 
-@image_comparison(baseline_images=['axes_labels_position'], remove_text=True,
-                  extensions=['png'])
+@image_comparison(baseline_images=['axes_labels_position'], extensions=['png'])
 def test_axes_labels_position():
     x, y = [-4, 4], [-0.2, 0.8]
     def do_plot(ax):
