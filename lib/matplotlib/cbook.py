22657e814729857b05b75b9f40e1b2d32f6e60d1
Mateusz Sokół <mat646@gmail.com>
2023-08-04 12:34:51 +0200

    ENH: Update numpy exceptions imports

--- lib/matplotlib/cbook.py
+++ lib/matplotlib/cbook.py
@@ -21,7 +21,13 @@ import traceback
 import types
 import weakref
 
+from packaging.version import parse as parse_version
 import numpy as np
+
+if parse_version(np.__version__) >= parse_version("1.25.0"):
+    from numpy.exceptions import VisibleDeprecationWarning
+else:
+    from numpy import VisibleDeprecationWarning
 
 import matplotlib
 from matplotlib import _api, _c_internal_utils
@@ -1064,7 +1070,7 @@ def _combine_masks(*args):
                 raise ValueError("Masked arrays must be 1-D")
             try:
                 x = np.asanyarray(x)
-            except (np.VisibleDeprecationWarning, ValueError):
+            except (VisibleDeprecationWarning, ValueError):
                 # NumPy 1.19 raises a warning about ragged arrays, but we want
                 # to accept basically anything here.
                 x = np.asanyarray(x, dtype=object)
@@ -1658,7 +1664,7 @@ def index_of(y):
         pass
     try:
         y = _check_1d(y)
-    except (np.VisibleDeprecationWarning, ValueError):
+    except (VisibleDeprecationWarning, ValueError):
         # NumPy 1.19 will warn on ragged input, and we can't actually use it.
         pass
     else:
