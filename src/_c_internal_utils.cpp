f33f6cfcef3670bcee0ba1f3643ff3d72851b3f1
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2023-09-14 07:25:26 -0400

    Convert internal utilities to pybind11

--- src/_c_internal_utils.cpp
+++ src/_c_internal_utils.cpp
@@ -1,11 +1,12 @@
+#include <stdexcept>
+
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 // Windows 10, for latest HiDPI API support.
 #define WINVER 0x0A00
 #define _WIN32_WINNT 0x0A00
 #endif
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
+#include <pybind11/pybind11.h>
 #ifdef __linux__
 #include <dlfcn.h>
 #endif
@@ -13,10 +14,16 @@
 #include <Objbase.h>
 #include <Shobjidl.h>
 #include <Windows.h>
+#define UNUSED_ON_NON_WINDOWS(x) x
+#else
+#define UNUSED_ON_NON_WINDOWS Py_UNUSED
 #endif
 
-static PyObject*
-mpl_display_is_valid(PyObject* module)
+namespace py = pybind11;
+using namespace pybind11::literals;
+
+static bool
+mpl_display_is_valid(void)
 {
 #ifdef __linux__
     void* libX11;
@@ -34,11 +41,10 @@ mpl_display_is_valid(PyObject* module)
             XCloseDisplay(display);
         }
         if (dlclose(libX11)) {
-            PyErr_SetString(PyExc_RuntimeError, dlerror());
-            return NULL;
+            throw std::runtime_error(dlerror());
         }
         if (display) {
-            Py_RETURN_TRUE;
+            return true;
         }
     }
     void* libwayland_client;
@@ -56,84 +62,74 @@ mpl_display_is_valid(PyObject* module)
             wl_display_disconnect(display);
         }
         if (dlclose(libwayland_client)) {
-            PyErr_SetString(PyExc_RuntimeError, dlerror());
-            return NULL;
+            throw std::runtime_error(dlerror());
         }
         if (display) {
-            Py_RETURN_TRUE;
+            return true;
         }
     }
-    Py_RETURN_FALSE;
+    return false;
 #else
-    Py_RETURN_TRUE;
+    return true;
 #endif
 }
 
-static PyObject*
-mpl_GetCurrentProcessExplicitAppUserModelID(PyObject* module)
+static py::object
+mpl_GetCurrentProcessExplicitAppUserModelID(void)
 {
 #ifdef _WIN32
     wchar_t* appid = NULL;
     HRESULT hr = GetCurrentProcessExplicitAppUserModelID(&appid);
     if (FAILED(hr)) {
-        return PyErr_SetFromWindowsErr(hr);
+        PyErr_SetFromWindowsErr(hr);
+        throw py::error_already_set();
     }
-    PyObject* py_appid = PyUnicode_FromWideChar(appid, -1);
+    auto py_appid = py::cast(appid);
     CoTaskMemFree(appid);
     return py_appid;
 #else
-    Py_RETURN_NONE;
+    return py::none();
 #endif
 }
 
-static PyObject*
-mpl_SetCurrentProcessExplicitAppUserModelID(PyObject* module, PyObject* arg)
+static void
+mpl_SetCurrentProcessExplicitAppUserModelID(const wchar_t* UNUSED_ON_NON_WINDOWS(appid))
 {
 #ifdef _WIN32
-    wchar_t* appid = PyUnicode_AsWideCharString(arg, NULL);
-    if (!appid) {
-        return NULL;
-    }
     HRESULT hr = SetCurrentProcessExplicitAppUserModelID(appid);
-    PyMem_Free(appid);
     if (FAILED(hr)) {
-        return PyErr_SetFromWindowsErr(hr);
+        PyErr_SetFromWindowsErr(hr);
+        throw py::error_already_set();
     }
-    Py_RETURN_NONE;
-#else
-    Py_RETURN_NONE;
 #endif
 }
 
-static PyObject*
-mpl_GetForegroundWindow(PyObject* module)
+static py::object
+mpl_GetForegroundWindow(void)
 {
 #ifdef _WIN32
-  return PyLong_FromVoidPtr(GetForegroundWindow());
+  return py::capsule(GetForegroundWindow(), "HWND");
 #else
-  Py_RETURN_NONE;
+  return py::none();
 #endif
 }
 
-static PyObject*
-mpl_SetForegroundWindow(PyObject* module, PyObject *arg)
+static void
+mpl_SetForegroundWindow(py::capsule UNUSED_ON_NON_WINDOWS(handle_p))
 {
 #ifdef _WIN32
-  HWND handle = PyLong_AsVoidPtr(arg);
-  if (PyErr_Occurred()) {
-    return NULL;
-  }
-  if (!SetForegroundWindow(handle)) {
-    return PyErr_Format(PyExc_RuntimeError, "Error setting window");
-  }
-  Py_RETURN_NONE;
-#else
-  Py_RETURN_NONE;
+    if (handle_p.name() != "HWND") {
+        throw std::runtime_error("Handle must be a value returned from Win32_GetForegroundWindow");
+    }
+    HWND handle = static_cast<HWND>(handle_p.get_pointer());
+    if (!SetForegroundWindow(handle)) {
+        throw std::runtime_error("Error setting window");
+    }
 #endif
 }
 
-static PyObject*
-mpl_SetProcessDpiAwareness_max(PyObject* module)
+static void
+mpl_SetProcessDpiAwareness_max(void)
 {
 #ifdef _WIN32
 #ifdef _DPI_AWARENESS_CONTEXTS_
@@ -171,49 +167,52 @@ mpl_SetProcessDpiAwareness_max(PyObject* module)
     SetProcessDPIAware();
 #endif
 #endif
-    Py_RETURN_NONE;
 }
 
-static PyMethodDef functions[] = {
-    {"display_is_valid", (PyCFunction)mpl_display_is_valid, METH_NOARGS,
-     "display_is_valid()\n--\n\n"
-     "Check whether the current X11 or Wayland display is valid.\n\n"
-     "On Linux, returns True if either $DISPLAY is set and XOpenDisplay(NULL)\n"
-     "succeeds, or $WAYLAND_DISPLAY is set and wl_display_connect(NULL)\n"
-     "succeeds.\n\n"
-     "On other platforms, always returns True."},
-    {"Win32_GetCurrentProcessExplicitAppUserModelID",
-     (PyCFunction)mpl_GetCurrentProcessExplicitAppUserModelID, METH_NOARGS,
-     "Win32_GetCurrentProcessExplicitAppUserModelID()\n--\n\n"
-     "Wrapper for Windows's GetCurrentProcessExplicitAppUserModelID.\n\n"
-     "On non-Windows platforms, always returns None."},
-    {"Win32_SetCurrentProcessExplicitAppUserModelID",
-     (PyCFunction)mpl_SetCurrentProcessExplicitAppUserModelID, METH_O,
-     "Win32_SetCurrentProcessExplicitAppUserModelID(appid, /)\n--\n\n"
-     "Wrapper for Windows's SetCurrentProcessExplicitAppUserModelID.\n\n"
-     "On non-Windows platforms, does nothing."},
-    {"Win32_GetForegroundWindow",
-     (PyCFunction)mpl_GetForegroundWindow, METH_NOARGS,
-     "Win32_GetForegroundWindow()\n--\n\n"
-     "Wrapper for Windows' GetForegroundWindow.\n\n"
-     "On non-Windows platforms, always returns None."},
-    {"Win32_SetForegroundWindow",
-     (PyCFunction)mpl_SetForegroundWindow, METH_O,
-     "Win32_SetForegroundWindow(hwnd, /)\n--\n\n"
-     "Wrapper for Windows' SetForegroundWindow.\n\n"
-     "On non-Windows platforms, does nothing."},
-    {"Win32_SetProcessDpiAwareness_max",
-     (PyCFunction)mpl_SetProcessDpiAwareness_max, METH_NOARGS,
-     "Win32_SetProcessDpiAwareness_max()\n--\n\n"
-     "Set Windows' process DPI awareness to best option available.\n\n"
-     "On non-Windows platforms, does nothing."},
-    {NULL, NULL}};  // sentinel.
-static PyModuleDef util_module = {
-    PyModuleDef_HEAD_INIT, "_c_internal_utils", NULL, 0, functions
-};
-
-#pragma GCC visibility push(default)
-PyMODINIT_FUNC PyInit__c_internal_utils(void)
+PYBIND11_MODULE(_c_internal_utils, m)
 {
-    return PyModule_Create(&util_module);
+    m.def(
+        "display_is_valid", &mpl_display_is_valid,
+        R"""(        --
+        Check whether the current X11 or Wayland display is valid.
+
+        On Linux, returns True if either $DISPLAY is set and XOpenDisplay(NULL)
+        succeeds, or $WAYLAND_DISPLAY is set and wl_display_connect(NULL)
+        succeeds.
+
+        On other platforms, always returns True.)""");
+    m.def(
+        "Win32_GetCurrentProcessExplicitAppUserModelID",
+        &mpl_GetCurrentProcessExplicitAppUserModelID,
+        R"""(        --
+        Wrapper for Windows's GetCurrentProcessExplicitAppUserModelID.
+
+        On non-Windows platforms, always returns None.)""");
+    m.def(
+        "Win32_SetCurrentProcessExplicitAppUserModelID",
+        &mpl_SetCurrentProcessExplicitAppUserModelID,
+        "appid"_a, py::pos_only(),
+        R"""(        --
+        Wrapper for Windows's SetCurrentProcessExplicitAppUserModelID.
+
+        On non-Windows platforms, does nothing.)""");
+    m.def(
+        "Win32_GetForegroundWindow", &mpl_GetForegroundWindow,
+        R"""(        --
+        Wrapper for Windows' GetForegroundWindow.
+
+        On non-Windows platforms, always returns None.)""");
+    m.def(
+        "Win32_SetForegroundWindow", &mpl_SetForegroundWindow,
+        "hwnd"_a,
+        R"""(        --
+        Wrapper for Windows' SetForegroundWindow.
+
+        On non-Windows platforms, does nothing.)""");
+    m.def(
+        "Win32_SetProcessDpiAwareness_max", &mpl_SetProcessDpiAwareness_max,
+        R"""(        --
+        Set Windows' process DPI awareness to best option available.
+
+        On non-Windows platforms, does nothing.)""");
 }
