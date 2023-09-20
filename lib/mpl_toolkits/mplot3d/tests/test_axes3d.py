2d4ac9f1efff0bfd1e8929e6c89fa56265d2f3f3
Chiraag Balu <86278218+chiraagbalu@users.noreply.github.com>
2023-09-19 17:51:21 -0700

    Update lib/mpl_toolkits/mplot3d/tests/test_axes3d.py
    Co-authored-by: Kyle Sunden <git@ksunden.space>

--- lib/mpl_toolkits/mplot3d/tests/test_axes3d.py
+++ lib/mpl_toolkits/mplot3d/tests/test_axes3d.py
@@ -2307,10 +2307,7 @@ def test_Poly3DCollection_init_value_error():
 def test_ndarray_color_kwargs_value_error():
     # smoke test
     # ensures ndarray can be passed to color in kwargs for 3d projection plot
-    try:
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(1, 0, 0, color=np.array([0, 0, 0, 1]))
-        fig.canvas.draw()
-    except ValueError:
-        pytest.fail("A ValueError was raised when it shouldn't have.")
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(1, 0, 0, color=np.array([0, 0, 0, 1]))
+    fig.canvas.draw()
