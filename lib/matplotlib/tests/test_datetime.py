74f562d6fbecfd7625d81e23b1a7cdf0f989cb41
Seohyeon Lee <96499556+seohyeon-lee-2025@users.noreply.github.com>
2023-09-22 14:47:27 -0700

    test_axes
    added 3 subplots with datetime array on the x axis, y axis, and both axes.

--- lib/matplotlib/tests/test_datetime.py
+++ lib/matplotlib/tests/test_datetime.py
@@ -279,9 +279,45 @@ class TestDatetimePlotting:
     @pytest.mark.xfail(reason="Test for scatter not written yet")
     @mpl.style.context("default")
     def test_scatter(self):
-        fig, ax = plt.subplots()
-        ax.scatter(...)
-
+        mpl.rcParams["date.converter"] = 'concise'
+        base = datetime.datetime(2005, 2, 1)
+        dates = [base + datetime.timedelta(hours=(2 * i)) for i in range(732)]
+        N = len(dates)
+        np.random.seed(19680801)
+        y = np.cumsum(np.random.randn(N))
+        
+        fig, axs = plt.subplots(3, 1, layout='constrained', figsize=(6, 6))
+        lims = [(np.datetime64('2005-02'), np.datetime64('2005-04')),
+                (np.datetime64('2005-02-03'), np.datetime64('2005-02-15')),
+                (np.datetime64('2005-02-03 11:00'), np.datetime64('2005-02-04 13:20'))]
+        
+        axs[0].set_title('Axes.scatter used with datetime on x axis')
+        
+        for elem in axs:
+            print (elem)
+        
+        #datetime on x axis
+        axs[0].scatter(dates, y)
+        #axs[0].set_xlim(lims[nn])
+            # rotate_labels...
+        for label in axs[0].get_xticklabels():
+            label.set_rotation(40)
+            label.set_horizontalalignment('right')
+     
+        #datetime on y axis 
+        axs[1].set_title('Axes.scatter used with datetime on y axis')
+        axs[1].scatter(y, dates)
+        for label in axs[1].get_xticklabels():
+            label.set_rotation(40)
+            label.set_horizontalalignment('right')
+        
+        #datetime on both x, y axes
+        axs[2].set_title('Axes.scatter used with datetime on both axes')
+        axs[2].scatter(dates, dates)
+        for label in axs[2].get_xticklabels():
+            label.set_rotation(40)
+            label.set_horizontalalignment('right')   
+ 
     @pytest.mark.xfail(reason="Test for semilogx not written yet")
     @mpl.style.context("default")
     def test_semilogx(self):
