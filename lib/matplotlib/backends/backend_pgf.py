8bf18f2873565ab7d9dc33c5e822d8cc20de87b7
Simon May <simon.may@protonmail.ch>
2023-09-23 03:52:25 -0400

    PGF: Set up font sizes to match font.size setting
    This allows the output .pgf file to be used in external documents
    with arbitrary font size settings (which should match the rc setting
    font.size). This avoids mismatches between matplotlib and external
    LaTeX documents for things that depend on the font size setting (e.g.
    unicode-math).
    
    If the LaTeX package scrextend is present, this also adjusts the
    standard LaTeX font commands (\tiny, ..., \normalsize, ..., \Huge)
    relative to font.size.

--- lib/matplotlib/backends/backend_pgf.py
+++ lib/matplotlib/backends/backend_pgf.py
@@ -23,6 +23,7 @@ from matplotlib.backends.backend_pdf import (
     _create_pdf_info_dict, _datetime_to_pdf)
 from matplotlib.path import Path
 from matplotlib.figure import Figure
+from matplotlib.font_manager import FontProperties
 from matplotlib._pylab_helpers import Gcf
 
 _log = logging.getLogger(__name__)
@@ -36,12 +37,27 @@ DOCUMENTCLASS = r"\documentclass{article}"
 
 def _get_preamble():
     """Prepare a LaTeX preamble based on the rcParams configuration."""
+    font_size_pt = FontProperties(
+        size=mpl.rcParams["font.size"]
+    ).get_size_in_points()
     return "\n".join([
         # Remove Matplotlib's custom command \mathdefault.  (Not using
         # \mathnormal instead since this looks odd with Computer Modern.)
         r"\def\mathdefault#1{#1}",
         # Use displaystyle for all math.
         r"\everymath=\expandafter{\the\everymath\displaystyle}",
+        # Set up font sizes to match font.size setting.
+        r"\makeatletter",
+        r"\IfFileExists{scrextend.sty}{",
+        r"  %s" % mpl.texmanager._usepackage_if_not_loaded(
+            "scrextend", option="fontsize=%fpt" % font_size_pt
+        ),
+        r"}{",
+        r"  \renewcommand{\normalsize}{\fontsize{%f}{%f}\selectfont}"
+        % (font_size_pt, 1.2 * font_size_pt),
+        r"  \normalsize",
+        r"}",
+        r"\makeatother",
         # Allow pgf.preamble to override the above definitions.
         mpl.rcParams["pgf.preamble"],
         *([
