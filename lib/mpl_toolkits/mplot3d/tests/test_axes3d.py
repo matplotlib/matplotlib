b438e12eb824d1861e4b7a2952da804d5a899520
Scott Shambaugh <scottshambaugh@users.noreply.github.com>
2023-09-24 02:57:04 -0600

    Implement dynamic clipping to axes box for 3D plots
    Make axlim_clip flag keyword only
    
    Updates
    
    test image
    
    test image restore

--- lib/mpl_toolkits/mplot3d/tests/test_axes3d.py
+++ lib/mpl_toolkits/mplot3d/tests/test_axes3d.py
@@ -1378,6 +1378,45 @@ def test_axes3d_isometric():
     ax.grid(True)
 
 
+@check_figures_equal(extensions=["png"])
+def test_axlim_clip(fig_test, fig_ref):
+    # With axlim clipping
+    ax = fig_test.add_subplot(projection="3d")
+    x = np.linspace(0, 1, 11)
+    y = np.linspace(0, 1, 11)
+    X, Y = np.meshgrid(x, y)
+    Z = X + Y
+    ax.plot_surface(X, Y, Z, facecolor='C1', edgecolors=None,
+                    rcount=50, ccount=50, axlim_clip=True)
+    # This ax.plot is to cover the extra surface edge which is not clipped out
+    ax.plot([0.5, 0.5], [0, 1], [0.5, 1.5],
+            color='k', linewidth=3, zorder=5, axlim_clip=True)
+    ax.scatter(X.ravel(), Y.ravel(), Z.ravel() + 1, axlim_clip=True)
+    ax.quiver(X.ravel(), Y.ravel(), Z.ravel() + 2,
+              0*X.ravel(), 0*Y.ravel(), 0*Z.ravel() + 1,
+              arrow_length_ratio=0, axlim_clip=True)
+    ax.plot(X[0], Y[0], Z[0] + 3, color='C2', axlim_clip=True)
+    ax.text(1.1, 0.5, 4, 'test', axlim_clip=True)  # won't be visible
+    ax.set(xlim=(0, 0.5), ylim=(0, 1), zlim=(0, 5))
+
+    # With manual clipping
+    ax = fig_ref.add_subplot(projection="3d")
+    idx = (X <= 0.5)
+    X = X[idx].reshape(11, 6)
+    Y = Y[idx].reshape(11, 6)
+    Z = Z[idx].reshape(11, 6)
+    ax.plot_surface(X, Y, Z, facecolor='C1', edgecolors=None,
+                    rcount=50, ccount=50, axlim_clip=False)
+    ax.plot([0.5, 0.5], [0, 1], [0.5, 1.5],
+            color='k', linewidth=3, zorder=5, axlim_clip=False)
+    ax.scatter(X.ravel(), Y.ravel(), Z.ravel() + 1, axlim_clip=False)
+    ax.quiver(X.ravel(), Y.ravel(), Z.ravel() + 2,
+              0*X.ravel(), 0*Y.ravel(), 0*Z.ravel() + 1,
+              arrow_length_ratio=0, axlim_clip=False)
+    ax.plot(X[0], Y[0], Z[0] + 3, color='C2', axlim_clip=False)
+    ax.set(xlim=(0, 0.5), ylim=(0, 1), zlim=(0, 5))
+
+
 @pytest.mark.parametrize('value', [np.inf, np.nan])
 @pytest.mark.parametrize(('setter', 'side'), [
     ('set_xlim3d', 'left'),
@@ -2137,10 +2176,10 @@ def test_computed_zorder():
         ax.add_collection3d(tri)
 
         # plot a vector
-        ax.plot((2, 2), (2, 2), (0, 4), c='red', zorder=2)
+        ax.plot((2, 2), (2, 2), (0, 4), c='red', zorder=2, axlim_clip=False)
 
         # plot some points
-        ax.scatter((3, 3), (1, 3), (1, 3), c='red', zorder=10)
+        ax.scatter((3, 3), (1, 3), (1, 3), c='red', zorder=10, axlim_clip=False)
 
         ax.set_xlim((0, 5.0))
         ax.set_ylim((0, 5.0))
