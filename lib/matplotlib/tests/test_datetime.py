b46f6d1aa668ce0614cec12292bbad87dd1cadc0
Seohyeon Lee <96499556+seohyeon-lee-2025@users.noreply.github.com>
2023-09-23 17:10:00 -0700

    fixed mistake in previous commit

--- lib/matplotlib/tests/test_datetime.py
+++ lib/matplotlib/tests/test_datetime.py
@@ -280,11 +280,12 @@ class TestDatetimePlotting:
     def test_scatter(self):
         mpl.rcParams["date.converter"] = 'concise'
         base = datetime.datetime(2005, 2, 1)
+        dates = [base + datetime.timedelta(hours=(2 * i)) for i in range(10)]
         N = len(dates)
         np.random.seed(19680801)
         y = np.cumsum(np.random.randn(N))
         fig, axs = plt.subplots(3, 1, layout='constrained', figsize=(6, 6))
-        dates = [(np.datetime64('2005-02'), np.datetime64('2005-04')),
+        lims = [(np.datetime64('2005-02'), np.datetime64('2005-04')),
                 (np.datetime64('2005-02-03'), np.datetime64('2005-02-15')),
                 (np.datetime64('2005-02-03 11:00'), np.datetime64('2005-02-04 13:20'))]
         axs[0].set_title('Axes.scatter used with datetime on x axis')
