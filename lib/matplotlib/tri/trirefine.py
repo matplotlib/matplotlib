75dd150e71e93b4da732fd0a45768475783a0d30
vigneshvetrivel8 <vigneshvetrivel88@gmail.com>
2023-09-26 13:45:51 +0530

    updated deprecated api

--- lib/matplotlib/tri/trirefine.py
+++ lib/matplotlib/tri/trirefine.py
@@ -3,3 +3,4 @@ from ._trirefine import *  # noqa: F401, F403
 raise ValueError(f"Importing {__name__} was deprecated in Matplotlib 3.7 and will
                 be removed two minor releases later. All functionality is 
                 available via the top-level module matplotlib.tri")
+                
