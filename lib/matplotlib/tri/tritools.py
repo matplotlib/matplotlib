7a7084a47258d26ad94b6541f91af43d156ac0a6
vigneshvetrivel8 <vigneshvetrivel88@gmail.com>
2023-09-26 13:58:15 +0530

    updated deprecated api

--- lib/matplotlib/tri/tritools.py
+++ lib/matplotlib/tri/tritools.py
@@ -1,6 +1,3 @@
 from ._tritools import *  # noqa: F401, F403
 
-raise ValueError(f"Importing {__name__} was deprecated in Matplotlib 3.7 and will
-                be removed two minor releases later. All functionality is 
-                available via the top-level module matplotlib.tri")
-                
+raise ValueError(f"Importing {__name__} was deprecated in Matplotlib 3.7 and will be removed two minor releases later. All functionality is available via the top-level module matplotlib.tri")
