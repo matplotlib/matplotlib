a59c2680dc176744282be1173f6a90cfb475d909
Kyle Sunden <git@ksunden.space>
2023-09-26 17:47:27 -0500

    Add alias decorator handling to the generated allowlist

--- tools/stubtest.py
+++ tools/stubtest.py
@@ -1,4 +1,5 @@
 import ast
+import os
 import pathlib
 import subprocess
 import sys
@@ -22,9 +23,33 @@ class Visitor(ast.NodeVisitor):
             if hasattr(node, "parent"):
                 parent = node.parent
                 while hasattr(parent, "parent") and not isinstance(parent, ast.Module):
-                    parents.append(parent.name)
+                    parents.insert(0, parent.name)
                     parent = parent.parent
             self.output.write(f"{'.'.join(self.context + parents)}.{node.name}\n")
+
+    def visit_ClassDef(self, node):
+        for dec in node.decorator_list:
+            if "define_aliases" in ast.unparse(dec):
+                parents = []
+                if hasattr(node, "parent"):
+                    parent = node.parent
+                    while hasattr(parent, "parent") and not isinstance(
+                        parent, ast.Module
+                    ):
+                        parents.insert(0, parent.name)
+                        parent = parent.parent
+                aliases = ast.literal_eval(dec.args[0])
+                # Written as a regex rather than two lines to avoid unused entries
+                # for setters on items with only a getter
+                for substitutions in aliases.values():
+                    parts = self.context + parents + [node.name]
+                    self.output.write(
+                        "\n".join(
+                            f"{'.'.join(parts)}.[gs]et_{a}\n" for a in substitutions
+                        )
+                    )
+        for child in ast.iter_child_nodes(node):
+            self.visit(child)
 
 
 with tempfile.NamedTemporaryFile("wt") as f:
@@ -48,6 +73,7 @@ with tempfile.NamedTemporaryFile("wt") as f:
             "matplotlib",
         ],
         cwd=root,
+        env=os.environ | {"MPLBACKEND": "agg"},
     )
 
 sys.exit(proc.returncode)
