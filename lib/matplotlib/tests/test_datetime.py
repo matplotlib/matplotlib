56e0b8a263f76207949f598d202472c535107504
Junpei Ota <otaj@purdue.edu>
2023-10-19 01:27:36 -0400

    Remove whitespace

--- lib/matplotlib/tests/test_datetime.py
+++ lib/matplotlib/tests/test_datetime.py
@@ -284,7 +284,6 @@ class TestDatetimePlotting:
         )
         x_ranges = np.array(range(1, range_threshold))
         y_ranges = np.array(range(1, range_threshold))
-        
         ax1.plot_date(x_dates, y_dates)
         ax2.plot_date(x_dates, y_ranges)
         ax3.plot_date(x_ranges, y_dates)
