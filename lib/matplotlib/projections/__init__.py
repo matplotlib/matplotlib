865e87c82144b11ce43d06912be196b70b7c6730
Kyle Sunden <git@ksunden.space>
2023-10-24 00:52:05 -0500

    Add a warning

--- lib/matplotlib/projections/__init__.py
+++ lib/matplotlib/projections/__init__.py
@@ -59,6 +59,10 @@ from .polar import PolarAxes
 try:
     from mpl_toolkits.mplot3d import Axes3D
 except ImportError:
+    import warnings
+    warnings.warn("Unable to import Axes3D. This may be due to multiple versions of"
+                  "Matplotlib being installed (e.g. as a system package and as a pip"
+                  "package). As a result, the 3D projection is not available.")
     Axes3D = None
 
 
