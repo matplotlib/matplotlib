1fa01f936c630e8c90921682e7f83e75009480ea
Marcel Ramos <LiNk-NY@users.noreply.github.com>
2023-10-31 17:46:12 +0000

    move get_cmap and avoid redirection

--- lib/matplotlib/cm.py
+++ lib/matplotlib/cm.py
@@ -225,35 +225,6 @@ class ColormapRegistry(Mapping):
         )
 
 
-def _get_cmap(name=None, lut=None):
-    """
-    Get a colormap instance, defaulting to rc values if *name* is None.
-
-    Parameters
-    ----------
-    name : `~matplotlib.colors.Colormap` or str or None, default: None
-        If a `.Colormap` instance, it will be returned. Otherwise, the name of
-        a colormap known to Matplotlib, which will be resampled by *lut*. The
-        default, None, means :rc:`image.cmap`.
-    lut : int or None, default: None
-        If *name* is not already a Colormap instance and *lut* is not None, the
-        colormap will be resampled to have *lut* entries in the lookup table.
-
-    Returns
-    -------
-    Colormap
-    """
-    if name is None:
-        name = mpl.rcParams['image.cmap']
-    if isinstance(name, colors.Colormap):
-        return name
-    _api.check_in_list(sorted(_colormaps), name=name)
-    if lut is None:
-        return _colormaps[name]
-    else:
-        return _colormaps[name].resampled(lut)
-
-
 def _auto_norm_from_scale(scale_cls):
     """
     Automatically generate a norm class from *scale_cls*.
--- lib/matplotlib/pyplot.py
+++ lib/matplotlib/pyplot.py
@@ -2371,8 +2371,32 @@ def get_cmap(
     name: Colormap | str | None = None,
     lut: int | None = None
 ) -> Colormap:
-    return cm._get_cmap(name=name, lut=lut)  # type: ignore
-get_cmap.__doc__ = cm._get_cmap.__doc__  # type: ignore
+    """
+    Get a colormap instance, defaulting to rc values if *name* is None.
+
+    Parameters
+    ----------
+    name : `~matplotlib.colors.Colormap` or str or None, default: None
+        If a `.Colormap` instance, it will be returned. Otherwise, the name of
+        a colormap known to Matplotlib, which will be resampled by *lut*. The
+        default, None, means :rc:`image.cmap`.
+    lut : int or None, default: None
+        If *name* is not already a Colormap instance and *lut* is not None, the
+        colormap will be resampled to have *lut* entries in the lookup table.
+
+    Returns
+    -------
+    Colormap
+    """
+    if name is None:
+        name = mpl.rcParams['image.cmap']
+    if isinstance(name, colors.Colormap):
+        return name
+    _api.check_in_list(sorted(_colormaps), name=name)
+    if lut is None:
+        return _colormaps[name]
+    else:
+        return _colormaps[name].resampled(lut)
 
 
 def set_cmap(cmap: Colormap | str) -> None:
