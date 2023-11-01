e3abb2e7d64cdcea8a216727a714ec7d8b359087
CozyFrog <garrett.sward@uconn.edu>
2023-11-01 11:26:00 -0400

    Added test for singular float xmin/xmax arguments

--- lib/matplotlib/tests/test_datetime.py
+++ lib/matplotlib/tests/test_datetime.py
@@ -221,7 +221,7 @@ class TestDatetimePlotting:
     @mpl.style.context("default")
     def test_hlines(self):
         mpl.rcParams["date.converter"] = 'concise'
-        fig, axs = plt.subplots(2, 3, layout='constrained')
+        fig, axs = plt.subplots(2, 4, layout='constrained')
         dateStrs = ['2023-03-08',
                     '2023-04-09',
                     '2023-05-13',
@@ -240,6 +240,9 @@ class TestDatetimePlotting:
         axs[0, 2].hlines(dates,
                          xmin=date_start,
                          xmax=date_end)
+        axs[0, 3].hlines(dates,
+                         xmin=0.45,
+                         xmax=0.65)
         axs[1, 0].hlines(y=npDates,
                          xmin=[0.5, 0.6, 0.7, 0.8, 0.9],
                          xmax=[0.1, 0.2, 0.3, 0.4, 0.5])
@@ -249,6 +252,9 @@ class TestDatetimePlotting:
         axs[1, 1].hlines(npDates,
                          xmin=datetime.datetime(2020, 5, 10),
                          xmax=datetime.datetime(2020, 5, 31))
+        axs[1, 3].hlines(npDates,
+                         xmin=0.45,
+                         xmax=0.65)
 
     @pytest.mark.xfail(reason="Test for imshow not written yet")
     @mpl.style.context("default")
