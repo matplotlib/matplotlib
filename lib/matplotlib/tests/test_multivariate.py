b39b644643437dd7362460f435e49b088d400dc7
Trygve Magnus Ræder <trygvrad@gmail.com>
2023-11-22 11:53:37 +0100

    test for multivariate pcolormesh specifying C, X, Y

--- lib/matplotlib/tests/test_multivariate.py
+++ lib/matplotlib/tests/test_multivariate.py
@@ -11,12 +11,17 @@ def test_bivariate_visualizations():
     x_0 = np.arange(25, dtype='float32').reshape(5, 5) % 5
     x_1 = np.arange(25, dtype='float32').reshape(5, 5).T % 5
 
-    fig, axes = plt.subplots(1, 5, figsize=(10, 2))
+    fig, axes = plt.subplots(1, 6, figsize=(10, 2))
 
     axes[0].imshow((x_0, x_1), cmap='BiPeak', interpolation='nearest')
     axes[1].matshow((x_0, x_1), cmap='BiPeak')
     axes[2].pcolor((x_0, x_1), cmap='BiPeak')
     axes[3].pcolormesh((x_0, x_1), cmap='BiPeak')
+
+    x = np.arange(5)
+    y = np.arange(5)
+    X, Y = np.meshgrid(x, y)
+    axes[4].pcolormesh(X, Y, (x_0, x_1), cmap='BiPeak')
 
     patches = [
         mpl.patches.Wedge((.3, .7), .1, 0, 360),             # Full circle
@@ -28,7 +33,7 @@ def test_bivariate_visualizations():
     colors_1 = np.arange(len(patches)) % 2
     p = mpl.collections.PatchCollection(patches, cmap='BiPeak', alpha=0.5)
     p.set_array((colors_0, colors_1))
-    axes[4].add_collection(p)
+    axes[5].add_collection(p)
 
     remove_ticks_and_titles(fig)
 
@@ -39,12 +44,17 @@ def test_multivariate_visualizations():
     x_1 = np.arange(25, dtype='float32').reshape(5, 5).T % 5
     x_2 = np.arange(25, dtype='float32').reshape(5, 5) % 6
 
-    fig, axes = plt.subplots(1, 5, figsize=(10, 2))
+    fig, axes = plt.subplots(1, 6, figsize=(10, 2))
 
     axes[0].imshow((x_0, x_1, x_2), cmap='3VarAddA', interpolation='nearest')
     axes[1].matshow((x_0, x_1, x_2), cmap='3VarAddA')
     axes[2].pcolor((x_0, x_1, x_2), cmap='3VarAddA')
     axes[3].pcolormesh((x_0, x_1, x_2), cmap='3VarAddA')
+
+    x = np.arange(5)
+    y = np.arange(5)
+    X, Y = np.meshgrid(x, y)
+    axes[4].pcolormesh(X, Y, (x_0, x_1, x_2), cmap='3VarAddA')
 
     patches = [
         mpl.patches.Wedge((.3, .7), .1, 0, 360),             # Full circle
@@ -57,7 +67,7 @@ def test_multivariate_visualizations():
     colors_2 = np.arange(len(patches)) % 3
     p = mpl.collections.PatchCollection(patches, cmap='3VarAddA', alpha=0.5)
     p.set_array((colors_0, colors_1, colors_2))
-    axes[4].add_collection(p)
+    axes[5].add_collection(p)
 
     remove_ticks_and_titles(fig)
 
