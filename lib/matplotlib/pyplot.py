e2c9cd4ac69d86d496ca4f171c3b6d57c2781e09
Samuel Diebolt <samuel@diebolt.io>
2023-11-27 17:22:21 +0100

    [MNT] fix type annotations of `fignum_exists` (#27376)
    * fix: fix type annotations of `fignum_exists`
    
    * docs: expand documentation of `fignum_exists`
    
    * fix(docs): fix documentation of `fignum_exists`
    
    Co-authored-by: Ruth Comer <10599679+rcomer@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ruth Comer <10599679+rcomer@users.noreply.github.com>

--- lib/matplotlib/pyplot.py
+++ lib/matplotlib/pyplot.py
@@ -999,9 +999,24 @@ def gcf() -> Figure:
         return figure()
 
 
-def fignum_exists(num: int) -> bool:
-    """Return whether the figure with the given id exists."""
-    return _pylab_helpers.Gcf.has_fignum(num) or num in get_figlabels()
+def fignum_exists(num: int | str) -> bool:
+    """Return whether the figure with the given id exists.
+
+    Parameters
+    ----------
+    num : int or str
+        A figure identifier.
+
+    Returns
+    -------
+    bool
+        Whether or not a figure with id *num* exists.
+    """
+    return (
+        _pylab_helpers.Gcf.has_fignum(num)
+        if isinstance(num, int)
+        else num in get_figlabels()
+    )
 
 
 def get_fignums() -> list[int]:
