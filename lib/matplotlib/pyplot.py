31e8225537b8316db5c5b3a6b4eaeae04a0b6e2e
sdiebolt <samuel@diebolt.io>
2023-11-27 13:22:51 +0100

    docs: expand documentation of `fignum_exists`

--- lib/matplotlib/pyplot.py
+++ lib/matplotlib/pyplot.py
@@ -1000,7 +1000,18 @@ def gcf() -> Figure:
 
 
 def fignum_exists(num: int | str) -> bool:
-    """Return whether the figure with the given id exists."""
+    """Return whether the figure with the given id exists.
+
+    Parameters
+    ----------
+    num : int or str
+        A figure identifier.
+
+    Returns
+    -------
+    bool
+        Whether or not a figure with id `num` exists.
+    """
     return (
         _pylab_helpers.Gcf.has_fignum(num)
         if isinstance(num, int)
