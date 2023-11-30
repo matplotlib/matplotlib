24842ec53f443e053723c79d2897f41dc4da84b4
ElisaHeck <62849498+ElisaHeck@users.noreply.github.com>
2023-11-30 11:43:03 +0100

    Add smoke test for Axes.barbs in test_datetime.py (#27331)
    * added smoke test for Axes.barbs in test_datetime.py
    
    * changed to use deterministic data

--- lib/matplotlib/tests/test_datetime.py
+++ lib/matplotlib/tests/test_datetime.py
@@ -146,11 +146,29 @@ class TestDatetimePlotting:
         fig, ax = plt.subplots()
         ax.bar_label(...)
 
-    @pytest.mark.xfail(reason="Test for barbs not written yet")
     @mpl.style.context("default")
     def test_barbs(self):
-        fig, ax = plt.subplots()
-        ax.barbs(...)
+        plt.rcParams["date.converter"] = 'concise'
+
+        start_date = datetime.datetime(2022, 2, 8, 22)
+        dates = [start_date + datetime.timedelta(hours=i) for i in range(12)]
+
+        numbers = np.sin(np.linspace(0, 2 * np.pi, 12))
+
+        u = np.ones(12) * 10
+        v = np.arange(0, 120, 10)
+
+        fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+
+        axes[0].barbs(dates, numbers, u, v, length=7)
+        axes[0].set_title('Datetime vs. Numeric Data')
+        axes[0].set_xlabel('Datetime')
+        axes[0].set_ylabel('Numeric Data')
+
+        axes[1].barbs(numbers, dates, u, v, length=7)
+        axes[1].set_title('Numeric vs. Datetime Data')
+        axes[1].set_xlabel('Numeric Data')
+        axes[1].set_ylabel('Datetime')
 
     @mpl.style.context("default")
     def test_barh(self):
