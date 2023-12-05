42b4d19a6391fbde17115fa735e86d21f8b5d3aa
ifEricReturnTrue <86877855+ifEricReturnTrue@users.noreply.github.com>
2023-12-04 21:59:34 -0500

    Update test_stairs as requested

--- lib/matplotlib/tests/test_datetime.py
+++ lib/matplotlib/tests/test_datetime.py
@@ -516,14 +516,17 @@ class TestDatetimePlotting:
         time_delta = datetime.timedelta(days=1)
 
         bin_edges = [start_date + i * time_delta for i in range(31)]
+        edge_int = [0 + i * 1 for i in range(31)]
         np.random.seed(123456)
         values1 = np.random.randint(1, 100, 30)
-        values2 = np.random.randint(1, 100, 30)
-        values3 = np.random.randint(1, 100, 30)
+        random_day = np.random.randint(1, 10000, 30)
+        values2 = [start_date + datetime.timedelta(days=int(random_day[i])) for i in range(30)]
+        random_day = np.random.randint(-10000, 10000, 30)
+        values3 = [start_date + datetime.timedelta(days=int(random_day[i])) for i in range(30)]
 
         fig, (ax1, ax2, ax3) = plt.subplots(3, 1, constrained_layout=True)
         ax1.stairs(values1, edges=bin_edges)
-        ax2.stairs(values2, edges=bin_edges)
+        ax2.stairs(values2, edges=edge_int)
         ax3.stairs(values3, edges=bin_edges)
 
     @pytest.mark.xfail(reason="Test for stem not written yet")
