1b9b6dc3cd96708d68574983bebde399a27b7a1c
Ruth Comer <ruth.comer@metoffice.gov.uk>
2024-01-09 15:17:06 +0000

    DOC: simplify histogram animation example

--- galleries/examples/animation/animated_histogram.py
+++ galleries/examples/animation/animated_histogram.py
@@ -12,38 +12,34 @@ import numpy as np
 
 import matplotlib.animation as animation
 
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-# Fixing bin edges
+# Setting up a random number generator with a fixed state for reproducibility.
+rng = np.random.default_rng(seed=19680801)
+# Fixing bin edges.
 HIST_BINS = np.linspace(-4, 4, 100)
 
-# histogram our data with numpy
-data = np.random.randn(1000)
+# Histogram our data with numpy.
+data = rng.standard_normal(1000)
 n, _ = np.histogram(data, HIST_BINS)
 
 # %%
 # To animate the histogram, we need an ``animate`` function, which generates
-# a random set of numbers and updates the heights of rectangles. We utilize a
-# python closure to track an instance of `.BarContainer` whose `.Rectangle`
-# patches we shall update.
+# a random set of numbers and updates the heights of rectangles. The ``animate``
+# function updates the `.Rectangle` patches on a global instance of
+# `.BarContainer` (which in this case is defined below).
 
 
-def prepare_animation(bar_container):
+def animate(frame_number):
+    # Simulate new data coming in.
+    data = rng.standard_normal(1000)
+    n, _ = np.histogram(data, HIST_BINS)
+    for count, rect in zip(n, bar_container.patches):
+        rect.set_height(count)
 
-    def animate(frame_number):
-        # simulate new data coming in
-        data = np.random.randn(1000)
-        n, _ = np.histogram(data, HIST_BINS)
-        for count, rect in zip(n, bar_container.patches):
-            rect.set_height(count)
-        return bar_container.patches
-    return animate
+    return bar_container.patches
 
 # %%
 # Using :func:`~matplotlib.pyplot.hist` allows us to get an instance of
-# `.BarContainer`, which is a collection of `.Rectangle` instances. Calling
-# ``prepare_animation`` will define ``animate`` function working with supplied
-# `.BarContainer`, all this is used to setup `.FuncAnimation`.
+# `.BarContainer`, which is a collection of `.Rectangle` instances.
 
 # Output generated via `matplotlib.animation.Animation.to_jshtml`.
 
@@ -52,8 +48,7 @@ _, _, bar_container = ax.hist(data, HIST_BINS, lw=1,
                               ec="yellow", fc="green", alpha=0.5)
 ax.set_ylim(top=55)  # set safe limit to ensure that all data is visible.
 
-ani = animation.FuncAnimation(fig, prepare_animation(bar_container), 50,
-                              repeat=False, blit=True)
+ani = animation.FuncAnimation(fig, animate, 50, repeat=False, blit=True)
 plt.show()
 
 # %%
