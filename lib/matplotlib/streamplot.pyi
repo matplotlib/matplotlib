b2843d54c57ad3b332e12d58b9d2deebbdd1ccd1
David Stansby <dstansby@gmail.com>
2024-01-13 11:42:18 +0000

    Fix new streamplot type stub
    Co-authored-by: Kyle Sunden <git@ksunden.space>

--- lib/matplotlib/streamplot.pyi
+++ lib/matplotlib/streamplot.pyi
@@ -29,7 +29,7 @@ def streamplot(
     integration_direction: Literal["forward", "backward", "both"] = ...,
     broken_streamlines: bool = ...,
     *,
-    n_arrows: int = 1
+    n_arrows: int = ...,
 ) -> StreamplotSet: ...
 
 class StreamplotSet:
