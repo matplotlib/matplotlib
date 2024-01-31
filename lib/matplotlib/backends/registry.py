c20c0ac994709953b0bbfdadf786eac12929ef50
Ian Thomas <ianthomas23@gmail.com>
2024-01-31 11:22:28 +0000

    Use class variables for immutable collections

--- lib/matplotlib/backends/registry.py
+++ lib/matplotlib/backends/registry.py
@@ -13,48 +13,47 @@ class BackendRegistry:
 
     This is the single source of truth for available backends.
     """
-    def __init__(self):
-        # Built-in backends are those which are included in the Matplotlib repo.
-        # A backend with name 'name' is located in the module
-        # f'matplotlib.backends.backend_{name.lower()}'
+    # Built-in backends are those which are included in the Matplotlib repo.
+    # A backend with name 'name' is located in the module
+    # f'matplotlib.backends.backend_{name.lower()}'
 
-        # The capitalized forms are needed for ipython at present; this may
-        # change for later versions.
-        self._builtin_interactive = [
-            "GTK3Agg", "GTK3Cairo", "GTK4Agg", "GTK4Cairo",
-            "MacOSX",
-            "nbAgg",
-            "QtAgg", "QtCairo", "Qt5Agg", "Qt5Cairo",
-            "TkAgg", "TkCairo",
-            "WebAgg",
-            "WX", "WXAgg", "WXCairo",
-        ]
-        self._builtin_not_interactive = [
-            "agg", "cairo", "pdf", "pgf", "ps", "svg", "template",
-        ]
-        self._framework_to_backend_mapping = {
-            "qt": "qtagg",
-            "gtk3": "gtk3agg",
-            "gtk4": "gtk4agg",
-            "wx": "wxagg",
-            "tk": "tkagg",
-            "macosx": "macosx",
-            "headless": "agg",
-        }
+    # The capitalized forms are needed for ipython at present; this may
+    # change for later versions.
+    _BUILTIN_INTERACTIVE = [
+        "GTK3Agg", "GTK3Cairo", "GTK4Agg", "GTK4Cairo",
+        "MacOSX",
+        "nbAgg",
+        "QtAgg", "QtCairo", "Qt5Agg", "Qt5Cairo",
+        "TkAgg", "TkCairo",
+        "WebAgg",
+        "WX", "WXAgg", "WXCairo",
+    ]
+    _BUILTIN_NOT_INTERACTIVE = [
+        "agg", "cairo", "pdf", "pgf", "ps", "svg", "template",
+    ]
+    _FRAMEWORK_TO_BACKEND_MAPPING = {
+        "qt": "qtagg",
+        "gtk3": "gtk3agg",
+        "gtk4": "gtk4agg",
+        "wx": "wxagg",
+        "tk": "tkagg",
+        "macosx": "macosx",
+        "headless": "agg",
+    }
 
     def framework_to_backend(self, interactive_framework):
-        return self._framework_to_backend_mapping.get(interactive_framework)
+        return self._FRAMEWORK_TO_BACKEND_MAPPING.get(interactive_framework)
 
     def list_builtin(self, filter_=None):
         if filter_ == BackendFilter.INTERACTIVE:
-            return self._builtin_interactive
+            return self._BUILTIN_INTERACTIVE
         elif filter_ == BackendFilter.INTERACTIVE_NON_WEB:
             return list(filter(lambda x: x.lower() not in ("webagg", "nbagg"),
-                               self._builtin_interactive))
+                               self._BUILTIN_INTERACTIVE))
         elif filter_ == BackendFilter.NON_INTERACTIVE:
-            return self._builtin_not_interactive
+            return self._BUILTIN_NOT_INTERACTIVE
 
-        return self._builtin_interactive + self._builtin_not_interactive
+        return self._BUILTIN_INTERACTIVE + self._BUILTIN_NOT_INTERACTIVE
 
 
 # Singleton
