0b56e073cc5768f6a940f430b6bc18e5cc17b8bd
Antony Lee <anntzer.lee@gmail.com>
2024-02-01 09:07:17 +0100

    Add an rcparam for image.interpolation_stage.

--- lib/matplotlib/rcsetup.py
+++ lib/matplotlib/rcsetup.py
@@ -1051,12 +1051,13 @@ _validators = {
                                 "bb", "frak", "scr", "regular"],
     "mathtext.fallback":       _validate_mathtext_fallback,
 
-    "image.aspect":          validate_aspect,  # equal, auto, a number
-    "image.interpolation":   validate_string,
-    "image.cmap":            _validate_cmap,  # gray, jet, etc.
-    "image.lut":             validate_int,  # lookup table
-    "image.origin":          ["upper", "lower"],
-    "image.resample":        validate_bool,
+    "image.aspect":              validate_aspect,  # equal, auto, a number
+    "image.interpolation":       validate_string,
+    "image.interpolation_stage": ["data", "rgba"],
+    "image.cmap":                _validate_cmap,  # gray, jet, etc.
+    "image.lut":                 validate_int,  # lookup table
+    "image.origin":              ["upper", "lower"],
+    "image.resample":            validate_bool,
     # Specify whether vector graphics backends will combine all images on a
     # set of Axes into a single composite image
     "image.composite_image": validate_bool,
