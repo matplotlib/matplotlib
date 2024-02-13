146deee4734cdcd33dfc9e932f9b232c9a588109
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2024-02-13 13:28:13 +0100

    Deprecate positional use of most arguments of plotting functions
    This increases maintainability for developers and disallows
    bad practice of passing lots of positional arguments for users.
    If in doubt, I've erred on the side of allowing as much positional
    arguments
    as possible as long as the intent of a call is still readable.
    
    Note: This was originally motivated by bxp() and boxplot() having many
    overlapping parameters but differently ordered.
    While at it, I think it's better to rollout the change to
    all plotting functions and communicate that in one go
    rather than having lots of individual change notices over
    time. Also, the freedom to reorder parameters only sets
    in after the deprecation. So let's start this now.

--- galleries/examples/lines_bars_and_markers/psd_demo.py
+++ galleries/examples/lines_bars_and_markers/psd_demo.py
@@ -30,7 +30,7 @@ fig, (ax0, ax1) = plt.subplots(2, 1, layout='constrained')
 ax0.plot(t, s)
 ax0.set_xlabel('Time (s)')
 ax0.set_ylabel('Signal')
-ax1.psd(s, 512, 1 / dt)
+ax1.psd(s, NFFT=512, Fs=1 / dt)
 
 plt.show()
 
