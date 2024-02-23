9e6bd19bb5f186637e37ad7e21adfe48a0923c76
Daniel Bergman <dbergma5@jh.edu>
2024-02-22 20:17:48 -0500

    Update lib/matplotlib/patches.py
    Co-authored-by: Elliott Sales de Andrade <quantum.analyst@gmail.com>

--- lib/matplotlib/patches.py
+++ lib/matplotlib/patches.py
@@ -1842,7 +1842,7 @@ class Annulus(Patch):
         """
         if width > min(self.a, self.b):
             raise ValueError(
-                'Width of annulus must be less than or equal semi-minor axis')
+                'Width of annulus must be less than or equal to semi-minor axis')
 
         self._width = width
         self._path = None
