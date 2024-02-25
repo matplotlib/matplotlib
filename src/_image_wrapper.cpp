06040b898d656b9ce162dd6a1f03cb8799339b24
DWesl <22566757+DWesl@users.noreply.github.com>
2024-02-25 07:21:21 -0500

    BLD: Include Python.h before pybind11/pybind11.h
    pybind11 assumes Python.h is included first.

--- src/_image_wrapper.cpp
+++ src/_image_wrapper.cpp
@@ -1,3 +1,4 @@
+#include <Python.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 
--- src/_ttconv.cpp
+++ src/_ttconv.cpp
@@ -5,6 +5,7 @@
 
   Python wrapper for TrueType conversion library in ../ttconv.
  */
+#include <Python.h>
 #include "mplutils.h"
 
 #include <pybind11/pybind11.h>
