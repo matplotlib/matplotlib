8bd9488b5fc4239b495d242bc239665a2fa35a58
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2024-03-06 22:25:37 -0500

    Update lib/matplotlib/collections.py
    Co-authored-by: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>

--- lib/matplotlib/collections.py
+++ lib/matplotlib/collections.py
@@ -152,8 +152,8 @@ class Collection(artist.Artist, cm.ScalarMappable):
             The drawing order, shared by all Patches in the Collection. See
             :doc:`/gallery/misc/zorder_demo` for all defaults and examples.
         **kwargs
-            Remaining kwargs will be used to set properties as
-             `Collection.set_{key}(val)` for each `key`, `val` kwarg pair.
+            Remaining keyword arguments will be used to set properties as
+            ``Collection.set_{key}(val)`` for each key-value pair in *kwargs*.
         """
         artist.Artist.__init__(self)
         cm.ScalarMappable.__init__(self, norm, cmap)
