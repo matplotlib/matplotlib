cc59fc405962cdb8b6a18188e976b6975cb9afaf
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2024-03-09 02:07:28 -0500

    Mark triangulation classes as final
    Prior to their conversion to pybind11 classes in #24522, these types
    had `tp_flags = Py_TPFLAGS_DEFAULT`, meaning they did not have the
    `Py_TPFLAGS_BASETYPE` flag and could not be subtyped. As these are
    internal classes, I don't believe this was intentionally changed, so
    restore that flag.
    
    Additionally, since we require C++17 now, mark the C++ classes
    themselves as `final`. I believe this really only makes a difference if
    we have `virtual` methods (which we don't here), but that doesn't mean
    the compiler can't infer some other optimizations with this information.

--- src/tri/_tri_wrapper.cpp
+++ src/tri/_tri_wrapper.cpp
@@ -3,7 +3,7 @@
 using namespace pybind11::literals;
 
 PYBIND11_MODULE(_tri, m) {
-    py::class_<Triangulation>(m, "Triangulation")
+    py::class_<Triangulation>(m, "Triangulation", py::is_final())
         .def(py::init<const Triangulation::CoordinateArray&,
                       const Triangulation::CoordinateArray&,
                       const Triangulation::TriangleArray&,
@@ -30,7 +30,7 @@ PYBIND11_MODULE(_tri, m) {
         .def("set_mask", &Triangulation::set_mask,
             "Set or clear the mask array.");
 
-    py::class_<TriContourGenerator>(m, "TriContourGenerator")
+    py::class_<TriContourGenerator>(m, "TriContourGenerator", py::is_final())
         .def(py::init<Triangulation&,
                       const TriContourGenerator::CoordinateArray&>(),
             "triangulation"_a,
@@ -43,7 +43,7 @@ PYBIND11_MODULE(_tri, m) {
         .def("create_filled_contour", &TriContourGenerator::create_filled_contour,
             "Create and return a filled contour.");
 
-    py::class_<TrapezoidMapTriFinder>(m, "TrapezoidMapTriFinder")
+    py::class_<TrapezoidMapTriFinder>(m, "TrapezoidMapTriFinder", py::is_final())
         .def(py::init<Triangulation&>(),
             "triangulation"_a,
             "Create a new C++ TrapezoidMapTriFinder object.\n"
