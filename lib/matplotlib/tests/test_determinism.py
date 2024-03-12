4a2ae997a97ce6d33443b5117117eb99f6bcc2c3
James Addison <jay@jp-hosting.net>
2024-03-12 19:31:29 +0000

    svg-determinism: share a circular clip-path across various artists during test

--- lib/matplotlib/tests/test_determinism.py
+++ lib/matplotlib/tests/test_determinism.py
@@ -9,6 +9,7 @@ import pytest
 
 import matplotlib as mpl
 import matplotlib.testing.compare
+from matplotlib import patches
 from matplotlib import pyplot as plt
 from matplotlib.testing._markers import needs_ghostscript, needs_usetex
 from matplotlib.testing import subprocess_run_for_testing
@@ -19,6 +20,7 @@ def _save_figure(objects='mhip', fmt="pdf", usetex=False):
     mpl.rcParams.update({'svg.hashsalt': 'asdf', 'text.usetex': usetex})
 
     fig = plt.figure()
+    circle = patches.Circle((0, 1), radius=0.3333)
 
     if 'm' in objects:
         # use different markers...
@@ -29,6 +31,8 @@ def _save_figure(objects='mhip', fmt="pdf", usetex=False):
         ax1.plot(x, [3] * 10, marker='^')
         ax1.plot(x, [4] * 10, marker='H')
         ax1.plot(x, [5] * 10, marker='v')
+        ax1img = ax1.imshow([[0]])
+        ax1img.set_clip_path(circle)
 
     if 'h' in objects:
         # also use different hatch patterns
@@ -36,6 +40,8 @@ def _save_figure(objects='mhip', fmt="pdf", usetex=False):
         bars = (ax2.bar(range(1, 5), range(1, 5)) +
                 ax2.bar(range(1, 5), [6] * 4, bottom=range(1, 5)))
         ax2.set_xticks([1.5, 2.5, 3.5, 4.5])
+        ax2img = ax2.imshow([[1]])
+        ax2img.set_clip_path(circle)
 
         patterns = ('-', '+', 'x', '\\', '*', 'o', 'O', '.')
         for bar, pattern in zip(bars, patterns):
@@ -52,7 +58,9 @@ def _save_figure(objects='mhip', fmt="pdf", usetex=False):
 
     if 'p' in objects:
         # add a polar projection
-        fig.add_subplot(projection="polar")
+        px = fig.add_subplot(projection="polar")
+        pimg = px.imshow([[2]])
+        pimg.set_clip_path(circle)
 
     x = range(5)
     ax = fig.add_subplot(1, 6, 6)
@@ -60,6 +68,8 @@ def _save_figure(objects='mhip', fmt="pdf", usetex=False):
     ax.set_title('A string $1+2+\\sigma$')
     ax.set_xlabel('A string $1+2+\\sigma$')
     ax.set_ylabel('A string $1+2+\\sigma$')
+    aimg = ax.imshow([[1]])
+    aimg.set_clip_path(circle)
 
     stdout = getattr(sys.stdout, 'buffer', sys.stdout)
     fig.savefig(stdout, format=fmt)
