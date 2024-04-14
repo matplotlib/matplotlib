0e52daad459d0f3b1f1238016533ebf4583a3438
Pranav <kingprawn5000@gmail.com>
2024-04-14 11:13:46 +0530

    Add hatch, linewidth and edgecolor parameters to multihist example

--- galleries/examples/statistics/histogram_multihist.py
+++ galleries/examples/statistics/histogram_multihist.py
@@ -27,19 +27,24 @@ x = np.random.randn(1000, 3)
 fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(nrows=2, ncols=2)
 
 colors = ['red', 'tan', 'lime']
-ax0.hist(x, n_bins, density=True, histtype='bar', color=colors, label=colors)
+ax0.hist(x, n_bins, density=True, histtype='bar', color=colors,
+         label=colors, hatch=['o', '*', '.'],
+         edgecolor=['black', 'red', 'blue'], linewidth=[1, 2, 3])
 ax0.legend(prop={'size': 10})
 ax0.set_title('bars with legend')
 
-ax1.hist(x, n_bins, density=True, histtype='bar', stacked=True)
+ax1.hist(
+    x, n_bins, density=True, histtype="bar", stacked=True,
+        edgecolor=["black", "yellow", "blue"])
 ax1.set_title('stacked bar')
 
-ax2.hist(x, n_bins, histtype='step', stacked=True, fill=False)
+ax2.hist(x, n_bins, histtype='step', stacked=True, fill=False,
+         linewidth=[1, 2, 3])
 ax2.set_title('stack step (unfilled)')
 
 # Make a multiple-histogram of data-sets with different length.
 x_multi = [np.random.randn(n) for n in [10000, 5000, 2000]]
-ax3.hist(x_multi, n_bins, histtype='bar')
+ax3.hist(x_multi, n_bins, histtype='bar', hatch=['\\', '/', '-'])
 ax3.set_title('different sample sizes')
 
 fig.tight_layout()
