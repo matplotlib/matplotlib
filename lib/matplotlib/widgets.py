f7b56892cc3a8fc180de24cc6f418635ed9d2aec
Kaustbh <desalekaustubh3@gmail.com>
2024-05-20 02:00:14 +0530

    Fix PolygonSelector cursor to temporarily hide during active zoom/pan

--- lib/matplotlib/widgets.py
+++ lib/matplotlib/widgets.py
@@ -4000,11 +4000,18 @@ class PolygonSelector(_SelectorWidget):
         # needs to process the move callback even if there is no button press.
         # _SelectorWidget.onmove include logic to ignore move event if
         # _eventpress is None.
-        if not self.ignore(event):
+
+        # Hide the cursor when interactive zoom/pan is active
+        if self.ignore(event):
+            if not self.canvas.widgetlock.available(self) and self._xys:
+                self._xys[-1] = (np.nan, np.nan)
+                self._draw_polygon()
+            return False
+
+        else:
             event = self._clean_event(event)
             self._onmove(event)
             return True
-        return False
 
     def _onmove(self, event):
         """Cursor move event handler."""
