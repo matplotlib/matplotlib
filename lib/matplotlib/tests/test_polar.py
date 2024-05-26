9841ce106dc61911965e824e13749518475dbafc
Michael <michal28burzynski@gmail.com>
2024-05-26 21:33:51 +0200

    Fixed PolarAxes not using fmt_xdata and added simple test (#4568)

--- lib/matplotlib/tests/test_polar.py
+++ lib/matplotlib/tests/test_polar.py
@@ -436,6 +436,28 @@ def test_cursor_precision():
     assert ax.format_coord(2, 1) == "θ=0.637π (114.6°), r=1.000"
 
 
+def test_custom_fmt_ydata():
+    ax = plt.subplot(projection="polar")
+    def millions(x):
+        return '$%1.1fM' % (x*1e-6)
+
+    ax.fmt_ydata = millions
+    assert ax.format_coord(12, 2e7) == "θ=3.8197186342π (687.54935416°), r=$20.0M"
+    assert ax.format_coord(1234, 2e6) == "θ=392.794399551π (70702.9919191°), r=$2.0M"
+    assert ax.format_coord(3, 100) == "θ=0.95493π (171.887°), r=$0.0M"
+
+
+def test_custom_fmt_xdata():
+    ax = plt.subplot(projection="polar")
+    def millions(x):
+        return '$%1.1fM' % (x*1e-6)
+
+    ax.fmt_xdata = millions
+    assert ax.format_coord(2e5, 1) == "θ=$0.2M, r=1.000"
+    assert ax.format_coord(1, .1) == "θ=$0.0M, r=0.100"
+    assert ax.format_coord(1e6, 0.005) == "θ=$1.0M, r=0.005"
+
+
 @image_comparison(['polar_log.png'], style='default')
 def test_polar_log():
     fig = plt.figure()
