79a15a74cf61b1f805749f5d84c16887990b46f6
muchojp <61620767+muchojp@users.noreply.github.com>
2024-06-04 15:07:15 +0000

    modify docstring

--- lib/matplotlib/quiver.py
+++ lib/matplotlib/quiver.py
@@ -286,8 +286,7 @@ class QuiverKey(martist.Artist):
             A dictionary with keyword arguments accepted by the
             `~matplotlib.font_manager.FontProperties` initializer:
             *family*, *style*, *variant*, *size*, *weight*.
-        zorder : float, default: None
-        zorder : float or None
+        zorder : float
             The zorder of the key. The default is 0.1 above *Q*.
         **kwargs
             Any additional keyword arguments are used to override vector
