0838a83ee4ba0ae5c0a84da45a5ada2d50d074e6
Satyam Kumar <121108110+isatyamks@users.noreply.github.com>
2024-06-04 15:28:27 +0530

    Update _mathtext.py

--- lib/matplotlib/_mathtext.py
+++ lib/matplotlib/_mathtext.py
@@ -2180,7 +2180,7 @@ class Parser:
         adjusted_tokens = self._adjust_spacing(tokens)
         parsed_expression = ''.join(adjusted_tokens)
         box = self.Parser.parse(parsed_expression, fonts_object, fontsize, dpi)
-        return T.cast(Hlist, result[0]), box # Known return type from main.
+        return T.cast(Hlist, result[0]), box        # Known return type from main.
         box = self.Parser.parse(parsed_expression, fonts_object, fontsize, dpi)
         return T.cast(Hlist, result[0]), box      # Known return type from main.
 
