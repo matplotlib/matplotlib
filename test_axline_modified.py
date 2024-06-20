87682c7ed50f35c1649dc3241c768748c3bda2c4
Andresporcruz <andresportillo@ufl.edu>
2024-06-20 18:36:25 -0400

    Update get_transform method to adjust tolerance for np.isclose based on view limits

---
+++ test_axline_modified.py
@@ -0,0 +1,13 @@
+import matplotlib
+import matplotlib.pyplot as plt 
+matplotlib.use('TkAgg')
+plt.axline(xy1=(0, 0), slope=1E-8)
+plt.show()
+
+# compare
+
+import matplotlib
+import matplotlib.pyplot as plt 
+matplotlib.use('TkAgg')
+plt.axline(xy1=(0, 0), slope=1.1E-8)
+plt.show()
