316d882f6ec81aad6ab0670b4b53beec6478efcb
Michael Droettboom <mdboom@gmail.com>
2024-07-09 13:54:59 -0400

    Fix import

--- lib/matplotlib/axes/_base.py
+++ lib/matplotlib/axes/_base.py
@@ -7,7 +7,7 @@ from numbers import Real
 from operator import attrgetter
 import re
 import types
-from typing import Callable
+from typing import Callable, Type
 
 import numpy as np
 
@@ -34,13 +34,13 @@ import matplotlib.transforms as mtransforms
 _log = logging.getLogger(__name__)
 
 
-coords_type_base = (
-    str |
-    martist.Artist |
-    mtransforms.Transform |
+_coords_type_base = (
+    Type[str] | 
+    martist.Artist | 
+    mtransforms.Transform | 
     Callable[[backend_bases.RendererBase], mtransforms.Bbox | mtransforms.Transform]
 )
-coords_type = coords_type_base | tuple[coords_type_base, coords_type_base]
+_coords_type = _coords_type_base | tuple[_coords_type_base, _coords_type_base]
 
 
 class _axis_method_wrapper:
