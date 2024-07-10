c7a128eec1123866183d412947270ab2c2b11ebc
Adam J. Stewart <ajstewart426@gmail.com>
2024-07-10 22:06:48 +0200

    Code needs two overloads

--- lib/matplotlib/pyplot.py
+++ lib/matplotlib/pyplot.py
@@ -1569,6 +1569,23 @@ def subplots(
     *,
     sharex: bool | Literal["none", "all", "row", "col"] = ...,
     sharey: bool | Literal["none", "all", "row", "col"] = ...,
+    squeeze: Literal[True],
+    width_ratios: Sequence[float] | None = ...,
+    height_ratios: Sequence[float] | None = ...,
+    subplot_kw: dict[str, Any] | None = ...,
+    gridspec_kw: dict[str, Any] | None = ...,
+    **fig_kw
+) -> tuple[Figure, Any]:
+    ...
+
+
+@overload
+def subplots(
+    nrows: int = ...,
+    ncols: int = ...,
+    *,
+    sharex: bool | Literal["none", "all", "row", "col"] = ...,
+    sharey: bool | Literal["none", "all", "row", "col"] = ...,
     squeeze: Literal[False],
     width_ratios: Sequence[float] | None = ...,
     height_ratios: Sequence[float] | None = ...,
