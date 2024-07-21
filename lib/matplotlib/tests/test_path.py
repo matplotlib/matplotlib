daf109098aa74d2bc2085856ad4ad4a9215330bf
r3kste <138380708+r3kste@users.noreply.github.com>
2024-07-21 13:37:21 +0530

    enhanced tests

--- lib/matplotlib/tests/test_path.py
+++ lib/matplotlib/tests/test_path.py
@@ -546,13 +546,30 @@ def test_cleanup_closepoly():
 def test_simplify_closepoly():
     # The values of the vertices in a CLOSEPOLY should always be ignored,
     # in favor of the most recent MOVETO's vertex values
-    path_ref = Path([(0, 0), (1, 0), (1, 1), (40, 50)],
-                    [Path.MOVETO, Path.LINETO, Path.LINETO, Path.CLOSEPOLY])
-    path_test = Path([(0, 0), (1, 0), (1, 1), (np.nan, np.nan)],
-                     [Path.MOVETO, Path.LINETO, Path.LINETO, Path.CLOSEPOLY])
+    paths = [Path([(1, 1), (2, 1), (2, 2), (np.nan, np.nan)],
+                  [Path.MOVETO, Path.LINETO, Path.LINETO, Path.CLOSEPOLY]),
+             Path([(1, 1), (2, 1), (2, 2), (40, 50)],
+                  [Path.MOVETO, Path.LINETO, Path.LINETO, Path.CLOSEPOLY])]
+    expected_path = Path([(1, 1), (2, 1), (2, 2), (1, 1), (1, 1), (0, 0)],
+                         [Path.MOVETO, Path.LINETO, Path.LINETO, Path.LINETO,
+                          Path.LINETO, Path.STOP])
 
-    path_ref_simplified = path_ref.cleaned(simplify=True)
-    path_test_simplified = path_test.cleaned(simplify=True)
+    for path in paths:
+        simplified_path = path.cleaned(simplify=True)
+        assert_array_equal(expected_path.vertices, simplified_path.vertices)
+        assert_array_equal(expected_path.codes, simplified_path.codes)
 
-    assert_array_equal(path_ref_simplified.vertices, path_test_simplified.vertices)
-    assert_array_equal(path_ref_simplified.codes, path_test_simplified.codes)
+    # test that a compound path also works
+    path = Path([(1, 1), (2, 1), (2, 2), (np.nan, np.nan),
+                 (-1, 0), (-2, 0), (-2, 1), (np.nan, np.nan)],
+                [Path.MOVETO, Path.LINETO, Path.LINETO, Path.CLOSEPOLY,
+                 Path.MOVETO, Path.LINETO, Path.LINETO, Path.CLOSEPOLY])
+    expected_path = Path([(1, 1), (2, 1), (2, 2), (1, 1),
+                          (-1, 0), (-2, 0), (-2, 1), (-1, 0), (-1, 0), (0, 0)],
+                         [Path.MOVETO, Path.LINETO, Path.LINETO, Path.LINETO,
+                          Path.MOVETO, Path.LINETO, Path.LINETO, Path.LINETO,
+                          Path.LINETO, Path.STOP])
+
+    simplified_path = path.cleaned(simplify=True)
+    assert_array_equal(expected_path.vertices, simplified_path.vertices)
+    assert_array_equal(expected_path.codes, simplified_path.codes)
