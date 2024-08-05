a46ecad34240487861bdf36980eb79c9adb850bd
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2024-08-05 11:46:08 +0200

    Rename _update_data to _update_ydata

--- galleries/examples/user_interfaces/embedding_in_qt_sgskip.py
+++ galleries/examples/user_interfaces/embedding_in_qt_sgskip.py
@@ -46,7 +46,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self._dynamic_ax = dynamic_canvas.figure.subplots()
         # Set up a Line2D.
         self.xdata = np.linspace(0, 10, 101)
-        self._update_data()
+        self._update_ydata()
         self._line, = self._dynamic_ax.plot(self.xdata, self.ydata)
         # The below two timers must be attributes of self, so that the garbage
         # collector won't clean them after we finish with __init__...
@@ -54,7 +54,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         # The data retrieval may be fast as possible (Using QRunnable could be
         # even faster).
         self.data_timer = dynamic_canvas.new_timer(1)
-        self.data_timer.add_callback(self._update_data)
+        self.data_timer.add_callback(self._update_ydata)
         self.data_timer.start()
         # Drawing at 50Hz should be fast enough for the GUI to feel smooth, and
         # not too fast for the GUI to be overloaded with events that need to be
@@ -63,7 +63,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.drawing_timer.add_callback(self._update_canvas)
         self.drawing_timer.start()
 
-    def _update_data(self):
+    def _update_ydata(self):
         # Shift the sinusoid as a function of time.
         self.ydata = np.sin(self.xdata + time.time())
 
