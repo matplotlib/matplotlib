50c671779c4efd5d63d1dd3f947a37d47c481d59
David Bakaj <106930686+dbakaj@users.noreply.github.com>
2024-08-13 17:15:28 +0100

    Fixed arrowstyle doc interpolation in FancyPatch.set_arrow() #28698. (#28704)
    * Fixed arrowstyle doc interpolation in FancyPatch.set_arrow() #28698.
    
    * Fixed arrowstyle doc interpolation in FancyPatch.set_arrow() #28698.

--- lib/matplotlib/patches.py
+++ lib/matplotlib/patches.py
@@ -4326,6 +4326,7 @@ default: 'arc3'
         """Return the `ConnectionStyle` used."""
         return self._connector
 
+    @_docstring.dedent_interpd
     def set_arrowstyle(self, arrowstyle=None, **kwargs):
         """
         Set the arrow style, possibly with further attributes.
