ed55f0994a9fdf5ea3205656d844e9180c65b517
anTon <138380708+r3kste@users.noreply.github.com>
2024-09-02 07:49:55 +0530

    fixed logic for inherit in collections

--- lib/matplotlib/collections.py
+++ lib/matplotlib/collections.py
@@ -174,7 +174,10 @@ class Collection(mcolorizer.ColorizingArtist):
         self._face_is_mapped = None
         self._edge_is_mapped = None
         self._mapped_colors = None  # calculated in update_scalarmappable
-        self._hatch_color = mcolors.to_rgba('black')
+        self._hatch_color = mpl.rcParams['hatch.color']
+        if self._hatch_color == 'inherit':
+            self._hatch_color = mpl.rcParams['patch.edgecolor']
+        self._hatch_color = mcolors.to_rgba(self._hatch_color)
         self._hatch_linewidth = mpl.rcParams['hatch.linewidth']
         self.set_facecolor(facecolors)
         self.set_edgecolor(edgecolors)
