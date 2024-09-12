a02506ff9503c695b426dcd758f94cc5bd9abde1
Moritz Wolter <moritz@wolter.tech>
2024-09-12 10:38:16 +0200

    add brackets to satisfy the new sequence requirement

--- galleries/examples/animation/frame_grabbing_sgskip.py
+++ galleries/examples/animation/frame_grabbing_sgskip.py
@@ -39,5 +39,5 @@ with writer.saving(fig, "writer_test.mp4", 100):
     for i in range(100):
         x0 += 0.1 * np.random.randn()
         y0 += 0.1 * np.random.randn()
-        l.set_data(x0, y0)
+        l.set_data([x0], [y0])
         writer.grab_frame()
