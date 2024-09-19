0fb6ac6524dc7704b318dd4e67ba08137ef7dc55
levinkhho <125771265+levinkhho@users.noreply.github.com>
2024-09-19 14:06:33 -0400

    Replaced deprecated QuadContourSet 'set' attribute

--- galleries/examples/misc/patheffect_demo.py
+++ galleries/examples/misc/patheffect_demo.py
@@ -29,7 +29,8 @@ arr = np.arange(25).reshape((5, 5))
 ax2.imshow(arr)
 cntr = ax2.contour(arr, colors="k")
 
-cntr.set(path_effects=[patheffects.withStroke(linewidth=3, foreground="w")])
+for collection in cntr.collections:
+    collection.set_path_effects([patheffects.withStroke(linewidth=3, foreground="w")])
 
 clbls = ax2.clabel(cntr, fmt="%2.0f", use_clabeltext=True)
 plt.setp(clbls, path_effects=[
