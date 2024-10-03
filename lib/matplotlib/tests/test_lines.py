1f7c55ba1a1378fa0230306bbba7669228e07f14
Antony Lee <anntzer.lee@gmail.com>
2024-10-03 13:43:18 +0200

    Switch AxLine.set_xy{1,2} to take a single argument.

--- lib/matplotlib/tests/test_lines.py
+++ lib/matplotlib/tests/test_lines.py
@@ -417,16 +417,20 @@ def test_axline_setters():
     line2 = ax.axline((.1, .1), (.8, .4))
     # Testing xy1, xy2 and slope setters.
     # This should not produce an error.
-    line1.set_xy1(.2, .3)
+    line1.set_xy1((.2, .3))
     line1.set_slope(2.4)
-    line2.set_xy1(.3, .2)
-    line2.set_xy2(.6, .8)
+    line2.set_xy1((.3, .2))
+    line2.set_xy2((.6, .8))
     # Testing xy1, xy2 and slope getters.
     # Should return the modified values.
     assert line1.get_xy1() == (.2, .3)
     assert line1.get_slope() == 2.4
     assert line2.get_xy1() == (.3, .2)
     assert line2.get_xy2() == (.6, .8)
+    with pytest.warns(mpl.MatplotlibDeprecationWarning):
+        line1.set_xy1(.2, .3)
+    with pytest.warns(mpl.MatplotlibDeprecationWarning):
+        line2.set_xy2(.6, .8)
     # Testing setting xy2 and slope together.
     # These test should raise a ValueError
     with pytest.raises(ValueError,
