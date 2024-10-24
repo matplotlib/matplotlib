masamenaza/git

Moved _set_hatch_linewidth to backend_bases

lib/matplotlib/backend_bases.pyi
@@ -168,6 +168,7 @@ class GraphicsContextBase:
     def set_hatch_color(self, hatch_color: ColorType) -> None: ...
     def get_hatch_linewidth(self) -> float: ...
     def set_hatch_linewidth(self, hatch_linewidth: float) -> None: ...
+    def _set_hatch_linewidth(self, hatch_linewidth: float) -> None: ...
     def get_sketch_params(self) -> tuple[float, float, float] | None: ...
     def set_sketch_params(
         self,

matplotlib/matplotlib

lib/matplotlib/backend_bases.pyi
@@ -167,6 +167,7 @@ class GraphicsContextBase:
     def get_hatch_color(self) -> ColorType: ...
     def set_hatch_color(self, hatch_color: ColorType) -> None: ...
     def get_hatch_linewidth(self) -> float: ...
+    def set_hatch_linewidth(self, hatch_linewidth: float) -> None: ...
     def get_sketch_params(self) -> tuple[float, float, float] | None: ...
     def set_sketch_params(
         self,