bdf5514fb0c3ee49cbb1cdf66834dab7a2c58c46
Victor Liu <vicliu2001@gmail.com>
2024-11-23 14:30:11 -0500

    added test cases for the modification to art3d.py

--- lib/mpl_toolkits/mplot3d/tests/test_art3d.py
+++ lib/mpl_toolkits/mplot3d/tests/test_art3d.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from matplotlib.backend_bases import MouseEvent
-from mpl_toolkits.mplot3d.art3d import Line3DCollection
+from mpl_toolkits.mplot3d.art3d import Line3DCollection, Poly3DCollection
 
 
 def test_scatter_3d_projection_conservation():
@@ -53,4 +53,33 @@ def test_zordered_error():
     ax = fig.add_subplot(projection="3d")
     ax.add_collection(Line3DCollection(lc))
     ax.scatter(*pc, visible=False)
+    plt.draw()
+
+
+def test_generate_normals():
+
+    # Following code is an example taken from
+    # https://stackoverflow.com/questions/18897786/transparency-for-poly3dcollection-plot-in-matplotlib
+    # and modified to test _generate_normals function
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    x = [0, 2, 1, 1]
+    y = [0, 0, 1, 0]
+    z = [0, 0, 0, 1]
+
+    # deliberately use nested tuple
+    vertices = ((0, 1, 2), (0, 1, 3), (0, 2, 3), (1, 2, 3))
+
+    tupleList = list(zip(x, y, z))
+
+    poly3d = [[tupleList[vertices[ix][iy]] for iy in range(len(vertices[0]))]
+              for ix in range(len(vertices))]
+    ax.scatter(x, y, z)
+    collection = Poly3DCollection(poly3d, alpha=0.2, edgecolors='r', shade=True)
+    face_color = [0.5, 0.5, 1]  # alternative: matplotlib.colors.rgb2hex([0.5, 0.5, 1])
+    collection.set_facecolor(face_color)
+    ax.add_collection3d(collection)
+
     plt.draw()
