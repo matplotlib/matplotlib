52c3f94d7ae650653ecccd6130bf08afcac3d74b
Eytan Adler <eytana@umich.edu>
2024-12-16 22:31:36 -0500

    Add test of inappropriate max step or error arguments

--- lib/matplotlib/tests/test_streamplot.py
+++ lib/matplotlib/tests/test_streamplot.py
@@ -156,8 +156,20 @@ def test_streamplot_grid():
     x = np.array([0, 20, 40])
     y = np.array([0, 20, 10])
 
-    with pytest.raises(ValueError, match="'y' must be strictly increasing"):
-        plt.streamplot(x, y, u, v)
+
+def test_streamplot_integration_params():
+    x = np.array([[10, 20], [10, 30]])
+    y = np.array([[10, 15], [20, 25]])
+    u = np.ones((2, 2))
+    v = np.zeros((2, 2))
+
+    err_str = "The value of integration_max_step must be > 0, got -0.5"
+    with pytest.raises(ValueError, match=err_str):
+        plt.streamplot(x, y, u, v, integration_max_step=-0.5)
+
+    err_str = "The value of integration_max_error must be > 0, got 0.0"
+    with pytest.raises(ValueError, match=err_str):
+        plt.streamplot(x, y, u, v, integration_max_error=0.0)
 
 
 def test_streamplot_inputs():  # test no exception occurs.
