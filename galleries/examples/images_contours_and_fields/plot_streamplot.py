0cd9bc2af956345ecc569a0eef9d3339721567d4
Eytan Adler <eytana@umich.edu>
2024-12-16 21:56:02 -0500

    Add arguments to streamplot to control integration max step and error

--- galleries/examples/images_contours_and_fields/plot_streamplot.py
+++ galleries/examples/images_contours_and_fields/plot_streamplot.py
@@ -14,6 +14,8 @@ example shows a few features of the `~.axes.Axes.streamplot` function:
 * Unbroken streamlines even when exceeding the limit of lines within a single
   grid cell.
 """
+import time
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -73,6 +75,91 @@ axs[6].set_aspect('equal')
 
 axs[7].streamplot(X, Y, U, V, broken_streamlines=False)
 axs[7].set_title('Streamplot with unbroken streamlines')
+
+plt.tight_layout()
+# plt.show()
+
+# %%
+# Streamline computation
+# ----------------------
+#
+# The streamlines are computed by integrating along the provided vector field
+# from the seed points, which are either automatically generated or manually
+# specified. The accuracy and smoothness of the streamlines can be adjusted using
+# the ``integration_max_step_scale`` and ``integration_max_error_scale`` optional
+# parameters. See the `~.axes.Axes.streamplot` function documentation for more
+# details.
+#
+# This example shows how adjusting the maximum allowed step size and error for
+# the integrator changes the appearance of the streamline. The differences can
+# be subtle, but can be observed particularly where the streamlines have
+# high curvature (as shown in the zoomed in region).
+
+# Linear potential flow over a lifting cylinder
+n = 50
+x, y = np.meshgrid(np.linspace(-2, 2, n), np.linspace(-3, 3, n))
+th = np.arctan2(y, x)
+r = np.sqrt(x**2 + y**2)
+vr = -np.cos(th) / r**2
+vt = -np.sin(th) / r**2 - 1 / r
+vx = vr * np.cos(th) - vt * np.sin(th) + 1.0
+vy = vr * np.sin(th) + vt * np.cos(th)
+
+# Seed points
+n_seed = 50
+seed_pts = np.column_stack((np.full(n_seed, -1.75), np.linspace(-2, 2, n_seed)))
+
+_, axs = plt.subplots(3, 1, figsize=(6, 14))
+th_circ = np.linspace(0, 2 * np.pi, 100)
+for ax, max_val in zip(axs, [0.05, 1, 5]):
+    ax_ins = ax.inset_axes([0.0, 0.7, 0.3, 0.35])
+    for ax_curr, is_inset in zip([ax, ax_ins], [False, True]):
+        t_start = time.time()
+        ax_curr.streamplot(
+            x,
+            y,
+            vx,
+            vy,
+            start_points=seed_pts,
+            broken_streamlines=False,
+            arrowsize=1e-10,
+            linewidth=2 if is_inset else 0.6,
+            color="k",
+            integration_max_step_scale=max_val,
+            integration_max_error_scale=max_val,
+        )
+        if is_inset:
+            t_total = time.time() - t_start
+
+        # Draw the cylinder
+        ax_curr.fill(
+            np.cos(th_circ),
+            np.sin(th_circ),
+            color="w",
+            ec="k",
+            lw=6 if is_inset else 2,
+        )
+
+        # Set axis properties
+        ax_curr.set_aspect("equal")
+
+    # Label properties of each circle
+    text = f"integration_max_step_scale: {max_val}\n" \
+        f"integration_max_error_scale: {max_val}\n" \
+        f"streamplot time: {t_total:.2f} sec"
+    if max_val == 1:
+        text += "\n(default)"
+    ax.text(0.0, 0.0, text, ha="center", va="center")
+
+    # Set axis limits and show zoomed region
+    ax_ins.set_xlim(-1.2, -0.7)
+    ax_ins.set_ylim(-0.8, -0.4)
+    ax_ins.set_yticks(())
+    ax_ins.set_xticks(())
+
+    ax.set_ylim(-1.5, 1.5)
+    ax.axis("off")
+    ax.indicate_inset_zoom(ax_ins, ec="k")
 
 plt.tight_layout()
 plt.show()
