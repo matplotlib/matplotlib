7299ae4b2d5c8eeb34f2873994e6bd8973b776b1
Stefan van der Walt <stefanv@berkeley.edu>
2024-12-17 16:16:52 -0800

    Tidy up Axes3D import

--- lib/matplotlib/figure.pyi
+++ lib/matplotlib/figure.pyi
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
-    import mpl_toolkits
+    from mpl_toolkits.mplot3d import Axes3D
 
 from collections.abc import Callable, Hashable, Iterable, Sequence
 import os
@@ -91,7 +91,7 @@ class FigureBase(Artist):
 
     # TODO: docstring indicates SubplotSpec a valid arg, but none of the listed signatures appear to be that
     @overload
-    def add_subplot(self, *args, projection: Literal["3d"], **kwargs) -> mpl_toolkits.mplot3d.Axes3D: ...
+    def add_subplot(self, *args, projection: Literal["3d"], **kwargs) -> Axes3D: ...
     @overload
     def add_subplot(
         self, nrows: int, ncols: int, index: int | tuple[int, int], **kwargs
