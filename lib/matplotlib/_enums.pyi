0189df79f81bf6a86eee2bba3d8bb5de13e0f5d9
Thomas A Caswell <tcaswell@gmail.com>
2024-12-20 15:52:11 -0500

    TYP: semantics of enums in stub files changed
    See https://typing.readthedocs.io/en/latest/spec/enums.html#defining-members
    for details

--- lib/matplotlib/_enums.pyi
+++ lib/matplotlib/_enums.pyi
@@ -1,18 +1,19 @@
+from typing import cast
 from enum import Enum
 
 class _AutoStringNameEnum(Enum):
     def __hash__(self) -> int: ...
 
 class JoinStyle(str, _AutoStringNameEnum):
-    miter: str
-    round: str
-    bevel: str
+    miter = cast(str, ...)
+    round = cast(str, ...)
+    bevel = cast(str, ...)
     @staticmethod
     def demo() -> None: ...
 
 class CapStyle(str, _AutoStringNameEnum):
-    butt: str
-    projecting: str
-    round: str
+    butt = cast(str, ...)
+    projecting = cast(str, ...)
+    round = cast(str, ...)
     @staticmethod
     def demo() -> None: ...
