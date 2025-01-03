0cf36120aed0c11c9d1cab7ba90bcd05bf12ffbb
landoskape <andrewtylerlandau@gmail.com>
2025-01-03 09:39:01 +0000

    Deprecate unused method

--- lib/matplotlib/colors.py
+++ lib/matplotlib/colors.py
@@ -259,6 +259,11 @@ def _has_alpha_channel(c):
     return False
 
 
+@_api.deprecated(
+    '3.11',
+    removal='3.14',
+    alternative="``use matplotlib.colors._has_alpha_channel`` with list comprehension"
+    )
 def _has_alpha_channel_array(cseq):
     """Return whether each element in *cseq* is a color with an alpha channel"""
     if is_color_like(cseq):
