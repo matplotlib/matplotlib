9bb047dffd8f5025769ff9305ba9e5b64a44a358
Andrew Landau <andrewtylerlandau@gmail.com>
2025-01-06 16:53:17 +0000

    Update for checking whether colors have an alpha channel (#27327)
    * Update _has_alpha_channel
    
    * Update lib/matplotlib/colors.py
    
    Co-authored-by: Oscar Gustafsson <oscar.gustafsson@gmail.com>
    
    * Clarify comment explanation
    
    * Remove unnecessary check about is_color_like
    
    * update docstring for undefined case
    
    * Deprecate unused method
    
    * Remove unnecessary method
    
    * Flake formatting update
    
    Co-authored-by: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
    
    * Don't specify what an undefined result does
    
    Co-authored-by: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
    
    * Recursive check which handles hex colors too
    
    Co-authored-by: Ruth Comer <10599679+rcomer@users.noreply.github.com>
    
    * Whitespace
    
    * Remove is_color_like check and explain
    
    Co-authored-by: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
    
    * Don't discuss undefined results
    
    Co-authored-by: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
    
    * Remove test of undefined result
    
    Co-authored-by: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
    
    * Spelling error
    
    Co-authored-by: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
    
    * improved grammar in comment
    
    ---------
    
    Co-authored-by: Oscar Gustafsson <oscar.gustafsson@gmail.com>
    Co-authored-by: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
    Co-authored-by: Ruth Comer <10599679+rcomer@users.noreply.github.com>

--- lib/matplotlib/colors.py
+++ lib/matplotlib/colors.py
@@ -234,9 +234,35 @@ def is_color_like(c):
 
 
 def _has_alpha_channel(c):
-    """Return whether *c* is a color with an alpha channel."""
-    # 4-element sequences are interpreted as r, g, b, a
-    return not isinstance(c, str) and len(c) == 4
+    """
+    Return whether *c* is a color with an alpha channel.
+
+    If *c* is not a valid color specifier, then the result is undefined.
+    """
+    # The following logic uses the assumption that c is a valid color spec.
+    # For speed and simplicity, we intentionally don't care about other inputs.
+    # Anything can happen with them.
+
+    # if c is a hex, it has an alpha channel when it has 4 (or 8) digits after '#'
+    if isinstance(c, str):
+        if c[0] == '#' and (len(c) == 5 or len(c) == 9):
+            # example: '#fff8' or '#0f0f0f80'
+            return True
+    else:
+        # if c isn't a string, it can be an RGB(A) or a color-alpha tuple
+        # if it has length 4, it has an alpha channel
+        if len(c) == 4:
+            # example: [0.5, 0.5, 0.5, 0.5]
+            return True
+
+        # if it has length 2, it's a color/alpha tuple
+        # if the second element isn't None or the first element has length = 4
+        if len(c) == 2 and (c[1] is not None or _has_alpha_channel(c[0])):
+            # example: ([0.5, 0.5, 0.5, 0.5], None) or ('r', 0.5)
+            return True
+
+    # otherwise it doesn't have an alpha channel
+    return False
 
 
 def _check_color_like(**kwargs):
