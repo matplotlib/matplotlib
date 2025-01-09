717ea248bd1cedda6e18336afa7879f4372e8324
Thomas A Caswell <tcaswell@gmail.com>
2025-01-09 13:41:32 -0500

    DOC: correct deepcopy docstring

--- lib/matplotlib/path.py
+++ lib/matplotlib/path.py
@@ -298,7 +298,7 @@ class Path:
 
     def deepcopy(self, memo=None):
         """
-        Return a shallow copy of the `Path`, which will share the
+        Return a deep copy of the `Path`, with copies of the
         vertices and codes with the source `Path`.
         """
         return copy.deepcopy(self, memo=memo)
