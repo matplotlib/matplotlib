7fcb06364e82ee7f6c833b86592439b5200f3c63
hu-xiaonan <152783907+hu-xiaonan@users.noreply.github.com>
2025-01-11 02:33:44 +0800

    fix #29410 Modifying Axes' position also alters the original Bbox object used for initialization (#29411)
    * fix #29410
    
    * Update test_axes_set_position_external_bbox_unchanged to use figure comparison

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -9610,3 +9610,14 @@ def test_bar_color_precedence():
     bars = ax.bar([31, 32, 33], [4, 5, 6], color='red', facecolor='green')
     for bar in bars:
         assert mcolors.same_color(bar.get_facecolor(), 'green')
+
+
+@check_figures_equal(extensions=['png'])
+def test_axes_set_position_external_bbox_unchanged(fig_test, fig_ref):
+    # From #29410: Modifying Axes' position also alters the original Bbox
+    # object used for initialization
+    bbox = mtransforms.Bbox([[0.0, 0.0], [1.0, 1.0]])
+    ax_test = fig_test.add_axes(bbox)
+    ax_test.set_position([0.25, 0.25, 0.5, 0.5])
+    assert (bbox.x0, bbox.y0, bbox.width, bbox.height) == (0.0, 0.0, 1.0, 1.0)
+    ax_ref = fig_ref.add_axes([0.25, 0.25, 0.5, 0.5])
