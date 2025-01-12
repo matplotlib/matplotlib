aeda076b445b5f62f0a7d228e9a7f2b9733863cf
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2025-01-12 09:53:10 +0100

    DOC: Add missing examples for legend outside positions
    Also made the background lighter, which IMHO looks more pleasing.

--- galleries/users_explain/axes/legend_guide.py
+++ galleries/users_explain/axes/legend_guide.py
@@ -175,7 +175,7 @@ fig.legend(loc='outside upper right')
 #
 ucl = ['upper', 'center', 'lower']
 lcr = ['left', 'center', 'right']
-fig, ax = plt.subplots(figsize=(6, 4), layout='constrained', facecolor='0.7')
+fig, ax = plt.subplots(figsize=(6, 4), layout='constrained', facecolor='0.95')
 
 ax.plot([1, 2], [1, 2], label='TEST')
 # Place a legend to the right of this smaller subplot.
@@ -188,12 +188,14 @@ for loc in [
         'outside lower right']:
     fig.legend(loc=loc, title=loc)
 
-fig, ax = plt.subplots(figsize=(6, 4), layout='constrained', facecolor='0.7')
+fig, ax = plt.subplots(figsize=(6, 4), layout='constrained', facecolor='0.95')
 ax.plot([1, 2], [1, 2], label='test')
 
 for loc in [
         'outside left upper',
         'outside right upper',
+        'outside left center',
+        'outside right center',
         'outside left lower',
         'outside right lower']:
     fig.legend(loc=loc, title=loc)
