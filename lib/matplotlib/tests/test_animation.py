6ff92ec80aed65f126be1ecbbf56ab1db6842097
Thomas A Caswell <tcaswell@gmail.com>
2025-01-16 20:43:51 -0500

    TST: drop quote in expected error string on windows for missing path

--- lib/matplotlib/tests/test_animation.py
+++ lib/matplotlib/tests/test_animation.py
@@ -520,7 +520,7 @@ def test_disable_cache_warning(anim):
 
 def test_movie_writer_invalid_path(anim):
     if sys.platform == "win32":
-        match_str = r"\[WinError 3] .*'\\\\foo\\\\bar\\\\aardvark'"
+        match_str = r"\[WinError 3] .*\\\\foo\\\\bar\\\\aardvark'"
     else:
         match_str = r"\[Errno 2] .*'/foo"
     with pytest.raises(FileNotFoundError, match=match_str):
