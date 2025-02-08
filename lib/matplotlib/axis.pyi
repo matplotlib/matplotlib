67e0226e25d77434f455c7637899e9b987157e73
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2025-02-08 02:38:33 +0100

    Update axis.pyi

--- lib/matplotlib/axis.pyi
+++ lib/matplotlib/axis.pyi
@@ -48,7 +48,7 @@ class Tick(martist.Artist):
         label2On: bool = ...,
         major: bool = ...,
         labelrotation: float = ...,
-        labelrotation_mode: Literal["default", "anchor", "xtick", "ytick"] = ...,
+        labelrotation_mode: Literal["default", "anchor", "xtick", "ytick"] | None = ...,
         grid_color: ColorType | None = ...,
         grid_linestyle: str | None = ...,
         grid_linewidth: float | None = ...,
