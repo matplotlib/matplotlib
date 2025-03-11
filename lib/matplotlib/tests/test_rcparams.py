2ed08c185c727f17a9b8d16ad8cb0c31cc48a0de
James Addison <jay@jp-hosting.net>
2025-03-11 12:47:31 +0000

    TST: coverage of backend fallback in headless mode
    Co-authored-by: Ian Thomas <ianthomas23@gmail.com>

--- lib/matplotlib/tests/test_rcparams.py
+++ lib/matplotlib/tests/test_rcparams.py
@@ -521,10 +521,11 @@ def test_rcparams_reset_after_fail():
 
 
 @pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
-def test_backend_fallback_headless(tmp_path):
+def test_backend_fallback_headless_invalid_backend(tmp_path):
     env = {**os.environ,
            "DISPLAY": "", "WAYLAND_DISPLAY": "",
            "MPLBACKEND": "", "MPLCONFIGDIR": str(tmp_path)}
+    # plotting should fail with the tkagg backend selected in a headless environment
     with pytest.raises(subprocess.CalledProcessError):
         subprocess_run_for_testing(
             [sys.executable, "-c",
@@ -534,6 +535,28 @@ def test_backend_fallback_headless(tmp_path):
              "matplotlib.pyplot.plot(42);"
              ],
             env=env, check=True, stderr=subprocess.DEVNULL)
+
+
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
+def test_backend_fallback_headless_auto_backend(tmp_path):
+    # specify a headless mpl environment, but request a graphical (tk) backend
+    env = {**os.environ,
+           "DISPLAY": "", "WAYLAND_DISPLAY": "",
+           "MPLBACKEND": "TkAgg", "MPLCONFIGDIR": str(tmp_path)}
+
+    # allow fallback to an available interactive backend explicitly in configuration
+    rc_path = tmp_path / "matplotlibrc"
+    rc_path.write_text("backend_fallback: true")
+
+    # plotting should succeed, by falling back to use the generic agg backend
+    backend = subprocess_run_for_testing(
+        [sys.executable, "-c",
+         "import matplotlib.pyplot;"
+         "matplotlib.pyplot.plot(42);"
+         "print(matplotlib.get_backend());"
+         ],
+        env=env, text=True, check=True, capture_output=True).stdout
+    assert backend.strip().lower() == "agg"
 
 
 @pytest.mark.skipif(
