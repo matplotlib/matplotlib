a18e072a13f40fe7fafc163eb0cf2d00800ae5d1
Antony Lee <anntzer.lee@gmail.com>
2025-03-31 10:16:28 +0200

    Switch Tfm metrics to TrueType-compatible API.
    Instead of directly exposing widths, heights, depths dicts, provide a
    get_metrics method to access a glyph's (tex) metrics.  This change is in
    preparation for {xe,lua}tex support, which would use an alternative
    metrics-loading class (`TtfMetrics`) where it would be excessive to load
    the metrics of all glyphs at once.

--- lib/matplotlib/dviread.pyi
+++ lib/matplotlib/dviread.pyi
@@ -1,3 +1,4 @@
+import dataclasses
 from pathlib import Path
 import io
 import os
@@ -68,13 +69,23 @@ class Vf(Dvi):
     def __init__(self, filename: str | os.PathLike) -> None: ...
     def __getitem__(self, code: int) -> Page: ...
 
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class TexMetrics:
+    tex_width: int
+    tex_height: int
+    tex_depth: int
+
 class Tfm:
     checksum: int
     design_size: int
-    width: dict[int, int]
-    height: dict[int, int]
-    depth: dict[int, int]
     def __init__(self, filename: str | os.PathLike) -> None: ...
+    def get_metrics(self, idx: int) -> TexMetrics | None: ...
+    @property
+    def width(self) -> dict[int, int]: ...
+    @property
+    def height(self) -> dict[int, int]: ...
+    @property
+    def depth(self) -> dict[int, int]: ...
 
 class PsFont(NamedTuple):
     texname: bytes
