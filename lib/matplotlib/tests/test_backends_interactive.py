b43b5688d4896017df874bbf1cd040443cbdcdc1
thomashopkins32 <thopkins1@bnl.gov>
2025-04-09 12:20:34 -0400

    Refactor test_cross_Qt_imports

--- lib/matplotlib/tests/test_backends_interactive.py
+++ lib/matplotlib/tests/test_backends_interactive.py
@@ -433,7 +433,8 @@ def _impl_test_cross_Qt_imports():
     host_app = host_qwidgets.QApplication(["mpl testing"])
     warnings.filterwarnings("error", message=r".*Mixing Qt major.*",
                             category=UserWarning)
-    matplotlib.backends.backend_qt._create_qApp()
+    with pytest.warns(UserWarning, match="Mixing Qt major versions may not work as expected."):
+        matplotlib.backends.backend_qt._create_qApp()
 
 
 def qt5_and_qt6_pairs():
@@ -460,17 +461,8 @@ def qt5_and_qt6_pairs():
     reason="$DISPLAY and $WAYLAND_DISPLAY are unset")
 @pytest.mark.parametrize('host, mpl', [*qt5_and_qt6_pairs()])
 def test_cross_Qt_imports(host, mpl):
-    try:
-        proc = _run_helper(_impl_test_cross_Qt_imports, host, mpl,
-                           timeout=_test_timeout)
-    except subprocess.CalledProcessError as ex:
-        # We do try to warn the user they are doing something that we do not
-        # expect to work, so we're going to ignore if the subprocess crashes or
-        # is killed, and just check that the warning is printed.
-        stderr = ex.stderr
-    else:
-        stderr = proc.stderr
-    assert "Mixing Qt major versions may not work as expected." in stderr
+    _run_helper(_impl_test_cross_Qt_imports, host, mpl,
+                        timeout=_test_timeout)
 
 
 @pytest.mark.skipif('TF_BUILD' in os.environ,
