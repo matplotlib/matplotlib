005904325a390ed630affe7d0ffaa24aa9aa885a
Hasan Rashid <hasanrashid@users.noreply.github.com>
2025-04-13 21:06:21 +0000

    Break line to reduce width in text_axes.py

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -4526,9 +4526,11 @@ def test_errorbar_linewidth_type(elinewidth):
     plt.errorbar([1, 2, 3], [1, 2, 3], yerr=[1, 2, 3], elinewidth=elinewidth)
 
 def test_errorbar_linestyle_type():
-    eb = plt.errorbar([1, 2, 3], [1, 2, 3], yerr=[1, 2, 3], elinestyle='-')
+    eb = plt.errorbar([1, 2, 3], [1, 2, 3], 
+                      yerr=[1, 2, 3], elinestyle='-')
     assert eb[-1][0].get_linestyle() == '-'
-    eb = plt.errorbar([1, 2, 3], [1, 2, 3], xerr=[1, 2, 3], yerr=[1, 2, 3], elinestyle=':')
+    eb = plt.errorbar([1, 2, 3], [1, 2, 3], xerr=[1, 2, 3], 
+                      yerr=[1, 2, 3], elinestyle=':')
     assert eb[-1][0].get_linestyle() == ':'
     assert eb[-1][1].get_linestyle() == ':'
 
