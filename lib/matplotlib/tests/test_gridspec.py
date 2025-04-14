ddcc0b4edf5a9eae078a131fad1db00950fa61fc
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-04-14 17:18:30 +0200

    increase coverage

--- lib/matplotlib/tests/test_gridspec.py
+++ lib/matplotlib/tests/test_gridspec.py
@@ -52,7 +52,6 @@ def test_SubplotParams():
         s = gridspec.SubplotParams(.1, .1, .09, .9)
 
 
-
 def test_repr():
     ss = gridspec.GridSpec(3, 3)[2, 1:3]
     assert repr(ss) == "GridSpec(3, 3)[2:3, 1:3]"
