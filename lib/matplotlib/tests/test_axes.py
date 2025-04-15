1995100077e1621a173b833ba3dd7d058721a9d5
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2025-04-15 05:34:59 -0400

    TST: Remove an extra space in test_hist_stacked_bar

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -4850,7 +4850,7 @@ def test_hist_stacked_bar():
     colors = [(0.5759849696758961, 1.0, 0.0), (0.0, 1.0, 0.350624650815206),
               (0.0, 1.0, 0.6549834156005998), (0.0, 0.6569064625276622, 1.0),
               (0.28302699607823545, 0.0, 1.0), (0.6849123462299822, 0.0, 1.0)]
-    labels = ['green', 'orange', ' yellow', 'magenta', 'black']
+    labels = ['green', 'orange', 'yellow', 'magenta', 'black']
     fig, ax = plt.subplots()
     ax.hist(d, bins=10, histtype='barstacked', align='mid', color=colors,
             label=labels)
