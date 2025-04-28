273a2fe36640f2229d85e574041c07170dc4898f
RogueRebel33 <charliethornton33@gmail.com>
2025-04-28 16:42:58 +0100

    Fix typo in DArrow drawing code

--- lib/matplotlib/patches.py
+++ lib/matplotlib/patches.py
@@ -2683,7 +2683,7 @@ class BoxStyle(_Style):
             # add padding to width and height
             width, height = width + 2 * pad, height + 2 * pad
             # boundary points of the padded box (arrow tail/body)
-            x0, y0 = x0 - pad, y0 - pad,
+            x0, y0 = x0 - pad, y0 - pad
             x1, y1 = x0 + width, y0 + height
             # half-width and quarter-width of arrow tail
             dx = (y1 - y0) / 2
