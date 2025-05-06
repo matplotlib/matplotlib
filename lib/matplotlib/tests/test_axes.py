334f61d1933d8d3242d4d12226e1f0095ed07d7d
albus-droid <albinbabuvarghese20@gmail.com>
2025-05-06 08:00:28 +0000

    Fixed linting issue using pre-commit

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -9748,6 +9748,7 @@ def test_pie_non_finite_values():
     with pytest.raises(ValueError, match='Wedge sizes must be finite numbers'):
         ax.pie(df, labels=['A', 'B', 'C'])
 
+
 def test_pie_all_zeros():
     fig, ax = plt.subplots()
     with pytest.raises(ValueError, match="All wedge sizes are zero"):
