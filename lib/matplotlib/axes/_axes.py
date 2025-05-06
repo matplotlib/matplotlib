91740933b658437016063f904030706d84b4502b
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2025-05-06 05:46:53 -0400

    FIX #30007: Raise ValueError when all wedge sizes are zero in ax.pie (#30019)
    * Added a condition that throws a ValueError if all wedges are zero
    
    * Removed "=" and added "==" for the condition statement
    
    * Added a test case with all wedges as zero
    
    * Fixed linting issue using pre-commit
    
    * Adding more linting changes missed by pre-commit
    
    * Running pre-commit hook again to remove the error

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -3308,6 +3308,9 @@ class Axes(_AxesBase):
 
         sx = x.sum()
 
+        if sx == 0:
+            raise ValueError('All wedge sizes are zero')
+
         if normalize:
             x = x / sx
         elif sx > 1:
