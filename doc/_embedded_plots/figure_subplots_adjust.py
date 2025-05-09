dd7f51b00956680ac768fea5fa51261c803f1548
ellie <e.walsh-oneill0220221@arts.ac.uk>
2025-05-09 14:53:49 +0100

    Corrected subplots_adjust diagram

--- doc/_embedded_plots/figure_subplots_adjust.py
+++ doc/_embedded_plots/figure_subplots_adjust.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+
 def arrow(p1, p2, **props):
     overlay.annotate(
         "", p1, p2, xycoords='figure fraction',
@@ -17,14 +18,14 @@ for ax in axs.flat:
 
 arrow((0, 0.75), (0.1, 0.75))  # left
 arrow((0.435, 0.25), (0.565, 0.25))  # wspace
-arrow((0.1, 0.8), (1, 0.8))  # right
+arrow((0, 0.8), (0.9, 0.8))  # right
 fig.text(0.05, 0.7, "left", ha="center")
 fig.text(0.5, 0.3, "wspace", ha="center")
-fig.text(0.95, 0.83, "right", ha="center")
+fig.text(0.05, 0.83, "right", ha="center")
 
 arrow((0.75, 0), (0.75, 0.1))  # bottom
 arrow((0.25, 0.435), (0.25, 0.565))  # hspace
-arrow((0.80, 0.1), (0.8, 1))  # top
+arrow((0.80, 0), (0.8, 0.9))  # top
 fig.text(0.65, 0.05, "bottom", va="center")
 fig.text(0.28, 0.5, "hspace", va="center")
-fig.text(0.75, 0.95, "top", va="center")
+fig.text(0.82, 0.05, "top", va="center")
