b98840be02ed00f397cd73fa3b03303b5ab06d23
Antony Lee <anntzer.lee@gmail.com>
2025-05-19 14:46:20 +0200

    Prepare for MetaFont/PK font support.
    TeX MetaFont (.mf) fonts (e.g., from `\usepackage{concmath}`) need to be
    converted to raster (.pk) fonts before usage.  In dvipng or pdftex this
    is done by invoking mktexpk via kpsewhich.
    
    This patch ensures that our calls to kpsewhich likewise also generate
    and finds the raster files (in Matplotlib's own temporary cache) when
    needed.  The resolution (600dpi) is the documented default (see e.g. the
    -D option in `man kpsewhich`).  Note that this is only a preliminary
    step towards full MF/PK font support, which would also require parsing
    the PK file and embedding the glyphs into our final output.

--- lib/matplotlib/mpl-data/kpsewhich.lua
+++ lib/matplotlib/mpl-data/kpsewhich.lua
@@ -1,3 +1,4 @@
 -- see dviread._LuatexKpsewhich
 kpse.set_program_name("latex")
-while true do print(kpse.lookup(io.read():gsub("\r", ""))); io.flush(); end
+kpse.init_prog("", 600, "ljfour")
+while true do print(kpse.lookup(io.read():gsub("\r", ""), {mktexpk=true})); io.flush(); end
