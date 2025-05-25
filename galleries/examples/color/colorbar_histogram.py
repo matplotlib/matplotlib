9931dba05d057e13825015c491c30d14e662fd02
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2025-05-25 17:50:02 +0000

    Refactor colorbar histogram example to use constrained layout and improve spacing

--- galleries/examples/color/colorbar_histogram.py
+++ galleries/examples/color/colorbar_histogram.py
@@ -12,6 +12,7 @@ visualizing the distribution of values mapped to colors.
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
+from matplotlib import gridspec
 
 # Generate random data
 x = np.random.random(100).reshape(10, 10)
@@ -23,21 +24,30 @@ counts, bins = np.histogram(x)
 cmap = plt.colormaps['viridis']
 norm = mcolors.BoundaryNorm(bins, cmap.N)
 
-fig, ax = plt.subplots()
-im = ax.imshow(x, cmap=cmap, norm=norm)
-cbar = plt.colorbar(im, ax=ax)
+# Create figure with constrained_layout for better spacing
+fig = plt.figure(figsize=(8, 4), constrained_layout=True)
+gs = gridspec.GridSpec(1, 3, width_ratios=[4, 0.2, 1], figure=fig)
+
+# Main image
+ax_img = fig.add_subplot(gs[0])
+im = ax_img.imshow(x, cmap=cmap, norm=norm)
+ax_img.set_title("Image")
+
+# Colorbar
+cax = fig.add_subplot(gs[1])
+cbar = plt.colorbar(im, cax=cax)
 cbar.set_label('Value')
 
-# Add histogram as an inset axis
-cax = ax.inset_axes([1.05, 0, .25, 1])
+# Histogram
+ax_hist = fig.add_subplot(gs[2])
 midpoints = bins[:-1] + np.diff(bins) / 2
-cax.barh(midpoints, counts, height=1/len(counts), color=cmap(norm(midpoints)))
-cax.set_yticks(bins)
-cax.margins(0)
-for spine in cax.spines.values():
+ax_hist.barh(midpoints, counts, height=np.diff(bins), color=cmap(norm(midpoints)), edgecolor='k')
+ax_hist.set_yticks(bins)
+ax_hist.set_xlabel('Count')
+ax_hist.set_ylabel('Value')
+ax_hist.margins(0)
+for spine in ax_hist.spines.values():
     spine.set_visible(False)
-cax.set_xlabel('Count')
-cax.set_ylabel('Value')
 
 plt.show()
 
