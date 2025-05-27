69a388feef64ef26d3f4c950946671bcfe6ba998
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2025-05-27 09:08:38 -0300

    Update colorbar_histogram.py
    Co-authored-by: hannah <story645@gmail.com>

--- galleries/examples/color/colorbar_histogram.py
+++ galleries/examples/color/colorbar_histogram.py
@@ -46,8 +46,7 @@ bar_height = 1 / len(counts)
 cax.barh(midpoints, counts, height=np.median(np.diff(bins))*0.8, color=cmap(norm(midpoints)))
 
 # === Clean up ===
-for spine in cax.spines.values():
-    spine.set_visible(False)
+cax.spines[:].set_visible(False)
 cax.margins(0)
 cax.tick_params(axis='both', which='both', length=0)
 
