a41d5460f0fa552bb51cbe46734625bdab12122c
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2025-06-06 19:38:25 +0000

    Changing comment style + simpler instructions

--- galleries/examples/color/colorbar_histogram.py
+++ galleries/examples/color/colorbar_histogram.py
@@ -8,38 +8,37 @@ add a histogram of the data values alongside it. This is useful for
 visualizing the distribution of values mapped to colors.
 
 """
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 import matplotlib.colors as mcolors
 
-# === Surface Data ===
+# Surface Data
 delta = 0.025
 x = y = np.arange(-2.0, 2.0, delta)
 X, Y = np.meshgrid(x, y)
-Z1 = np.exp(-((X+1)*1.3)**2 - ((Y+1)*1.3)**2)
-Z2 = 2.5*np.exp(-(X - 1)**2 - (Y - 1)**2)
-Z = (Z1**0.25 - Z2**0.5)
+Z1 = np.exp(-(((X + 1) * 1.3) ** 2) - ((Y + 1) * 1.3) ** 2)
+Z2 = 2.5 * np.exp(-((X - 1) ** 2) - (Y - 1) ** 2)
+Z = Z1**0.25 - Z2**0.5
 
-# === Histogram from actual Z data ===
+# Histogram from actual Z data
 counts, bins = np.histogram(Z, bins=30)
 
-# === Colormap & Normalization ===
-cmap = plt.get_cmap('RdYlBu_r')
+# Colormap & Normalization
+cmap = plt.get_cmap("RdYlBu_r")
 norm = mcolors.BoundaryNorm(bins, cmap.N)
 
-# === Main Plot ===
+# Main Plot
 fig, ax = plt.subplots(layout="constrained")
-im = ax.imshow(Z, cmap=cmap,
-               origin='lower', extent=[-3, 3, -3, 3],
-               norm=norm)
+im = ax.imshow(Z, cmap=cmap, origin="lower", extent=[-3, 3, -3, 3], norm=norm)
 
-# === Inset Histogram – Positioning adjusted ===
+# Inset Histogram
 cax = ax.inset_axes([1.18, 0.02, 0.25, 0.95])  # left, bottom, width, height
 
-# === Plot Histogram ===
-midpoints = midpoints = (bins[:-1] + bins[1:]) / 2
-height = np.median(np.diff(bins)) * 0.8
+# Plot Histogram
+midpoints = (bins[:-1] + bins[1:]) / 2
+
 colors = cmap(norm(midpoints))
 
 distance = midpoints[1] - midpoints[0]
@@ -48,10 +47,9 @@ cax.barh(midpoints, counts, height=0.8 * distance, color=cmap(norm(midpoints)))
 
 # styling
 cax.spines[:].set_visible(False)
-cax.margins(0)
-cax.tick_params(axis='both', which='both', length=0)
 
-# === Ticks ===
+# Ticks
 cax.set_yticks(bins)
+cax.tick_params(axis="both", which="both", length=0)
 
 plt.show()
