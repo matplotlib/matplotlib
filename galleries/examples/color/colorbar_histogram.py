080066412a52ddcd61f631a69d108ac4f1075d88
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2025-06-07 21:30:27 +0000

    Fix comment inconsistencies and remove unnecessary lines in colorbar_histogram.py

--- galleries/examples/color/colorbar_histogram.py
+++ galleries/examples/color/colorbar_histogram.py
@@ -6,7 +6,6 @@ Histogram as colorbar
 This example demonstrates how to use a colored histogram instead of a colorbar
 to not only show the color-to-value mapping, but also visualize the
 distribution of values.
-
 """
 
 import matplotlib.pyplot as plt
@@ -14,15 +13,13 @@ import numpy as np
 
 import matplotlib.colors as mcolors
 
-# surface Data
+# surface data
 delta = 0.025
 x = y = np.arange(-2.0, 2.0, delta)
 X, Y = np.meshgrid(x, y)
 Z1 = np.exp(-(((X + 1) * 1.3) ** 2) - ((Y + 1) * 1.3) ** 2)
 Z2 = 2.5 * np.exp(-((X - 1) ** 2) - (Y - 1) ** 2)
 Z = Z1**0.25 - Z2**0.5
-
-# histogram from actual Z data
 
 # colormap & normalization
 bins = 30
@@ -39,18 +36,13 @@ cax = ax.inset_axes([1.18, 0.02, 0.25, 0.95])  # left, bottom, width, height
 
 # plot histogram
 counts, bin_edges = np.histogram(Z, bins=bins)
-
 midpoints = (bin_edges[:-1] + bin_edges[1:]) / 2
-
 distance = midpoints[1] - midpoints[0]
-
 cax.barh(midpoints, counts, height=0.8 * distance, color=cmap(norm(midpoints)))
 
 # styling
 cax.spines[:].set_visible(False)
-
 cax.set_yticks(bin_edges)
-
 cax.tick_params(axis="both", which="both", length=0)
 
 plt.show()
