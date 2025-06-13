b02ed41eb521146db3921c624bf10d04f8c45189
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2025-06-14 00:13:13 +0200

    FIX: Ensure Locators on RadialAxis are always correctly wrapped
    All the wrapping logic is now contained in RadialAxis
    
    Closes #30164 and rearchitects #29798.

--- lib/matplotlib/tests/test_polar.py
+++ lib/matplotlib/tests/test_polar.py
@@ -3,8 +3,10 @@ from numpy.testing import assert_allclose
 import pytest
 
 import matplotlib as mpl
+from matplotlib.projections.polar import RadialLocator
 from matplotlib import pyplot as plt
 from matplotlib.testing.decorators import image_comparison, check_figures_equal
+import matplotlib.ticker as mticker
 
 
 @image_comparison(['polar_axes.png'], style='default', tol=0.012)
@@ -526,3 +528,43 @@ def test_radial_limits_behavior():
     # negative data also autoscales to negative limits
     ax.plot([1, 2], [-1, -2])
     assert ax.get_ylim() == (-2, 2)
+
+
+def test_radial_locator_wrapping():
+    # Check that the locator is always wrapped inside a RadialLocator
+    # and that RaidialAxis.isDefault_majloc is set correctly.
+    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+    assert ax.yaxis.isDefault_majloc
+    assert isinstance(ax.yaxis.get_major_locator(), RadialLocator)
+
+    # set an explicit locator
+    locator = mticker.MaxNLocator(3)
+    ax.yaxis.set_major_locator(locator)
+    assert not ax.yaxis.isDefault_majloc
+    assert isinstance(ax.yaxis.get_major_locator(), RadialLocator)
+    assert ax.yaxis.get_major_locator().base is locator
+
+    ax.clear()  # reset to the default locator
+    assert ax.yaxis.isDefault_majloc
+    assert isinstance(ax.yaxis.get_major_locator(), RadialLocator)
+
+    ax.set_rticks([0, 1, 2, 3])  # implicitly sets a FixedLocator
+    assert not ax.yaxis.isDefault_majloc  # because of the fixed ticks
+    assert isinstance(ax.yaxis.get_major_locator(), RadialLocator)
+    assert isinstance(ax.yaxis.get_major_locator().base, mticker.FixedLocator)
+
+    ax.clear()
+
+    ax.set_rgrids([0, 1, 2, 3])  # implicitly sets a FixedLocator
+    assert not ax.yaxis.isDefault_majloc  # because of the fixed ticks
+    assert isinstance(ax.yaxis.get_major_locator(), RadialLocator)
+    assert isinstance(ax.yaxis.get_major_locator().base, mticker.FixedLocator)
+
+    ax.clear()
+
+    ax.set_yscale("log")  # implicitly sets a LogLocator
+    # Note that the LogLocator is still considered the default locator
+    # for the log scale
+    assert ax.yaxis.isDefault_majloc
+    assert isinstance(ax.yaxis.get_major_locator(), RadialLocator)
+    assert isinstance(ax.yaxis.get_major_locator().base, mticker.LogLocator)
