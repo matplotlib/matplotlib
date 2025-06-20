54147e65bab7d2f85344cc46ddb2ee130819fdf0
RogueRebel33 <charliethornton33@gmail.com>
2025-06-20 16:12:17 +0100

    Added test for arrow-head adjustments

--- lib/matplotlib/tests/test_arrow_patches.py
+++ lib/matplotlib/tests/test_arrow_patches.py
@@ -49,6 +49,47 @@ def test_boxarrow():
                  bbox=dict(boxstyle=stylename, fc="w", ec="k"))
 
 
+@image_comparison(['boxarrow_adjustment_test_image.png'], style='mpl20')
+def test_boxarrow_adjustment():
+
+    styles = ['larrow', 'rarrow', 'darrow']
+
+    # Cases [head_width, head_angle] to test for each arrow style
+    cases = [
+        [1.5, 90],
+        [1.5, 170],     # Test dynamic padding
+        [0.75, 30],
+        [0.5, -10],     # Should just give a rectangle
+        [2, -90],
+        [2, -15]        # None of arrow body is outside of head
+    ]
+
+    # Horizontal and vertical spacings of arrow centres
+    spacing_horizontal = 3.5
+    spacing_vertical = 1.6
+
+    # Numbers of styles and cases
+    m = len(styles)
+    n = len(cases)
+
+    figwidth = (m * spacing_horizontal)
+    figheight = (n * spacing_vertical) + .5
+
+    fig = plt.figure(figsize=(figwidth / 1.5, figheight / 1.5))
+
+    fontsize = 0.3 * 72
+
+    for i, stylename in enumerate(styles):
+        for j, case in enumerate(cases):
+            # Draw arrow
+            fig.text(((m - i) * spacing_horizontal - 1.5) / figwidth,
+                ((n - j) * spacing_vertical - 0.5) / figheight,
+                stylename, ha='center', va='center',
+                size=fontsize, transform=fig.transFigure,
+                bbox=dict(boxstyle=f"{stylename}, head_width={case[0]}, \
+                    head_angle={case[1]}", fc="w", ec="k"))
+
+
 def __prepare_fancyarrow_dpi_cor_test():
     """
     Convenience function that prepares and returns a FancyArrowPatch. It aims
