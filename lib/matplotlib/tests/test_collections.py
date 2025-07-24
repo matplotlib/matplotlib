7e87cb27444eafae914b0ebfc728b03a36d887e3
Thomas A Caswell <tcaswell@gmail.com>
2025-07-24 14:11:42 -0400

    Backport PR #30327: FIX Update Axes limits from Axes.add_collection(... autolim=True)

--- lib/matplotlib/tests/test_collections.py
+++ lib/matplotlib/tests/test_collections.py
@@ -877,17 +877,24 @@ def test_collection_set_array():
 
 
 def test_blended_collection_autolim():
-    a = [1, 2, 4]
-    height = .2
-
-    xy_pairs = np.column_stack([np.repeat(a, 2), np.tile([0, height], len(a))])
-    line_segs = xy_pairs.reshape([len(a), 2, 2])
-
     f, ax = plt.subplots()
+
+    # sample data to give initial data limits
+    ax.plot([2, 3, 4], [0.4, 0.6, 0.5])
+    np.testing.assert_allclose((ax.dataLim.xmin, ax.dataLim.xmax), (2, 4))
+    data_ymin, data_ymax = ax.dataLim.ymin, ax.dataLim.ymax
+
+    # LineCollection with vertical lines spanning the Axes vertical, using transAxes
+    x = [1, 2, 3, 4, 5]
+    vertical_lines = [np.array([[xi, 0], [xi, 1]]) for xi in x]
     trans = mtransforms.blended_transform_factory(ax.transData, ax.transAxes)
-    ax.add_collection(LineCollection(line_segs, transform=trans))
-    ax.autoscale_view(scalex=True, scaley=False)
-    np.testing.assert_allclose(ax.get_xlim(), [1., 4.])
+    ax.add_collection(LineCollection(vertical_lines, transform=trans))
+
+    # check that the x data limits are updated to include the LineCollection
+    np.testing.assert_allclose((ax.dataLim.xmin, ax.dataLim.xmax), (1, 5))
+    # check that the y data limits are not updated (because they are not transData)
+    np.testing.assert_allclose((ax.dataLim.ymin, ax.dataLim.ymax),
+                               (data_ymin, data_ymax))
 
 
 def test_singleton_autolim():
