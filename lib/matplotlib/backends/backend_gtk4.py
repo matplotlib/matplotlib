c578ec6bdf1528efe60802eedf30a6b0523ff6cc
Thomas A Caswell <tcaswell@gmail.com>
2025-07-30 11:38:04 -0400

    FIX: do not add super().destroy to Canvas destroy method
    This goes up the chain to the GUI classes that do not have this method.

--- lib/matplotlib/backends/backend_gtk4.py
+++ lib/matplotlib/backends/backend_gtk4.py
@@ -90,7 +90,6 @@ class FigureCanvasGTK4(_FigureCanvasGTK, Gtk.DrawingArea):
 
     def destroy(self):
         CloseEvent("close_event", self)._process()
-        super().destroy()
 
     def set_cursor(self, cursor):
         # docstring inherited
