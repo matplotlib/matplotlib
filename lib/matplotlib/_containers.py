8438d060cef4d7e2ebc97901f4d28e83d2c6af5c
Kyle Sunden <git@ksunden.space>
2025-08-21 12:54:59 -0500

    WIP, lines containers decorator

---
+++ lib/matplotlib/_containers.py
@@ -0,0 +1,37 @@
+from mpl_data_containers.description import Desc, desc_like
+from mpl_data_containers.graph import Graph
+from mpl_data_containers.conversion_edge import TransformEdge
+
+
+def containerize_draw(draw_func):
+    def draw(self, renderer, *, graph=None):
+        if graph is None:
+            graph = Graph([])
+
+        ax = self.axes
+        if ax is None:
+            implicit_graph = Graph([])
+        else:
+            desc: Desc = Desc(("N",), coordinates="data")
+            xy: dict[str, Desc] = {"x": desc, "y": desc}
+            implicit_graph = Graph(
+                [
+                    TransformEdge(
+                        "data",
+                        xy,
+                        desc_like(xy, coordinates="axes"),
+                        transform=ax.transData - ax.transAxes,
+                    ),
+                    TransformEdge(
+                        "axes",
+                        desc_like(xy, coordinates="axes"),
+                        desc_like(xy, coordinates="display"),
+                        transform=ax.transAxes,
+                    ),
+                ],
+                aliases=(("parent", "axes"),),
+            )
+
+        return draw_func(self, renderer, graph=graph+implicit_graph)
+
+    return draw
