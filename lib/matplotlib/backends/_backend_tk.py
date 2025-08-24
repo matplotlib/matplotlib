6d1f6d3548dbe82aa3b63007a76ec59ed63ac4de
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2025-08-24 03:24:28 +0200

    whitespace fix

--- lib/matplotlib/backends/_backend_tk.py
+++ lib/matplotlib/backends/_backend_tk.py
@@ -39,8 +39,8 @@ except ImportError as e:
             raise ImportError(
                 "Failed to import tkagg backend. You appear to be using an outdated "
                 "version of uv's managed Python distribution which is not compatible "
-                " with Tk. Please upgrade to the latest uv version, then update "
-                " Python with: `uv python upgrade --reinstall`"
+                "with Tk. Please upgrade to the latest uv version, then update "
+                "Python with: `uv python upgrade --reinstall`"
                 ) from e
         else:
             raise ImportError(
