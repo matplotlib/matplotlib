913e52829834e9b284cc55ebf65bfb4b75c218ac
Aasma Gupta <aasmagupta1971@gmail.com>
2025-09-02 02:02:40 +0530

    Update lib/matplotlib/widgets.py
    Co-authored-by: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>

--- lib/matplotlib/widgets.py
+++ lib/matplotlib/widgets.py
@@ -2172,9 +2172,8 @@ class _SelectorWidget(AxesWidget):
         # `release` can call a draw event even when `ignore` is True.
         if not self.useblit:
             return
-        # Skip blitting if we are saving to disk or if the backend doesn’t support it
         if self.canvas.is_saving():
-            return
+            return  # saving does not use blitting
         # Make sure that widget artists don't get accidentally included in the
         # background, by re-rendering the background if needed (and then
         # re-re-rendering the canvas with the visible widget artists).
