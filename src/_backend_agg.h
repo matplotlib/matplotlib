faa6717ffa1a58f6ffbde2c459937f2e38ef3533
Melwyn Francis Carlo <66683108+melwyncarlo@users.noreply.github.com>
2025-09-19 19:19:14 +0530

    Edited path_converters.h

--- src/_backend_agg.h
+++ src/_backend_agg.h
@@ -511,12 +511,8 @@ inline void RendererAgg::draw_markers(GCAgg &gc,
 
     transformed_path_t marker_path_transformed(marker_path, marker_trans);
     nan_removed_t marker_path_nan_removed(marker_path_transformed, true, marker_path.has_codes());
-    e_snap_mode snap_mode = gc.snap_mode;
-    if (snap_mode == SNAP_AUTO) {
-        snap_mode = SNAP_TRUE;
-    }
     snap_t marker_path_snapped(marker_path_nan_removed,
-                               snap_mode,
+                               gc.snap_mode,
                                marker_path.total_vertices(),
                                points_to_pixels(gc.linewidth));
     curve_t marker_path_curve(marker_path_snapped);
