b843483d308ce1f8bb94e1449bdb2b77da9a327c
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2025-10-21 03:55:51 -0400

    Backport PR #30657: Fix `AttributeError: module 'gi' has no attribute 'require_version'`

--- lib/matplotlib/backends/backend_gtk4.py
+++ lib/matplotlib/backends/backend_gtk4.py
@@ -9,19 +9,20 @@ from matplotlib.backend_bases import (
     KeyEvent, LocationEvent, MouseEvent, ResizeEvent, CloseEvent)
 
 try:
-    import gi
+    from gi import require_version as gi_require_version
 except ImportError as err:
     raise ImportError("The GTK4 backends require PyGObject") from err
 
 try:
     # :raises ValueError: If module/version is already loaded, already
     # required, or unavailable.
-    gi.require_version("Gtk", "4.0")
+    gi_require_version("Gtk", "4.0")
 except ValueError as e:
     # in this case we want to re-raise as ImportError so the
     # auto-backend selection logic correctly skips.
     raise ImportError(e) from e
 
+import gi
 from gi.repository import Gio, GLib, Gtk, Gdk, GdkPixbuf
 from . import _backend_gtk
 from ._backend_gtk import (  # noqa: F401 # pylint: disable=W0611
