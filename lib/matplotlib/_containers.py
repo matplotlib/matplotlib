0982207ec2b5fa2dfef9a988e7fa0010a8a5c368
Kyle Sunden <git@ksunden.space>
2025-12-04 12:56:59 -0600

    Add DPI transform to auto generated graph

--- lib/matplotlib/_containers.py
+++ lib/matplotlib/_containers.py
@@ -14,24 +14,39 @@ def containerize_draw(draw_func):
         else:
             desc: Desc = Desc(("N",), coordinates="data")
             xy: dict[str, Desc] = {"x": desc, "y": desc}
-            implicit_graph = Graph(
-                [
-                    TransformEdge(
-                        "data",
-                        xy,
-                        desc_like(xy, coordinates="axes"),
-                        transform=ax.transData - ax.transAxes,
-                    ),
-                    TransformEdge(
-                        "axes",
-                        desc_like(xy, coordinates="axes"),
-                        desc_like(xy, coordinates="display"),
-                        transform=ax.transAxes,
-                    ),
-                ],
-                aliases=(("parent", "axes"),),
-            )
+            implicit_graph = _get_graph(ax)
 
         return draw_func(self, renderer, graph=graph+implicit_graph)
 
     return draw
+
+
+def _get_graph(ax):
+    if ax is None:
+        return Graph([])
+    desc: Desc = Desc(("N",), coordinates="data")
+    xy: dict[str, Desc] = {"x": desc, "y": desc}
+    implicit_graph = Graph(
+        [
+            TransformEdge(
+                "data",
+                xy,
+                desc_like(xy, coordinates="axes"),
+                transform=ax.transData - ax.transAxes,
+            ),
+            TransformEdge(
+                "axes",
+                desc_like(xy, coordinates="axes"),
+                desc_like(xy, coordinates="display"),
+                transform=ax.transAxes,
+            ),
+            TransformEdge(
+                "dpi",
+                desc_like(xy, coordinates="display_inches"),
+                desc_like(xy, coordinates="display"),
+                transform=ax.figure.dpi_scale_trans,
+            ),
+        ],
+        aliases=(("parent", "axes"),),
+    )
+    return implicit_graph
