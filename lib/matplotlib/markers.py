3db87c607903192e262403a761727d39ee46f15f
Hassan Kibirige <has2k1@gmail.com>
2025-12-11 19:22:26 +0300

    Do not fail when when markers are numpy integers

--- lib/matplotlib/markers.py
+++ lib/matplotlib/markers.py
@@ -308,7 +308,7 @@ class MarkerStyle:
         """
         if isinstance(marker, str) and cbook.is_math_text(marker):
             self._marker_function = self._set_mathtext_path
-        elif isinstance(marker, (int, str)) and marker in self.markers:
+        elif isinstance(marker, (int, str, np.integer)) and marker in self.markers:
             self._marker_function = getattr(self, '_set_' + self.markers[marker])
         elif (isinstance(marker, np.ndarray) and marker.ndim == 2 and
                 marker.shape[1] == 2):
