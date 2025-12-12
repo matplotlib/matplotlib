ea7f0bceb1a9a375e851d329d16bddbf76d71d0d
Kadyn <kbelesca@aol.com>
2025-12-12 16:57:00 -0500

    Updated backend_ps.py to call the proper function to get the font size

--- lib/matplotlib/generate_pstext.py
+++ lib/matplotlib/generate_pstext.py
@@ -3,10 +3,15 @@ import matplotlib.pyplot as plt
 plt.rcParams['ps.pathtext'] = True
 fig, ax = plt.subplots()
 <<<<<<< HEAD
+<<<<<<< HEAD
 ax.set_xlabel('Testing X')
 ax.set_ylabel('Testing Y')
 =======
 >>>>>>> 9eb51af777 (Implemented pytest for new feature)
+=======
+ax.set_xlabel('Testing X')
+ax.set_ylabel('Testing Y')
+>>>>>>> 723f9f5931 (Updated backend_ps.py to call the proper function)
 ax.text(0.25, 0.25, 'c')
 ax.text(0.25, 0.5, 'a')
 ax.text(0.25, 0.75, 'x')
