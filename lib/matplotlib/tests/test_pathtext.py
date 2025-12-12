f8e5be104662a5f2b200bb9b15c59aae84218a67
Kadyn <kbelesca@aol.com>
2025-12-12 16:23:52 -0500

    Fixed coding style

--- lib/matplotlib/tests/test_pathtext.py
+++ lib/matplotlib/tests/test_pathtext.py
@@ -2,15 +2,20 @@ import matplotlib.pyplot as plt
 from matplotlib.testing.decorators import image_comparison
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> 9eb51af777 (Implemented pytest for new feature)
+=======
+
+>>>>>>> 28390f61bd (Fixed coding style)
 @image_comparison(baseline_images=['text_as_path.eps'])
 def test_text_as_path_ps():
     plt.rcParams['ps.pathtext'] = True
     fig, ax = plt.subplots()
     ax.text(0.25, 0.25, 'c')
     ax.text(0.25, 0.5, 'a')
+<<<<<<< HEAD
 <<<<<<< HEAD
     ax.text(0.25, 0.75, 'x')
 <<<<<<< HEAD
@@ -21,3 +26,7 @@ def test_text_as_path_ps():
 =======
     ax.text(0.25, 0.75, 'x')
 >>>>>>> 08be6cf794 (Removed whitespace)
+=======
+    ax.text(0.25, 0.75, 'x')
+    
+>>>>>>> 28390f61bd (Fixed coding style)
