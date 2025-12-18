57ad96d45c23e070331fa99547cf79321eb7c3c6
Melwyn Francis Carlo <66683108+melwyncarlo@users.noreply.github.com>
2025-12-18 21:38:37 +0530

    Implement warning for Text3D's rotation/rotation_mode parameters (#30600)
    Co-authored-by: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>

--- lib/mpl_toolkits/mplot3d/art3d.py
+++ lib/mpl_toolkits/mplot3d/art3d.py
@@ -123,6 +123,16 @@ class Text3D(mtext.Text):
 
     def __init__(self, x=0, y=0, z=0, text='', zdir='z', axlim_clip=False,
                  **kwargs):
+        if 'rotation' in kwargs:
+            _api.warn_external(
+                "The `rotation` parameter has not yet been implemented "
+                "and is currently ignored."
+            )
+        if 'rotation_mode' in kwargs:
+            _api.warn_external(
+                "The `rotation_mode` parameter has not yet been implemented "
+                "and is currently ignored."
+            )
         mtext.Text.__init__(self, x, y, text, **kwargs)
         self.set_3d_properties(z, zdir, axlim_clip)
 
