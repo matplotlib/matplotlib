f8c47c6ac1b110c9faf614682c76780b46f2b557
Julian Chen <gapplef@gmail.com>
2025-12-25 10:34:16 +0800

    refine the test: handle single color for multiple datasets in hist

--- lib/matplotlib/tests/test_axes.py
+++ lib/matplotlib/tests/test_axes.py
@@ -2584,9 +2584,9 @@ def test_hist_single_color_multiple_datasets():
     data = [[0, 1, 2], [3, 4, 5]]
     _, _, bar_containers = plt.hist(data, color='k')
     for p in bar_containers[0].patches:
-        assert mcolor.same_color(p.get_facecolor(), 'k')
+        assert mcolors.same_color(p.get_facecolor(), 'k')
     for p in bar_containers[1].patches:
-        assert mcolor.same_color(p.get_facecolor(), 'k')
+        assert mcolors.same_color(p.get_facecolor(), 'k')
 
 
 def test_stairs_no_baseline_fill_warns():
