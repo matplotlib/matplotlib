9c412bd3c0abc86d53a407f38e08aa8324d1c972
Kavya Agrawal <ka7788158@gmail.com>
2026-01-03 15:28:30 +0530

    DOC: improve variable naming for clarity in simple line plot example
    Use more descriptive variable names to improve readability without changing
    the plot output.

--- galleries/examples/lines_bars_and_markers/simple_plot.py
+++ galleries/examples/lines_bars_and_markers/simple_plot.py
@@ -9,12 +9,12 @@ Create a basic line plot.
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Data for plotting
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2 * np.pi * t)
+# Data for plotting: time values and corresponding signal
+time = np.arange(0.0, 2.0, 0.01)
+signal = 1 + np.sin(2 * np.pi * time)
 
 fig, ax = plt.subplots()
-ax.plot(t, s)
+ax.plot(time, signal)
 
 ax.set(xlabel='time (s)', ylabel='voltage (mV)',
        title='About as simple as it gets, folks')
