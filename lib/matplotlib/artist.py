9a9722f6f381f86c2c7f4a427418f78a87b3e48d
Scott Shambaugh <scottshambaugh@users.noreply.github.com>
2026-02-02 11:53:16 -0700

    Simplify _cm_set fast path

--- lib/matplotlib/artist.py
+++ lib/matplotlib/artist.py
@@ -1294,22 +1294,11 @@ Supported properties are
         """
         `.Artist.set` context-manager that restores original values at exit.
         """
-        orig_vals = {}
-        for k, v in kwargs.items():
-            orig = getattr(self, f"get_{k}")()
-            try:
-                changed = orig != v
-                if hasattr(changed, 'any'):  # Handle numpy arrays
-                    changed = changed.any()
-            except ValueError:
-                changed = True
-            if changed:
-                orig_vals[k] = orig
-
-        if not orig_vals:  # Nothing actually changed, no need to update
+        if not kwargs:
             yield
             return
 
+        orig_vals = {k: getattr(self, f"get_{k}")() for k in kwargs}
         try:
             self._internal_update({k: kwargs[k] for k in orig_vals})
             yield
