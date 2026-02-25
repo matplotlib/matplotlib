371fcb29cd7a8ac71f613af30f0c7fcecc9dde68
Scott Shambaugh <14363975+scottshambaugh@users.noreply.github.com>
2026-02-24 21:50:15 -0700

    Update lib/matplotlib/text.py
    Co-authored-by: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>

--- lib/matplotlib/text.py
+++ lib/matplotlib/text.py
@@ -1063,8 +1063,8 @@ class Text(Artist):
     def get_tightbbox(self, renderer=None):
         # Exclude text at data coordinates outside the valid domain of the axes
         # scales (e.g., negative coordinates with a log scale).
-        if (self._outside_axes_domain(*self.get_unitless_position())
-                and self.get_transform() == self.axes.transData):
+        if (self.axes and self.get_transform() == self.axes.transData  # text is a data Artist
+                and self._outside_axes_domain(*self.get_unitless_position())):
             return Bbox.null()
         return super().get_tightbbox(renderer)
 
