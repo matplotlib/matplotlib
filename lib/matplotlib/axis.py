d8708bc24c73ad32128f90cebb00491f984691c0
Chirag3841 <chiragsharma23082005@gmail.com>
2026-03-11 12:53:09 +0530

    remove none fallback

--- lib/matplotlib/axis.py
+++ lib/matplotlib/axis.py
@@ -2175,17 +2175,16 @@ class Axis(martist.Artist):
         ticks = self.convert_units(ticks)
         locator = mticker.FixedLocator(ticks)  # validate ticks early.
         if len(ticks):
-            get_lim = getattr(self.axes, f"get_{self.axis_name}lim", None)
-            old_limits = get_lim() if get_lim is not None else None
+            get_lim = getattr(self.axes, f"get_{self.axis_name}lim")
+            old_limits = get_lim()
             for axis in self._get_shared_axis():
                 # set_view_interval maintains any preexisting inversion.
                 axis.set_view_interval(min(ticks), max(ticks))
-            if get_lim is not None:
-                new_limits = get_lim()
-                if new_limits != old_limits:
-                    self.axes.callbacks.process(
-                        f"{self.axis_name}lim_changed", self.axes
-                    )
+            new_limits = get_lim()
+            if new_limits != old_limits:
+                self.axes.callbacks.process(
+                    f"{self.axis_name}lim_changed", self.axes
+                )
         self.axes.stale = True
         if minor:
             self.set_minor_locator(locator)
