3503062d2d066cd5ffe0cd2fa2739cc69e67c11e
Akki-jaiswal <jaiswalakshay2709@gmail.com>
2026-03-11 06:51:42 +0530

    Doc: Improve Simple Plot styling and add legend

--- galleries/examples/lines_bars_and_markers/simple_plot.py
+++ galleries/examples/lines_bars_and_markers/simple_plot.py
@@ -9,19 +9,26 @@ Create a basic line plot.
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 # Data for plotting
 t = np.arange(0.0, 2.0, 0.01)
 s = 1 + np.sin(2 * np.pi * t)
 
+# Creating the figure and axis
 fig, ax = plt.subplots()
-ax.plot(t, s)
 
-ax.set(xlabel='time (s)', ylabel='voltage (mV)',
-       title='About as simple as it gets, folks')
-ax.grid()
+# Improvement: Explicitly setting line properties for better visibility
+ax.plot(t, s, color='tab:blue', linewidth=2.5, label='Sine wave')
 
-fig.savefig("test.png")
+# Improvement: Standardizing label formatting and adding a legend
+ax.set(xlabel='Time (s)', ylabel='Voltage (mV)',title='Basic Sine Wave Plot')
+
+# Improvement: Adding a styled grid for easier data reading
+# ax.grid(True, linestyle='--', alpha=0.7)
+# ax.legend()
+
 plt.show()
+
 
 # %%
 #
@@ -38,3 +45,6 @@ plt.show()
 #
 #    plot-type: line
 #    level: beginner
+
+# %%
+
