062e1421d73fd7d528818e903bc0f4678429ce59
Chirag3841 <chiragsharma23082005@gmail.com>
2026-03-12 08:29:28 +0530

    test file updated

--- lib/matplotlib/tests/test_axis.py
+++ lib/matplotlib/tests/test_axis.py
@@ -114,13 +114,15 @@ def test_grid_rcparams():
 
 
 def test_set_ticks_emits_lim_changed():
-    fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw=[{}, {"projection": "polar"}])
+    fig, ax1 = plt.subplots()
     ax1.set_xlim(0.5, 1)
     called_cartesian = []
     ax1.callbacks.connect("xlim_changed", called_cartesian.append)
     ax1.set_xticks([0, 100])
     assert called_cartesian
 
+    fig = plt.figure()
+    ax2 = fig.add_subplot(projection="polar")
     ax2.set_ylim(0.5, 1)
     called_polar = []
     ax2.callbacks.connect("ylim_changed", called_polar.append)
