9e770691142b6df7ee3a18841cad403895140f7e
Aryan Gore <aryangore130@gmail.com>
2026-03-19 09:13:27 +0530

    Fix indentation in SketchParams type caster

--- src/_backend_agg_basic_types.h
+++ src/_backend_agg_basic_types.h
@@ -223,7 +223,7 @@ namespace PYBIND11_NAMESPACE { namespace detail {
          
 
             auto params = src.cast<std::tuple<double, double, double, int>>();
-std::tie(value.scale, value.length, value.randomness, value.seed) = params;
+            std::tie(value.scale, value.length, value.randomness, value.seed) = params;
 
             return true;
         }
