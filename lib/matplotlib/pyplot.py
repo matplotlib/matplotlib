c1653cffde70916599fcf2b6bb0a66fdad213303
Vikash Kumar <163628932+Vikash-Kumar-23@users.noreply.github.com>
2026-03-31 05:36:11 +0530

    Update lib/matplotlib/pyplot.py
    Co-authored-by: Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>

--- lib/matplotlib/pyplot.py
+++ lib/matplotlib/pyplot.py
@@ -947,9 +947,8 @@ def figure(
           "px".
         - a tuple ``(x, y)``, which is interpreted as ``(x, y, "inch")``.
 
-        One of *width* or *height* may be ``None``; the missing value is taken
-        from :rc:`figure.figsize`. Passing ``(None, None)`` is not allowed and
-        raises a `ValueError`.
+        One of *width* or *height* may be ``None``; the respective value is taken
+        from :rc:`figure.figsize`.
 
     dpi : float, default: :rc:`figure.dpi`
         The resolution of the figure in dots-per-inch.
