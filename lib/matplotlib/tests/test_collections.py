c19bfe85534b7524b682316e1ea5d3b03b5d2d67
beelauuu <blau1@terpmail.umd.edu>
2026-05-02 12:28:58 -0400

    comment

--- lib/matplotlib/tests/test_collections.py
+++ lib/matplotlib/tests/test_collections.py
@@ -706,6 +706,9 @@ def test_set_wrong_linestyle():
 
 @pytest.mark.parametrize('ls', ['', ' ', 'none'])
 def test_scatter_empty_linestyle_pdf(ls):
+    # Regression test: '', ' ', and 'none' are documented "draw nothing"
+    # linestyle aliases but were not recognized by _get_dash_pattern, causing
+    # savefig to PDF to crash with "zero-size array to reduction operation maximum".
     plt.switch_backend('pdf')
     fig, ax = plt.subplots()
     ax.scatter([0, 1], [0, 1], ls=ls)
