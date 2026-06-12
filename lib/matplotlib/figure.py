62c8a544f99f745e94d47b68146812356f6597a2
Vikash Kumar <vikashsio793@gmail.com>
2026-06-12 16:26:24 +0530

    Finish pre_draw_event investigation

--- lib/matplotlib/figure.py
+++ lib/matplotlib/figure.py
@@ -3268,7 +3268,6 @@ None}, default: None
 
         with self._render_lock:
 
-            artists = self._get_draw_artists(renderer)
             try:
                 renderer.open_group('figure', gid=self.get_gid())
                 if self.axes and self.get_layout_engine() is not None:
@@ -3277,6 +3276,12 @@ None}, default: None
                     except ValueError:
                         pass
                         # ValueError can occur when resizing a window.
+
+                # Notify callbacks that a draw is about to happen, so they can
+                # modify the figure before rendering occurs.
+                DrawEvent("pre_draw_event", self.canvas, renderer)._process()
+
+                artists = self._get_draw_artists(renderer)
 
                 self.patch.draw(renderer)
                 mimage._draw_list_compositing_images(
