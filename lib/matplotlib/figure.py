4a3e2b50590c50c72072bf5fe536e9153419a5a1
Vikash Kumar <vikashsio793@gmail.com>
2026-06-13 19:35:02 +0530

    Restore _get_draw_artists ordering

--- lib/matplotlib/figure.py
+++ lib/matplotlib/figure.py
@@ -3268,7 +3268,6 @@ None}, default: None
 
         with self._render_lock:
 
-            artists = self._get_draw_artists(renderer)
             try:
                 renderer.open_group('figure', gid=self.get_gid())
                 if self.axes and self.get_layout_engine() is not None:
@@ -3278,6 +3277,7 @@ None}, default: None
                         pass
                         # ValueError can occur when resizing a window.
 
+                artists = self._get_draw_artists(renderer)
                 DrawEvent("pre_draw_event", self.canvas, renderer)._process()
 
                 self.patch.draw(renderer)
