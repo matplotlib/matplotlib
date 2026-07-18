f23f94f12dc367752ff598bc508466c0286c1e62
unknown <lol>
2026-07-18 14:15:50 -0500

    Revert "Narrow base class of exception that can be thrown in PyFT2Font_init"
    This reverts commit 6f228a3cdaeebc383c49f271b43c5384fe04d161.

--- src/ft2font_wrapper.cpp
+++ src/ft2font_wrapper.cpp
@@ -553,11 +553,9 @@ PyFT2Font_init(py::object filename, std::optional<long> hinting_factor = std::nu
             // 2. `read` raising an error.
             // 3. `read` returning something other than `bytes`.
             auto data = filename.attr("read")(0).cast<py::bytes>();
-        } catch (const py::builtin_exception &) {
-            // Only exception that can possibly be raised is `cast_error` from
-            // the .cast<py::bytes>() call.
+        } catch (const std::exception&) {
             throw py::type_error(
-                "First argument must be among the following types: str, bytes, os.PathLike, or io.BinaryIO");
+                "First argument must be a path to a font file or a binary-mode file object");
         }
         self->py_file = filename;
         self->stream.close = nullptr;
