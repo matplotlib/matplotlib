8968f2a46b9f3680123fab68fafb73057d3235d7
Ricci Adams <git@ricciadams.com>
2026-08-02 08:57:45 -0700

    Fix shim for test case failure

--- lib/matplotlib/backends/backend_macosx.py
+++ lib/matplotlib/backends/backend_macosx.py
@@ -1,21 +1,25 @@
-import sys
 from . import backend_legacymac as _legacymac
 from matplotlib.backend_bases import _Backend
 
-_RENAMES = {
-    'FigureCanvasMac': 'FigureCanvasLegacyMac',
-    'FigureManagerMac': 'FigureManagerLegacyMac',
-    'NavigationToolbar2': 'NavigationToolbar2LegacyMac'
-}
 
-_mod = sys.modules[__name__]
-for _new, _old in _RENAMES.items():
-    setattr(_mod, _new, getattr(_legacymac, _old))
-del _mod, _new, _old
+class TimerMac(_legacymac.TimerLegacyMac):
+    pass
+
+
+class FigureCanvasMac(_legacymac.FigureCanvasLegacyMac):
+    pass
+
+
+class FigureManagerMac(_legacymac.FigureManagerLegacyMac):
+    pass
+
+
+class NavigationToolbar2Mac(_legacymac.NavigationToolbar2LegacyMac):
+    pass
 
 
 @_Backend.export
 class _BackendMac(_Backend):
-    FigureCanvas = _legacymac.FigureCanvasLegacyMac
-    FigureManager = _legacymac.FigureManagerLegacyMac
+    FigureCanvas = FigureCanvasMac
+    FigureManager = FigureManagerMac
     mainloop = _legacymac.FigureManagerLegacyMac.start_main_loop
