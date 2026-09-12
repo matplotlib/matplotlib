678e3e009bb8f68c185a6d5f6716b9e1f66daa6e
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2026-09-12 15:10:18 +0200

    Backport PR #32344: TYP: PieContainer does not inherit from Container (cherry picked from commit a99bc61d82b76eab80509a57e39fa20a556d1985)

--- lib/matplotlib/container.pyi
+++ lib/matplotlib/container.pyi
@@ -53,7 +53,7 @@ class ErrorbarContainer(Container):
         **kwargs
     ) -> None: ...
 
-class PieContainer(Container):
+class PieContainer:
     wedges: list[Wedge]
     def __init__(
         self,
@@ -70,6 +70,7 @@ class PieContainer(Container):
     def add_texts(self,
         texts: list[Text],
     ) -> None: ...
+    def remove(self) -> None: ...
 
 class StemContainer(Container):
     markerline: Line2D
