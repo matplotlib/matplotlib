8e46532d681743ee8e9f7cc5fa976a8a36582d38
Roberto Moura <roberto@ffmoura.com>
2026-09-16 19:46:54 +0100

    Convert indentation to spaces

--- lib/matplotlib/tests/test_pie.py
+++ lib/matplotlib/tests/test_pie.py
@@ -2,25 +2,25 @@ import matplotlib.pyplot as plt
 
 
 def test_pie_remove():
-	fig, ax = plt.subplots()
-	pie = ax.pie([2, 3], wedge_labels=['foo', 'bar'], autopct="%1.0f%%")
-	ax.pie_label(pie, ['baz', 'qux'])
+    fig, ax = plt.subplots()
+    pie = ax.pie([2, 3], wedge_labels=['foo', 'bar'], autopct="%1.0f%%")
+    ax.pie_label(pie, ['baz', 'qux'])
 
-	assert len(ax.patches) == 0
-	assert len(ax.texts) == 0
-	assert pie in ax._children
+    assert len(ax.patches) == 0
+    assert len(ax.texts) == 0
+    assert pie in ax._children
 
-	pie.remove()
-	assert pie not in ax._children
-	assert not ax.texts
+    pie.remove()
+    assert pie not in ax._children
+    assert not ax.texts
 
 
 def test_pie_unpack_backcompat():
-	fig, ax = plt.subplots()
-	wedges, texts, autotexts = ax.pie(
-		[2, 3], labels=['foo', 'bar'], autopct="%1.0f%%", labeldistance=None)
+    fig, ax = plt.subplots()
+    wedges, texts, autotexts = ax.pie(
+        [2, 3], labels=['foo', 'bar'], autopct="%1.0f%%", labeldistance=None)
 
-	assert len(wedges) == 2
-	assert isinstance(texts, list)
-	assert not texts
-	assert len(autotexts) == 2
+    assert len(wedges) == 2
+    assert isinstance(texts, list)
+    assert not texts
+    assert len(autotexts) == 2
