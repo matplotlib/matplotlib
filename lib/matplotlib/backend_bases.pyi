8925298270c2a573a1df12a034f73d9a368c7608
Vikash Kumar <vikashsio793@gmail.com>
2026-09-25 08:06:20 +0530

    TYP: Add supports_layers to FigureCanvasBase stub in backend_bases.pyi

--- lib/matplotlib/backend_bases.pyi
+++ lib/matplotlib/backend_bases.pyi
@@ -314,6 +314,7 @@ class FigureCanvasBase:
 
     @_api.classproperty
     def supports_blit(cls) -> bool: ...
+    supports_layers: bool
 
     figure: Figure
     manager: None | FigureManagerBase
